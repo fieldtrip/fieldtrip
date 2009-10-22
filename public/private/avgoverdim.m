850244c5862ac147af2b646444aab0620eb3b132
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2009-10-22 10:20:57 +0000

    first commitment into svn
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ public/private/avgoverdim.m
@@ -0,0 +1,46 @@
+function data = avgoverdim(data, avgdim)
+
+dimtok    = tokenize(data.dimord, '_');
+avgdimnum = find(strcmp(avgdim, dimtok)); % the selected dimension as number
+
+if length(avgdimnum)<1
+  error('the "%s" dimension is not present in the data', avgdim)
+elseif length(avgdimnum)>1
+  error('cannot average over multiple dimensions at the same time')
+end
+
+reduceddim = dimlength(data);
+reduceddim(avgdimnum) = 1;
+
+param = selparam(data);
+for i=1:length(param)
+  fprintf('averaging %s over %s\n', param{i}, avgdim);
+  tmp = data.(param{i});
+  tmp = reshape(nanmean(tmp, avgdimnum), reduceddim);
+  data.(param{i}) = tmp;
+end
+
+switch avgdim
+  case 'rpt'
+    for i=1:length(param)
+      fprintf('removing dimension %s from %s\n', avgdim, param{i});
+      tmp = data.(param{i});
+      tmp = reshape(tmp, reduceddim(2:end));
+      data.(param{i}) = tmp;
+    end
+    data.dimord = '';
+    for i=2:length(dimtok)
+      data.dimord = [data.dimord,'_',dimtok{i}];
+    end
+    data.dimord = data.dimord(2:end);
+  case 'rpttap'
+    % nothing to do
+  case 'chan'
+    data.label = avgoverlabel(data.label);
+  case 'freq'
+    data.freq = mean(data.freq);
+  case 'time'
+    data.time = mean(data.time);
+  otherwise
+    error('unknown dimension "%s"', avgdim);
+end
