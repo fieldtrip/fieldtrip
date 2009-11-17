804b255d57384ef4df2639a44c84f8678a05ee6b
Cristiano Micheli <michelic72@googlemail.com>
2009-11-17 15:27:55 +0000

    added freezeColors for multiple funtional colorbar
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@136 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/plot_slice.m
+++ plotting/plot_slice.m
@@ -12,6 +12,7 @@ function plot_slice(data,varargin)
 %                       'auto', full range of data
 %                       [min max], coordinates of first and last slice in voxels
 %   'slicedim'      = dimension to slice 1 (x-axis) 2(y-axis) 3(z-axis) (default = 3)
+%   'sliceindex'    = progressive integer index of the slice (1..N) in the selected dimension
 %   'title'         = string, title of the figure window
 %   'colorbar'      = 'yes' or 'no' (default = 'yes')
 %   'map'           = colormap assigned to the slices (default='gray')
@@ -31,8 +32,9 @@ function plot_slice(data,varargin)
 slicerange    = keyval('slicerange',  varargin); if isempty(slicerange),slicerange='auto'; end
 nslices       = keyval('nslices',  varargin);  if isempty(nslices),nslices=4; end
 slicedim      = keyval('slicedim',  varargin);  if isempty(slicedim),slicedim=3; end
+sliceindex    = keyval('sliceindex',  varargin); if isempty(sliceindex),sliceindex=[]; end
 title_        = keyval('title',  varargin);  if isempty(title_),title_=''; end
-colorbar1     = keyval('colorbar',  varargin);  if isempty(colorbar1),colorbar1='yes'; end
+colorbar1     = keyval('colorbar',  varargin);  if isempty(colorbar1),colorbar1='no'; end
 funparameter  = keyval('funparameter',  varargin);  if isempty(funparameter),funparameter=[]; end
 anaparameter  = keyval('anaparameter',  varargin);  if isempty(anaparameter),anaparameter='anatomy'; end
 maskparameter = keyval('maskparameter',  varargin);  if isempty(maskparameter),maskparameter=[]; end
@@ -53,6 +55,7 @@ if ~isempty(funparameter)
 else
   hasfun = 0;
   fprintf('no functional parameter\n');
+  fun = [];
 end
 if hasfun
   handle_fun(fun)
@@ -100,181 +103,61 @@ if ~isempty(maskparameter)
 else
   hasmsk = 0;
   fprintf('no masking parameter\n');
+  msk = [];
 end
 if hasmsk
   handle_msk(msk);
 end
 
  
-  %%%%% select slices
-  ss = setdiff([1 2 3], slicedim);
-  
-  if ~isstr(slicerange)
-    ind_fslice = slicerange(1);
-    ind_lslice = slicerange(2);
-  elseif isequal(slicerange, 'auto')
-    if hasfun %default
-      if isfield(data,'inside')
-        ind_fslice = min(find(max(max(data.inside,[],ss(1)),[],ss(2))));
-        ind_lslice = max(find(max(max(data.inside,[],ss(1)),[],ss(2))));
-      else
-        ind_fslice = min(find(~isnan(max(max(fun,[],ss(1)),[],ss(2)))));
-        ind_lslice = max(find(~isnan(max(max(fun,[],ss(1)),[],ss(2)))));
-      end
-    elseif hasana %if only ana, no fun
-      ind_fslice = min(find(max(max(ana,[],ss(1)),[],ss(2))));
-      ind_lslice = max(find(max(max(ana,[],ss(1)),[],ss(2))));
+%%%%% select slices
+ss = setdiff([1 2 3], slicedim);
+
+if ~isstr(slicerange)
+  ind_fslice = slicerange(1);
+  ind_lslice = slicerange(2);
+elseif isequal(slicerange, 'auto')
+  if hasfun %default
+    if isfield(data,'inside')
+      ind_fslice = min(find(max(max(data.inside,[],ss(1)),[],ss(2))));
+      ind_lslice = max(find(max(max(data.inside,[],ss(1)),[],ss(2))));
     else
-      error('no functional parameter and no anatomical parameter, can not plot');
+      ind_fslice = min(find(~isnan(max(max(fun,[],ss(1)),[],ss(2)))));
+      ind_lslice = max(find(~isnan(max(max(fun,[],ss(1)),[],ss(2)))));
     end
+  elseif hasana %if only ana, no fun
+    ind_fslice = min(find(max(max(ana,[],ss(1)),[],ss(2))));
+    ind_lslice = max(find(max(max(ana,[],ss(1)),[],ss(2))));
   else
-    error('do not understand slicerange');
+    error('no functional parameter and no anatomical parameter, can not plot');
   end
-  
-  if nslices==1
-    ind_allslice = (ind_fslice+ind_lslice)/2;
-  elseif nslices==2
-    ind_allslice = [ind_fslice+(ind_lslice-ind_fslice)/5 ind_fslice+4*(ind_lslice-ind_fslice)/5];
-  else
-    ind_allslice = linspace(ind_fslice,ind_lslice,nslices);
-  end
-  ind_allslice = round(ind_allslice);
-  
-  
-  % if i want to plot a 2D representation of several slices
-  if flat2D 
+else
+  error('do not understand slicerange');
+end
 
-    % make new ana, fun, msk, mskana with only the slices that will be plotted (slice dim is always third dimension)
-    if slicedim == 3 
-      if hasana; new_ana = ana(:,:,ind_allslice); clear ana; ana=new_ana; clear new_ana; end;
-      if hasfun; new_fun = fun(:,:,ind_allslice); clear fun; fun=new_fun; clear new_fun; end;
-      if hasmsk; new_msk = msk(:,:,ind_allslice); clear msk; msk=new_msk; clear new_msk; end;
-    elseif slicedim == 2
-      if hasana; new_ana = ana(:,ind_allslice,:); clear ana; ana=new_ana; clear new_ana; end;
-      if hasfun; new_fun = fun(:,ind_allslice,:); clear fun; fun=new_fun; clear new_fun; end;
-      if hasmsk; new_msk = msk(:,ind_allslice,:); clear msk; msk=new_msk; clear new_msk; end;    
-    elseif slicedim == 1
-      if hasana; new_ana = ana(ind_allslice,:,:); clear ana; ana=new_ana; clear new_ana; end;
-      if hasfun; new_fun = fun(ind_allslice,:,:); clear fun; fun=new_fun; clear new_fun; end;
-      if hasmsk; new_msk = msk(ind_allslice,:,:); clear msk; msk=new_msk; clear new_msk; end;
-    else
-      error('Error: incorrect slice dimension specification')
-    end
-    %if hasmskana; new_mskana = mskana(:,:,ind_allslice); clear mskana; mskana=new_mskana; clear new_mskana; end;
+if nslices==1
+  ind_allslice = (ind_fslice+ind_lslice)/2;
+elseif nslices==2
+  ind_allslice = [ind_fslice+(ind_lslice-ind_fslice)/5 ind_fslice+4*(ind_lslice-ind_fslice)/5];
+else
+  ind_allslice = linspace(ind_fslice,ind_lslice,nslices);
+end
+ind_allslice = round(ind_allslice);
 
-    % update the dimensions of the volume
-    if hasana; dim=size(ana); else dim=size(fun); end;
 
-    %%%%% make "quilts", that contain all slices on 2D patched sheet
-    % Number of patches along sides of Quilt (M and N)
-    % Size (in voxels) of side of patches of Quilt (m and n)
-
-    if slicedim == 3 
-      m = dim(1);
-      n = dim(2);
-      if length(dim)==2
-        dim(3) = 1;
-      end
-      M = ceil(sqrt(dim(3)));
-      N = ceil(sqrt(dim(3)));
-    elseif slicedim == 2 
-      m = dim(1);
-      n = dim(3);    
-      M = ceil(sqrt(dim(2)));
-      N = ceil(sqrt(dim(2)));   
-    elseif slicedim == 1
-      m = dim(2);
-      n = dim(3);    
-      M = ceil(sqrt(dim(1)));
-      N = ceil(sqrt(dim(1)));     
-    end
-
-    num_patch = N*M;
-  %   if slicedim~=3
-  %     error('only supported for slicedim=3');
-  %   end
-    num_slice = (dim(slicedim));
-    num_empt = num_patch-num_slice;
-
-    % put empty slides on ana, fun, msk, mskana to fill Quilt up
-    if slicedim == 3 
-      if hasana; ana(:,:,end+1:num_patch)=0; end;
-      if hasfun; fun(:,:,end+1:num_patch)=0; end;
-      if hasmsk; msk(:,:,end+1:num_patch)=0; end;
-    elseif slicedim == 2
-      if hasana; ana(:,end+1:num_patch,:)=0; end;
-      if hasfun; fun(:,end+1:num_patch,:)=0; end;
-      if hasmsk; msk(:,end+1:num_patch,:)=0; end;    
-    elseif slicedim == 1
-      if hasana; ana(end+1:num_patch,:,:)=0; end;
-      if hasfun; fun(end+1:num_patch,:,:)=0; end;
-      if hasmsk; msk(end+1:num_patch,:,:)=0; end;   
-    end
-
-    %if hasmskana; mskana(:,:,end:num_patch)=0; end;
-    % put the slices in the quilt
-    for iSlice = 1:num_slice
-      xbeg = floor((iSlice-1)./M);
-      ybeg = mod(iSlice-1, M);
-      if slicedim == 3
-        if hasana
-          quilt_ana(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(ana(:,:,iSlice));
-        end
-        if hasfun
-          quilt_fun(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(fun(:,:,iSlice));
-        end
-        if hasmsk
-          quilt_msk(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(msk(:,:,iSlice));
-        end
-      elseif slicedim == 2
-        if hasana
-          quilt_ana(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(ana(:,iSlice,:));
-        end
-        if hasfun
-          quilt_fun(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(fun(:,iSlice,:));
-        end
-        if hasmsk
-          quilt_msk(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(msk(:,iSlice,:));
-        end      
-      elseif slicedim == 1
-        if hasana
-          quilt_ana(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(ana(iSlice,:,:));
-        end
-        if hasfun
-          quilt_fun(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(fun(iSlice,:,:));
-        end
-        if hasmsk
-          quilt_msk(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(msk(iSlice,:,:));
-        end      
-      end
-
-      %     if hasmskana
-      %       quilt_mskana(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(mskana(:,:,iSlice));
-      %     end
-    end
-    % make vols and scales, containes volumes to be plotted (fun, ana, msk) %added ingnie
-    if hasana; vols2D{1} = quilt_ana; scales{1} = []; end; % needed when only plotting ana
-    if hasfun; vols2D{2} = quilt_fun; scales{2} = [fcolmin fcolmax]; end;
-    if hasmsk; vols2D{3} = quilt_msk; scales{3} = [opacmin opacmax]; end;
-
-    plot2D(vols2D, scales);
-    axis off
-
-    if strcmp(colorbar1,  'yes'),
-      if hasfun
-        % use a normal Matlab colorbar
-        hc = colorbar;
-        set(hc, 'YLim', [fcolmin fcolmax]);
-      else
-        warning('no colorbar possible without functional data')
-      end
-    end
-    
-  else
+% if i want to plot a 2D representation of several slices
+if flat2D 
+    plot_flatslice(hasana,hasfun,hasmsk,ana,fun,msk,slicedim,ind_allslice,colorbar1)
+else
+  if isempty(sliceindex)
     plot_slice_sub(ana,slicedim,ind_allslice,map,transform); 
-  end
-   
-  if ~isempty(title_), title(title_); end
+  else
+    plot_slice_sub(ana,slicedim,sliceindex,map,transform);
+  end 
+end
+if ~isempty(title_), title(title_); end
+
+
 
 function plot_slice_sub(data,slicedim,ind_allslice,map,transform) 
 if ~ishold, hold on, end
@@ -289,7 +172,7 @@ ijk = [I(:) J(:) K(:) ones(prod(ds),1)]';
 
 % determine location of each anatomical voxel in head coordinates
 xyz = transform * ijk;
-xyz = permute(xyz,[2 1 3]);
+% xyz = permute(xyz,[2 1 3]);
 xdata = reshape(xyz(:,1), [ds(2) ds(1) ds(3)]);
 ydata = reshape(xyz(:,2), [ds(2) ds(1) ds(3)]);
 zdata = reshape(xyz(:,3), [ds(2) ds(1) ds(3)]);
@@ -480,4 +363,132 @@ function handle_msk(msk)
     end
   end % handling opacitylim and opacitymap
   clear mskmin mskmax;
+    
+function plot_flatslice(hasana,hasfun,hasmsk,ana,fun,msk,slicedim,ind_allslice,colorbar1)
+    
+% make new ana, fun, msk, mskana with only the slices that will be plotted (slice dim is always third dimension)
+if slicedim == 3
+  if hasana; new_ana = ana(:,:,ind_allslice); clear ana; ana=new_ana; clear new_ana; end;
+  if hasfun; new_fun = fun(:,:,ind_allslice); clear fun; fun=new_fun; clear new_fun; end;
+  if hasmsk; new_msk = msk(:,:,ind_allslice); clear msk; msk=new_msk; clear new_msk; end;
+elseif slicedim == 2
+  if hasana; new_ana = ana(:,ind_allslice,:); clear ana; ana=new_ana; clear new_ana; end;
+  if hasfun; new_fun = fun(:,ind_allslice,:); clear fun; fun=new_fun; clear new_fun; end;
+  if hasmsk; new_msk = msk(:,ind_allslice,:); clear msk; msk=new_msk; clear new_msk; end;
+elseif slicedim == 1
+  if hasana; new_ana = ana(ind_allslice,:,:); clear ana; ana=new_ana; clear new_ana; end;
+  if hasfun; new_fun = fun(ind_allslice,:,:); clear fun; fun=new_fun; clear new_fun; end;
+  if hasmsk; new_msk = msk(ind_allslice,:,:); clear msk; msk=new_msk; clear new_msk; end;
+else
+  error('Error: incorrect slice dimension specification')
+end
+%if hasmskana; new_mskana = mskana(:,:,ind_allslice); clear mskana; mskana=new_mskana; clear new_mskana; end;
+
+% update the dimensions of the volume
+if hasana; dim=size(ana); else dim=size(fun); end;
+
+%%%%% make "quilts", that contain all slices on 2D patched sheet
+% Number of patches along sides of Quilt (M and N)
+% Size (in voxels) of side of patches of Quilt (m and n)
+
+if slicedim == 3
+  m = dim(1);
+  n = dim(2);
+  if length(dim)==2
+    dim(3) = 1;
+  end
+  M = ceil(sqrt(dim(3)));
+  N = ceil(sqrt(dim(3)));
+elseif slicedim == 2
+  m = dim(1);
+  n = dim(3);
+  M = ceil(sqrt(dim(2)));
+  N = ceil(sqrt(dim(2)));
+elseif slicedim == 1
+  m = dim(2);
+  n = dim(3);
+  M = ceil(sqrt(dim(1)));
+  N = ceil(sqrt(dim(1)));
+end
+
+num_patch = N*M;
+%   if slicedim~=3
+%     error('only supported for slicedim=3');
+%   end
+num_slice = (dim(slicedim));
+num_empt = num_patch-num_slice;
+
+% put empty slides on ana, fun, msk, mskana to fill Quilt up
+if slicedim == 3
+  if hasana; ana(:,:,end+1:num_patch)=0; end;
+  if hasfun; fun(:,:,end+1:num_patch)=0; end;
+  if hasmsk; msk(:,:,end+1:num_patch)=0; end;
+elseif slicedim == 2
+  if hasana; ana(:,end+1:num_patch,:)=0; end;
+  if hasfun; fun(:,end+1:num_patch,:)=0; end;
+  if hasmsk; msk(:,end+1:num_patch,:)=0; end;
+elseif slicedim == 1
+  if hasana; ana(end+1:num_patch,:,:)=0; end;
+  if hasfun; fun(end+1:num_patch,:,:)=0; end;
+  if hasmsk; msk(end+1:num_patch,:,:)=0; end;
+end
+
+%if hasmskana; mskana(:,:,end:num_patch)=0; end;
+% put the slices in the quilt
+for iSlice = 1:num_slice
+  xbeg = floor((iSlice-1)./M);
+  ybeg = mod(iSlice-1, M);
+  if slicedim == 3
+    if hasana
+      quilt_ana(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(ana(:,:,iSlice));
+    end
+    if hasfun
+      quilt_fun(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(fun(:,:,iSlice));
+    end
+    if hasmsk
+      quilt_msk(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(msk(:,:,iSlice));
+    end
+  elseif slicedim == 2
+    if hasana
+      quilt_ana(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(ana(:,iSlice,:));
+    end
+    if hasfun
+      quilt_fun(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(fun(:,iSlice,:));
+    end
+    if hasmsk
+      quilt_msk(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(msk(:,iSlice,:));
+    end
+  elseif slicedim == 1
+    if hasana
+      quilt_ana(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(ana(iSlice,:,:));
+    end
+    if hasfun
+      quilt_fun(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(fun(iSlice,:,:));
+    end
+    if hasmsk
+      quilt_msk(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(msk(iSlice,:,:));
+    end
+  end
+
+  %     if hasmskana
+  %       quilt_mskana(ybeg.*m+1:(ybeg+1).*m, xbeg.*n+1:(xbeg+1).*n)=squeeze(mskana(:,:,iSlice));
+  %     end
+end
+% make vols and scales, containes volumes to be plotted (fun, ana, msk) %added ingnie
+if hasana; vols2D{1} = quilt_ana; scales{1} = []; end; % needed when only plotting ana
+if hasfun; vols2D{2} = quilt_fun; scales{2} = [fcolmin fcolmax]; end;
+if hasmsk; vols2D{3} = quilt_msk; scales{3} = [opacmin opacmax]; end;
+
+plot2D(vols2D, scales);
+axis off
+
+if strcmp(colorbar1,  'yes'),
+  if hasfun
+    % use a normal Matlab colorbar
+    hc = colorbar;
+    set(hc, 'YLim', [fcolmin fcolmax]);
+  else
+    warning('no colorbar possible without functional data')
+  end
+end
     
