d38b702bd8b80754e9ab9100fcdd1e2ebef4a00e
Ali Bahramisharif <ali.bahramisharif@donders.ru.nl>
2009-11-30 12:39:02 +0000

    cfg.saveLI is used to save lateralization index on the local machine.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@211 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- realtime/rt_onlineprocess.m
+++ realtime/rt_onlineprocess.m
@@ -37,6 +37,7 @@ if ~isfield(cfg, 'nexample'),       cfg.nexample = inf;       end
 if ~isfield(cfg, 'foi'),            cfg.foi = 10;             end % from alpha frequency
 if ~isfield(cfg, 'ntraining'),      cfg.ntraining= inf;       end % the number of trials to be used in the training phase
 if ~isfield(cfg, 'verbose'),        cfg.verbose=0;            end % verbose
+if ~isfield(cfg, 'saveLI'),         cfg.saveLI=0;             end % save lateralization index on the local machine
 if ~isfield(cfg, 'channel'),        cfg.channel = {'MLO' 'MRO'}';               end %channels to be used
 if ~isfield(cfg, 'datafile'),       cfg.datafile='shm://';                      end %input stream
 if ~isfield(cfg, 'ostream'),        cfg.ostream='tcp://presentation011:1976';   end %output stream
@@ -201,7 +202,7 @@ while cfg.count<cfg.nexample
             %accumulative command is saving for updating mean and std.
             ac_cmd=[ac_cmd,cmd1];
             if cfg.verbose
-                sprintf('trainig sample %d',cfg.count);
+                disp(sprintf('trainig sample %d',cfg.count));
             end
         end
         R_threshold=mean(ac_cmd)+1*std(ac_cmd);
@@ -223,9 +224,11 @@ while cfg.count<cfg.nexample
             cmdevent.value = cmd;
             write_event(cfg.ostream,cmdevent);
         end
-        %for saving ac_cmd to check it.
-        if (cfg.count/200)==floor(cfg.count/200)
-            save('~/Desktop/ac_cmd','ac_cmd');
+        if cfg.saveLI
+            %for saving ac_cmd to check it.
+            if (cfg.count/200)==floor(cfg.count/200)
+                save('ac_cmd','ac_cmd');
+            end
         end
         cfg.first=0;
     end % if enough new samples
