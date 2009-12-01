acc53f57e2d863c5289b496ecadeb7855429e4da
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2009-12-01 10:01:47 +0000

    added pointsize/pointsymbol/pointcolor/labelsize options and set verbose default to false
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@224 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/plot_lay.m
+++ plotting/plot_lay.m
@@ -7,25 +7,34 @@ function plot_lay(lay, varargin)
 % where the layout is a FieldTrip structure obtained from PREPARE_LAYOUT.
 %
 % Additional options should be specified in key-value pairs and can be
-%   'point'    = yes/no
-%   'box'      = yes/no
-%   'label'    = yes/no
-%   'mask'     = yes/no
-%   'outline'  = yes/no
+%   'point'         = yes/no
+%   'box'           = yes/no
+%   'label'         = yes/no
+%   'labelsize'     = number indicating font size (e.g. 6)
+%   'mask'          = yes/no
+%   'outline'       = yes/no
+%   'verbose'       = yes/no
+%   'pointsymbol'   = string with symbol (e.g. 'o') - all three point options need to be used together
+%   'pointcolor'    = string with color (e.g. 'k')
+%   'pointsize'     = number indicating size (e.g. 8)
 
 
 % Copyright (C) 2009, Robert Oostenveld
 %
 % Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
-keyvalcheck(varargin, 'optional', {'hpos', 'vpos', 'point', 'box', 'label', 'mask', 'outline', 'verbose'});
-hpos    = keyval('hpos',    varargin{:}); if isempty(hpos),     hpos = 0;       end
-vpos    = keyval('vpos',    varargin{:}); if isempty(vpos),     vpos = 0;       end
-point   = keyval('point',   varargin{:}); if isempty(point),    point = true;   end
-box     = keyval('box',     varargin{:}); if isempty(box),      box = true;     end
-label   = keyval('label',   varargin{:}); if isempty(label),    label = true;   end
-mask    = keyval('mask',    varargin{:}); if isempty(mask),     mask = true;    end
-outline = keyval('outline', varargin{:}); if isempty(outline),  outline = true; end
-verbose = keyval('verbose', varargin{:}); if isempty(verbose),  verbose = true; end
+keyvalcheck(varargin, 'optional', {'hpos', 'vpos', 'point', 'box', 'label','labelsize', 'mask', 'outline', 'verbose','pointsymbol','pointcolor','pointsize'});
+hpos         = keyval('hpos',        varargin{:}); if isempty(hpos),      hpos = 0;            end
+vpos         = keyval('vpos',        varargin{:}); if isempty(vpos),      vpos = 0;            end
+point        = keyval('point',       varargin{:}); if isempty(point),     point = true;        end
+box          = keyval('box',         varargin{:}); if isempty(box),       box = true;          end
+label        = keyval('label',       varargin{:}); if isempty(label),     label = true;        end
+labelsize    = keyval('labelsize',   varargin{:}); if isempty(labelsize), labelsize = 10;      end
+mask         = keyval('mask',        varargin{:}); if isempty(mask),      mask = true;         end
+outline      = keyval('outline',     varargin{:}); if isempty(outline),   outline = true;      end
+verbose      = keyval('verbose',     varargin{:}); if isempty(verbose),   verbose = false;     end
+pointsymbol  = keyval('pointsymbol', varargin{:});
+pointcolor   = keyval('pointcolor',  varargin{:});
+pointsize    = keyval('pointsize',   varargin{:});
 
 % convert between true/false/yes/no etc. statements
 point   = istrue(point);
@@ -47,12 +56,16 @@ Height = lay.height;
 Lbl    = lay.label;
 
 if point
-  plot(X, Y, 'b.');
-  plot(X, Y, 'yo');
+  if ~isempty(pointsymbol) && ~isempty(pointcolor) && ~isempty(pointsize) % if they all non-empty, don't use the default
+    plot(X, Y, 'marker',pointsymbol,'color',pointcolor,'markersize',pointsize,'linestyle','none');
+  else
+    plot(X, Y, 'marker','.','color','b','linestyle','none');
+    plot(X, Y, 'marker','o','color','y','linestyle','none');
+  end
 end
 
 if label
-  text(X, Y, Lbl);
+  text(X, Y, Lbl,'fontsize',labelsize);
 end
 
 if box
