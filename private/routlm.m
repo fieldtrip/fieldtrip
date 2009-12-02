a91a94441496457d7881526ad5b03343ce6c69f7
Robert Oostenveld <r.oostenveld@gmail.com>
2009-12-02 16:18:13 +0000

    added matlab code for auto-compilation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@237 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/routlm.m
+++ private/routlm.m
@@ -1,17 +1,51 @@
-function [proj] = routlm(v1, v2, v3, la, mu);
+function [varargout] = funname(varargin)
 
 % ROUTLM computes the projection of a point from its la/mu parameters
 % these equal the "Barycentric" coordinates
 %
-% [proj] = routlm(v1, v2, v3, la, mu)
-% 
+% Use as
+%   [proj] = routlm(v1, v2, v3, la, mu)
 % where v1, v2 and v3 are three vertices of the triangle
-%
-% also implemented as MEX file
 
-% Copyright (C) 2002, Robert Oostenveld
+% Copyright (C) 2002-2009, Robert Oostenveld
 %
 % Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
 
-% determine the projection onto the plane of the triangle
-proj  = (1-la-mu)*v1 + la*v2 + mu*v3;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is the native Matlab implementation.
+% The mex file is many times faster and is therefore preferred.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function [proj] = routlm(v1, v2, v3, la, mu);
+% proj = (1-la-mu)*v1 + la*v2 + mu*v3;
+
+% compile the missing mex file on the fly
+% remember the original working directory
+pwdir = pwd;
+
+% determine the name and full path of this function
+funname = mfilename('fullpath');
+mexsrc  = [funname '.c'];
+[mexdir, mexname] = fileparts(funname);
+
+try
+  % try to compile the mex file on the fly
+  warning('trying to compile MEX file from %s', mexsrc);
+  cd(mexdir);
+  mex(mexsrc);
+  cd(pwdir);
+  success = true;
+
+catch
+  % compilation failed
+  disp(lasterr);
+  error('could not locate MEX file for %s', mexname);
+  cd(pwdir);
+  success = false;
+end
+
+if success
+  % execute the mex file that was juist created
+  funname   = mfilename;
+  funhandle = str2func(funname);
+  [varargout{1:nargout}] = funhandle(varargin{:});
+end
