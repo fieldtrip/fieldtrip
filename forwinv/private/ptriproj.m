f4986839afc23ce3783c0b076857b9dc0f1eabb1
Robert Oostenveld <r.oostenveld@gmail.com>
2009-12-02 16:15:02 +0000

    added matlab code for auto-compilation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@236 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forwinv/private/ptriproj.m
+++ forwinv/private/ptriproj.m
@@ -1,20 +1,48 @@
-function [proj, dist] = ptriproj(v1, v2, v3, r, flag);
+function [varargout] = funname(varargin)
 
 % PTRIPROJ projects a point onto the plane going through a triangle
 %
-% [proj, dist] = ptriproj(v1, v2, v3, r, flag)
-% 
+% Use as
+%   [proj, dist] = ptriproj(v1, v2, v3, r, flag)
 % where v1, v2 and v3 are three vertices of the triangle, and r is 
 % the point that is projected onto the plane spanned by the vertices
 %
 % the optional flag can be:
 %   0 (default)  project the point anywhere on the complete plane
 %   1            project the point within or on the edge of the triangle
-%
-% implemented as MEX file
 
-% Copyright (C) 2002, Robert Oostenveld
+% Copyright (C) 2002-2009, Robert Oostenveld
 %
 % Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
 
-error(sprintf('could not locate MEX file for %s', mfilename))
+% compile the missing mex file on the fly
+% remember the original working directory
+pwdir = pwd;
+
+% determine the name and full path of this function
+funname = mfilename('fullpath');
+mexsrc  = [funname '.c'];
+[mexdir, mexname] = fileparts(funname);
+
+try
+  % try to compile the mex file on the fly
+  warning('trying to compile MEX file from %s', mexsrc);
+  cd(mexdir);
+  mex(mexsrc);
+  cd(pwdir);
+  success = true;
+
+catch
+  % compilation failed
+  disp(lasterr);
+  error('could not locate MEX file for %s', mexname);
+  cd(pwdir);
+  success = false;
+end
+
+if success
+  % execute the mex file that was juist created
+  funname   = mfilename;
+  funhandle = str2func(funname);
+  [varargout{1:nargout}] = funhandle(varargin{:});
+end
--- forwinv/private/solid_angle.m
+++ forwinv/private/solid_angle.m
@@ -1,4 +1,4 @@
-function [w] = solid_angle(r1, r2, r3);
+function [varargout] = funname(varargin)
 
 % SOLID_ANGLE of a planar triangle as seen from the origin
 %
@@ -8,55 +8,93 @@ function [w] = solid_angle(r1, r2, r3);
 % corresponding to all of space being subtended is 4*pi sterradians.
 % 
 % Use: 
-%   [w] = solid_angle(v1, v2, v3) or
+%   [w] = solid_angle(v1, v2, v3)
+% or
 %   [w] = solid_angle(pnt, tri)
 % where v1, v2 and v3 are the vertices of a single triangle in 3D or
 % pnt and tri contain a description of a triangular mesh (this will
 % compute the solid angle for each triangle)
-% 
-% also implemented as MEX file
 
-% Copyright (C) 2003, Robert Oostenveld
+% Copyright (C) 2003-2009, Robert Oostenveld
 %
 % Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
 
-if nargin==2
-  % reassign the input arguments 
-  pnt = r1;
-  tri = r2;
-  npnt = size(pnt,1);
-  ntri = size(tri,1);
-  w    = zeros(ntri,1);
-  % compute solid angle for each triangle
-  for i=1:ntri
-    r1 = pnt(tri(i,1),:);
-    r2 = pnt(tri(i,2),:);
-    r3 = pnt(tri(i,3),:);
-    w(i) = solid_angle(r1, r2, r3);
-  end 
-  return
-elseif nargin==3
-  % compute the solid angle for this triangle  
-  cp23_x = r2(2) * r3(3) - r2(3) * r3(2);
-  cp23_y = r2(3) * r3(1) - r2(1) * r3(3);
-  cp23_z = r2(1) * r3(2) - r2(2) * r3(1);
-  nom = cp23_x * r1(1) + cp23_y * r1(2) + cp23_z * r1(3);
-  n1 = sqrt (r1(1) * r1(1) + r1(2) * r1(2) + r1(3) * r1(3));
-  n2 = sqrt (r2(1) * r2(1) + r2(2) * r2(2) + r2(3) * r2(3));
-  n3 = sqrt (r3(1) * r3(1) + r3(2) * r3(2) + r3(3) * r3(3));
-  ip12 = r1(1) * r2(1) + r1(2) * r2(2) + r1(3) * r2(3);
-  ip23 = r2(1) * r3(1) + r2(2) * r3(2) + r2(3) * r3(3);
-  ip13 = r1(1) * r3(1) + r1(2) * r3(2) + r1(3) * r3(3);
-  den = n1 * n2 * n3 + ip12 * n3 + ip23 * n1 + ip13 * n2; 
-  if (nom == 0)
-    if (den <= 0)
-      w = nan;
-      return
-    end
-  end
-  w = 2 * atan2 (nom, den);
-  return
-else
-  error('invalid input');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The first section contains the plain Matlab implementation. The mex file
+% is many times faster and this function is called so frequently (for
+% large meshes), that the mex file should be used in all practical cases.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function [w] = solid_angle(r1, r2, r3);
+%
+% if nargin==2
+%   % reassign the input arguments 
+%   pnt = r1;
+%   tri = r2;
+%   npnt = size(pnt,1);
+%   ntri = size(tri,1);
+%   w    = zeros(ntri,1);
+%   % compute solid angle for each triangle
+%   for i=1:ntri
+%     r1 = pnt(tri(i,1),:);
+%     r2 = pnt(tri(i,2),:);
+%     r3 = pnt(tri(i,3),:);
+%     w(i) = solid_angle(r1, r2, r3);
+%   end 
+%   return
+% elseif nargin==3
+%   % compute the solid angle for this triangle  
+%   cp23_x = r2(2) * r3(3) - r2(3) * r3(2);
+%   cp23_y = r2(3) * r3(1) - r2(1) * r3(3);
+%   cp23_z = r2(1) * r3(2) - r2(2) * r3(1);
+%   nom = cp23_x * r1(1) + cp23_y * r1(2) + cp23_z * r1(3);
+%   n1 = sqrt (r1(1) * r1(1) + r1(2) * r1(2) + r1(3) * r1(3));
+%   n2 = sqrt (r2(1) * r2(1) + r2(2) * r2(2) + r2(3) * r2(3));
+%   n3 = sqrt (r3(1) * r3(1) + r3(2) * r3(2) + r3(3) * r3(3));
+%   ip12 = r1(1) * r2(1) + r1(2) * r2(2) + r1(3) * r2(3);
+%   ip23 = r2(1) * r3(1) + r2(2) * r3(2) + r2(3) * r3(3);
+%   ip13 = r1(1) * r3(1) + r1(2) * r3(2) + r1(3) * r3(3);
+%   den = n1 * n2 * n3 + ip12 * n3 + ip23 * n1 + ip13 * n2; 
+%   if (nom == 0)
+%     if (den <= 0)
+%       w = nan;
+%       return
+%     end
+%   end
+%   w = 2 * atan2 (nom, den);
+%   return
+% else
+%   error('invalid input');
+% end
+
+% compile the missing mex file on the fly
+% remember the original working directory
+pwdir = pwd;
+
+% determine the name and full path of this function
+funname = mfilename('fullpath');
+mexsrc  = [funname '.c'];
+[mexdir, mexname] = fileparts(funname);
+
+try
+  % try to compile the mex file on the fly
+  warning('trying to compile MEX file from %s', mexsrc);
+  cd(mexdir);
+  mex(mexsrc);
+  cd(pwdir);
+  success = true;
+
+catch
+  % compilation failed
+  disp(lasterr);
+  error('could not locate MEX file for %s', mexname);
+  cd(pwdir);
+  success = false;
+end
+
+if success
+  % execute the mex file that was juist created
+  funname   = mfilename;
+  funhandle = str2func(funname);
+  [varargout{1:nargout}] = funhandle(varargin{:});
 end
 
