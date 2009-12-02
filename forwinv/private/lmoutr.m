3f2900ff5feb8853fe9c7a9ecb86463a4d48be4f
Robert Oostenveld <r.oostenveld@gmail.com>
2009-12-02 16:20:25 +0000

    added matlab code for auto-compilation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@238 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forwinv/private/lmoutr.m
+++ forwinv/private/lmoutr.m
@@ -1,31 +1,66 @@
-function [la, mu, dist] = lmoutr(v1, v2, v3, r);
+function [varargout] = funname(varargin)
 
 % LMOUTR computes the la/mu parameters of a point projected to a triangle
 %
-% [la, mu, dist] = lmoutr(v1, v2, v3, r)
-% 
+% Use as
+%   [la, mu, dist] = lmoutr(v1, v2, v3, r)
 % where v1, v2 and v3 are three vertices of the triangle, and r is 
 % the point that is projected onto the plane spanned by the vertices
-%
-% also implemented as MEX file
 
-% Copyright (C) 2002, Robert Oostenveld
+% Copyright (C) 2002-2009, Robert Oostenveld
 %
 % Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
 
-% compute la/mu parameters
-vec0 = r  - v1;
-vec1 = v2 - v1;
-vec2 = v3 - v2;
-vec3 = v3 - v1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This is the Matlab implementation.
+% The mex file is many times faster and therefore preferred.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function [la, mu, dist] = lmoutr(v1, v2, v3, r);
+% 
+% % compute la/mu parameters
+% vec0 = r  - v1;
+% vec1 = v2 - v1;
+% vec2 = v3 - v2;
+% vec3 = v3 - v1;
+% 
+% tmp   = [vec1' vec3'] \ (vec0');
+% la    = tmp(1);
+% mu    = tmp(2);
+% 
+% % determine the projection onto the plane of the triangle
+% proj  = v1 + la*vec1 + mu*vec3;
+% 
+% % determine the distance from the original point to its projection
+% dist = norm(r-proj);
 
-tmp   = [vec1' vec3'] \ (vec0');
-la    = tmp(1);
-mu    = tmp(2);
+% compile the missing mex file on the fly
+% remember the original working directory
+pwdir = pwd;
 
-% determine the projection onto the plane of the triangle
-proj  = v1 + la*vec1 + mu*vec3;
+% determine the name and full path of this function
+funname = mfilename('fullpath');
+mexsrc  = [funname '.c'];
+[mexdir, mexname] = fileparts(funname);
 
-% determine the distance from the original point to its projection
-dist = norm(r-proj);
+try
+  % try to compile the mex file on the fly
+  warning('trying to compile MEX file from %s', mexsrc);
+  cd(mexdir);
+  mex(mexsrc);
+  cd(pwdir);
+  success = true;
 
+catch
+  % compilation failed
+  disp(lasterr);
+  error('could not locate MEX file for %s', mexname);
+  cd(pwdir);
+  success = false;
+end
+
+if success
+  % execute the mex file that was juist created
+  funname   = mfilename;
+  funhandle = str2func(funname);
+  [varargout{1:nargout}] = funhandle(varargin{:});
+end
