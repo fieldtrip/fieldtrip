ba677626f020aabd639241f657323e7b638b6de2
Robert Oostenveld <r.oostenveld@gmail.com>
2009-12-08 19:53:58 +0000

    fixed bug in nanmean, now consistent with the version inside fieldtrip/private
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@257 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- classification/utilities/nanmean.m
+++ classification/utilities/nanmean.m
@@ -1,11 +1,1 @@
-function m = nanmean(data,dim)
-%NANMEAN take mean ignoring nans along any dimension
-
-    if nargin<2, dim=1; end
-
-    tmpdata = data;
-    tmpdata(isnan(tmpdata(:))) = 0;
-            
-    m = mean(tmpdata,dim);
-
-end
+../../private/nanmean.m
