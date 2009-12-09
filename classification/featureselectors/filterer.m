256e0db23c76fe0c0d5c6242cd44d2080ae02df8
Marcel van Gerven <marcel.van.gerven@gmail.com>
2009-12-09 19:04:56 +0000

    rewrote featureselector and filterer
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@269 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- classification/featureselectors/filterer.m
+++ classification/featureselectors/filterer.m
@@ -5,132 +5,131 @@ classdef filterer < featureselector
 %   and uses that ordering to greedily determine the 'optimal' feature set
 %   or to select the best m features.
 %
-% OPTIONS
-%
-%
 %   Copyright (c) 2008, Marcel van Gerven
 %
 %   $Log: filterer.m,v $
 %
 
-    properties
-        
-        % the filter function should accept a dataset and design and output
-        % a measure of desirability for that dataset. In principle, a
-        % classifier could be used to compute univariate accuracies as a
-        % measure (also see wrapper). 
-        %
-        % We can use any of:
-        %
-        % [] : uses the validator to order the features (not yet implemented)
-        % [10 100 2 ...] : uses the ordering specified
-        % @meandiff : order according to the norm of differences of means
-        % @mutual_information : order according to the mutual information
-        % @anova : order according to anova test
-        
-        filter = @meandiff
+  properties
+  
+    validator = []; % e.g., crossvalidator('procedure',clfproc({nb()}),'cvfolds',0.9);
+    metric = 'accuracy'; % evaluation metric
+    criterion; % keeps track of the evaluation metric
     
-        value; % filter function outputs
-        order; % ordering of the variables
-        top; % selection of the best features (false by default)
-     end
-
-    methods
-        function obj = filterer(varargin)
-
-            obj = obj@featureselector(varargin{:});
-                    
-        end
-        function obj = train(obj,data,design)
-            
-            if obj.verbose
-                if obj.top
-                    fprintf('selecting %d features based on %s filter\n',obj.top,func2str(obj.filter));
-                else
-                    fprintf('selecting optimal features based on %s filter\n',func2str(obj.filter));
-                end
-            end           
-            
-            if iscell(data)
-
-              cvalue = cell(1,length(data));
-              corder = cell(1,length(data));
-              ccriterion = cell(1,length(data));
-              csubset = cell(1,length(data));
-              
-              for c=1:length(data)
-                obj = obj.train(data{c},design{c});
-                cvalue{c} = obj.value;
-                corder{c} = obj.order;
-                ccriterion{c} = obj.criterion;
-                csubset{c} = obj.subset;
-              end
-              
-              obj.value = cvalue;
-              obj.order = corder;
-              obj.criterion = ccriterion;
-              obj.subset = csubset;
-
-            else
-                
-                % compute function values
-                nfeatures = size(data,2);
-                obj.value = zeros(1,nfeatures);
-                for j=1:nfeatures
-                    obj.value(j) = obj.filter(data(:,j),design);
-                end
-                
-                % get ordering of the features
-                [a,obj.order] = sort(obj.value,'descend');
-
-                if obj.top
-                    % select the best top features
-                    obj.subset = obj.order(1:obj.top);
-                else
-                  % use procedure to determine the best feature subset
-                  [obj.subset,obj.criterion] = obj.select_features(data,design);
-                end
-            end
-                       
-        end
+    nfeatures = Inf; % maximum number of used features
         
-        function [subset,criterion] = select_features(obj,data,design) 
-            % select features
-            % assumes availability of evaluation function and feature
-            % ordering
-
-                subset = [];
-                metric = -Inf;
-            
-                % use a more fancy procedure
-                features = obj.order;
-
-                criterion = zeros(1,length(features));
-                for f=1:min(obj.maxfeatures,length(features))
-                   
-                    if obj.verbose
-                        fprintf('evaluating %d out of %d features; ',f,length(features));
-                    end
-                    
-                    cv = obj.validator.validate(data(:,features(1:f)),design);
-
-                    m = evaluate(cv.post,cv.design,'metric',obj.metric);
-                                        
-                    if obj.verbose, fprintf('criterion: %.2g\n',m); end
-                    
-                    if m > metric
-                        subset = features(1:f);
-                        metric = m;
-                    end
-                   
-                    criterion(f) = m;
-                    
-                end
-
-        end
-        
+    % filter is used to order the features
+    %
+    % options:
+    % [10 100 2 ...] : uses the ordering specified
+    % @meandiff : order according to the norm of differences of means
+    % @mutual_information : order according to the mutual information (default)
+    % @anova : order according to anova test    
+    filter = @mutual_information;
+    
+    value; % filter function outputs
+    order; % ordering of the variables
+    
+  end
+  
+  methods
+    function obj = filterer(varargin)
+      
+      obj = obj@featureselector(varargin{:});
+      
     end
-
-
+    function obj = train(obj,data,design)
+      
+      obj.nfeatures = min(obj.nfeatures,size(data,2));
+      
+      if obj.verbose
+        if isempty(obj.validator)
+          fprintf('selecting %d features based on %s filter\n',obj.nfeatures,func2str(obj.filter));
+        else
+          fprintf('selecting optimal features based on %s filter\n',func2str(obj.filter));
+        end
+      end
+      
+      if iscell(data)
+        
+        cvalue = cell(1,length(data));
+        corder = cell(1,length(data));
+        ccriterion = cell(1,length(data));
+        csubset = cell(1,length(data));
+        
+        for c=1:length(data)
+          obj = obj.train(data{c},design{c});
+          cvalue{c} = obj.value;
+          corder{c} = obj.order;
+          ccriterion{c} = obj.criterion;
+          csubset{c} = obj.subset;
+        end
+        
+        obj.value = cvalue;
+        obj.order = corder;
+        obj.criterion = ccriterion;
+        obj.subset = csubset;
+        
+      else
+        
+        % compute function values
+        nfeatures = size(data,2);
+        obj.value = zeros(1,nfeatures);
+        for j=1:nfeatures
+          obj.value(j) = obj.filter(data(:,j),design);
+        end
+        
+        % get ordering of the features
+        [a,obj.order] = sort(obj.value,'descend');
+        
+        if isempty(obj.validator)
+          % select the best n features
+          obj.subset = obj.order(1:obj.nfeatures);
+        else
+          % use procedure to determine the best feature subset
+          [obj.subset,obj.criterion] = obj.select_features(data,design);
+        end
+      end
+      
+    end
+    
+    function [subset,criterion] = select_features(obj,data,design)
+      % select features
+      % assumes availability of evaluation function and feature
+      % ordering
+      
+      subset = [];
+      metric = -Inf;
+      
+      % use a more fancy procedure
+      features = obj.order;
+      
+      criterion = zeros(1,length(features));
+      for f=1:min(obj.nfeatures,length(features))
+        
+        if obj.verbose
+          fprintf('evaluating %d out of %d features; ',f,length(features));
+        end
+        
+        cv = obj.validator.validate(data(:,features(1:f)),design);
+        
+        m = evaluate(cv.post,cv.design,'metric',obj.metric);
+        
+        if obj.verbose, fprintf('criterion: %.2g\n',m); end
+        
+        if m > metric
+          subset = features(1:f);
+          metric = m;
+        end
+        
+        criterion(f) = m;
+        
+      end
+      
+    end
+    
+  end
+  
+  
 end
 
--- classification/featureselectors/wrapper.m
+++ classification/featureselectors/wrapper.m
@@ -20,158 +20,167 @@ classdef wrapper < featureselector
 
     properties
        
-        search = 'hillclimbing'; % type of search strategy (maxfeatures determines evaluations for hillclimbing)
-        expansions = 0; % maximum number of expansions without improvement for bestfirst search
-        epsilon = 0.01; % minimum relative improvement for acceptance in bestfirst search
+      validator = []; % e.g., crossvalidator('procedure',clfproc({nb()}),'cvfolds',0.9);
+      metric = 'accuracy'; % evaluation metric
+      criterion; % keeps track of the evaluation metric
+      
+      maxfeatures = Inf; % maximum number of used features
+      
+      search = 'hillclimbing'; % type of search strategy (maxfeatures determines evaluations for hillclimbing)
+      expansions = 0; % maximum number of expansions without improvement for bestfirst search
+      epsilon = 0.01; % minimum relative improvement for acceptance in bestfirst search
+
     end
 
     methods
+      
         function obj = wrapper(varargin)
-
-            obj = obj@featureselector(varargin{:});
-                    
-        end
-        function obj = train(obj,data,design)
-            
-            switch obj.search
-                
-                case 'hillclimbing'
-
-                    cur = [];
-                    metric = -Inf;
-                    nfeat = min(obj.maxfeatures,size(data,2));
-
-                    candidates = 1:size(data,2);
-                    obj.criterion = zeros(1,nfeat);
-                    for f=1:nfeat
-
-                        if obj.verbose
-                            fprintf('evaluating %d out of %d features; ',f,nfeat);
-                        end
-
-                        oldmetric = metric;
-                        for j=candidates
-
-                            tmp = [cur j];
-
-                            cv = obj.validator.validate(data(:,tmp),design);
-                            m = evaluate(cv.post,cv.design,'metric',obj.metric);
-
-                            if m > metric
-                                subset = tmp;
-                                metric = m;
-                            end
-                        end
-                        
-                        if obj.verbose
-                            fprintf('%.2g\n',metric)
-                        end
-                        obj.criterion(f) = metric;
-                   
-                        if metric > oldmetric % better candidate found
-                            cur = subset;
-                        else
-                            %break; % no improvement so break out of loop
-                        end
-                    end
-
-                    obj.subset = cur;
-                    
-                case 'bestfirst'
-                    
-                    nfeat = size(data,2);
-
-                    % we represent features as binary numbers
-                    open = 0;
-                    closed = [];
-                                        
-                    % performance of the zero subset is assumed infinitely
-                    % bad
-                    metric = -Inf;
-                    openacc = metric;
-                     
-                    best = 0;
-                    v = best;
-                    lastchange = 0;
-                    while lastchange <= obj.expansions
-                        
-                        if obj.verbose
-                            fprintf('evaluating candidate subset; ');
-                        end
-                        
-                        % find children
-                        candidates = obj.expand(v,nfeat);
-
-                        vb = (open == v);
-                        open = open(~vb);
-                        openacc = openacc(~vb);
-                        closed = [closed v];
-                        
-                        % remove candidates in closed or open
-                        candidates = setdiff(candidates,open);
-                        candidates = setdiff(candidates,closed);
-
-                        acc = zeros(1,length(candidates));
-                        for j=1:length(candidates)
-                           
-                            cv = obj.validator.validate(data(:,boolean(bitget(candidates(j),1:nfeat))),design);
-                            acc(j) = evaluate(cv.post,cv.design,'metric',obj.metric);                                               
-                        end
-                        
-                        open = [open candidates];
-                        openacc = [openacc acc];
-                        
-                        [a,b] = max(openacc);
-                        v = open(b);
-                        
-                        if a > (1+obj.epsilon)*metric
-                            metric = a;
-                            best = v;
-                            
-                            lastchange = 0;
-                        else
-                            lastchange = lastchange + 1;
-                        end
-                        
-                        if obj.verbose
-                            fprintf('%.2g\n',metric)
-                        end
-                        
-                    end
-                    
-                    obj.subset = find(bitget(best,1:nfeat));
-                    
-                otherwise 
-                    error('unknown search strategy');
-            
-            end
-            
-        end
-                
-    end
-
-    methods(Access = private)
-       
-        function candidates = expand(obj,v,nfeat)
-            
-            % create children of v
-            b = boolean(bitget(v,1:nfeat));
-            
-            cnd = find(~b);
-            
-            candidates = zeros(1,length(cnd));
-            for c=1:length(cnd)
-            
-                % make candidate
-                tmp = b;
-                tmp(cnd(c)) = true;
-                
-                % convert binary candidate back to decimal
-                cp = fliplr([1 cumprod(2*ones(1,length(tmp)-1))]);                
-                candidates(c) = sum(cp(boolean(tmp)));
-            end
+          
+          obj = obj@featureselector(varargin{:});
+          
         end
         
+        function obj = train(obj,data,design)
+          
+          switch obj.search
+            
+            case 'hillclimbing'
+              
+              cur = [];
+              metric = -Inf;
+              nfeat = min(obj.maxfeatures,size(data,2));
+              
+              candidates = 1:size(data,2);
+              obj.criterion = zeros(1,nfeat);
+              for f=1:nfeat
+                
+                if obj.verbose
+                  fprintf('evaluating %d out of %d features; ',f,nfeat);
+                end
+                
+                oldmetric = metric;
+                for j=candidates
+                  
+                  tmp = [cur j];
+                  
+                  cv = obj.validator.validate(data(:,tmp),design);
+                  m = evaluate(cv.post,cv.design,'metric',obj.metric);
+                  
+                  if m > metric
+                    subset = tmp;
+                    metric = m;
+                  end
+                end
+                
+                if obj.verbose
+                  fprintf('%.2g\n',metric)
+                end
+                obj.criterion(f) = metric;
+                
+                if metric > oldmetric % better candidate found
+                  cur = subset;
+                else
+                  %break; % no improvement so break out of loop
+                end
+              end
+              
+              obj.subset = cur;
+              
+            case 'bestfirst'
+              
+              nfeat = size(data,2);
+              
+              % we represent features as binary numbers
+              open = 0;
+              closed = [];
+              
+              % performance of the zero subset is assumed infinitely
+              % bad
+              metric = -Inf;
+              openacc = metric;
+              
+              best = 0;
+              v = best;
+              lastchange = 0;
+              while lastchange <= obj.expansions
+                
+                if obj.verbose
+                  fprintf('evaluating candidate subset; ');
+                end
+                
+                % find children
+                candidates = obj.expand(v,nfeat);
+                
+                vb = (open == v);
+                open = open(~vb);
+                openacc = openacc(~vb);
+                closed = [closed v];
+                
+                % remove candidates in closed or open
+                candidates = setdiff(candidates,open);
+                candidates = setdiff(candidates,closed);
+                
+                acc = zeros(1,length(candidates));
+                for j=1:length(candidates)
+                  
+                  cv = obj.validator.validate(data(:,boolean(bitget(candidates(j),1:nfeat))),design);
+                  acc(j) = evaluate(cv.post,cv.design,'metric',obj.metric);
+                end
+                
+                open = [open candidates];
+                openacc = [openacc acc];
+                
+                [a,b] = max(openacc);
+                v = open(b);
+                
+                if a > (1+obj.epsilon)*metric
+                  metric = a;
+                  best = v;
+                  
+                  lastchange = 0;
+                else
+                  lastchange = lastchange + 1;
+                end
+                
+                if obj.verbose
+                  fprintf('%.2g\n',metric)
+                end
+                
+              end
+              
+              obj.subset = find(bitget(best,1:nfeat));
+              
+            otherwise
+              error('unknown search strategy');
+              
+          end
+          
+        end
+        
+    end
+    
+    methods(Access = private)
+      
+      function candidates = expand(obj,v,nfeat)
+        
+        % create children of v
+        b = boolean(bitget(v,1:nfeat));
+        
+        cnd = find(~b);
+        
+        candidates = zeros(1,length(cnd));
+        for c=1:length(cnd)
+          
+          % make candidate
+          tmp = b;
+          tmp(cnd(c)) = true;
+          
+          % convert binary candidate back to decimal
+          cp = fliplr([1 cumprod(2*ones(1,length(tmp)-1))]);
+          candidates(c) = sum(cp(boolean(tmp)));
+        end
+      end
+      
     end
 end
 
