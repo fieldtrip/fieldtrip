1ab30a27220758d421be410df0af09fd9ad39034
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2010-01-13 14:38:41 +0000

    added functionality to standardise with respect to a specified interval (third input) [begsample endsample]
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@397 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/standardise.m
+++ private/standardise.m
@@ -1,4 +1,4 @@
-function [x,mx,sx] = standardise(x,dim)
+function [x,mx,sx] = standardise(x,dim,lim)
 
 % X = STANDARDISE(X, DIM) computes the zscore of a matrix along dimension dim
 % has similar functionality as the stats-toolbox's zscore function
@@ -7,12 +7,28 @@ function [x,mx,sx] = standardise(x,dim)
 %
 % Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
 
-if nargin == 1, dim = find(size(x)>1,1,'first'); end
+if nargin == 1, 
+  dim = find(size(x)>1,1,'first');
+  lim = [1 size(x,dim)];
+elseif nargin == 2,
+  lim = [1 size(x,dim)];
+end
 
-n      = size(x,dim);
-mx     = mean(x,dim);
+ndim   = numel(size(x));
+ix     = cell(1,6);
+for k = 1:numel(ix)
+  if k>ndim
+    ix{k} = 1;
+  else
+    ix{k} = 1:size(x,k);
+  end
+end
+ix{dim} = lim(1):lim(2);
+
+n      = numel(ix{dim});
+mx     = mean(x(ix{1},ix{2},ix{3},ix{4},ix{5},ix{6}),dim);
 %sx     = std(x,0,dim);
-sx     = std(x,1,dim);
-repvec = ones(1,length(size(x)));
-repvec(dim) = n; 
+sx     = std(x(ix{1},ix{2},ix{3},ix{4},ix{5},ix{6}),1,dim);
+repvec = ones(1,ndim);
+repvec(dim) = size(x,dim); 
 x      = (x - repmat(mx,repvec))./repmat(sx,repvec);
