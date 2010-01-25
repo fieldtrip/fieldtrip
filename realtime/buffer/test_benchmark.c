aa8f1cfb665df6cce0d3c30a2671eefcb261d80d
Robert Oostenveld <r.oostenveld@gmail.com>
2010-01-25 16:59:34 +0000

    moved two buffer test applications from cvs into svn
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@443 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ realtime/buffer/test_benchmark.c
@@ -0,0 +1,203 @@
+/*
+ * Use as
+ *    ./test_benchmark localhost 1972 32 512 1
+ *
+ * Copyright (C) 2008, Christian Hesse & Robert Oostenveld
+ * F.C. Donders Centre for Cognitive Neuroimaging, Radboud University Nijmegen,
+ * Kapittelweg 29, 6525 EN Nijmegen, The Netherlands
+ *
+ * $Log: test_benchmark.c,v $
+ * Revision 1.3  2009/06/17 15:09:02  roboos
+ * cleanup of indentation, moved random number generation outside while-loop
+ *
+ * Revision 1.2  2009/05/14 20:26:52  roboos
+ * fixed bug in time keeping, it was using an incorretc clock (not in seconds)
+ *
+ * Revision 1.1  2009/05/13 20:46:05  roboos
+ * added test application that writes as fast as it can
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+
+#include "message.h"
+#include "buffer.h"
+#include "unix_includes.h"
+#include "socket_includes.h"
+
+int main(int argc, char *argv[]) {
+  host_t host;
+  
+    /* these variables are for the threading */
+  int rc;
+  pthread_t tid;
+  
+    /* these variables are for writing the data */
+  int i, j, k, status = 0, verbose = 0, samplecount = 0;
+  time_t tic, toc;
+  time_t elapsed;
+  
+  /* these represent the acquisition system properties */
+  int fsample        = 512; /* this is just for the header */
+  int nchans         = 32;
+  int nsamples       = 64;
+  int stateless      = 1;	  /* boolean */
+  
+    /* start with defaults */
+  sprintf(host.name, DEFAULT_HOSTNAME);
+  host.port = DEFAULT_PORT;
+  
+  if (argc>1)
+    sprintf(host.name, argv[1]);
+  
+  if (argc>2)
+    host.port = atoi(argv[2]);
+  
+  if (argc>3)
+    nchans = atoi(argv[3]);
+  
+  if (argc>4)
+    nsamples = atoi(argv[4]);
+  
+  if (argc>5)
+    stateless = atoi(argv[5]);
+  
+  /* these are used in the communication and represent statefull information */
+  int server             = -1;
+  message_t    *request  = NULL;
+  message_t    *response = NULL;
+  header_t     *header   = NULL;
+  data_t       *data     = NULL;
+  event_t      *event    = NULL;
+  
+  check_datatypes();
+  
+  if (verbose>0) fprintf(stderr, "test_benchmark: host.name =  %s\n", host.name);
+  if (verbose>0) fprintf(stderr, "test_benchmark: host.port =  %d\n", host.port);
+  
+  /* allocate the elements that will be used in the communication */
+  request      = malloc(sizeof(message_t));
+  request->def = malloc(sizeof(messagedef_t));
+  request->buf = NULL;
+  request->def->version = VERSION;
+  request->def->bufsize = 0;
+  
+  header      = malloc(sizeof(header_t));
+  header->def = malloc(sizeof(headerdef_t));
+  header->buf = NULL;
+  
+  data      = malloc(sizeof(data_t));
+  data->def = malloc(sizeof(datadef_t));
+  data->buf = NULL;
+  
+  event      = malloc(sizeof(event_t));
+  event->def = malloc(sizeof(eventdef_t));
+  event->buf = NULL;
+  
+  /* define the header */
+  header->def->nchans    = nchans;
+  header->def->nsamples  = 0;
+  header->def->nevents   = 0;
+  header->def->fsample   = fsample;
+  header->def->data_type = DATATYPE_FLOAT32;
+  header->def->bufsize   = 0;
+  FREE(header->buf);
+  
+  /* define the constant part of the data and allocate space for the variable part */
+  data->def->nchans    = nchans;
+  data->def->nsamples  = nsamples;
+  data->def->data_type = DATATYPE_FLOAT32;
+  data->def->bufsize   = WORDSIZE_FLOAT32*nchans*nsamples;
+  FREE(data->buf);
+  data->buf            = malloc(WORDSIZE_FLOAT32*nchans*nsamples);
+    
+    /* create the random data */
+    for (j=0; j<nsamples; j++)
+      for (i=0; i<nchans;    i++)
+        ((FLOAT32_T *)(data->buf))[j*nchans+i] = 2.0*((FLOAT32_T)rand())/RAND_MAX - 1.0;
+  
+  /* initialization phase, send the header */
+  request->def->command = PUT_HDR;
+  request->def->bufsize = append(&request->buf, request->def->bufsize, header->def, sizeof(headerdef_t));
+  request->def->bufsize = append(&request->buf, request->def->bufsize, header->buf, header->def->bufsize);
+  
+  server = open_connection(host.name, host.port);
+  status = clientrequest(server, request, &response);
+  if (verbose>0) fprintf(stderr, "test_benchmark: clientrequest returned %d\n", status);
+  if (status) {
+    fprintf(stderr, "random: err1\n");
+    goto cleanup;
+  }
+  
+  if (stateless) {
+    status = close_connection(server);
+    if (status) {
+      fprintf(stderr, "random: err2\n");
+      goto cleanup;
+    }
+  }
+  
+  cleanup_message(&request);
+  cleanup_message(&response);
+  request = NULL;
+  response = NULL;
+  
+  tic = time(NULL);
+  
+  while (1) {
+    
+    /* create the request */
+    request      = malloc(sizeof(message_t));
+    request->def = malloc(sizeof(messagedef_t));
+    request->buf = NULL;
+    request->def->version = VERSION;
+    request->def->bufsize = 0;
+    request->def->command = PUT_DAT;
+    request->def->bufsize = append(&request->buf, request->def->bufsize, data->def, sizeof(datadef_t));
+    request->def->bufsize = append(&request->buf, request->def->bufsize, data->buf, data->def->bufsize);
+    
+    if (stateless)
+      server = open_connection(host.name, host.port);
+    
+    status = clientrequest(server, request, &response);
+    if (verbose>0) fprintf(stderr, "random: clientrequest returned %d\n", status);
+    if (status) {
+      fprintf(stderr, "random: err3\n");
+      goto cleanup;
+    }
+    
+    if (stateless) {
+      status = close_connection(server);
+      if (status) {
+        fprintf(stderr, "random: err4\n");
+        goto cleanup;
+      }
+    }
+    
+    cleanup_message(&request);
+    cleanup_message(&response);
+    request = NULL;
+    response = NULL;
+    
+    samplecount += nsamples*nchans;
+    toc = time(NULL);
+    elapsed = toc-tic;
+    
+    fprintf(stderr, "samplecount = %d, elapsed = %d, samples/sec = %d\n", samplecount, elapsed, samplecount/elapsed);
+    
+  } /* while(1) */
+  
+  cleanup:
+    cleanup_event(&event);
+    cleanup_data(&data);
+    cleanup_header(&header);
+    cleanup_message(&request);
+    cleanup_message(&response);
+    
+    pthread_exit(0);
+    return 0;
+}
+
---
+++ realtime/buffer/test_pthread.c
@@ -0,0 +1,60 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifdef __BORLANDC__
+#include <windows.h>
+#include "gettimeofday.h"
+#else
+#include <unistd.h>
+#include <strings.h>
+#endif
+#ifdef WIN32
+#define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
+#define usleep(x)    (Sleep((x)/1000))
+#endif
+#if defined (__BORLANDC__) || defined (__WIN32__)
+#define strcasecmp(a,b) (strcmpi(a,b))
+#endif
+
+pthread_mutex_t mutexthreadcount = PTHREAD_MUTEX_INITIALIZER;
+int threadcount = 0;
+
+void *hello_thread( void *arg ) {
+		printf( "thread: threadcount = %d\n", threadcount );
+		usleep(100000);
+		pthread_mutex_lock(&mutexthreadcount);
+		threadcount--;
+		pthread_mutex_unlock(&mutexthreadcount);
+		pthread_exit( 0 );
+		return NULL;
+}
+
+int main( int argc, char *argv[] ) {
+	int		n;
+	pthread_t	tid;
+
+	for (;;) {
+		if ( n = pthread_create( &tid, NULL, hello_thread, NULL ) ) {
+				fprintf( stderr, "pthread_create: %s\n", strerror( n ) );
+				exit( 1 );
+		}
+
+		pthread_mutex_lock(&mutexthreadcount);
+		threadcount++;
+		pthread_mutex_unlock(&mutexthreadcount);
+		printf("main: threadcount = %d\n", threadcount);
+
+		if ( n = pthread_detach( tid ) ) {
+			fprintf( stderr, "pthread_detach: %s\n", strerror( n ) );
+			exit( 1 );
+		}
+
+		//if ( n = pthread_join( tid, NULL ) ) {
+		//		fprintf( stderr, "pthread_join: %s\n", strerror( n ) );
+		//		exit( 1 );
+		//}
+
+	}
+	return( 0 );
+}
