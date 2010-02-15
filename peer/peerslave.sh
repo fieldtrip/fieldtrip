76b89aa91dac497be8ed1639f80f550ca730a56e
Robert Oostenveld <r.oostenveld@gmail.com>
2010-02-15 16:59:12 +0000

    don't assume that the matlab path is set, cd into correct directory
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@552 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- peer/peerslave.sh
+++ peer/peerslave.sh
@@ -3,6 +3,6 @@
 # shell script to start a peer in slave mode, restart if it exits
 
 while [ true ] ; do 
-  /opt/matlab78/bin/matlab -nodesktop -r "peerslave('threads', 1)"
+  /opt/matlab78/bin/matlab -nodesktop -r "cd /home/common/matlab/fieldtrip/peer; peerslave('threads', 1);"
 done
 
