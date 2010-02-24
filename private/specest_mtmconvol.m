d75a41b2f6cd9047c511648786c37e19997c41d8
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2010-02-24 11:18:16 +0000

    added some basics (padding,foi,fboi)
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@593 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/specest_mtmconvol.m
+++ private/specest_mtmconvol.m
@@ -1,13 +1,13 @@
-function [out,foi,toi] = specest_mtmconvol(dat, fsample, varargin) 
+function [out,foi,toi] = specest_mtmconvol(dat, time, varargin) 
 
 % SPECEST_MTMCONVOL performs wavelet convolution in the time domain by multiplication in the frequency domain
 %
 %
 % Use as
-%   [out,foi,toi] = specest_mtmfft(dat,fsample,...)  %%% DECIDE: WHICH INPUT IS REQUIRED AND WHICH WILL BE DEFAULTED?
+%   [out,foi,toi] = specest_mtmfft(dat,time,...)  %%% DECIDE: WHICH INPUT IS REQUIRED AND WHICH WILL BE DEFAULTED?
 %
 %   dat      = matrix of chan*sample 
-%   fsample  = number indicating sampling rate
+%   time     = vector, containing time in seconds for each sample
 %   out      = matrix of taper*chan*sample of fourier coefficients
 %   foi      = vector of frequencies in out
 %   toi      = vector of timebins in out
@@ -17,10 +17,10 @@ function [out,foi,toi] = specest_mtmconvol(dat, fsample, varargin)
 %
 % Optional arguments should be specified in key-value pairs and can include:
 %   taper      = 'dpss', 'hanning' or many others, see WINDOW (default = 'dpss')
-%   pad        = number, total number of samples after zero padding               %%% COULD ALSO BE GIVEN IN SECONDS....
-%   time       = vector, containing time points of interest (in seconds, analysis window will be centered around these time points)                                           
+%   pad        = number, indicating time-length of data to be padded out to          %%% IS IN SECONDS, MTMFFT HAS IT SAMPLES ATM
+%   toi       = vector, containing time points of interest (in seconds, analysis window will be centered around these time points)                                           
 %   timwin     = vector, containing length of time windows (in seconds)
-%   freq       = vector, containing frequencies (in Hz)                                              
+%   foi       = vector, containing frequencies (in Hz)                                              
 %   tapsmofrq  = the amount of spectral smoothing through multi-tapering. Note: 4 Hz smoothing means plus-minus 4 Hz, i.e. a 8 Hz smoothing box
 %
 %
@@ -32,21 +32,45 @@ function [out,foi,toi] = specest_mtmconvol(dat, fsample, varargin)
 %
 
 % get the optional input arguments
-keyvalcheck(varargin, 'optional', {'dpss','pad','time','timwin','freq','tapsmofrq'});
+keyvalcheck(varargin, 'optional', {'dpss','pad','toi','timwin','foi','tapsmofrq'});
 taper     = keyval('dpss',        varargin); if isempty(taper),    taper   = 'dpss';     end
-pad       = keyval('pad',         varargin); if isempty(pad),      pad     = 0;          end
-time      = keyval('time',        varargin); if isempty(time),     time    = 'max';      end  
+pad       = keyval('pad',         varargin); if isempty(pad),      pad     = [];         end 
+toi       = keyval('toi',         varargin); if isempty(toi),      toi     = 'max';      end  
 timwin    = keyval('timwin',      varargin); % will be defaulted below
-freq      = keyval('freq',        varargin); if isempty(freq),     freq    = 'max';      end  
+foi       = keyval('foi',         varargin); if isempty(foi),      foi     = 'max';      end  
 tapsmofrq = keyval('tapsmofrq',   varargin);
 
+% set n's
+[nchan,nsample] = size(dat);
+
+% determine fsample
+fsample = nsample / (time(end) - time(1));
+
+% zero padding (if pad is empty, no padding wil be performed in the end)
+if pad < (time(end) - time(1))
+  error('the padding that you specified is shorter than the data');
+end
+pad = round(((pad - (time(end) - time(1))) * fsample) / 2);
+prepad  = zeros(1,pad);
+pstpad  = zeros(1,pad);
 
 
+% set fboi and foi
+if isnumeric(foi) % if input is a vector
+fboi   = round(foi ./ (fsample ./ pad)) + 1;
+numboi = size(fboi,2);
+foi    = (fboi-1) ./ (pad / fsample); % boi - 1 because 0 Hz is included in fourier output..... is this going correctly?
+elseif strcmp(foi,'max') % if input was 'max'
+  fboilim = round([0 fsample/2] ./ (fsample ./ pad)) + 1;
+  fboi    = fboilim(1):fboilim(2);
+  numboi = size(fboi,2);
+  foi    = (fboi-1) ./ (pad / fsample);
+end
 
-
-
-
-
+% set tboi and toi
+if isnumeric(toi) % if input is a vector
+elseif strcmp(toi,'max') % if input was 'max'
+end
 
 
 
