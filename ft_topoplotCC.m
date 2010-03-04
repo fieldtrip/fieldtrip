49d70f9e34139c1637aef4f8be38cdc4e826c066
Robert Oostenveld <r.oostenveld@gmail.com>
2010-03-04 14:19:09 +0000

    use the sparse representation, support the plotting of bidirectional arrows
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@662 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_topoplotCC.m
+++ ft_topoplotCC.m
@@ -17,7 +17,7 @@ function cfg = topoplotCC(cfg, freq)
 % The widthparam should be indicated in pixels, e.g. usefull numbers are 1
 % and larger.
 %
-% The alphaparam should be indicated as opacity between 0 (fully transparent) 
+% The alphaparam should be indicated as opacity between 0 (fully transparent)
 % and 1 (fully opaque).
 %
 % See also: PREPARE_LAYOUT, MULTIPLOTCC
@@ -27,38 +27,40 @@ function cfg = topoplotCC(cfg, freq)
 fieldtripdefs
 
 % check if the input data is valid for this function
-freq = checkdata(freq, 'cmbrepresentation', 'full');
+freq = checkdata(freq, 'cmbrepresentation', 'sparse');
 
 % check if the input configuration is valid for this function
 cfg = checkconfig(cfg, 'trackconfig', 'on');
 cfg = checkconfig(cfg, 'required', {'foi', 'layout'});
 
 % set the defaults
-if ~isfield(cfg, 'feedback'), cfg.feedback = 'textbar'; end
-if ~isfield(cfg, 'alphaparam'), cfg.alphaparam = []; end
-if ~isfield(cfg, 'widthparam'), cfg.widthparam = []; end
+if ~isfield(cfg, 'feedback'),   cfg.feedback = 'text';        end
+if ~isfield(cfg, 'alphaparam'), cfg.alphaparam = [];          end
+if ~isfield(cfg, 'widthparam'), cfg.widthparam = [];          end
 if ~isfield(cfg, 'colorparam'), cfg.colorparam = 'cohspctrm'; end
+if ~isfield(cfg, 'newfigure'),  cfg.newfigure = 'yes';        end
+
+if ~isfield(cfg, 'arrowhead'),   cfg.arrowhead = 'no';         end % no, stop, start, both
+if ~isfield(cfg, 'arrowlength'), cfg.arrowlength = 1;          end % relative to the complete line
+if ~isfield(cfg, 'arrowoffset'), cfg.arrowoffset = 0;          end % absolute, should be in the same units as the layout
 
 lay = prepare_layout(cfg, freq);
 
-reflabel  = freq.label;
-chanlabel = freq.label;
-refindx   = match_str(lay.label, reflabel);
-chanindx  = match_str(lay.label, chanlabel);
-nref      = length(refindx);
-nchan     = length(chanindx);
+beglabel = freq.labelcmb(:,1);
+endlabel = freq.labelcmb(:,2);
+ncmb     = size(freq.labelcmb,1);
 
 % select the data to be used in the figure
 fbin = nearest(freq.freq, cfg.foi);
 
 if isfield(freq, cfg.widthparam)
-  widthparam = freq.(cfg.widthparam)(:,:,fbin);
+  widthparam = freq.(cfg.widthparam)(:,fbin);
 else
-  widthparam = ones(nref,nchan);
+  widthparam = ones(ncmb,1);
 end
 
 if isfield(freq, cfg.alphaparam)
-  alphaparam = freq.(cfg.alphaparam)(:,:,fbin);
+  alphaparam = freq.(cfg.alphaparam)(:,fbin);
 else
   alphaparam = [];
 end
@@ -69,10 +71,10 @@ else
   colorparam = [];
 end
 
-k = 0;
-progress('init', cfg.feedback, 'plotting connections...');
+if strcmp(cfg.newfigure, 'yes')
+  figure
+end
 
-figure
 hold on
 
 rgb  = colormap;
@@ -81,72 +83,104 @@ cmax = max(colorparam(:));
 colorparam = (colorparam - cmin)./(cmax-cmin);
 colorparam = round(colorparam * (size(rgb,1)-1) + 1);
 
-for i=1:nref
-  for j=1:nchan
-    if i==j
-      % don't plot the connection with itself
-      continue
+if strcmp(cfg.newfigure, 'yes')
+  % also plot the position of the electrodes
+  plot_vector(lay.pos(:,1), lay.pos(:,2), 'style','k.');
+
+  % also plot the outline, i.e. head shape or sulci
+  if isfield(lay, 'outline')
+    fprintf('solid lines indicate the outline, e.g. head shape or sulci\n');
+    for i=1:length(lay.outline)
+      if ~isempty(lay.outline{i})
+        X = lay.outline{i}(:,1);
+        Y = lay.outline{i}(:,2);
+        plot_line(X, Y, 'color', 'k', 'linewidth', 1.5, 'linestyle', '-');
+      end
     end
-    k = k+1;
+  end
 
-    progress(k/(nref*nchan), 'plotting connection %d from %d (%d -> %d)\n',k, nref*nchan, i, j);
+  % also plot the mask, i.e. global outline for masking the topoplot
+  if isfield(lay, 'mask')
+    fprintf('dashed lines indicate the mask for topograpic interpolation\n');
+    for i=1:length(lay.mask)
+      if ~isempty(lay.mask{i})
+        X = lay.mask{i}(:,1);
+        Y = lay.mask{i}(:,2);
+        % the polygon representing the mask should be closed
+        X(end+1) = X(1);
+        Y(end+1) = Y(1);
+        plot_line(X, Y, 'color', 'k', 'linewidth', 1.5, 'linestyle', '-');
+      end
+    end
+  end
+end % if newfigure
 
-    if widthparam(i,j)>0
-      xbeg = lay.pos(refindx(i),1);
-      ybeg = lay.pos(refindx(i),2);
-      xend = lay.pos(chanindx(j),1);
-      yend = lay.pos(chanindx(j),2);
+% fix the limits for the axis
+axis(axis);
+
+progress('init', cfg.feedback, 'plotting connections...');
+
+for i=1:ncmb
+
+  if strcmp(beglabel{i}, endlabel{i})
+    % skip autocombinations
+    continue
+  end
+
+  progress(i/ncmb, 'plotting connection %d from %d (%s -> %s)\n', i, ncmb, beglabel{i}, endlabel{i});
+
+  if widthparam(i)>0
+    begindx = strmatch(beglabel{i}, lay.label);
+    endindx = strmatch(endlabel{i}, lay.label);
+    xbeg = lay.pos(begindx,1);
+    ybeg = lay.pos(begindx,2);
+    xend = lay.pos(endindx,1);
+    yend = lay.pos(endindx,2);
+    
+    if strcmp(cfg.arrowhead, 'no')
       x = [xbeg xend]';
       y = [ybeg yend]';
       % h = line(x, y);
       h = patch(x, y, 1);
+    else
+      arrowbeg  = [xbeg ybeg];
+      arrowend  = [xend yend];
+      center    = (arrowbeg+arrowend)/2;
+      direction = (arrowend - arrowbeg);
+      direction = direction/norm(direction);
+      offset    = [direction(2) -direction(1)];
+      arrowbeg  = cfg.arrowlength * (arrowbeg-center) + center + cfg.arrowoffset * offset;
+      arrowend  = cfg.arrowlength * (arrowend-center) + center + cfg.arrowoffset * offset;
 
-      if ~isempty(widthparam)
-        set(h, 'LineWidth', widthparam(i,j));
-      end
+      switch cfg.arrowhead
+        case {'yes' 'stop'}
+          h = arrow(arrowbeg, arrowend, 'Ends', 'stop');
+        case 'start'
+          h = arrow(arrowbeg, arrowend, 'Ends', 'start');
+        case 'both'
+          h = arrow(arrowbeg, arrowend, 'Ends', 'both');
+        case 'none'
+          h = arrow(arrowbeg, arrowend, 'Ends', 'none');
+        otherwise
+          error('unsupported value for cfg.arrowhead')
+      end % switch arrowhead
+    end % if arrow
 
-      if ~isempty(alphaparam)
-        set(h, 'EdgeAlpha', alphaparam(i,j));
-      end
-
-      if ~isempty(colorparam)
-        set(h, 'EdgeColor', rgb(colorparam(i,j),:));
-      end
-
+    if ~isempty(widthparam)
+      set(h, 'LineWidth', widthparam(i));
     end
+
+    if ~isempty(alphaparam)
+      set(h, 'EdgeAlpha', alphaparam(i));
+    end
+
+    if ~isempty(colorparam)
+      set(h, 'EdgeColor', rgb(colorparam(i),:));
+    end
+
   end
 end
 progress('close');
-
-% also plot the position of the electrodes
-plot_vector(lay.pos(:,1), lay.pos(:,2), 'style','k.');
-
-% also plot the outline, i.e. head shape or sulci
-if isfield(lay, 'outline')
-  fprintf('solid lines indicate the outline, e.g. head shape or sulci\n');
-  for i=1:length(lay.outline)
-    if ~isempty(lay.outline{i})
-      X = lay.outline{i}(:,1);
-      Y = lay.outline{i}(:,2);
-      plot_line(X, Y, 'color', 'k', 'linewidth', 1.5, 'linestyle', '-');
-    end
-  end
-end
-
-% also plot the mask, i.e. global outline for masking the topoplot
-if isfield(lay, 'mask')
-  fprintf('dashed lines indicate the mask for topograpic interpolation\n');
-  for i=1:length(lay.mask)
-    if ~isempty(lay.mask{i})
-      X = lay.mask{i}(:,1);
-      Y = lay.mask{i}(:,2);
-      % the polygon representing the mask should be closed
-      X(end+1) = X(1);
-      Y(end+1) = Y(1);
-      h = plot_line(X, Y, 'color', 'k', 'linewidth', 1.5, 'linestyle', '-');
-    end
-  end
-end
 
 % get the output cfg
 cfg = checkconfig(cfg, 'trackconfig', 'off', 'checksize', 'yes');
