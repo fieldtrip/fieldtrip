846aca7e768f43d5494fc0fd2f6e3ac16d301f76
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2010-03-05 09:42:30 +0000

    taper computation now works as it should
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@676 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/specest_mtmconvol.m
+++ private/specest_mtmconvol.m
@@ -1,16 +1,17 @@
-function [out,foi,toi] = specest_mtmconvol(dat, time, varargin)
+function [spectrum,ntaper,foi,toi] = specest_mtmconvol(dat, time, varargin)
 
 % SPECEST_MTMCONVOL performs wavelet convolution in the time domain by multiplication in the frequency domain
 %
 %
 % Use as
-%   [out,foi,toi] = specest_mtmfft(dat,time,...)  %%% DECIDE: WHICH INPUT IS REQUIRED AND WHICH WILL BE DEFAULTED?
+%   [spectrum,foi,toi] = specest_mtmfft(dat,time,...)  %%% DECIDE: WHICH INPUT IS REQUIRED AND WHICH WILL BE DEFAULTED?
 %
 %   dat      = matrix of chan*sample
 %   time     = vector, containing time in seconds for each sample
-%   out      = matrix of taper*chan*sample of fourier coefficients
-%   foi      = vector of frequencies in out
-%   toi      = vector of timebins in out
+%   spectrum = matrix of taper*chan*sample of fourier coefficients
+%   ntaper   = vector containing number of tapers per element of foi
+%   foi      = vector of frequencies in spectrum
+%   toi      = vector of timebins in spectrum
 %
 %
 %
@@ -40,38 +41,37 @@ timwin    = keyval('timwin',      varargin); % will be defaulted below
 foi       = keyval('foi',         varargin); if isempty(foi),      foi     = 'max';      end
 tapsmofrq = keyval('tapsmofrq',   varargin);
 
+
 % Set n's
 [nchan,nsample] = size(dat);
 
+
 % Determine fsample
 fsample = nsample / (time(end) - time(1));
+
 
 % Zero padding (if pad is empty, no padding wil be performed in the end)
 if pad < (time(end) - time(1))
   error('the padding that you specified is shorter than the data');
 end
-if isempty(pad) || (pad == (time(end) - time(1))) % if no padding is specified or if padding is equal to current data length
+if isempty(pad) % if no padding is specified padding is equal to current data length
   pad = (time(end)-time(1));
-else
-  pad = round(((pad - (time(end) - time(1))) * fsample) / 2); 
 end
-prepad  = zeros(1,pad);
-postpad = zeros(1,pad);
+prepad  = zeros(1,(round(pad - (time(end)-time(1))) * fsample)/2);
+postpad = zeros(1,(round(pad - (time(end)-time(1))) * fsample)/2);
 
 
 
 % Set fboi and foi and default tapsmofrq
-% fboi = frequency bins of interest (index to be used in fft-output)
-% foi  = frequencies of interest (Hz)
 if isnumeric(foi) % if input is a vector
-  fboi   = round(foi ./ pad) + 1;
-  numboi = size(fboi,2);
+  fboi   = round(foi ./ (fsample ./ (pad * fsample))) + 1;
+  nfboi  = size(fboi,2);
   foi    = (fboi-1) ./ pad; % boi - 1 because 0 Hz is included in fourier output..... is this going correctly?
 elseif strcmp(foi,'max') % if input was 'max'
-  fboilim = round([0 fsample/2] ./ (pad)) + 1;
+  fboilim = round([0 fsample/2] ./ (fsample ./ (pad * fsample))) + 1;
   fboi    = fboilim(1):1:fboilim(2);
-  numboi = size(fboi,2);
-  foi    = (fboi-1) ./ pad;
+  nfboi   = size(fboi,2);
+  foi     = (fboi-1) ./ pad;
 end
 nfoi = length(foi);
 if isempty(tapsmofrq) % default tapsmofrq
@@ -80,8 +80,6 @@ end
 
 
 % Set tboi and toi
-% tboi = time bins of interest (index to be used in fft-output)
-% toi  = time-points of interest (s)
 if isnumeric(toi) % if input is a vector
   tboi = round(toi .* fsample) + 1;
   toi  = round(toi .* fsample) ./ fsample;
@@ -91,16 +89,20 @@ elseif strcmp(toi,'max') % if input was 'max'
 end
 
 
-% time-windows %%% FIXME: IF 0 HZ IS REQUESTED, THIS BREAKS
+% Time-windows
 if isempty(timwin)
   timwin = (1 ./ foi) * 3; % default is 3 cycles of a frequency
 end
-% set number of samples per time-window
-timwinsmp = timwin * fsample;
+% exception for foi = 0
+if foi(1)==0
+  timwin(1) = pad; % timwin for foi = 0 is equal to entire data-length, not sure whether we should do this
+end
+% set number of samples per time-window (timwin is in seconds)
+timwinsmp = round(timwin * fsample);
 
 
 
-% Compute tapers per frequency and compute their fft
+% Compute tapers per frequency, multiply with wavelets and compute their fft
 wltspctrm = cell(nfoi,1);
 ntaper    = zeros(nfoi,1);
 for ifoi = 1:nfoi
@@ -108,50 +110,51 @@ for ifoi = 1:nfoi
   switch taper
     case 'dpss'
       % create a sequence of DPSS tapers, ensure that the input arguments are double precision
-      tap = double_dpss(timwinsmp(ifoi), timwinsmp(ifoi) .* (tapsmofrq(ifoi) ./ fsample));
+      tap = double_dpss(timwinsmp(ifoi), timwinsmp(ifoi) .* (tapsmofrq(ifoi) ./ fsample))';
       % remove the last taper
-      tap = tap(1:(end-1), :);
-      
+      tap = tap(1:(end-1), :);% WHY ON EARTH IS THIS NECESSARY?
+
     case 'sine'
-      tap = sine_taper(timwinsmp(ifoi), timwinsmp(ifoi) .* (tapsmofrq(ifoi) ./ fsample));
+      tap = sine_taper(timwinsmp(ifoi), timwinsmp(ifoi) .* (tapsmofrq(ifoi) ./ fsample))';
       
     case 'alpha'
-      tap = alpha_taper(timwinsmp(ifoi), foi(ifoi)./ fsample);
-      tap = tap./norm(tap);
+      tap = alpha_taper(timwinsmp(ifoi), foi(ifoi)./ fsample)';
+      tap = tap./norm(tap)';
       
     otherwise
       % create a single taper according to the window specification as a replacement for the DPSS (Slepian) sequence
-      tap = window(cfg.taper, timwinsmp(ifoi));
-      tap = tap ./ norm(tap);
+      tap = window(taper, timwinsmp(ifoi))';
+      tap = tap ./ norm(tap,'fro'); % make it explicit that the frobenius norm is being used
   end
   
-  ntaper(ifoi) = size(tap,2);
+  ntaper(ifoi) = size(tap,1);
+  
   % give error/warning about number of tapers
   if isempty(tap)
     error('datalength to short for specified smoothing\ndatalength: %.3f s, smoothing: %.3f Hz, minimum smoothing: %.3f Hz',nsample/fsample,tapsmofrq,fsample/fsample);
-  elseif size(tap,1) == 1
+  elseif (ntaper(ifoi) == 1) && strcmp(taper,'dpss')
     warning('using only one taper for specified smoothing')
   end
   
   
   % Wavelet construction
-  ins      = ceil(nsample ./ 2) - floor(timwinsmp(ifoi) ./ 2);
-  prezero  = zeros(ins,1);
-  postzero = zeros(nsample - ((ins-1) + timwinsmp(ifoi))-1,1);
-  ind      = (0:timwinsmp(ifoi)-1)' .* ((2.*pi./fsample) .* foi(ifoi));
+  tappad   = ceil(nsample ./ 2) - floor(timwinsmp(ifoi) ./ 2);
+  prezero  = zeros(1,tappad);
+  postzero = zeros(1,nsample - ((tappad-1) + timwinsmp(ifoi))-1);
+  angle    = (0:timwinsmp(ifoi)-1)' .* ((2.*pi./fsample) .* foi(ifoi));
   wltspctrm{ifoi} = complex(zeros(size(tap,1),nsample));
   for itaper = 1:ntaper(ifoi)
-    % construct the complex wavelet
-    coswav  = vertcat(prezero, tap(:,itaper) .* cos(ind), postzero);
-    sinwav  = vertcat(prezero, tap(:,itaper) .* sin(ind), postzero);
-    wavelet = complex(coswav, sinwav);
-    % store the fft of the complex wavelet
-    wltspctrm{ifoi}(taplop,:) = fft(wavelet,[],1)';
+    try % this try loop tries to fit the wavelet into wltspctrm, when it's length is smaller than nsample, it the rest is 'filled' with zeros because of above code
+      % if a wavelet is longer than nsample, it doesn't fit and it is kept at zeros, which is translated to NaN's in the output
+      % construct the complex wavelet
+      coswav  = horzcat(prezero, tap(itaper,:) .* cos(angle)', postzero);
+      sinwav  = horzcat(prezero, tap(itaper,:) .* sin(angle)', postzero);
+      wavelet = complex(coswav, sinwav);
+      % store the fft of the complex wavelet
+      wltspctrm{ifoi}(itaper,:) = fft(wavelet,[],1);
+    end
   end
 end
-
-
-
 
 
 
