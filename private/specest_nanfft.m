590787ee0182d2f9d7a8d3caea4a1222e53519df
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2010-03-24 13:27:11 +0000

    changed some minor things like variable names to make it fit in with the other specest (more to do here), and expanded documantation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@821 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/specest_nanfft.m
+++ private/specest_nanfft.m
@@ -1,14 +1,29 @@
-function [y, opt] = specest_nanfft(x, varargin)
+function [spectrum] = specest_nanfft(dat, time, varargin)
 
 % SPECEST_NANFFT computes a fast Fourier transform in the presence of NaNs
 % in the data
 %
 % Use as
-%   [y] = specest_nanfft(x, ...)
+%   [spectrum] = specest_nanfft(dat, ...)
 %
-% Optional arguments should be specified in key-value pairs and can include
+%
+%   dat      = matrix of chan*sample
+%   time     = vector, containing time in seconds for each sample
+%   spectrum = matrix of taper*chan*foi*toi of fourier coefficients
+%
+%
+%
+%
+% Optional arguments should be specified in key-value pairs and can include:
 %   basis      = precomputes set of basis functions (sines/cosines)
 %   datataype  = 0, 1, 2
+%
+%
+%
+%
+% See also SPECEST_MTMFFT, SPECEST_MTMCONVOL, SPECEST_TFR, SPECEST_HILBERT
+
+
 
 % Copyright (C) 2008, Robert Oostenveld
 %
@@ -18,23 +33,26 @@ function [y, opt] = specest_nanfft(x, varargin)
 basis     = keyval('basis',     varargin);
 datatype  = keyval('datatype',  varargin);
 
-[m, n] = size(x);
+% determine the data characteristics
+[nchan, nsample] = size(dat);
+fsample = 1/(time(2)-time(1));
 
-if mod(n,2)==0
+
+if mod(nsample,2)==0
   % the number of samples is even
-  k = n/2+1;
+  k = nsample/2+1;
 else
   % the number of samples is odd
-  k = floor(n/2+1);
+  k = floor(nsample/2+1);
 end
 
 % determine the type of data and thereby the most suitable algorithm to use
-nancount = sum(isnan(x), 1);
+nancount = sum(isnan(dat), 1);
 if isempty(datatype)
   if all(nancount==0)
     % there is no missing data
     datatype = 0;
-  elseif all(nancount==0 | nancount==m)
+  elseif all(nancount==0 | nancount==nchan)
     % the missing data is at the same location for all channels
     datatype = 1;
   else
@@ -48,30 +66,30 @@ if datatype==0
 
 elseif datatype~=0 && isempty(basis)
   % create a seperate set of basis functions for the cosine and sine
-  basis_c = zeros(k, n);
-  basis_s = zeros(k, n);
+  basis_c = zeros(k, nsample);
+  basis_s = zeros(k, nsample);
 
   % create the time axis
-  t = linspace(0, 2*pi, n+1);
+  t = linspace(0, 2*pi, nsample+1);
   t = t(1:end-1);
 
   for w=1:k
     c = cos((w-1)*t);
     s = sin((w-1)*t);
-    if w==1 || (w==(k) && mod(n,2)==0)
+    if w==1 || (w==(k) && mod(nsample,2)==0)
       % the normalization for the lowest (DC) and the highest frequency component is different
-      s = s/(n);
-      c = c/(n);
+      s = s/(nsample);
+      c = c/(nsample);
     else
-      s = s/(n/2);
-      c = c/(n/2);
+      s = s/(nsample/2);
+      c = c/(nsample/2);
     end
     basis_c(w,:) = c;
     basis_s(w,:) = s;
   end
   % concatenate the sine and cosine basis functions
   % leaving the first and last sine functions out, since those are all zero
-  if mod(n,2)==0
+  if mod(nsample,2)==0
     % the number of samples is even -> the last sine wave basis function is zero
     basis = cat(1, basis_c, basis_s(2:end-1, :));
   else
@@ -85,36 +103,36 @@ switch datatype
   case 0
     % there is no missing data
     % use the standard FFT implementation
-    y = fft(x, [], 2);
+    y = fft(dat, [], 2);
 
   case 1
     % the missing data is at the same location for all channels
     % remove that piece from the data and from the basis functions and use linear estimation
     
-    keep = ~isnan(x(1,:));
+    keep = ~isnan(dat(1,:));
 
     if all(~keep)
       % the data is all NaN, no reason to try to estimate the basis
       % functions
-      y = nan(size(x));
+      y = nan(size(dat));
       return
     end
 
     basis = basis(:,keep);
-    x = x(:,keep);
+    dat = dat(:,keep);
 
-    % do the linear estimation based on x=y*basis
-    % y = x / basis;
-    y = x * pinv(basis);
+    % do the linear estimation based on dat=y*basis
+    % y = dat / basis;
+    y = dat * pinv(basis);
 
     % disentagle the estimated components
 
-    if mod(n,2)==0
+    if mod(nsample,2)==0
       % the number of samples is even -> the last sine wave basis function is zero
       sel1 = 1;       % lowest cosine, i.e. DC
       sel2 = 2:(k-1); % all cosines in between
       sel3 = k;       % highest cosine
-      sel4 = (k+1):n; % all sines
+      sel4 = (k+1):nsample; % all sines
 
       est1 = y(:,sel1);
       est2 = y(:,sel2);
@@ -123,14 +141,14 @@ switch datatype
 
       % combine the various estimates into a complex representation compatible with standard FFT
       y_real = cat(2, est1,        est2, est3,          fliplr(est2));
-      y_imag = cat(2, zeros(m,1), -est4, zeros(m,1),    fliplr(est4));
+      y_imag = cat(2, zeros(nchan,1), -est4, zeros(nchan,1),    fliplr(est4));
       y = y_real + i*y_imag;
 
     else
       % the number of samples is odd -> also include the last sine wave basis function
       sel1 = 1;       % lowest cosine, i.e. DC
       sel2 = 2:k;     % all other cosines
-      sel3 = (k+1):n; % all sines
+      sel3 = (k+1):nsample; % all sines
 
       est1 = y(:,sel1);
       est2 = y(:,sel2);
@@ -138,18 +156,22 @@ switch datatype
 
       % combine the various estimates into a complex representation compatible with standard FFT
       y_real = cat(2, est1,        est2, fliplr(est2));
-      y_imag = cat(2, zeros(m,1), -est3, fliplr(est3));
+      y_imag = cat(2, zeros(nchan,1), -est3, fliplr(est3));
       y = y_real + i*y_imag;
     end
 
   case 2
     % the missing data is at different timepoints for different channels
     % use recursion to compute the nanfft for each channel
-    y = zeros(size(x));
-    for k=1:m
-      y(k,:) = specest_nanfft(x(k,:), 'basis', basis);
+    y = zeros(size(dat));
+    for k=1:nchan
+      y(k,:) = specest_nanfft(dat(k,:), 'basis', basis);
     end
 
   otherwise
     error('unsupported configuration of NaNs in the data');
 end
+
+% set output
+spectrum = y;
+
