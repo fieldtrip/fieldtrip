f24a2f9b6792396d0ad8cf6bff21476e121fed72
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2010-03-24 13:11:11 +0000

    added possibility of toi as varargin, added removal of foi==0, and some documentation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@817 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/specest_hilbert.m
+++ private/specest_hilbert.m
@@ -1,7 +1,30 @@
-function [spec, freq, time] = specest_hilbert(dat, time, varargin)
+function [spectrum,foi,toi] = specest_hilbert(dat, time, varargin)
 
 % SPECEST_HILBERT performs a spectral estimation of data by repeatedly
 % applying a bandpass filter and then doing a hilbert transform.
+%
+% Use as
+%   [spectrum,foi,toi] = specest_hilbert(dat,time,...)  
+%
+%   dat      = matrix of chan*sample
+%   time     = vector, containing time in seconds for each sample
+%   spectrum = matrix of taper*chan*foi*toi of fourier coefficients
+%   foi      = vector of frequencies in spectrum
+%   toi      = vector of timebins in spectrum
+%
+%
+%
+%
+% Optional arguments should be specified in key-value pairs and can include:
+%   toi       = vector, containing time points of interest (in seconds)
+%   foi       = vector, containing frequencies (in Hz)
+%   width     = 
+%   filttype  = 
+%   filtorder = 
+%   filtdir   = 
+%
+%
+%
 %
 % See also SPECEST_MTMFFT, SPECEST_MTMCONVOL, SPECEST_TFR
 
@@ -10,21 +33,43 @@ function [spec, freq, time] = specest_hilbert(dat, time, varargin)
 % $Rev$
 
 % get the optional input arguments
+keyvalcheck(varargin, 'optional', {'foi','foi','width','filttype','filtorder','filtdir'});
 foi       = keyval('foi',       varargin);
-width     = keyval('width',     varargin); if isempty(width), width = 1; end
+toi       = keyval('toi',       varargin);   if isempty(toi),      toi     = 'max';      end
+width     = keyval('width',     varargin);   if isempty(width),    width   = 1;          end
 filttype  = keyval('filttype',  varargin);
 filtorder = keyval('filtorder', varargin);
 filtdir   = keyval('filtdir',   varargin);
+
 
 % determine the data characteristics
 [nchans, ntime] = size(dat);
 fsample = 1/(time(2)-time(1));
 
+
 % set a default sampling for the frequencies-of-interest
 if isempty(foi),
   foi = linspace(2*width, (fsample/3), 50);
 end
+% check for foi = 0 and remove it
+if any(foi==0)
+  foi(foi==0) = [];
+end
 nfreq = length(foi);
+
+
+% Set tboi and toi
+if isnumeric(toi) % if input is a vector
+  tboi  = round(toi .* fsample) + 1;
+  ntboi = length(tboi);
+  toi   = round(toi .* fsample) ./ fsample;
+elseif strcmp(toi,'max') % if input was 'max'
+  tboi  = 1:length(time);
+  ntboi = length(tboi);
+  toi   = time;
+end
+
+
 
 % each frequency can have its own width
 if numel(width)==1
@@ -32,12 +77,17 @@ if numel(width)==1
 end
 
 % preallocate the result
-spec = complex(zeros(nchans, nfreq, ntime));
-% these are also returned
-freq = foi;
-time = time;
+spectrum = complex(zeros(nchans, nfreq, ntime));
 
 for i=1:nfreq
   flt = preproc_bandpassfilter(dat, fsample, [foi(i)-width(i) foi(i)+width(i)], filtorder, filttype, filtdir);
-  spec(:,i,:) = transpose(hilbert(transpose(flt)));
+  spectrum(:,i,:) = transpose(hilbert(transpose(flt)));
 end
+
+% get tboi out of spectrum
+spectrum = spectrum(:,:,tboi);
+
+
+
+
+
