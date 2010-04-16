a3009f8817dc49feab5c60cdbd4bcc536174eadb
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2010-04-16 06:52:30 +0000

    added some code to substantially speed up if the original data contains only one trial. in addition, some speed up is provided in the case of multiple original trials, by filling the output matrix blockwise (per original trial) rather than sample wise
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@915 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- public/fetch_data.m
+++ public/fetch_data.m
@@ -11,20 +11,21 @@ function [dat] = fetch_data(data, varargin)
 % Copyright (C) 2008, Esther Meeuwissen
 %
 % Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
-
+    
 % check whether input is data
 data = checkdata(data, 'datatype', 'raw');
-
+    
 % get the options
 hdr           = keyval('header',        varargin);
 begsample     = keyval('begsample',     varargin);
 endsample     = keyval('endsample',     varargin);
 chanindx      = keyval('chanindx',      varargin);
-
+    
 if isempty(hdr)
   hdr = fetch_header(data);
 end
-
+    
+    
 if isempty(begsample) || isempty(endsample)
   error('begsample and endsample must be specified');
 end
@@ -36,70 +37,108 @@ end
 % get trial definition according to original data file
 trl    = findcfg(data.cfg, 'trl');
 trlnum = length(data.trial);
-trllen = zeros(trlnum,1);
-for trllop=1:trlnum
-  trllen(trllop) = size(data.trial{trllop},2);
-end
 
-% check whether data.trial is consistent with trl
-if size(trl,1)~=length(data.trial)
-  error('trial definition is not internally consistent')
-elseif any(trllen~=(trl(:,2)-trl(:,1)+1))
-  error('trial definition is not internally consistent')
-end
-
-minchan = min(chanindx);
-maxchan = max(chanindx);
-if minchan<1 || maxchan>hdr.nChans
-  error('selected channels are not present in the data')
-end
-
-% these are for bookkeeping
-maxsample = max(trl(:,2)); 
-count     = zeros(1, maxsample, 'int32');
-trialnum  = zeros(1, maxsample, 'int32');
-samplenum = zeros(1, maxsample, 'int32');
-
-% determine for each sample in the data where it originates from
-for trllop=1:trlnum
-  trlbeg = trl(trllop,1);
-  trlend = trl(trllop,2);
-  if trlbeg>endsample || trlend<begsample
-    % this piece of data is not interesting because it falls outside the requested range
-    % skip it to speed up the indexing of the trial and sample numbers
-    continue
+if trlnum>1,
+  % original implementation 
+  
+  trllen = zeros(trlnum,1);
+  for trllop=1:trlnum
+    trllen(trllop) = size(data.trial{trllop},2);
   end
-  % make vector with 0= no sample of old data, 1= one sample of old data, 2= two samples of old data, etc
-  count(trlbeg:trlend) = count(trlbeg:trlend) + 1;
-  % make vector with 1's if samples belong to trial 1, 2's if samples belong to trial 2 etc. overlap/ no data --> Nan
-  trialnum(trlbeg:trlend) = trllop;
-  % make samplenum vector with samplenrs for each sample in the old trials
-  samplenum(trlbeg:trlend) = 1:trllen(trllop);
-end
-
-% overlap --> NaN
-%trialnum(count>1)  = NaN;
-%samplenum(count>1) = NaN;
-
-% make a subselection for the desired samples
-count     = count(begsample:endsample);
-trialnum  = trialnum(begsample:endsample);
-samplenum = samplenum(begsample:endsample);
-
-% check if all samples are present and are not present twice or more 
-if any(count==0)
-  warning('not all requested samples are present in the data, filling with NaNs');
-elseif any(count>1)
-  error('some of the requested samples occur twice in the data');
-end
-
-% construct the output data array
-dat = nan(length(chanindx), length(samplenum));
-for smplop=1:length(samplenum)
-  if samplenum(smplop)==0
-   dat(:, smplop) = nan; 
+  
+  % check whether data.trial is consistent with trl
+  if size(trl,1)~=length(data.trial)
+    error('trial definition is not internally consistent')
+  elseif any(trllen~=(trl(:,2)-trl(:,1)+1))
+    error('trial definition is not internally consistent')
+  end
+  
+  minchan = min(chanindx);
+  maxchan = max(chanindx);
+  if minchan<1 || maxchan>hdr.nChans
+    error('selected channels are not present in the data')
+  end
+  
+  % these are for bookkeeping
+  maxsample = max(trl(:,2)); 
+  count     = zeros(1, maxsample, 'int32');
+  trialnum  = zeros(1, maxsample, 'int32');
+  samplenum = zeros(1, maxsample, 'int32');
+  
+  % determine for each sample in the data where it originates from
+  for trllop=1:trlnum
+    trlbeg = trl(trllop,1);
+    trlend = trl(trllop,2);
+    if trlbeg>endsample || trlend<begsample
+      % this piece of data is not interesting because it falls outside the requested range
+      % skip it to speed up the indexing of the trial and sample numbers
+      continue
+    end
+    % make vector with 0= no sample of old data, 1= one sample of old data, 2= two samples of old data, etc
+    count(trlbeg:trlend) = count(trlbeg:trlend) + 1;
+    % make vector with 1's if samples belong to trial 1, 2's if samples belong to trial 2 etc. overlap/ no data --> Nan
+    trialnum(trlbeg:trlend) = trllop;
+    % make samplenum vector with samplenrs for each sample in the old trials
+    samplenum(trlbeg:trlend) = 1:trllen(trllop);
+  end
+  
+  % overlap --> NaN
+  %trialnum(count>1)  = NaN;
+  %samplenum(count>1) = NaN;
+  
+  % make a subselection for the desired samples
+  count     = count(begsample:endsample);
+  trialnum  = trialnum(begsample:endsample);
+  samplenum = samplenum(begsample:endsample);
+  
+  % check if all samples are present and are not present twice or more 
+  if any(count==0)
+    warning('not all requested samples are present in the data, filling with NaNs');
+  elseif any(count>1)
+    error('some of the requested samples occur twice in the data');
+  end
+  
+  % construct the output data array
+  %dat = nan(length(chanindx), length(samplenum));
+  %for smplop=1:length(samplenum)
+  %  if samplenum(smplop)==0
+  %   dat(:, smplop) = nan; 
+  %  else
+  %   dat(:, smplop) = data.trial{trialnum(smplop)}(chanindx,samplenum(smplop)); 
+  %  end
+  %end
+  
+  % the following piece of code achieves the same as the commented code above,
+  % but much smaller. rather than looping over samples it loops over the blocks
+  % of samples defined by the original trials
+  utrl = unique(trialnum);
+  utrl(~isfinite(utrl)) = 0;
+  utrl(utrl==0) = [];
+  if length(utrl)==1,
+    ok   = trialnum==utrl;
+    smps = samplenum(ok);
+    dat(:,ok) = data.trial{utrl}(chanindx,smps);
   else
-   dat(:, smplop) = data.trial{trialnum(smplop)}(chanindx,samplenum(smplop)); 
+    for xlop=1:length(utrl)
+      ok   = trialnum==utrl(xlop);
+      smps = samplenum(ok);
+      dat(:,ok) = data.trial{utrl(xlop)}(chanindx,smps);
+    end
   end
-end
 
+else
+  % only 1 trial present in the input data, so it's quite simple
+  % and can be done fast
+  
+  % check whether the requested samples are present in the input
+  if endsample>trl(2) || begsample<trl(1)
+    error('some of the requested samples are outside the input data')
+  end
+  
+  % get the indices
+  begindx = begsample - trl(1) + 1;
+  endindx = endsample - trl(1) + 1;
+  
+  % fetch the data
+  dat = data.trial{1}(chanindx,begindx:endindx);  
+end
