f98c6e753e728cd67bde4dba1d37695a62b4d82e
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2010-04-27 13:10:31 +0000

    made more consistent with other specest
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@977 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/specest_hilbert.m
+++ private/specest_hilbert.m
@@ -1,23 +1,23 @@
-function [spectrum,foi,toi] = specest_hilbert(dat, time, varargin)
+function [spectrum,freqoi,timeoi] = specest_hilbert(dat, time, varargin)
 
 % SPECEST_HILBERT performs a spectral estimation of data by repeatedly
 % applying a bandpass filter and then doing a hilbert transform.
 %
 % Use as
-%   [spectrum,foi,toi] = specest_hilbert(dat,time,...)  
+%   [spectrum,freqoi,timeoi] = specest_hilbert(dat,time,...)  
 %
 %   dat      = matrix of chan*sample
 %   time     = vector, containing time in seconds for each sample
-%   spectrum = matrix of taper*chan*foi*toi of fourier coefficients
-%   foi      = vector of frequencies in spectrum
-%   toi      = vector of timebins in spectrum
+%   spectrum = matrix of taper*chan*freqoi*timeoi of fourier coefficients
+%   freqoi   = vector of frequencies in spectrum
+%   timeoi   = vector of timebins in spectrum
 %
 %
 %
 %
 % Optional arguments should be specified in key-value pairs and can include:
-%   toi       = vector, containing time points of interest (in seconds)
-%   foi       = vector, containing frequencies (in Hz)
+%   timeoi    = vector, containing time points of interest (in seconds)
+%   freqoi    = vector, containing frequencies (in Hz)
 %   width     = 
 %   filttype  = 
 %   filtorder = 
@@ -33,59 +33,61 @@ function [spectrum,foi,toi] = specest_hilbert(dat, time, varargin)
 % $Rev$
 
 % get the optional input arguments
-keyvalcheck(varargin, 'optional', {'foi','foi','width','filttype','filtorder','filtdir'});
-foi       = keyval('foi',       varargin);
-toi       = keyval('toi',       varargin);   if isempty(toi),      toi     = 'max';      end
+keyvalcheck(varargin, 'optional', {'freqoi','timeoi','width','filttype','filtorder','filtdir'});
+freqoi    = keyval('freqoi',    varargin);
+timeoi    = keyval('timeoi',    varargin);   if isempty(timeoi),   timeoi  = 'all';      end
 width     = keyval('width',     varargin);   if isempty(width),    width   = 1;          end
 filttype  = keyval('filttype',  varargin);
 filtorder = keyval('filtorder', varargin);
 filtdir   = keyval('filtdir',   varargin);
 
 
-% determine the data characteristics
-[nchans, ntime] = size(dat);
+% Set n's
+[nchan,ndatsample] = size(dat);
+
+
+% Determine fsample and set total time-length of data
 fsample = 1/(time(2)-time(1));
 
 
 % set a default sampling for the frequencies-of-interest
-if isempty(foi),
-  foi = linspace(2*width, (fsample/3), 50);
+if isempty(freqoi),
+  freqoi = linspace(2*width, (fsample/3), 50);
 end
-% check for foi = 0 and remove it
-if any(foi==0)
-  foi(foi==0) = [];
+% check for freqoi = 0 and remove it
+if any(freqoi==0)
+  freqoi(freqoi==0) = [];
 end
-nfreq = length(foi);
+nfreq = length(freqoi);
 
 
-% Set tboi and toi
-if isnumeric(toi) % if input is a vector
-  tboi  = round(toi .* fsample) + 1;
-  ntboi = length(tboi);
-  toi   = round(toi .* fsample) ./ fsample;
-elseif strcmp(toi,'max') % if input was 'max'
-  tboi  = 1:length(time);
-  ntboi = length(tboi);
-  toi   = time;
+% Set timeboi and timeoi
+if isnumeric(timeoi) % if input is a vector
+  timeboi  = round(timeoi .* fsample) + 1;
+  ntimeboi = length(timeboi);
+  timeoi   = round(timeoi .* fsample) ./ fsample;
+elseif strcmp(timeoi,'all') % if input was 'all'
+  timeboi  = 1:length(time);
+  ntimeboi = length(timeboi);
+  timeoi   = time;
 end
-
 
 
 % each frequency can have its own width
 if numel(width)==1
-  width = width*ones(size(foi));
+  width = width*ones(size(freqoi));
 end
 
 % preallocate the result
 spectrum = complex(zeros(nchans, nfreq, ntime));
 
 for i=1:nfreq
-  flt = preproc_bandpassfilter(dat, fsample, [foi(i)-width(i) foi(i)+width(i)], filtorder, filttype, filtdir);
+  flt = preproc_bandpassfilter(dat, fsample, [freqoi(i)-width(i) freqoi(i)+width(i)], filtorder, filttype, filtdir);
   spectrum(:,i,:) = transpose(hilbert(transpose(flt)));
 end
 
-% get tboi out of spectrum
-spectrum = spectrum(:,:,tboi);
+% get timeboi out of spectrum
+spectrum = spectrum(:,:,timeboi);
 
 
 
