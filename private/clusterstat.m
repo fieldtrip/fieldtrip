46f4c4e866568a6e36689c1d842b46eab15d1b4e
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2010-05-03 13:30:24 +0000

    applied a workaround to clustering errors with cfg.avgoverchan = 'yes'. The bug (bug 60) was two-fold, not only did clusterstat used incorrect information on channel-numbers (cfg.channel) but the default neighbourselection is still called while avgoverchan = 'yes' (hence, nchan = set to 1 and cfg.neighbours = [] in subfunction: canneighbstructmat). Workaround is applied here to avoid breaking things by changing statistics_wrapper
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1014 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/clusterstat.m
+++ private/clusterstat.m
@@ -490,7 +490,14 @@ function channeighbstructmat = makechanneighbstructmat(cfg);
 % MAKECHANNEIGHBSTRUCTMAT makes the makes the matrix containing the channel
 % neighbourhood structure.
 
-nchan=length(cfg.channel);
+% because clusterstat has no access to the actual data (containing data.label), this workaround is required
+% cfg.neighbours is cleared here because it is not done where avgoverchan is effectuated (it should actually be changed there)
+if strcmp(cfg.avgoverchan, 'no')
+  nchan=length(cfg.channel);
+elseif strcmp(cfg.avgoverchan, 'yes')
+  nchan = 1;
+  cfg.neighbours = [];
+end
 channeighbstructmat = false(nchan,nchan);
 for chan=1:length(cfg.neighbours)
     [seld] = match_str(cfg.channel, cfg.neighbours{chan}.label);
