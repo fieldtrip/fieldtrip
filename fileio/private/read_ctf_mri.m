edc0f04cc652f57a15e34b26aba73bb38c053217
Robert Oostenveld <r.oostenveld@gmail.com>
2010-05-07 07:22:52 +0000

    added some comments to clarify the 202 or 204 byte padding issue explicitely read uint8 and typecast to char
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1047 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/private/read_ctf_mri.m
+++ fileio/private/read_ctf_mri.m
@@ -1,12 +1,13 @@
 function [mri, hdr] = read_ctf_mri(filename);
 
-% READ_CTF_MRI reads header and imnage data from CTF format MRI file
+% READ_CTF_MRI reads header and image data from a CTF version 2.2 MRI file
 %
-% [mri, hdr] = read_ctf_mri(filename)
+% Use as
+%   [mri, hdr] = read_ctf_mri(filename)
 %
-% See also READ_CTF_MEG4, READ_CTF_RES4
+% See also READ_CTF_MRI4
 
-% Copyright (C) 2003 Robert Oostenveld
+% Copyright (C) 2003-2010 Robert Oostenveld
 %
 % This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
 % for the documentation and details.
@@ -36,9 +37,10 @@ end
 % READ THE IMAGE HEADER
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-warning off
+ws = warning('off');
+
 % general header information
-hdr.identifierString = char(fread(fid,32,'char'))'; % CTF_MRI_FORMAT VER 2.2
+hdr.identifierString = fread(fid,[1 32],'uint8=>char'); % CTF_MRI_FORMAT VER 2.2
 hdr.imageSize = fread(fid,1,'int16'); % always = 256
 hdr.dataSize = fread(fid,1,'int16'); % 1 or 2(bytes)
 hdr.clippingRange = fread(fid,1,'int16'); % max.integer value of data
@@ -57,7 +59,7 @@ hdr.HeadModel.LeftEar_Axi = fread(fid,1,'int16'); % left ear - axial
 hdr.HeadModel.RightEar_Sag = fread(fid,1,'int16'); % right ear - sagittal
 hdr.HeadModel.RightEar_Cor = fread(fid,1,'int16'); % right ear - coronal
 hdr.HeadModel.RightEar_Axi = fread(fid,1,'int16'); % right ear - axial
-fread(fid,2,'char'); % padding to 4 byte boundary
+fread(fid,2,'uint8'); % padding to 4 byte boundary
 hdr.HeadModel.defaultSphereX = fread(fid,1,'float'); % sphere origin x coordinate(in mm)
 hdr.HeadModel.defaultSphereY = fread(fid,1,'float'); % sphere origin y coordinate(in mm)
 hdr.HeadModel.defaultSphereZ = fread(fid,1,'float'); % sphere origin z coordinate(in mm)
@@ -65,14 +67,14 @@ hdr.HeadModel.defaultSphereRadius = fread(fid,1,'float'); % default sphere radiu
 
 % Image_Info specific header items
 hdr.Image.modality = fread(fid,1,'int16'); % 0 = MRI, 1 = CT, 2 = PET, 3 = SPECT, 4 = OTHER
-hdr.Image.manufacturerName = char(fread(fid,64,'char'))';
-hdr.Image.instituteName = char(fread(fid,64,'char'))';
-hdr.Image.patientID = char(fread(fid,32,'char'))';
-hdr.Image.dateAndTime = char(fread(fid,32,'char'))';
-hdr.Image.scanType = char(fread(fid,32,'char'))';
-hdr.Image.contrastAgent = char(fread(fid,32,'char'))';
-hdr.Image.imagedNucleus = char(fread(fid,32,'char'))';
-fread(fid,2,'char'); % padding to 4 byte boundary
+hdr.Image.manufacturerName = fread(fid,[1 64],'uint8=>char');
+hdr.Image.instituteName = fread(fid,[1 64],'uint8=>char');
+hdr.Image.patientID = fread(fid,[1 32],'uint8=>char');
+hdr.Image.dateAndTime = fread(fid,[1 32],'uint8=>char');
+hdr.Image.scanType = fread(fid,[1 32],'uint8=>char');
+hdr.Image.contrastAgent = fread(fid,[1 32],'uint8=>char');
+hdr.Image.imagedNucleus = fread(fid,[1 32],'uint8=>char');
+fread(fid,2,'uint8'); % padding to 4 byte boundary
 hdr.Image.Frequency = fread(fid,1,'float');
 hdr.Image.FieldStrength = fread(fid,1,'float');
 hdr.Image.EchoTime = fread(fid,1,'float');
@@ -81,8 +83,8 @@ hdr.Image.InversionTime = fread(fid,1,'float');
 hdr.Image.FlipAngle = fread(fid,1,'float');
 hdr.Image.NoExcitations = fread(fid,1,'int16');
 hdr.Image.NoAcquisitions = fread(fid,1,'int16');
-hdr.Image.commentString = char(fread(fid,256,'char'))';
-hdr.Image.forFutureUse = char(fread(fid,64,'char'))';
+hdr.Image.commentString = fread(fid,[1 256],'uint8=>char');
+hdr.Image.forFutureUse = fread(fid,[1 64],'uint8=>char');
 
 % continuation general header
 hdr.headOrigin_sagittal = fread(fid,1,'float'); % voxel location of head origin
@@ -95,13 +97,23 @@ hdr.rotate_axial = fread(fid,1,'float'); % 3. rotate in axial plane by this angl
 hdr.orthogonalFlag = fread(fid,1,'int16'); % if set then image is orthogonal
 hdr.interpolatedFlag = fread(fid,1,'int16'); % if set than image was interpolated
 hdr.originalSliceThickness = fread(fid,1,'float'); % original spacing between slices before interpolation
-transformMatrix = fread(fid,[4 4],'float')'; % transformation matrix head->MRI[column][row]
-fread(fid,204,'char'); % unused, padding to 1028 bytes
-warning on
+hdr.transformMatrix = fread(fid,[4 4],'float')'; % transformation matrix head->MRI[column][row]
+fread(fid,202,'uint8'); % unused, padding to 1028 bytes
+
+% note that although the 202 bytes padding is according to the CTF
+% documentation, that results in the file pointer being at 1026 and
+% not 1028, which is INconsistent with the CTF documentation.
+
+% turn all warnings back on
+warning(ws);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % READ THE IMAGE DATA
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% explicitely seek to position 1028, the remainder of the file is
+% exactly 256*256*256 times 1 (uint8) or times 2 (uint16) bytes
+fseek(fid, 1028, 'bof');
 
 if hdr.dataSize==1
   mri = uint8(fread(fid, 256*256*256, 'uint8'));
@@ -113,9 +125,13 @@ end
 mri = reshape(mri, [256 256 256]);
 fclose(fid);
 
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % DO POST-PROCESSING
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+transformMatrix = hdr.transformMatrix;
 
 % reorient the image data to obtain corresponding image data and transformation matrix
 mri = permute(mri, [3 1 2]);    % this was determined by trial and error
