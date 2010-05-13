347e7e7e044942b4b5a41799928a5345af0e3626
Vladimir Litvak <v.litvak@ucl.ac.uk>
2010-05-13 10:50:16 +0000

    Bug fix from Laurence Hunt
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1065 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/ft_read_header.m
+++ fileio/ft_read_header.m
@@ -902,7 +902,8 @@ switch headerformat
     if iscontinuous
       raw = fiff_setup_read_raw(filename);
       hdr.nSamples    = raw.last_samp - raw.first_samp + 1; % number of samples per trial
-      hdr.nSamplesPre = -raw.first_samp;                    % represented as negative number in fif file
+      hdr.nSamplesPre = raw.first_samp;                     % this should be kept without a negative sign,
+							    % otherwise conflicts will occur in read_data
       hdr.nTrials     = 1;
       orig.raw        = raw; % keep all the details
 
