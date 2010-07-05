28dbf261b59f847b0062866d9e8562497e9c0c07
Robert Oostenveld <r.oostenveld@gmail.com>
2010-07-05 06:59:27 +0000

    explicitely use \r\n for line endings in the *.vhdr ascii header file
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1350 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/private/write_brainvision_eeg.m
+++ fileio/private/write_brainvision_eeg.m
@@ -68,30 +68,30 @@ end
 fclose(fid);
 
 % open the header file and write the ascii header information
-fid = fopen(headerfile, 'wt');
-fprintf(fid, 'Brain Vision Data Exchange Header File Version 1.0\n');
-fprintf(fid, '; Data created by FieldTrip\n');
-fprintf(fid, '\n');
-fprintf(fid, '[Common Infos]\n');
-fprintf(fid, 'DataFile=%s\n',          datafile);
+fid = fopen(headerfile, 'wb');
+fprintf(fid, 'Brain Vision Data Exchange Header File Version 1.0\r\n');
+fprintf(fid, '; Data created by FieldTrip\r\n');
+fprintf(fid, '\r\n');
+fprintf(fid, '[Common Infos]\r\n');
+fprintf(fid, 'DataFile=%s\r\n',          datafile);
 if ~isempty(markerfile)
-  fprintf(fid, 'MarkerFile=%s\n',      markerfile);
+  fprintf(fid, 'MarkerFile=%s\r\n',      markerfile);
 end
-fprintf(fid, 'DataFormat=%s\n',        hdr.DataFormat);
-fprintf(fid, 'DataOrientation=%s\n',   hdr.DataOrientation);
-fprintf(fid, 'NumberOfChannels=%d\n',  hdr.nChans);
+fprintf(fid, 'DataFormat=%s\r\n',        hdr.DataFormat);
+fprintf(fid, 'DataOrientation=%s\r\n',   hdr.DataOrientation);
+fprintf(fid, 'NumberOfChannels=%d\r\n',  hdr.nChans);
 % Sampling interval in microseconds
-fprintf(fid, 'SamplingInterval=%d\n',  round(1e6/hdr.Fs));
-fprintf(fid, '\n');
-fprintf(fid, '[Binary Infos]\n');
-fprintf(fid, 'BinaryFormat=%s\n',      hdr.BinaryFormat);
-fprintf(fid, '\n');
-fprintf(fid, '[Channel Infos]\n');
+fprintf(fid, 'SamplingInterval=%d\r\n',  round(1e6/hdr.Fs));
+fprintf(fid, '\r\n');
+fprintf(fid, '[Binary Infos]\r\n');
+fprintf(fid, 'BinaryFormat=%s\r\n',      hdr.BinaryFormat);
+fprintf(fid, '\r\n');
+fprintf(fid, '[Channel Infos]\r\n');
 % Each entry: Ch<Channel number>=<Name>,<Reference channel name>,<Resolution in microvolts>,<Future extensions>...
 % Fields are delimited by commas, some fields might be omitted (empty).
 % Commas in channel names should be coded as "\1", but are not supported here
 for i=1:hdr.nChans
-  fprintf(fid, 'Ch%d=%s,,%g\n', i, hdr.label{i}, hdr.resolution(i));
+  fprintf(fid, 'Ch%d=%s,,%g\r\n', i, hdr.label{i}, hdr.resolution(i));
 end
 fclose(fid);
 
