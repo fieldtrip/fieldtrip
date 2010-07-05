d2e4c292b01988012d77fc7fa5d88c906ad14118
Robert Oostenveld <r.oostenveld@gmail.com>
2010-07-05 12:06:55 +0000

    replaced the peercompile script by the better solution that Stefan made for realtime/buffer recompiled the mex files on mentat001 with matlab75
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1353 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- peer/peercompile.m
+++ peer/peercompile.m
@@ -1,10 +1,23 @@
-function peercompile(action)
+function peercompile(cc)
 
-% PEERCOMPILE compile the low-level mex file implements that implements
-% the TCP/IP, announce and discover services.
+% PEERCOMPILE  This script/function is used for compiling and linking the 'peer' MEX file.
+%
+% On Linux and MacOS X, you should just be able to call this function without arguments.
+%
+% On Windows, you need to select a compiler using one of the following options:
+%   compile('bcb')   - Borland C++ Builder
+%   compile('bcc55') - Borland C++ 5.5 (free command line tools)
+%   compile('mingw') - MinGW (GCC port without cygwin dependency)
+%   compile('vc')    - Visual Studio C++ 2005 or 2008
+%
+% Please note that this script does NOT set up your MEX environment for you, so in case
+% you haven't selected a C compiler on Windows yet, you need to type 'mex -setup' first
+% to choose either the Borland or Microsoft compiler. If you want to use MinGW, you also
+% need to install Gnumex (http://gnumex.sourceforget.net), which comes with its own
+% procedure for setting up the MEX environment.
 
 % -----------------------------------------------------------------------
-% Copyright (C) 2010, Robert Oostenveld
+% Copyright (C) 2010, Stefan Klanke
 %
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -20,81 +33,85 @@ function peercompile(action)
 % along with this program.  If not, see <http://www.gnu.org/licenses/
 % -----------------------------------------------------------------------
 
-if ~nargin, action = 'all'; end
+% You can tweak this a bit for setting platform-independent options, e.g for optimisation
+% or debugging. The default is to enable debug information (-g).
+% The -I. is for including the header files.
+cflags = '-I. -g';
 
+% change to the directory containing the source code
 cwd = pwd;
 src = fullfile(fileparts(which(mfilename)),'src');
+cd(src);
 
-switch action
+if ispc
+    % On Windows, we need to link against different libraries
+    % If you are having problems compiling the peer mex file, you can try to tweak
+    % the variables 'extra_cflags' and 'ldflags' specific to your compiler,
+    % or you can add your own compiler flags.
     
-    case 'all'
-        try
-            cd(src);
-            
-            if ispc
-                includes     = '-I..\pthreads-win32\include';
-                libdirs      = '-L..\pthreads-win32\lib';
-                switch upper(computer)
-                    case 'PCWIN32'
-                        libs = '-lpthreadVC2';
-                    case 'PCWIN64'
-                        libs = '-lpthreadGC2';
-                end
-                objext       = '.obj';
-            else
-                includes     = '-I.';
-                libdirs      = '-L.';
-                libs         = '-lpthread';
-                objext       = '.o';
-            end
-            
-            % create object files
-            cfiles = {'announce.c' 'discover.c' 'expire.c' 'extern.c' ...
-                'fairshare.c' 'peerinit.c' 'util.c' 'tcpserver.c' 'tcpsocket.c' ...
-                'security.c' 'localhost.c'};
-            ofiles = cell(1,numel(cfiles));
-            for i=1:numel(cfiles)
-                mex(includes,'-c',cfiles{i});
-                [p, n]    = fileparts(cfiles{i});
-                ofiles{i} = [n objext];
-            end
-            
-            % compile the peer gateway
-            mex(includes, libdirs, '-outdir', '..', 'peer.c', ofiles{:}, libs);
-            
-            % compile the memory profiler
-            mex(includes, libdirs, '-outdir', '..', 'memprofile.c', libs);
-            
-            % compile the delayed exit
-            mex(includes, libdirs, '-outdir', '..', 'delayedexit.c', libs);
-            
-            cd(cwd);
-        catch
-            cd(cwd);
-            rethrow(lasterror);
-        end
-        
-    case 'clean'
-        try
-            cd(src);
-            delete('*.o*');
-            cd(cwd);
-        catch
-            cd(cwd);
-            rethrow(lasterror);
-        end
-        
-    case 'distclean'
-        peercompile('clean');
-        try
-            cd(fullfile(src,'..'));
-            delete(['*.' mexext]);
-            cd(cwd);
-        catch
-            cd(cwd);
-            rethrow(lasterror);
-        end
-        
-    otherwise
-        error('Unknown action.');
+    extra_cflags = '-I../pthreads-win32/include';
+    suffix = 'obj';
+    
+    if nargin<1	        % this is just to make sure the switch statement works
+        cc = 'NONE';    % but you can also put a default entry here
+    end
+    switch upper(cc)
+        case 'BCB'
+            ldflags = '-L../src -L../pthreads-win32/lib -lpthreadVC2.bcb';
+        case 'BCC55'
+            ldflags = '-L../src -L../pthreads-win32/lib -lpthreadVC2_bcc55';
+        case 'MINGW'
+            %ldflags = '-L../src -L../pthreads-win32/lib -lpthreadGC2';
+            % For MinGW/Gnumex, it seems to be easier to just directly refer to the archives
+            ldflags = '../pthreads-win32/lib/libpthreadGC2.a C:/mingw/lib/libws2_32.a';
+        case 'VC'
+            ldflags = '-L../src -L../pthreads-win32/lib -lpthreadVC2 ';
+        otherwise
+            error 'On a PC, you must call this function with a string argument to select a compiler';
+    end
+else
+    % On POSIX systems such as MacOS X and Linux, the following should work without tweaking
+    ldflags = '';
+    extra_cflags = '';
+    suffix = 'o';
 end
+
+% If you want to add a new helper function to the MEX file, you should just add
+% its name here (without the .c suffix)
+helpers = {'announce' 'discover' 'expire' 'extern' ...
+    'fairshare' 'peerinit' 'util' 'tcpserver' 'tcpsocket' ...
+    'security' 'localhost'};
+
+%%%% Please do not change anything below this line %%%%
+
+% this will become the list of objects files for inclusion during linking
+allObjects = '';
+
+% This is for compiling all the helper functions (no linking yet).
+for i=1:length(helpers)
+    fprintf(1,'Compiling helper functions in %s ...\n', helpers{i});
+    cmd = sprintf('mex -c %s %s %s.c', cflags, extra_cflags, helpers{i});
+    disp(cmd);
+    eval(cmd);
+    
+    % append newly created object file to the list of files we need to link
+    allObjects = sprintf('%s %s.%s', allObjects, helpers{i}, suffix);
+end
+
+% This is for compiling peer.c and linking everything into the MEX file.
+fprintf(1,'Compiling and linking MEX file:\n');
+cmd = sprintf('mex -outdir .. %s %s peer.c %s %s',cflags,extra_cflags,allObjects,ldflags);
+disp(cmd);
+eval(cmd);
+
+cmd = sprintf('mex -outdir .. %s %s memprofile.c %s',cflags,extra_cflags,ldflags);
+disp(cmd);
+eval(cmd);
+
+cmd = sprintf('mex -outdir .. %s %s delayedexit.c %s',cflags,extra_cflags,ldflags);
+disp(cmd);
+eval(cmd);
+
+% change back to the original directory
+cd(cwd);
+
