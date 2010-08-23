032829791e527a8e52af1961648d8c7fedec04dd
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2010-08-23 07:12:05 +0000

    changed input variable from cfg and data matrix into data matrix and key-value pairs.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1539 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- connectivity/ft_connectivity_dtf.m
+++ connectivity/ft_connectivity_dtf.m
@@ -1,4 +1,10 @@
-function [dtf, dtfvar, n] = ft_connectivity_dtf(cfg, input, hasjack)
+function [dtf, dtfvar, n] = ft_connectivity_dtf(input, varargin)
+
+hasjack = keyval('hasjack', varargin{:}); if isempty(hasjack), hasjack = 0; end
+powindx = keyval('powindx', varargin{:});
+% FIXME build in feedback
+% FIXME build in proper documentation
+% FIXME build in dDTF etc
 
 siz    = size(input);
 n      = siz(1);
@@ -6,18 +12,17 @@ ncmb   = siz(2);
 outsum = zeros(siz(2:end));
 outssq = zeros(siz(2:end));
 
-if isempty(cfg.powindx)
+if isempty(powindx)
   % data are represented as chan_chan_therest
   for j = 1:n
     tmph   = reshape(input(j,:,:,:,:), siz(2:end));
     den    = sum(abs(tmph).^2,2);
     tmpdtf = abs(tmph)./sqrt(repmat(den, [1 siz(2) 1 1 1]));
-    %if ~isempty(cfg.submethod), tmpdtf = baseline(tmpdtf, cfg.submethod, baselineindx); end
     outsum = outsum + tmpdtf;
     outssq = outssq + tmpdtf.^2;
     %tmp    = outsum; tmp(2,1,:,:) = outsum(1,2,:,:); tmp(1,2,:,:) = outsum(2,1,:,:); outsum = tmp;
     %tmp    = outssq; tmp(2,1,:,:) = outssq(1,2,:,:); tmp(1,2,:,:) = outssq(2,1,:,:); outssq = tmp;
-    % swap the order of the cross-terms to achieve the convention such that
+    % FIXME swap the order of the cross-terms to achieve the convention such that
     % labelcmb {'a' 'b'} represents: a->b
   end
 else
@@ -31,14 +36,14 @@ else
     indx    = [iauto1 icross2 icross1 iauto2];
     
     if isempty(intersect(sortindx, sort(indx), 'rows')),
-      sortindx = [sortindx;sort(indx)];
+      sortindx = [sortindx; sort(indx)];
       for j = 1:n
         tmph    = reshape(input(j,indx,:,:), [2 2 siz(3:end)]);
         den     = sum(abs(tmph).^2,2);
         tmpdtf  = reshape(abs(tmph)./sqrt(repmat(den, [1 2 1 1])), [4 siz(3:end)]);
         outsum(indx,:) = outsum(indx,:) + tmpdtf([1 3 2 4],:);
         outssq(indx,:) = outssq(indx,:) + tmpdtf([1 3 2 4],:).^2;
-        % swap the order of the cross-terms to achieve the convention such that
+        % FIXME swap the order of the cross-terms to achieve the convention such that
         % labelcmb {'a' 'b'} represents: a->b
       end
     end
@@ -47,7 +52,11 @@ end
 dtf = outsum./n;
 
 if n>1, %FIXME this is strictly only true for jackknife, otherwise other bias is needed
-  bias   = (n - 1).^2;
+  if hasjack,
+    bias = (n - 1).^2;
+  else
+    bias = 1;
+  end
   dtfvar = bias.*(outssq - (outsum.^2)/n)./(n-1);
 else
   dtfvar = [];
