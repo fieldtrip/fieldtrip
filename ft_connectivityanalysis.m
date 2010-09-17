322a3fc8e1ea97e0d766470e4019fe8f785f55a1
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2010-09-17 15:06:05 +0000

    moved subfunction univariate2bivariate to separate function in connectivity module. changed input arguments into key-value pairs rather than fixed order hard coded
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1705 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_connectivityanalysis.m
+++ ft_connectivityanalysis.m
@@ -203,10 +203,10 @@ if any(~isfield(data, inparam)) || (isfield(data, 'crsspctrm') && (ischar(inpara
     case 'freq'
       if strcmp(inparam, 'crsspctrm')
         if isfield(data, 'fourierspctrm')
-          [data, powindx, hasrpt] = univariate2bivariate(data, 'fourierspctrm', 'crsspctrm', dtype, 0, cfg.channelcmb);
+          [data, powindx, hasrpt] = univariate2bivariate(data, 'fourierspctrm', 'crsspctrm', dtype, 'cmb', cfg.channelcmb);
         elseif strcmp(inparam, 'crsspctrm') && isfield(data, 'powspctrm')
           % if input data is old-fashioned, i.e. contains powandcsd
-          [data, powindx, hasrpt] = univariate2bivariate(data, 'powandcsd', 'crsspctrm', dtype, 0, cfg.channelcmb);
+          [data, powindx, hasrpt] = univariate2bivariate(data, 'powandcsd', 'crsspctrm', dtype, 'cmb', cfg.channelcmb);
         elseif isfield(data, 'labelcmb')
           powindx = labelcmb2indx(data.labelcmb);
         else 
@@ -214,18 +214,18 @@ if any(~isfield(data, inparam)) || (isfield(data, 'crsspctrm') && (ischar(inpara
         end
       elseif strcmp(inparam, 'powcovspctrm')
         if isfield(data, 'powspctrm'),
-          [data, powindx] = univariate2bivariate(data, 'powspctrm', 'powcovspctrm', dtype, strcmp(cfg.removemean,'yes'), cfg.channelcmb, strcmp(cfg.method,'amplcorr'));
+          [data, powindx] = univariate2bivariate(data, 'powspctrm', 'powcovspctrm', dtype, 'demeanflag', strcmp(cfg.removemean,'yes'), 'cmb', cfg.channelcmb, 'sqrtflag', strcmp(cfg.method,'amplcorr'));
         elseif isfield(data, 'fourierspctrm'),
-          [data, powindx] = univariate2bivariate(data, 'fourierspctrm', 'powcovspctrm', dtype, strcmp(cfg.removemean,'yes'), cfg.channelcmb, strcmp(cfg.method,'amplcorr'));
+          [data, powindx] = univariate2bivariate(data, 'fourierspctrm', 'powcovspctrm', dtype, 'demeanflag', strcmp(cfg.removemean,'yes'), 'cmb', cfg.channelcmb, 'sqrtflag', strcmp(cfg.method,'amplcorr'));
         end
       end
     case 'source'
       if strcmp(inparam, 'crsspctrm')
-        [data, powindx, hasrpt] = univariate2bivariate(data, 'mom', 'crsspctrm', dtype, 0, cfg.refindx, [], 0);
+        [data, powindx, hasrpt] = univariate2bivariate(data, 'mom', 'crsspctrm', dtype, 'cmb', cfg.refindx, 'keeprpt', 0);
         %[data, powindx, hasrpt] = univariate2bivariate(data, 'fourierspctrm', 'crsspctrm', dtype, 0, cfg.refindx, [], 1);
       elseif strcmp(inparam, 'powcov')
         data            = checkdata(data, 'haspow', 'yes');
-        [data, powindx] = univariate2bivariate(data, 'pow', 'powcov', dtype, strcmp(cfg.removemean,'yes'), cfg.refindx, strcmp(cfg.method,'amplcorr'), 0);
+        [data, powindx] = univariate2bivariate(data, 'pow', 'powcov', dtype, 'demeanflag', strcmp(cfg.removemean,'yes'), 'cmb', cfg.refindx, 'sqrtflag', strcmp(cfg.method,'amplcorr'), 'keeprpt', 0);
       end
     otherwise
   end
@@ -785,173 +785,5 @@ for k = 1:nchan
   indx(sel2,2) = autoindx;
 end
 
-
 %------------------------------------------------------------------------------------------------------------------
-function [data, powindx, hasrpt] = univariate2bivariate(data, inparam, outparam, dtype, demeanflag, cmb, sqrtflag, keeprpt)
-
-if nargin<8, keeprpt    = 1;  end
-if nargin<7, sqrtflag   = 0;  end
-if nargin<6, cmb        = []; end
-if nargin<5, demeanflag = 0;  end
-
-switch dtype
-  case 'freq'
-    ncmb  = size(cmb,1);
-    nchan = numel(data.label);
-    getpowindx = 0;
-    if ncmb==0,
-      error('no channel combinations are specified');
-    elseif ncmb==nchan^2 || ncmb==(nchan+1)*nchan*0.5,
-      dofull = 1;
-    else
-      dofull = 0;
-    end
-    
-    if strcmp(inparam, 'fourierspctrm') && strcmp(outparam, 'crsspctrm'),
-      %fourier coefficients -> cross-spectral density
-      if dofull
-        data    = checkdata(data, 'cmbrepresentation', 'full');
-      else
-        data    = checkdata(data, 'cmbrepresentation', 'sparse', 'channelcmb', cmb);
-      end
-    elseif strcmp(inparam, 'powandcsd') && strcmp(outparam, 'crsspctrm'),
-      if ~isempty(cmb),
-        data    = checkdata(data, 'cmbrepresentation', 'sparse', 'channelcmb', cmb);
-        
-        %ensure getting powindx later on to prevent crash
-        getpowindx = 1;
-      else
-        %data    = checkdata(data, 'cmbrepresentation', 'full');
-        %this should not be possible
-        error('cannot convert to a full csd representation');
-        
-        
-      end
-    elseif strcmp(inparam, 'fourierspctrm') && strcmp(outparam, 'powcovspctrm'),
-      %fourier coefficients -> power covariance
-      data = checkdata(data, 'cmbrepresentation', 'sparsewithpow', 'channelcmb', {});
-      
-      if sqrtflag, data.powspctrm = sqrt(data.powspctrm); end
-      
-      %get covariance by using checkdata
-      if demeanflag,
-        nrpt = size(data.powspctrm,1);
-        mdat = nanmean(data.powspctrm,1);
-        data.powspctrm = data.powspctrm - mdat(ones(1,nrpt),:,:,:,:,:);
-      end
-      data.fourierspctrm = data.powspctrm; %this is necessary for checkdata to work
-      data.dimord        = ['rpttap',data.dimord(4:end)];
-      data               = rmfield(data, 'powspctrm');
-      data.cumtapcnt(:)  = 1;
-      data.cumsumcnt(:)  = 1;
-      if ncmb < (nchan-1)*nchan*0.5,
-        data    = checkdata(data, 'cmbrepresentation', 'sparse', 'channelcmb', cmb);
-      else
-        data    = checkdata(data, 'cmbrepresentation', 'full');
-      end
-      data.powcovspctrm = data.crsspctrm;
-      data              = rmfield(data, 'crsspctrm');
-    elseif strcmp(inparam, 'powspctrm') && strcmp(outparam, 'powcovspctrm'),
-      %power-spectral density -> power covariance
-      
-      if sqrtflag, data.powspctrm = sqrt(data.powspctrm); end
-      
-      %get covariance by using checkdata
-      if demeanflag,
-        nrpt = size(data.powspctrm,1);
-        mdat = nanmean(data.powspctrm,1);
-        data.powspctrm = data.powspctrm - mdat(ones(1,nrpt),:,:,:,:,:);
-      end
-      data.fourierspctrm = data.powspctrm; %this is necessary for checkdata to work
-      data.dimord        = ['rpttap',data.dimord(4:end)];
-      data               = rmfield(data, 'powspctrm');
-      data.cumtapcnt(:)  = 1;
-      data.cumsumcnt(:)  = 1;
-      if ncmb < (nchan-1)*nchan*0.5,
-        data    = checkdata(data, 'cmbrepresentation', 'sparse', 'channelcmb', cmb);
-      else
-        data    = checkdata(data, 'cmbrepresentation', 'full');
-      end
-      data.powcovspctrm = data.crsspctrm;
-      data = rmfield(data, 'crsspctrm');
-    else
-      error('unknown conversion from univariate to bivariate representation');
-    end
-    
-    if ~isempty(cmb) && (ncmb < (nchan-1)*nchan*0.5 || getpowindx==1),
-      powindx = labelcmb2indx(data.labelcmb);
-    else
-      powindx = [];
-    end
-  case 'source'
-    ncmb = numel(cmb);
-    
-    if strcmp(inparam, 'pow') && strcmp(outparam, 'powcov'),
-      [nrpt,nvox] = size(data.pow);
-      if sqrtflag, data.pow = sqrt(data.pow); end
-      if demeanflag,
-        mdat = nanmean(data.pow,1);
-        data.pow = data.pow - mdat(ones(1,nrpt),:); %FIXME only works for 1 frequency
-      end
-      
-      data.powcov = [data.pow .* data.pow(:,ones(1,nvox)*cmb) data.pow.*data.pow];
-      data        = rmfield(data, 'pow');
-      powindx     = [nvox+(1:nvox) nvox+(1:nvox); cmb*ones(1,nvox) nvox+(1:nvox)]';
-      
-      data.pos    = [data.pos repmat(data.pos(cmb,:),[nvox 1]);data.pos data.pos];
-      data.inside = [data.inside(:); data.inside(:)+nvox];
-      data.outside = [data.outside(:); data.outside(:)+nvox];
-      data.dim(2) = size(data.pos,1);
-    elseif strcmp(inparam, 'mom') && strcmp(outparam, 'crsspctrm'),
-      %get mom as rpttap_pos_freq matrix
-      %FIXME this assumes only 1 freq bin
-      mom = zeros(size(data.mom{data.inside(1)},1), size(data.pos,1));
-      mom(:, data.inside) = cat(2, data.mom{data.inside});
-      if keeprpt,
-        [nrpt,nvox]    = size(mom);
-        data.crsspctrm = [mom.*conj(mom(:,ones(1,nvox)*cmb)) abs(mom).^2];
-        data           = rmfield(data, 'mom');
-        data           = rmfield(data, 'momdimord');
-        powindx     = [nvox+(1:nvox) nvox+(1:nvox); cmb*ones(1,nvox) nvox+(1:nvox)]';
-        
-        data.pos    = [data.pos repmat(data.pos(cmb,:),[nvox 1]);data.pos data.pos];
-        data.inside = [data.inside(:); data.inside(:)+nvox];
-        data.outside = [data.outside(:); data.outside(:)+nvox];
-      elseif ncmb<size(mom,2)
-        %do it computationally more efficient
-        [nrpt,nvox]    = size(mom);
-        
-        data.crsspctrm = reshape((transpose(mom)*conj(mom(:,cmb)))./nrpt, [nvox*ncmb 1]);
-        tmppow         = mean(abs(mom).^2)';
-        data.crsspctrm = cat(1, data.crsspctrm, tmppow);
-        tmpindx1       = transpose(ncmb*nvox + ones(ncmb+1,1)*(1:nvox));
-        tmpindx2       = repmat(tmpindx1(cmb(:),end), [1 nvox])';
-        tmpindx3       = repmat(cmb(:), [1 nvox])'; %expressed in original voxel indices
-        powindx        = [tmpindx1(:) [tmpindx2(:);tmpindx1(:,end)]];
-        
-        data.pos       = [repmat(data.pos, [ncmb 1]) data.pos(tmpindx3(:),:); data.pos data.pos];
-        data.inside    = data.inside(:)*ones(1,ncmb+1) + (ones(length(data.inside),1)*nvox)*(0:ncmb);
-        data.inside    = data.inside(:);
-        data.outside   = setdiff((1:nvox*(ncmb+1))', data.inside);
-        if isfield(data, 'momdimord'),
-          data.crsspctrmdimord = ['pos_',data.momdimord(14:end)];%FIXME this assumes dimord to be 'rpttap_...'
-        end
-        data           = rmfield(data, 'mom');
-        data           = rmfield(data, 'momdimord');
-      else
-        [nrpt,nvox]    = size(mom);
-        data.crsspctrm = (transpose(mom)*conj(mom))./nrpt;
-        data           = rmfield(data, 'mom');
-        data           = rmfield(data, 'momdimord');
-        powindx        = [];
-        data.crsspctrmdimord = 'pos_pos_freq'; %FIXME hard coded
-      end
-      data.dimord = data.crsspctrmdimord;
-      clear mom;
-    else
-      error('unknown conversion from univariate to bivariate representation');
-    end
-  otherwise
-end
-
-hasrpt  = (isfield(data, 'dimord') && ~isempty(strfind(data.dimord, 'rpt')));
+%function [data, powindx, hasrpt] = univariate2bivariate(data, inparam, outparam, dtype, demeanflag, cmb, sqrtflag, keeprpt)
