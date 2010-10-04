e6e6690950531978da02654e6742505f896c5bdb
Stefan Klanke <s.klanke@donders.ru.nl>
2010-10-04 15:20:41 +0000

    Exchanged using read_ctf_res4 by readCTFds in part of code that reads a .res4 chunk from the FieldTrip buffer. Will actually call ft_read_header recursively now to avoid code duplication. Also added support for split sample files (>2GB) in reading fcdc_buffer_offline data.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1829 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/ft_read_header.m
+++ fileio/ft_read_header.m
@@ -731,16 +731,49 @@ switch headerformat
 	
 	% add the contents of attached .res4 file to the .orig field similar to offline data
 	if isfield(orig, 'ctf_res4')
-		tmp_name = tempname;
-		F = fopen(tmp_name, 'wb');
-		fwrite(F, orig.ctf_res4, 'uint8');
-		fclose(F);
-		R4F = read_ctf_res4(tmp_name);
-		delete(tmp_name);
+		if 0
+			% using  READ_CTF_RES4 -- this does not produce a proper .grad structure
+			% TODO: remove this code, and possibly read_ctf_res4 as well
+			tmp_name = tempname;
+			F = fopen(tmp_name, 'wb');
+			fwrite(F, orig.ctf_res4, 'uint8');
+			fclose(F);
+			R4F = read_ctf_res4(tmp_name);
+			delete(tmp_name);
+		else
+			% using FT_READ_HEADER recursively, and then readCTFds in the second call
+			% this will also call ctf2grad and yield correct gradiometer information
+			tmp_name = tempname;
+			[dirname, fname] = fileparts(tmp_name);
+			res4fn = [tmp_name '.ds/' fname '.res4'];
+			meg4fn = [tmp_name '.ds/' fname '.meg4'];
+			dsname = [tmp_name '.ds'];
+			
+			mkdir(dsname);
+			
+			F = fopen(res4fn, 'wb');
+			fwrite(F, orig.ctf_res4, 'uint8');
+			fclose(F);
+			
+			F = fopen(meg4fn, 'wb');
+			fwrite(F, 'MEG42CP');
+			fclose(F);
+			
+			%R4F = read_ctf_res4(tmp_name);
+			R4F = ft_read_header(dsname);
+			
+			delete(res4fn);
+			delete(meg4fn);
+			rmdir(dsname);
+		end
+		
 		% copy over the labels
 		hdr.label = R4F.label;
 		% copy over the 'original' header
 		hdr.orig = R4F;
+		if isfield(R4F,'grad')
+			hdr.grad = R4F.grad;
+		end
 		% add the raw chunk as well
 		hdr.orig.ctf_res4 = orig.ctf_res4;
 	end
--- fileio/private/read_buffer_offline_data.m
+++ fileio/private/read_buffer_offline_data.m
@@ -5,13 +5,54 @@ function dat = read_buffer_offline_data(datafile, hdr, range)
 
 % (C) 2010 S. Klanke
 
-F = fopen(datafile,'rb',hdr.orig.endianness);
-
-nStart = range(1)-1; % 0-based offset in samples
-nStart = nStart * hdr.orig.wordsize * hdr.nChans;
-nRead = range(2)+1-range(1);
-
 type2type = [hdr.orig.data_type '=>' hdr.orig.data_type];
-fseek(F, nStart, 'bof');
-dat = fread(F,[hdr.nChans nRead], type2type);
-fclose(F);
+nStart = range(1)-1; % 0-based offset in samples
+bStart = nStart * hdr.orig.wordsize * hdr.nChans;
+nRead = range(2)+1-range(1);
+bRead = nRead * hdr.orig.wordsize * hdr.nChans;
+
+k = 0;
+sizeSoFar = 0;
+% start with name_k = datafile (= '.../samples' )
+name_k = datafile;
+
+while true
+  F = fopen(datafile,'rb',hdr.orig.endianness);
+  fseek(F, 0, 'eof');
+  size_k = ftell(F);
+   
+  if nStart >= sizeSoFar && nStart < sizeSoFar + size_k
+    fseek(F, bStart - sizeSoFar, 'bof');
+    if bStart + bRead <= sizeSoFar + size_k
+      % desired region is completely contained in this file
+	  dat = fread(F,[hdr.nChans nRead], type2type);
+	  bReadRemain = 0;
+	else
+	  % desired region starts in this file, but then goes on
+	  bRead_k = sizeSoFar + size_k - bStart;
+	  nRead_k = bRead_k / (hdr.orig.wordsize * hdr.nChans);
+	  
+	  dat = fread(F,[hdr.nChans nRead_k], type2type);
+	  bReadRemain = nRead - nRead_k;
+	end
+  else
+    fseek(F, 0, 'bof');
+	if bReadRemain <= size_k
+	  % this is the last file we need
+	  bRead_k = bReadRemain;
+	  bReadRemain = 0;
+	else 
+	  bRead_k = size_k;
+	  bReadRemain = bReadRemain - size_k;
+	end
+	nRead_k = bRead_k / (hdr.orig.wordsize * hdr.nChans);
+	dat = [dat fread(F,[hdr.nChans nRead_k], type2type)];
+  end	  
+  fclose(F);		 
+  if bReadRemain == 0
+     break;
+  end
+  % update name_k for next iteration
+  k=k+1;
+  name_k = sprintf('%s%i', datafile, k);
+end
