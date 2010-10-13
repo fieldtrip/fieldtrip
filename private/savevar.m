22bde494e2919574dda0cc0276ba5daf838d8f36
Craig Richter <craiggrichter@gmail.com>
2010-10-13 10:01:18 +0000

    added -v7.3 switch for saving large files
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@1921 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/savevar.m
+++ private/savevar.m
@@ -1,6 +1,6 @@
 function savevar(filename, varname, value)
 
-% LOADVAR is a helper function for cfg.inputfile
+% SAVEVAR is a helper function for cfg.outputfile
 
 % Copyright (C) 2010, Robert Oostenveld
 %
@@ -9,4 +9,4 @@ function savevar(filename, varname, value)
 fprintf('writing ''%s'' to file ''%s''\n', varname, filename);
 
 eval(sprintf('%s = value;', varname));
-save(filename, varname);
+save(filename, varname, '-v7.3');
