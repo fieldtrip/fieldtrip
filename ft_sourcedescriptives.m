5dc6aad0c50ff233e73a3a5373ab5395249bd763
Arjen Stolk <a.stolk@donders.ru.nl>
2010-11-03 10:04:24 +0000

    changed savevar(~,data) to savevar(~,source)
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2060 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_sourcedescriptives.m
+++ ft_sourcedescriptives.m
@@ -869,7 +869,7 @@ source.cfg = cfg;
 
 % the output data should be saved to a MATLAB file
 if ~isempty(cfg.outputfile)
-  savevar(cfg.outputfile, 'data', source); % use the variable name "data" in the output file
+  savevar(cfg.outputfile, 'source', source); % use the variable name "data" in the output file
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
