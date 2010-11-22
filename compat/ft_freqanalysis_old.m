bfec545da95be87dbf68b836e7c49861bc7e6c0a
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2010-11-22 14:20:14 +0000

    forgot some comment changes relating to previous commit
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2152 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- compat/ft_freqanalysis_old.m
+++ compat/ft_freqanalysis_old.m
@@ -18,9 +18,9 @@ function [freq] = ft_freqanalysis_old(cfg, data)
 %                     based on multiplication in the frequency domain
 %                    'mtmwelch', performs frequency analysis using Welch's averaged
 %                     modified periodogram method of spectral estimation
-%                    'wltconvol', implements wavelet time frequency transformation
+%                    'wavelet', implements wavelet time frequency transformation
 %                     (using Morlet wavelets) based on multiplication in the frequency domain
-%                    'tfr', implements wavelet time frequency transformation
+%                    'convol', implements wavelet time frequency transformation
 %                     (using Morlet wavelets) based on convolution in the time domain
 %
 %
