ff4dccfd43b71154968ccaec60eed4e72a2c168c
Robert Oostenveld <r.oostenveld@gmail.com>
2010-11-29 17:17:00 +0000

    some code cleanup and more strict checking on correct use of the units and user input
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2223 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_checkcoordsys.m
+++ ft_checkcoordsys.m
@@ -1,35 +1,32 @@
 function [varargout] = ft_checkcoordsys(cfg, varargin)
 
-% FT_CHECKCOORDSYS allows the user t o perform  a check on the coordinatesystem
-% and units in which the input object is expressed. 
+% FT_CHECKCOORDSYS allows the user to perform  a check on the coordinatesystem
+% and units in which the input object is expressed.
 %
 % Use as
 %   [output] = ft_checkcoordsys(cfg, data)
 % or as
 %   [output1, output2, ...] = ft_checkcoordsys(cfg, data1, data2, ...)
 %
-% 
+% See also FT_VOLUMEREALIGN, FT_VOLUMERESLICE
 
 ndata   = numel(varargin);
 nargout = ndata;
+
 for k = 1:ndata
   dtype{k} = datatype(varargin{k});
-
-  % try to ensure the data to have a unit FIXME consider to move this to checkdata
-  if k==1,
-    try,
-      varargin{k} = ft_convert_units(varargin{k});
-    end
-    unit = varargin{k}.unit;
-  else
-    try,
-      varargin{k} = ft_convert_units(varargin{k}, unit);
-    end
-  end
 end
 
-if exist('unit', 'var')
-  switch unit
+% determine the geometrical units of the first object
+varargin{1} = ft_convert_units(varargin{1});
+unit = varargin{1}.unit;
+for k = 2:ndata
+  % convert all subsequent objects to the same units as the first
+  varargin{k} = ft_convert_units(varargin{k}, unit);
+end
+
+% determine the size of the "unit" sphere in the origin and the length of the axes
+switch unit
   case 'mm'
     axmax = 150;
     rbol  = 5;
@@ -40,75 +37,73 @@ if exist('unit', 'var')
     axmax = 0.15;
     rbol  = 0.005;
   otherwise
-    % assume cm
-    axmax = 15;
-    rbol  = 5;
-  end
-else
-  axmax = 15;
+    error('unknown units (%s)', unit);
 end
 
 for k = 1:ndata
   
+  % the plotting style depends on the data content
+  haspos        = false;
+  hastri        = false;
+  hastransform  = false;
+  
   % do some checks on the geometrical object
   data = varargin{k};
   switch dtype{k}
-  case 'volume'
-    % contains transform
-    transform    = data.transform;
-    hastransform = 1;
-    haspos       = 0;
-  case 'source'
-    % contains pos
-    pos          = data.pos;
-    hastransform = 0;
-    haspos       = 1;
-    hastri       = 0; %FIXME may need to be supported in the future
-  case 'dip'
-    % contains pos
-    pos          = data.pos;
-    hastransform = 0;
-    haspos       = 1;
-    hastri       = 0; %FIXME may need to be supported in the future
-  case {'freq', 'raw', 'timelock', 'spike', 'mvar', 'freqmvar'}
-    % these may contain geometrical objects in grad or elec fields
-    if isfield(data, 'grad')
-      tmpdata = data.grad;
-      tmpdata = ft_checkcoordinates([], tmpdata);
-      data.grad = tmpdata;
-    elseif isfield(data, 'elec')
-      tmpdata = data.elec;
-      tmpdata = ft_checkcoordinates([], tmpdata);
-      data.grad = tmpdata;
-    end
-    %haspos       = 1;
-  case 'unknown'
-    % other geometrical objects typically contain geo info in pnt-field
-    if isfield(data, 'bnd')
-      data = data.bnd;
-    end
-
-    if isfield(data, 'pnt')
-      pos          = data.pnt;
-      hastransform = 0;
-      haspos       = 1;
-      if isfield(data, 'tri')
-        tri    = data.tri;
-        hastri = 1;
-      else
-        hastri = 0;
+    case 'volume'
+      % contains transform
+      hastransform = true;
+    case 'source'
+      % contains pos
+      haspos       = true;
+      hastri       = false; % FIXME may need to be supported in the future
+    case 'dip'
+      % contains pos
+      haspos       = true;
+      hastri       = false; % FIXME may need to be supported in the future
+    case {'freq', 'raw', 'timelock', 'spike', 'mvar', 'freqmvar'}
+      % these may contain geometrical objects in grad or elec fields
+      if isfield(data, 'grad')
+        data = data.grad;
+        haspos = true;
+      elseif isfield(data, 'elec')
+        data = data.elec;
+        haspos = true;
       end
-    else
-      error('no geometrical data seems to be present in the input');
-    end  
+    case 'unknown'
+      % other geometrical objects typically contain geometric information in the bnd.pnt or pnt field
+      if isfield(data, 'bnd')
+        data = data.bnd;
+      end
+      
+      if isfield(data, 'pnt')
+        haspos       = true;
+      end
+      if isfield(data, 'tri')
+        hastri = true;
+      end
   end % switch dtype{k}
   
-  if ~isfield(data, 'coordinates'), 
+  if haspos
+    if isfield(data, 'pnt')
+      pos = data.pnt;
+    else isfield(data, 'pos')
+      pos = data.pos;
+    end
+  end
+  
+  if hastri
+    tri = data.tri;
+  end
+  
+  if ~isfield(data, 'coordinates'),
     data.coordinates = [];
   end
+  
+  % create the labels that are to be plotted along the axes
   [labelx, labely, labelz] = xyz2label(data.coordinates);
- 
-  % plot the geometrical object 
+  
+  % plot the geometrical object
   
   % plot the mesh
   if haspos && hastri
@@ -117,27 +112,27 @@ for k = 1:ndata
     ft_plot_mesh(mesh, 'edgecolor','none', 'facecolor', [0.6 0.8 0.6], 'facealpha', 0.6);
     camlight;
   end
-
+  
   % plot the points
   if haspos && ~hastri
     shape.pnt = pos;
     ft_plot_headshape(shape);
-    camlight 
+    camlight
   end
- 
-  % plot 3 slices 
+  
+  % plot 3 slices
   if hastransform
-    ft_plot_ortho(data.anatomy, 'transform', data.transform, 'resolution', 1, 'style', 'intersect'); 
+    ft_plot_ortho(data.anatomy, 'transform', data.transform, 'resolution', 1, 'style', 'intersect');
     axis vis3d
     view([110 36]);
   end
-
+  
 end
 
 % get the xyz-axes
-xdat  = [-axmax 0 0; axmax 0 0];  
-ydat  = [0 -axmax 0; 0 axmax 0];  
-zdat  = [0 0 -axmax; 0 0 axmax];  
+xdat  = [-axmax 0 0; axmax 0 0];
+ydat  = [0 -axmax 0; 0 axmax 0];
+zdat  = [0 0 -axmax; 0 0 axmax];
 
 % plot axes
 hl = line(xdat, ydat, zdat);
@@ -158,12 +153,21 @@ hy = text(xdat(2,2),ydat(2,2),zdat(2,2),labely{2},'color','y','fontsize',15,'lin
 hz = text(xdat(2,3),ydat(2,3),zdat(2,3),labelz{2},'color','y','fontsize',15,'linewidth',2);
 
 % print some feedback to the user
-fprintf('you can now check whether the axes and the origin are where you expect them to be. you can rotate the object, by switching on the rotation utility in the figure panel\n');
+fprintf('You should now check whether the axes and the origin are consistent with what you expect them to be\n');
+fprintf('Switch on the rotation utility in the figure panel to see the figure from all angles\n');
+fprintf('For each of the following questions press one of the buttons: r(ight),l(eft),a(nterior),p(osterior),s(uperior),i(nferior)\n');
 
 % interactively determine orientation
-str{1} = input('In which direction is the positive X-axis pointing?\nr(ight),l(eft),a(nterior),p(osterior),s(uperior),i(nferior)', 's');
-str{2} = input('In which direction is the positive Y-axis pointing?\nr(ight),l(eft),a(nterior),p(osterior),s(uperior),i(nferior)', 's');
-str{3} = input('In which direction is the positive Z-axis pointing?\nr(ight),l(eft),a(nterior),p(osterior),s(uperior),i(nferior)', 's');
+str = {'', '', ''};
+while ~ismember(str{1}, {'r', 'l', 'a', 'p', 's', 'i'})
+  str{1} = input('In which direction is the positive X-axis pointing? ', 's');
+end
+while ~ismember(str{2}, {'r', 'l', 'a', 'p', 's', 'i'})
+  str{2} = input('In which direction is the positive Y-axis pointing? ', 's');
+end
+while ~ismember(str{3}, {'r', 'l', 'a', 'p', 's', 'i'})
+  str{3} = input('In which direction is the positive Z-axis pointing? ', 's');
+end
 
 % create output
 for k = 1:ndata
@@ -172,28 +176,29 @@ for k = 1:ndata
 end
 
 
-%-------------------------------------------------
-%function to go from aplrsi to better interpretable format
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION to go from aplrsi to better interpretable format
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [labelx, labely, labelz] = xyz2label(str)
 
 if ~isempty(str) && ~strcmp(str, 'unknown'),
   strx = tokenize(str, '_');
- 
+  
   switch strx{1}
-  case 'ras'
-    labelx = {'left'      'right'   };
-    labely = {'posterior' 'anterior'};
-    labelz = {'inferior'  'superior'};
-  case 'als'
-    labelx = {'posterior' 'anterior'};
-    labely = {'right'     'left'};
-    labelz = {'inferior'  'superior'};
-  otherwise
-  end  
-
+    case 'ras'
+      labelx = {'left'      'right'   };
+      labely = {'posterior' 'anterior'};
+      labelz = {'inferior'  'superior'};
+    case 'als'
+      labelx = {'posterior' 'anterior'};
+      labely = {'right'     'left'};
+      labelz = {'inferior'  'superior'};
+    otherwise
+  end
+  
 else
-  labelx = {'-X' 'X'};
-  labely = {'-Y' 'Y'};
-  labelz = {'-Z' 'Z'};
+  labelx = {'-X' '+X'};
+  labely = {'-Y' '+Y'};
+  labelz = {'-Z' '+Z'};
 end
 
