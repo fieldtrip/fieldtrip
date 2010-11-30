9d5d1ff9537af63c695ca343218fbea04940ab64
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2010-11-30 11:08:05 +0000

    added this function temporarily to provide users with feedback regarding the absence of this functionality in the release version of FieldTrip
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2230 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ connectivity/private/blockwise_conditionalgranger.m
@@ -0,0 +1,6 @@
+function blockwise_conditionalgranger(varargin)
+
+% BLOCKWISE_CONDITIONALGRANGER is a place holder for a function which is not
+% part of FieldTrip. It generates an error message when called. 
+
+error('support for the computation of blockwise conditional granger is not provided in the present release of FieldTripi. Please contact Jan-Mathijs Schoffelen if you would like to contribute to the development and release of this functionality');
