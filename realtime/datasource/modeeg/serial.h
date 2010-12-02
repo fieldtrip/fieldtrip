47c82d729d63be656c4e91aa6491ec604742175e
Stefan Klanke <s.klanke@donders.ru.nl>
2010-12-02 09:18:00 +0000

    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2260 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ realtime/datasource/modeeg/serial.h
@@ -0,0 +1,50 @@
+/* Simple serial port library for Windows and Linux, written in plain C
+ * (C) 2008 Stefan Klanke
+ */
+
+#ifndef __SERIAL_H
+#define __SERIAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef WIN32
+
+#include <windows.h>
+
+typedef struct {
+   HANDLE comPort;
+   DCB oldDCB;
+   COMMTIMEOUTS oldTimeOuts;
+} SerialPort;
+
+#else
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+typedef struct {
+   int comPort;
+   struct termios oldTermios;
+} SerialPort;
+
+#endif
+
+int serialOpenByNumber(SerialPort *SP, int port);
+int serialOpenByName(SerialPort *SP, const char *name);
+int serialClose(SerialPort *SP);
+int serialWrite(SerialPort *SP, int size, void *buffer);
+int serialRead(SerialPort *SP, int size, void *buffer);
+int serialSetParameters(SerialPort *SP, int baudrate, int bits, int parity, int stops, int timeout);
+void serialFlushInput(SerialPort *SP);
+void serialFlushOutput(SerialPort *SP);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
