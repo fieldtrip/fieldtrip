d60a6899284fe7554d4013c183a402c642539578
Cristiano Micheli <michelic72@googlemail.com>
2010-12-08 10:09:25 +0000

    changed help
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2333 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/private/skin.m
+++ plotting/private/skin.m
@@ -2,9 +2,9 @@ function rgb = rgbcolor
 
 % returns a predefined color as [red green blue] values
 %
-% skin   = [255 213 119]/255;
-% skull  = [140  85  85]/255;
-% brain  = [202 100 100]/255;
+% skin_surface         = [255 213 119]/255;
+% outer_skull_surface  = [140  85  85]/255;
+% inner_skull_surface  = [202 100 100]/255;
 % cortex = [255 213 119]/255;
 % black  = [0   0   0  ]/255;
 % white  = [255 255 255]/255;
@@ -12,6 +12,7 @@ function rgb = rgbcolor
 % green  = [0   192 0  ]/255;
 % blue   = [0   0   255]/255;
 % yellow = [255 255 0  ]/255; 
+
 
 rgb = [255 213 119]/255;
 
