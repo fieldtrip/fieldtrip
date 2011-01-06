df1d715b2d8951b4a02ec8591ff1095c41ce6326
Robert Oostenveld <r.oostenveld@gmail.com>
2011-01-06 09:51:43 +0000

    remove the dimord field from volume structures
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2531 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- public/private/fixvolume.m
+++ public/private/fixvolume.m
@@ -10,10 +10,13 @@ function data = fixvolume(data)
 % See also FT_CHECKDATA, FIXSOURCE
 
 % remove the unwanted fields
-if isfield(data, 'pos'),    data = rmfield(data, 'pos');    end
-if isfield(data, 'xgrid'),  data = rmfield(data, 'xgrid');  end
-if isfield(data, 'ygrid'),  data = rmfield(data, 'ygrid');  end
-if isfield(data, 'zgrid'),  data = rmfield(data, 'zgrid');  end
+if isfield(data, 'pos'),     data = rmfield(data, 'pos');     end
+if isfield(data, 'xgrid'),   data = rmfield(data, 'xgrid');   end
+if isfield(data, 'ygrid'),   data = rmfield(data, 'ygrid');   end
+if isfield(data, 'zgrid'),   data = rmfield(data, 'zgrid');   end
+
+% volume data is always 3-D and therefore does not need a dimord
+if isfield(data, 'dimord'),  data = rmfield(data, 'dimord');  end
 
 % although volume data should be 3D, better do this conversion anyway
 % old data structures may use latency/frequency instead of time/freq
@@ -37,4 +40,3 @@ end
 if hasfreq
   error('the volume data representation should be 3-D and is not allowed to contain a freq field');
 end
-
