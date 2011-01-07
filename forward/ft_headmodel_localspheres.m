ce9e252e1b72dabb08340930ac6119f20833d405
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-01-07 10:44:23 +0000

    created working version
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2540 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/ft_headmodel_localspheres.m
+++ forward/ft_headmodel_localspheres.m
@@ -1,7 +1,7 @@
-function vol = ft_headmodel_localspheres(geom, grad, ...)
+function vol = ft_headmodel_localspheres(geometry, grad, varargin)
   
 % FT_HEADMODEL_LOCALSPHERES constructs a MEG volume conduction model in
-% with a local sphere fitted to the head or brain surface for each seperate
+% with a local sphere fitted to the head or brain surface for each separate
 % channel
 %
 % This implements
@@ -14,28 +14,30 @@ function vol = ft_headmodel_localspheres(geom, grad, ...)
 %
 % See also FT_PREPARE_VOL_SENS, FT_COMPUTE_LEADFIELD
 
-headshape    = keyval('headshape', varargin);
+% get the additional inputs and set the defaults
+% headshape    = keyval('headshape', varargin);
 feedback     = keyval('feedback',  varargin); if isempty(feedback), feedback = true; end
+radius       = keyval('radius',    varargin); if isempty(radius),   radius   = 8.5;  end
+maxradius    = keyval('maxradius', varargin); if isempty(maxradius),maxradius = 20;  end
+baseline     = keyval('baseline',  varargin); if isempty(baseline), baseline  = 5;   end
 
-% set the defaults
-if ~isfield(cfg, 'radius'),        cfg.radius = 8.5;        end
-if ~isfield(cfg, 'maxradius'),     cfg.maxradius = 20;      end
-if ~isfield(cfg, 'baseline'),      cfg.baseline = 5;        end
+if ischar(feedback)
+  if strcmp(feedback, 'yes') 
+    feedback = true;
+  else 
+    feedback = false;
+  end
+end
 
 % start with an empty volume conductor
 vol = [];
 
-if ~isempty(headshape)
-  headshape = ft_read_headshape(headshape);
-  % use the headshape points instead of the user-specified ones
-  pnt = headshape.pnt;
-  if isfield(headshape, 'unit')
-    % copy the geometrical units into he volume conductor
-    vol.unit = headshape.unit;
-  end
+if isfield(geometry, 'unit')
+  % copy the geometrical units into he volume conductor
+  vol.unit = geometry.unit;
 end
 
-Nshape = size(headshape.pnt,1);
+Nshape = size(geometry.pnt,1);
 Nchan  = numel(grad.label);
 
 % set up an empty figure
@@ -52,12 +54,12 @@ end
 if istrue(feedback)
   cla
   ft_plot_sens(grad);
-  ft_plot_mesh(headshape, 'vertexcolor', 'g', 'facecolor', 'none', 'edgecolor', 'none');
+  ft_plot_mesh(geometry, 'vertexcolor', 'g', 'facecolor', 'none', 'edgecolor', 'none');
   drawnow
 end
 
 % fit a single sphere to all headshape points
-[single_o, single_r] = fitsphere(headshape.pnt);
+[single_o, single_r] = fitsphere(geometry.pnt);
 fprintf('single sphere,   %5d surface points, center = [%4.1f %4.1f %4.1f], radius = %4.1f\n', Nshape, single_o(1), single_o(2), single_o(3), single_r);
 
 
@@ -94,23 +96,23 @@ for chan=1:Nchan
   
   [m, i] = min(dist);
   % check whether the minimum difference is larger than a typical distance
-  if abs(m)>(cfg.baseline/4)
+  if abs(m)>(baseline/4)
     % replace the position of this channel by the coil that is the closest to the head (axial gradiometer)
     % except when the center of the channel is approximately just as good (planar gradiometer)
     thispnt = allpnt(i,:);
   end
   
   % find the headshape points that are close to this channel
-  dist = sqrt(sum((headshape.pnt-repmat(thispnt,Nshape,1)).^2, 2));
-  shapesel = find(dist<cfg.radius);
-  if strcmp(cfg.feedback, 'yes')
-    ft_plot_mesh(headshape.pnt(shapesel,:), 'vertexcolor', 'g');
+  dist = sqrt(sum((geometry.pnt-repmat(thispnt,Nshape,1)).^2, 2));
+  shapesel = find(dist<radius);
+  if feedback
+    ft_plot_mesh(geometry.pnt(shapesel,:), 'vertexcolor', 'g');
     drawnow
   end
   
   % fit a sphere to these headshape points
   if length(shapesel)>10
-    [o, r] = fitsphere(headshape.pnt(shapesel,:));
+    [o, r] = fitsphere(geometry.pnt(shapesel,:));
     fprintf('channel = %s, %5d surface points, center = [%4.1f %4.1f %4.1f], radius = %4.1f\n', grad.label{chan}, length(shapesel), o(1), o(2), o(3), r);
   else
     fprintf('channel = %s, not enough surface points, using all points\n', grad.label{chan});
@@ -118,7 +120,7 @@ for chan=1:Nchan
     r = single_r;
   end
   
-  if r > cfg.maxradius
+  if r > maxradius
     fprintf('channel = %s, not enough surface points, using all points\n', grad.label{chan});
     o = single_o;
     r = single_r;
