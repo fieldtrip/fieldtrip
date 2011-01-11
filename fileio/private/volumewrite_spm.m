5ab29ce779682c8303899807996e06375ffaead2
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-01-11 13:56:20 +0000

    moved to fileio/private
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2551 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ fileio/private/volumewrite_spm.m
@@ -0,0 +1,68 @@
+function [Va] = volumewrite_spm(filename, data, transform, spmversion)
+
+% VOLUMEWRITE_SPM write a anatomical or functional volume to img/hdr file
+% using the SPM toolbox
+%
+% Use as
+%   [Va] = volumewrite_spm(filename, data, transform)
+
+% Copyright (C) 2006, Robert Oostenveld
+%
+% Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
+
+if nargin<4,
+  spmversion = 'SPM2';
+end
+
+% check whether the required SPM toolbox is available
+hastoolbox(upper(spmversion), 1);
+
+if 0
+  % not all datatypes are supported by SPM, this can be checked with
+  spm_type('double') %           -- Double precision floating point number array
+  spm_type('single') %           -- Single precision floating point number array
+  spm_type('logical') %          -- Logical array
+  spm_type('char') %             -- Character array
+  spm_type('cell') %             -- Cell array
+  spm_type('struct') %           -- Structure array
+  spm_type('int8') %             -- 8-bit signed integer array
+  spm_type('uint8') %            -- 8-bit unsigned integer array
+  spm_type('int16') %            -- 16-bit signed integer array
+  spm_type('uint16') %           -- 16-bit unsigned integer array
+  spm_type('int32') %            -- 32-bit signed integer array
+  spm_type('uint32') %           -- 32-bit unsigned integer array
+  spm_type('int64') %            -- 64-bit signed integer array
+  spm_type('uint64') %           -- 64-bit unsigned integer array
+end
+
+typ       = spm_type(class(data));
+dim       = size(data);
+if isnan(typ)
+  % convert every unsupported data type into double
+  data      = double(data);
+end
+
+switch lower(spmversion)
+case {'spm2'}
+  %see spm_vol
+  Va         = [];
+  Va.mat     = transform;
+  Va.fname   = filename;
+  Va.dim     = [dim typ];
+  Va.n       = 1;
+  Va.pinfo   = [1 0 0]';
+  Va.private.hdr.dime.datatype = typ;
+case {'spm8'}
+  Va         = [];
+  Va.mat     = transform;
+  Va.fname   = filename;
+  Va.dim     = dim;
+  Va.n       = 1;
+  Va.pinfo   = [1 0 0]';
+  %Va.dt      = [typ 1]; % this is not necessary because assigned in spm_create_vol
+otherwise
+  error('unsupported version of spm requested');
+end
+Va         = spm_create_vol(Va);
+Va         = spm_write_vol(Va,data);
+
