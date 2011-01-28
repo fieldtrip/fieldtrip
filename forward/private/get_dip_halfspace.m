ed2a42f518cd858d42ced4f60740c73231d834e4
Cristiano Micheli <michelic72@googlemail.com>
2011-01-28 11:49:00 +0000

    implements a check about dipoles position in the halfspace
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2703 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ forward/private/get_dip_halfspace.m
@@ -0,0 +1,35 @@
+function is_in_empty = get_dip_halfspace(P,vol);
+
+% GET_DIP_HALFSPACE checks if the dipole is in the empty halfspace and
+% returns true if this happens. The normal of the plane points by
+% convention to the empty halfspace.
+%       
+% is_in_empty = get_dip_halfspace(P,vol);
+
+% Copyright (C) 2011, Cristiano Micheli 
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id: get_dip_halfspace.m $
+
+is_in_empty = false;
+ori = vol.ori;
+pnt = vol.pnt;
+
+if dot(ori,(P-pnt)) < pi/2
+  is_in_empty = true;
+end
