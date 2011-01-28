440cf7a457ea9056b0c517d2f622d07f927053b2
Cristiano Micheli <michelic72@googlemail.com>
2011-01-28 13:36:46 +0000

    completed implementation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2706 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/private/get_mirror_pos.m
+++ forward/private/get_mirror_pos.m
@@ -1,7 +1,10 @@
-function P2 = get_mirror_pos(P1,vol);
+function P2 = get_mirror_pos(P1,vol)
 
 % GET_MIRROR_POS calculates the position of a point symmetric to pnt with respect to a plane
-%       
+% 
+% P1 is a [1x3] point
+% vol is the headmodel for halfspace
+%
 % P2 = get_mirror_pos(P1,vol);
 
 % Copyright (C) 2011, Cristiano Micheli 
@@ -28,6 +31,27 @@ P2 = [];
 
 % define the plane
 pnt = vol.pnt;
-ori = vol.ori;
-% plane = defplane(pnt,ori);
-% P2 = symm(plane,P1);
+ori = vol.ori; % already normalized
+
+if dot(P1-pnt,ori)<eps
+  warning(sprintf ('point %f %f %f lies in the symmetry plane'),P1(1),P1(2),P1(3)))
+  P2 = P1;
+else
+  % define the plane in parametric form
+  % define a non colinear vector vc with respect to the plane normal
+  vc = [1 0 0];    
+  if abs(cross(ori, vc, 2))<eps
+      vc = [0 1 0];
+  end
+  % define plane's direction vectors
+  v1 = cross(ori, vc, 2);  v1 = v1/norm(v1);
+  v2 = cross(pnt, ori, 2); v2 = v2/norm(v2);
+  plane = [pnt v1 v2];
+ 
+  % distance plane-point P1
+  d = -dot(ori, plane(:,1:3)-P1(:,1:3), 2);
+
+  % symmetric point
+  P2 = P1 + 2*d*ori;
+end
+
