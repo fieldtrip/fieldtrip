eac3f16074687935e060612cf7d0f4e7db424cda
Cristiano Micheli <michelic72@googlemail.com>
2011-01-28 10:03:01 +0000

    extended help and fixed some miinor bug
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2690 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- external/openmeeg/openmeeg_helper.m
+++ external/openmeeg/openmeeg_helper.m
@@ -1,29 +1,24 @@
-function lf=openmeeg_helper(pos,vol,dimdip)
+function lf=openmeeg_helper(pos,vol,splitdim)
 %
 % Splits the calculation of leadfields in smaller chunks
 % pos: positions of dipoles
 % vol: volume conductor
-% dimdip: number of dipoles to calculate at a time
+% splitdim: splitting factor 
 
 lf   = [];
 ndip = size(pos,1);
-splitdim = dimdip;
 numsol   = fix(ndip./splitdim);
 nleft    = rem(ndip,splitdim);
 lf = zeros(size(vol.mat,1),ndip*3);
 if isfield(vol,'mat')
-  if ft_hastoolbox('openmeeg', 1);
-    for i = 1:numsol
-      dsm = openmeeg_dsm(pos((splitdim*(i - 1) + 1):splitdim*i,:),vol);
+    for i = 1:splitdim
+      dsm = openmeeg_dsm(pos((numsol*(i - 1) + 1):numsol*i,:),vol);
       tmp = vol.mat*dsm;
-      lf(:,(splitdim*(i - 1)*3 + 1):splitdim*i*3) = tmp;
+      lf(:,(numsol*(i - 1)*3 + 1):numsol*i*3) = tmp;
     end
     dsm = openmeeg_dsm(pos(ndip-nleft+1:ndip,:),vol);
     tmp = vol.mat*dsm;
-    lf(:,ndip-nleft+1:ndip,:) = tmp;
-  else
-    error('Openmeeg toolbox not installed')
-  end
+    lf(:,(ndip-nleft)*3+1:ndip*3) = tmp;
 else
   error('No system matrix is present, BEM head model not calculated yet')
 end
