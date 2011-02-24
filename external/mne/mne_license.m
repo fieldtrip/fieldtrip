79025ae0dfa12ea395ef3442f5c31c7970bb9ebd
Robert Oostenveld <r.oostenveld@gmail.com>
2011-02-24 08:19:39 +0000

    added automatic display of external/mne license if the external/xxx toolbox coontains a xxx_license.m function, it will be called by ft_hastoolbox small layout change to license for openmeeg
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@2944 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ external/mne/mne_license.m
@@ -0,0 +1,33 @@
+function mne_license
+
+% MNE_LICENSE prints the license only once upon the first call to
+% this function. If the user does a "clear all", the license will
+% again be shown.  This function should be included in every openmeeg
+% function to ensure that the license is displayed at least once.
+
+% Copyright (C) 2009-2011, Robert Oostenveld
+
+persistent status
+
+if isempty(status)
+  % this is for the first time
+  status = false;
+end
+
+if ~status
+  % show the license
+  try
+    tmp = which('mne_license');
+  catch
+    tmp = mfilename; 
+  end
+  [p, f] = fileparts(tmp);
+  clc
+  fprintf('==============================================================================\n');
+  type(fullfile(p, 'LICENSE.txt'));
+  fprintf('==============================================================================\n');
+  % remember that it has been shown
+  status = true;
+  pause(3)
+end
+
