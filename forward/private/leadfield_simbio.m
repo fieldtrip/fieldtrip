d4d0fe4c3bfd47ac9b392851379c51eacfd9d5b2
Cristiano Micheli <michelic72@googlemail.com>
2011-03-15 16:36:16 +0000

    first implementation of the SimBio wrapper
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3108 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ forward/private/leadfield_simbio.m
@@ -0,0 +1,67 @@
+function [lf] = leadfield_simbio(dip, elc, vol)
+
+% LEADFIELD_SIMBIO leadfields for a set of dipoles
+%
+% [lf] = leadfield_simbio(dip, elc, vol);
+%
+% with input arguments
+%   dip     position dipole (vector of length 3)
+%   elc     position electrodes
+% and vol being a structure with the element
+%   vol.meshfile file containing the 3D mesh
+
+% Copyright (C) 2011, Cristiano Micheli
+
+% store the current path and change folder to the temporary one
+tmpfolder = cd;
+
+try
+  if ~ispc
+    cd(tempdir)
+    [junk,tname] = fileparts(tempname);
+    exefile = [tname '.sh'];
+    [junk,tname] = fileparts(tempname);
+    elcfile  = [tname '.elc'];
+    [junk,tname] = fileparts(tempname);
+    dipfile  = [tname '.dip'];
+    [junk,tname] = fileparts(tempname);
+    outfile  = [tname];
+    
+    % write the electrodes and dipoles positions in the temporary folder
+    sb_write_elc(elc.pnt,elc.labels,elcfile);
+    sb_write_dip(dip,dipfile);
+    
+    % Exe file
+    % Does SimBio have a switch for parallel processing to run on more cores?
+    efid = fopen(exefile, 'w');
+    if ~ispc
+      fprintf(efid,'#!/usr/bin/env bash\n');
+      fprintf(efid,['ipm_linux_opt_Venant -i sourcesimulation -h ' vol.meshfile ' -s ./' elcfile, ...
+        ' -dip ./' dipfile ' -o ' outfile ' -p parameterfile.par -fwd FEM -sens EEG 2>&1 > /dev/null\n']);
+    end
+    fclose(efid);
+    
+    dos(sprintf('chmod +x %s', exefile));
+    dos(['./' exefile]);
+    cleaner(dipfile,elcfile,outfile,exefile)
+    [lf] = sb_read_msr(outfile);
+  end
+
+catch
+  warning('an error occurred while running SimBio');
+  rethrow(lasterror)
+  cleaner(dipfile,elcfile,outfile,exefile)
+  cd(tmpfolder)
+end
+
+function cleaner(dipfile,elcfile,outfile,exefile)
+delete(dipfile);
+delete(elcfile);
+delete(outfile);
+delete(exefile);
+delete([outfile '.fld']);
+delete([outfile '.hex']);
+delete([outfile '.pot']);
+delete([outfile '.pts']);
+delete(['*.v.ascii']);
+delete(['*.v.potential']);
