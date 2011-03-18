9f62f34c276e4484ec935db4e898792a4383fd2c
Robert Oostenveld <r.oostenveld@gmail.com>
2011-03-18 10:57:03 +0000

    deal with the exportfile, in case the histfile is empty
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3169 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/qualitycheck_exportname.m
+++ private/qualitycheck_exportname.m
@@ -3,13 +3,19 @@ function exportname = exportname_qualitycheck(dataset)
 % EXPORTNAME_QUALITYCHECK is used in ft_qualitycheck and the associated cron job
 
 [p, f, x] = fileparts(dataset);
-histfile = fullfile(p, [f x], [f '.hist']); % this only applies to some CTF datasets
 
-if isdir(dataset) && exist(histfile, 'file')
-  info = read_ctf_hist(histfile);
-  c = datevec(info.startdate);
-  % construct something like 20070320_1230, i.e. YYYYMMDD_HHMM
-  exportname = sprintf('%04d%02d%02d_%s%s.mat', c(1), c(2), c(3), info.starttime(1:2), info.starttime(4:5));
+if ft_filetype(dataset, 'ctf_ds')
+  try
+    info = read_ctf_hist(dataset);
+    c = datevec(info.startdate);
+    % construct something like 20070320_1230, i.e. YYYYMMDD_HHMM
+    exportname = sprintf('%04d%02d%02d_%s%s.mat', c(1), c(2), c(3), info.starttime(1:2), info.starttime(4:5));
+  catch
+    % the hist file is sometines empty
+    % just use the dataset name, but exclude the full path
+    exportname = [f, '.mat'];
+  end
+
 else
   % just use the dataset name, but exclude the full path
   exportname = [f, '.mat'];
