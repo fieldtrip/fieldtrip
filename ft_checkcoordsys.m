9be62a68ffcc84cbe80e8622d4f6132d58df9f01
Robert Oostenveld <r.oostenveld@gmail.com>
2011-03-23 05:12:16 +0000

    changed the interaction with the user, it now asks the question whether you want to change it and then gives the opportunity to (re)assign the anatomical labels for the axes. Also updated the help.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3200 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_checkcoordsys.m
+++ ft_checkcoordsys.m
@@ -1,20 +1,26 @@
 function [data] = ft_checkcoordsys(cfg, data)
 
-% FT_CHECKCOORDSYS allows the user to perform  a check on 
-% the coordinatesystem and units in which the input object
-% is expressed.
+% FT_CHECKCOORDSYS plots a geometrical object, allowing you to perform a visual
+% check on the coordinatesystem, the units and on the anatomical labels for the
+% coordinate system axes.
 %
 % Use as
-%   [output] = ft_checkcoordsys(cfg, data)
+%   [dataout] = ft_checkcoordsys(cfg, datain)
+% where the input data structure can be
+%  - an anatomical MRI, which can be segmented
+%  - an electrode or gradiometer definition
+%  - a volume conduction model
+% or most other FieldTrip structures that represent geometrical information.
 %
 % The configuration options are
-%   cfg.interactive  = 'yes' (default) or 'no'
+%   cfg.interactive  = string, 'yes' or 'no' (default = 'yes')
 %
-% The input data can be any of the following:
-% -an MRI structure either containing an anatomy, or gray matter
-% probabilistic mask.
-% -any data structure containing a .pos field, or electrode position
-% information (.grad or .elec)
+% This function wil pop up a figure that allows you to check whether the
+% aligntment of the object relative to the coordinate system axes is correct
+% and what the anatomical labels of the coordinate system axes are. You should
+% switch on the 3D rotation option in the figure panel to rotate and see the
+% figure from all angles. To change the anatomical labels of the coordinate
+% system, you should press the corresponding keyboard button.
 %
 % See also FT_VOLUMEREALIGN, FT_VOLUMERESLICE
 
@@ -37,9 +43,6 @@ function [data] = ft_checkcoordsys(cfg, data)
 % $Id$
 
 if ~isfield(cfg, 'interactive'), cfg.interactive = 'yes'; end
-
-dointeractive = strcmp(cfg.interactive, 'yes');
-doplot        = 1;
 
 dtype = ft_datatype(data);
 data  = ft_convert_units(data);
@@ -92,172 +95,215 @@ switch dtype
     if isfield(data, 'bnd')
       data = data.bnd;
     end
-    
     if isfield(data, 'pnt')
-      haspos       = true;
+      haspos = true;
     end
     if isfield(data, 'tri')
       hastri = true;
     end
 end % switch dtype{k}
 
-if haspos
-  if isfield(data, 'pnt')
-    pos = data.pnt;
-  else isfield(data, 'pos')
-    pos = data.pos;
-  end
+if haspos && isfield(data, 'pos')
+  pos = data.pos;
+elseif haspos && isfield(data, 'pnt')
+  pos = data.pnt;
+elseif haspos && issubfield(data, 'bnd.pnt')
+  pos = data.bnd.pnt;
 end
 
-if hastri
+if hastri && isfield(data, 'tri')
   tri = data.tri;
+elseif hastri && issubfield(data, 'bnd.tri')
+  tri = data.bnd.tri;
 end
 
-if ~isfield(data, 'coordsys'),
-  data.coordsys = [];
+if isfield(data, 'coordsys')
+  if length(data.coordsys)==3 && length(intersect(data.coordsys, 'rlasif'))==3
+    for i=1:3
+      switch data.coordsys(i)
+        case 'l'
+          label{i} = 'the left';
+        case 'r'
+          label{i} = 'the right';
+        case 'i'
+          label{i} = 'inferior';
+        case 's'
+          label{i} = 'superior';
+        case 'a'
+          label{i} = 'anterior';
+        case 'p'
+          label{i} = 'posterior';
+        otherwise
+          error('incorrect letter in the coordsys');
+      end % switch
+    end % for each of the three axes
+  elseif strcmpi(data.coordsys, 'itab') || strcmpi(data.coordsys, 'neuromag')
+    label{1} = 'the right';
+    label{2} = 'anterior';
+    label{3} = 'superior';
+  elseif strcmpi(data.coordsys, 'ctf') || strcmpi(data.coordsys, '4d') || strcmpi(data.coordsys, 'bti')
+    label{1} = 'anterior';
+    label{2} = 'the left';
+    label{3} = 'superior';
+  elseif strcmpi(data.coordsys, 'tal') || strcmpi(data.coordsys, 'mni') || strcmpi(data.coordsys, 'spm')
+    label{1} = 'the right';
+    label{2} = 'anterior';
+    label{3} = 'superior';
+  else
+    error('unsupported coordsys');
+  end
+  
+  fprintf('The positive x-axis is pointing towards %s\n', label{1});
+  fprintf('The positive y-axis is pointing towards %s\n', label{2});
+  fprintf('The positive z-axis is pointing towards %s\n', label{3});
 end
 
-if doplot,
-  % create the labels that are to be plotted along the axes
+% create the labels that are to be plotted along the axes
+if isfield(data, 'coordsys')
   [labelx, labely, labelz] = xyz2label(data.coordsys);
-  
-  % plot the geometrical object
-  
-  % plot the mesh
-  if haspos && hastri
-    mesh.pnt = pos;
-    mesh.tri = tri;
-    ft_plot_mesh(mesh, 'edgecolor','none', 'facecolor', [0.6 0.8 0.6], 'facealpha', 0.6);
-    camlight;
-  end
-  
-  % plot the points
-  if haspos && ~hastri
-    shape.pnt = pos;
-    ft_plot_headshape(shape);
-    camlight
-  end
-  
-  % plot 3 slices
-  if hastransform
-    if isfield(data, 'anatomy')
-      funparam = 'anatomy';
-    elseif isfield(data, 'gray')
-      funparam = 'gray';
-    else
-      error('don''t know what volumetric parameter to plot');
-    end
-    ft_plot_ortho(data.(funparam), 'transform', data.transform, 'resolution', 1, 'style', 'intersect');
-    axis vis3d
-    view([110 36]);
-  end
-  
-  % get the xyz-axes
-  xdat  = [-axmax 0 0; axmax 0 0];
-  ydat  = [0 -axmax 0; 0 axmax 0];
-  zdat  = [0 0 -axmax; 0 0 axmax];
-  
-  % get the xyz-axes dotted
-  xdatdot = (-axmax:(axmax/15):axmax);
-  xdatdot = xdatdot(1:floor(numel(xdatdot)/2)*2);
-  xdatdot = reshape(xdatdot, [2 numel(xdatdot)/2]);
-  n       = size(xdatdot,2);
-  ydatdot = [zeros(2,n) xdatdot zeros(2,n)];
-  zdatdot = [zeros(2,2*n) xdatdot];
-  xdatdot = [xdatdot zeros(2,2*n)];
-  
-  % plot axes
-  hl = line(xdat, ydat, zdat);
-  set(hl(1), 'linewidth', 1, 'color', 'r');
-  set(hl(2), 'linewidth', 1, 'color', 'g');
-  set(hl(3), 'linewidth', 1, 'color', 'b');
-  hld = line(xdatdot, ydatdot, zdatdot);
-  for k = 1:n
-    set(hld(k    ), 'linewidth', 3, 'color', 'r');
-    set(hld(k+n*1), 'linewidth', 3, 'color', 'g');
-    set(hld(k+n*2), 'linewidth', 3, 'color', 'b');
-  end
-  
-  % create the ball at the origin
-  [O.pnt, O.tri] = icosahedron42;
-  O.pnt = O.pnt.*rbol;
-  ft_plot_mesh(O, 'edgecolor', 'none');
-  
-  % add the labels to the axis
-  text(xdat(1,1),ydat(1,1),zdat(1,1),labelx{1},'color','y','fontsize',15,'linewidth',2);
-  text(xdat(1,2),ydat(1,2),zdat(1,2),labely{1},'color','y','fontsize',15,'linewidth',2);
-  text(xdat(1,3),ydat(1,3),zdat(1,3),labelz{1},'color','y','fontsize',15,'linewidth',2);
-  text(xdat(2,1),ydat(2,1),zdat(2,1),labelx{2},'color','y','fontsize',15,'linewidth',2);
-  text(xdat(2,2),ydat(2,2),zdat(2,2),labely{2},'color','y','fontsize',15,'linewidth',2);
-  text(xdat(2,3),ydat(2,3),zdat(2,3),labelz{2},'color','y','fontsize',15,'linewidth',2);
+else
+  [labelx, labely, labelz] = xyz2label('unknown');
 end
 
-if dointeractive,
-  % print some feedback to the user
-  fprintf('You should now check whether the axes and the origin are consistent with what you expect them to be\n');
-  fprintf('Switch on the rotation utility in the figure panel to see the figure from all angles\n');
-  fprintf('For each of the following questions press one of the buttons:\nr(ight),l(eft),a(nterior),p(osterior),s(uperior),i(nferior)\n');
+% plot the mesh
+if haspos && hastri
+  mesh.pnt = pos;
+  mesh.tri = tri;
+  ft_plot_mesh(mesh, 'edgecolor','none', 'facecolor', [0.6 0.8 0.6], 'facealpha', 0.6);
+  camlight;
+end
+
+% plot the points
+if haspos && ~hastri
+  shape.pnt = pos;
+  ft_plot_headshape(shape);
+  camlight
+end
+
+% plot 3 slices
+if hastransform
+  if isfield(data, 'anatomy')
+    funparam = 'anatomy';
+  elseif isfield(data, 'gray')
+    funparam = 'gray';
+  else
+    error('don''t know what volumetric parameter to plot');
+  end
+  ft_plot_ortho(data.(funparam), 'transform', data.transform, 'resolution', 1, 'style', 'intersect');
+  axis vis3d
+  view([110 36]);
+end
+
+% get the xyz-axes
+xdat  = [-axmax 0 0; axmax 0 0];
+ydat  = [0 -axmax 0; 0 axmax 0];
+zdat  = [0 0 -axmax; 0 0 axmax];
+
+% get the xyz-axes dotted
+xdatdot = (-axmax:(axmax/15):axmax);
+xdatdot = xdatdot(1:floor(numel(xdatdot)/2)*2);
+xdatdot = reshape(xdatdot, [2 numel(xdatdot)/2]);
+n       = size(xdatdot,2);
+ydatdot = [zeros(2,n) xdatdot zeros(2,n)];
+zdatdot = [zeros(2,2*n) xdatdot];
+xdatdot = [xdatdot zeros(2,2*n)];
+
+% plot axes
+hl = line(xdat, ydat, zdat);
+set(hl(1), 'linewidth', 1, 'color', 'r');
+set(hl(2), 'linewidth', 1, 'color', 'g');
+set(hl(3), 'linewidth', 1, 'color', 'b');
+hld = line(xdatdot, ydatdot, zdatdot);
+for k = 1:n
+  set(hld(k    ), 'linewidth', 3, 'color', 'r');
+  set(hld(k+n*1), 'linewidth', 3, 'color', 'g');
+  set(hld(k+n*2), 'linewidth', 3, 'color', 'b');
+end
+
+% create the ball at the origin
+[O.pnt, O.tri] = icosahedron42;
+O.pnt = O.pnt.*rbol;
+ft_plot_mesh(O, 'edgecolor', 'none');
+
+% add the labels to the axis
+text(xdat(1,1),ydat(1,1),zdat(1,1),labelx{1},'color','y','fontsize',15,'linewidth',2);
+text(xdat(1,2),ydat(1,2),zdat(1,2),labely{1},'color','y','fontsize',15,'linewidth',2);
+text(xdat(1,3),ydat(1,3),zdat(1,3),labelz{1},'color','y','fontsize',15,'linewidth',2);
+text(xdat(2,1),ydat(2,1),zdat(2,1),labelx{2},'color','y','fontsize',15,'linewidth',2);
+text(xdat(2,2),ydat(2,2),zdat(2,2),labely{2},'color','y','fontsize',15,'linewidth',2);
+text(xdat(2,3),ydat(2,3),zdat(2,3),labelz{2},'color','y','fontsize',15,'linewidth',2);
+
+if strcmp(cfg.interactive, 'yes')
+  
+  if ~isfield(data, 'coordsys') || isempty(data.coordsys)
+    % default is yes
+    value = smartinput('Do you want to change the anatomical labels for the axes [Y, n]? ', 'y');
+  else
+    % default is no
+    value = smartinput('Do you want to change the anatomical labels for the axes [y, N]? ', 'n');
+  end
+  
+  if strcmpi(value, 'n')
+    return
+  end
   
   % interactively determine orientation
-  str = {'', '', ''};
-  while ~ismember(str{1}, {'r', 'l', 'a', 'p', 's', 'i'})
-    str{1} = input('In which direction is the positive X-axis pointing? ', 's');
+  orientation = '   ';
+  while ~ismember(orientation(1), {'r', 'l', 'a', 'p', 's', 'i'})
+    orientation(1) = smartinput('What is the anatomical label for the positive X-axis [r, l, a, p, s, i]? ', '');
   end
-  while ~ismember(str{2}, {'r', 'l', 'a', 'p', 's', 'i'})
-    str{2} = input('In which direction is the positive Y-axis pointing? ', 's');
+  while ~ismember(orientation(2), {'r', 'l', 'a', 'p', 's', 'i'})
+    orientation(2) = smartinput('What is the anatomical label for the positive Y-axis [r, l, a, p, s, i]? ', '');
   end
-  while ~ismember(str{3}, {'r', 'l', 'a', 'p', 's', 'i'})
-    str{3} = input('In which direction is the positive Z-axis pointing? ', 's');
+  while ~ismember(orientation(3), {'r', 'l', 'a', 'p', 's', 'i'})
+    orientation(3) = smartinput('What is the anatomical label for the positive Z-axis [r, l, a, p, s, i]? ', '');
   end
   
   % interactively determine origin
-  str2 = input('Does the origin of the coordinate system look as any of the following?\na(nterior commissure),i(nterauricular),n(ot a landmark)', 's');
-  
-  % create output
-  str = cat(2, str{:});
-  switch str2
-    case 'a'
-      if strcmp(str, 'ras')
-        %str = [str,'_tal'];
-        str = 'spm';
-      else
-      end
-    case 'i'
-      if strcmp(str, 'als')
-        %str = [str,'_ctf'];
-        str = 'ctf';
-      elseif strcmp(str, 'ras')
-        %str = [str,'_Neuromag'];
-        str = neuromag;
-      end
-    otherwise
+  origin = ' ';
+  while ~ismember(origin, {'a', 'i', 'n'})
+    origin = input('Is the origin of the coordinate system at the a(nterior commissure), i(nterauricular), n(ot a landmark)? ', 's');
   end
   
-  data.coordsys = str;
-end
+  if origin=='a' && strcmp(orientation, 'ras')
+    coordsys = 'spm';
+  elseif origin=='i' && strcmp(orientation, 'als')
+    coordsys = 'ctf';
+  elseif origin=='i' && strcmp(orientation, 'ras')
+    coordsys = 'neuromag'; % also used for itab
+  else
+    % just use the orientation
+    coordsys = orientation;
+  end
+  
+  data.coordsys = coordsys;
+end % if interactive
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SUBFUNCTION to go from aplrsi to better interpretable format
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [labelx, labely, labelz] = xyz2label(str)
 
-if ~isempty(str) && ~strcmp(str, 'unknown'),
+if ~isempty(str) && ~strcmp(str, 'unknown')
+  % the first part is important for th eorientations
+  % the second part optionally contains information on the origin
   strx = tokenize(str, '_');
   
   switch lower(strx{1})
     case {'ras' 'itab' 'neuromag'}
-      labelx = {'-X (left)'      'X (right)'   };
-      labely = {'-Y (posterior)' 'Y (anterior)'};
-      labelz = {'-Z (inferior)'  'Z (superior)'};
-    case {'als' 'ctf' '4d'}
-      labelx = {'-X (posterior)' 'X (anterior)'};
-      labely = {'-Y (right)'     'Y (left)'};
-      labelz = {'-Z (inferior)'  'Z (superior)'};
+      labelx = {'-X (left)'      '+X (right)'   };
+      labely = {'-Y (posterior)' '+Y (anterior)'};
+      labelz = {'-Z (inferior)'  '+Z (superior)'};
+    case {'als' 'ctf' '4d', 'bti'}
+      labelx = {'-X (posterior)' '+X (anterior)'};
+      labely = {'-Y (right)'     '+Y (left)'};
+      labelz = {'-Z (inferior)'  '+Z (superior)'};
     otherwise
+      error('unknown coordsys');
   end
   
 else
-  labelx = {'-X' '+X'};
-  labely = {'-Y' '+Y'};
-  labelz = {'-Z' '+Z'};
+  labelx = {'-X (unknown)' '+X (unknown)'};
+  labely = {'-Y (unknown)' '+Y (unknown)'};
+  labelz = {'-Z (unknown)' '+Z (unknown)'};
 end
