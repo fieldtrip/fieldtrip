73dbb42cd3bd67ce1ed36569fc28198c93721a5f
Robert Oostenveld <r.oostenveld@gmail.com>
2011-03-23 03:42:45 +0000

    multiple changes to facilitate clean FT pipeline for MNE source estimation on ITAB independent component data: - changed ft_datatype_comp so that it updates the comp structure to the latest version, including the "raw" part - extended ft_checkdata so that it can convert comp to timelock (via raw) - added a check on isempty(trl) to prevent an irrelevant warning in fixtrialdef - added documentationi to ft_topoplotIC on zlim scaling of the colorbar (no new functionality) - added itab_mhd to ft_read_sens
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3198 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/ft_read_sens.m
+++ fileio/ft_read_sens.m
@@ -138,13 +138,11 @@ switch fileformat
     sens.label = tmp{1};
     sens.pnt   = [tmp{2:4}];
    
-  case 'itab_raw'
-    ft_hastoolbox('fileio');
+  case {'itab_raw' 'itab_mhd'}
     hdr = ft_read_header(filename);
     sens = hdr.grad;
     
   case 'neuromag_mne'
-    ft_hastoolbox('fileio');
     hdr = ft_read_header(filename,'headerformat','neuromag_mne');
     sens = hdr.elec;    
     
@@ -156,16 +154,12 @@ switch fileformat
   case {'ctf_ds', 'ctf_res4', 'neuromag_fif', '4d', '4d_pdf', '4d_m4d', '4d_xyz', 'yokogawa_ave', 'yokogawa_con', 'yokogawa_raw'}
     % check the availability of the required low-level toolbox
     % this is required because the read_sens function is also on itself included in the forwinv toolbox
-    ft_hastoolbox('fileio');
     hdr = ft_read_header(filename, 'headerformat', fileformat);
     sens = hdr.grad;
     
-    
   case 'neuromag_mne_grad'
-    ft_hastoolbox('fileio');
     hdr = ft_read_header(filename,'headerformat','neuromag_mne');
     sens = hdr.grad;
-    
     
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % This is for EEG formats where electrode positions can be stored with the data
@@ -174,7 +168,6 @@ switch fileformat
   case {'spmeeg_mat', 'eeglab_set'}
     % check the availability of the required low-level toolbox
     % this is required because the read_sens function is also on itself included in the forwinv toolbox
-    ft_hastoolbox('fileio');
     hdr = ft_read_header(filename);
     
     if isfield(hdr, 'grad')
