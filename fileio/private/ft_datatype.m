e98068e610756cd1625c3e0dfe15f6a510b01497
Robert Oostenveld <r.oostenveld@gmail.com>
2011-04-04 14:57:16 +0000

    automatically synchronized identical files to revision 3271
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3272 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/private/ft_datatype.m
+++ fileio/private/ft_datatype.m
@@ -31,16 +31,16 @@ function [type, dimord] = ft_datatype(data, desired)
 % $Id$
 
 % determine the type of input data, this can be raw, freq, timelock, comp, spike, source, volume, dip
-israw      = isfield(data, 'label') && isfield(data, 'time') && isa(data.time, 'cell') && isfield(data, 'trial') && isa(data.trial, 'cell');
+israw      =  isfield(data, 'label') && isfield(data, 'time') && isa(data.time, 'cell') && isfield(data, 'trial') && isa(data.trial, 'cell');
 isfreq     = (isfield(data, 'label') || isfield(data, 'labelcmb')) && isfield(data, 'freq'); %&& (isfield(data, 'powspctrm') || isfield(data, 'crsspctrm') || isfield(data, 'cohspctrm') || isfield(data, 'fourierspctrm') || isfield(data, 'powcovspctrm'));
-istimelock = isfield(data, 'label') && isfield(data, 'time') && ~isfield(data, 'freq'); %&& ((isfield(data, 'avg') && isnumeric(data.avg)) || (isfield(data, 'trial') && isnumeric(data.trial) || (isfield(data, 'cov') && isnumeric(data.cov))));
-iscomp     = isfield(data, 'topo') || isfield(data, 'topolabel');
-isspike    = isfield(data, 'label') && isfield(data, 'waveform') && isa(data.waveform, 'cell') && isfield(data, 'timestamp') && isa(data.timestamp, 'cell');
-isvolume   = isfield(data, 'transform') && isfield(data, 'dim');
-issource   = isfield(data, 'pos');
-isdip      = isfield(data, 'dip');
-ismvar     = isfield(data, 'dimord') && ~isempty(strfind(data.dimord, 'lag'));
-isfreqmvar = isfield(data, 'freq') && isfield(data, 'transfer');
+istimelock =  isfield(data, 'label') && isfield(data, 'time') && ~isfield(data, 'freq'); %&& ((isfield(data, 'avg') && isnumeric(data.avg)) || (isfield(data, 'trial') && isnumeric(data.trial) || (isfield(data, 'cov') && isnumeric(data.cov))));
+isspike    =  isfield(data, 'label') && isfield(data, 'waveform') && isa(data.waveform, 'cell') && isfield(data, 'timestamp') && isa(data.timestamp, 'cell');
+iscomp     =  isfield(data, 'label') && isfield(data, 'topo') || isfield(data, 'topolabel');
+isvolume   =  isfield(data, 'transform') && isfield(data, 'dim');
+issource   =  isfield(data, 'pos');
+isdip      =  isfield(data, 'dip');
+ismvar     =  isfield(data, 'dimord') && ~isempty(strfind(data.dimord, 'lag'));
+isfreqmvar =  isfield(data, 'freq') && isfield(data, 'transfer');
 
 if iscomp
   type = 'comp';  
