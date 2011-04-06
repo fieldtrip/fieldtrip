7ff355561842a6d58e134cbf0bcbaa5f37023466
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-04-06 15:09:40 +0000

    enhancement - enhanced support for source level data to compute all-to-all power correlations
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3310 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/univariate2bivariate.m
+++ private/univariate2bivariate.m
@@ -109,66 +109,134 @@ switch dtype
         mdat = nanmean(data.pow,2);
         data.pow = data.pow - mdat(:,ones(1,nrpt)); %FIXME only works for 1 frequency
       end
-      data.powcov = [reshape(data.pow * data.pow(cmb,:)', [ncmb*nvox 1]); sum(data.pow.^2,2)];
-      data        = rmfield(data, 'pow');
-      data        = rmfield(data, 'powdimord');
-      %powindx     = [nvox+(1:nvox) nvox+(1:nvox); cmb*ones(1,nvox) nvox+(1:nvox)]';
-      powindx     = [repmat(ncmb*nvox+(1:nvox)',[ncmb 1]) reshape(repmat(ncmb*nvox+cmb(:)', [nvox 1]),[nvox*ncmb 1]); ...
-                            ncmb*nvox+(1:nvox)' ncmb*nvox+(1:nvox)'];
       
-      data.pos    = [repmat(data.pos, [ncmb+1 1])];%FIXME come up with something reshape( repmat(data.pos(cmb,:),[nvox 1]);data.pos data.pos];
-      data.inside = reshape(repmat(data.inside(:), [1 ncmb+1])+repmat(nvox*(0:ncmb), [nvox 1]), [nvox*(ncmb+1) 1]);
-      if ~isempty(data.outside)
-        data.outside = reshape(repmat(data.outside(:), [1 ncmb+1])+repmat(nvox*(0:ncmb), [nvox 1]), [nvox*(ncmb+1) 1]);
-      end
-      data.powcovdimord = 'pos';
-      %data.dim(2) = size(data.pos,1);
+      if ncmb == size(data.pow,1)
+        data.powcov = data.pow * data.pow';
+        data.powcovdimord = 'pos_pos';
+        powindx     = [];
+      else
+        data.powcov = [reshape(data.pow * data.pow(cmb,:)', [ncmb*nvox 1]); sum(data.pow.^2,2)];
+        data        = rmfield(data, 'pow');
+        data        = rmfield(data, 'powdimord');
+        %powindx     = [nvox+(1:nvox) nvox+(1:nvox); cmb*ones(1,nvox) nvox+(1:nvox)]';
+        powindx     = [repmat(ncmb*nvox+(1:nvox)',[ncmb 1]) reshape(repmat(ncmb*nvox+cmb(:)', [nvox 1]),[nvox*ncmb 1]); ...
+                              ncmb*nvox+(1:nvox)' ncmb*nvox+(1:nvox)'];
+
+        data.pos    = [repmat(data.pos, [ncmb+1 1])];%FIXME come up with something reshape( repmat(data.pos(cmb,:),[nvox 1]);data.pos data.pos];
+        data.inside = reshape(repmat(data.inside(:), [1 ncmb+1])+repmat(nvox*(0:ncmb), [nvox 1]), [nvox*(ncmb+1) 1]);
+        if ~isempty(data.outside)
+          data.outside = reshape(repmat(data.outside(:), [1 ncmb+1])+repmat(nvox*(0:ncmb), [nvox 1]), [nvox*(ncmb+1) 1]);
+        end
+        data.powcovdimord = 'pos';
+        %data.dim(2) = size(data.pos,1);
+      end 
+      
     elseif strcmp(inparam, 'mom') && strcmp(outparam, 'crsspctrm'),
       %get mom as rpttap_pos_freq matrix
       %FIXME this assumes only 1 freq bin
-      mom = zeros(size(data.mom{data.inside(1)},1), size(data.pos,1));
-      mom(:, data.inside) = cat(2, data.mom{data.inside});
-      if keeprpt,
-        [nrpt,nvox]    = size(mom);
-        data.crsspctrm = [mom.*conj(mom(:,ones(1,nvox)*cmb)) abs(mom).^2];
-        data           = rmfield(data, 'mom');
-        data           = rmfield(data, 'momdimord');
-        powindx     = [nvox+(1:nvox) nvox+(1:nvox); cmb*ones(1,nvox) nvox+(1:nvox)]';
-        
-        data.pos    = [data.pos repmat(data.pos(cmb,:),[nvox 1]);data.pos data.pos];
-        data.inside = [data.inside(:); data.inside(:)+nvox];
-        data.outside = [data.outside(:); data.outside(:)+nvox];
-      elseif ncmb<size(mom,2)
-        %do it computationally more efficient
-        [nrpt,nvox]    = size(mom);
-        
-        data.crsspctrm = reshape((transpose(mom)*conj(mom(:,cmb)))./nrpt, [nvox*ncmb 1]);
-        tmppow         = mean(abs(mom).^2)';
-        data.crsspctrm = cat(1, data.crsspctrm, tmppow);
-        tmpindx1       = transpose(ncmb*nvox + ones(ncmb+1,1)*(1:nvox));
-        tmpindx2       = repmat(tmpindx1(cmb(:),end), [1 nvox])';
-        tmpindx3       = repmat(cmb(:), [1 nvox])'; %expressed in original voxel indices
-        powindx        = [tmpindx1(:) [tmpindx2(:);tmpindx1(:,end)]];
-        
-        data.pos       = [repmat(data.pos, [ncmb 1]) data.pos(tmpindx3(:),:); data.pos data.pos];
-        data.inside    = data.inside(:)*ones(1,ncmb+1) + (ones(length(data.inside),1)*nvox)*(0:ncmb);
-        data.inside    = data.inside(:);
-        data.outside   = setdiff((1:nvox*(ncmb+1))', data.inside);
-        if isfield(data, 'momdimord'),
-          data.crsspctrmdimord = ['pos_',data.momdimord(14:end)];%FIXME this assumes dimord to be 'rpttap_...'
+      sizmom = size(data.mom{data.inside(1)});
+      
+      if sizmom(2)==1,
+        mom = zeros(sizmom(1), size(data.pos,1));
+        mom(:, data.inside) = cat(2, data.mom{data.inside});
+        if keeprpt,
+          [nrpt,nvox]    = size(mom);
+          data.crsspctrm = [mom.*conj(mom(:,ones(1,nvox)*cmb)) abs(mom).^2];
+          data           = rmfield(data, 'mom');
+          data           = rmfield(data, 'momdimord');
+          powindx     = [nvox+(1:nvox) nvox+(1:nvox); cmb*ones(1,nvox) nvox+(1:nvox)]';
+
+          data.pos    = [data.pos repmat(data.pos(cmb,:),[nvox 1]);data.pos data.pos];
+          data.inside = [data.inside(:); data.inside(:)+nvox];
+          data.outside = [data.outside(:); data.outside(:)+nvox];
+        elseif ncmb<size(mom,2)
+          %do it computationally more efficient
+          [nrpt,nvox]    = size(mom);
+
+          data.crsspctrm = reshape((transpose(mom)*conj(mom(:,cmb)))./nrpt, [nvox*ncmb 1]);
+          tmppow         = mean(abs(mom).^2)';
+          data.crsspctrm = cat(1, data.crsspctrm, tmppow);
+          tmpindx1       = transpose(ncmb*nvox + ones(ncmb+1,1)*(1:nvox));
+          tmpindx2       = repmat(tmpindx1(cmb(:),end), [1 nvox])';
+          tmpindx3       = repmat(cmb(:), [1 nvox])'; %expressed in original voxel indices
+          powindx        = [tmpindx1(:) [tmpindx2(:);tmpindx1(:,end)]];
+
+          data.pos       = [repmat(data.pos, [ncmb 1]) data.pos(tmpindx3(:),:); data.pos data.pos];
+          data.inside    = data.inside(:)*ones(1,ncmb+1) + (ones(length(data.inside),1)*nvox)*(0:ncmb);
+          data.inside    = data.inside(:);
+          data.outside   = setdiff((1:nvox*(ncmb+1))', data.inside);
+          if isfield(data, 'momdimord'),
+            data.crsspctrmdimord = ['pos_',data.momdimord(14:end)];%FIXME this assumes dimord to be 'rpttap_...'
+          end
+          data           = rmfield(data, 'mom');
+          data           = rmfield(data, 'momdimord');
+        else
+          [nrpt,nvox]    = size(mom);
+          data.crsspctrm = (transpose(mom)*conj(mom))./nrpt;
+          data           = rmfield(data, 'mom');
+          data           = rmfield(data, 'momdimord');
+          powindx        = [];
+          data.crsspctrmdimord = 'pos_pos_freq'; %FIXME hard coded
         end
-        data           = rmfield(data, 'mom');
-        data           = rmfield(data, 'momdimord');
-      else
-        [nrpt,nvox]    = size(mom);
-        data.crsspctrm = (transpose(mom)*conj(mom))./nrpt;
-        data           = rmfield(data, 'mom');
-        data           = rmfield(data, 'momdimord');
-        powindx        = [];
-        data.crsspctrmdimord = 'pos_pos_freq'; %FIXME hard coded
-      end
-      data.dimord = data.crsspctrmdimord;
-      clear mom;
+        data.dimord = data.crsspctrmdimord;
+        clear mom;
+      elseif sizmom(2)>1
+        % source moments are multivariate
+        
+        tmpindx   = reshape(1:size(data.pos,1)*sizmom(2), [sizmom(2) size(data.pos,1)]);
+        tmpinside = tmpindx(:, data.inside);
+        tmpcmb    = tmpindx(:, cmb);
+        tmpncmb   = numel(tmpcmb);
+
+	mom               = zeros(sizmom(1), sizmom(2)*size(data.pos,1));
+        mom(:, tmpinside(:)) = cat(2, data.mom{data.inside});
+        if keeprpt,
+          error('keeprpt with multivariate dipole moments is not supported');
+          %FIXME should this be supported
+        elseif tmpncmb<size(mom,2)
+          %do it computationally more efficient
+          [nrpt,nvox]    = size(mom);
+
+          %linearly represent the voxel-pair csd matrices
+          data.crsspctrm = reshape((transpose(mom)*conj(mom(:,tmpcmb)))./nrpt, [nvox*tmpncmb 1]);
+          offsetauto     = size(data.crsspctrm,1);
+
+	  %linearly represent the per voxel csd matrices
+          tmppow = zeros(numel(tmpindx), size(tmpindx,1));
+	  for k = 1:size(tmpindx,2)
+	    tmppow(tmpindx(:,k), :) = (transpose(mom(:,tmpindx(:,k)))*conj(mom(:,tmpindx(:,k))))./nrpt;
+	  end
+	  tmppow = reshape(tmppow, [numel(tmppow) 1]);
+          data.crsspctrm = cat(1, data.crsspctrm, tmppow);
+          
+	  tmpindx1       = transpose(ncmb*nvox + ones(ncmb+1,1)*(1:nvox));
+          tmpindx2       = repmat(tmpindx1(cmb(:),end), [1 nvox])';
+          tmpindx3       = repmat(cmb(:), [1 nvox])'; %expressed in original voxel indices
+          powindx        = [tmpindx1(:) [tmpindx2(:);tmpindx1(:,end)]];
+
+          data.pos       = [repmat(data.pos, [ncmb 1]) data.pos(tmpindx3(:),:); data.pos data.pos];
+          data.inside    = data.inside(:)*ones(1,ncmb+1) + (ones(length(data.inside),1)*nvox)*(0:ncmb);
+          data.inside    = data.inside(:);
+          data.outside   = setdiff((1:nvox*(ncmb+1))', data.inside);
+          if isfield(data, 'momdimord'),
+            data.crsspctrmdimord = ['pos_',data.momdimord(14:end)];%FIXME this assumes dimord to be 'rpttap_...'
+          end
+          data           = rmfield(data, 'mom');
+          data           = rmfield(data, 'momdimord');
+        else
+          [nrpt,nvox]    = size(mom);
+          data.crsspctrm = (transpose(mom)*conj(mom))./nrpt;
+          data           = rmfield(data, 'mom');
+          data           = rmfield(data, 'momdimord');
+          powindx        = [];
+          data.crsspctrmdimord = 'pos_pos_freq'; %FIXME hard coded
+        end
+        data.dimord = data.crsspctrmdimord;
+        clear mom;
+      
+        
+      end % if sizmom(2)
+      
     else
       error('unknown conversion from univariate to bivariate representation');
     end
