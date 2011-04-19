b14f02c265c83c5e333ea60d5080524e7559b219
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-04-19 09:29:57 +0000

    automatically synchronized identical files to revision 3357
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3358 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/dimlength.m
+++ private/dimlength.m
@@ -112,6 +112,7 @@ else
         if isfield(data, 'powspctrm'),     n = [n size(data.powspctrm,     1)]; end
         if isfield(data, 'trial'),         n = [n size(data.trial,         1)]; end
         if isfield(data, 'fourierspctrm'), n = [n size(data.fourierspctrm, 1)]; end
+        if isfield(data, 'individual'),    n = [n size(data.individual,    1)]; end
         if ~all(n==n(1))
           error('inconsistent number of repetitions for dim "%s"', seldim);
         end
