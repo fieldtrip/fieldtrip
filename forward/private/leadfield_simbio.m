61dd33ae38c3bcbb09183b114258980e36747ec5
Cristiano Micheli <michelic72@googlemail.com>
2011-05-04 08:32:37 +0000

    implemented working version of simbio wrapper
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3430 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/private/leadfield_simbio.m
+++ forward/private/leadfield_simbio.m
@@ -1,15 +1,19 @@
-function [lf] = leadfield_simbio(dip, elc, vol)
+function [lf] = leadfield_simbio(elc, vol, dip)
 
 % LEADFIELD_SIMBIO leadfields for a set of dipoles
 %
-% [lf] = leadfield_simbio(dip, elc, vol);
+% [lf] = leadfield_simbio(elc, vol, headmeshfile);
 %
 % with input arguments
-%   dip     positions of the dipoles (matrix of dimensions NX3)
 %   elc     positions of the electrodes (matrix of dimensions MX3)
-% 
-% and vol being a structure with the element
-%   vol.meshfile file containing the 3D mesh filename
+%   vol.brainmesh contains the positions of the vertices on which
+%           the potentials are calculated
+%   vol.headmesh contains the position of the vertices of the 'head mesh'
+%           which is needed to perform a FEM analysis, and an integer
+%           number.
+%           This is the output of a step in which from a labelled MRI a
+%           volumetric mesh of points is generated, and a label (or compartment) 
+%           is assigned to each point
 %
 % The output lf is the leadfields matrix of dimensions M (rows) X N*3 (cols)
 
@@ -26,21 +30,35 @@ try
     [junk,tname] = fileparts(tempname);
     elcfile  = [tname '.elc'];
     [junk,tname] = fileparts(tempname);
-    dipfile  = [tname '.dip'];
+    dipfile  = [tname '.dip'];    
+    [junk,tname] = fileparts(tempname);
+    parfile  = [tname '.par'];
     [junk,tname] = fileparts(tempname);
     outfile  = [tname];
     
     % write the electrodes and dipoles positions in the temporary folder
-    sb_write_elc(elc.pnt,elc.labels,elcfile);
+    sb_write_elc(elc.pnt,elc.label,elcfile);
     sb_write_dip(dip,dipfile);
+    % writes the parameters file
+    cfg = [];
+    cfg.cond   = vol.cond;
+    cfg.labels = vol.labels;
+    sb_write_par(cfg,parfile);
+    
+    % FIXME: vol in future will contain the mesh as well (right?)
+    % at the moment contains only a name of the vista file for memory
+    % reasons (vol.headmesh)
     
     % Exe file
     % FIXME: does SimBio have a switch for parallel processing (to run on more cores)?
     efid = fopen(exefile, 'w');
     if ~ispc
       fprintf(efid,'#!/usr/bin/env bash\n');
-      fprintf(efid,['ipm_linux_opt_Venant -i sourcesimulation -h ' vol.meshfile ' -s ./' elcfile, ...
-        ' -dip ./' dipfile ' -o ' outfile ' -p parameterfile.par -fwd FEM -sens EEG 2>&1 > /dev/null\n']);
+%       fprintf(efid,['ipm_linux_opt_Venant -i leadfieldmatrix_onbrainsurface -h ' vol.headmesh ' -s ./' elcfile, ...
+%         ' -g ' vol.brainmesh ' -leadfieldfile ' outfile ' -p ' parfile ' -fwd FEM -sens EEG 2>&1 > /dev/null\n']);
+
+      fprintf(efid,['ipm_linux_opt_Venant -i sourcesimulation -h ' vol.headmesh ' -s ./' elcfile, ...
+                    ' -dip ' dipfile ' -o ' outfile ' -p ' parfile ' -fwd FEM -sens EEG 2>&1 > /dev/null\n']);
     end
     fclose(efid);
     
