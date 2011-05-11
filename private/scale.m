413d40b2cdce57fab3402b802da1e9636857339d
Robert Oostenveld <r.oostenveld@gmail.com>
2011-05-11 06:59:09 +0000

    bugfix - added the translate/rotate and scale functions back to private, they are directly used in electrorerealign
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3500 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ private/scale.m
@@ -0,0 +1,57 @@
+function [H] = scale(f)
+
+% SCALE returns the homogenous coordinate transformation matrix
+% corresponding to a scaling along the x, y and z-axis
+% 
+% Use as
+%   [H] = translate(S)
+% where
+%   S       [sx, sy, sz] scaling along each of the axes
+%   H   corresponding homogenous transformation matrix
+
+% Copyright (C) 2000-2005, Robert Oostenveld
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
+if numel(f)~=3
+  error('incorrect input vector');
+end
+
+H = [
+  f(1) 0    0    0 
+  0    f(2) 0    0
+  0    0    f(3) 0
+  0    0    0    1
+  ];
+
+  
