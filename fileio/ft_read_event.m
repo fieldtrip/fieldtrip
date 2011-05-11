e67ac6dafc1a1ee7000da2e7a9980e9ad1a5015a
Ingrid Nieuwenhuis <ingrid.nieuwenhuis@gmail.com>
2011-05-11 23:04:16 +0000

    bugfix: changed offset of event to 0, to get correct timing of events through EEGlab fileo read-in. Added removal of events that occur before the start of the recording.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3523 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/ft_read_event.m
+++ fileio/ft_read_event.m
@@ -714,7 +714,7 @@ switch eventformat
     % construct info needed for FieldTrip Event
     eventNames = fieldnames(xml);
     begTime = hdr.orig.xml.info.recordTime;
-    begTime(11) = ' '; begTime(end-6:end) = [];
+    begTime(11) = ' '; begTime(end-5:end) = [];
     begSDV = datenum(begTime);
     % find out if there are epochs in this dataset
     if isfield(hdr.orig.xml,'epoch') && length(hdr.orig.xml.epoch) > 1
@@ -731,31 +731,39 @@ switch eventformat
     eventCount = 0;
     for iXml = 1:length(eventNames)
       for iEvent = 1:length(xml.(eventNames{iXml}))
-        eventCount = eventCount+1;
         eventTime  = xml.(eventNames{iXml})(iEvent).event.beginTime;
-        eventTime(11) = ' '; eventTime(end-6:end) = [];
-        eventSDV = datenum(eventTime);
-        eventOffset = round((eventSDV - begSDV)*24*60*60*hdr.Fs); %in samples
-        % eventSample   
-        if isfield(hdr.orig.xml,'epoch') && length(hdr.orig.xml.epoch) > 1
-          for iEpoch = 1:size(hdr.orig.epochdef,1)
-            [dum,dum2,dum3] = intersect(squeeze(Msamp2offset(2,iEpoch,:)), eventOffset);
-            if ~isempty(dum2)
-              EpochNum = iEpoch;
-              SampIndex = dum2;
-            end
-          end
-          eventSample = Msamp2offset(1,EpochNum,SampIndex);
+        eventTime(11) = ' '; eventTime(end-5:end) = [];
+        if strcmp('-',eventTime(21))
+          % event out of range (before recording started): do nothing.
         else
-          eventSample = eventOffset+1;
-        end
+          eventSDV = datenum(eventTime);
+          eventOffset = round((eventSDV - begSDV)*24*60*60*hdr.Fs); %in samples, relative to start of recording
+          if eventOffset < 0
+            % event out of range (before recording started): do nothing
+          else
+            eventCount = eventCount+1;
+            % calculate eventSample, relative to start of epoch
+            if isfield(hdr.orig.xml,'epoch') && length(hdr.orig.xml.epoch) > 1
+              for iEpoch = 1:size(hdr.orig.epochdef,1)
+                [dum,dum2] = intersect(squeeze(Msamp2offset(2,iEpoch,:)), eventOffset);
+                if ~isempty(dum2)
+                  EpochNum = iEpoch;
+                  SampIndex = dum2;
+                end
+              end
+              eventSample = Msamp2offset(1,EpochNum,SampIndex);
+            else
+              eventSample = eventOffset+1;
+            end
 
-        event(eventCount).type     = eventNames{iXml}(8:end);
-        event(eventCount).sample   = eventSample;
-        event(eventCount).offset   = eventOffset;
-        event(eventCount).duration = str2double(xml.(eventNames{iXml})(iEvent).event.duration)./1000000000*hdr.Fs;
-        event(eventCount).value    = xml.(eventNames{iXml})(iEvent).event.code;
-      end
+            event(eventCount).type     = eventNames{iXml}(8:end);
+            event(eventCount).sample   = eventSample;
+            event(eventCount).offset   = 0;
+            event(eventCount).duration = str2double(xml.(eventNames{iXml})(iEvent).event.duration)./1000000000*hdr.Fs;
+            event(eventCount).value    = xml.(eventNames{iXml})(iEvent).event.code;
+          end  %if that takes care of non "-" events that are still out of range
+        end %if that takes care of "-" events, which are out of range
+      end %iEvent
     end
 
 
