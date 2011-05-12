1f17a1c2289656e30af1bf2ae664c2d20c39297b
Robert Oostenveld <r.oostenveld@gmail.com>
2011-05-12 15:16:13 +0000

    enhancement - added support for yokogawa64 and yokogawa64_planar, although the latter does not work yet (see http://bugzilla.fcdonders.nl/show_bug.cgi?id=663)
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3535 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_prepare_layout.m
+++ ft_prepare_layout.m
@@ -695,13 +695,10 @@ fprintf('creating layout for %s system\n', ft_senstype(sens));
 % apply rotation
 if isempty(rz)
   switch ft_senstype(sens)
-    case {'ctf151', 'ctf275', 'bti148', 'bti248', 'ctf151_planar', 'ctf275_planar', 'bti148_planar', ...
-            'bti248_planar', 'yokogawa160', 'yokogawa160_planar', 'magnetometer', 'meg'}
+    case {'ctf151', 'ctf275', 'bti148', 'bti248', 'ctf151_planar', 'ctf275_planar', 'bti148_planar', 'bti248_planar', 'yokogawa160', 'yokogawa160_planar', 'yokogawa64', 'yokogawa64_planar', 'magnetometer', 'meg'}
       rz = 90;
     case {'neuromag122', 'neuromag306'}
       rz = 0; 
-    case {'yokogawa160','yokogawa160_planar'}
-      rz = 90;
     case 'electrode'
       rz = 90;
     otherwise
