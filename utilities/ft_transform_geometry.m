95f47ea4dbaa9394b6891e1334790185f0752b54
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-05-27 20:36:05 +0000

    created placeholder file
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3588 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ utilities/ft_transform_geometry.m
@@ -0,0 +1,30 @@
+function [output] = ft_transform_geometry(transform, input)
+
+% FT_TRANSFORM_GEOMETRY applies a homogeneous coordinate transformation to
+% a structure with geometric information. The homogenous
+% transformation matrix should be limited to a rigid-body translation
+% plus rotation and a global rescaling.
+%
+% Use as
+%   output = ft_transform_geometry(transform, input)
+%
+
+% Copyright (C) 2011, Jan-Mathijs Schoffelen
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id: ft_transform_geometry.m$
