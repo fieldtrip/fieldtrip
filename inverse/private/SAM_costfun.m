c6505503b14e47cc9d8df9c0fc5b74dd916a452a
Robert Oostenveld <r.oostenveld@gmail.com>
2011-06-06 18:36:16 +0000

    bugfix - copied missing beamformer_sam from th eold cvs to svn, the method=sam was documented in sourceanalysis but missing from the release. I did not test it recently so am not sure whether it still works.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3628 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ inverse/private/SAM_costfun.m
@@ -0,0 +1,45 @@
+function inv_pseudo_Z = SAM_costfun(angle, position, tanu, tanv, lf, covariance, inv_covariance, noise_covariance);
+        
+% costfunction for non-linear beamformer. Use this cost-function to find the optimum orientation (in the tangential plane formed by tanu and tanv) of the targetvoxel
+% maximizes the pseudo_Z (i.e. minimises the inverse of pseudo_Z)
+%
+% positions in mm in CTF co-ordinate system
+%
+% AH, 05april 2005: if origin = [], then the multisphere headmodel will be used for the forward calculations. The multisphere porigins should be 
+% given in forward_resource (in mm in CTF co-ordinates)
+
+
+MDip = settang(angle, tanu, tanv);
+MagDip = sqrt(dot(MDip,MDip));
+UnitMDip = MDip/MagDip;
+
+surface.positions = position;
+surface.vert_normals = UnitMDip;
+
+gain = lf * UnitMDip';
+
+% if ~isempty(origin)
+%     % single sphere forward calculation
+%     [junk, gain] = ComputeLeads(surface, forward_resource, origin, [], 0); 
+% else
+%     % multi sphere forward calculation
+%     % disp(sprintf('\n\n USING MULTISPHERES\n\n'))
+%     gain = Compute_leads_Multi_fastM(forward_resource, surface.positions, surface.vert_normals, forward_resource.startsensor);
+% end
+
+trgain_invC = gain' * inv_covariance;
+SAMweights =  trgain_invC / (trgain_invC * gain);
+
+P = SAMweights * covariance * SAMweights';
+N = SAMweights * noise_covariance * SAMweights'; 
+inv_pseudo_Z = sqrt(N/P);
+
+% make sure the angle stays between 0 and 180 degrees
+if (angle > pi) | (angle < 0) 
+    inv_pseudo_Z = inv_pseudo_Z*10^4;
+else
+%    hold on
+%    plot(angle*180/pi, inv_pseudo_Z,'b*')
+%    drawnow    
+end  
+
