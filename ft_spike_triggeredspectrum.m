c7161ffe63794d784ceadd060eb123645868d140
Martin Vinck <martinvinck@gmail.com>
2011-06-09 20:26:57 +0000

    nearest_nd removed
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3669 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_spike_triggeredspectrum.m
+++ ft_spike_triggeredspectrum.m
@@ -136,7 +136,10 @@ for iTrial = 1:nTrials
         % get all the samples at once without using loops
         hasTrial = spike.trial{spikesel} == iTrial;
         ts       = spike.time{spikesel}(hasTrial);
-        spikesmp = nearest_nd(data.time{iTrial},ts); % this gives unique samples back     
+        spikesmp = zeros(1,length(ts));
+        for k = 1:length(ts)
+          spikesmp(k) = nearest(data.time{iTrial},ts(k)); % this gives unique samples back     
+        end
 
         % calculate the phase shift based on difference between 'exact' and sample time
         ts       = ts(:);
@@ -351,105 +354,6 @@ end
 try, cfg.previous = data.cfg; end
 % remember the exact configuration details in the output
 freq.cfg = cfg;
-
-function [indx] = nearest_nd(x,y)
-
-% NEAREST return the index of an n-d matrix to an n-d matrix.
-% 
-% [indx] = nearest_nd(x, y)
-%
-% Inputs: 
-%   X can be a n-d matrix of any size (scalar, vector, n-d matrix).
-%   Y can be an n-d matrix of any size (scalar, vector, n-d matrix).
-%   
-% If Y is larger than any X, we return the last index that the maximum value of X occurred.
-% Otherwise, we return the first occurence of the nearest X.
-%
-% If Y contains NaNs, we return a NaN for every NaN in Y.
-%
-% Outputs:
-%   INDX is a vector of size Y and contains the indices of the values in X that are
-%   closest to the respective value of Y. INDX is a linear index, such that x(INDX) gives
-%   the nearest values of X to Y. To convert INDX to subscripts, see IND2SUB.
-%
-% Example:
-% y = 100*rand(5,10);
-% x = 1:100;
-% indx = nearest_nd(x,y);
-% disp(max(max(y-x(indx))))
-%
-% x = reshape([1:100 100*ones(1,20)],[20 6]);
-% y = rand(5,10)*max(x(:));
-% y(1) = 100.2;
-% indx = nearest_nd(x,y);
-% disp(max(max(y-x(indx))))
-% Note that indx(1) returns the last occurence and indx is a linear index to 2-d X.
-% 
-% Demonstrate the use with a 3-D x and y array.
-% x = reshape([1:100],[10 5 2]);
-% y = rand(5,10,2)*max(x(:));
-% indx = nearest_nd(x,y);
-% d = x(indx)-y; disp(max(d(:)));
-% y(1) = NaN;
-% indx = nearest_nd(x,y);
-% disp(indx(1))
-%
-
-% Copyright, Martin Vinck, 2009.
-
-% store the sizes of x and y, this is used to reshape INDX later on
-szY = size(y);
-
-% vectorize both x and y
-x = x(:);
-y = y(:);
-
-% from now on we can treat X and Y as vectors
-nY = length(y);
-nX = length(x);
-hasNan = isnan(y); % indices with nans in y, indx(hasNaN) will be set to NaN later.
-
-if nX==1, 
-  indx = ones(1,nY);  % only one x value, so nearest is always only element
-else
-  if nY==1 % in this case only one y value, so use the old NEAREST code
-    if y>max(x)
-      % return the last occurence of the nearest number
-      [dum, indx] = max(flipud(x));
-      indx = nX + 1 - indx;
-    else
-      % return the first occurence of the nearest number
-      [mindist, indx] = min(abs(x(:) - y));
-    end
-  else
-    if any(y>max(x))
-      % for these return the last occurence of every number as in NEAREST
-      indx = zeros(1,nY);
-      i = y>max(x);
-      [dum,indx] = max(flipud(x));
-      indx(i)       = nX + 1 - indx;    
-      % for the rest return the first occurence of every number
-      x = x(:); 
-      y = y(~i)';
-      xRep = x(:,ones(1,length(y)));
-      yRep = y(ones(nX,1),:);
-      [mindist,indx(~i)] = min(abs(xRep-yRep));
-    else
-      x = x(:); 
-      y = y';
-      xRep = x(:,ones(1,nY));
-      yRep = y(ones(nX,1),:);
-      [mindist,indx] = min(abs(xRep-yRep));
-    end        
-  end
-end
-% return a NaN in INDX for a NaN in Y
-indx(hasNan) = NaN;
-
-% reshape the indx back to the y format
-if (sum(szY>1)>1 || length(szY)>2) % in this case we are dealing with a matrix
-  indx = reshape(indx,[szY]);
-end
 
 function y = linspacevec(d1, d2, n)
 
