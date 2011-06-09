a020818ed8e2cca3464f5cb79ba22cb039ba39d4
Robert Oostenveld <r.oostenveld@gmail.com>
2011-06-09 07:38:37 +0000

    automatically synchronized identical files to revision 3654
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3655 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/private/channelposition.m
+++ plotting/private/channelposition.m
@@ -27,34 +27,8 @@ function [pnt, ori, lab] = channelposition(sens, varargin)
 %
 % $Id$
 
-while isfield(sens, 'balance') && isfield(sens.balance, 'current') && ~strcmp(sens.balance.current, 'none')
-  fnames = setdiff(fieldnames(sens.balance), 'current');
-  indx   = find(ismember(fnames, sens.balance.current));
-
-  if length(indx)==1,
-    % undo the synthetic gradient balancing
-    fprintf('undoing the %s balancing\n', sens.balance.current);
-    
-    % if componentanalysis was followed by rejectcomponent, the balancing matrix is rank deficient
-    % leading to problems in the correct allocation of the coils to the channels 
-    if strcmp(sens.balance.current, 'invcomp') && strcmp(sens.balance.previous{1}, 'comp')
-      tra1 = full(sens.balance.invcomp.tra);
-      tra2 = full(sens.balance.comp.tra);
-      tmp  = tra1*tra2;
-      tmp  = null(tmp); % nullspace after componentanalysis and rejectcomponent
-      tmp  = tmp*tmp';  % this is the part which was removed at some point
-      [ix,iy] = match_str(sens.balance.comp.labelorg, sens.balance.invcomp.labelnew);
-      tra3(iy,iy) = (eye(numel(ix))+tmp(ix,ix))*tra1(iy,iy);
-      sens.balance.invcomp.tra = sparse(tra3);
-      % FIXME check whether this is robust
-    end
-      
-    sens = ft_apply_montage(sens, sens.balance.(sens.balance.current), 'inverse', 'yes', 'keepunused', 'yes');
-  else
-    warning('cannot undo %s balancing\n', sens.balance.current);
-    break
-  end
-end
+% remove the balancing from the sensor definition, e.g. 3rd order gradients, PCA-cleaned data or ICA projections
+sens = undobalancing(sens);
 
 switch ft_senstype(sens)
   case {'ctf151', 'ctf275' 'bti148', 'bti248', 'itab153', 'yokogawa160', 'yokogawa64'}
