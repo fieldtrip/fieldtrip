9f3321d3422c74ca601a10ba798086564c4cd9fc
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-06-15 09:06:44 +0000

    bugfix - #763 added helper functions  in csd2transfer
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3695 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/csd2transfer.m
+++ private/csd2transfer.m
@@ -683,3 +683,109 @@ gp = reshape(transpose(gp), [2 2 ncmb 2*(nfreq-1)]);
 %    gp(k1,k2,:) = fft(squeeze(gamp(k1,k2,:)));
 %  end
 %end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%subfunctions for the 2x2 functionality
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%determinant of a 2x2 matrix
+function [d] = det2x2(x)
+
+%computes determinant of matrix x, using explicit analytic definition if
+%size(x,1) < 4, otherwise use matlab det-function
+
+siz = size(x);
+if all(siz(1:2)==2),
+  d = x(1,1,:,:).*x(2,2,:,:) - x(1,2,:,:).*x(2,1,:,:);
+elseif all(siz(1:2)==3),
+  d = x(1,1,:,:).*x(2,2,:,:).*x(3,3,:,:) - ...
+      x(1,1,:,:).*x(2,3,:,:).*x(3,2,:,:) - ...
+      x(1,2,:,:).*x(2,1,:,:).*x(3,3,:,:) + ...
+      x(1,2,:,:).*x(2,3,:,:).*x(3,1,:,:) + ...
+      x(1,3,:,:).*x(2,1,:,:).*x(3,2,:,:) - ...
+      x(1,3,:,:).*x(2,2,:,:).*x(3,1,:,:);
+elseif numel(siz)==2,
+  d = det(x);
+else
+  %error   
+  %write for loop
+  %for
+  %end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% inverse of a 2x2 matrix
+function [d] = inv2x2(x)
+
+%computes inverse of matrix x, using explicit analytic definition if
+%size(x,1) < 4, otherwise use matlab inv-function
+
+siz = size(x);
+if all(siz(1:2)==2),
+  adjx  = [x(2,2,:,:) -x(1,2,:,:); -x(2,1,:,:) x(1,1,:,:)];
+  denom = det2x2(x);
+  d     = adjx./denom([1 1],[1 1],:,:);
+elseif all(siz(1:2)==3),
+  adjx = [ det2x2(x([2 3],[2 3],:,:)) -det2x2(x([1 3],[2 3],:,:))  det2x2(x([1 2],[2 3],:,:)); ...
+          -det2x2(x([2 3],[1 3],:,:))  det2x2(x([1 3],[1 3],:,:)) -det2x2(x([1 2],[1 3],:,:)); ...
+	   det2x2(x([2 3],[1 2],:,:)) -det2x2(x([1 3],[1 2],:,:))  det2x2(x([1 2],[1 2],:,:))];
+  denom = det2x2(x);
+  d     = adjx./denom([1 1 1],[1 1 1],:,:);
+elseif numel(siz)==2,
+  d = inv(x);
+else
+  error('cannot compute slicewise inverse');
+  %write for loop for the higher dimensions, using normal inv
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% matrix multiplication 2x2
+function [z] = mtimes2x2(x, y)
+
+% compute x*y 
+% and dimensionatity is 2x2xN
+
+z     = complex(zeros(size(x)));
+xconj = conj(x);
+
+z(1,1,:,:) = x(1,1,:,:).*y(1,1,:,:) + x(1,2,:,:).*y(2,1,:,:);
+z(1,2,:,:) = x(1,1,:,:).*y(1,2,:,:) + x(1,2,:,:).*y(2,2,:,:);
+z(2,1,:,:) = x(2,1,:,:).*y(1,1,:,:) + x(2,2,:,:).*y(2,1,:,:);
+z(2,2,:,:) = x(2,1,:,:).*y(1,2,:,:) + x(2,2,:,:).*y(2,2,:,:);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% quadratic multiplication 2x2 matrix,  sandwiched matrix assumed hermitian
+function [z] = sandwich2x2(x, y)
+
+% compute x*y*x' provided y = hermitian
+% and dimensionatity is 2x2xN
+
+%FIXME build in check for hermitianity
+z     = complex(zeros(size(x)));
+xconj = conj(x);
+xabs2 = abs(x).^2;
+
+z(1,1,:,:) = xabs2(1,1,:,:) .* y(1,1,:,:) + ...
+             xabs2(1,2,:,:) .* y(2,2,:,:) + ...
+           2.*real(y(2,1,:,:).*xconj(1,1,:,:).*x(1,2,:,:));
+z(2,1,:,:) = y(1,1,:,:).*xconj(1,1,:,:).*x(2,1,:,:) + ...
+           y(2,1,:,:).*xconj(1,1,:,:).*x(2,2,:,:) + ...
+           y(1,2,:,:).*xconj(1,2,:,:).*x(2,1,:,:) + ...
+           y(2,2,:,:).*xconj(1,2,:,:).*x(2,2,:,:);
+z(1,2,:,:) = conj(z(2,1,:,:));
+z(2,2,:,:) = xabs2(2,1,:,:) .* y(1,1,:,:) + ...
+             xabs2(2,2,:,:) .* y(2,2,:,:) + ...
+           2.*real(y(1,2,:,:).*xconj(2,2,:,:).*x(2,1,:,:));
+
+%b1 b2     a1 a2'   b1' b3'
+%b3 b4     a2 a3    b2' b4'
+%
+%b1*a1+b2*a2  b1*a2'+b2*a3  b1' b3'
+%b3*a1+b4*a2  b3*a2'+b4*a3  b2' b4'
+%
+%b1*a1*b1'+b2*a2*b1'+b1*a2'*b2'+b2*a3*b2' b1*a1*b3'+b2*a2*b3'+b1*a2'*b4'+b2*a3*b4'
+%b3*a1*b1'+b4*a2*b1'+b3*a2'*b2'+b4*a3*b2' b3*a1*b3'+b4*a2*b3'+b3*a2'*b4'+b4*a3*b4'
+%
+%a1*abs(b1)^2 + a2*(b1'*b2) + a2'*(b1*b2') + a3*abs(b2)^2    a1*b1*b3'    + a2*b2*b3'   + a2'*b1*b4'   + a3*b2*b4'
+%a1*b1'*b3    + a2*b1'*b4   + a2'*b2'*b3   + a3*b2'*b4       a1*abs(b3)^2 + a2*(b3'*b4) + a2'*(b3*b4') + a3*abs(b4)^2
