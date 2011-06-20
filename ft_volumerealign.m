f1d2fd42c233f1ad6556045826119bbd3ef440c1
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-06-20 14:19:06 +0000

    enhancement - #770 allow for the use of arrow-keys to scroll through the volume. Also improved the division of the figure into subplots (made them slightly larger) to facilitate selection. Built-in new (experimental) functionality to register arbitrary points by clicking the right mouse button
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3714 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_volumerealign.m
+++ ft_volumerealign.m
@@ -152,6 +152,10 @@ if iscell(cfg.parameter)
   cfg.parameter = cfg.parameter{1};
 end
 
+h1 = subplot('position',[0.02 0.55 0.44 0.44]);%subplot(2,2,1);
+h2 = subplot('position',[0.52 0.55 0.44 0.44]);%subplot(2,2,2);
+h3 = subplot('position',[0.02 0.05 0.44 0.44]);%subplot(2,2,3);
+handles = {h1 h2 h3};
 switch cfg.method
   case 'fiducial'
     % do nothing
@@ -161,7 +165,8 @@ switch cfg.method
     
   case 'interactive'
     showcrosshair = true;
-    dat = getsubfield(mri, cfg.parameter);
+    showmarkers   = true;
+    dat = mri.(cfg.parameter);
     nas = [];
     lpa = [];
     rpa = [];
@@ -175,51 +180,78 @@ switch cfg.method
     yc = round(mri.dim(2)/2);
     zc = round(mri.dim(3)/2);
     
+    updatepanel = [1 2 3];
+    pnt         = zeros(0,3);
+    markerpos   = zeros(0,3);
+    markerlabel = {};
+    markercolor = {};
     while true % break when 'q' is pressed
       fprintf('click with mouse button to reslice the display to a new position\n');
       fprintf('press n/l/r on keyboard to record the current position as fiducial location\n');
       fprintf('press a/p/z on keyboard to record the current position as anatomical landmark\n');
-      fprintf('press i,j,k or I,J,K on the keyboard to increment or decrement the slice number by one\n');
+      fprintf('press the arrow keys on the keyboard to increment or decrement the slice number by one\n');
       fprintf('press c or C on the keyboard to show or hide the crosshair\n');
       fprintf('press q on keyboard to quit interactive mode\n');
+      
       xc = round(xc); xc = max(1,xc); xc = min(mri.dim(1),xc);
       yc = round(yc); yc = max(1,yc); yc = min(mri.dim(2),yc);
       zc = round(zc); zc = max(1,zc); zc = min(mri.dim(3),zc);
-      volplot(x, y, z, dat, [xc yc zc], cfg.clim, showcrosshair);
+      markers = {markerpos markerlabel markercolor};
+      [h1, h2, h3] = volplot(x, y, z, dat, [xc yc zc], cfg.clim, showcrosshair, updatepanel, handles, showmarkers, markers);
       drawnow;
       try, [d1, d2, key] = ginput(1); catch, key='q'; end
-      if key=='q'
+      switch key
+      case 113 % 'q'
         break;
-      elseif key=='l'
+      case 108 % 'l'
         lpa = [xc yc zc];
-      elseif key=='r'
+      case 114 % 'r'
         rpa = [xc yc zc];
-      elseif key=='n'
+      case 110 % 'n'
         nas = [xc yc zc];
-      elseif key=='a'
+      case 97  % 'a'
         antcomm = [xc yc zc];
-      elseif key=='p'
+      case 112 % 'p'
         pstcomm = [xc yc zc];
-      elseif key=='z'
+      case 122 % 'z'
         xzpoint = [xc yc zc];
-      elseif key=='c'
+      case 99  % 'c'
         showcrosshair = true;
-      elseif key=='C'
+      case 67  % 'C'
         showcrosshair = false;
-      elseif key=='i'
-        xc = xc+1;
-      elseif key=='I'
-        xc = xc-1;
-      elseif key=='j'
-        yc = yc+1;
-      elseif key=='J'
-        yc = yc-1;
-      elseif key=='k'
-        zc = zc+1;
-      elseif key=='K'
-        zc = zc-1;
-      else
+      case 1 % left mouse click
         % update the view to a new position
+        l1 = get(get(gca, 'xlabel'), 'string');
+        l2 = get(get(gca, 'ylabel'), 'string');
+        switch l1,
+          case 'i'
+            xc = d1;
+          case 'j'
+            yc = d1;
+          case 'k'
+            zc = d1;
+          otherwise
+            continue;
+        end
+        switch l2,
+          case 'i'
+            xc = d2;
+          case 'j'
+            yc = d2;
+          case 'k'
+            zc = d2;
+          otherwise
+            continue;
+        end
+        if l1=='i' && l2=='j'
+          updatepanel = [1 2 3];
+        elseif l1=='i' && l2=='k'
+          updatepanel = [2 3 1];
+        elseif l1=='j' && l2=='k'
+          updatepanel = [3 1 2];
+        end
+      case 3 % right mouse click  
+        % add point to a list
         l1 = get(get(gca, 'xlabel'), 'string');
         l2 = get(get(gca, 'ylabel'), 'string');
         switch l1,
@@ -238,38 +270,139 @@ switch cfg.method
           case 'k'
             zc = d2;
         end
+        pnt = [pnt; xc yc zc];
+        if l1=='i' && l2=='j'
+          updatepanel = [1 2 3];
+        elseif l1=='i' && l2=='k'
+          updatepanel = [2 3 1];
+        elseif l1=='j' && l2=='k'
+          updatepanel = [3 1 2];
+        end
+      case 2 % middle mouse click
+        l1 = get(get(gca, 'xlabel'), 'string');
+        l2 = get(get(gca, 'ylabel'), 'string');
+        
+        % remove the previous point
+        if size(pnt,1)>0
+          pnt(end,:) = [];
+        end
+        
+        if l1=='i' && l2=='j'
+          updatepanel = [1 2 3];
+        elseif l1=='i' && l2=='k'
+          updatepanel = [2 3 1];
+        elseif l1=='j' && l2=='k'
+          updatepanel = [3 1 2];
+        end
+      case 28 % arrow left
+        % update the coordinates
+        l1 = get(get(gca, 'xlabel'), 'string');
+        l2 = get(get(gca, 'ylabel'), 'string');
+        if l1=='i' && l2=='j'
+          xc = xc-1; updatepanel = [1 2 3];
+        elseif l1=='i' && l2=='k'
+          xc = xc-1; updatepanel = [2 3 1];
+        elseif l1=='j' && l2=='k'
+          yc = yc-1; updatepanel = [3 1 2];
+        end
+      case 30 % arrow up
+        % update the coordinates
+        l1 = get(get(gca, 'xlabel'), 'string');
+        l2 = get(get(gca, 'ylabel'), 'string');
+        if l1=='i' && l2=='j'
+          yc = yc+1; updatepanel = [1 2 3];
+        elseif l1=='i' && l2=='k'
+          zc = zc+1; updatepanel = [2 3 1];
+        elseif l1=='j' && l2=='k'
+          zc = zc+1; updatepanel = [3 1 2];
+        end
+      case 29 % arrow right
+        % update the coordinates
+        l1 = get(get(gca, 'xlabel'), 'string');
+        l2 = get(get(gca, 'ylabel'), 'string');
+        if l1=='i' && l2=='j'
+          xc = xc+1; updatepanel = [1 2 3];
+        elseif l1=='i' && l2=='k'
+          xc = xc+1; updatepanel = [2 3 1];
+        elseif l1=='j' && l2=='k'
+          yc = yc+1; updatepanel = [3 1 2];
+        end
+      case 31 % arrow down
+        % update the coordinates
+        l1 = get(get(gca, 'xlabel'), 'string');
+        l2 = get(get(gca, 'ylabel'), 'string');
+        if l1=='i' && l2=='j'
+          yc = yc-1; updatepanel = [1 2 3];
+        elseif l1=='i' && l2=='k'
+          zc = zc-1; updatepanel = [2 3 1];
+        elseif l1=='j' && l2=='k'
+          zc = zc-1; updatepanel = [3 1 2];
+        end
+      otherwise
+        % do nothing
       end
-      
       fprintf('============================================================\n');
       
-      str = sprintf('voxel %d, indices [%d %d %d]', sub2ind(mri.dim(1:3), round(xc), round(yc), round(zc)), round([xc yc zc]));
-      
-      if isfield(mri, 'coordsys') && isfield(mri, 'unit')
-        str = sprintf('%s, %s coordinates [%.1f %.1f %.1f] %s', str, mri.coordsys, warp_apply(mri.transform, [xc yc zc]), mri.unit);
-      elseif ~isfield(mri, 'coordsys') && isfield(mri, 'unit')
-        str = sprintf('%s, location [%.1f %.1f %.1f] %s', str, warp_apply(mri.transform, [xc yc zc]), mri.unit);
-      elseif isfield(mri, 'coordsys') && ~isfield(mri, 'unit')
-        str = sprintf('%s, %s coordinates [%.1f %.1f %.1f]', str, mri.coordsys, warp_apply(mri.transform, [xc yc zc]));
-      elseif ~isfield(mri, 'coordsys') && ~isfield(mri, 'unis')
-        str = sprintf('%s, location [%.1f %.1f %.1f]', str, warp_apply(mri.transform, [xc yc zc]));
+      if all(round([xc yc zc])<=mri.dim)
+        str = sprintf('voxel %d, indices [%d %d %d]', sub2ind(mri.dim(1:3), round(xc), round(yc), round(zc)), round([xc yc zc]));
+            
+        if isfield(mri, 'coordsys') && isfield(mri, 'unit')
+          str = sprintf('%s, %s coordinates [%.1f %.1f %.1f] %s', str, mri.coordsys, warp_apply(mri.transform, [xc yc zc]), mri.unit);
+        elseif ~isfield(mri, 'coordsys') && isfield(mri, 'unit')
+          str = sprintf('%s, location [%.1f %.1f %.1f] %s', str, warp_apply(mri.transform, [xc yc zc]), mri.unit);
+        elseif isfield(mri, 'coordsys') && ~isfield(mri, 'unit')
+          str = sprintf('%s, %s coordinates [%.1f %.1f %.1f]', str, mri.coordsys, warp_apply(mri.transform, [xc yc zc]));
+        elseif ~isfield(mri, 'coordsys') && ~isfield(mri, 'unis')
+          str = sprintf('%s, location [%.1f %.1f %.1f]', str, warp_apply(mri.transform, [xc yc zc]));
+        end
+        fprintf('%s\n', str);
+        % fprintf('cur_voxel = [%f %f %f], cur_head = [%f %f %f]\n', [xc yc zc], warp_apply(mri.transform, [xc yc zc]));
       end
-      fprintf('%s\n', str);
-      % fprintf('cur_voxel = [%f %f %f], cur_head = [%f %f %f]\n', [xc yc zc], warp_apply(mri.transform, [xc yc zc]));
-
+      
+      markerpos   = zeros(0,3);
+      markerlabel = {};
+      markercolor = {};
       if ~isempty(nas),
         fprintf('nas_voxel = [%f %f %f], nas_head = [%f %f %f]\n', nas, warp_apply(mri.transform, nas));
-      else
-        fprintf('nas = undefined\n');
+        markerpos   = [markerpos; nas];
+        markerlabel = [markerlabel; {'nas'}];
+        markercolor = [markercolor; {'b'}];
       end
       if ~isempty(lpa),
         fprintf('lpa_voxel = [%f %f %f], lpa_head = [%f %f %f]\n', lpa, warp_apply(mri.transform, lpa));
-      else
-        fprintf('lpa = undefined\n');
+        markerpos   = [markerpos; lpa];
+        markerlabel = [markerlabel; {'lpa'}];
+        markercolor = [markercolor; {'g'}];
       end
       if ~isempty(rpa),
         fprintf('rpa_voxel = [%f %f %f], rpa_head = [%f %f %f]\n', rpa, warp_apply(mri.transform, rpa));
-      else
-        fprintf('rpa = undefined\n');
+        markerpos   = [markerpos; rpa];
+        markerlabel = [markerlabel; {'rpa'}];
+        markercolor = [markercolor; {'r'}];
+      end
+      if ~isempty(antcomm),
+        fprintf('antcomm_voxel = [%f %f %f], antcomm_head = [%f %f %f]\n', antcomm, warp_apply(mri.transform, antcomm));
+        markerpos   = [markerpos; antcomm];
+        markerlabel = [markerlabel; {'antcomm'}];
+        markercolor = [markercolor; {'b'}];
+      end
+      if ~isempty(pstcomm),
+        fprintf('pstcomm_voxel = [%f %f %f], pstcomm_head = [%f %f %f]\n', pstcomm, warp_apply(mri.transform, pstcomm));
+        markerpos   = [markerpos; pstcomm];
+        markerlabel = [markerlabel; {'pstcomm'}];
+        markercolor = [markercolor; {'g'}];
+      end
+      if ~isempty(xzpoint),
+        fprintf('xzpoint_voxel = [%f %f %f], xzpoint_head = [%f %f %f]\n', xzpoint, warp_apply(mri.transform, xzpoint));
+        markerpos   = [markerpos; xzpoint];
+        markerlabel = [markerlabel; {'xzpoint'}];
+        markercolor = [markercolor; {'r'}];
+      end
+      if ~isempty(pnt)
+        fprintf('%f extra points selected\n', size(pnt,1));
+        markerpos   = [markerpos; pnt];
+        markerlabel = [markerlabel; repmat({''}, size(pnt,1), 1)];
+        markercolor = [markercolor; repmat({'m'}, size(pnt,1), 1)];
       end
     end % while true
     
@@ -332,6 +465,10 @@ else
   warning('no coordinate system reallignment has been done');
 end
 
+if exist('pnt', 'var')
+  mri.pnt = pnt;
+end
+
 % accessing this field here is needed for the configuration tracking
 % by accessing it once, it will not be removed from the output cfg
 cfg.outputfile;
@@ -344,7 +481,7 @@ cfg.version.name = mfilename('fullpath');
 cfg.version.id = '$Id$';
 
 % add information about the Matlab version used to the configuration
-cfg.callinfo.matlab = version();
+cfg.version.matlab = version();
   
 % add information about the function call to the configuration
 cfg.callinfo.proctime = toc(ftFuncTimer);
@@ -362,7 +499,8 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % helper function to show three orthogonal slices
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function volplot(x, y, z, dat, c, cscale, showcrosshair)
+function [h1, h2, h3] = volplot(x, y, z, dat, c, cscale, showcrosshair, updatepanel, handles, showmarkers, markers)
+
 xi = c(1);
 yi = c(2);
 zi = c(3);
@@ -376,35 +514,87 @@ else
   cmax = cscale(2);
 end
 
-h1 = subplot(2,2,1);
-h2 = subplot(2,2,2);
-h3 = subplot(2,2,3);
-
-subplot(h1);
-imagesc(x, z, squeeze(dat(:,yi,:))'); set(gca, 'ydir', 'normal')
-axis equal; axis tight;
-xlabel('i'); ylabel('k');
-caxis([cmin cmax]);
-if showcrosshair
-  crosshair([x(xi) z(zi)], 'color', 'yellow');
+if nargin<8
+  updatepanel = [1 2 3];
 end
 
-subplot(h2);
-imagesc(y, z, squeeze(dat(xi,:,:))'); set(gca, 'ydir', 'normal')
-axis equal; axis tight;
-xlabel('j'); ylabel('k');
-caxis([cmin cmax]);
-if showcrosshair
-  crosshair([y(yi) z(zi)], 'color', 'yellow');
+if nargin<9
+  h1 = [];
+  h2 = [];
+  h3 = [];
+else
+  h1 = handles{1};
+  h2 = handles{2};
+  h3 = handles{3};
 end
 
-subplot(h3);
-imagesc(x, y, squeeze(dat(:,:,zi))'); set(gca, 'ydir', 'normal')
-axis equal; axis tight;
-xlabel('i'); ylabel('j');
-caxis([cmin cmax]);
-if showcrosshair
-  crosshair([x(xi) y(yi)], 'color', 'yellow');
+if showmarkers
+  markerpos = round(markers{1});
+  markercolor = markers{3};
+  sel1 = find(markerpos(:,2)==repmat(c(2),size(markerpos,1),1));
+  sel2 = find(markerpos(:,1)==repmat(c(1),size(markerpos,1),1));
+  sel3 = find(markerpos(:,3)==repmat(c(3),size(markerpos,1),1));
+end
+
+for k = 1:numel(updatepanel)
+
+  update = updatepanel(k);
+  if update==1
+    subplot(h1);
+    imagesc(x, z, squeeze(dat(:,yi,:))'); set(gca, 'ydir', 'normal')
+    axis equal; axis tight;
+    xlabel('i'); ylabel('k');
+    caxis([cmin cmax]);
+    if showcrosshair
+      crosshair([x(xi) z(zi)], 'color', 'yellow');
+    end
+    if showmarkers && numel(sel1)>0
+      hold on;
+      for kk = 1:numel(sel1)
+        plot(markerpos(sel1(kk),1), markerpos(sel1(kk),3), 'marker', '.', 'color', markercolor{sel1(kk)});
+      end
+      hold off;
+    end
+  end
+  
+  if update==2
+    subplot(h2);
+    imagesc(y, z, squeeze(dat(xi,:,:))'); set(gca, 'ydir', 'normal')
+    axis equal; axis tight;
+    xlabel('j'); ylabel('k');
+    caxis([cmin cmax]);
+    if showcrosshair
+      crosshair([y(yi) z(zi)], 'color', 'yellow');
+    end
+    if showmarkers && numel(sel2)>0
+      hold on;
+      for kk = 1:numel(sel2)
+        plot(markerpos(sel2(kk),2), markerpos(sel2(kk),3), 'marker', '.', 'color', markercolor{sel2(kk)});
+      end
+      hold off;
+    end  
+  end
+  
+  if update==3
+    subplot(h3); 
+    imagesc(x, y, squeeze(dat(:,:,zi))'); set(gca, 'ydir', 'normal')
+    axis equal; axis tight;
+    xlabel('i'); ylabel('j');
+    caxis([cmin cmax]);
+    if showcrosshair
+      crosshair([x(xi) y(yi)], 'color', 'yellow');
+    end
+    if showmarkers && numel(sel3)>0
+      hold on;
+      for kk = 1:numel(sel3)
+        plot(markerpos(sel3(kk),1), markerpos(sel3(kk),2), 'marker', '.', 'color', markercolor{sel3(kk)});
+      end
+      hold off;
+    end
+  end
+
 end
 
 colormap gray
+
+h = gca;
