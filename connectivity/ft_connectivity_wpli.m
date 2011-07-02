be96ce46b784cd10b28f37b9284f4b9bba5df223
Martin Vinck <martinvinck@gmail.com>
2011-07-02 02:58:11 +0000

    put in support for nans in combination with jackkniving
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3756 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- connectivity/ft_connectivity_wpli.m
+++ connectivity/ft_connectivity_wpli.m
@@ -46,11 +46,11 @@ function [wpli, v, n] = ft_connectivity_wpli(input, varargin)
 %    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
 %
 % $Id$
-
+disp('there')
 feedback    = keyval('feedback', varargin); if isempty(feedback), feedback = 'none'; end
 debias      = keyval('debias',   varargin);
 dojack      = keyval('dojack',   varargin);
-
+keyboard
 siz = size(input);
 n = siz(1);
 ft_progress('init', feedback, 'computing metric...');
@@ -70,7 +70,7 @@ else
   warning('ft_connectivity_wpli:nTrials', 'computation wpli requires >1 trial, returning NaNs');
 end
 
-[leave1outsum, leave1outssq] = deal(0);
+[leave1outsum, leave1outssq] = deal(zeros(size(outsum)));
 if dojack && n>2 % n needs to be larger than 2 to get a meaningful variance
   for k = 1:n
     s  = outsum  - input(k,:,:,:,:,:,:); % works for any array up to 7-D
@@ -83,15 +83,18 @@ if dojack && n>2 % n needs to be larger than 2 to get a meaningful variance
       num   = s; % this is estimator of E(Im(X))
       denom = sw; % estimator of E(|Im(X)|)
     end        
-    leave1outsum = leave1outsum + num./denom;
-    leave1outssq = leave1outssq + (num./denom).^2;                              
+    leave1outsum = nansum([leave1outsum;num./denom]);
+    leave1outssq = nansum([leave1outssq;(num./denom).^2]);                              
   end  
   % compute the sem here 
-  v = (n-1).^2*(leave1outssq - (leave1outsum.^2)./n)./(n - 1); % 11.5 efron, sqrt and 1/n done in ft_connectivityanalysis
+  n = nansum(~isnan(input));
+  v = (n-1).^2.*(leave1outssq - (leave1outsum.^2)./n)./(n - 1); % 11.5 efron, sqrt and 1/n done in ft_connectivityanalysis
   v = reshape(v,siz(2:end)); % remove the first singular dimension   
+  n = reshape(n,siz(2:end));  
 elseif dojack && n<=2
   v = NaN(siz(2:end));
 else
   v = [];
 end
 ft_progress('close');
+n = reshape(n,siz(2:end));
