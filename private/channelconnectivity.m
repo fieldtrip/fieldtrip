b9b372337924ca66567a8a521d8f88dc3987f3d5
Jorn Horschig <jm.horschig@donders.ru.nl>
2011-07-09 13:04:36 +0000

    bugfix - forgot to upload new channelconnectivity, needed for channelrepair
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3811 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/channelconnectivity.m
+++ private/channelconnectivity.m
@@ -1,25 +1,42 @@
-function [connectivity] = channelconnectivity(cfg)
+function [connectivity] = channelconnectivity(cfg, data)
 
 % CHANNELCONNECTIVIY creates a NxN matrix that describes whether channels 
 % are connected as neighbours
 %
 % See also FT_NEIGHBOURSELECTION
 
-if strcmp(cfg.avgoverchan, 'yes')
-  nchan = 1;
-  connectivity = false(nchan,nchan);
+if isfield(cfg, 'avgoverchan') && strcmp(cfg.avgoverchan, 'yes')
+    nchan = 1;
+    connectivity = false(nchan,nchan);
 else
-  nchan = length(cfg.channel);
-  connectivity = false(nchan,nchan);
-  for chan=1:length(cfg.neighbours)
-    [seld] = match_str(cfg.channel, cfg.neighbours{chan}.label);
-    [seln] = match_str(cfg.channel, cfg.neighbours{chan}.neighblabel);
-    if isempty(seld)
-      % this channel was not present in the data
-      continue;
+    if nargin < 2
+        nchan = length(cfg.channel);
+        connectivity = false(nchan,nchan);
+        for chan=1:length(cfg.neighbours)
+            [seld] = match_str(cfg.channel, cfg.neighbours{chan}.label);
+            [seln] = match_str(cfg.channel, cfg.neighbours{chan}.neighblabel);
+            if isempty(seld)
+                % this channel was not present in the data
+                continue;
+            else
+                % add the neighbours of this channel to the matrix
+                connectivity(seld, seln) = true;
+            end
+        end
     else
-      % add the neighbours of this channel to the matrix
-      connectivity(seld, seln) = true;
+        
+        nchan = length(data.label);
+        connectivity = false(nchan,nchan);
+        for chan=1:length(cfg.neighbours)
+            [seld] = match_str(data.label, cfg.neighbours{chan}.label);
+            [seln] = match_str(data.label, cfg.neighbours{chan}.neighblabel);
+            if isempty(seld)
+                % this channel was not present in the data
+                continue;
+            else
+                % add the neighbours of this channel to the matrix
+                connectivity(seld, seln) = true;
+            end
+        end
     end
-  end
 end
