47f1703495f6fc10ff91724129db98981f2f25d1
Robert Oostenveld <r.oostenveld@gmail.com>
2011-07-14 09:13:48 +0000

    enhancement - added a test script for ft_getopt
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3859 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_ft_getopt.m
@@ -0,0 +1,37 @@
+% TEST: ft_getopt
+
+success = true;
+
+success = success && isequal(ft_getopt({'key1', 'val1'}, 'key1'), 'val1')
+success = success && isequal(ft_getopt({'key1', 'val1'}, 'key2'), [])
+success = success && isequal(ft_getopt({'key1', 'val1'}, 'key2', 'default'), 'default')
+success = success && isequal(ft_getopt({'key1', 'val1'}, 'key2', 'default'), 'default')
+clear cfg
+cfg.key1 = 'val1';
+success = success && isequal(ft_getopt(cfg, 'key1'), 'val1')
+success = success && isequal(ft_getopt(cfg, 'key2'), [])
+success = success && isequal(ft_getopt(cfg, 'key2', 'default'), 'default')
+success = success && isequal(ft_getopt(cfg, 'key2', 'default'), 'default')
+
+success = success && isequal(ft_getopt({'key1', []}, 'key1'), [])
+success = success && isequal(ft_getopt({'key1', []}, 'key1', 'default'), 'default')
+clear cfg
+cfg.key1 = [];
+success = success && isequal(ft_getopt(cfg, 'key1'), [])
+success = success && isequal(ft_getopt(cfg, 'key1', 'default'), 'default')
+
+success = success && isequal(ft_getopt({'key1', 'val1', 'key2', 'val2', 'key3', 'val3'}, 'key3'), 'val3')
+success = success && isequal(ft_getopt({'key1', 'val1', 'key2', 'val2', 'key3', 'val3'}, 'key4'), [])
+success = success && isequal(ft_getopt({'key1', 'val1', 'key2', 'val2', 'key3', 'val3'}, 'key4', 'default'), 'default')
+clear cfg
+cfg.key1 = 'val1';
+cfg.key2 = 'val2';
+cfg.key3 = 'val3';
+success = success && isequal(ft_getopt(cfg, 'key3'), 'val3')
+success = success && isequal(ft_getopt(cfg, 'key4'), [])
+success = success && isequal(ft_getopt(cfg, 'key4', 'default'), 'default')
+
+if ~success
+  error('there is a problem with ft_getopt');
+end
+
