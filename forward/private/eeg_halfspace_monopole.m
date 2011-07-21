c449f81cd71effc646243ce01d7e5dc0df9ddd51
Cristiano Micheli <michelic72@googlemail.com>
2011-07-21 15:34:46 +0000

    enhancement - added one specific private function to the code and corrected typos
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3891 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/private/eeg_halfspace_monopole.m
+++ forward/private/eeg_halfspace_monopole.m
@@ -1,6 +1,6 @@
 function [lf] = eeg_halfspace_monopole(rd, elc, vol)
 
-% HALFSPACE_MEDIUM_LEADFIELD calculate the halfspace medium leadfield
+% EEG_HALFSPACE_MONOPOLE calculate the halfspace medium leadfield
 % on positions pnt for a monopole at position rd and conductivity cond
 % The halfspace solution requires a plane dividing a conductive zone of
 % conductivity cond, from a non coductive zone (cond = 0)
@@ -51,14 +51,14 @@ for i=1:Npoles
   % this is the position of dipole "i"
   pole1 = rd((1:3) + 3*(i-1));
   
-  % distances electrodes - dipole
+  % distances electrodes - corrent poles
   r1 = elc - ones(Nelc,1) * pole1;
   
-  % Method of mirror dipoles:
-  % Defines the position of mirror dipoles being symmetric to the plane
+  % Method of mirror charges:
+  % Defines the position of mirror charge being symmetric to the plane
   pole2 = get_mirror_pos(pole1,vol);
   
-  % distances electrodes - mirror dipole
+  % distances electrodes - mirror charge
   r2 = elc - ones(Nelc,1) * pole2;
   
   % denominator
@@ -66,16 +66,47 @@ for i=1:Npoles
   % denominator, mirror term
   R2 = -(4*pi*vol.cond) * (sum(r2' .^2 ) )';
   
-  % condition of dipoles falling in the non conductive halfspace    
+  % condition of poles falling in the non conductive halfspace    
   invacuum = acos(dot(vol.ori,(pole1-vol.pnt)./norm(pole1-vol.pnt))) < pi/2;
   
   if invacuum
-    warning('dipole lies on the vacuum side of the plane');
+    warning('a pole lies on the vacuum side of the plane');
     lf(:,i) = NaN(Nelc,1);
   elseif any(R1)==0
-    warning('dipole coincides with one of the electrodes');
+    warning('a pole coincides with one of the electrodes');
     lf(:,i) = NaN(Nelc,1);
   else
     lf(:,i) = (1 ./ R1) + (1 ./ R2);
   end
+end
+
+function P2 = get_mirror_pos(P1,vol)
+% calculates the position of a point symmetric to pnt with respect to a plane
+
+P2 = [];
+
+% define the plane
+pnt = vol.pnt;
+ori = vol.ori; % already normalized
+
+if abs(dot(P1-pnt,ori))<eps
+  warning(sprintf ('point %f %f %f lies in the symmetry plane',P1(1),P1(2),P1(3)))
+  P2 = P1;
+else
+  % define the plane in parametric form
+  % define a non colinear vector vc with respect to the plane normal
+  vc = [1 0 0];    
+  if abs(cross(ori, vc, 2))<eps
+      vc = [0 1 0];
+  end
+  % define plane's direction vectors
+  v1 = cross(ori, vc, 2);  v1 = v1/norm(v1);
+  v2 = cross(pnt, ori, 2); v2 = v2/norm(v2);
+  plane = [pnt v1 v2];
+ 
+  % distance plane-point P1
+  d = -dot(ori, plane(:,1:3)-P1(:,1:3), 2);
+
+  % symmetric point
+  P2 = P1 - 2*d*ori;
 end
