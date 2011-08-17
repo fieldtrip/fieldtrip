533e8131967fb9d081685a3d4bca12282b88f3f9
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-08-17 13:50:19 +0000

    enhancement - some cosmetics. changed to ft_getopt
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3969 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_freqgrandaverage.m
+++ ft_freqgrandaverage.m
@@ -1,4 +1,4 @@
-function [grandavg] = ft_freqgrandaverage(cfg, varargin);
+function [grandavg] = ft_freqgrandaverage(cfg, varargin)
 
 % FT_FREQGRANDAVERAGE computes the average powerspectrum or time-frequency spectrum
 % over multiple subjects
@@ -7,8 +7,8 @@ function [grandavg] = ft_freqgrandaverage(cfg, varargin);
 %   [grandavg] = ft_freqgrandaverage(cfg, freq1, freq2, freq3...)
 %
 % The input data freq1..N are obtained from either FT_FREQANALYSIS with
-% keeptrials=no or from FT_FREQDESCRIPTIVES. The configuration structure
-% can contain
+% keeptrials=no, from FT_FREQDESCRIPTIVES, or from FT_CONNECTIVITYANALYSIS. 
+% The configuration structure can contain
 %  cfg.keepindividual = 'yes' or 'no' (default = 'no')
 %  cfg.foilim         = [fmin fmax] or 'all', to specify a subset of frequencies (default = 'all')
 %  cfg.toilim         = [tmin tmax] or 'all', to specify a subset of latencies (default = 'all')
@@ -27,9 +27,8 @@ function [grandavg] = ft_freqgrandaverage(cfg, varargin);
 %
 % See also FT_TIMELOCKGRANDAVERAGE, FT_FREQANALYSIS, FT_FREQDESCRIPTIVES
 
-% FIXME averaging coherence is not possible if inputs contain different amounts of data (i.e. chan/freq/time)
-
 % Copyright (C) 2005-2006, Robert Oostenveld
+% Copyright (C) 2006-2011, Robert Oostenveld, Jan-Mathijs Schoffelen
 %
 % This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
 % for the documentation and details.
@@ -51,24 +50,22 @@ function [grandavg] = ft_freqgrandaverage(cfg, varargin);
 
 ft_defaults
 
-% record start time and total processing time
-ftFuncTimer = tic();
-ftFuncClock = clock();
-
 cfg = ft_checkconfig(cfg, 'trackconfig', 'on');
 
 % set the defaults
-if ~isfield(cfg, 'inputfile'),    cfg.inputfile  = [];         end
-if ~isfield(cfg, 'outputfile'),   cfg.outputfile = [];         end
+cfg.inputfile  = ft_getopt(cfg, 'inputfile',  []);
+cfg.outputfile = ft_getopt(cfg, 'outputfile', []);
 
-hasdata = nargin>1;
-if ~isempty(cfg.inputfile) % the input data should be read from file
-  if hasdata
-    error('cfg.inputfile should not be used in conjunction with giving input data to this function');
-  else
-    for i=1:numel(cfg.inputfile)
-      varargin{i} = loadvar(cfg.inputfile{i}, 'freq'); % read datasets from array inputfile
-    end
+hasdata      = nargin>1;
+hasinputfile = ~isempty(cfg.inputfile);
+
+if hasdata && hasinputfile
+  error('cfg.inputfile should not be used in conjunction with giving input data to this function');
+end
+
+if hasinputfile
+  for i=1:numel(cfg.inputfile)
+    varargin{i} = loadvar(cfg.inputfile{i}, 'freq'); % read datasets from array inputfile
   end
 end
 
@@ -78,32 +75,14 @@ for i=1:length(varargin)
 end
 
 % set the defaults
-if ~isfield(cfg, 'keepindividual'), cfg.keepindividual = 'no'; end
-if ~isfield(cfg, 'channel'),        cfg.channel = 'all';       end
-if ~isfield(cfg, 'foilim'),         cfg.foilim = 'all';        end
-if ~isfield(cfg, 'toilim'),         cfg.toilim = 'all';        end
-
-% for backward compatibility with old data structures
-if isfield(varargin{1}, 'sgn') && ~isfield(varargin{1}, 'label')
-  warning('renaming "sng" field into label for backward compatibility');
-  for s=1:Nsubj
-    varargin{s}.label = varargin{s}.sgn;
-  end
-end
-
-% for backward compatibility with old data structures
-if isfield(varargin{1}, 'sgncmb') && ~isfield(varargin{1}, 'labelcmb')
-  warning('renaming "sngcmb" field into labelcmb for backward compatibility');
-  for s=1:Nsubj
-    varargin{s}.labelcmb = varargin{s}.sgncmb;
-  end
-end
+cfg.keepindividual = ft_getopt(cfg, 'keepindividual', 'no');
+cfg.channel        = ft_getopt(cfg, 'channel',        'all');
+cfg.foilim         = ft_getopt(cfg, 'foilim',         'all');
+cfg.toilim         = ft_getopt(cfg, 'toilim',         'all');
+cfg.parameter      = ft_getopt(cfg, 'parameter',      'powspctrm');
 
 Nsubj    = length(varargin);
 dimord   = varargin{1}.dimord;
-haspow   = isfield(varargin{1}, 'powspctrm');             % this should always be true
-hascoh   = isfield(varargin{1}, 'cohspctrm');
-hasplv   = isfield(varargin{1}, 'plvspctrm');
 hasfreq  = ~isempty(strfind(varargin{i}.dimord, 'freq')); % this should always be true
 hastime  = ~isempty(strfind(varargin{i}.dimord, 'time'));
 hasrpt   = ~isempty(strfind(varargin{i}.dimord, 'rpt'));
@@ -111,7 +90,7 @@ hastap   = ~isempty(strfind(varargin{i}.dimord, 'tap'));
 
 % check whether the input data is suitable
 if hasrpt
-  error('the input data of each subject should be an average, use FREQDESCRIPTIVES first');
+  error('the input data of each subject should not contain single observations, use FT_FREQDESCRIPTIVES or FT_CONNECTIVITYANALYSIS first');
 end
 if hastap
   error('multiple tapers in the input are not supported');
@@ -268,17 +247,14 @@ cfg.version.name = mfilename('fullpath');
 cfg.version.id = '$Id$';
 
 % add information about the Matlab version used to the configuration
-cfg.callinfo.matlab = version();
-  
-% add information about the function call to the configuration
-cfg.callinfo.proctime = toc(ftFuncTimer);
-cfg.callinfo.calltime = ftFuncClock;
-cfg.callinfo.user = getusername();
+cfg.version.matlab = version();
 
 % remember the configuration details of the input data
 cfg.previous = [];
 for i=1:length(varargin)
-  try, cfg.previous{i} = varargin{i}.cfg; end
+  if isfield(varargin{i}, 'previous'),
+    cfg.previous{i} = varargin{i}.cfg;
+  end
 end
 
 % remember the exact configuration details in the output
