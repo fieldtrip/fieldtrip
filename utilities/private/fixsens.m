995f9becb5a3f85e8031b5bc9f3d6a2edba7dd3b
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-08-18 06:53:22 +0000

    enhancement - built-in break in while loop when channelselection fails. improved fixsens
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@3975 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/private/fixsens.m
+++ utilities/private/fixsens.m
@@ -8,6 +8,7 @@ function [sens] = fixsens(sens)
 % 		   sens.tra -> balancing matrix from coils to channels
 %		   sens.label -> channel labels
 %		   sens.balance -> additional structure containing info about the balancing
+%          sens.unit
 %
 % New style (MEG): sens.coilpos -> coil positions
 %		   sens.coilori -> coil orientations
@@ -15,28 +16,45 @@ function [sens] = fixsens(sens)
 %		   sens.label -> channel labels
 %		   sens.balance -> additional structure containing info about the balancing
 %		   sens.chanpos -> channel positions
+%          sens.unit
 %
 % Old style (EEG/ECoG): sens.pnt -> electrode positions
 % 		   sens.tra -> balancing matrix from electrodes to channels
 %		   sens.label -> channel labels
+%          sens.unit
 %
 % New style (EEG/ECoG): sens.elecpos -> electrode positions
 % 		   sens.tra -> balancing matrix from electrodes to channels
 %		   sens.label -> channel labels
 %		   sens.chanpos -> channel positions
+%          sens.unit
 %
 % Use as [sens] = fixsens(sens)
 
 if isfield(sens, 'ori')
+  isgrad    = 1;
+  doconvert = 1;
+elseif (isfield(sens, 'coilori') && isfield(sens, 'coilpos') && isfield(sens, 'chanpos'))
+  isgrad    = 1;
+  doconvert = 0;
+elseif (isfield(sens, 'elecpos') && isfield(sens, 'chanpos'))
+  isgrad    = 0;
+  doconvert = 0;
+else
+  isgrad    = 0;
+  doconvert = 1;
+end
+
+if isgrad && doconvert
   % sensor description is a MEG sensor-array, containing oriented coils
   
-  chanpos      = channelposition(sens);
+  chanpos      = channelposition(sens, 'channel', 'all');
   sens.coilori = sens.ori; sens = rmfield(sens, 'ori');
   sens.coilpos = sens.pnt; sens = rmfield(sens, 'pnt');
   sens.chanpos = chanpos;  
-else
+elseif doconvert
   % sensor description is something else, EEG/ECoG etc
   
-  chanpos      = channelposition(sens);
-  sens.elecpos = sens.pnt; sens = rmfield(sens, 'pnt');
+  chanpos      = channelposition(sens, 'channel', 'all');
+  sens.elecpos = chanpos; sens = rmfield(sens, 'pnt');
 end
