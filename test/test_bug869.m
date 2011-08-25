b874a11934ec2c301b76fdf65ee3f2d0ca40a6ad
Robert Oostenveld <r.oostenveld@gmail.com>
2011-08-25 08:17:19 +0000

    enhancement - added test script for bug 869, the data is on home/common
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4012 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug869.m
@@ -0,0 +1,25 @@
+% TEST: ft_multiplotTFR, ft_prepare_layout, ft_datatype, ft_datatype_freq
+
+load /home/common/matlab/fieldtrip/data/test/bug896.mat
+
+ft_checkdata(stat_coh, 'datatype', 'freq');
+ft_checkdata(stat_coh_full, 'datatype', 'freq');
+ft_checkdata(stat_coh, 'datatype', 'freq', 'cmbrepresentation', 'full');
+ft_checkdata(stat_coh_full, 'datatype', 'freq', 'cmbrepresentation', 'full');
+
+if ~ft_datatype(stat_coh, 'freq')
+  error('ft_datatype failed on the "stat_coh" input data');
+end
+
+if ~ft_datatype(stat_coh_full, 'freq')
+  error('ft_datatype failed on the "stat_coh_full" input data');
+end
+
+
+figure
+ft_multiplotTFR(cfg, stat_coh);
+close
+
+figure
+ft_multiplotTFR(cfg, stat_coh_full);
+close
