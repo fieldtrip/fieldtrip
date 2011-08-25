6b62884d8e89948275701d33608284ea05fa2f90
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-08-25 09:47:42 +0000

    enhancement - filled in placeholder file with some code
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4017 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/test/test_ft_read_header.m
+++ fileio/test/test_ft_read_header.m
@@ -1,1 +1,19 @@
 function test_ft_read_header
+
+% TEST: test_ft_read_header ft_read_header
+
+addpath('/home/common/matlab/fieldtrip/data/test/mfiles/');
+datainfo = datasets;
+datainfo = datainfo(1:17); % lfp does not yet work because of the data
+
+for k = 1:numel(datainfo)
+  subject  = datainfo(k);
+  filename =  [subject.origdir,'original/',subject.type,subject.datatype,'/',subject.filename];
+
+  % get header and event information
+  if ~isempty(subject.dataformat)
+    hdr   = ft_read_header(filename, 'headerformat', subject.dataformat);
+  else
+    hdr   = ft_read_header(filename);
+  end
+end
