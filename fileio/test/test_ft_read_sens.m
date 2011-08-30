1899b72c9e915390b652938d953e355cc58461fe
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-08-30 08:23:13 +0000

    enhancement - updated test-script
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4051 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/test/test_ft_read_sens.m
+++ fileio/test/test_ft_read_sens.m
@@ -1,1 +1,19 @@
 function test_ft_read_sens
+
+% TEST: test_ft_read_sens ft_read_sens
+
+addpath('/home/common/matlab/fieldtrip/data/test/mfiles/');
+datainfo = datasets;
+use      = match_str({datainfo.datatype},{'bti148' 'bti248' 'bti248grad' 'ctf151' 'ctf275' 'itab153' 'neuromag122' 'neuromag306'});
+datainfo = datainfo(use);
+for k = 1:numel(datainfo)
+  subject  = datainfo(k);
+  filename =  [subject.origdir,'original/',subject.type,subject.datatype,'/',subject.filename];
+
+  % get sensor information
+  if ~isempty(subject.dataformat)
+    sens = ft_read_sens(filename, 'headerformat', subject.dataformat);
+  else
+    sens = ft_read_sens(filename);
+  end
+end
