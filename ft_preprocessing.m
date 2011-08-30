3426a305042934006f6afbe1e6a7c82b32db51b8
Robert Oostenveld <r.oostenveld@gmail.com>
2011-08-30 20:52:51 +0000

    enhancement - added memtic/memtoc to the begin and end of the function, store the emmory estimate along with the time estimate in the output cfg
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4069 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_preprocessing.m
+++ ft_preprocessing.m
@@ -183,6 +183,7 @@ ft_defaults
 % record start time and total processing time
 ftFuncTimer = tic();
 ftFuncClock = clock();
+ftFuncMem   = memtic();
 
 if nargin==0
   help(mfilename);
@@ -577,8 +578,9 @@ cfg.callinfo.matlab = version();
   
 % add information about the function call to the configuration
 cfg.callinfo.proctime = toc(ftFuncTimer);
+cfg.callinfo.procmem  = memtoc(ftFuncMem);
 cfg.callinfo.calltime = ftFuncClock;
-cfg.callinfo.user = getusername();
+cfg.callinfo.user     = getusername();
 
 if hasdata && isfield(data, 'cfg')
   % remember the configuration details of the input data
