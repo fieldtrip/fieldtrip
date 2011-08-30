3fb7c388c498223686d4bb2df4e2fcbcd10e8685
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-08-30 12:57:38 +0000

    enhancement - added test script
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4058 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_ft_timelockanalysis.m
@@ -0,0 +1,31 @@
+function test_ft_timelockanalysis(datainfo, writeflag)
+
+% TEST: test_ft_timelockanalysis ft_timelockanalysis
+
+% the optional writeflag determines whether the output should be saved to
+% disk
+
+if nargin<2
+  writeflag = 0;
+end
+if nargin<1
+  datainfo = test_datasets;
+end
+for k = 1:numel(datainfo)
+  datanew = timelockanalysis10trials(datainfo(k), writeflag);
+
+  fname = [datainfo(k).origdir,'timelock/',datainfo(k).type,'timelock_',datainfo(k).datatype];
+  load(fname);
+  datanew = rmfield(datanew, 'cfg'); % these are per construction different if writeflag = 0;
+  data    = rmfield(data,    'cfg');
+  assert(isequal(data, datanew));
+end
+
+function [tlck] = timelockanalysis10trials(dataset, writeflag)
+
+cfg        = [];
+cfg.inputfile  = [dataset.origdir,'raw/',dataset.type,'preproc_',dataset.datatype];
+if writeflag
+  cfg.outputfile = [dataset.origdir,'timelock/',dataset.type,'timelock_',dataset.datatype];
+end
+tlck = ft_timelockanalysis(cfg);
