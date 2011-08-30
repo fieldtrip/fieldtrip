7016d5ac6d674a294ec34c1d864894a6ecd89d43
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-08-30 08:57:36 +0000

    enhancement - added some code
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4053 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/test/test_ft_average_sens.m
+++ forward/test/test_ft_average_sens.m
@@ -1,1 +1,41 @@
 function test_ft_average_sens
+
+% TEST: test_ft_average_sens ft_average_sens ft_transform_sens
+% ft_transform_geometry
+
+% this function so far only tests whether it runs through for a set of eeg
+% or meg sensor arrays
+
+% create a set of dummy sensor arrays
+sens1 = [];
+sens1.pnt   = randn(10,3);
+sens1.label = {'1' '2' '3' '4' '5' '6' '7' '8' '9' '10'}';
+
+sens2 = [];
+sens2.pnt   = randn(10,3);
+sens2.label = {'1' '2' '3' '4' '5' '6' '7' '8' '9' '10'}';
+
+sens(1) = sens1;
+sens(2) = sens2;
+
+eeg = ft_average_sens(sens);
+
+% also do this for meg
+clear sens;
+
+sens1 = [];
+sens1.pnt   = randn(10,3);
+sens1.ori   = randn(10,3);
+sens1.tra   = eye(10);
+sens1.label = {'1' '2' '3' '4' '5' '6' '7' '8' '9' '10'}';
+
+sens2 = [];
+sens2.pnt   = randn(10,3);
+sens2.ori   = randn(10,3);
+sens2.tra   = eye(10);
+sens2.label = {'1' '2' '3' '4' '5' '6' '7' '8' '9' '10'}';
+
+sens(1) = sens1;
+sens(2) = sens2;
+
+meg = ft_average_sens(sens);
