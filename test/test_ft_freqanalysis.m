30551bf6ee143d1d6f1da41955f78c0e3cb45338
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-08-30 13:16:04 +0000

    enhancement - added test-file
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4060 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_ft_freqanalysis.m
@@ -0,0 +1,63 @@
+function test_ft_freqanalysis(datainfo, writeflag)
+
+% TEST: test_ft_preprocessing ft_preprocessing test_datasets
+
+% the optional writeflag determines whether the output 
+% should be saved to disk
+
+if nargin<2
+  writeflag = 0;
+end
+if nargin<1
+  datainfo = test_datasets;
+end
+for k = 1:numel(datainfo)
+  datanew = freqanalysisMtmfft(datainfo(k), writeflag);
+
+  fname = [datainfo(k).origdir,'freq/',datainfo(k).type,'freq_mtmfft_',datainfo(k).datatype];
+  load(fname);
+  datanew = rmfield(datanew, 'cfg'); % these are per construction different if writeflag = 0;
+  freq    = rmfield(freq,    'cfg');
+  assert(isequal(freq, datanew));
+end
+
+for k = 1:numel(datainfo)
+  datanew = freqanalysisMtmconvol(datainfo(k), writeflag);
+
+  fname = [datainfo(k).origdir,'freq/',datainfo(k).type,'freq_mtmconvol_',datainfo(k).datatype];
+  load(fname);
+  datanew = rmfield(datanew, 'cfg'); % these are per construction different if writeflag = 0;
+  freq    = rmfield(freq,    'cfg');
+  assert(isequalwithequalnans(freq, datanew));
+end
+
+%----------------------
+% subfunctions
+%----------------------
+function [freq] = freqanalysisMtmconvol(dataset, writeflag)
+
+cfg        = [];
+cfg.method = 'mtmconvol';
+cfg.output = 'pow';
+cfg.foi    = 2:2:30;
+cfg.taper  = 'hanning';
+cfg.t_ftimwin = ones(1,numel(cfg.foi)).*0.5;
+cfg.toi    = (250:50:750)./1000;
+cfg.inputfile  = [dataset.origdir,'raw/',dataset.type,'preproc_',dataset.datatype];
+if writeflag,
+  cfg.outputfile = [dataset.origdir,'freq/',dataset.type,'freq_mtmconvol_',dataset.datatype];
+end
+freq = ft_freqanalysis(cfg);
+
+function [freq] = freqanalysisMtmfft(dataset, writeflag)
+
+cfg        = [];
+cfg.method = 'mtmfft';
+cfg.output = 'pow';
+cfg.foilim = [0 100];
+cfg.taper  = 'hanning';
+cfg.inputfile  = [dataset.origdir,'raw/',dataset.type,'preproc_',dataset.datatype];
+if writeflag,
+  cfg.outputfile = [dataset.origdir,'freq/',dataset.type,'freq_mtmfft_',dataset.datatype];
+end
+freq = ft_freqanalysis(cfg);
