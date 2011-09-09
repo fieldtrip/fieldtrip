c14b233cc1a800490c6abb9a18cb5aca72b861e8
Robert Oostenveld <r.oostenveld@gmail.com>
2011-09-09 09:34:16 +0000

    enhancement - some small changes to the code to speed up qsub and some cosmetic changes
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4136 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- qsub/private/generatejobid.m
+++ qsub/private/generatejobid.m
@@ -1,20 +1,21 @@
 function id = generatejobid(batch)
 
 % GENERATEJOBID generates a unique identifier for a job to be submitted to
-% the batch queueing system. It consists of:
+% the batch queueing system.
 %
+% Use as
+%   id = generatejobid(batch)
+% where the batch number should be supplied. The result is a string like
 %   user_host_pN_bM_jL
 %
-% where N,M,L are the calling matlab's process ID, batch number (to be provided
-% as an argument) and the sequential job number (per batch), respectively.
+% where N,M,L are the calling matlab's process ID, batch number (to be
+% provided as an argument) and the sequential job number (per batch),
+% respectively.
 %
-% generatejobid() without any arguments generates just the first part of a
-% possible job ID, so user_host_pN.
+% If you call this function without any arguments, it  generates just the
+% first part of a possible job ID, so user_host_pN.
 
 persistent jobNum; % jobnum will be numQueues X 1 vector
-
-user = getusername();
-host = gethostname();
 
 if (nargin>0 && batch > 0)
   
@@ -27,10 +28,10 @@ if (nargin>0 && batch > 0)
     jobNum(batch) = jobNum(batch)+1;
   end
 
-  id = sprintf('%s_%s_p%d_b%d_j%03d', user, host, getpid(), batch, jobNum(batch));
+  id = sprintf('%s_%s_p%d_b%d_j%03d', getusername(), gethostname(), getpid(), batch, jobNum(batch));
   
 else
-  % batch<0 (or batch=NaN) is used to generate not a real job ID, but just
-  % the first part of it (see qsublisten)
-  id = [user '_' host '_p' num2str(getpid())];
+  % batch<0 (or batch=NaN) is used to generate not a real job ID, but just the first part of it (see qsublisten)
+  id = sprintf('%s_%s_p%d', getusername(), gethostname(), getpid());
 end
+
