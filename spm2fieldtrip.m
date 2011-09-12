2861d560e393e8648001dc75cbf0388c519d840a
Robert Oostenveld <r.oostenveld@gmail.com>
2011-09-12 08:15:31 +0000

    enhancement - renamed spm2ft to spm2fieldtrip for consistency with other xxx2fieldtrip functions, see also http://bugzilla.fcdonders.nl/show_bug.cgi?id=929
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4150 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- spm2fieldtrip.m
+++ spm2fieldtrip.m
@@ -1,9 +1,9 @@
-function [ftdata] = spm2ft(D)
+function [data] = spm2ft(D)
 
-% SPM2FT converts an SPM8 meeg object into a FieldTrip raw data structure
+% SPM2FIELDTRIP converts an SPM8 meeg object into a FieldTrip raw data structure
 %
 % Use as
-%   data = spm2ft(D)
+%   data = spm2fieldtrip(D)
 % where D is the SPM8 meeg object which you can load in with SPM_EEG_LOAD
 % and where data is a FieldTrip raw data structure as if it were returned
 % by FT_PREPROCESSING.
@@ -29,33 +29,33 @@ end
 spmdata = D.ftraw;
 
 % convert it into a normal MATLAB structure, i.e. get rid of all the SPM8 objects
-ftdata = [];
-ftdata.fsample = spmdata.fsample;
-ftdata.label   = spmdata.label(:);
-ftdata.time    = spmdata.time;
+data = [];
+data.fsample = spmdata.fsample;
+data.label   = spmdata.label(:);
+data.time    = spmdata.time;
 
 ntrials = numel(spmdata.trial);
 for i=1:ntrials
   % this converts it from a file_array (on disk) into a normal array (in memory)
-  ftdata.trial{i} = spmdata.trial{i}(:,:);
+  data.trial{i} = spmdata.trial{i}(:,:);
 end
 
 clist      = D.condlist;
 conditions = D.conditions;
 
-ftdata.trialinfo = zeros(ntrials,1);
+data.trialinfo = zeros(ntrials,1);
 for k = 1:numel(clist)
   fprintf('mapping condition label "%s" to condition code %d\n', clist{k}, k);
   sel=strcmp(clist{k}, conditions);
-  ftdata.trialinfo(sel) = k;
+  data.trialinfo(sel) = k;
 end
 
 % FIXME the following is not correct
 %
-% ftdata.sampleinfo = zeros(ntrials,2);
+% data.sampleinfo = zeros(ntrials,2);
 % for i=1:ntrials
-%   ftdata.sampleinfo(i,1) = D.indsample(i);
-%   ftdata.sampleinfo(i,2) = D.indsample(i) + D.nsamples;
+%   data.sampleinfo(i,1) = D.indsample(i);
+%   data.sampleinfo(i,2) = D.indsample(i) + D.nsamples;
 % end
 
 % add the version details of this function call to the configuration
@@ -72,4 +72,6 @@ cfg.callinfo.calltime = ftFuncClock;
 cfg.callinfo.user = getusername();
 fprintf('the call to "%s" took %d seconds and an estimated %d MB\n', mfilename, round(cfg.callinfo.proctime), round(cfg.callinfo.procmem/(1024*1024)));
 
-ftdata.cfg = cfg;
+% remember the configuration details
+data.cfg = cfg;
+
