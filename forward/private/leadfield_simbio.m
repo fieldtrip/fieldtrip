2545a4a1a7a9d1b91e068ebbb335f25c06951f2a
Cristiano Micheli <michelic72@googlemail.com>
2011-09-13 12:53:17 +0000

    bugfix - exceptions are now handled
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4165 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/private/leadfield_simbio.m
+++ forward/private/leadfield_simbio.m
@@ -1,93 +1,95 @@
 function [lf] = leadfield_simbio(dip, elc, vol)
 
-% LEADFIELD_SIMBIO leadfields for a set of dipoles
+% leadfield_simbio leadfields for a set of dipoles
 %
 % [lf] = leadfield_simbio(elc, vol, headmeshfile);
 %
 % with input arguments
-%   elc     positions of the electrodes (matrix of dimensions MX3)
+%   elc     positions of the electrodes (matrix of dimensions mx3)
 %           contains the positions of the vertices on which
 %           the potentials are calculated
-%           There can be 'deep electrodes' too!
-%   vol.wf  contains a wireframe structure (or FEM grid) of 'elements'
-%           which is needed to perform a FEM analysis.
-%   dip     a matrix of dipoles (of dimensions NX3)
+%           there can be 'deep electrodes' too!
+%   vol.wf  contains a wireframe structure (or fem grid) of 'elements'
+%           which is needed to perform a fem analysis.
+%   dip     a matrix of dipoles (of dimensions nx3)
 %
-% The output lf is the leadfields matrix of dimensions M (rows) X N*3 (cols)
+% the output lf is the leadfields matrix of dimensions m (rows) x n*3 (cols)
 
-% Copyright (C) 2011, Cristiano Micheli
+% copyright (c) 2011, cristiano micheli
 
 % store the current path and change folder to the temporary one
 tmpfolder = cd;
 
-try
+% add the simbio folder to the path
+if ~isunix
+  error('this only works on linux and osx')
+end
+
+if ~ft_hastoolbox('simbio',1,0)
+  error('you must install the simbio toolbox first')
+end
   
-  % add the Simbio folder to the path
-  if ft_hastoolbox('simbio',1,0)
-    if ~ispc
-      
-      cd(tempdir)
-      [~,tname] = fileparts(tempname);
-      exefile   = [tname '.sh'];
-      [~,tname] = fileparts(tempname);
-      elcfile   = [tname '.elc'];
-      [~,tname] = fileparts(tempname);
-      dipfile   = [tname '.dip'];
-      [~,tname] = fileparts(tempname);
-      parfile   = [tname '.par'];
-      [~,tname] = fileparts(tempname);
-      meshfile  = [tname '.v'];
-      [~,tname] = fileparts(tempname);
-      transfermatrix = [tname];
-      [~,tname] = fileparts(tempname);
-      outfile   = [tname];      
-      
-      % write the electrodes and dipoles positions in the temporary folder
-      disp('Writing the electrodes file on disk...')
-      if ~isfield(vol,'deepelec')
-        sb_write_elc(warp_apply(inv(vol.transform),elc.pnt),elc.label,elcfile);
-      else
-        sb_write_elc(warp_apply(inv(vol.transform),elc.pnt),elc.label,elcfile,1);
-      end
-      disp('Writing the dipoles file on disk...')
-      sb_write_dip(warp_apply(inv(vol.transform),dip),dipfile);
-      
-      % write the parameters file, contains tissues conductivities, the FE
-      % grid and Simbio call details, mixed together
-      disp('Writing the parameters file on disk...')
-      sb_write_par(parfile,'cond',vol.cond,'labels',unique(vol.wf.labels));
-      
-      % write the vol.wf in a Vista format .v file
-      ft_write_headshape(meshfile,vol.wf,'format','vista');
-      
-      % Exe file
-      efid = fopen(exefile, 'w');
-      if ~ispc
-        fprintf(efid,'#!/usr/bin/env bash\n');
-%         fprintf(efid,['ipm_linux_opt_Venant -i FEtransfermatrix -h ./' meshfile ' -s ./' elcfile, ...
-%           ' -o ./' transfermatrix ' -p ./' parfile ' -sens EEG 2>&1 > /dev/null\n']); 
-%         fprintf(efid,['ipm_linux_opt_Venant -i sourcesimulation -s ./' elcfile ' -t ./' transfermatrix, ...
-%           ' -dip ./' dipfile ' -o ./' outfile ' -p ./' parfile ' -fwd FEM -sens EEG 2>&1 > /dev/null\n']);         
-        fprintf(efid,['ipm_linux_opt_Venant -i sourcesimulation -h ./' meshfile ' -s ./' elcfile, ...
-          ' -dip ./' dipfile ' -o ./' outfile ' -p ./' parfile ' -fwd FEM -sens EEG 2>&1 > /dev/null\n']);
-      end
-      fclose(efid);
-      
-      dos(sprintf('chmod +x %s', exefile));
-      disp('SimBio is calculating the LeadFields, this may take some time ...')
-      
-      stopwatch = tic;
-      dos(['./' exefile]);
-      disp([ 'elapsed time: ' num2str(toc(stopwatch)) ])
-      
-      [lf] = sb_read_msr(outfile);
-      cleaner(exefile,elcfile,dipfile,parfile,meshfile,outfile)
-      cd(tmpfolder)
-    end
+try 
+  cd(tempdir)
+  [~,tname] = fileparts(tempname);
+  exefile   = [tname '.sh'];
+  [~,tname] = fileparts(tempname);
+  elcfile   = [tname '.elc'];
+  [~,tname] = fileparts(tempname);
+  dipfile   = [tname '.dip'];
+  [~,tname] = fileparts(tempname);
+  parfile   = [tname '.par'];
+  [~,tname] = fileparts(tempname);
+  meshfile  = [tname '.v'];
+  [~,tname] = fileparts(tempname);
+  transfermatrix = [tname];
+  [~,tname] = fileparts(tempname);
+  outfile   = [tname];
+  
+  % write the electrodes and dipoles positions in the temporary folder
+  disp('writing the electrodes file on disk...')
+  if ~isfield(vol,'deepelec')
+    sb_write_elc(warp_apply(inv(vol.transform),elc.pnt),elc.label,elcfile);
+  else
+    sb_write_elc(warp_apply(inv(vol.transform),elc.pnt),elc.label,elcfile,1);
   end
+  disp('writing the dipoles file on disk...')
+  sb_write_dip(warp_apply(inv(vol.transform),dip),dipfile);
+  
+  % write the parameters file, contains tissues conductivities, the fe
+  % grid and simbio call details, mixed together
+  disp('writing the parameters file on disk...')
+  sb_write_par(parfile,'cond',vol.cond,'labels',unique(vol.wf.labels));
+  
+  % write the vol.wf in a vista format .v file
+  ft_write_headshape(meshfile,vol.wf,'format','vista');
+  
+  % exe file
+  efid = fopen(exefile, 'w');
+  if ~ispc
+    fprintf(efid,'#!/usr/bin/env bash\n');
+    %         fprintf(efid,['ipm_linux_opt_venant -i fetransfermatrix -h ./' meshfile ' -s ./' elcfile, ...
+    %           ' -o ./' transfermatrix ' -p ./' parfile ' -sens eeg 2>&1 > /dev/null\n']);
+    %         fprintf(efid,['ipm_linux_opt_venant -i sourcesimulation -s ./' elcfile ' -t ./' transfermatrix, ...
+    %           ' -dip ./' dipfile ' -o ./' outfile ' -p ./' parfile ' -fwd fem -sens eeg 2>&1 > /dev/null\n']);
+    fprintf(efid,['ipm_linux_opt_venant -i sourcesimulation -h ./' meshfile ' -s ./' elcfile, ...
+      ' -dip ./' dipfile ' -o ./' outfile ' -p ./' parfile ' -fwd fem -sens eeg 2>&1 > /dev/null\n']);
+  end
+  fclose(efid);
+  
+  dos(sprintf('chmod +x %s', exefile));
+  disp('simbio is calculating the leadfields, this may take some time ...')
+  
+  stopwatch = tic;
+  dos(['./' exefile]);
+  disp([ 'elapsed time: ' num2str(toc(stopwatch)) ])
+  
+  [lf] = sb_read_msr(outfile);
+  cleaner(exefile,elcfile,dipfile,parfile,meshfile,outfile)
+  cd(tmpfolder)
   
 catch
-  warning('an error occurred while running SimBio');
+  warning('an error occurred while running simbio');
   rethrow(lasterror)
   cleaner(dipfile,elcfile,outfile,exefile)
   cd(tmpfolder)
