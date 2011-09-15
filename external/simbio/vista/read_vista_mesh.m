02ae9c65c1474fa9cb91a5a13d163b66a88fc539
Cristiano Micheli <michelic72@googlemail.com>
2011-09-15 14:28:31 +0000

    bugfix - resynched to the original version
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4228 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- external/simbio/vista/read_vista_mesh.m
+++
@@ -1,8 +0,0 @@
-function read_vista_mesh()
-% Reads a vista format mesh. This is achieved by a correspondently named compiled
-% version.
-%
-% Use as
-% [nodes,elements,labels] = read_vista_mesh(filename);
-%
-% filename          the name of the Vista mesh (with extension .v)
