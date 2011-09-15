7f3dace409cbb7c229252ecaa48395fc0809c7de
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-09-15 07:31:04 +0000

    enhancement - wrote test script
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4217 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/test/test_ft_apply_montage.m
+++ forward/test/test_ft_apply_montage.m
@@ -1,1 +1,24 @@
-function test_ft_apply_montage
+%TEST: test_ft_apply_montage ft_apply_montage
+
+pwdir = pwd;
+cd('/home/common/matlab/fieldtrip/data/');
+cfg = [];
+cfg.dataset = 'Subject01.ds';
+cfg.trl     = [1 1200 0];
+cfg.continuous = 'yes';
+data = ft_preprocessing(cfg);
+
+mont          = [];
+mont.tra      = -eye(151); %flip sign
+mont.labelorg = ft_channelselection({'MEG'}, data.label);
+mont.labelnew = ft_channelselection({'MEG'}, data.label);
+
+data2 = ft_apply_montage(data, mont, 'keepunused', 'yes');
+data3 = ft_apply_montage(data, mont, 'keepunused', 'no');
+grad  = ft_apply_montage(data.grad, mont, 'keepunused', 'yes');
+
+[a,b] = match_str(data2.label, data.label);
+assert(all(all(data2.trial{1}(a(1:151),:)==-data.trial{1}(b(1:151),:)))==1);
+assert(all(all(data.grad.tra(1:151,:)==-grad.tra(1:151,:)))==1);
+
+cd(pwdir);
