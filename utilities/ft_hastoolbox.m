37949e3132c891ae871ff5e7f7aed8bd4f2a9fd8
Cristiano Micheli <michelic72@googlemail.com>
2011-10-03 12:15:47 +0000

    enhancement - added toolbox vgrid
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4329 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/ft_hastoolbox.m
+++ utilities/ft_hastoolbox.m
@@ -98,6 +98,7 @@ url = {
   'PEER'       'see http://fieldtrip.fcdonders.nl/development/peer'
   'FREESURFER' 'see http://surfer.nmr.mgh.harvard.edu/fswiki'
   'SIMBIO'     'see https://www.mrt.uni-jena.de/simbio/index.php/Main_Page'
+  'VGRID'      'see http://www.rheinahrcampus.de/~medsim/vgrid/manual.html'
   'FNS'        'see http://hhvn.nmsu.edu/wiki/index.php/FNS'
   'GIFTI'      'see http://www.artefact.tk/software/matlab/gifti'
   'XML4MAT'    'see http://www.mathworks.com/matlabcentral/fileexchange/6268-xml4mat-v2-0'
@@ -227,7 +228,13 @@ switch toolbox
   case 'FNS'
     status  = exist('elecsfwd', 'file') && exist('img_get_gray', 'file');
   case 'SIMBIO'
-    status  = exist('ipm_linux_opt_Venant', 'file') && exist('sb_write_dip.m','file');
+    status  = exist('ipm_linux_opt', 'file') && exist('sb_write_dip.m','file');
+  case 'VGRID'
+    if ~ft_hastoolbox('simbio')
+      error('you need to install simbio too!')
+    end
+    prefix = fileparts(which('sb_write_dip.m'));
+    status = exist([prefix '/vgrid1.3.1/program/vgrid'], 'file');
   case 'GIFTI'
     status  = exist('gifti', 'file');
   case 'XML4MAT'
