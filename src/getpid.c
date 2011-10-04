1d844661767aba1c752e2bf891970efaa4dce7a2
Robert Oostenveld <r.oostenveld@gmail.com>
2011-10-04 19:10:26 +0000

    bugfix - added missing getpid.c to src, set autosync=true and compiled on 32 bit OS X
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4342 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ src/getpid.c
@@ -0,0 +1,12 @@
+#include <sys/types.h> 
+#include <unistd.h> 
+
+#include "mex.h" 
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) 
+{ 
+  unsigned int *pr; 
+  plhs[0] = mxCreateNumericMatrix(1, 1, mxUINT32_CLASS, mxREAL); 
+  pr = mxGetData(plhs[0]); 
+  *pr = (unsigned int)getpid(); 
+} 
