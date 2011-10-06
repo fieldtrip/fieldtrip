20e420f7c87499fd8d11c790950a818fc3f40ecf
Robert Oostenveld <r.oostenveld@gmail.com>
2011-10-06 11:12:11 +0000

    added a test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1014
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4360 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1014.m
@@ -0,0 +1,33 @@
+function test_bug1014
+
+% TEST test_bug1014
+% TEST ft_checkdata ft_appendtimelock ft_selectdata
+
+a.time   = [1 2];
+a.label  = {'chan'};
+a.dimord = 'chan_time';
+a.avg    = [1 1];
+a.trial  = reshape([3 3; 0 0; 0 0], [3 1 2]);
+
+b.time   = [1 2];
+b.label  = {'chan'};
+b.dimord = 'chan_time';
+b.avg    = [2 2];
+b.trial  = reshape([6 6; 0 0; 0 0], [3 1 2]);
+
+% the following is to work around
+% http://bugzilla.fcdonders.nl/show_bug.cgi?id=1013
+
+a.sampleinfo = [1 2; 3 4; 5 6];
+b.sampleinfo = [1 2; 3 4; 5 6];
+
+% re-test bug 1013
+ft_checkdata(a, 'datatype', 'timelock', 'hassampleinfo', 'ifmakessense');
+ft_checkdata(b, 'datatype', 'timelock', 'hassampleinfo', 'ifmakessense');
+
+
+c = ft_appendtimelock([], a, b)
+
+if isfield(c, 'avg') && numel(size(c.avg))>2
+  error('the average has incorrect dimensions');
+end
