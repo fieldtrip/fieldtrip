2d0b0c8c116193905bd6174ba731543ff1e3f732
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2011-10-06 12:09:00 +0000

    updated convol to current status of the other specest functions
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4364 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- specest/ft_specest_convol.m
+++ specest/ft_specest_convol.m
@@ -17,11 +17,9 @@ function [spectrum, freqoi, timeoi] = ft_specest_convol(dat, time, varargin)
 %   freqoi        = vector, containing frequencies (in Hz)
 %   waveletwidth  = number, 'width' of wavelets expressed in cycles (default = 7)
 %
-%  OPTION DOWNSAMPLE: this looks like something that would fit in better in the (freqanalysis) wrapper I think
-%  OPTION LATENCY, WHAT TO DO WITH THIS?
-%  HOW TO MAKE CONSISTENT freqoi'S OVER TRIALS (e.g. multiple runs of this function)? ZERO-PADDING NOT AN OPTION... YET WE SHOULD STILL ONLY HAVE freqoi'S THAT ACTUALLY MATCH THE FREQUENCIES IN OUTPUT
+% 
 %
-% See also SPECEST_MTMFFT, SPECEST_MTMCONVOL, SPECEST_HILBERT, SPECEST_NANFFT, SPECEST_MVAR, SPECEST_WAVELET
+% See also SPECEST_MTMFFT, SPECEST_MTMCONVOL, SPECEST_HILBERT, SPECEST_NANFFT, SPECEST_WAVELET
 
 % Copyright (C) 2010, Donders Institute for Brain, Cognition and Behaviour
 %
@@ -33,6 +31,11 @@ timeoi        = keyval('timeoi',        varargin); if isempty(timeoi),       tim
 freqoi        = keyval('freqoi',        varargin); if isempty(freqoi),       freqoi       = 'all';    end
 waveletwidth  = keyval('waveletwidth',  varargin); if isempty(waveletwidth), waveletwidth = 7;        end
 polyorder     = keyval('polyremoval', varargin); if isempty(polyorder), polyorder = 1; end
+
+if isempty(fbopt),
+  fbopt.i = 1;
+  fbopt.n = 1;
+end
 
 % Set n's
 [nchan,ndatsample] = size(dat);
@@ -79,6 +82,15 @@ wavfam = waveletfam(freqoi,fsample,waveletwidth);
 % compute spectrum by convolving the wavelets with the data
 spectrum = zeros(nchan, nfreqoi, nsample);
 for ifreqoi = 1:nfreqoi
+  str = sprintf('frequency %d (%.2f Hz)', ifreqoi,freqoi(ifreqoi));
+  [st, cws] = dbstack;
+  if length(st)>1 && strcmp(st(2).name, 'ft_freqanalysis') && verbose
+    % specest_convol has been called by ft_freqanalysis, meaning that ft_progress has been initialised
+    ft_progress(fbopt.i./fbopt.n, ['trial %d, ',str,'\n'], fbopt.i);
+  elseif verbose
+    fprintf([str, '\n']);
+  end
+  
   wavelet = wavfam{ifreqoi};
   for ichan = 1:nchan
     spectrum(ichan,ifreqoi,:) = conv(dat(ichan,:),  wavelet, 'same');
@@ -110,95 +122,3 @@ for k=1:length(foi)
   M{k}= A*exp(-toi.^2/(2*st^2)).*exp(i*2*pi*foi(k).*toi);
 end
 
-
-
-% state         = keyval('state',         varargin);
-% waveletwidth  = keyval('waveletwidth',  varargin); if isempty(waveletwidth), waveletwidth = 7; end
-% downsample    = keyval('downsample',    varargin);
-% time          = keyval('time',          varargin);
-% toi           = keyval('toi',           varargin);
-%
-% % FIXME add width
-%
-% if ~isempty(downsample) && ~isempty(toi)
-%   error('the downsample and toi options are mutually exclusive');
-% end
-%
-% nchans   = size(dat,1);
-% nsamples = size(dat,2);
-%
-% if isempty(time)
-%   if ~isempty(toi)
-%     error('specification of toi without time is not permitted');
-%   end
-%   time = (0:(nsamples-1))/fsample;
-% end
-%
-% if ~isempty(state) && isequal(state.fsample, fsample)  && isequal(state.foi, foi) && isequal(state.waveletwidth, waveletwidth)
-%   % reuse the wavelet family from the previous state
-%   disp('using previous state');
-%   M = state.M;
-% else
-%   if ~isempty(state)
-%     state = [];
-%     warning('recomputing the state');
-%   end
-%   % recompute the wavelet family
-%   M = waveletfam(foi,fsample,waveletwidth);
-% end
-%
-% % compute freq by convolving the wavelets with the data
-% nfreq = length(foi);
-% spectrum = zeros(nchans, nsamples, nfreq);
-%
-% for f=1:nfreq
-%   wavelet = M{f};
-%   for c=1:nchans
-%     spectrum(c,:,f) = conv(dat(c,:),  wavelet, 'same');
-%   end
-%   % pad the edges with nans to indicate that the wavelet was not fully immersed in the data
-%   pad = ceil(length(M{f})/2);
-%   % the padding should not be longer than the actual data
-%   pad = min(pad, nsamples);
-%   begpad = 1:pad;
-%   endpad = (nsamples-pad+1):nsamples;
-%   spectrum(:,begpad,f) = nan;
-%   spectrum(:,endpad,f) = nan;
-% end
-%
-% if ~isempty(downsample)
-%   % this would be done in case of downsample
-%   tbin = 1:downsample:nsamples;
-% elseif ~isempty(toi)
-%   % this would be done in case of toi/time specification
-%   tbin = zeros(size(toi));
-%   for i=1:length(tbin)
-%     tbin = nearest(time, toi(i));
-%   end
-% else
-%   tbin = 1:nsamples;
-% end
-%
-% % select the samples for the output
-% spectrum = spectrum(:,tbin,:);
-%
-% % remember the wavelets so that they can be reused in a subsequent call
-% state.fsample = fsample;
-% state.foi     = foi;
-% state.waveletwidth = waveletwidth;
-% state.M       = M;
-%
-% % %convolves data in chan by time matrix (from one trial) with 1 wavelet for
-% % %each specified frequency of interest
-% % spectrum = zeros(size(data,1),length(foi), size(data,2));
-% % for k=1:size(data,1) %nchans
-% %   for j=1:length(foi)
-% %     cTmp = conv(data(k,:),M{j});
-% %     cTmp = 2*(abs(cTmp).^2)/fsample;
-% %     cTmp = cTmp(ceil(length(M{j})/2):length(cTmp)-floor(length(M{j})/2));
-% %     spectrum(k,j,:) = cTmp;
-% %   end
-% %
-% % end
-%
-% %output should be chan by freq by time
