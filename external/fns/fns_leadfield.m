24693cfb45ea5f45a23b7828e91f0abea47f950a
Hung Dang <hungptit@gmail.com>
2011-10-10 03:28:59 +0000

    1. Add elecsfwd1 which compute the potential for all nodes which belong to the head model (nodes that are outside of the head have zero potential). 2. Update fns_leadfield, which computes the lead field matrix for a given node (in MRI coordinate).
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4408 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- external/fns/fns_leadfield.m
+++ external/fns/fns_leadfield.m
@@ -1,45 +1,54 @@
-function L = fns_leadfield(data,compress,node_sizes,row,col,slice)
+function L = fns_leadfield(forwardSolutions, modelSizes, row, col, slice)
 % 
-% fns_leadfield is a function to extract the lead field matrix from the
-% reciprocity data matrix
+% fns_leadfield is a function that compute the lead field matrix from the
+% potential data obtained using Finite Different Method.
 %
-% L = fns_leadfield(recipdata,compress,node_sizes,row,col,slice)
+% L = fns_leadfield(forwardSolutions, row, col, slice)
 %
-% L:            Lead field matrix
-% recipdata:    Reciprocity data matrix
-% compress:     Compress vector
-% node_sizes:   The sizes of the node volume
-% row:          Row position of the given node
-% col:          Column position of the given node
-% slice:        Slice position of the given node
-
+% L:                Lead field matrix
+% forwardSolutions: Forward solution data
+% row:              Row position of the given node
+% col:              Column position of the given node
+% slice:            Slice position of the given node
 
 % $Copyright (C) 2010 by Hung Dang$
 
-
 % Position of the head and tail in the x, y and z direction
-ROW = node_sizes(1);
-COL = node_sizes(2);
-N = length(compress);
+ROW = modelSizes(1);
+COL = modelSizes(2);
+[M, N] = size(forwardSolutions); %#ok<NASGU>
 
 % Add 1 to convert between MATLAB index and C index
 nnode = row + col * ROW + slice * COL * ROW + 1; 
 
 % Compute the lead field matrix for the given node
 if (nnode <= N)
-    head_x = compress(nnode + 1);
-    tail_x = compress(nnode - 1);
-    head_y = compress(nnode + ROW);
-    tail_y = compress(nnode - ROW);
-    head_z = compress(nnode + COL * ROW);
-    tail_z = compress(nnode - COL * ROW);
-        
+%     head_x = forwardSolutions.Compress(nnode + 1);
+%     tail_x = forwardSolutions.Compress(nnode - 1);
+%     head_y = forwardSolutions.Compress(nnode + ROW);
+%     tail_y = forwardSolutions.Compress(nnode - ROW);
+%     head_z = forwardSolutions.Compress(nnode + COL * ROW);
+%     tail_z = forwardSolutions.Compress(nnode - COL * ROW);
+
+    head_x = nnode + 1;
+    tail_x = nnode - 1;
+    head_y = nnode + ROW;
+    tail_y = nnode - ROW;
+    head_z = nnode + COL * ROW;
+    tail_z = nnode - COL * ROW;
+
+
     % Extract the lead field matrix
-    if ((head_x > 0) && (tail_x > 0) && (head_y > 0) && (tail_y > 0) && (head_z > 0) && (tail_z > 0))
+    if (...
+            (head_x > 0) && (tail_x > 0) && ...
+            (head_y > 0) && (tail_y > 0) && ...
+            (head_z > 0) && (tail_z > 0))
+        
         % Extract the lead field matrix for x, y and z component
-        lx = data(:,head_x) - data(:,tail_x);
-        ly = data(:,head_y) - data(:,tail_y);
-        lz = data(:,head_z) - data(:,tail_z);
+        lx = forwardSolutions(:,head_x) - forwardSolutions(:,tail_x);
+        ly = forwardSolutions(:,head_y) - forwardSolutions(:,tail_y);
+        lz = forwardSolutions(:,head_z) - forwardSolutions(:,tail_z);
+        
         % Form the final lead field matrix
         L = [lx,ly,lz];
     else
