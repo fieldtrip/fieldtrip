5398610784aff0e7e6f7ba1701979bb4433a89c1
Jorn Horschig <jm.horschig@donders.ru.nl>
2011-10-11 13:56:58 +0000

    enhancement -#1028- making use of warning_once instead of using an own persistent variable
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4428 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- qsub/private/getcustompwd.m
+++ qsub/private/getcustompwd.m
@@ -5,7 +5,6 @@ function p = getcustompwd
 
 % these are for faster processing on subsequent calls
 persistent previous_pwd previous_argout
-persistent previous_warn
 
 if isequal(pwd, previous_pwd)
   % don't do the processing again, but return the previous values from cache
@@ -16,11 +15,7 @@ end
 % don't use the present directory if it contains the peer code
 % it will confuse the slave with a potentially different mex file
 if strcmp(pwd, fileparts(mfilename('fullpath')))
-  if ~strcmp(previous_warn, pwd)
-    warning('the peer slave will not change directory to %s', pwd);
-    % warn only once
-    previous_warn = pwd;
-  end
+  warning_once('the peer slave will not change directory to %s', pwd);    
   p = [];
 else
   p = pwd;
