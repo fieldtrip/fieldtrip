97c45511ecb0ced5172f947a6395496160bb8d29
Robert Oostenveld <r.oostenveld@gmail.com>
2011-10-12 10:48:49 +0000

    added test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=367
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4442 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug367.m
@@ -0,0 +1,69 @@
+function test_bug367
+
+% this test script is for http://bugzilla.fcdonders.nl/show_bug.cgi?id=367
+% and was constructed from http://fieldtrip.fcdonders.nl/example/megrealign
+
+% TEST test_bug367
+% TEST ft_megrealign ft_read_sens ft_dipolesimulation ft_timelockanalysis ft_multiplotER
+
+% the two example files with the 151 and 275 channel CTF specifications are available from
+% ftp://ftp.fcdonders.nl/pub/fieldtrip/example/megrealign
+
+cd /home/common/matlab/fieldtrip/data/test/bug367
+
+grad151 = ft_read_sens('ctf151.mat');
+grad275 = ft_read_sens('ctf275.mat');
+
+vol = [];
+vol.r = 12;
+vol.o = [0 0 4];
+
+% in this example the dipole is carefully positioned on the same depth as
+% where the dipole layer for the interpolation will be located. The center
+% of the head is at [0 0 4], the radius is 12 cm, and the dipole layer that is
+% specified in ft_megrealign is 2.5 cm shifted inward from the head surface
+cfg = [];
+cfg.dip.pos = [0 0 13.5];  % 4 + 12 - 2.5
+cfg.dip.frequency = 1;
+cfg.vol = vol;
+cfg.grad = grad151;
+data151 = ft_dipolesimulation(cfg);
+cfg.grad = grad275;
+data275 = ft_dipolesimulation(cfg);
+
+cfg = [];
+avg151 = ft_timelockanalysis(cfg, data151);
+avg275 = ft_timelockanalysis(cfg, data275);
+
+% here I'll not only realign the 151 channel MEG data to the 275 channel and
+% vice versa, but also apply the ft_megrealign function from 151 to 151. In the
+% realignment the signal will be slightly distorted as a comparison of avg151_151
+% to avg151 would show.
+cfg = [];
+cfg.inwardshift = 3;
+cfg.vol = vol;
+cfg.template{1} = grad151; avg151_151 = ft_timelockanalysis([], ft_megrealign(cfg, avg151));
+cfg.template{1} = grad275; avg151_275 = ft_timelockanalysis([], ft_megrealign(cfg, avg151));
+cfg.template{1} = grad151; avg275_151 = ft_timelockanalysis([], ft_megrealign(cfg, avg275));
+cfg.template{1} = grad275; avg275_275 = ft_timelockanalysis([], ft_megrealign(cfg, avg275));
+
+% plot the realigned datasets
+cfg = [];
+figure; ft_multiplotER(cfg, avg151_151);
+title('Interpolated from 151 to 151');
+
+figure; ft_multiplotER(cfg, avg151_275);
+title('Interpolated from 151 to 151');
+
+figure; ft_multiplotER(cfg, avg275_151);
+title('Interpolated from 151 to 151');
+
+figure; ft_multiplotER(cfg, avg275_275);
+title('Interpolated from 151 to 151');
+
+% now plot them together
+cfg = [];
+figure; ft_multiplotER(cfg, avg151, avg151_151, avg275_151);
+figure; ft_multiplotER(cfg, avg275, avg275_275, avg151_275);
+
+
