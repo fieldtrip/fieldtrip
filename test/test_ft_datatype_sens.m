e293bf88581ea9029047daf4430606bf8ea70cc6
Robert Oostenveld <r.oostenveld@gmail.com>
2011-10-17 19:47:23 +0000

    restructuring - implemented an alternative to fixsens, which also takes care of documenting the changes to the end-user, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1050
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4495 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_ft_datatype_sens.m
@@ -0,0 +1,86 @@
+% function test_fixsens
+
+% TEST test_fixsens
+% TEST fixsens ft_read_sens ft_read_header ctf2grad bti2grad itab2grad fif2grad yokogawa2grad
+
+% the fixsens function is in fieldtrip/private
+cd(fullfile(fileparts(fileparts(mfilename('fullpath'))), 'private'));
+
+path1 = '/home/common/matlab/fieldtrip/data/test/original/meg';
+path2 = '/Users/robert/Manzana/data/dataformat/testdata';
+
+filename = {
+  'ctf151/Subject01.ds'
+  % this one fails because the ctf64 dataset requires special headerformat/dataformat flags
+  % 'ctf64/Wat123r1raw.ds'
+  'ctf275/A0132_Aud-Obj-Recognition_20051115_02.ds'
+  'neuromag122/nmStim150.fif'
+  'neuromag306/raw.fif'
+  'itab153/srgcst85_0105.raw'
+  'itab153/srgcst85_0105.raw.mhd'
+  'yokogawa160/Continuous1.con'
+  'itab28/gibb0101.raw'
+  'itab28/gibb0101.raw.mhd'
+  'bti148/c,rfhp0.1Hz'
+  'bti248/e,rfDC'
+  'bti248/e,rfDC,F,a'
+  'itab28_old/gibb0101.raw'
+  'itab28_old/gibb0101.raw.mhd'
+  'yokogawa64/2011_01_28_0354_ME053_AEF.con'
+  'yokogawa440/S1_MEG_Epoch.raw'
+  };
+
+for i=1:length(filename)
+  
+  if isdir(path1)
+    % this is for running it on a mentat node
+    dataset = fullfile(path1, filename{i});
+  elseif isdir(path2)
+    % the following part allows the code to run on the laptop of Robert
+    [p, f, x] = fileparts(filename{i});
+    [a, b] = system(sprintf('find %s -name %s', path2, [f x]));
+    if length(b)>1
+      dataset = deblank(b);
+    else
+      warning('unable to test with the dataset "%s" because it cannot be found', filename{i});
+      continue
+    end
+  else
+    error('unable to test with the dataset "%d" because it cannot be found', filename{i});
+  end
+  
+  sens1 = ft_read_sens(dataset);
+  
+  assert(isfield(sens1, 'chanpos'));
+  assert(isfield(sens1, 'chanori'));
+  assert(isfield(sens1, 'chantype'));
+  assert(isfield(sens1, 'chanunit'));
+  % assert(isfield(sens1, 'type'));
+  % assert(isfield(sens1, 'unit'));
+  
+  sens2         = [];
+  sens2.pnt     = sens1.coilpos;
+  sens2.ori     = sens1.coilori;
+  sens2.tra     = sens1.tra;
+  sens2.label   = sens1.label;
+  if isfield(sens1, 'balance')
+    sens2.balance = sens1.balance;
+  end
+  
+  % reconstruct the representation with chanpos and coilpos
+  % this should also add the chantype and chanunit fields
+  sens2 = fixsens(sens2);
+  
+  disp(dataset);
+  disp('');
+  disp(sens1)
+  disp(sens2)
+  
+  try
+    % they should be identical
+    assert(isequal(sens1, sens2));
+  catch
+    warning(lasterr);
+  end
+end
+
---
+++ utilities/ft_datatype_sens.m
@@ -0,0 +1,147 @@
+function [sens] = ft_datatype_sens(sens, varargin)
+
+% FT_DATATYPE_SENS describes the FieldTrip structure that represents
+% an EEG, ECoG, or MEG sensor array. This structure is commonly called
+% "elec" for EEG and "grad" for MEG, or more general "sens" for either
+% one.
+%
+% The structure for MEG gradiometers and/or magnetometers contains
+%    sens.label    = Mx1 cell-array with channel labels
+%    sens.chanpos  = Mx3 matrix with channel positions
+%    sens.chanori  = Mx3 matrix with channel orientations, used for synthetic planar gradient computation
+%    sens.tra      = MxN matrix to combine coils into channels
+%    sens.coilpos  = Nx3 matrix with coil positions
+%    sens.coilori  = Nx3 matrix with coil orientations
+%    sens.balance  = structure containing info about the balancing, See FT_APPLY_MONTAGE
+%
+% The structure for EEG or ECoG channels contains
+%    sens.label    = Mx1 cell-array with channel labels
+%    sens.chanpos  = Mx3 matrix with channel positions
+%    sens.tra      = MxN matrix to combine electrodes into channels
+%    sens.elecpos  = Nx3 matrix with coil positions
+% In case sens.tra is not present in the EEG sensor array, the channels
+% are assumed to be average referenced.
+%
+% The following fields are optional
+%    sens.type     = string with the MEG or EEG acquisition system, see FT_SENSTYPE
+%    sens.chantype = Mx1 cell-array with the type of the channel, see FT_CHANTYPE
+%    sens.chanunit = Mx1 cell-array with the units of the channel signal, e.g. 'T', 'fT' or 'fT/cm'
+%
+% Revision history:
+%
+% (2011v2/latest) The chantype and chanunit have been added for MEG.
+%
+% (2011v1) To facilitate determining the position of channels (e.g. for plotting)
+%  in case of balanced MEG or bipolar EEG, an explicit distinction has been made
+%  between chanpos+chanori and coilpos+coilori (for MEG) and chanpos and elecpos
+%  (for EEG). The pnt and ori fields are removed
+%
+% (2010) Added support for bipolar or otherwise more complex linear combinations
+%  of EEG electrodes using sens.tra, similar to MEG.
+%
+% (2009) Noice reduction has been added for MEG systems in the balance field.
+%
+% (2006) The optional fields sens.type and sens.unit were added.
+%
+% (2003) The initial version was defined, which looked like this for EEG
+%    sens.pnt     = Mx3 matrix with electrode positions
+%    sens.label   = Mx1 cell-array with channel labels
+% and like this for MEG
+%    sens.pnt     = Nx3 matrix with coil positions
+%    sens.ori     = Nx3 matrix with coil orientations
+%    sens.tra     = MxN matrix to combine coils into channels
+%    sens.label   = Mx1 cell-array with channel labels
+%
+% See also FT_READ_SENS, FT_SENSTYPE, FT_CHANTYPE, FT_APPLY_MONTAGE, CTF2GRAD, FIF2GRAD,
+% BTI2GRAD, YOKOGAWA2GRAD, ITAB2GRAD
+
+% get the optional input arguments, which should be specified as key-value pairs
+version       = ft_getopt(varargin, 'version', 'latest');
+
+if strcmp(version, 'latest')
+  version = '2011v2';
+end
+
+% there are many cases which deal with either eeg or meg
+isgrad = ft_senstype(sens, 'meg');
+
+switch version
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  case '2011v2'
+    
+    if isfield(sens, 'pnt') || isfield(sens, 'ori')
+      if isgrad
+        % sensor description is a MEG sensor-array, containing oriented coils
+        [chanpos, chanori, tmp] = channelposition(sens, 'channel', 'all');
+        sens.coilori = sens.ori; sens = rmfield(sens, 'ori');
+        sens.coilpos = sens.pnt; sens = rmfield(sens, 'pnt');
+        sens.chanpos = chanpos;
+        sens.chanori = chanori;
+      else
+        % sensor description is something else, EEG/ECoG etc
+        chanpos      = channelposition(sens, 'channel', 'all');
+        sens.elecpos = chanpos; sens = rmfield(sens, 'pnt');
+        sens.chanpos = chanpos;
+      end
+    end
+    
+    if ~isfield(sens, 'chanpos')
+      if isgrad
+        % sensor description is a MEG sensor-array, containing oriented coils
+        [chanpos, chanori, lab] = channelposition(sens, 'channel', 'all');
+        sens.chanpos = chanpos;
+        sens.chanori = chanori;
+      else
+        % sensor description is something else, EEG/ECoG etc
+        [chanpos, lab] = channelposition(sens, 'channel', 'all');
+        sens.chanpos   = chanpos;
+      end
+    end
+    
+    if ~isfield(sens, 'chantype')
+      if isgrad
+        sens.chantype = ft_chantype(sens);
+      else
+        % FIXME for EEG we have not yet figured out how to deal with this
+      end
+    end
+    
+    if ~isfield(sens, 'chanunit')
+      if ft_senstype(sens, 'ctf')
+        sens.chanunit = repmat({'T'}, size(sens.label));
+      elseif ft_senstype(sens, 'bti')
+        % FIXME
+        sens.chanunit = repmat({'unknown'}, size(sens.label));
+      elseif ft_senstype(sens, 'neuromag')
+        % FIXME
+        sens.chanunit = repmat({'unknown'}, size(sens.label));
+      elseif ft_senstype(sens, 'itab')
+        % FIXME
+        sens.chanunit = repmat({'unknown'}, size(sens.label));
+      elseif ft_senstype(sens, 'yokogawa')
+        % FIXME
+        sens.chanunit = repmat({'unknown'}, size(sens.label));
+      else
+        % FIXME for EEG or other MEG systems we have not yet figured out how to deal with this
+      end
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  otherwise
+    error('converting to version %s is not supported', version);
+    
+end % switch
+
+% this makes the display with the "disp" command look better
+sens = sortfieldnames(sens);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function b = sortfieldnames(a)
+fn = sort(fieldnames(a));
+for i=1:numel(fn)
+  b.(fn{i}) = a.(fn{i});
+end
+
+
