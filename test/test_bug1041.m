6a507337b992b5a360c023a271780528fefa20f1
Robert Oostenveld <r.oostenveld@gmail.com>
2011-10-18 13:23:13 +0000

    enhancement - added test script for bug 1041
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4502 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1041.m
@@ -0,0 +1,32 @@
+function test_bug1041
+
+% TEST test_bug1041
+% TEST ft_freqdescriptives memtic memtoc
+
+% this is a bug that Jorn reported but that I am not able to reproduce
+% see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1041
+
+% generate some data
+data          = [];
+data.trial{1} = randn(2,1000);
+data.time{1}  = [0:0.001:0.999] - 0.5;
+data.trial{2} = randn(2,1000);
+data.time{2}  = [0.001:0.001:1] - 0.5;
+data.label    = {'chan1';'chan2'};
+data.fsample  = 1000;
+data.cfg.trl  = zeros(2,3);
+
+cfg = [];
+cfg.method    = 'mtmfft';
+cfg.output    = 'powandcsd';
+cfg.tapsmofrq = 4;
+cfg.foilim    = [8 40];
+freq = ft_freqanalysis(cfg, data);
+
+cfg = [];
+fd = ft_freqdescriptives(cfg, freq);
+
+if isnan(fd.cfg.callinfo.procmem)
+  error('memtoc failed for ft_freqdescriptives');
+end
+
