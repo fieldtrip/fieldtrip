5b01d16c1bc5ef72df6d79ab8ac5564d19d0164d
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-10-18 13:26:13 +0000

    bugfix - corrected the bug number (1043 -> 1051)
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4504 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1051.m
@@ -0,0 +1,17 @@
+function test_bug1051
+
+% TEST test_bug1051 ft_megplanar ft_apply_montage
+
+% the bug consists of ft_apply_montage not adequately dealing with 
+% sensor descriptions that contain coilori/pos chanori/pos.
+% The chanori/chanpos get lost along the way
+
+cd /home/common/matlab/fieldtrip/data/test/raw/meg/
+load preproc_ctf151
+
+cfg.method = 'triangulation';
+neighbours = ft_prepare_neighbours(cfg, data);
+cfg        = [];
+cfg.planarmethod = 'sincos';
+cfg.neighbours   = neighbours;
+planar     = ft_megplanar(cfg, data);
