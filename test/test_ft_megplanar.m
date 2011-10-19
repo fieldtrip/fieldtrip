39aa7ecd31bc097392f392da99c5629729b1463e
Jorn Horschig <jm.horschig@donders.ru.nl>
2011-10-19 15:09:17 +0000

    bugfix -#948- megplanar testscript fixed and restored
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4524 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_ft_megplanar.m
+++ test/test_ft_megplanar.m
@@ -1,13 +1,12 @@
 %function test_ft_megplanar
 
-% TEST: ft_megplanar test_ft_megplanar ft_prepare_neighbours
+% TEST: ft_megplanar test_ft_megplanar ft_prepare_neighbours ft_topoplotER
 
 datainfo = test_datasets;
 sel      = match_str({datainfo.datatype}',{'bti148' 'bti248' 'ctf151' 'ctf275' 'itab153' 'yokogawa160'}');
 datainfo = datainfo(sel);
 
-% as of yet, the code does not explicitly test anything, it just checks
-% whether all runs through smoothly
+%% check whether all runs through smoothly
 for k = 1:numel(datainfo)
   fname = [datainfo(k).origdir,'raw/',datainfo(k).type,'preproc_',datainfo(k).datatype];
   load(fname);
@@ -93,106 +92,65 @@ for k = 1:numel(datainfo)
   
 end
 
+%% check for qualitative changes in transformation
+% load exampledata from ER tutorial, simplified
+load avgFIC
 
-% % load exampledata from ER tutorial, simplified
-% load avgFIC
-% 
-% % compute neighbours
-% cfg = [];
-% cfg.method =  'distance';
-% cfg.neighbours = ft_prepare_neighbours(cfg, avgFIC);
-% 
-% % megplanar
-% cfg.planarmethod = 'sincos';
-% avgFICplanar = ft_megplanar(cfg, avgFIC);
-% 
-% % timelock and combine planar
-% cfg = [];
-% avgFICplanar     = ft_timelockanalysis(cfg, avgFICplanar);
-% avgFICplanarComb = ft_combineplanar(cfg,avgFICplanar);
-% 
-% 
-% % check whether maxima and minima on sensor level are the same
-% it = 5; % #iterations
-% i = 0;
-% while it < i
-%     [a, b] = nanmax(nanmax(avgFICplanarComb.avg, [], 2))
-%     if (b~=29) || b~= 53 || b~=4 || b~=28 || b~=3
-%         error('the global maxima has moved location');
-%     else
-%         avgFICplanarComb.avg(b, :) = nan;
-%     end
-%     i = t+1;
-% end
-% fprintf('global maxima seems all fine\n');
-% 
-% i = 0;
-% while it < i
-%     [a, b] = nanmin(nanmin(avgFICplanarComb.avg, [], 2))
-%     if b~=21|| b~=33 || b~=133 || b~=92 || b~=144
-%         error('the global minima has moved location');
-%     else
-%         avgFICplanarComb.avg(b, :) = nan;
-%     end
-%     i = t+1;
-% end
-% fprintf('global minima seems all fine\n');
-% 
-% 
-% % plotting
-% cfg = [];
-% clf
-% subplot(121)
-% cfg.xlim = [0.3 0.5];
-% cfg.zlim = 'maxmin';
-% cfg.colorbar = 'yes';
-% ft_topoplotER(cfg,avgFIC)
-% colorbar
-% subplot(122)
-% cfg.zlim = 'maxabs';
-% ft_topoplotER(cfg,avgFICplanarComb)
-% 
-% fprintf('compare with http://fieldtrip.fcdonders.nl/tutorial/eventrelatedaveraging#plot_the_results_planar_gradients\n');
-% 
-% 
-% 
-% end
-% 
-% function topoplotCompare(data)
-% % compute neighbours
-% cfg = [];
-% cfg.method =  'distance';
-% cfg.neighbours = ft_prepare_neighbours(cfg, data);
-% 
-% % megplanar
-% % cfg.planarmethod = 'orig';
-% % if strcmp(data.grad.unit, 'cm')
-% %     cfg.neighbourdist = 4; % not used in new version anymore!
-% % elseif strcmp(data.grad.unit, 'dm')
-% %     cfg.neighbourdist = 0.4; % not used in new version anymore!
-% % elseif strcmp(data.grad.unit, 'm')
-% %     cfg.neighbourdist = 0.04; % not used in new version anymore!
-% % else % make an assumption
-% %     data.grad.unit = 'm'; 
-% %     cfg.neighbourdist = 0.04;
-% % end
-% dataplanar = ft_megplanar(cfg, data);
-% 
-% % timelock and combine planar
-% cfg = [];
-% dataplanar     = ft_timelockanalysis(cfg, dataplanar);
-% dataplanarComb = ft_combineplanar(cfg,dataplanar);
-% 
-% % plotting
-% cfg = [];
-% clf
-% subplot(121)
-% cfg.zlim = 'maxmin';
-% cfg.colorbar = 'yes';
-% ft_topoplotER(cfg,data)
-% colorbar
-% subplot(122)
-% cfg.zlim = 'maxabs';
-% ft_topoplotER(cfg,dataplanarComb)
-% 
-% end
+% compute neighbours
+cfg = [];
+cfg.method =  'distance';
+cfg.neighbours = ft_prepare_neighbours(cfg, avgFIC);
+
+% megplanar
+cfg.planarmethod = 'sincos';
+avgFICplanar = ft_megplanar(cfg, avgFIC);
+
+% timelock and combine planar
+cfg = [];
+avgFICplanar     = ft_timelockanalysis(cfg, avgFICplanar);
+avgFICplanarComb = ft_combineplanar(cfg,avgFICplanar);
+
+
+% check whether maxima and minima on sensor level are the same as in summer
+% 2011
+it = 5; % #iterations
+i = 0;
+while it < i
+    [a, b] = nanmax(nanmax(avgFICplanarComb.avg, [], 2))
+    if (b~=29) || b~= 53 || b~=4 || b~=28 || b~=3
+        error('the global maxima has moved location');
+    else
+        avgFICplanarComb.avg(b, :) = nan;
+    end
+    i = t+1;
+end
+fprintf('global maxima seems all fine\n');
+
+i = 0;
+while it < i
+    [a, b] = nanmin(nanmin(avgFICplanarComb.avg, [], 2))
+    if b~=21|| b~=33 || b~=133 || b~=92 || b~=144
+        error('the global minima has moved location');
+    else
+        avgFICplanarComb.avg(b, :) = nan;
+    end
+    i = t+1;
+end
+fprintf('global minima seems all fine\n');
+
+
+% plotting
+cfg = [];
+clf
+subplot(121)
+cfg.xlim = [0.3 0.5];
+cfg.zlim = 'maxmin';
+cfg.colorbar = 'yes';
+ft_topoplotER(cfg,avgFIC)
+colorbar
+subplot(122)
+cfg.zlim = 'maxabs';
+cfg.layout = 'CTF151.lay';
+ft_topoplotER(cfg,avgFICplanarComb)
+
+fprintf('compare with http://fieldtrip.fcdonders.nl/tutorial/eventrelatedaveraging#plot_the_results_planar_gradients\n');
