36f33d648f6d8d7d8c1672916ccf5e6b064edbf1
Robert Oostenveld <r.oostenveld@gmail.com>
2011-10-21 13:42:41 +0000

    added test script for bug 804
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4560 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug804.m
@@ -0,0 +1,12 @@
+function test_bug804
+
+% TEST test_bug804
+% TEST channeposition ft_datatype_sens yokogawa2grad ft_read_header
+
+cd /home/common/matlab/fieldtrip/data/test/bug804
+
+cfg = [];
+cfg.dataset = 'amano.ave';
+data = ft_preprocessing(cfg);
+
+ft_plot_sens(data.grad);
