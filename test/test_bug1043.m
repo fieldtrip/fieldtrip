a815fda909211c98e8b475521ccd2cbbee3544a4
Robert Oostenveld <r.oostenveld@gmail.com>
2011-10-21 13:12:27 +0000

    bugfix - improved the channelposition function, now also works for the old yokogawa reader. Extended the test script. See also http://bugzilla.fcdonders.nl/show_bug.cgi?id=1043
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4554 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1043.m
+++ test/test_bug1043.m
@@ -1,17 +1,27 @@
 function test_bug1043
 
-% TEST test_bug1043 ft_megplanar ft_apply_montage
+% TEST test_bug1043 
+% TEST ft_megplanar ft_apply_montage yokogawa2grad channelposition
 
-% the bug consists of ft_apply_montage not adequately dealing with 
+% the bug consists of ft_apply_montage not adequately dealing with
 % sensor descriptions that contain coilori/pos chanori/pos.
 % The chanori/chanpos get lost along the way
 
 cd /home/common/matlab/fieldtrip/data/test/raw/meg/
 load preproc_ctf151
 
+cfg        = [];
 cfg.method = 'triangulation';
 neighbours = ft_prepare_neighbours(cfg, data);
-cfg        = [];
-cfg.planarmethod = 'sincos';
-cfg.neighbours   = neighbours;
-planar     = ft_megplanar(cfg, data);
+
+cfg               = [];
+cfg.planarmethod  = 'sincos';
+cfg.neighbours    = neighbours;
+planar            = ft_megplanar(cfg, data);
+
+% this is another test case that failed to work according to the bug report
+
+cd /home/common/matlab/fieldtrip/data/test/bug1043
+cfg = [];
+cfg.dataset = 'chki250110.0100.fs1khz.corr_ch_names-ave.ave';
+data = ft_preprocessing(cfg);
