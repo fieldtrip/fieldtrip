9a0ff18795c1e9f84e75d1593f5e5c59e700017c
Cristiano Micheli <michelic72@googlemail.com>
2011-10-24 13:44:46 +0000

    enhancement - yet another surface function
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4572 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ ft_surfaceretriangulate.m
@@ -0,0 +1,14 @@
+function bnd = ft_surfaceretriangulate(cfg,bnd)
+% FT_SURFACERETIRANGULATE 
+% 
+% Use as
+%   bnd = ft_surfaceretriangulate(cfg,bnd)
+% 
+% See also FT_SURFACECHECK, FT_SURFACEEXTRACT
+% 
+% This function is a placeholder and is going to be filled with useful
+% functionality soon.
+
+% Copyright (C) 2011, Cristiano Micheli, Robert Oostenveld
+% 
+% $Id: $
