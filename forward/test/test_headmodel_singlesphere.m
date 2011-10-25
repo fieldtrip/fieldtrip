b9684f5e5576481a4114d2d94d5a7daa0ab12643
Cristiano Micheli <michelic72@googlemail.com>
2011-10-25 12:19:20 +0000

    bugfix - corrected call to ft_headmodel_singlesphere in the test file
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4588 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/test/test_headmodel_singlesphere.m
+++ forward/test/test_headmodel_singlesphere.m
@@ -46,7 +46,9 @@ for i=1:length(pnt)
   pnt(i,:) = pnt(i,:) / norm(pnt(i,:));
 end
 
-vol = ft_headmodel_singlesphere(pnt, 'conductivity', [0.42]);
+geom = [];
+geom.pnt = pnt;
+vol = ft_headmodel_singlesphere(geom, 'conductivity', [0.42]);
 
 % generate a unit sphere
 [pnt, tri] = icosahedron162;
@@ -65,7 +67,7 @@ end
 lf = ft_compute_leadfield([0 0 50], sens, vol);
 
 % figure;
-% subplot(2,2,1); ft_plot_topo3d(sens.pnt, lf(:,1))
-% subplot(2,2,2); ft_plot_topo3d(sens.pnt, lf(:,2))
-% subplot(2,2,3); ft_plot_topo3d(sens.pnt, lf(:,3))
-% 
+% subplot(2,2,1); ft_plot_topo3d(sens.chanpos, lf(:,1))
+% subplot(2,2,2); ft_plot_topo3d(sens.chanpos, lf(:,2))
+% subplot(2,2,3); ft_plot_topo3d(sens.chanpos, lf(:,3))
+
