899dd962d48aa2d1c9032640081a8710584c9638
Cristiano Micheli <michelic72@googlemail.com>
2011-11-02 10:27:12 +0000

    enahncement - added private nesting function
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4649 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ forward/private/surface_nesting.m
@@ -0,0 +1,43 @@
+function order = surface_nesting(bnd, desired)
+
+% SURFACE_NESTING
+%
+% Note that it does not check for intersections
+
+numboundaries = numel(bnd);
+
+% determine the nesting of the compartments
+nesting = zeros(numboundaries);
+for i=1:numboundaries
+  for j=1:numboundaries
+    if i~=j
+      % determine for a single vertex on each surface if it is inside or outside the other surfaces
+      curpos = vol.bnd(i).pnt(1,:); % any point on the boundary is ok
+      curpnt = vol.bnd(j).pnt;
+      curtri = vol.bnd(j).tri;
+      nesting(i,j) = bounding_mesh(curpos, curpnt, curtri);
+    end
+  end
+end
+
+if sum(nesting(:))~=(numboundaries*(numboundaries-1)/2)
+  error('the compartment nesting cannot be determined');
+end
+
+if desired='outsidefirst'
+  % usually the skin will be the outermost, and this should be the first
+  % for a three compartment model, the nesting matrix should look like
+  %    0 1 1     the first is nested inside the 2nd and 3rd, i.e. the inner skull
+  %    0 0 1     the second is nested inside the 3rd, i.e. the outer skull
+  %    0 0 0     the third is the most outside, i.e. the skin
+  [dum, order] = sort(-sum(nesting,2));
+
+elseif desired='insidefirst'
+  % usually the brain (i.e. the inside skull) will be the innermost, and this should be the first
+  % for a three compartment model, the nesting matrix should look like
+  %    0 0 0     the first is the most outside, i.e. the skin
+  %    0 0 1     the second is nested inside the 3rd, i.e. the outer skull
+  %    0 1 1     the third is nested inside the 2nd and 3rd, i.e. the inner skull
+  [dum, order] = sort(sum(nesting,2));
+end
+
