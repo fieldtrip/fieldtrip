99e5d2de3b32506fa5be85443a36cd4ff7290425
Robert Oostenveld <r.oostenveld@gmail.com>
2011-11-04 08:33:27 +0000

    restructuring - arred warning and immediate return to test_bug298 for keyvalx, because it is not pertinent any more
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4672 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug298.m
+++ test/test_bug298.m
@@ -3,6 +3,9 @@ function test_bug298
 % TEST test_bug298
 % TEST keyval keyvalx
 
+warning('this test has become obsolete because ot the ft_getopt implementation');
+return
+
 keyVals  = {'frequency', 100,'order', 6,'decimation', 4,'steepness', 20,'name' ,'Stefan','matrix', magic(4)};
 keyValsA = {'frequencyA',100,'orderA',6,'decimationA',4,'steepnessA',20,'nameA','Stefan','matrixA',magic(4)};
 keyValsB = {'frequencyB',100,'orderB',6,'decimationB',4,'steepnessB',20,'nameB','Stefan','matrixB',magic(4)};
