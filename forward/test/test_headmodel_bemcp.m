c0d0fe93e8e0835cbeebfe184b8f6a1ba755f9de
Cristiano Micheli <michelic72@googlemail.com>
2011-11-08 15:27:59 +0000

    bugfix - changed chanpos in test file, now working
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4697 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/test/test_headmodel_bemcp.m
+++ forward/test/test_headmodel_bemcp.m
@@ -20,6 +20,7 @@ vol = ft_headmodel_bemcp(geom, 'conductivity', [1 1/20 1]);
 % create a set of electrodes
 sel = find(pnt(:,3)>0);
 sens.elecpos = pnt(sel,:) * 100;
+sens.chanpos = pnt(sel,:) * 100;
 for i=1:length(sel)
   sens.label{i} = sprintf('chan%03d', i);
 end
