92ca33cdec1942d822b0d7782d6fb4c6836a2ffc
Robert Oostenveld <r.oostenveld@gmail.com>
2011-11-17 13:04:35 +0000

    enhancement - added test script for bug 1162
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4765 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1162.m
@@ -0,0 +1,31 @@
+function test_bug1162
+
+% TEST test_bug1162
+% TEST ft_postamble ft_postamble_history ft_freqgrandaverage
+
+timelock1 = [];
+timelock1.label = {'1' '2'};
+timelock1.time  = 1:5;
+timelock1.dimord = 'rpt_chan_time';
+timelock1.avg = randn(2,5);
+timelock1.cfg = 'this is number 1';
+
+timelock2 = [];
+timelock2.label = {'1' '2'};
+timelock2.time  = 1:5;
+timelock2.dimord = 'rpt_chan_time';
+timelock2.avg = randn(2,5);
+timelock2.cfg = 'this is number 2';
+
+cfg = [];
+grandavg = ft_timelockgrandaverage(cfg, timelock1, timelock2);
+
+% grandavg.cfg.previous{1} should be 'this is number 1'
+% grandavg.cfg.previous{2} should be 'this is number 1'
+disp(grandavg.cfg.previous);
+
+assert(iscell(grandavg.cfg.previous));
+assert(~isempty(grandavg.cfg.previous));
+assert(~isempty(grandavg.cfg.previous{1}));
+assert(~isempty(grandavg.cfg.previous{2}));
+
