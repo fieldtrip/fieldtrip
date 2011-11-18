d656409703ac01fc70640b86741102e11d316478
Robert Oostenveld <r.oostenveld@gmail.com>
2011-11-18 14:49:39 +0000

    enhancement - added test script for bug 1168 (multiplot dimord)
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4773 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1168.m
@@ -0,0 +1,99 @@
+function test_bug1168
+
+% TEST test_bug1168
+% TEST ft_multiplotTFR
+
+cfg = [];
+cfg.layout = 'CTF151.lay';
+lay = ft_prepare_layout(cfg);
+
+label = ft_channelselection('MEG', lay.label);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+nchan = length(label);
+nfreq = 50;
+ntime = 100;
+
+freq = [];
+freq.label = label;
+freq.freq = 1:nfreq;
+freq.time = 1:ntime;
+freq.dimord = 'chan_freq_time';
+freq.powspctrm = randn(nchan, nfreq, ntime);
+
+cfg = [];
+cfg.parameter = 'powspctrm';
+cfg.layout = lay;
+ft_multiplotTFR(cfg, freq) % sofar it works
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+freq = [];
+freq.label = label;
+freq.freq = 1:nfreq;
+freq.time = 1:ntime;
+freq.dimord = 'chan_time_freq'; % swap time and freq
+freq.powspctrm = randn(nchan, ntime, nfreq);
+
+cfg = [];
+cfg.parameter = 'powspctrm';
+cfg.layout = lay;
+ft_multiplotTFR(cfg, freq) % this fails, visual inspection is needed to observe the problem
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% the following one is more specifically for Linsey
+
+nchan = length(label);
+nfreq = 1;
+ntime = 100;
+
+freq = [];
+freq.label = label;
+freq.freq = 1:nfreq;
+freq.time = 1:ntime;
+freq.dimord = 'chan_time_freq';
+freq.powspctrm = randn(nchan, nfreq, ntime);
+ft_multiplotTFR(cfg, freq) % this fails, visual inspection is needed to observe the problem
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+if false
+  % the following rightfully fails because the data structure is not recognized as freq structure
+  % but if the data structure would have had a freq dimension, it should have worked
+  nax1 = 50;
+  nax2 = 100;
+  
+  freq = [];
+  freq.label = label;
+  freq.ax1 = 1:nax1;
+  freq.ax2 = 1:nax2;
+  freq.dimord = 'chan_ax1_ax2';
+  freq.powspctrm = randn(nchan, nax1, nax2);
+  
+  cfg = [];
+  cfg.parameter = 'powspctrm';
+  cfg.layout = lay;
+  ft_multiplotTFR(cfg, freq)
+end
+
+if false
+  % the following rightfully fails because ax2 is not known as dimension
+  nfreq = 50;
+  nax2 = 100;
+  
+  freq = [];
+  freq.label = label;
+  freq.freq = 1:nfreq;
+  freq.ax2 = 1:nax2;
+  freq.dimord = 'chan_freq_ax2';
+  freq.powspctrm = randn(nchan, nfreq, nax2);
+  
+  cfg = [];
+  cfg.parameter = 'powspctrm';
+  cfg.layout = lay;
+  ft_multiplotTFR(cfg, freq)
+end
