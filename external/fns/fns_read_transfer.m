cb13367d10b5c0750354819d512d222f0a4fc297
Cristiano Micheli <michelic72@googlemail.com>
2011-11-20 20:36:55 +0000

    enhancement - latest modifications to FNS toolbox
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4780 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- external/fns/fns_read_transfer.m
+++ external/fns/fns_read_transfer.m
@@ -1,12 +1,10 @@
-function forwardSolutions = fns_read_transfer(datafile)
-forwardSolutions = hdf5read(datafile, '/forward/transfer_matrix');
-% forwardSolutions = struct('Data', [], 'Compress', [], ...
-%     'ModelSizes', [], 'VoxelSizes', []);
-% 
-% % Note that Data only stores the potential values for nodes, which are belong to
-% % the head.
-% forwardSolutions.Data       = hdf5read(datafile, '/forward/transfer_matrix');
-% 
+function [trMatrix,elecStatus] = fns_read_transfer(datafile)
+% Note that Data only stores the potential values for nodes, which  belong to
+% the head
+
+trMatrix      = hdf5read(datafile, '/forward/transfer_matrix');
+elecStatus    = hdf5read(datafile, '/forward/status');
+
 % % These parameters are used to compute the lead field matrices.
 % forwardSolutions.Compress   = hdf5read(datafile, '/sparse/compress');
 % forwardSolutions.ModelSizes = hdf5read(datafile, '/model/node_sizes');
--- forward/ft_headmodel_fdm_fns.m
+++ forward/ft_headmodel_fdm_fns.m
@@ -33,7 +33,6 @@ tissueval    = ft_getopt(varargin, 'tissueval', []);
 tissuecond   = ft_getopt(varargin, 'tissuecond', []);
 transform    = ft_getopt(varargin, 'transform', eye(4));
 units        = ft_getopt(varargin, 'units', 'cm');
-% bnd          = ft_getopt(varargin, 'bnd', []);
 sens         = ft_getopt(varargin, 'sens', []);
 deepelec     = ft_getopt(varargin, 'deepelec', []); % used in the case of deep voxel solution
 tolerance    = ft_getopt(varargin, 'tolerance', 1e-8);
@@ -42,8 +41,8 @@ if isempty(sens)
   error('A set of sensors is required')
 end
 
-if ~isunix
-  error('this only works on linux and osx')
+if ispc
+  error('FNS only works on Linux and OSX')
 end
 
 % check the consistency between tissue values and the segmentation
@@ -92,8 +91,10 @@ try
     % write the positions of the electrodes on disk
     disp('writing the electrodes file...')
     pos = warp_apply(inv(transform),sens.chanpos); % in voxel coordinates!
-    fns_elec_write(int32(round(pos)), [1 1 1], size(seg), elecfile); % Hung: convert pos to int32 datatype. 
-
+%     fns_elec_write(int32(round(pos)), [1 1 1], size(seg), elecfile); 
+    % convert pos into int32 datatype. 
+    hdf5write(elecfile, '/electrodes/gridlocs', int32(pos));
+    
     % Exe file 
     efid = fopen(exefile, 'w');
     if ~ispc
@@ -107,7 +108,8 @@ try
     dos(sprintf('chmod +x %s', exefile));
     dos(['./' exefile]);
     
-    transfer = fns_read_transfer(datafile);
+    [transfer,status] = fns_read_transfer(datafile);
+    
     % FIXME : the result of elecsfwd has to be read in a transfer matrix
     % (has to be implemented!)
     cleaner(segfile,confile,elecfile,exefile,datafile)
