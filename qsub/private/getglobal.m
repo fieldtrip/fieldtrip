60d8e5178a650b658e18bdf2c8975da6f5f52a00
Robert Oostenveld <r.oostenveld@gmail.com>
2011-11-23 05:16:05 +0000

    bugfix - solved a problem when the same global varable name would get a different value between calls, see 1145
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4786 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- qsub/private/getglobal.m
+++ qsub/private/getglobal.m
@@ -8,20 +8,8 @@ function var = getglobal
 %
 % See also SETGLOBAL
 
-persistent previous_list previous_varout
-
-list = whos('global');
-
-if isequal(list, previous_list)
-    var = previous_varout;
-    return;
-end
-    
 var = [];
 for i=1:length(list)
   eval(sprintf('global %s', list(i).name));
   var.(list(i).name) = eval(list(i).name);
 end
-
-previous_list = list;
-previous_varout = var;
