8a24d7ba20db7b040a954b8d06e0bc423642fc80
Robert Oostenveld <r.oostenveld@gmail.com>
2011-11-23 05:17:35 +0000

    bugfix - added missing settang function, see bug 1117
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4788 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ inverse/private/settang.m
@@ -0,0 +1,11 @@
+function [MDip] = settang(theta, tanu, tanv);
+
+% set the dipole cartesian direction, given:
+% 1) the instantenious decomposition vectors tanu and tanv
+% 2) the instanteneous dipole orientation theta
+
+u=cos(theta);
+v=sin(theta);
+for ix=1:3
+  MDip(ix)=u*tanu(ix)+v*tanv(ix);
+end;
