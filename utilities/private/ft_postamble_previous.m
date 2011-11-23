64dc2fb2624805b1de5439babcbf2fd2061b451c
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2011-11-23 14:44:01 +0000

    bugfix - also allow to deal with variables that are cell-arrays (varargin) rather than a structure. This addresses bug 1162
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4805 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/private/ft_postamble_previous.m
+++ utilities/private/ft_postamble_previous.m
@@ -10,19 +10,34 @@ global ft_default
 
 % remember the cfg history of the input data structures
 cfg.previous = {};
+cnt = 0;
 for tmpindx=1:length(ft_default.postamble)
   if exist(ft_default.postamble{tmpindx}, 'var')
     tmpvar = eval(ft_default.postamble{tmpindx});
   else
     tmpvar = [];
   end
-  if isfield(tmpvar, 'cfg')
-    cfg.previous{tmpindx} = tmpvar.cfg;
-  else
-    cfg.previous{tmpindx} = [];
+  if isa(tmpvar, 'struct')
+    % the variable is a data structure
+    cnt=cnt+1;
+    if isfield(tmpvar, 'cfg')
+      cfg.previous{cnt} = tmpvar.cfg;
+    else
+      cfg.previous{cnt} = [];
+    end
+  elseif isa(tmpvar, 'cell')
+    % the variable is a cell-array (i.e. most likely a varargin)
+    for cellindx=1:numel(tmpvar)
+      cnt=cnt+1;
+      if isfield(tmpvar{cellindx}, 'cfg')
+        cfg.previous{cnt} = tmpvar{cellindx}.cfg;
+      else
+        cfg.previous{cnt} = [];
+      end
+    end
   end
 end
-clear tmpindx tmpvar
+clear tmpindx tmpvar cellindx cnt
 
 if length(cfg.previous)==1
   % replace the cell-array by the single struct
