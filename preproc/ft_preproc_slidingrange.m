88dd8a99cefea21b1d44e9b939ca29139fe9c597
Eelke Spaak <eelke.spaak@donders.ru.nl>
2011-11-23 09:58:57 +0000

    added reference documentation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4798 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- preproc/ft_preproc_slidingrange.m
+++ preproc/ft_preproc_slidingrange.m
@@ -1,9 +1,15 @@
 function y = ft_preproc_slidingrange(dat, width, varargin)
 
-% FT_PREPROC_SLIDINGRANGE
+% FT_PREPROC_SLIDINGRANGE computes the range of the data in a sliding time
+% window of the width specified. Width should be an odd number (since the
+% window needs to be centered on an individual sample).
 %
 % Use as
 %   y = ft_preproc_slidingrange(dat, width, ...)
+%
+% Optional key-value pair arguments are:
+%   'normalize', whether to normalize the range of the data with the square
+%                root of the window size
 
 normalize = ft_getopt(varargin, 'normalize', false);
 
