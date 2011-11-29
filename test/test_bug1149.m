73effda61f1e702342e00f2fa65ce907538a9c3c
Cristiano Micheli <michelic72@googlemail.com>
2011-11-29 09:53:10 +0000

    bugfix - added test for bug #1149
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4886 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1149.m
@@ -0,0 +1,24 @@
+function test_bug1149
+% TEST test_bug1149
+
+% generate a unit sphere
+[pnt, tri] = icosahedron162;
+
+% create the sphere
+bnd.pnt = pnt * 90;
+bnd.tri = tri;
+
+vol = [];
+vol.bnd = bnd;
+vol.type = 'dipoli';
+
+figure,ft_plot_mesh(bnd,'faceindex','yes')
+figure,ft_plot_mesh(bnd,'faceindex','none')
+figure,ft_plot_mesh(bnd,'facecolor','g')
+figure,ft_plot_mesh(bnd,'facecolor','none')
+
+
+figure,ft_plot_vol(vol,'faceindex','yes')
+figure,ft_plot_vol(vol,'faceindex','none')
+figure,ft_plot_vol(vol,'facecolor','g')
+figure,ft_plot_vol(vol,'facecolor','none')
