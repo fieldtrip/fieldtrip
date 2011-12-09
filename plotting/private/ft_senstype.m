b030b118fa6912f1b72e53b1049f7f53455c0836
Tilmann Sander-Thoemmes <Tilmann.Sander-Thoemmes@ptb.de>
2011-12-09 14:30:27 +0000

    automatically synchronized identical files to revision 4967
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4968 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/private/ft_senstype.m
+++ plotting/private/ft_senstype.m
@@ -265,10 +265,10 @@ else
     elseif any(ismember(ft_senslabel('ctfref'), sens.label))
       type = 'ctf'; % it might be 151 or 275 channels
     elseif isfield(sens, 'pnt') && isfield(sens, 'ori') && numel(sens.label)==size(sens.pnt,1)
-      warning('could be Yokogawa system');
+      warning_once('could be Yokogawa system');
       type = 'magnetometer';
     else
-      warning('could be Yokogawa system');
+      warning_once('could be Yokogawa system');
       type = 'meg';
     end
     
