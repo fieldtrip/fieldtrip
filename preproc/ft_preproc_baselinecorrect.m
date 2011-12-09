fab44eefca8d4b68bf001c6c3cd325381fbaa6fa
Eelke Spaak <eelke.spaak@donders.ru.nl>
2011-12-09 11:53:02 +0000

    changed ft_preproc_baselinecorrect and ft_preproc_detrend into wrappers around ft_preproc_polyremoval (bug 1181) and updated some documentation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@4962 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- preproc/ft_preproc_baselinecorrect.m
+++ preproc/ft_preproc_baselinecorrect.m
@@ -1,4 +1,4 @@
-function [dat, baseline] = ft_preproc_baselinecorrect(dat, begsample, endsample)
+function [dat,baseline] = ft_preproc_baselinecorrect(dat, begsample, endsample)
 
 % FT_PREPROC_BASELINECORRECT performs a baseline correction, e.g. using the
 % prestimulus interval of the data or using the complete data
@@ -13,7 +13,9 @@ function [dat, baseline] = ft_preproc_baselinecorrect(dat, begsample, endsample)
 % If no begin and end sample are specified for the baseline estimate, it
 % will be estimated on the complete data.
 %
-% See also PREPROC
+% This function is a wrapper around FT_PREPROC_POLYREMOVAL.
+%
+% See also FT_PREPROC_POLYREMOVAL
 
 % Copyright (C) 1998-2008, Robert Oostenveld
 %
@@ -35,38 +37,44 @@ function [dat, baseline] = ft_preproc_baselinecorrect(dat, begsample, endsample)
 %
 % $Id$
 
-persistent hasbsxfun
-if isempty(hasbsxfun)
-  hasbsxfun = exist('bsxfun', 'builtin')==5;
-end
+% the beta returned by polyremoval should exactly be equal to the mean,
+% since we assume that polyremoval uses ones as its constant regressor
+[dat,baseline] = ft_preproc_polyremoval(dat,0,begsample,endsample);
 
-% determine the size of the data
-[Nchans, Nsamples] = size(dat);
-
-% determine the interval to use for baseline correction
-if nargin<2 || isempty(begsample)
-  begsample = 1;
-end
-if nargin<3 || isempty(endsample)
-  endsample = Nsamples;
-end
-
-% estimate the baseline and subtract it
-baseline = mean(dat(:,begsample:endsample), 2);
-
-% ensure that the data is not represented as integer, otherwise "minus" fails
-dat = double(dat);
-
-if hasbsxfun
-  % it is even faster to do this
-  dat = bsxfun(@minus,dat,baseline);
-else
-  % it is faster to loop over samples than over channels due to the internal memory representation of Matlab
-  % for chan=1:Nchans
-  %  dat(chan,:) = dat(chan,:) - baseline(chan);
-  % end
-  
-  for sample=1:Nsamples
-    dat(:,sample) = dat(:,sample) - baseline;
-  end
-end
+% save this old code because it looks non-trivially optimized
+%
+% persistent hasbsxfun
+% if isempty(hasbsxfun)
+%   hasbsxfun = exist('bsxfun', 'builtin')==5;
+% end
+% 
+% % determine the size of the data
+% [Nchans, Nsamples] = size(dat);
+% 
+% % determine the interval to use for baseline correction
+% if nargin<2 || isempty(begsample)
+%   begsample = 1;
+% end
+% if nargin<3 || isempty(endsample)
+%   endsample = Nsamples;
+% end
+% 
+% % estimate the baseline and subtract it
+% baseline = mean(dat(:,begsample:endsample), 2);
+% 
+% % ensure that the data is not represented as integer, otherwise "minus" fails
+% dat = double(dat);
+% 
+% if hasbsxfun
+%   % it is even faster to do this
+%   dat = bsxfun(@minus,dat,baseline);
+% else
+%   % it is faster to loop over samples than over channels due to the internal memory representation of Matlab
+%   % for chan=1:Nchans
+%   %  dat(chan,:) = dat(chan,:) - baseline(chan);
+%   % end
+%   
+%   for sample=1:Nsamples
+%     dat(:,sample) = dat(:,sample) - baseline;
+%   end
+% end
--- preproc/ft_preproc_detrend.m
+++ preproc/ft_preproc_detrend.m
@@ -1,7 +1,7 @@
-function [dat, beta, x] = ft_preproc_detrend(dat, begsample, endsample, order)
+function [dat,beta,x] = ft_preproc_detrend(dat, begsample, endsample)
 
-% FT_PREPROC_DETREND removes linear or higher order polynomial trends from the
-% data using using General Linear Modeling
+% FT_PREPROC_DETREND removes mean and linear trend from the
+% data using using General Linear Modeling.
 %
 % Use as
 %   [dat] = ft_preproc_detrend(dat, begin, end, order)
@@ -9,12 +9,13 @@ function [dat, beta, x] = ft_preproc_detrend(dat, begsample, endsample, order)
 %   dat        data matrix (Nchans X Ntime)
 %   begsample  index of the begin sample for the trend estimate
 %   endsample  index of the end sample for the trend estimate
-%   order      number representing the polynomial order (default = 1, i.e. linear)
 %
 % If no begin and end sample are specified for the trend estimate, it
 % will be estimated on the complete data.
 %
-% See also PREPROC
+% This function is a wrapper around FT_PREPROC_POLYREMOVAL.
+%
+% See also FT_PREPROC_POLYREMOVAL
 
 % Copyright (C) 2008, Robert Oostenveld
 %
@@ -36,29 +37,4 @@ function [dat, beta, x] = ft_preproc_detrend(dat, begsample, endsample, order)
 %
 % $Id$
 
-% determine the size of the data
-[Nchans, Nsamples] = size(dat);
-
-% determine the interval to use for baseline correction
-if nargin<2 || isempty(begsample)
-  begsample = 1;
-end
-if nargin<3 || isempty(endsample)
-  endsample = Nsamples;
-end
-
-% determine the order of the polynomial trend to be removed, default is linear
-if nargin<4 || isempty(order)
-  order = 1;
-end
-
-% create a matrix with regressor components
-basis    = 1:Nsamples;
-x        = zeros(order+1,Nsamples);
-for i=0:order
-  x(i+1,:) = basis.^(i);
-end
-% estimate the polynomial trend using General Linear Modeling, where dat=beta*x+noise
-beta = dat(:,begsample:endsample)/x(:,begsample:endsample);
-% subtract the trend from the complete data
-dat  = dat - beta*x;
+[dat,beta,x] = ft_preproc_polyremoval(dat,1,begsample,endsample);
