b55657813edf912779ba63ae892c42281b88b541
Martin Vinck <martinvinck@gmail.com>
2011-12-10 11:28:26 +0000

    test_bug1207.m added
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5000 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- contrib/spike/test/test_bug1207.m
+++ contrib/spike/test/test_bug1207.m
@@ -1,9 +1,7 @@
-
 % test_bug1207.m
-
-cd /contrib/spike
 
 % TT5.ntt in this directory
 % TT5_1.t in this directory
 
-% 
+spike1 = ft_read_spike('/contrib/spike/test/TT5.ntt');
+spike2 = ft_read_spike('/contrib/spike/test/TT5_1.t');
