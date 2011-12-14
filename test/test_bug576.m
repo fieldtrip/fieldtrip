a7e498907fd95778e133a4bdfa9ef4ba9d60720f
Robert Oostenveld <r.oostenveld@gmail.com>
2011-12-14 11:18:22 +0000

    enhancement - added a test script for the neuromag122/306 data confusion, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=576
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5039 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug576.m
@@ -0,0 +1,47 @@
+function test_bug576
+
+% TEST test_bug576
+% TEST ft_checkdata ft_senstype
+
+dataset122 = {
+  '/home/common/matlab/fieldtrip/data/test/raw/meg/preproc_neuromag122.mat'
+  '/home/common/matlab/fieldtrip/data/test/raw/meg/old/preproc_10trials_neuromag122.mat'
+  '/home/common/matlab/fieldtrip/data/test/comp/meg/comp_neuromag122.mat'
+  '/home/common/matlab/fieldtrip/data/test/freq/meg/freq_mtmfft_neuromag122.mat'
+  '/home/common/matlab/fieldtrip/data/test/freq/meg/freq_mtmconvol_neuromag122.mat'
+  '/home/common/matlab/fieldtrip/data/test/freq/meg/old/freq_neuromag122.mat'
+  '/home/common/matlab/fieldtrip/data/test/timelock/meg/timelock_neuromag122.mat'
+  };
+
+dataset306 = {
+  '/home/common/matlab/fieldtrip/data/test/raw/meg/preproc_neuromag306.mat'
+  '/home/common/matlab/fieldtrip/data/test/raw/meg/old/preproc_10trials_neuromag306.mat'
+  '/home/common/matlab/fieldtrip/data/test/comp/meg/comp_neuromag306.mat'
+  '/home/common/matlab/fieldtrip/data/test/freq/meg/freq_mtmfft_neuromag306.mat'
+  '/home/common/matlab/fieldtrip/data/test/freq/meg/freq_mtmconvol_neuromag306.mat'
+  '/home/common/matlab/fieldtrip/data/test/freq/meg/old/freq_neuromag306.mat'
+  '/home/common/matlab/fieldtrip/data/test/timelock/meg/timelock_neuromag306.mat'
+  };
+
+
+for i=1:length(dataset122)
+  filename = dataset122{i};
+  tmp = load(filename);
+  fn = fieldnames(tmp);
+  for j=1:length(fn)
+    data = tmp.(fn{j});
+    ft_checkdata(data, 'senstype', 'neuromag122');
+  end % for j
+end % for i
+
+
+for i=1:length(dataset306)
+  filename = dataset306{i};
+  tmp = load(filename);
+  fn = fieldnames(tmp);
+  for j=1:length(fn)
+    data = tmp.(fn{j});
+    ft_checkdata(data, 'senstype', 'neuromag122');
+  end % for j
+end % for i
+
