e724d2031c6f7f502b59f912c13465bc802bdfbd
Johanna Zumer <j.zumer@donders.ru.nl>
2011-12-14 17:17:16 +0000

    enhancement, bug 1205. randomseed function allows to remember and reset state for random numbers, to replicate output of functions that use rand/n/i
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5044 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_randomseed.m
@@ -0,0 +1,49 @@
+function test_randomseed
+
+% TEST test_randomseed
+
+% this is related to bug 1205
+% call this for every possible MATLAB version
+
+state1=randomseed([]);
+x=rand;
+randomseed(state1);
+y=rand;
+if x~=y, error('1'), end
+
+state2=randomseed(state1);
+z=rand;
+randomseed(state2);
+a=rand;
+if z~=y, error('2'), end
+if z~=a, error('3'), end
+
+state1=randomseed(4);
+x=rand;
+state2=randomseed(state1);
+y=rand;
+if x~=y, error('4'), end
+
+state1=randomseed(state2);
+z=rand;
+state2=randomseed(state1);
+a=rand;
+if z~=y, error('5'), end
+if z~=a, error('6'), end
+
+
+try
+    state3=randomseed([5 6]);
+    error('this should not work')
+catch 
+end
+
+try
+    state3=randomseed('no');
+    error('this should not work')
+catch 
+end
+
+
+
+
