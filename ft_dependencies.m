1674df763ea45976dd4781568f46fafc747ee711
Boris Reuderink <b.reuderink@gmail.com>
2012-01-04 14:43:05 +0000

    enhancement - added missing and removed non-exisitent mex-files in ft_dependencies.m
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5094 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_dependencies.m
+++ ft_dependencies.m
@@ -31,59 +31,57 @@ function items = ft_dependencies(cfg)
 %
 % $Id: ft_compile_mex.m 5057 2011-12-20 14:03:21Z jansch $
 
-items        = mex_item('fileio/@uint64','abs');
-items(end+1) = mex_item('fileio/@uint64','min');
-items(end+1) = mex_item('fileio/@uint64','max');
-items(end+1) = mex_item('fileio/@uint64','plus');
-items(end+1) = mex_item('fileio/@uint64','minus');
-items(end+1) = mex_item('fileio/@uint64','times');
-items(end+1) = mex_item('fileio/@uint64','rdivide');
 
-items(end+1) = mex_item('@config/private','deepcopy');
+% Mex files can be found using:
+% `find . -name "*.c" | xargs grep -l mexFunction | sort`
+% TODO: what should we do with external mex-files?
+
+warning('This function is not finished yet!');
+
+items        = mex_item('@config/private','deepcopy');
 items(end+1) = mex_item('@config/private','increment');
 items(end+1) = mex_item('@config/private','reset');
-
+items(end+1) = mex_item('fileio/@uint64','abs');
+items(end+1) = mex_item('fileio/@uint64','max');
+items(end+1) = mex_item('fileio/@uint64','min');
+items(end+1) = mex_item('fileio/@uint64','minus');
+items(end+1) = mex_item('fileio/@uint64','plus');
+items(end+1) = mex_item('fileio/@uint64','rdivide');
+items(end+1) = mex_item('fileio/@uint64','times');
+items(end+1) = mex_item('src','det2x2');
 items(end+1) = mex_item('src','ft_getopt');
-items(end+1) = mex_item('src','keyval');
+items(end+1) = mex_item('src','ft_spike_sub_crossx');
+items(end+1) = mex_item('src','getpid');
+items(end+1) = mex_item('src','inv2x2');
+items(end+1) = mex_item('src','meg_leadfield1');
+items(end+1) = mex_item('src','mtimes2x2');
+items(end+1) = mex_item('src','mxDeserialize');
+items(end+1) = mex_item('src','mxSerialize');
+items(end+1) = mex_item('src','nanmean');
+items(end+1) = mex_item('src','nanstd');
+items(end+1) = mex_item('src','nanvar');
+items(end+1) = mex_item('src','nansum');
+items(end+1) = mex_item('src','plgndr');
 items(end+1) = mex_item('src','read_16bit');
 items(end+1) = mex_item('src','read_24bit');
-items(end+1) = mex_item('src','mxSerialize');
-items(end+1) = mex_item('src','mxDeserialize');
-items(end+1) = mex_item('src','read_ctf_shm', 'matchPlatform', {'GitemsNX86'});
-items(end+1) = mex_item('src','write_ctf_shm', 'matchPlatform', {'GitemsNX86'});
-
-items(end+1) = mex_item('src','lmoutr', 'extras', 'geometry.c -I.');
-items(end+1) = mex_item('src','ltrisect', 'extras' ,'geometry.c -I.');
+items(end+1) = mex_item('src','rename');
+items(end+1) = mex_item('src','sandwich2x2');
+items(end+1) = mex_item('src','splint_gh');
+items(end+1) = mex_item('src','lmoutr'  , 'extras', 'geometry.c -I.');
+items(end+1) = mex_item('src','ltrisect', 'extras', 'geometry.c -I.');
 items(end+1) = mex_item('src','plinproj', 'extras' ,'geometry.c -I.');
 items(end+1) = mex_item('src','ptriproj', 'extras' ,'geometry.c -I.');
 items(end+1) = mex_item('src','routlm', 'extras','geometry.c -I.');
 items(end+1) = mex_item('src','solid_angle', 'extras','geometry.c -I.');
-items(end+1) = mex_item('src','rfbevent', ...
-	'excludePlatform',{'PCWIN', 'PCWIN64'}, ...
-	'extras', 'd3des.c -I.');
-
-items(end+1) = mex_item('src','meg_leadfield1');
-items(end+1) = mex_item('src','splint_gh');
-items(end+1) = mex_item('src','plgndr');
-
-items(end+1) = mex_item('src','ft_spike_sub_crossx');
-items(end+1) = mex_item('src','rename');
-items(end+1) = mex_item('src','getpid');
-
-items(end+1) = mex_item('src','nanmean');
-items(end+1) = mex_item('src','nanstd');
-items(end+1) = mex_item('src','nansum');
-items(end+1) = mex_item('src','nanstd');
-items(end+1) = mex_item('src','det2x2');
-items(end+1) = mex_item('src','inv2x2');
-items(end+1) = mex_item('src','mtimes2x2');
-items(end+1) = mex_item('src','sandwich2x2');
+items(end+1) = mex_item('src','rfbevent', 'excludePlatform',{'PCWIN', 'PCWIN64'}, 'extras', 'd3des.c -I.');
+items(end+1) = mex_item('src','read_ctf_shm', 'matchPlatform', {'GitemsNX86'});
+items(end+1) = mex_item('src','write_ctf_shm', 'matchPlatform', {'GitemsNX86'});
 
 
-function item  = mex_item(directory, relName, varargin)
+function item = mex_item(directory, relName, varargin)
 p = inputParser;
-p.addRequired('directory'       , @ischar);
-p.addRequired('relName'         , @ischar);
+p.addRequired('directory'         , @ischar);
+p.addRequired('relName'           , @ischar);
 p.addParamValue('matchPlatform'   , []        , @iscellstr);
 p.addParamValue('excludePlatform' , []        , @iscellstr);
 p.addParamValue('extras'          , ''        , @ischar);
