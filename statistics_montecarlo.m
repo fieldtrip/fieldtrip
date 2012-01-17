ed911fe98393f569e7b10882948c28cc448d4e9e
Eelke Spaak <eelke.spaak@donders.ru.nl>
2012-01-17 10:45:39 +0000

    added documentation for cfg.clusterstatistic='wcm' to statistics_montecarlo (bug 1259)
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5142 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- statistics_montecarlo.m
+++ statistics_montecarlo.m
@@ -34,6 +34,10 @@ function [stat, cfg] = statistics_montecarlo(cfg, dat, design, varargin)
 % statistics will be thresholded and combined into one statistical
 % value per cluster.
 %   cfg.clusterstatistic = how to combine the single samples that belong to a cluster, 'maxsum', 'maxsize', 'wcm' (default = 'maxsum')
+%                          option 'wcm' refers to 'weighted cluster mass',
+%                          a statistic that combines cluster size and
+%                          weight; see Hayasaka & Nichols (2004) NeuroImage
+%                          for details
 %   cfg.clusterthreshold = method for single-sample threshold, 'parametric', 'nonparametric_individual', 'nonparametric_common' (default = 'parametric')
 %   cfg.clusteralpha     = for either parametric or nonparametric thresholding per tail (default = 0.05)
 %   cfg.clustercritval   = for parametric thresholding (default is determined by the statfun)
