5f50d67e6dfca87ab41d25b9f149f0e37acb2486
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-01-25 09:15:20 +0000

    documentation change - improved documentation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5173 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_networkanalysis.m
+++ ft_networkanalysis.m
@@ -1,33 +1,36 @@
 function [stat] = http://www.brain-connectivity-toolbox.net(cfg, data)
 
-% FT_NETWORKANALYSIS computes various network graph measures from between-channel
-% or between source-level EEG/MEG signals. This function acts as a wrapper aroun
-% the network metrics implemented in the brain connectivity toolbox developed by
-% Olaf Sporns and colleagues.
+% FT_NETWORKANALYSIS computes various network graph measures from
+% between-channel or between source-level EEG/MEG signals. This function
+% acts as a wrapper aroun the network metrics implemented in the brain
+% connectivity toolbox developed by Olaf Sporns and colleagues.
 %
 % Use as
 %   stat = ft_networkanalysis(cfg, data)
 %
-% where the first input argument is a configuration structure (see
-% below) and the second argument is the output of FT_CONNECTIVITYANALYSIS.
+% where the first input argument is a configuration structure (see below)
+% and the second argument is the output of FT_CONNECTIVITYANALYSIS.
 %
 % At present the input data should be channel-level data with dimord
-% 'chan_chan(_freq)(_time)' or source data with dimord 'pos_pos(_freq)(_time)'.
+% 'chan_chan(_freq)(_time)' or source data with dimord
+% 'pos_pos(_freq)(_time)'.
 %
 % The configuration structure has to contain
-%   cfg.method  = string, can be 'clustering_coef', 'degrees'
+%   cfg.method    = string, specifying the graph measure that will be
+%                   computed. See below for a list of supported measures. 
+%   cfg.parameter = string specifying the bivariate parameter in the data 
+%                   for which the graph measure will be computed.
 %
-% Additional configuration options are
-%   cfg.parameter   = string specifying the metric
+% Supported methods are:
+%   assortatitivity, betweenness, clustering_coef, degrees
 %
-% To facilitate data-handling and distributed computing with the peer-to-peer
-% module, this function has the following options:
-%   cfg.inputfile   =  ...
-%   cfg.outputfile  =  ...
-% If you specify one of these (or both) the input data will be read from a *.mat
-% file on disk and/or the output data will be written to a *.mat file. These mat
-% files should contain only a single variable, corresponding with the
-% input/output structure.
+% To facilitate data-handling and distributed computing with the
+% peer-to-peer module, this function has the following options:
+%   cfg.inputfile   =  ... cfg.outputfile  =  ...
+% If you specify one of these (or both) the input data will be read from a
+% *.mat file on disk and/or the output data will be written to a *.mat
+% file. These mat files should contain only a single variable,
+% corresponding with the input/output structure.
 %
 % See also FT_CONNECTIVITYANALYSIS, FT_CONNECTIVITYPLOT
 
