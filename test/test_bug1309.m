6d22e5e020b0bc4901163b8c79631640e61c5be9
Cristiano Micheli <michelic72@googlemail.com>
2012-02-15 10:24:47 +0000

    bugfix - this refers to bug 1309, fixed the elseif ladder to convert the inputs
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5283 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1309.m
@@ -0,0 +1,27 @@
+function test_bug1309
+
+% TEST test_bug1309
+
+% This tests for the reliability of the ft_convert_units function when dealing
+% with different input types (vol,sens,etc.)
+
+ft_defaults;
+% artifically create timelock data
+timelock_data.fsamepl = 500;
+timelock_data.dimord = 'chan_time';
+timelock_data.time = zeros(1, 500);
+timelock_data.avg = zeros(31, 500);
+for i = 1:31
+    timelock_data.label{i} = sprintf('label%d', i);
+end
+
+cfg = [];
+cfg.elec = ft_read_sens('standard_1020.elc');
+cfg.vol = ft_read_vol('standard_bem.mat');
+cfg.grid.resolution = 3;
+cfg.model = 'regional';
+cfg.latency = [0.14,0.17];
+cfg.numdipoles = 1;
+timelock_data.label = cfg.elec.label(1:31);
+
+dip = ft_dipolefitting(cfg, timelock_data);
