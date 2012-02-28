68b6fe0d5bbe53e7d2e108f9e3f136da644ca8c7
Robert Oostenveld <r.oostenveld@gmail.com>
2012-02-28 21:18:42 +0000

    enhancement - I found a test script for bug 1150, better have it in svn than on my local disk
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5327 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1150.m
@@ -0,0 +1,19 @@
+function test_bug1150
+
+% TEST test_bug1150
+% TEST ft_sourcestatistics
+
+load /home/common/matlab/fieldtrip/data/test/bug1150.mat
+
+% the following failed
+% stat = ft_sourcestatistics(cfg, temp);
+% which could be tracked down to the dimord being incorrect after
+% ft_checkdata
+
+sourcenew = ft_checkdata(temp, 'sourcerepresentation', 'new');
+if ~strcmp(sourcenew.powdimord, 'pos_rpt')
+  error('incorrect posdimord');
+end
+
+
+
