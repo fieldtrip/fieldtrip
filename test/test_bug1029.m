c66a56407847b626e4e36234766172364e16e2db
Cristiano Micheli <michelic72@googlemail.com>
2012-02-29 23:50:23 +0000

    bugfix - added testfile for bug #1029
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5355 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1029.m
@@ -0,0 +1,45 @@
+function test_bug1029
+%TEST test_bug1029 ft_read_vol ft_headmodel_bem_asa ft_prepare_headmodel
+% 
+% tests the functionality of the forward models after changing the names of
+% the headmodels
+
+%%%%%%%%%%%%%%%%
+% BEM_ASA
+%%%%%%%%%%%%%%%%
+filename = '/home/common/matlab/fieldtrip/template/headmodel/skin/standard_skin_1222.vol';
+vol = ft_read_vol(filename);
+
+vol = ft_headmodel_bem_asa(filename);
+
+cfg = [];
+cfg.method = 'asa';
+cfg.hdmfile = filename;
+vol = ft_prepare_headmodel(cfg);
+
+%%%%%%%%%%%%%%%%
+% BEMCP
+%%%%%%%%%%%%%%%%
+% create the BEM geometry
+[pnt, tri] = icosahedron162;
+geom = [];
+geom.bnd(1).pnt = pnt * 100;
+geom.bnd(1).tri = tri;
+geom.bnd(2).pnt = pnt * 90;
+geom.bnd(2).tri = tri;
+geom.bnd(3).pnt = pnt * 80;
+geom.bnd(3).tri = tri;
+
+vol = ft_headmodel_bemcp(geom, 'conductivity', [1 1/80 1]);
+
+cfg=[];
+cfg.method = 'bemcp';
+cfg.geom = geom;
+cfg.conductivity = [1 1/80 1];
+vol = ft_prepare_headmodel(cfg);
+
+%%%%%%%%%%%%%%%%
+% DIPOLI
+%%%%%%%%%%%%%%%%
+vol = ft_headmodel_bem_dipoli(geom, 'conductivity', [1 1/20 1]);
+
