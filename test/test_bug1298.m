ad513046f47b074e76395ad555e2ec40e9d05d56
Johanna Zumer <j.zumer@donders.ru.nl>
2012-02-29 15:19:16 +0000

    example code for bug1298
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5338 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1298.m
@@ -0,0 +1,35 @@
+% test_bug1298
+
+megraw=load('/home/common/matlab/fieldtrip/data/ftp/tutorial/beamformer/dataFIC.mat')
+
+cfg=[];
+cfg.covariance='yes';
+cfg.keeptrials='yes';
+megtlock=ft_timelockanalysis(cfg,megraw.dataFIC);
+
+load /home/common/matlab/fieldtrip/data/test/latest/vol/Subject01vol_localspheres.mat
+
+cfg=[];
+cfg.vol=vol;
+grid=ft_prepare_leadfield(cfg,megtlock);
+
+cfg=[];
+cfg.vol=vol;
+cfg.method='lcmv';
+cfg.grid=grid;
+cfg.keepleadfield='yes';
+cfg.lcmv.keepfilter='yes';
+megsource1=ft_sourceanalysis(cfg,megtlock);
+
+cfg=[];
+cfg.vol=vol;
+cfg.method='lcmv';
+cfg.grid=grid;
+cfg.grid.leadfield=megsource1.leadfield;
+% cfg.grid.filter=megsource1.avg.filter;
+cfg.keeptrials='yes';
+cfg.rawtrial='yes';
+megsource11=ft_sourceanalysis(cfg,megtlock);
+
+% and then single trial estimates are in megsource11.trial
+
