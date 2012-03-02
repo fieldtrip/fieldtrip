c01f37114452238d44dc0fc29f8b67991036ff3f
Robert Oostenveld <r.oostenveld@gmail.com>
2012-03-02 16:11:40 +0000

    bugfix - the previous commit was an empty test file, now it contains the actual lines of code that cause the (by now fixed) crash of the function
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5381 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1348.m
+++ test/test_bug1348.m
@@ -3,4 +3,8 @@ function test_bug1348
 % TEST test_bug1348
 % TEST ft_read_header ft_read_data loadcnt
 
-filename = '/home/common/matlab/fieldtrip/data/test/bug1348/E3a.cnt'
+filename = '/home/common/matlab/fieldtrip/data/test/bug1348/E3a.cnt';
+
+hdr = ft_read_header(filename);
+disp(hdr);
+
