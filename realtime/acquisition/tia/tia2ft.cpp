be4d3547f8ccc59632832e6369effaf427383e89
Boris Reuderink <b.reuderink@gmail.com>
2012-03-09 09:55:05 +0000

    enhancement - refactored tia2ft and added linking with libbuffer.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5422 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- realtime/acquisition/tia/tia2ft.cpp
+++ realtime/acquisition/tia/tia2ft.cpp
@@ -4,21 +4,84 @@
 
 // STL
 #include <iostream>
-#include <algorithm>
 
 // Boost
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
-#include <boost/thread.hpp>
-#include <boost/cstdint.hpp>
 
-// local
+// TiA
 #include "tia/data_packet_interface.h"
 #include "tia/tia_client.h"
 #include "tia/ssconfig.h"
 #include "tia/defines.h"
 
+// FieldTrip buffer
+extern "C" { // TODO: is this needed?
+#include "buffer.h"
+}
+
 using namespace std;
+
+bool connect_tia_client(tia::TiAClient &client, const string srv_addr, 
+  int srv_port) {
+  // connect to TiA server
+  try {
+    client.connect(srv_addr, srv_port);
+    client.requestConfig();
+  } 
+  catch(std::exception &e) {
+    cerr << e.what() << endl;
+    return 1;
+  }
+}
+
+
+/* Starts FieldTrip buffer server on this host. */
+void start_ft_buffer(int port) {
+  // TODO: start in separate thread
+  host_t host;
+  host.port = port;
+  check_datatypes();  // sanity check for sizes of different data types.
+  tcpserver((void *)(&host));  // start server
+}
+
+
+/* Read meta information (channel configuration etc.) from TiA, and write this
+ * to a FieldTrip buffer. So far, it only *prints* the meta info. */
+int sync_meta_info(tia::TiAClient &tia_client, void *ft_buffer) {
+  // Request TiA config:
+  try {
+    tia_client.requestConfig();
+  } 
+  catch (std::exception &e) {
+    cerr << "Requesting config failed -- Error:" << "--> " << e.what() << endl;
+    return 1;
+  }
+
+  tia::SignalInfo sigInfo = tia_client.config().signal_info;
+
+  // Print some signal statistics
+  cout << "Detected the following meta information:" << endl;
+  cout << "Sampling rate: " << sigInfo.masterSamplingRate() << endl;
+  cout << "Block size: " << sigInfo.masterBlockSize() << endl;
+
+  tia::SignalInfo::SignalMap::iterator i;
+  for(i = sigInfo.signals().begin(); i != sigInfo.signals().end(); ++i) {
+    // i contains (string, Signal) pairs
+    cout << i->first << endl;
+
+    tia::Signal signal (i-> second);
+    cout << "Signal (modality) fs: " << signal.samplingRate() << endl;
+
+    std::vector<tia::Channel> channels = signal.channels();
+    cout << "Detected " << channels.size() << " channels:" << endl;
+    for(int i = 0; i < channels.size(); ++i) {
+      cout << channels[i].id() << (i < channels.size() - 1 ? ", " : "");
+    }
+  }
+  cout << "." << endl;  // end enumeration.
+
+  // TODO: Send info to FT-buffer
+}
 
 int main(int argc, const char *argv[])
 {
@@ -31,13 +94,14 @@ int main(int argc, const char *argv[])
     5) start streaming, put data in buffer
 
     TODO:
-    [ ] add error handling
-    [ ] consider removing thread
+    [ ] add (consistent) error handling
+    [V] consider removing thread
     [ ] should we support UDP? -> I think not
   */
 
   string srv_addr = "127.0.0.1";
   boost::uint16_t srv_port = 9000;
+  bool host_ft_buffer = false;
 
   // parse command line
   if(argc == 1) {
@@ -61,67 +125,27 @@ int main(int argc, const char *argv[])
     return(-1);
   }
 
-  tia::TiAClient client(true);
+  // Initialize connections
+  tia::TiAClient client(true);  // use new-style TiA implementation
+  connect_tia_client(client, srv_addr, srv_port);
+  cout << "Connected to TiA server." << endl;
 
-  // connect to TiA server
-  try {
-    client.connect(srv_addr, srv_port);
-    client.requestConfig();
-    cout << "Connected to TiA server." << endl;
-  } catch(std::exception &e) {
-    cerr << e.what() << endl;
-    return 1;
-  }
-
-  // TODO: connect to FieldTrip buffer
-
-#ifdef WIN32
-  SetPriorityClass(GetCurrentProcess(), ABOVE_NORMAL_PRIORITY_CLASS);
-  SetPriorityClass(reader_thread.native_handle(), REALTIME_PRIORITY_CLASS);
-  SetThreadPriority(reader_thread.native_handle(), THREAD_PRIORITY_TIME_CRITICAL );
-#endif
-
-  // Request TiA config:
-  try {
-    client.requestConfig();
-    tia::SignalInfo sigInfo = client.config().signal_info;
-
-    // Print some signal statistics
-    cout << "Sampling rate: " << sigInfo.masterSamplingRate() << endl;
-    cout << "Block size: " << sigInfo.masterBlockSize() << endl;
-
-    tia::SignalInfo::SignalMap sigMap = sigInfo.signals();
-    tia::SignalInfo::SignalMap::iterator i;
-    for(i = sigMap.begin(); i != sigMap.end(); ++i) {
-      // i contains (string, Signal) pairs
-      cout << i->first << endl;
-
-      tia::Signal signal (i-> second);
-      cout << "Signal (modality) fs: " << signal.samplingRate() << endl;
-
-      std::vector<tia::Channel> channels = signal.channels();
-      cout << "Detected " << channels.size() << " channels:" << endl;
-      for(int i = 0; i < channels.size(); ++i) {
-        cout << channels[i].id() << (i < channels.size() - 1 ? ", " : "");
-      }
-    }
-    cout << "." << endl;
-  } catch (std::exception &e) {
-    cerr << "Requesting config failed -- Error:" << "--> " << e.what() << endl;
-  }
+  if (host_ft_buffer)
+    start_ft_buffer(1972);
 
   // Start receiving:
   client.startReceiving(false);  // use TCP
   tia::DataPacket *packet = client.getEmptyDataPacket();
 
+  // TODO connect to FT buffer
+
+  sync_meta_info(client, NULL);
+
   // Main loop
   while (true) {
     cout << "Getting packet..." << flush;
     client.getDataPacket(*packet);
-    cout << "yay!" << endl;
 
-
-    // TODO learn to use and inspect packet.
     cout << "#: " << packet->getConnectionPacketNr() << endl;
     cout << "Flags: " << packet->getFlags() << endl;
     cout << "time: " << packet->getTimestamp() << endl;
