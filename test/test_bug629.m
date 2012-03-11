484c140914cfdd8184f592f5e8dae9fa4f4a579d
Robert Oostenveld <r.oostenveld@gmail.com>
2012-03-11 12:11:16 +0000

    enhancement - added test script, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=629
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5436 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug629.m
@@ -0,0 +1,19 @@
+% function test_bug629
+
+% TEST test_bug629
+% TEST ft_read_header ft_read_data ft_read_event read_mff_header read_mff_data
+
+% note that read_mff_event does not exist, handling of the events is coded in ft_read_event
+
+datadir = '/Volumes/Data/roboos/data/dataformat/testdata/egi/egi_mff Ingrid';
+
+if ~isdir(datadir)
+  warning('the test script could not run because the test data is not present');
+  return
+end
+
+cd(datadir);
+dataset = 'pilot05_test 20110120 1433.mff';
+
+hdr   = ft_read_header(dataset);
+event = ft_read_event(dataset);
