21e2ea0e178b22d1c40a6ec27651ce5b46b123da
Jorn Horschig <jm.horschig@donders.ru.nl>
2012-03-14 11:25:26 +0000

    enhancement-#1067-script that outputs cfg fieldnames in source* functions that are not used in freq* functions and vice versa
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5463 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1067.m
@@ -0,0 +1,98 @@
+function test_bug1067
+% tests whether parameters that are in ft_freq* but not in ft_source* will
+% be caught by ft_source* (and vice versa)
+
+
+
+% get all functions
+source_functions = dir(fullfile('..','ft_source*.m'));
+freq_functions = dir(fullfile('..','ft_freq*.m'));
+
+% get function names
+postfix = '.m';
+source_prefix = 'ft_source';
+source_fnc_names = cell(numel(source_functions), 1);
+for i=1:numel(source_functions)
+  source_fnc_names{i} = source_functions(i).name(numel(source_prefix)+1:end-numel(postfix));
+end
+
+freq_prefix = 'ft_freq';
+freq_fnc_names = cell(numel(freq_functions), 1);
+for i=1:numel(freq_functions)
+  freq_fnc_names{i} = freq_functions(i).name(numel(freq_prefix)+1:end-numel(postfix));
+end
+
+% find common functions
+freq_idx = find(ismember(freq_fnc_names, source_fnc_names));
+source_idx = find(ismember(source_fnc_names, freq_fnc_names));
+num_fncs = numel(freq_idx);
+
+% loop through all functions
+cfg_prefix = 'cfg.';
+for i=1:num_fncs
+  % open files
+  s_fid = fopen(fullfile('..', source_functions(source_idx(i)).name));
+  f_fid = fopen(fullfile('..', freq_functions(freq_idx(i)).name));
+  
+  s_content = fscanf(s_fid, '%c');
+  f_content = fscanf(f_fid, '%c');
+    
+  fclose(s_fid); % we don't need that anymore
+  fclose(f_fid); % we don't need that anymore
+  
+  % find cfg.* for each function
+  s_cfg = strfind(s_content, cfg_prefix);
+  f_cfg = strfind(f_content, cfg_prefix);
+  
+  % find options per function (hashmap like)
+  s_opts = [];
+  s_hash = [];
+  delimiters = '=.%(,)\t '';/*-+{}<>[]';
+  for j=1:numel(s_cfg)
+    s_opts = s_content(s_cfg(j)+numel(cfg_prefix):end);
+    s_opts = textscan(s_opts, '%s', 'delimiter', delimiters);
+    if isempty(s_opts{1}{1})
+      continue;
+    end
+    s_hash.(s_opts{1}{1}) = true;
+  end  
+
+  f_opts = [];
+  f_hash = [];
+  for j=1:numel(f_cfg)
+    f_opts = f_content(f_cfg(j)+numel(cfg_prefix):end);
+    f_opts = textscan(f_opts, '%s', 'delimiter', delimiters);
+    if isempty(f_opts{1}{1})
+      continue;
+    end
+    f_hash.(f_opts{1}{1}) = true;
+  end
+  
+  % find the not common options
+  s_opts = fieldnames(s_hash);
+  f_opts = fieldnames(f_hash);
+  
+  % options ft_source* uses but not ft_freq*
+  s_opts = s_opts(~ismember(s_opts, f_opts));
+  
+  % options ft_freq* uses but not ft_source*
+  f_opts = f_opts(~ismember(f_opts, s_opts));
+  
+  % output
+  fprintf('\n');
+  fprintf('Function %s uses the following fields that %s does not use:\n', ...
+    source_functions(source_idx(i)).name, ...
+    freq_functions(freq_idx(i)).name);
+  for j=1:numel(s_opts)
+    fprintf('\t cfg.%s\n', s_opts{j});
+  end
+ fprintf('\n');
+
+  fprintf('Function %s uses the following fields that %s does not use:\n', ...
+    freq_functions(freq_idx(i)).name, ...
+    source_functions(source_idx(i)).name);
+  for j=1:numel(f_opts)
+    fprintf('\t cfg.%s\n', f_opts{j});
+  end
+
+end
