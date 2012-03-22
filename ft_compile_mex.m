dbb7d7d32f30c4b903a757272fd77e10e6c8f61b
Robert Oostenveld <r.oostenveld@gmail.com>
2012-03-22 09:31:58 +0000

    restructuring - updated ft_compile_mex helper function. Recompiled the windows 32 bit mex files on FCDC273 with MATLAB 2009a and LCC compiler (for most) or MSVC 2008 (for some), see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1384
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5511 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_compile_mex.m
+++ ft_compile_mex.m
@@ -1,20 +1,24 @@
 function ft_compile_mex(force)
 
-% FT_COMPILE_MEX is used for compiling most of the MEX files that are used in FieldTrip
+% FT_COMPILE_MEX can be used for compiling most of the FieldTrip MEX files
+% Note that this function does not put the MEX files in the correct location,
+% this is managed by SVN autosync.
 %
-% Please note that this script does NOT set up your MEX environment for you, so in case
-% you haven't selected the C compiler on Windows yet, you need to type 'mex -setup' first
-% to choose either the Borland or Microsoft compiler. If you want to use MinGW, you also
-% need to install Gnumex (http://gnumex.sourceforget.net), which comes with its own
-% procedure for setting up the MEX environment.
+% Please note that this script does NOT set up your MEX environment for you, so
+% in case you haven't selected the C compiler on Windows yet, you need to type
+% 'mex -setup' first to choose either the Borland or Microsoft compiler. If you
+% want to use MinGW, you also need to install Gnumex
+% (http://gnumex.sourceforget.net), which comes with its own procedure for
+% setting up the MEX environment.
 %
-% The logic in this script is to first build a list of files that actually need compilation for the
-% particular platform that Matlab is running on, and then to go through that list.
-% Functions are added to the list by giving their destination directory and (relative to that) the 
-% name of the source file (without the .c). Optionally, you can specify a list of platform this
-% file needs to be compiled on only, and a list of platforms where you don't compile it on.
-% Finally, you can give extra arguments to the MEX command, e.g., for including other c-sources or
-% giving compiler flags.
+% The logic in this script is to first build a list of files that actually need
+% compilation for the particular platform that Matlab is running on, and then to
+% go through that list. Functions are added to the list by giving their
+% destination directory and (relative to that) the name of the source file
+% (without the .c). Optionally, you can specify a list of platform this file
+% needs to be compiled on only, and a list of platforms where you don't compile
+% it on. Finally, you can give extra arguments to the MEX command, e.g., for
+% including other c-sources or giving compiler flags.
 %
 % See also MEX
 
@@ -46,10 +50,9 @@ L = add_mex_source(L,'fileio/@uint64','rdivide');
 
 L = add_mex_source(L,'@config/private','deepcopy');
 L = add_mex_source(L,'@config/private','increment');
-L = add_mex_source(L,'@config/private','reset');
+L = add_mex_source(L,'@config/private','setzero');
 
 L = add_mex_source(L,'src','ft_getopt');
-L = add_mex_source(L,'src','keyval');
 L = add_mex_source(L,'src','read_16bit');
 L = add_mex_source(L,'src','read_24bit');
 L = add_mex_source(L,'src','mxSerialize');
@@ -63,7 +66,7 @@ L = add_mex_source(L,'src','plinproj',[],[],'geometry.c -I.');
 L = add_mex_source(L,'src','ptriproj',[],[],'geometry.c -I.');
 L = add_mex_source(L,'src','routlm',[],[],'geometry.c -I.');
 L = add_mex_source(L,'src','solid_angle',[],[],'geometry.c -I.');
-L = add_mex_source(L,'src','rfbevent',[],{'PCWIN', 'PCWIN64'},'d3des.c -I.'); % not compilable in WIN32 and WIN64
+L = add_mex_source(L,'src','rfbevent',[],{'PCWIN', 'PCWIN64'},'d3des.c -I.'); % do not compile on WIN32 and WIN64
 L = add_mex_source(L,'src','meg_leadfield1');
 L = add_mex_source(L,'src','splint_gh');
 L = add_mex_source(L,'src','plgndr');
@@ -81,7 +84,6 @@ L = add_mex_source(L,'src','inv2x2');
 L = add_mex_source(L,'src','mtimes2x2');
 L = add_mex_source(L,'src','sandwich2x2');
 
-
 L = add_mex_source(L,'realtime/online_mri','ft_omri_smooth_volume');
 L = add_mex_source(L,'realtime/acquisition/siemens', 'sap2matlab',[],[],'siemensap.c -I.');
 
@@ -96,4 +98,58 @@ catch
   rethrow(me);
 end
 cd(oldDir);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function L = add_mex_source(L, directory, relName, includePlatform, excludePlatform, extras)
+
+% ADD_MEX_SOURCE is a helper function for compiling mex files
+%
+% Use as
+%   list = add_mex_source(list, directory, relName, includePlatform, excludePlatform, extras)
+%
+% The list is a structure array of directory names, source file names, and 
+% extra arguments required for the compilation of MEX files. This function will
+% create a new element of this structure and append it to L.
+%
+%   directory       = target directory of the mex-file
+%   relName         = source file relative to 'directory'
+%   includePlatform   = list of platforms this MEX file should only be compiled for.
+%                     use an empty matrix [] to compile for all platforms
+%   excludePlatform = list of platforms this MEX file should NOT be compiled for.
+%   extras          = extra arguments to the MEX command, e.g. additional source files
+
+% (C) 2010 S. Klanke
+
+% Check if this file only needs compilation on certain platforms (including this one)
+if nargin>3 && ~isempty(includePlatform)
+  ok = false;
+  for k=1:numel(includePlatform)
+    if strcmp(includePlatform{k}, computer)
+      ok = true;
+      break;
+    end
+  end
+  if ~ok
+    return
+  end
+end
+
+% Check if this file cannot be compiled on certain platforms (including this one)
+if nargin>4 && ~isempty(excludePlatform)
+  ok = true;
+  for k=1:numel(excludePlatform)
+    if strcmp(excludePlatform{k}, computer)
+      return;
+    end
+  end
+end
+
+L(end+1).dir   = directory;
+L(end).relName = relName;
+if nargin>5
+  L(end).extras = extras;
+end
 
--- src/getpid.c
+++ src/getpid.c
@@ -1,5 +1,12 @@
 #include <sys/types.h> 
 
+/*
+ * This mex file fails to compile using the LCC compiler that is included 
+ * in 32-bit version of MATLAB. With MSVC 2008 it does compile.
+ *
+ * See also http://bugzilla.fcdonders.nl/show_bug.cgi?id=1384
+ */
+
 #if defined(_WIN32) || defined(_WIN64)
     #include <process.h>
 #else
