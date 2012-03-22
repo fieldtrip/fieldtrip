e1e88f99eab456f120f563c289f030bf9ca4947c
Robert Oostenveld <r.oostenveld@gmail.com>
2012-03-22 09:45:02 +0000

    restructuring - updated the documentation  in ft_compile_mex, changed the two helper functions into subfunctions
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5513 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_compile_mex.m
+++ ft_compile_mex.m
@@ -1,26 +1,35 @@
 function ft_compile_mex(force)
 
 % FT_COMPILE_MEX can be used for compiling most of the FieldTrip MEX files
-% Note that this function does not put the MEX files in the correct location,
-% this is managed by SVN autosync.
+% Note that this function does not put the MEX files in the correct
+% location in the private folders, this is managed by SVN autosync. In case
+% you are not working with SVN and you want to recompile the mex files for
+% your platform, you can find all mex files for your platform and move them
+% to a backup directory that is not on your MATLAB path. Subsequently you
+% can rtun this function to recompile it on your platform with your
+% compiler settings
+% 
+% The standards procedure for compiling mex files is detailled on
+% http://fieldtrip.fcdonders.nl/code_guidelines#compiling_mex_files
 %
-% Please note that this script does NOT set up your MEX environment for you, so
-% in case you haven't selected the C compiler on Windows yet, you need to type
-% 'mex -setup' first to choose either the Borland or Microsoft compiler. If you
-% want to use MinGW, you also need to install Gnumex
-% (http://gnumex.sourceforget.net), which comes with its own procedure for
-% setting up the MEX environment.
+% Please note that this script does NOT set up your MEX environment for
+% you, so in case you haven't selected the C compiler on Windows yet, you
+% need to type 'mex -setup' first to choose either the LCC, Borland or
+% Microsoft compiler. If you want to use MinGW, you also need to install
+% Gnumex (http://gnumex.sourceforget.net), which comes with its own
+% procedure for setting up the MEX environment.
 %
-% The logic in this script is to first build a list of files that actually need
-% compilation for the particular platform that Matlab is running on, and then to
-% go through that list. Functions are added to the list by giving their
-% destination directory and (relative to that) the name of the source file
-% (without the .c). Optionally, you can specify a list of platform this file
-% needs to be compiled on only, and a list of platforms where you don't compile
-% it on. Finally, you can give extra arguments to the MEX command, e.g., for
-% including other c-sources or giving compiler flags.
+% The logic in this script is to first build a list of files that actually
+% need compilation for the particular platform that Matlab is running on,
+% and then to go through that list. Functions are added to the list by
+% giving their destination directory and (relative to that) the name of the
+% source file (without the .c). Optionally, you can specify a list of
+% platform this file needs to be compiled on only, and a list of platforms
+% where you don't compile it on. Finally, you can give extra arguments to
+% the MEX command, e.g., for including other c-sources or giving compiler
+% flags.
 %
-% See also MEX
+% See also MEX 
 
 % Copyright (C) 2010, Stefan Klanke
 %
@@ -51,6 +60,9 @@ L = add_mex_source(L,'fileio/@uint64','rdivide');
 L = add_mex_source(L,'@config/private','deepcopy');
 L = add_mex_source(L,'@config/private','increment');
 L = add_mex_source(L,'@config/private','setzero');
+
+L = add_mex_source(L,'realtime/online_mri','ft_omri_smooth_volume');
+L = add_mex_source(L,'realtime/acquisition/siemens', 'sap2matlab',[],[],'siemensap.c -I.');
 
 L = add_mex_source(L,'src','ft_getopt');
 L = add_mex_source(L,'src','read_16bit');
@@ -84,9 +96,6 @@ L = add_mex_source(L,'src','inv2x2');
 L = add_mex_source(L,'src','mtimes2x2');
 L = add_mex_source(L,'src','sandwich2x2');
 
-L = add_mex_source(L,'realtime/online_mri','ft_omri_smooth_volume');
-L = add_mex_source(L,'realtime/acquisition/siemens', 'sap2matlab',[],[],'siemensap.c -I.');
-
 oldDir = pwd;
 [baseDir, myName] = fileparts(mfilename('fullpath'));
 try
@@ -101,16 +110,11 @@ cd(oldDir);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SUBFUNCTION
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function L = add_mex_source(L, directory, relName, includePlatform, excludePlatform, extras)
-
-% ADD_MEX_SOURCE is a helper function for compiling mex files
 %
 % Use as
 %   list = add_mex_source(list, directory, relName, includePlatform, excludePlatform, extras)
 %
-% The list is a structure array of directory names, source file names, and 
+% The list is a structure array of directory names, source file names, and
 % extra arguments required for the compilation of MEX files. This function will
 % create a new element of this structure and append it to L.
 %
@@ -120,8 +124,9 @@ function L = add_mex_source(L, directory, relName, includePlatform, excludePlatf
 %                     use an empty matrix [] to compile for all platforms
 %   excludePlatform = list of platforms this MEX file should NOT be compiled for.
 %   extras          = extra arguments to the MEX command, e.g. additional source files
-
-% (C) 2010 S. Klanke
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function L = add_mex_source(L, directory, relName, includePlatform, excludePlatform, extras)
 
 % Check if this file only needs compilation on certain platforms (including this one)
 if nargin>3 && ~isempty(includePlatform)
@@ -153,3 +158,41 @@ if nargin>5
   L(end).extras = extras;
 end
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION
+%
+% Use as
+%  compile_mex_list(L, baseDir)
+%
+% Compile a list of MEX files as determined by the input argument L.
+% The second argument 'baseDir' is the common base directory for the
+% files listed in L. The third argument is a flag that determines
+% whether to force (re-)compilation even if the MEX file is up-to-date.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function compile_mex_list(L, baseDir, force)
+
+for i=1:length(L)
+  [relDir, name] = fileparts(L(i).relName);
+  
+  sfname = [baseDir filesep L(i).dir filesep L(i).relName '.c'];
+  SF = dir(sfname);
+  if numel(SF)<1
+    fprintf(1,'Error: source file %s cannot be found.', sfname);
+    continue;
+  end
+  
+  if ~force
+    mfname = [baseDir filesep L(i).dir filesep name '.' mexext];
+    MF = dir(mfname);
+    if numel(MF)==1 && datenum(SF.date) <= datenum(MF.date)
+      fprintf(1,'Skipping up-to-date MEX file %s/%s\n', L(i).dir, name);
+      continue;
+    end
+  end
+  fprintf(1,'Compiling MEX file %s/%s ...\n', L(i).dir, name);
+  cd([baseDir '/' L(i).dir]);
+  cmd = sprintf('mex %s.c %s', L(i). relName, L(i).extras);
+  eval(cmd);
+end
