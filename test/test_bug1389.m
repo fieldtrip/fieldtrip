fca3cfb87c0d80abf2392b6979635a303549b184
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-03-23 14:39:53 +0000

    first commit
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5523 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1389.m
@@ -0,0 +1,34 @@
+function test_bug1389
+
+% there is confusion as to at what level in the cfg tre preproc options
+% need to be specified: for ft_preprocessing this is to be at the main
+% level of the cfg, for all other functions that rely on preproc, it should
+% be in cfg.preproc.XXX
+
+ok = true;
+
+data = [];
+data.trial{1} = randn(2,100)+10;
+data.time{1}  = (-20:79)./100;
+data.label    = {'1';'2'};
+
+% this is how it should work
+cfg = [];
+cfg.demean = 'yes';
+cfg.baselinewindow = [-inf 0];
+data2 = ft_preprocessing(cfg, data);
+ok = ok && all(mean(data.trial{1},2)~=mean(data2.trial{1},2));
+
+% this is how it shouldn't work
+cfg = [];
+cfg.preproc.demean = 'yes';
+cfg.preproc.baselinewindwo = [-inf 0];
+data2b = ft_preprocessing(cfg, data);
+ok = ok && all(mean(data.trial{1},2)==mean(data2b.trial{1},2));
+
+% this is how it should work
+cfg = [];
+cfg.preproc.demean = 'yes';
+cfg.preproc.baselinewindow = [-inf 0];
+data3 = ft_timelockanalysis(cfg, data);
+ok = ok && all(mean(data.trial{1},2)~=mean(data3.avg,2));
