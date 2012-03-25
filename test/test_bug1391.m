9ead7dc4347fb7727c7ef222fc08fec93c5dc31f
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-03-25 17:52:36 +0000

    enhancement - first implementation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5526 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1391.m
@@ -0,0 +1,23 @@
+function test_bug1391
+
+% this functions tests some potential issues with numerical accuracy
+% of the time axis in preproc, which converts the time axis into a 
+% fsample and offset, and then back into a time axis again.
+
+cfg = [];
+cfg.dataset = '/home/common/matlab/fieldtrip/data/Subject01.ds';
+cfg.trl     = [10001 11200 -600];
+cfg.channel = 'MLC';
+cfg.continuous = 'yes';
+data1 = ft_preprocessing(cfg);
+
+cfg.dftfilter = 'yes';
+cfg.padding   = 10;
+cfg.channel   = 'MRC';
+cfg.continuous = 'yes';
+data2 = ft_preprocessing(cfg);
+
+if ~all(data1.time{1}==data2.time{1})
+  error('numerical round off issues detected in the time axes');
+end
+
