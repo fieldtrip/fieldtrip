289d78c5e5ff5e80934cf8c1e7699bae7dba1c7e
Robert Oostenveld <r.oostenveld@gmail.com>
2012-04-02 15:30:18 +0000

    enhancement - added test script for reading neurosim data, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1404
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5577 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1404.m
@@ -0,0 +1,21 @@
+function test_bug1404
+
+% TEST test_bug1404
+% TEST ft_read_header ft_read_data ft_read_spike
+
+% this is only available on the DCCN mentat cluster
+dataset = '/opt/neurosim/Examples/AlphaColumns/dense_smalltest';
+
+hdr = ft_read_header(dataset);
+
+cfg = [];
+cfg.dataset = dataset;
+data = ft_preprocessing(cfg);
+
+% the first channel is the time axis
+assert(all((data.time{1} - data.trial{1}(1,:))<0.01/hdr.Fs))
+
+spike = ft_read_spike(dataset);
+
+ft_checkdata(data, 'datatype', 'raw');
+ft_checkdata(spike, 'datatype', 'spike');
