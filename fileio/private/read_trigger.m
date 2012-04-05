cc5e279d0745cbcd9575e67116294c1a178b318e
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-04-05 08:49:19 +0000

    enhancement - fixed issue to address incorrectly typecasted digital trigger for Neuromag, thanks to Ralph Huonker, addressing bug 731
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5596 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/private/read_trigger.m
+++ fileio/private/read_trigger.m
@@ -120,6 +120,18 @@ if fixctf
   dat(dat<0) = dat(dat<0) + 2^32;
 end
 
+% fix suggested by Ralph Huonker to deal with triggers that need to be
+% interpreted as unsigned integers, rather than signed
+if strncmpi(dataformat, 'neuromag', 8) && ~fixneuromag
+  if any(dat<0)
+    tmpdat = zeros(size(dat));
+    for k = 1:size(dat,1)
+      tmpdat(k,:) = double(typecast(int16(dat(k,:)), 'uint16'));
+    end
+    dat = tmpdat; clear tmpdat;  
+  end
+end
+
 if fixneuromag
   % according to Joachim Gross, real events always have triggers > 5
   % this is probably to avoid the noisefloor
