1f62163daa93cc58d16697a2c3a5ea6e86f1ed1b
Robert Oostenveld <r.oostenveld@gmail.com>
2012-04-11 07:46:31 +0000

    enhancement - added test script for ERP size issue, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1403
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5603 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1403.m
@@ -0,0 +1,21 @@
+function test_bug1403
+
+% TEST test_bug1403
+% TEST ft_read_header
+
+cd /home/common/matlab/fieldtrip/data/test/bug1403
+
+cfg=[];
+cfg.dataset = 'LauraPP1_SEM_MATCH_Average_AUT2.vhdr';
+data = ft_preprocessing(cfg);
+
+cfg=[];
+cfg.layout = '61chan_MPI.lay';
+cfg.interactive = 'yes';
+figure
+ft_multiplotER(cfg, data);
+
+figure
+plot(data.time{1}, data.trial{1})
+legend(data.label)
+
