d7e9a269f11308403b495af6f0554836114236c0
Eelke Spaak <eelke.spaak@donders.ru.nl>
2012-04-17 13:42:16 +0000

    fixed compatibility of ft_progress with command-line only matlab
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5641 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_ft_progress.m
+++ test/test_ft_progress.m
@@ -3,45 +3,49 @@ function test_ft_progress
 % TEST test_ft_progress
 % TEST ft_progress
 
+% testing parameters
+pause_delay = 0.1;
+num_iterations = 42;
+
 ft_progress('init', 'etf',     'Please wait...');
-for i=1:42
-  ft_progress(i/42, 'Processing event %d from %d', i, 42);
-  pause(0.01);
+for i=1:num_iterations
+  ft_progress(i/num_iterations, 'Processing event %d from %d', i, num_iterations);
+  pause(pause_delay);
 end
 ft_progress('close');
 
 ft_progress('init', 'gui',     'Please wait...');
-for i=1:42
-  ft_progress(i/42, 'Processing event %d from %d', i, 42);
-  pause(0.01);
+for i=1:num_iterations
+  ft_progress(i/num_iterations, 'Processing event %d from %d', i, num_iterations);
+  pause(pause_delay);
 end
 ft_progress('close');
 
 ft_progress('init', 'text',     'Please wait...');
-for i=1:42
-  ft_progress(i/42, 'Processing event %d from %d', i, 42);
-  pause(0.01);
+for i=1:num_iterations
+  ft_progress(i/num_iterations, 'Processing event %d from %d', i, num_iterations);
+  pause(pause_delay);
 end
 ft_progress('close');
 
 ft_progress('init', 'dial',     'Please wait...');
-for i=1:42
-  ft_progress(i/42, 'Processing event %d from %d', i, 42);
-  pause(0.01);
+for i=1:num_iterations
+  ft_progress(i/num_iterations, 'Processing event %d from %d', i, num_iterations);
+  pause(pause_delay);
 end
 ft_progress('close');
 
 ft_progress('init', 'none',     'Please wait...');
-for i=1:42
-  ft_progress(i/42, 'Processing event %d from %d', i, 42);
-  pause(0.01);
+for i=1:num_iterations
+  ft_progress(i/num_iterations, 'Processing event %d from %d', i, num_iterations);
+  pause(pause_delay);
 end
 ft_progress('close');
 
 ft_progress('init', 'textbar',     'Please wait...');
-for i=1:42
-  ft_progress(i/42, 'Processing event %d from %d', i, 42);
-  pause(0.01);
+for i=1:num_iterations
+  ft_progress(i/num_iterations, 'Processing event %d from %d', i, num_iterations);
+  pause(pause_delay);
 end
 ft_progress('close');
 
