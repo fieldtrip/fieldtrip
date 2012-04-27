8c3fa526e3c0d5e22ca3094e72dcaeb123b0d3f9
Boris Reuderink <b.reuderink@gmail.com>
2012-04-27 10:01:16 +0000

    bugfix - renamed test_bug1223 to inspect_bug1223 to prevent triggering the testing framework.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5713 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/inspect_bug1223.m
+++ test/inspect_bug1223.m
@@ -3,7 +3,7 @@ function test_bug1223
 % TEST test_bug1223
 % TEST ft_read_mri ft_sourceplot
 
-cd /home/common/matlab/fieldtrip/data/test/bug1223
+cd H:/common/matlab/fieldtrip/data/test/bug1223
 
 mri = ft_read_mri('Num1_MinDef_M_Normal_age12_num10Atlas.hdr');
 
