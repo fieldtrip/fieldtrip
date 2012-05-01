992823dff54eecdb3239174e43b8c1bcc252dacd
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2012-05-01 10:42:00 +0000

    added test script for bug1448
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5717 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1448.m
@@ -0,0 +1,18 @@
+function test_bug1448
+
+% TEST test_bug1448
+
+% this function tests whether the mask is kept inside the call to singleplotTFR
+% load data
+load('/home/common/matlab/fieldtrip/data/test/latest/freq/meg/freq_mtmconvol_trl_ctf275.mat')
+
+% create mask-field
+freqsize = size(freq.powspctrm);
+freq.maskfieldname = zeros(freqsize(2:4));
+freq.maskfieldname(:,1:round(freqsize(3)/2),:) = 1;
+
+% plot using singleplotTFR
+cfg = [];
+cfg.channel = 1;
+cfg.maskparameter = 'maskfieldname';
+ft_singleplotTFR(cfg,freq)
