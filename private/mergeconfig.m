19d92bb445dd7afc6966e360e703a1fccfa95d7a
Robert Oostenveld <r.oostenveld@gmail.com>
2012-05-02 21:24:22 +0000

    automatically synchronized identical files to revision 5729
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5730 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/mergeconfig.m
+++ private/mergeconfig.m
@@ -1,31 +1,41 @@
-function output = mergeconfig(input, default)
+function input = mergeconfig(input, default)
 
-% MERGECONFIG
+% MERGECONFIG merges the fields of a default structure into an input
+% configuration structure
+%
+% Use as
+%   output = mergeconfig(input, default)
 
-% Copyright (C) 2009, Robert Oostenveld
+% Copyright (C) 2009-2012, Robert Oostenveld
 %
 % $Id$
 
-% FIXME also deal with configuration objects
-if ~isstruct(input)
-  input = struct([]);
+if isempty(input) && ~isstruct(input)
+  % ensure that it is an empty struct, not an empty double
+  input = struct;
 end
 
-% FIXME also deal with configuration objects
-if ~isstruct(default)
-  default = struct([]);
+if isempty(default) && ~isstruct(default)
+  % ensure that it is an empty struct, not an empty double
+  default = struct;
 end
 
-fni = fieldnames(input);
-fnd = fieldnames(default);
-fnd = setdiff(fnd, fni);
-
-for i=1:length(fnd)
-  output.(fnd{i}) = default.(fnd{i});
-end
-
-for i=1:length(fni)
-  output.(fni{i}) = input.(fni{i});
-end
-
-
+% merge the input with the fields from default
+fieldsused = fieldnames(default);
+for i=1:length(fieldsused)
+  fn = fieldsused{i};
+  
+  if     ~isfield(input, fn) && ~isstruct(default.(fn))
+    % simply copy the value over
+    input.(fn) = default.(fn);
+  elseif ~isfield(input, fn) &&  isstruct(default.(fn))
+    % simply copy the substructure over
+    input.(fn) = default.(fn);
+  elseif  isfield(input, fn) && ~isstruct(default.(fn))
+    % do not copy it over, keep the original value
+  elseif  isfield(input, fn) &&  isstruct(default.(fn))
+    % merge the two substructures
+    input.(fn) = mergeconfig(input.(fn), default.(fn));
+  end
+  
+end % for all fields
