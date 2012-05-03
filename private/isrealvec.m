e074f6a4042ac4a8d8ff73c9e4752184bb5dc556
Robert Oostenveld <r.oostenveld@gmail.com>
2012-05-03 19:38:23 +0000

    enhancement - added drop-in replacement functions for two isrealxxx functions from ident toolbox (thanks to Josha)
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5731 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ private/isrealvec.m
@@ -0,0 +1,39 @@
+function status = isrealvec(x)
+
+% ISREALVEC returns true for a real row or column vector
+%
+% Use as
+%   status = isrealvec(x)
+%
+% See also ISNUMERIC, ISREAL, ISVECTOR, ISREALMAT
+
+% Copyright (C) 2012, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
+% This is a drop-in replacement for MATLAB/toolbox/ident/idutils/isrealvec.m
+% which requires the Ident toolbox. The following can be used instead to
+% replicate the same functionality.
+%
+% built-in (/Applications/MATLAB_R2010a.app/toolbox/matlab/datatypes/isnumeric)
+% built-in (/Applications/MATLAB_R2010a.app/toolbox/matlab/elfun/isreal)
+% built-in (/Applications/MATLAB_R2010a.app/toolbox/matlab/elmat/isvector)
+% /Applications/MATLAB_R2010a.app/toolbox/matlab/elmat/ndims.m
+
+status = isnumeric(x) && isreal(x) && length(size(x))==2 && any(size(x)==1);
