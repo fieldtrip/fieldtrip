d008140aa19bff17391e2177a84c9f26b93196f3
Boris Reuderink <b.reuderink@gmail.com>
2012-05-08 14:02:41 +0000

    enhancement - added script to test for bug 1262.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5749 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1262.m
@@ -0,0 +1,17 @@
+function test_bug1262
+% Quick and dirty sanity check for reading of fcdc_buffer_offline data.
+% TEST test_bug1262
+% TEST ft_read_header ft_read_data ft_read_event
+
+% Example data generated with sine example, saved with record.exe:
+dirname = 'test_bug1262/0001/';  
+
+hdr = ft_read_header(dirname);
+assert(hdr.nChans == 16);
+assert(hdr.nSamples == 1216);
+
+dat = ft_read_data(dirname);
+assert(all(size(dat) == [hdr.nChans, hdr.nSamples]));
+
+evt = ft_read_event(dirname);
+assert(numel(evt) == 0);  % there are no events
