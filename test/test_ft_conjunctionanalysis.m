fd62434a46dc00bf068cbc456ab0faf0e3d2d29a
Arjen Stolk <a.stolk@donders.ru.nl>
2012-05-18 07:38:10 +0000

    added testfunction for ft_conjunctionanalysis. also re-uncommented two lines in test_ft_freqstatistics
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5786 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_ft_conjunctionanalysis.m
@@ -0,0 +1,78 @@
+function test_ft_conjunctionanalysis
+
+% TEST test_ft_conjunctionanalysis
+% FT_CONJUNCTIONANALYSIS finds the minimum statistic common across two or 
+% more contrasts, i.e. data following ft_xxxstatistics. Furthermore, it 
+% finds the overlap of sensors/voxels that show statistically significant 
+% results (a logical AND on the mask fields).
+%
+% Alternatively, it finds minimalistic mean power values in the
+% input datasets. Here, a type 'relative change' baselinecorrection
+% prior to conjunction is advised.
+% A. Stolk
+
+% chan-time data
+timelock1.label = {'chan1';'chan2'};
+timelock1.time  = 1:10;
+timelock1.dimord = 'chan_time';
+timelock1.stat  = randn(2,10);
+timelock2 = timelock1;
+timelock2.stat  = randn(2,10);
+
+stat = ft_conjunctionanalysis([], timelock1, timelock2);
+
+timelock3 = timelock2;
+timelock3.stat  = randn(2,10);
+
+stat = ft_conjunctionanalysis([], timelock1, timelock2, timelock3);
+
+% chan-freq data
+freq1.label = {'chan1';'chan2'};
+freq1.freq  = 1:10;
+freq1.dimord = 'chan_freq';
+freq1.stat  = randn(2,10);
+freq2 = freq1;
+freq2.stat  = randn(2,10);
+
+stat = ft_conjunctionanalysis([], freq1, freq2);
+
+freq3 = freq2;
+freq3.stat  = randn(2,10);
+
+stat = ft_conjunctionanalysis([], freq1, freq2, freq3);
+
+% create some chan-freq-time data
+freq1.label = {'chan1';'chan2'};
+freq1.freq  = 1:10;
+freq1.time  = 1:5;
+freq1.dimord = 'chan_freq_time';
+freq1.stat  = randn(2,10,5);
+freq2 = freq1;
+freq2.stat  = randn(2,10,5);
+
+stat = ft_conjunctionanalysis([], freq1, freq2);
+
+freq3 = freq2;
+freq3.stat  = randn(2,10,5);
+
+stat = ft_conjunctionanalysis([], freq1, freq2, freq3);
+
+% source data
+source1.dim = [50 1];
+source1.inside = [1:25];
+source1.outside = [26:50];
+source1.pos = randn(50,3);
+source1.stat = randn(50,1);
+source1.prob = randn(50,1);
+source1.mask = ones(50,1);
+source2 = source1;
+source2.stat = randn(50,1);
+source2.prob = randn(50,1);
+
+stat = ft_conjunctionanalysis([], source1, source2);
+
+source3 = source2;
+source3.stat = randn(50,1);
+source3.prob = randn(50,1);
+
+stat = ft_conjunctionanalysis([], source1, source2, source3);
