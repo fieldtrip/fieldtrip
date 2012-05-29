e58e2d07b869fed2f5efa6d611da9f4222971fea
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2012-05-29 10:36:02 +0000

    added a simple basic test function for ft_multiplotTFR, to test changes I'll be making soon to multiplotTFR
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5814 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_ft_multiplotTFR.m
@@ -0,0 +1,28 @@
+function test_ft_multiplotTFR
+
+% TEST test_ft_multiplotTFR 
+% TEST ft_freqanalysis ft_multiplotTFR ft_prepare_layout
+
+% writeflag determines whether the output should be saved to disk
+% version determines the output directory
+
+% dataset taken from ref_datasets, using the tutorial one for testing
+load('/home/common/matlab/fieldtrip/data/test/latest/raw/meg/preproc_ctf275.mat');
+
+% get freqs
+cfg            = [];
+cfg.channel    = {'MEG'};
+cfg.method     = 'mtmconvol';
+cfg.output     = 'pow';
+cfg.keeptrials = 'no';
+cfg.foi        = 2:2:30;
+cfg.taper      = 'hanning';
+cfg.t_ftimwin  = ones(1,numel(cfg.foi)).*0.5;
+cfg.toi        = (250:50:750)./1000;
+freq = ft_freqanalysis(cfg,data);
+
+
+% plot them
+cfg = [];
+ft_multiplotTFR(cfg,freq)
+
