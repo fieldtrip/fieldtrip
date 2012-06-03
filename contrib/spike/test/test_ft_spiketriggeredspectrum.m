341a0360d8ce989350319c5aa17578a079b0a4e5
Martin Vinck <martinvinck@gmail.com>
2012-06-03 16:43:04 +0000

    updated test script ft_spiketriggeredspectrum.m
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5844 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- contrib/spike/test/test_ft_spiketriggeredspectrum.m
+++ contrib/spike/test/test_ft_spiketriggeredspectrum.m
@@ -21,29 +21,34 @@ time(1:NTRIALS) = {(0:0.001:2)};
 
 % enter a sinusoid with a known phase, without noise for now
 SINFREQ = 50; %hz
-trial(1:NTRIALS) = {[cos(SINFREQ*2*pi*time{1});sin(SINFREQ*2*pi*time{1})]}; % so a spike at t = 0 should mean a phase of 0. 
+trial(1:NTRIALS) = {[rand(1,length(time{1}))+cos(SINFREQ*2*pi*time{1});rand(1,length(time{1}))+sin(SINFREQ*2*pi*time{1})]}; % so a spike at t = 0 should mean a phase of 0. 
 
 % enter spikes with known phases, use a different phase for every trial
 Spike = [];
-[Spike.trial{1},Spike.time{1}] = deal([]);
-rho = linspace(0,1,20);
+[Spike.trial{1},Spike.time{1}, Spike.timestamp{1}] = deal([]);
+rho = linspace(0,1,NTRIALS);
 for iTrial = 1:NTRIALS
-    SPKFREQ = 50;
+    SPKFREQ = SINFREQ;
     isi = 1/SPKFREQ; % in that way we will have a perfect phase.
     
     % in cycles
     phase = rho(iTrial);
-    nSpikes = 2/isi - 5; % 95 spikes per trials
+    nSpikes = 2/isi; % 95 spikes per trials
     spikeSteps = ones(1,nSpikes)*isi;% + 2*rand(1,nSpikes)
     spikeTim = 0 + cumsum(spikeSteps) + phase*isi;
     % find the number of samples
-    spikesamples = round(spikeTim/(1/data.fsample))+1;    
+    spikesamples = round(spikeTim/(1/data.fsample))+1; 
+    
+    spikesamples - (spikeTim/(1/data.fsample)+1) 
+   
+    spikesamples(spikesamples>length(time{iTrial})) = [];
     trial{iTrial}(3,:) = 0;
     trial{iTrial}(3,spikesamples) = 1;
     
     Spike.time{1}                = [Spike.time{1} spikeTim];
     Spike.trial{1}               = [Spike.trial{1}; iTrial*ones(length(spikeTim),1)];
     Spike.trialtime(iTrial,:)         = [time{1}(1) time{1}(end)];
+    Spike.timestamp{1}           = [Spike.timestamp{1} spikeTim*1000000];
 end
 
 Spike.label = {'spk1'};
@@ -58,11 +63,85 @@ data.hdr = struct([]);
 % test with a simple data structure as the input
 cfg = [];
 cfg.spikechannel = 3;
+cfg.timwin       = [-0.1 0.1];
 tic
 freq1 = ft_spiketriggeredspectrum(cfg,data);
-foi = nearest(freq1.freq,50);
-phases1 = angle(freq1.fourierspctrm{1}(:,1,foi));
-figure, plot(phases1)
-
+toc
+%
+cfg = [];
+cfg.method = 'convol';
+cfg.foi = [SINFREQ];
+cfg.t_ftimwin = 10./cfg.foi;
+cfg.taper = 'hanning';
+cfg.borderspikes = 'yes';
+tic
+freq2 = ft_spiketriggeredspectrum(cfg,data);
+toc
+foi = nearest(freq2.freq,SINFREQ);
+%
+foi = nearest(freq1.freq,SINFREQ);
+inTrial = freq1.trial{1}>0;
+phases1 = angle(freq1.fourierspctrm{1}(inTrial,1,foi));
+figure, plot(freq1.time{1}(inTrial),phases1,'bx')
+%
+foi = nearest(freq2.freq,SINFREQ);
+inTrial = freq2.trial{1}>0;
+phases1 = angle(freq2.fourierspctrm{1}(inTrial,1,foi));
+hold on
+plot(freq2.time{1}(inTrial),phases1,'ro-')
+pause
+close 
+%%
+% test with a simple data structure as the input
+cfg = [];
+cfg.spikechannel = 3;
+cfg.timwin       = [-0.1 0.1];
+tic
+freq1 = ft_spiketriggeredspectrum(cfg,data);
+toc
+%
+cfg = [];
+cfg.method = 'convol';
+cfg.foi = [SINFREQ];
+cfg.t_ftimwin = 10./cfg.foi;
+cfg.taper = 'hanning';
+cfg.borderspikes = 'no';
+tic
+freq2 = ft_spiketriggeredspectrum(cfg,data);
+toc
+%
+cfg = [];
+cfg.method = 'convol';
+cfg.foi = [SINFREQ];
+cfg.t_ftimwin = 10./cfg.foi;
+cfg.taper = 'hanning';
+cfg.borderspikes = 'no';
+cfg.channel = data.label(1:2);
+tic
+freq3 = ft_spiketriggeredspectrum(cfg,data,Spike);
+toc
 %%
 
+
+foi = nearest(freq2.freq,SINFREQ);
+%
+foi = nearest(freq1.freq,SINFREQ);
+inTrial = freq1.trial{1}>0;
+phases1 = angle(freq1.fourierspctrm{1}(inTrial,1,foi));
+figure, plot(freq1.time{1}(inTrial),phases1,'bx')
+%
+foi = nearest(freq2.freq,SINFREQ);
+inTrial = freq2.trial{1}>0;
+phases1 = angle(freq2.fourierspctrm{1}(inTrial,1,foi));
+hold on
+plot(freq2.time{1}(inTrial),phases1,'ro-')
+hold on
+foi = nearest(freq3.freq,SINFREQ);
+inTrial = freq3.trial{1}>0;
+phases1 = angle(freq3.fourierspctrm{1}(inTrial,1,foi));
+plot(freq3.time{1}(inTrial),phases1,'gd-')
+
+pause
+close 
+% NOTE THAT THE PHASE USING THE TRUE TIMES IS MORE RELIABLE!
+%%
