366f797314d1036165b2d0c38d25cc10205393f3
Martin Vinck <martinvinck@gmail.com>
2012-06-07 14:46:38 +0000

    made ft_spike_xcorr.m output more consistent with other fieldtrip functions
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5971 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- contrib/spike/ft_spike_xcorr.m
+++ contrib/spike/ft_spike_xcorr.m
@@ -1,4 +1,4 @@
-function [Xcorr] = ft_spike_xcorr(cfg,spike)
+function [stat] = ft_spike_xcorr(cfg,spike)
 
 % FT_SPIKE_XCORR computes the cross-correlation histogram and shift predictor.
 %
@@ -14,13 +14,11 @@ function [Xcorr] = ft_spike_xcorr(cfg,spike)
 %                          cross-correlation function in sec (default = 0.1 sec).
 %   cfg.biased           = 'yes' or 'no' (default). If 'no', we scale the
 %                          cross-correlogram by M/(M-abs(lags)), where M = 2*N -1 with N
-%                          the length of the data segment. Although this scaling reduces the
-%                          bias, it can give a higher variance which might be more
-%                          problematic in some cases.
-%   cfg.shiftpredictor   = 'no' (default) or 'yes'. The shift-predictor is
-%   calculated from
-%                          channel x in every trial to the channel y in the previous
-%                          trial. If two channels are independent, then the shift
+%                          the length of the data segment. 
+%   cfg.method           = 'xcorr' or 'shiftpredictor'. If 'shiftpredictor'
+%                           we do not compute the normal cross-correlation
+%                           but shuffle the subsequent trials.
+%                          If two channels are independent, then the shift
 %                          predictor should give the same correlogram as the raw
 %                          correlogram calculated from the same trials.
 %                          Typically, the shift predictor is subtracted from the
@@ -28,10 +26,8 @@ function [Xcorr] = ft_spike_xcorr(cfg,spike)
 %   cfg.outputunit       = 'proportion' (value in each bin indicates proportion of occurence)
 %                          'center' (values are scaled to center value which is set to 1)
 %                          'raw' (default) unnormalized crosscorrelogram.
-%
 %   cfg.binsize          = [binsize] in sec (default = 0.001 sec).
 %
-% Configuration options of channel selection & checking, latency and trial selection:
 %   cfg.channelcmb       = Mx2 cell-array with selection of channel pairs (default = {'all' 'all'}),
 %                          see FT_CHANNELCOMBINATION for details
 %   cfg.latency          = [begin end] in seconds, 'max' (default), 'min', 'prestim'(t<=0), or
@@ -58,16 +54,19 @@ function [Xcorr] = ft_spike_xcorr(cfg,spike)
 % otherwise we do not compute the shift predictor.
 %
 % Output:
-%   xcorr.xcorr            = (2*nlags+1)-by-nchans-by-nchans cross correlation histogram
-%   xcorr.lags             = (2*nlags + 1) vector with lags in seconds.
-%   xcorr.shiftpredictor   = (2*nlags+1)-by-nchans-by-nchans shift predictor.
-%   xcorr.dimord
-%   xcorr.trial            = (2*nlags + 1)-by-ntrials-by-nchans-by-nchans with single
+%    xcorr.xcorr            = (2*nlags+1)-by-nchans-by-nchans cross correlation histogram
+%   or 
+%    xcorr.shiftpredictor   = (2*nlags+1)-by-nchans-by-nchans shift predictor.
+%
+%    xcorr.lags             = (2*nlags + 1) vector with lags in seconds.
+%
+%    xcorr.dimord
+%    xcorr.trial            = (2*nlags + 1)-by-ntrials-by-nchans-by-nchans with single
 %                            trials.
-%   xcorr.label            = corresponding labels to channels in xcorr.xcorr
-%   xcorr.cfg              = configurations used in this function.
+%    xcorr.label            = corresponding labels to channels in xcorr.xcorr
+%    xcorr.cfg              = configurations used in this function.
 
-% Copyright (C) 2010, Martin Vinck
+% Copyright (C) 2010-2012, Martin Vinck
 %
 % $Id$
 
@@ -86,7 +85,7 @@ spike = ft_checkdata(spike,'datatype', 'spike', 'feedback', 'yes');
 cfg.trials         = ft_getopt(cfg,'trials', 'all');
 cfg.latency        = ft_getopt(cfg,'latency','maxperiod');
 cfg.keeptrials     = ft_getopt(cfg,'keeptrials', 'yes');
-cfg.shiftpredictor = ft_getopt(cfg,'shiftpredictor', 'no');
+cfg.method         = ft_getopt(cfg,'method', 'xcorr');
 cfg.channelcmb     = ft_getopt(cfg,'channelcmb', 'all');
 cfg.vartriallen    = ft_getopt(cfg,'vartriallen', 'no');
 cfg.biased         = ft_getopt(cfg,'biased', 'no');
@@ -95,23 +94,25 @@ cfg.binsize        = ft_getopt(cfg,'binsize', 0.001);
 cfg.outputunit     = ft_getopt(cfg,'outputunit', 'proportion');
 
 % ensure that the options are valid
-cfg = ft_checkopt(cfg,'latency', {'char', 'doublevector'});
+cfg = ft_checkopt(cfg,'latency', {'char', 'ascendingdoublebivector'});
 cfg = ft_checkopt(cfg,'trials', {'char', 'doublevector', 'logical'}); 
 cfg = ft_checkopt(cfg,'keeptrials', 'char', {'yes', 'no'});
-cfg = ft_checkopt(cfg,'shiftpredictor', 'char', {'yes', 'no'});
+cfg = ft_checkopt(cfg,'method', 'char', {'xcorr', 'shiftpredictor'});
 cfg = ft_checkopt(cfg,'channelcmb', {'char', 'cell'});
 cfg = ft_checkopt(cfg,'vartriallen', 'char', {'no', 'yes'});
 cfg = ft_checkopt(cfg,'biased', 'char', {'yes', 'no'});
-cfg = ft_checkopt(cfg,'maxlag', 'double');
-cfg = ft_checkopt(cfg,'binsize', 'double');
+cfg = ft_checkopt(cfg,'maxlag', 'doublescalar');
+cfg = ft_checkopt(cfg,'binsize', 'doublescalar');
 cfg = ft_checkopt(cfg,'outputunit', 'char', {'proportion', 'center', 'raw'});
 
-doShiftPredictor  = strcmp(cfg.shiftpredictor,'yes'); % shift predictor
+cfg = ft_checkconfig(cfg,'allowed', {'latency', 'trials', 'keeptrials', 'method', 'channelcmb', 'vartriallen', 'biased', 'maxlag', 'binsize', 'outputunit'});
+
+doShiftPredictor  = strcmp(cfg.method,'shiftpredictor'); % shift predictor
 doBiased          = strcmp(cfg.biased,'yes'); % debiasing
 
 % determine the corresponding indices of the requested channel combinations
 cfg.channelcmb = ft_channelcombination(cfg.channelcmb, spike.label,true);
-cmbindx = zeros(size(cfg.channelcmb));
+cmbindx        = zeros(size(cfg.channelcmb));
 for k=1:size(cfg.channelcmb,1)
   cmbindx(k,1) = strmatch(cfg.channelcmb(k,1), spike.label, 'exact');
   cmbindx(k,2) = strmatch(cfg.channelcmb(k,2), spike.label, 'exact');
@@ -119,10 +120,7 @@ end
 nCmbs 	   = size(cmbindx,1);
 chansel    = unique(cmbindx(:)); % get the unique channels
 nChans     = length(chansel);
-if isempty(chansel),
-  error('MATLAB:ft_spike_xcorr:cfg:channelcombination',...
-    'No channel was selected')
-end
+if isempty(chansel), error('No channel was selected'); end
 
 % get the number of trials or change SPIKE according to cfg.trials
 nTrials = size(spike.trialtime,1);
@@ -132,12 +130,8 @@ elseif islogical(cfg.trials)
   cfg.trials = find(cfg.trials);
 end
 cfg.trials = sort(cfg.trials(:));
-if max(cfg.trials)>nTrials, error('MATLAB:ft_spike_xcorr:cfg:trials:maxExceeded',...
-    'maximum trial number in cfg.trials should not exceed length of DATA.trial')
-end
-if isempty(cfg.trials),
-  errors('MATLAB:fieldtrip:spike_xcorr:cfg:trials','No trials were selected in cfg.trials');
-end
+if max(cfg.trials)>nTrials, error('maximum trial number in cfg.trials should not exceed length of DATA.trial'); end
+if isempty(cfg.trials), errors('No trials were selected in cfg.trials'); end
 nTrials = length(cfg.trials);
 
 % get the latencies
@@ -154,29 +148,20 @@ elseif strcmp(cfg.latency,'prestim')
   cfg.latency = [min(begTrialLatency) 0];
 elseif strcmp(cfg.latency,'poststim')
   cfg.latency = [0 max(endTrialLatency)];
-elseif ~isrealvec(cfg.latency)||length(cfg.latency)~=2
-  error('MATLAB:ft_spike_xcorr:cfg:latency',...
-    'cfg.latency should be "max", "min", "prestim", "poststim" or 1-by-2 numerical vector');
-end
-if cfg.latency(1)>cfg.latency(2), error('MATLAB:ft_spike_xcorr:incorrectLatencyWindow',...
-    'cfg.latency should be a vector in ascending order, i.e., cfg.latency(2)>cfg.latency(1)');
 end
 
 % check whether the time window fits with the data
 if (cfg.latency(1) < min(begTrialLatency)), cfg.latency(1) = min(begTrialLatency);
-  warning('MATLAB:ft_spike_xcorr:incorrectLatencyWindow',...
-    'Correcting begin latency of averaging window');
+  warning('Correcting begin latency of averaging window');
 end
 if (cfg.latency(2) > max(endTrialLatency)), cfg.latency(2) = max(endTrialLatency);
-  warning('MATLAB:ft_spike_xcorr:incorrectLatencyWindow',...
-    'Correcting begin latency of averaging window');
+  warning('Correcting begin latency of averaging window');
 end
 
 % get the maximum number of lags in samples
 fsample = (1/cfg.binsize);
 if cfg.maxlag>(cfg.latency(2)-cfg.latency(1))
-  warning('MATLAB:ft_spike_xcorr:incorrectLatencyWindow',...
-    'Correcting cfg.maxlag since it exceeds latency period');
+  warning('Correcting cfg.maxlag since it exceeds latency period');
   cfg.maxlag = (cfg.latency(2) - cfg.latency(1));
 end
 nLags        = round(cfg.maxlag*fsample);
@@ -193,22 +178,14 @@ if strcmp(cfg.vartriallen,'no') % only select trials that fully cover our latenc
 end
 trialSel           = fullDur(:) & overlaps(:) & hasWindow(:);
 cfg.trials         = cfg.trials(trialSel);
-if isempty(cfg.trials),
-  warning('MATLAB:ft_spike_xcorr:cfg:trials','No trials were selected');
-end
+if isempty(cfg.trials), warning('No trials were selected'); end
 if length(cfg.trials)<2&&doShiftPredictor
-  warning('MATLAB:ft_spike_xcorr:cfg:shiftpredictor:trials',...
-    'Shift predictor can only be calculated with more than 1 selected trial')
-  doShiftPredictor = 0;
+  error('Shift predictor can only be calculated with more than 1 selected trial.');
 end
 
 % if we allow variable trial lengths
 if strcmp(cfg.vartriallen,'yes') && doShiftPredictor && nTrials~=length(cfg.trials)
-  warning('MATLAB:ft_spike_xcorr:cfg:vartriallen:shiftpredictor:latency',...
-    '%s\n%s\n%s', 'Variable trial length and shift predictor are only possible when all trials',...
-    'contain the full window period, ignoring request to compute shift predictor',...
-    'please adjust the latency so it includes all trials or set vartriallen to no');
-  doShiftPredictor = 0;
+  error('cfg.vartriallen = "yes" and shift predictor method are only possible when all trials contain the full window period');
 end
 nTrials   = length(cfg.trials); % only now reset nTrials
 
@@ -225,13 +202,13 @@ for iTrial = 1:nTrials
     indx  = cmbindx(iCmb,:);
     inTrial1 = spike.trial{indx(1)}==origTrial;
     inTrial2 = spike.trial{indx(2)}==origTrial;
-    ts1 = sort(spike.time{indx(1)}(inTrial1));
-    ts2 = sort(spike.time{indx(2)}(inTrial2));
-
+    inWindow1 = spike.time{indx(1)}>=cfg.latency(1) &  spike.time{indx(1)}<=cfg.latency(2);
+    inWindow2 = spike.time{indx(2)}>=cfg.latency(1) &  spike.time{indx(2)}<=cfg.latency(2);    
+    ts1 = sort(spike.time{indx(1)}(inTrial1(:) & inWindow1(:)));
+    ts2 = sort(spike.time{indx(2)}(inTrial2(:) & inWindow2(:)));
+    
     % compute the xcorr if both are non-empty
     if ~isempty(ts1) && ~isempty(ts2)
-      ts1(:)
-      ts2(:)
       if indx(1)<=indx(2)
         [x]   = spike_crossx(ts1(:),ts2(:),cfg.binsize,nLags*2+1);
       else
@@ -254,9 +231,9 @@ for iTrial = 1:nTrials
 
       % symmetric, get x21 to x12 and x22 to x11
       inTrial1_old = spike.trial{indx(1)}==cfg.trials(iTrial-1);
-      ts1_old      = sort(spike.time{indx(1)}(inTrial1_old));
+      ts1_old      = sort(spike.time{indx(1)}(inTrial1_old(:) & inWindow1(:)));
       inTrial2_old = spike.trial{indx(2)}==cfg.trials(iTrial-1);
-      ts2_old      = sort(spike.time{indx(2)}(inTrial2_old));
+      ts2_old      = sort(spike.time{indx(2)}(inTrial2_old(:) & inWindow2(:)));
 
       % compute both combinations
       for k = 1:2
@@ -270,9 +247,6 @@ for iTrial = 1:nTrials
           B = ts2;
         end
         if ~isempty(A) && ~isempty(B),
-
-          A(:)
-          B(:)
           if indx(1)<=indx(2)            
             [x]   = spike_crossx(A(:),B(:),cfg.binsize,nLags*2+1);
           else
@@ -287,10 +261,9 @@ for iTrial = 1:nTrials
   end % combinations
 end % trial loop
 
-% multiply the shift sum by a factor so it has the same scale: note it is not raw anymore
+% multiply the shift sum by a factor so it has the same scale as raw 
 dofShiftPred = 2*(nTrials-1);
 if doShiftPredictor, shiftSum = shiftSum*nTrials/dofShiftPred; end
-
 switch cfg.outputunit
   case 'proportion'
     if doShiftPredictor
@@ -310,16 +283,18 @@ switch cfg.outputunit
 end
 
 % return the results
-Xcorr.xcorr          = s;
-Xcorr.lags           = lags;
-if doShiftPredictor
-  Xcorr.shiftpredictor = shiftSum;
+if strcmp(cfg.method,'xcorr')
+  stat.xcorr            = s;
+else
+  stat.shiftpredictor   = shiftSum;
 end
+stat.time              = lags;
+stat.dimord             = 'time_chan_chan'; 
 if keepTrials
-  Xcorr.trial = singleTrials;
+  stat.trial = singleTrials;
+  dimord     = 'trial_time_chan_chan';
 end
-Xcorr.label       = spike.label(chansel);
-Xcorr.labelcmb    = cfg.channelcmb;
+stat.label       = spike.label(chansel);
 
 % do the general cleanup and bookkeeping at the end of the function
 ft_postamble trackconfig
