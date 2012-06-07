8cebf2e731ae49edf0fb11d79955dd0a461e8652
Martin Vinck <martinvinck@gmail.com>
2012-06-07 10:20:49 +0000

    simplified ft_spike_isi script and will add separate parameter script with cfg.method
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5962 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ contrib/spike/ft_spike_isi.m
@@ -0,0 +1,143 @@
+function [isih] = ft_spike_isi(cfg,spike)
+
+% FT_SPIKE_ISI computes the interspike interval histogram
+%
+% The input SPIKE should be organised as the spike or the raw datatype
+% (containing channels with binary or M-ary spike-trains).
+%
+% Use as
+%   [isih] = ft_spike_isi(cfg, spike)
+%
+% Configurations:
+%   cfg.outputunit       = 'spikecount' (default) or 'proportion' (sum of all bins = 1).
+%   cfg.spikechannel     = string or index of single spike channel to trigger on (default = 'all')
+%                          See FT_CHANNELSELECTION for details
+%   cfg.trials           = numeric selection of trials (default = 'all')
+%   cfg.bins             = vector of isi bins.
+%   cfg.latency          = [begin end] in seconds, 'max' (default), 'min', 'prestim'(t<=0), or
+%                          'poststim' (t>=0).
+%                          If 'max', we use all available latencies.
+%                          If 'min', we use only the time window contained by all trials.
+%                          If 'prestim' or 'poststim', we use time to or from 0.
+%`  cfg.keeptrials       = 'yes' or 'no'. If 'yes', we keep the individual
+%                           isis between spikes and output as isih.isi
+% Outputs:
+%   isih.avg             = nUnits-by-nBins interspike interval histogram
+%   isih.time            = bincenters corresponding to isih.avg
+%   isih.isi             = 1-by-nUnits cell with interval to next spike per spike.
+%   isih.label           = 1-by-nUnits cell array with labels
+
+% Copyright (C) 2010, Martin Vinck
+%
+% $Id: ft_spike_isihist.m 5957 2012-06-07 09:12:26Z marvin $
+
+revision = '$Id: ft_spike_isihist.m 5957 2012-06-07 09:12:26Z marvin $';
+
+% do the general setup of the function
+ft_defaults
+ft_preamble help
+ft_preamble callinfo
+ft_preamble trackconfig
+
+% check if data is of proper format 
+spike = ft_checkdata(spike,'datatype', 'spike', 'feedback', 'yes');
+
+% get the default options
+cfg.outputunit   = ft_getopt(cfg,'outputunit','spikecount');
+cfg.spikechannel = ft_getopt(cfg,'spikechannel', 'all');
+cfg.trials       = ft_getopt(cfg,'trials', 'all');
+cfg.latency      = ft_getopt(cfg,'latency','maxperiod');
+cfg.keeptrials   = ft_getopt(cfg,'keeptrials', 'yes');
+cfg.bins         = ft_getopt(cfg,'bins', 0:0.002:1);
+
+% ensure that the options are valid
+cfg = ft_checkopt(cfg,'outputunit','char', {'spikecount', 'proportion'});
+cfg = ft_checkopt(cfg,'bins', 'doublevector');
+cfg = ft_checkopt(cfg,'spikechannel',{'cell', 'char', 'double'});
+cfg = ft_checkopt(cfg,'latency', {'char', 'ascenddoublebivector'});
+cfg = ft_checkopt(cfg,'trials', {'char', 'doublevector', 'logical'}); 
+cfg = ft_checkopt(cfg,'keeptrials', 'char', {'yes', 'no'});
+
+cfg = ft_checkconfig(cfg,'allowed', {'outputunit', 'bins', 'spikechannel', 'latency', 'trials', 'keeptrials'});
+
+% get the number of trials or change DATA according to cfg.trials
+if  strcmp(cfg.trials,'all')
+  cfg.trials = 1:size(spike.trialtime,1);
+elseif islogical(cfg.trials)
+  cfg.trials = find(cfg.trials);
+end
+cfg.trials = sort(cfg.trials);
+nTrials    = length(cfg.trials);
+
+cfg.channel = ft_channelselection(cfg.spikechannel, spike.label);
+spikesel    = match_str(spike.label, cfg.channel);
+nUnits      = length(spikesel); % number of spike channels
+if nUnits==0, error('No spikechannel selected by means of cfg.spikechannel'); end
+
+% determine the duration of each trial
+begTrialLatency = spike.trialtime(cfg.trials,1);
+endTrialLatency = spike.trialtime(cfg.trials,2);
+
+% select the latencies
+if strcmp(cfg.latency,'minperiod')
+  cfg.latency = [max(begTriallatency) min(endTriallatency)];
+elseif strcmp(cfg.latency,'maxperiod')
+  cfg.latency = [min(begTrialLatency) max(endTrialLatency)];
+elseif strcmp(cfg.latency,'prestim')
+  cfg.latency = [min(begTrialLatency) 0];
+elseif strcmp(cfg.latency,'poststim')
+  cfg.latency = [0 max(endTrialLatency)];
+end
+
+% construct the isi bins, we let it run to maximum trial duration by default
+bins  = cfg.bins;
+nBins = length(bins)-1;
+
+% compute the interspike interval
+keepTrials = strcmp(cfg.keeptrials,'yes');
+if keepTrials, isiSpike = cell(1,nUnits); end  % contains the individual histogram spike times
+isihist  = zeros(nUnits,nBins+1); % isi histogram
+
+for iUnit = 1:nUnits
+  unitIndx = spikesel(iUnit);
+  
+  % only select the spikes in the right latencies and trials
+  spikeTrials    = spike.trial{unitIndx}(:)';
+  spikeTimes     = spike.time{unitIndx}(:)';
+  
+  % select only the spikes in the window and with the selected trials
+  spikesInTrials = ismember(spikeTrials, cfg.trials);
+  spikesInWin    = spikeTimes>=cfg.latency(1)&spikeTimes<=cfg.latency(2);
+  spikeTimes     = spikeTimes(spikesInTrials & spikesInWin);
+  
+  % find the spikes that jumped to the next trial, replace them with NaNs
+  trialJump = logical([1 diff(spikeTrials)]);
+  isi = [NaN diff(spikeTimes)];
+  isi(trialJump) = NaN;
+  
+  % convert and store the isi
+  if keepTrials, isiSpike{iUnit} = isi; end
+  isihist(iUnit,:)   = histc(isi,bins);
+end
+
+isihist(:,end) = []; % the last number is only an equality to a bin edge
+if strcmp(cfg.outputunit,'proportion'),
+  isihist = isihist./repmat(nansum(isihist,2),1,size(isihist,2));
+end
+
+% gather the rest of the results
+if strcmp(cfg.keeptrials,'yes')
+  isih.isi       = isiSpike;
+end
+isih.time        = bins(1:end-1);
+isih.avg         = isihist;
+isih.dimord      = 'chan_bins';
+isih.label       = spike.label(spikesel);
+
+% do the general cleanup and bookkeeping at the end of the function
+ft_postamble trackconfig
+ft_postamble callinfo
+ft_postamble previous spike
+ft_postamble history isih
+
+
