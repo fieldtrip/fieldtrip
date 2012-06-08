4d919510e59c7291a31196ff55909f2f18f82cf5
Robert Oostenveld <r.oostenveld@gmail.com>
2012-06-08 13:58:12 +0000

    documentation - added copyright statement
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5999 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- statfun/statfun_pooledT.m
+++ statfun/statfun_pooledT.m
@@ -35,6 +35,27 @@ function [s] = statfun_pooledT(cfg, dat, design)
 % See FT_TIMELOCKSTATISTICS, FT_FREQSTATISTICS or FT_SOURCESTATISTICS
 % for details.
 
+% Copyright (C) 2007, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
+
 selA = find(design(cfg.ivar,:)==1);
 selB = find(design(cfg.ivar,:)==2);
 dfA  = length(selA);
