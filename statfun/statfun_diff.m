86810f87cffe0b0ff70bbe81722742e1f0e24c1a
Robert Oostenveld <r.oostenveld@gmail.com>
2012-06-08 11:00:26 +0000

    documentation - added a license, to test the svn hooks
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5995 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- statfun/statfun_diff.m
+++ statfun/statfun_diff.m
@@ -13,6 +13,26 @@ function [s] = statfun_diff(cfg, dat, design)
 %
 % See also STATFUN_MEAN for an other example function
 
+% Copyright (C) 2006, Robert Oostenveld 
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
 selA = find(design(cfg.ivar,:)==1); % selecton condition 1 or A
 selB = find(design(cfg.ivar,:)==2); % selecton condition 2 or B
 dfA  = length(selA);
