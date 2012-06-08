54e33fe1bbfc4dfe703c09332bf2a60fea7a4c56
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-06-08 14:33:09 +0000

    made test function and reach revision 6000!
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6000 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1483.m
@@ -0,0 +1,7 @@
+function test_bug1483
+
+% TEST test_bug1483
+% TEST ft_selectdata ft_selectdata_new ft_selectdata_old
+
+% first confirm the reported bug, i.e. the incapability of ft_selectdata_new
+% to select 'rpt'
