03eed3ac81a25dfdd25c14b40ff58a7155a04077
Robert Oostenveld <r.oostenveld@gmail.com>
2012-06-08 10:57:05 +0000

    documentation - updated license to test the svn hooks
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5993 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- statfun/statfun_depsamplesT.m
+++ statfun/statfun_depsamplesT.m
@@ -43,7 +43,23 @@ function [s, cfg] = statfun_depsamplesT(cfg, dat, design)
 
 % Copyright (C) 2006, Eric Maris
 %
-% Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
 
 % set defaults
 if ~isfield(cfg, 'computestat'),    cfg.computestat    = 'yes'; end 
