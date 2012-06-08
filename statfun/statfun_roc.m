e979e9240a99fda1d5af3bd26c49ea1930227c7b
Robert Oostenveld <r.oostenveld@gmail.com>
2012-06-08 13:55:35 +0000

    documentation - added license statement
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@5998 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- statfun/statfun_roc.m
+++ statfun/statfun_roc.m
@@ -29,6 +29,24 @@ function [s, cfg] = statfun_roc(cfg, dat, design)
 %   auc = statfun_roc([], [a b], design);
 
 % Copyright (C) 2008, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
 
 if ~isfield(cfg, 'ivar'),         cfg.ivar   =  1;         end
 if ~isfield(cfg, 'logtransform'), cfg.logtransform = 'no'; end
