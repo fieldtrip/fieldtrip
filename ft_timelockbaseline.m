545d8adc29244b488fe80fa0cbeee16f48222b13
Jorn Horschig <jm.horschig@donders.ru.nl>
2012-06-13 13:38:26 +0000

    bugfix-#1484-freqbaseline works on cfg.parameter now (as it should be, renamed from cfg.param). timelockbaseline does support cfg.parameter as well as ft_freqanalysis does.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6054 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_timelockbaseline.m
+++ ft_timelockbaseline.m
@@ -8,7 +8,9 @@ function [timelock] = ft_timelockbaseline(cfg, timelock)
 % configuration should contain
 %   cfg.baseline     = [begin end] (default = 'no')
 %   cfg.channel      = cell-array, see FT_CHANNELSELECTION
-%
+%   cfg.parameter    = field for which to apply baseline normalization, or
+%                      cell array of strings to specify multiple fields to normalize
+%                      (default = 'avg')
 % To facilitate data-handling and distributed computing with the peer-to-peer
 % module, this function has the following options:
 %   cfg.inputfile   =  ...
@@ -62,8 +64,24 @@ cfg = ft_checkconfig(cfg, 'renamed', {'blc', 'demean'});
 cfg = ft_checkconfig(cfg, 'renamed', {'blcwindow', 'baselinewindow'});
 
 % set the defaults
-cfg.baseline = ft_getopt(cfg, 'baseline', 'no');
-cfg.channel  = ft_getopt(cfg, 'channel',  'all');
+cfg.baseline  = ft_getopt(cfg, 'baseline',  'no');
+cfg.channel   = ft_getopt(cfg, 'channel',   'all');
+cfg.parameter = ft_getopt(cfg, 'parameter', '');
+
+if isempty(cfg.parameter)
+  if isfield(timelock, 'avg')
+    cfg.parameter = 'avg';
+  elseif strcmp(timelock.dimord, 'rpt_chan_time')
+    cfg.parameter = 'trial';
+  elseif strcmp(timelock.dimord, 'subj_chan_time')
+    cfg.parameter = 'individual';
+  end
+end
+
+% make sure cfg.parameter is a cell array of strings
+if (~isa(cfg.parameter, 'cell'))
+  cfg.parameter = {cfg.parameter};
+end
 
 % the cfg.blc/blcwindow options are used in preprocessing and in
 % ft_timelockanalysis (i.e. in private/preproc), hence make sure that
@@ -87,6 +105,10 @@ if ischar(cfg.baseline)
   elseif strcmp(cfg.baseline, 'all')
     % do correction on the whole time interval
     cfg.baseline = [-inf inf];
+    % is input consistent?
+  elseif strcmp(cfg.baseline, 'no')
+    warning('no baseline correction done');
+    return;
   end
 end
 
@@ -100,34 +122,48 @@ if ~(ischar(cfg.baseline) && strcmp(cfg.baseline, 'no'))
   % update the configuration
   cfg.baseline(1) = timelock.time(tbeg);
   cfg.baseline(2) = timelock.time(tend);
-   
-  if isfield(timelock, 'avg')
-    timelock.avg(chansel,:) = ft_preproc_baselinecorrect(timelock.avg(chansel,:), tbeg, tend);
-  end
   
-  if strcmp(timelock.dimord, 'rpt_chan_time')
-    fprintf('applying baseline correction on each individual trial\n');
-    ntrial = size(timelock.trial,1);
-    for i=1:ntrial
-      timelock.trial(i,chansel,:) = ft_preproc_baselinecorrect(shiftdim(timelock.trial(i,chansel,:),1), tbeg, tend);
+   for k = 1:numel(cfg.parameter)
+    par = cfg.parameter{k};
+  
+    % this if-statement is just there to give more specific text output
+    if isequal(par, 'trial')
+      fprintf('applying baseline correction on each individual trial\n');
+      ntrial = size(timelock.(par),1);
+      for i=1:ntrial
+        timelock.(par)(i,chansel,:) = ft_preproc_baselinecorrect(shiftdim(timelock.(par)(i,chansel,:),1), tbeg, tend);
+      end
+    elseif isequal(par, 'individual')
+      fprintf('applying baseline correction on each individual subject\n');
+      nsubj = size(timelock.(par),1);
+      for i=1:nsubj
+        timelock.(par)(i,chansel,:) = ft_preproc_baselinecorrect(shiftdim(timelock.(par)(i,chansel,:),1), tbeg, tend);
+      end
+    else
+      fprintf('applying baseline correction on %s\n', par);
+      d = ndims(timelock.(par));
+      if d == 3
+        for i=1:size(timelock.(par),1)
+          timelock.(par)(i,chansel,:) = ft_preproc_baselinecorrect(shiftdim(timelock.(par)(i,chansel,:),1), tbeg, tend);
+        end
+      elseif d == 2
+        timelock.(par)(chansel,:) = ft_preproc_baselinecorrect(timelock.(par)(chansel,:), tbeg, tend);
+      else
+        warning('Not doing anything -  matrices up to only three dimensions are supported');
+      end
+
     end
-  elseif strcmp(timelock.dimord, 'subj_chan_time')
-    fprintf('applying baseline correction on each individual subject\n');
-    nsubj = size(timelock.individual,1);
-    for i=1:nsubj
-      timelock.individual(i,chansel,:) = ft_preproc_baselinecorrect(shiftdim(timelock.individual(i,chansel,:),1), tbeg, tend);
+
+    if isfield(timelock, 'var')
+      fprintf('baseline correction invalidates previous variance estimate, removing var\n');
+      timelock = rmfield(timelock, 'var');
     end
-  end
-  
-  if isfield(timelock, 'var')
-    fprintf('baseline correction invalidates previous variance estimate, removing var\n');
-    timelock = rmfield(timelock, 'var');
-  end
-  
-  if isfield(timelock, 'cov')
-    fprintf('baseline correction invalidates previous covariance estimate, removing cov\n');
-    timelock = rmfield(timelock, 'cov');
-  end
+
+    if isfield(timelock, 'cov')
+      fprintf('baseline correction invalidates previous covariance estimate, removing cov\n');
+      timelock = rmfield(timelock, 'cov');
+    end
+   end
   
 end % ~strcmp(cfg.baseline, 'no')
 
