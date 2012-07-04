336f9c2239017888a7eb6bacc52a658f4b9fba29
Jorn Horschig <jm.horschig@donders.ru.nl>
2012-07-04 13:18:14 +0000

    enhancement-#bug1562-throws a warning if doing SSI to check for spherical coordinates
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6230 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_channelrepair.m
+++ ft_channelrepair.m
@@ -257,6 +257,8 @@ if strcmp(cfg.method, 'nearest')
   
   
 elseif strcmp(cfg.method, 'spline') || strcmp(cfg.method, 'slap')
+  warning('make sure that your sensor coordinates are in spherical coordinates');
+  
   if ~isempty(cfg.badchannel) || ~isempty(cfg.missingchannel)
     fprintf('Spherical spline and surface Laplacian interpolation will treat bad and missing channels the same. Missing channels will be concatenated.\n');
   end
