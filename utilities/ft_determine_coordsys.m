6dcb19ed9c3418662a9bf0b11d3af7d5f37768df
Robert Oostenveld <r.oostenveld@gmail.com>
2012-07-08 09:14:55 +0000

    documentation - extended the help, list the coordinate systems
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6247 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/ft_determine_coordsys.m
+++ utilities/ft_determine_coordsys.m
@@ -5,23 +5,27 @@ function [data] = ft_determine_coordsys(data, varargin)
 % labels for the coordinate system axes.
 %
 % Use as
-%   [dataout] = ft_determine_coordsys(datain, 'key1', value1, ...)
+%   [dataout] = ft_determine_coordsys(datain, ...)
 % where the input data structure can be
 %  - an anatomical MRI
 %  - an electrode or gradiometer definition
-%  - a volume conduction model
+%  - a volume conduction model of the head
 % or most other FieldTrip structures that represent geometrical information.
 %
 % Additional optional input arguments should be specified as key-value pairs 
 % and can include
 %   interactive  = string, 'yes' or 'no' (default = 'yes')
+%   axisscale    = scaling factor for the reference axes and sphere (default = 1)
 %
 % This function wil pop up a figure that allows you to check whether the
 % alignment of the object relative to the coordinate system axes is correct
-% and what the anatomical labels of the coordinate system axes are. You should
-% switch on the 3D rotation option in the figure panel to rotate and see the
-% figure from all angles. To change the anatomical labels of the coordinate
-% system, you should press the corresponding keyboard button.
+% and what the anatomical labels of the coordinate system axes are. You
+% should switch on the 3D rotation option in the figure panel to rotate and
+% see the figure from all angles. To change the anatomical labels of the
+% coordinate system, you should press the corresponding keyboard button.
+%
+% Recognized and supported coordinate systems include: ctf, 4d, bti, itab,
+% neuromag, spm, mni, als, ras. T
 %
 % See also FT_VOLUMEREALIGN, FT_VOLUMERESLICE
 
