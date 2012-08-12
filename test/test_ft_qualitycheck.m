320fd35a2f22c6b7fc11325d4e424f8fe80fbc79
Arjen Stolk <a.stolk@donders.ru.nl>
2012-08-12 15:54:58 +0000

    enhancement
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6349 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_ft_qualitycheck.m
+++ test/test_ft_qualitycheck.m
@@ -6,15 +6,9 @@ function test_ft_qualitycheck
 global ft_default;
 ft_default.feedback = 'no';
 
-pwdir = pwd;
-cd('/home/common/matlab/fieldtrip/data/test/original/meg/');
-
-cd('ctf275');
 cfg           = [];
-cfg.dataset   = 'A0132_Aud-Obj-Recognition_20051115_02.ds'; 
+cfg.dataset   = '/home/common/matlab/fieldtrip/data/test/original/meg/ctf275/A0132_Aud-Obj-Recognition_20051115_02.ds'; 
 cfg.savemat   = 'no';
 cfg.visualize = 'no';
 cfg.saveplot  = 'no';
 ft_qualitycheck(cfg);
-
-cd(pwdir);
