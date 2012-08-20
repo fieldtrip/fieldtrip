04576021bea66764c4acca88cbe9a7c6f62e2e7d
Robert Oostenveld <r.oostenveld@gmail.com>
2012-08-20 08:50:56 +0000

    bugfix - also deal with linux case when HOSTNAME is empty
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6383 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/private/gethostname.m
+++ utilities/private/gethostname.m
@@ -1,6 +1,7 @@
 function host = gethostname()
 
 % this is to speed up subsequent calls
+% the hostname will not change over multiple calls
 persistent previous_argout
 if ~isempty(previous_argout)
   host = previous_argout;
@@ -11,7 +12,13 @@ if (ispc())
   host = getenv('COMPUTERNAME');
 else
   host = getenv('HOSTNAME');
+  if isempty(host)
+    [status, host] = system('hostname -s');
+  end
 end
+
+% remove trailing whitespace and blank lines
+host = strtrim(host);
 
 if (isempty(host))
   host = 'unknown';
