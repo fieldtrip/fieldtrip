16a6b8dddf19f3c447c6f9f1987efe0f797b590e
Robert Oostenveld <r.oostenveld@gmail.com>
2012-08-22 14:12:24 +0000

    enhancement - made the test more elaborate to catch another failure, see the bugzilla comments. Note that I have not solved the bug yet, but it has been pinpointed in http://bugzilla.fcdonders.nl/show_bug.cgi?id=1490
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6395 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1490.m
+++ test/test_bug1490.m
@@ -53,3 +53,32 @@ if isempty(reference)
   save(referencefile, 'hdr', 'dat');
 end
 
+%% this is a second test, pertaining to http://bugzilla.fcdonders.nl/show_bug.cgi?id=1490#c11
+
+filename = fullfile(datadir, filelist{2});
+hdr = ft_read_header(filename);
+% try to titrate the option related to the error, the first ones all fail
+dat = ft_read_data(filename, 'header',  hdr, 'begsample',  105704, 'endsample', 206440, 'chanindx', 1:hdr.nChans, 'checkboundary', 1);
+dat = ft_read_data(filename, 'header',  hdr, 'begsample',  105704, 'endsample', 206440, 'chanindx', 1:hdr.nChans);
+dat = ft_read_data(filename, 'header',  hdr, 'begsample',  105704, 'endsample', 206440, 'chanindx', 1);
+dat = ft_read_data(filename,                 'begsample',  105704, 'endsample', 206440, 'chanindx', 1);
+dat = ft_read_data(filename,                 'begsample',  105704, 'endsample', inf,    'chanindx', 1); % this failed on 22 aug 2012
+dat = ft_read_data(filename,                 'begsample',  105704, 'endsample', inf);                   % this failed on 22 aug 2012
+dat = ft_read_data(filename,                 'begsample',       1, 'endsample', inf,    'chanindx', 1); % this worked on 22 aug 2012
+dat = ft_read_data(filename,                 'begsample',       1, 'endsample', inf);                   % this worked on 22 aug 2012
+
+% this particular file seems to be in 40-sample blocks
+dataformat= 'ns_cnt16'; % ns_cnt16, ns_cnt32
+dat = ft_read_data(filename, 'dataformat', dataformat, 'begsample', 1, 'endsample', 80);
+assert(size(dat,2)==80, 'incorrect number of samples');
+dat = ft_read_data(filename, 'dataformat', dataformat, 'begsample', 1, 'endsample', 40);
+assert(size(dat,2)==40, 'incorrect number of samples');
+dat = ft_read_data(filename, 'dataformat', dataformat, 'begsample', 1, 'endsample', 39);
+assert(size(dat,2)==39, 'incorrect number of samples');
+dat = ft_read_data(filename, 'dataformat', dataformat, 'begsample', 1, 'endsample', 41);
+assert(size(dat,2)==41, 'incorrect number of samples');
+dat = ft_read_data(filename, 'dataformat', dataformat, 'begsample', 2, 'endsample', 41);
+assert(size(dat,2)==40, 'incorrect number of samples');
+
+
+
