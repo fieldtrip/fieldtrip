3838d202b53abed773985897893b8a891180a15b
Johanna Zumer <j.zumer@donders.ru.nl>
2012-08-22 15:26:18 +0000

    dashboard fail fixes.  pathname had changed to include /latest/
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6399 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/test/test_ft_compute_leadfield.m
+++ forward/test/test_ft_compute_leadfield.m
@@ -8,22 +8,22 @@ function test_ft_compute_leadfield
 % FIXME eeg
 
 % meg singlesphere
-vol = ft_read_vol('/home/common/matlab/fieldtrip/data/test/vol/Subject01vol_singlesphere.mat');
-load('/home/common/matlab/fieldtrip/data/test/raw/meg/preproc_ctf151');
+vol = ft_read_vol('/home/common/matlab/fieldtrip/data/test/latest/vol/Subject01vol_singlesphere.mat');
+load('/home/common/matlab/fieldtrip/data/test/latest/raw/meg/preproc_ctf151');
 sens = data.grad;
 [vol, sens] = ft_prepare_vol_sens(vol, sens);
 lf = ft_compute_leadfield([4 3 8], sens, vol);
 
 % meg singleshell
-vol = ft_read_vol('/home/common/matlab/fieldtrip/data/test/vol/Subject01vol_singleshell.mat');
-load('/home/common/matlab/fieldtrip/data/test/raw/meg/preproc_ctf151');
+vol = ft_read_vol('/home/common/matlab/fieldtrip/data/test/latest/vol/Subject01vol_singleshell.mat');
+load('/home/common/matlab/fieldtrip/data/test/latest/raw/meg/preproc_ctf151');
 sens = data.grad;
 [vol, sens] = ft_prepare_vol_sens(vol, sens);
 lf = ft_compute_leadfield([4 3 8], sens, vol);
 
 % meg localsphere
-vol = ft_read_vol('/home/common/matlab/fieldtrip/data/test/vol/Subject01vol_localspheres.mat');
-load('/home/common/matlab/fieldtrip/data/test/raw/meg/preproc_ctf151');
+vol = ft_read_vol('/home/common/matlab/fieldtrip/data/test/latest/vol/Subject01vol_localspheres.mat');
+load('/home/common/matlab/fieldtrip/data/test/latest/raw/meg/preproc_ctf151');
 sens = data.grad;
 [vol, sens] = ft_prepare_vol_sens(vol, sens);
 lf = ft_compute_leadfield([4 3 8], sens, vol);
