517852f5e67acac9d70441cb3d45741e6cdbc036
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2012-08-28 12:01:31 +0000

    switched to ft_freqanalysis for freqdata
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6412 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/browse_simpleFFT.m
+++ private/browse_simpleFFT.m
@@ -12,38 +12,31 @@ function browse_simpleFFT(cfg, data)
 
 % Copyright (C) 2011, Roemer van der Meij
 
+% call ft_freqanalysis on data
+cfgfreq = [];
+cfgfreq.method = 'mtmfft';
+cfgfreq.taper  = 'boxcar';
+freqdata = ft_freqanalysis(cfgfreq,data);
 
-
-% select  the requested data segment and do fft
-dat     = data.trial{1};
-fsample = data.fsample;
-nsample = size(dat,2);
-fftdat = transpose(fft(transpose(dat))); % double
-% scale the same way as mtmfft
-fftdat = fftdat .* sqrt(2 ./ size(dat,2));
-% compute powerspectrum (my pref would be to actually don't square, to make line spectra easier to visualize in case of low freq stuff, but doing it so people dont get confused)
-fftdat = abs(fftdat) .^ 2;
-
-% create proper xaxis
-freqboilim = round([0 fsample/2] ./ (fsample ./ nsample)) + 1;
-freqboi    = freqboilim(1):1:freqboilim(2);
-freqoi     = (freqboi-1) ./ (nsample * (1/fsample));
-xaxis = freqoi;
+% remove zero-bin for plotting
+if freqdata.freq(1) == 0
+  freqdata.freq = freqdata.freq(2:end);
+  freqdata.powspctrm = freqdata.powspctrm(:,2:end);
+end
+  
+%%% FIXME: call ft_singleplotER for everything below 
 
 % make figure window for fft
 ffth = figure('name',cfg.figurename,'numbertitle','off','units','normalized');
 % set button
-butth = uicontrol('tag', 'simplefft_l2', 'parent', ffth, 'units', 'normalized', 'style', 'togglebutton', 'string','log10 of power','position', [0.87, 0.6 , 0.12, 0.10],  'value',1,'callback',{@draw_simple_fft_cb});
+butth = uicontrol('tag', 'simplefft_l2', 'parent', ffth, 'units', 'normalized', 'style', 'checkbox', 'string','log10 of power','position', [0.87, 0.6 , 0.12, 0.05],  'value',1,'callback',{@draw_simple_fft_cb},'backgroundcolor',[.8 .8 .8]);
 uicontrol('tag', 'simplefft_l2_chansel', 'parent', ffth, 'units', 'normalized', 'style', 'pushbutton', 'string','select channels','position', [0.87, 0.45 , 0.12, 0.10],'callback',{@selectchan_fft_cb});
 
 % put dat in fig (sparse)
 fftopt = [];
+fftopt.freqdata   = freqdata;
 fftopt.chancolors = cfg.chancolors;
-fftopt.xaxis      = xaxis;
-fftopt.xaxis      = xaxis;
-fftopt.chanlabel  = data.label;
-fftopt.chansel    = 1:size(fftdat,1);
-fftopt.fftdat     = fftdat(:,freqboi);
+fftopt.chansel    = 1:numel(data.label);
 fftopt.butth      = butth;
 setappdata(ffth, 'fftopt', fftopt);
 
@@ -58,7 +51,7 @@ ffth = get(h,'parent');
 fftopt = getappdata(ffth, 'fftopt');
 
 % open chansel dialog
-chansel = select_channel_list(fftopt.chanlabel, fftopt.chansel, 'select channels for viewing power');
+chansel = select_channel_list(fftopt.opt.freq.label, fftopt.chansel, 'select channels for viewing power');
 
 % output data
 fftopt.chansel = chansel;
@@ -77,11 +70,9 @@ cla
 
 % switch log10 or nonlog10
 if togglestate == 0
-  dat = fftopt.fftdat;
-  set(h,'backgroundColor',[0.8 0.8 0.8])
-elseif togglestate == 1
-  dat = log10(fftopt.fftdat);
-  set(h,'backgroundColor','g')
+  dat = fftopt.freqdata.powspctrm;
+ elseif togglestate == 1
+  dat = log10(fftopt.freqdata.powspctrm);
 end
 
 % select data and chanel colors
@@ -92,20 +83,20 @@ dat        = dat(fftopt.chansel,:);
 set(0,'currentFigure',ffth)
 for ichan = 1:size(dat,1)
   color = chancolors(ichan,:);
-  ft_plot_vector(fftopt.xaxis, dat(ichan,:), 'box', false, 'color', color)
+  ft_plot_vector(fftopt.freqdata.freq, dat(ichan,:), 'box', false, 'color', color)
 end
 ylabel('log10(power)')
 xlabel('frequency (hz)')
 yrange = abs(max(max(dat)) - min(min(dat)));
-axis([fftopt.xaxis(1) fftopt.xaxis(end) (min(min(dat)) - yrange.*.1) (max(max(dat)) + yrange*.1)]) 
+axis([fftopt.freqdata.freq(1) fftopt.freqdata.freq(end) (min(min(dat)) - yrange.*.1) (max(max(dat)) + yrange*.1)]) 
 
 % switch log10 or nonlog10
 if togglestate == 0
   ylabel('power')
-  axis([fftopt.xaxis(1) fftopt.xaxis(end) 0 (max(max(dat)) + yrange*.1)]) 
+  axis([fftopt.freqdata.freq(1) fftopt.freqdata.freq(end) 0 (max(max(dat)) + yrange*.1)]) 
 elseif togglestate == 1
   ylabel('log10(power)')
-  axis([fftopt.xaxis(1) fftopt.xaxis(end) (min(min(dat)) - yrange.*.1) (max(max(dat)) + yrange*.1)]) 
+  axis([fftopt.freqdata.freq(1) fftopt.freqdata.freq(end) (min(min(dat)) - yrange.*.1) (max(max(dat)) + yrange*.1)]) 
 end
 set(gca,'Position', [0.13 0.11 0.725 0.815])
 
