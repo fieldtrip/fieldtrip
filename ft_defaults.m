736785b654140e945623315b0bcdbc2fb9af0d6d
Robert Oostenveld <r.oostenveld@gmail.com>
2012-09-02 10:17:36 +0000

    enhancement - use explicit file prefixes for all checks on multiple toolbox installations
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6414 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_defaults.m
+++ ft_defaults.m
@@ -58,32 +58,32 @@ end
 % Some people mess up their path settings and then have
 % different versions of certain toolboxes on the path.
 % The following will issue a warning
-checkMultipleToolbox('FieldTrip',           'ft_defaults');
-checkMultipleToolbox('mne',                 'fiff_copy_tree');
+checkMultipleToolbox('FieldTrip',           'ft_defaults.m');
+checkMultipleToolbox('mne',                 'fiff_copy_tree.m');
 checkMultipleToolbox('eeglab',              'eeglab2fieldtrip.m');
 checkMultipleToolbox('dipoli',              'write_tri.m');
 checkMultipleToolbox('eeprobe',             'read_eep_avr.mexa64');
 checkMultipleToolbox('yokogawa',            'GetMeg160ChannelInfoM.p');
-checkMultipleToolbox('simbio',              'sb_compile_vista');
+checkMultipleToolbox('simbio',              'sb_compile_vista.m');
 checkMultipleToolbox('fns',                 'fns_region_read.m');
-checkMultipleToolbox('bemcp',               'bem_Cii_cst');
+checkMultipleToolbox('bemcp',               'bem_Cii_cst.mexa64');
 checkMultipleToolbox('bci2000',             'load_bcidat.m');
-checkMultipleToolbox('openmeeg',            'openmeeg_helper');
+checkMultipleToolbox('openmeeg',            'openmeeg_helper.m');
 checkMultipleToolbox('freesurfer',          'vox2ras_ksolve.m');
-checkMultipleToolbox('fastica',             'fastica');
+checkMultipleToolbox('fastica',             'fastica.m');
 checkMultipleToolbox('besa',                'readBESAmul.m');
-checkMultipleToolbox('neuroshare',          'ns_GetAnalogData');
+checkMultipleToolbox('neuroshare',          'ns_GetAnalogData.m');
 checkMultipleToolbox('ctf',                 'setCTFDataBalance.m');
 checkMultipleToolbox('afni',                'WriteBrikHEAD.m');
-checkMultipleToolbox('gifti',               '@gifti/display');
-checkMultipleToolbox('sqdproject',          'sqdread');
-checkMultipleToolbox('xml4mat',             'xml2mat');
+checkMultipleToolbox('gifti',               '@gifti/display.m');
+checkMultipleToolbox('sqdproject',          'sqdread.m');
+checkMultipleToolbox('xml4mat',             'xml2mat.m');
 checkMultipleToolbox('cca',                 'ccabss.m');
 checkMultipleToolbox('bsmart',              'armorf.m');
-checkMultipleToolbox('iso2mesh',            'iso2meshver');
+checkMultipleToolbox('iso2mesh',            'iso2meshver.m');
 checkMultipleToolbox('bct',                 'degrees_und.m');
 checkMultipleToolbox('yokogawa_meg_reader', 'getYkgwHdrEvent.p');
-checkMultipleToolbox('biosig',              'sopen');
+checkMultipleToolbox('biosig',              'sopen.m');
 
 % check for different SPM versions, which also includes a general warning about addpath(genpath(...))
 list = which('spm', '-all');
