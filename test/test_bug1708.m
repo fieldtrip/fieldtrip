c4c28fcca6cfc5f2d69802b2be74d7456e79e4e8
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-09-14 09:10:09 +0000

    added test script
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6457 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1708.m
@@ -0,0 +1,17 @@
+function test_bug1708
+
+% TEST: test_bug1708
+% TEST ft_denoise_synthetic
+
+% reported bug is that ft_denoise_synthetic leads to nans in coilpos and
+% coilori
+
+% try to reproduce first
+load('/home/common/matlab/fieldtrip/data/test/latest/raw/meg/preproc_ctf275.mat');
+
+cfg  = [];
+cfg.gradient = 'G3BR';
+data = ft_denoise_synthetic(cfg, data);
+
+assert(all(isfinite(data.grad.coilori(:))));
+assert(all(isfinite(data.grad.coilpos(:))));
