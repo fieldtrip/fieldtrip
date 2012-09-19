14c3c31c5331d5a5cae7b34e71ff702b0135a346
Robert Oostenveld <r.oostenveld@gmail.com>
2012-09-19 21:52:44 +0000

    enhacement - added test script for new anatomical atlas representations
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6487 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1725.m
@@ -0,0 +1,57 @@
+function test_bug1725
+
+% TEST test_bug1725
+% TEST ft_read_atlas ft_prepare_atlas
+
+% try to read a collection of atlasses
+% the first one is from AFNI
+filename = '/Users/robert/tmp/TTatlas+tlrc.BRIK';
+atlas = ft_read_atlas(filename)
+
+% these ones are from http://fmri.wfubmc.edu/
+filename = {
+% 'MNI_atlas_templates/MNI_T1.nii'
+  'MNI_atlas_templates/TD_brodmann.nii'
+  'MNI_atlas_templates/TD_hemisphere.nii'
+  'MNI_atlas_templates/TD_label.nii'
+% 'MNI_atlas_templates/TD_label_extended_modified.nii'
+  'MNI_atlas_templates/TD_lobe.nii'
+  'MNI_atlas_templates/TD_type.nii'
+  'MNI_atlas_templates/aal_MNI_V4.nii'
+  'MNI_atlas_templates/atlas116.nii'
+  'MNI_atlas_templates/atlas71.nii'
+  % 'TD-ICBM_MNI_atlas_templates/MNI_T1.nii'
+  'TD-ICBM_MNI_atlas_templates/TD_brodmann.nii'
+  'TD-ICBM_MNI_atlas_templates/TD_hemisphere.nii'
+  'TD-ICBM_MNI_atlas_templates/TD_label.nii'
+  'TD-ICBM_MNI_atlas_templates/TD_lobe.nii'
+  'TD-ICBM_MNI_atlas_templates/TD_type.nii'
+  % 'mouse_atlas_templates/c57_15_M_Normal_age12.nii'
+  'mouse_atlas_templates/mouseLabels.nii'
+  'rhesus_atlas_templates/SubCortical.nii'
+  % 'rhesus_atlas_templates/template.nii'
+  % 'rhesus_atlas_templates/templateSkull.nii'
+  'rhesus_atlas_templates/template_hardseg.nii'
+  'rhesus_atlas_templates/template_parc.nii'
+  % 'vervet_atlas_templates/Vervet_T1_Template_WholeHead.nii'
+  'vervet_atlas_templates/vervetLabels.nii'
+  'vervet_atlas_templates/vervetType.nii'
+  };
+
+for i=14:length(filename)
+  % just try to read it
+  disp(i);
+  disp(filename{i});
+  atlas = ft_read_atlas(filename{i})
+end
+
+% plot the template MRI together with the atlas
+mri = ft_read_mri('MNI_atlas_templates/MNI_T1.nii');
+atlas = ft_read_atlas('MNI_atlas_templates/aal_MNI_V4.nii'); % FIXME this is how it should be
+atlas = ft_prepare_atlas('MNI_atlas_templates/aal_MNI_V4.nii'); % use the old representation
+
+cfg = [];
+cfg.atlas = atlas;
+cfg.interactive = 'yes';
+ft_sourceplot(cfg, mri);
+
