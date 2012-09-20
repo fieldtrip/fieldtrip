deee6d77c2806657ec81e5bc3da31f1c3f75b86e
Jorn Horschig <jm.horschig@donders.ru.nl>
2012-09-20 09:32:13 +0000

    added testscript for bug #1637, and added a minor testscript update for ft_megplanar
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6491 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1637.m
@@ -0,0 +1,128 @@
+function test_bug1637
+% this function checks whether megplanar_sincos relies on a fixed channel
+% order or whether this can be totally mixed up (it should be able to deal
+% with that!)
+%
+% http://bugzilla.fcdonders.nl/show_bug.cgi?id=1637
+
+% TEST test_bug1637
+% TEST megplanar_sincos channelconnectivity ft_prepare_neighbours ft_channelselection
+
+
+% load neighbours
+cfg = [];
+cfg.method = 'template';
+cfg.template = 'CTF275_neighb';
+neighbours = ft_prepare_neighbours(cfg);
+
+load test_bug1637_hdr;
+load test_bug1637_grad;
+
+d = pwd;
+ftPath = fileparts(mfilename('fullpath')); % get path, strip away 'ft_defaults'
+ftPath = strrep(ftPath, '\', '\\');
+
+cd(fullfile(ftPath, '..', 'private'));
+errored = false;
+try
+  cfg = [];
+  cfg.neighbours = neighbours;
+  cfg.channel    = ft_channelselection('MEG', hdr.label);
+  [neighbsel] = match_str({cfg.neighbours.label}, cfg.channel);
+  cfg.neighbours = cfg.neighbours(neighbsel);
+  cfg.neighbsel = channelconnectivity(cfg);
+  
+  montage_other = megplanar_sincos(cfg, hdr.grad);
+  
+  montage = [];
+  montage{1} = megplanar_sincos(cfg, grad);
+  
+  [sel12, sel22] = match_str(montage{1}.labelorg, montage_other.labelorg);
+  [sel11, sel21] = match_str(montage{1}.labelnew, montage_other.labelnew);
+  if ~isequal(montage{1}.tra(sel11, sel12), montage_other.tra(sel21, sel22))
+    warning('tra matrix differs - but that''s because the montage_other.tra consists of nans - checking whether nonzero elements are the same')
+    idx1 = montage{1}.tra(sel11, sel12)~=0;
+    idx2 = montage_other.tra(sel21, sel22)~=0;
+    if ~isequal(idx1, idx2)
+      errored = true;
+      error('tra matrix qualitatively differ!');
+    else
+      disp('...passed!')
+    end
+  elseif ~(all(cellfun(@isequal, montage{1}.labelorg(sel12), montage_other.labelorg(sel22))))
+    errored = true;
+    error('labelorg mismatch');
+  elseif ~(all(cellfun(@isequal, montage{1}.labelnew(sel11), montage_other.labelnew(sel21))))
+    errored = true;
+    error('labelnew mismatch');
+  end
+  
+  for i=1:10 % 10 is an arbitrary number here, just do it for some time
+    labperm = randperm(numel(grad.label));
+    % permute everything
+    grad.chanori    = grad.chanori(labperm, :);
+    grad.chanpos    = grad.chanpos(labperm, :);
+    grad.chantype   = grad.chantype(labperm, :);
+    grad.chanunit   = grad.chanunit(labperm, :);
+    grad.coiloriri  = grad.coilori(labperm, :);
+    grad.coilpos    = grad.coilpos(labperm, :);
+    grad.label      = grad.label(labperm, :);
+    grad.tra        = grad.tra(labperm, :);
+    
+    montage{2} = megplanar_sincos(cfg, grad);
+    
+    % since we now reordered everything according to cfg.channel, the
+    % following should not be necessary - but hey!
+    [sel12, sel22] = match_str(montage{1}.labelorg, montage{2}.labelorg);
+    [sel11, sel21] = match_str(montage{1}.labelnew, montage{2}.labelnew);
+    if ~(isequal(montage{1}.tra(sel11, sel12), montage{2}.tra(sel21, sel22)))
+      errored = true;
+      error('tra matrix differs')
+    elseif ~(all(cellfun(@isequal, montage{1}.labelorg(sel12), montage{2}.labelorg(sel22))))
+      errored = true;
+      error('labelorg mismatch');
+    elseif ~(all(cellfun(@isequal, montage{1}.labelnew(sel11), montage{2}.labelnew(sel21))))
+      errored = true;
+      error('labelnew mismatch');
+    end
+  end
+  
+  
+  for i=1:10 % 10 is an arbitrary number here, just do it for some time
+    labperm = randperm(numel(grad.label));
+    % permute everything
+    grad.chanori    = hdr.grad.chanori(labperm, :);
+    grad.chanpos    = hdr.grad.chanpos(labperm, :);
+    grad.chantype   = hdr.grad.chantype(labperm, :);
+    grad.chanunit   = hdr.grad.chanunit(labperm, :);
+    grad.coiloriri  = hdr.grad.coilori(labperm, :);
+    grad.coilpos    = hdr.grad.coilpos(labperm, :);
+    grad.label      = hdr.grad.label(labperm, :);
+    grad.tra        = hdr.grad.tra(labperm, :);
+    
+    montage{2} = megplanar_sincos(cfg, hdr.grad);
+    
+    % since we now reordered everything according to cfg.channel, the
+    % following should not be necessary - but hey!
+    [sel12, sel22] = match_str(montage{1}.labelorg, montage{2}.labelorg);
+    [sel11, sel21] = match_str(montage{1}.labelnew, montage{2}.labelnew);
+    idx1 = montage{1}.tra(sel11, sel12)~=0;
+    idx2 = montage_other.tra(sel21, sel22)~=0;
+    if ~isequal(idx1, idx2)
+      errored = true;
+      error('tra matrix qualitatively differ!');
+    elseif ~(all(cellfun(@isequal, montage{1}.labelorg(sel12), montage_other.labelorg(sel22))))
+      errored = true;
+      error('labelorg mismatch');
+    elseif ~(all(cellfun(@isequal, montage{1}.labelnew(sel11), montage_other.labelnew(sel21))))
+      errored = true;
+      error('labelnew mismatch');
+    end
+  end
+  
+end
+cd(d)
+
+if errored
+  error('channelorder matters whereas it should not!');
+end
