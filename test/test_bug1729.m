7115be7e45d58907d75a741e1a9c0342d7accec6
Robert Oostenveld <r.oostenveld@gmail.com>
2012-09-20 14:22:41 +0000

    enhancement - added test script for bug 1729, this does not solve the bug
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6501 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1729.m
@@ -0,0 +1,42 @@
+function test_bug1729
+
+% TEST test_bug1729
+% TEST ft_multiplotTFR ft_singleplotTFR ft_plot_matrix
+
+% reproduce the incorrect display
+freq.label = {'chan'};
+freq.time = [0 1 2];
+freq.freq = [1:10 21:30];
+freq.powspctrm = zeros(1, 20, 3);
+freq.powspctrm(1,:,1) = freq.freq;
+freq.powspctrm(1,:,2) = freq.freq;
+freq.powspctrm(1,:,3) = freq.freq;
+freq.dimord = 'chan_freq_time';
+
+cfg = [];
+ft_singleplotTFR(cfg, freq);
+
+% try with another spacing, this also does not look
+freq.label = {'chan'};
+freq.time = [0 1 2];
+freq.freq = logspace(log10(1),log10(30),20);
+freq.powspctrm = zeros(1, 20, 3);
+freq.powspctrm(1,:,1) = 1:20;
+freq.powspctrm(1,:,2) = 1:20;
+freq.powspctrm(1,:,3) = 1:20;
+freq.dimord = 'chan_freq_time';
+
+cfg = [];
+ft_singleplotTFR(cfg, freq);
+
+% the following causes a probably unrelated problem
+% please also have a look at this
+freq.label = {'chan'};
+freq.time = 0;
+freq.freq = [1:10 21:30];
+freq.powspctrm = zeros(1, 20, 1);
+freq.powspctrm(1,:,1) = freq.freq;
+freq.dimord = 'chan_freq_time';
+
+cfg = [];
+ft_singleplotTFR(cfg, freq);
