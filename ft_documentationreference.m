eee4eff0bdb620f2653514901dfbf82d2a824442
Robert Oostenveld <r.oostenveld@gmail.com>
2012-09-27 08:10:10 +0000

    enhancement - added utilities and contrib/trentool for automatic generation of the reference documentation on the wiki
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6553 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_documentationreference.m
+++ ft_documentationreference.m
@@ -80,7 +80,13 @@ f15 = {f15.name}';
 f16 = dir(fullfile(p, 'contrib', 'spike', '*.m'));
 f16 = {f16.name}';
 
-funname = cat(1, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16);
+f17 = dir(fullfile(p, 'contrib', 'trentool', '*.m'));
+f17 = {f17.name}';
+
+f18 = dir(fullfile(p, 'utility', '*.m'));
+f18 = {f18.name}';
+
+funname = cat(1, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16, f17, f18);
 
 for i=1:length(funname)
   [p, funname{i}, x] = fileparts(funname{i});
