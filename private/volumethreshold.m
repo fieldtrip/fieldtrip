53ccc5eb800bb6f44b71e7f5d1121c18664402bb
Robert Oostenveld <r.oostenveld@gmail.com>
2012-09-28 07:59:03 +0000

    automatically synchronized identical files to revision 6565
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6566 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/volumethreshold.m
+++ private/volumethreshold.m
@@ -1,8 +1,16 @@
 function [output] = volumethreshold(input, thresh, str)
 
 % VOLUMETHRESHOLD is a helper function for segmentations
+%
+% See also VOLUMEFILLHOLES, VOLUMESMOOTH
 
 fprintf('thresholding %s at a relative threshold of %0.3f\n', str, thresh);
+
+% check for any version of SPM
+if ~ft_hastoolbox('spm')
+  % add SPM8 to the path
+  ft_hastoolbox('spm8', 1);
+end
 
 % mask by taking the negative of the brain, thus ensuring
 % that no holes are within the compartment and do a two-pass
