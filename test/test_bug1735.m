2c1ddf047deaada99ad1cd98ea30614fa1062753
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-01 14:31:36 +0000

    enhancement - made the checks in the test script more strict, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1735#c6
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6611 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1735.m
+++ test/test_bug1735.m
@@ -5,7 +5,30 @@ function test_bug1735
 
 load test_bug1735.mat
 
-cfg = [];
-test1 = ft_appenddata([], data_trgtOnstimOnBTrgtLDetected_sourceL, data_trgtOnstimOnBTrgtLNotDetected_sourceL);
+part1 = data_trgtOnstimOnBTrgtLDetected_sourceL;
+part2 = data_trgtOnstimOnBTrgtLNotDetected_sourceL;
 
-assert(length(test1.trial)==79);
+cfg = [];
+combined = ft_appenddata(cfg, part1, part2);
+
+assert(length(combined.trial)==79);
+
+N1 = size(part1.sampleinfo,1);
+N2 = size(part2.sampleinfo,1);
+
+for i=1:N1
+  disp(i)
+  assert(isequal(part1.trial{i}, combined.trial{i}));
+end
+
+for i=1:N2
+  disp(i)
+  assert(isequal(part2.trial{i}, combined.trial{i+N1}));
+end
+
+% the data in the two conditions should be different
+for i=1:N1
+  for j=1:N2
+    assert(~isequal(part1.trial{i}, part2.trial{j}));
+  end
+end
