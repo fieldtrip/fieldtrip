7d421117519adbbc40219872a624f451b72956f2
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-02 16:08:20 +0000

    bugfix - don't crash on an input headmodel that does not contain the type field, see bug 1756
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6635 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1756.m
+++ test/test_bug1756.m
@@ -1,7 +1,7 @@
 function test_bug1756
 
 % TEST test_bug1756
-% TEST assert(ft_voltype ft_prepare_headmodel
+% TEST ft_voltype ft_prepare_headmodel
 
 cd /home/common/matlab/fieldtrip/data/test/bug1756
 
@@ -26,39 +26,39 @@ cfg = [];
 % EEG volume conduction models
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-cfg.method ='singlesphere'       % analytical single sphere model
+cfg.method = 'singlesphere'; % analytical single sphere model
 singlesphere_eeg = ft_prepare_headmodel(cfg, mesh1);
 
-cfg.method ='concentricspheres'  % analytical concentric sphere model with up to 4 spheres
+cfg.method = 'concentricspheres'; % analytical concentric sphere model with up to 4 spheres
 concentricspheres = ft_prepare_headmodel(cfg, mesh3);
 
-% this one does not run on my (=roboos) Apple Desktop computer, hence I skip it for the moment
-% cfg.method ='openmeeg'           % boundary element method, based on the OpenMEEG software
+% this one does not run on my ( = roboos) Apple Desktop computer, hence I skip it for the moment
+% cfg.method = 'openmeeg' % boundary element method, based on the OpenMEEG software
 % openmeeg = ft_prepare_headmodel(cfg, mesh3);
 
-cfg.method ='bemcp'              % boundary element method, based on the implementation from Christophe Phillips
+cfg.method = 'bemcp'; % boundary element method, based on the implementation from Christophe Phillips
 bemcp = ft_prepare_headmodel(cfg, mesh3);
 
-cfg.method ='dipoli'             % boundary element method, based on the implementation from Thom Oostendorp
+cfg.method = 'dipoli'; % boundary element method, based on the implementation from Thom Oostendorp
 dipoli = ft_prepare_headmodel(cfg, mesh3);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % MEG volume conduction models
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-cfg.method ='singlesphere'       % analytical single sphere model
+cfg.method = 'singlesphere'; % analytical single sphere model
 singlesphere_meg = ft_prepare_headmodel(cfg, mesh1);
 
-cfg.method ='localspheres'       % local spheres model for MEG, one sphere per channel
+cfg.method = 'localspheres'; % local spheres model for MEG, one sphere per channel
 cfg.grad = grad;
 localspheres = ft_prepare_headmodel(cfg, mesh1);
 cfg = rmfield(cfg, 'grad');
 
-cfg.method ='singleshell'        % realisically shaped single shell approximation, based on the implementation from Guido Nolte
+cfg.method = 'singleshell'; % realisically shaped single shell approximation, based on the implementation from Guido Nolte
 singleshell = ft_prepare_headmodel(cfg, mesh1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% ensure assert(ft_voltype works for each of the volume conduction models
+% ensure ft_voltype works for each of the volume conduction models
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 assert(ft_voltype(bemcp, 'bemcp'))
@@ -69,8 +69,30 @@ assert(ft_voltype(localspheres, 'localspheres'))
 assert(ft_voltype(singlesphere_eeg, 'singlesphere'))
 assert(ft_voltype(singlesphere_meg, 'singlesphere'))
 
-assert(ft_voltype(bemcp, 'bem'))
-assert(ft_voltype(dipoli, 'bem'))
+assert( ft_voltype(bemcp, 'bem'))
+assert( ft_voltype(dipoli, 'bem'))
 assert(~ft_voltype(singleshell, 'bem'))
+
+% remove the type and see whether the function can recover it
+t_bemcp = rmfield(bemcp,'type');
+t_dipoli = rmfield(dipoli,'type');
+t_singleshell = rmfield(singleshell,'type');
+t_concentricspheres = rmfield(concentricspheres,'type');
+t_localspheres = rmfield(localspheres,'type');
+t_singlesphere_eeg = rmfield(singlesphere_eeg,'type');
+t_singlesphere_meg = rmfield(singlesphere_meg,'type');
+
+% these three are unknown, as they cannot be distinguished
+% this is where the original bug crashed on
+ft_voltype(t_bemcp, 'bemcp');
+ft_voltype(t_dipoli, 'dipoli');
+ft_voltype(t_singleshell, 'singleshell');
+% the following ones can be detected on the fly
+assert(ft_voltype(t_concentricspheres, 'concentricspheres'))
+assert(ft_voltype(t_localspheres, 'localspheres'))
+assert(ft_voltype(t_singlesphere_eeg, 'singlesphere'))
+assert(ft_voltype(t_singlesphere_meg, 'singlesphere'))
+
+
 
 
