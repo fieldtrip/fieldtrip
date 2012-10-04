b62b9711a9c4679fc8b5e82637d61a3d56223603
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-04 20:51:41 +0000

    enhancement - removed try-catch from test script, compare the leadfields rather than vol structures
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6691 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/test/test_headmodel_localspheres_new_old.m
+++ forward/test/test_headmodel_localspheres_new_old.m
@@ -1,5 +1,8 @@
 function test_headmodel_localspheres_new_old
 
+% TEST test_headmodel_localspheres_new_old
+% TEST ft_prepare_localspheres ft_prepare_headmodel
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % get the data which is needed
 
@@ -17,7 +20,7 @@ shape = ft_read_headshape(hdmfile);
 %%%%%%%%%%%%%%%%%%%%%
 % do the computations
 
-% new way - high level 
+% new way - high level
 cfg         = [];
 cfg.method  = 'localspheres';
 cfg.grad    = grad;
@@ -25,7 +28,7 @@ cfg.singlesphere = 'yes';
 cfg.hdmfile = hdmfile;
 vol{1}      = ft_prepare_headmodel(cfg);
 
-% old way 
+% old way
 cfg              = [];
 cfg.singlesphere = 'yes';
 cfg.headshape    = hdmfile;
@@ -33,14 +36,14 @@ cfg.grad         = grad;
 cfg.singlesphere = 'yes';
 vol{2}           = ft_prepare_localspheres(cfg);
 
-% old way 
+% old way
 cfg              = [];
 cfg.headshape    = shape;
 cfg.singlesphere = 'yes';
 cfg.grad         = grad;
 vol{3}           = ft_prepare_localspheres(cfg);
 
-% new way - high level 
+% new way - high level
 cfg         = [];
 cfg.method  = 'localspheres';
 cfg.singlesphere = 'yes';
@@ -51,21 +54,31 @@ vol{4}      = ft_prepare_headmodel(cfg);
 % compare the volume conductor structures
 comb = nchoosek(1:numel(vol),2);
 
-for i=1:size(comb,1)
-  chk = comb(i,:);
-  try
-    if ~isequal(vol{chk(1)},vol{chk(2)})
-      str = sprintf('combination %d %d not successful\n',chk(1),chk(2));
-      error(str)
-    end
-  catch me
-    fprintf(me.message)
+% compute the leadfields for a comparison
+[vol{1}, grad] = ft_prepare_vol_sens(vol{1}, grad);
+[vol{2}, grad] = ft_prepare_vol_sens(vol{2}, grad);
+[vol{3}, grad] = ft_prepare_vol_sens(vol{3}, grad);
+[vol{4}, grad] = ft_prepare_vol_sens(vol{3}, grad);
+lf{1} = ft_compute_leadfield([0 10 60], grad, vol{1});
+lf{2} = ft_compute_leadfield([0 10 60], grad, vol{2});
+lf{3} = ft_compute_leadfield([0 10 60], grad, vol{3});
+lf{4} = ft_compute_leadfield([0 10 60], grad, vol{4});
+
+% compare the leadfields in all possible combinations
+comb = nchoosek(1:numel(vol),2);
+for j=1:size(comb,1)
+  chk = comb(j,:);
+  err = norm(lf{chk(1)} - lf{chk(2)}) / norm(lf{chk(1)});
+  if err>0.001
+    error('combination %d %d not successful\n',chk(1),chk(2));
   end
 end
 
-clear vol
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% new way - high level 
+clear vol lf
+
+% new way - high level
 cfg         = [];
 cfg.method  = 'localspheres';
 cfg.grad    = grad;
@@ -73,19 +86,19 @@ cfg.hdmfile = hdmfile;
 vol{1}      = ft_prepare_headmodel(cfg);
 vol{1} = rmfield(vol{1},'unit');
 
-% old way 
+% old way
 cfg              = [];
 cfg.headshape    = hdmfile;
 cfg.grad         = grad;
 vol{2}           = ft_prepare_localspheres(cfg);
 
-% old way 
+% old way
 cfg              = [];
 cfg.headshape    = shape;
 cfg.grad         = grad;
 vol{3}           = ft_prepare_localspheres(cfg);
 
-% new way - high level 
+% new way - high level
 cfg         = [];
 cfg.method  = 'localspheres';
 cfg.grad    = grad;
@@ -93,18 +106,24 @@ cfg.geom    = shape;
 vol{4}      = ft_prepare_headmodel(cfg);
 vol{4} = rmfield(vol{4},'unit');
 
-% compare the volume conductor structures
-comb = nchoosek(1:numel(vol),2);
+% compute the leadfields for a comparison
+[vol{1}, grad] = ft_prepare_vol_sens(vol{1}, grad);
+[vol{2}, grad] = ft_prepare_vol_sens(vol{2}, grad);
+[vol{3}, grad] = ft_prepare_vol_sens(vol{3}, grad);
+[vol{4}, grad] = ft_prepare_vol_sens(vol{3}, grad);
+lf{1} = ft_compute_leadfield([0 10 60], grad, vol{1});
+lf{2} = ft_compute_leadfield([0 10 60], grad, vol{2});
+lf{3} = ft_compute_leadfield([0 10 60], grad, vol{3});
+lf{4} = ft_compute_leadfield([0 10 60], grad, vol{4});
 
-for i=1:size(comb,1)
-  chk = comb(i,:);
-  try
-    if ~isequal(vol{chk(1)},vol{chk(2)})
-      str = sprintf('combination %d %d not successful\n',chk(1),chk(2));
-      error(str)
-    end
-  catch me
-    fprintf(me.message)
+% compare the leadfields in all possible combinations
+comb = nchoosek(1:numel(vol),2);
+for j=1:size(comb,1)
+  chk = comb(j,:);
+  err = norm(lf{chk(1)} - lf{chk(2)}) / norm(lf{chk(1)});
+  if err>0.001
+    error('combination %d %d not successful\n',chk(1),chk(2));
   end
 end
+
 
