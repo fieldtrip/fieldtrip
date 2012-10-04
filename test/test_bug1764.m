19d139d025d786a14e02d7cf9e458e5fccd9cb23
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-04 07:47:52 +0000

    enhancement - adedd test script for simple singleshell construction, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1764
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6668 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1764.m
@@ -0,0 +1,24 @@
+function test_bug1764
+
+% TEST test_bug1764
+% TEST ft_prepare_singleshell ft_prepare_mesh
+
+% This test is inspired by test test_tutorial_beamformer20120321 which uses
+% a call to ft_prepare_singleshell without specifying any cfg options. The
+% idea is of course to get a singleshell headmodel based on the brain. This
+% should not take long. If it does, it might be because ft_prepare_mesh is
+% meshing the complex gray, white and csf segmentations rather than the
+% simple brain.
+
+load('/home/common/matlab/fieldtrip/data/ftp/tutorial/beamformer/segmentedmri.mat')
+
+stopwatch = tic;
+
+cfg = [];
+vol = ft_prepare_singleshell(cfg, segmentedmri);
+
+elapsed = toc(stopwatch);
+
+if elapsed>60
+  error('ft_prepare_singleshell took too long, probably due to ft_prepare_mesh');
+end
