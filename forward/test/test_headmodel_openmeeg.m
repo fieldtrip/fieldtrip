0b0e52c08a6d8c1442c5d9dd24c5d6878f8db0f3
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-10-04 07:56:21 +0000

    bugfix - fixed some issues that caused the test function to crash
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6674 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/test/test_headmodel_openmeeg.m
+++ forward/test/test_headmodel_openmeeg.m
@@ -15,7 +15,7 @@ geom.bnd(2).tri = tri;
 geom.bnd(3).pnt = pnt * 80;
 geom.bnd(3).tri = tri;
 
-vol = ft_headmodel_bem_openmeeg(geom, 'conductivity', [1 1/20 1]);
+vol = ft_headmodel_openmeeg(geom, 'conductivity', [1 1/20 1]);
 
 % create a set of electrodes
 sel = find(pnt(:,3)>0);
@@ -74,7 +74,7 @@ geom = [];
 geom.bnd(1).pnt = pnt * 100;
 geom.bnd(1).tri = tri;
 
-vol = ft_headmodel_bem_openmeeg(geom, 'conductivity', [1/80]);
+vol = ft_headmodel_openmeeg(geom, 'conductivity', [1/80]);
 
 % project the electrodes on the volume conduction model
 [vol, sens] = ft_prepare_vol_sens(vol, sens);
