aa3d1312d389db7cac07bbabfdb31c81ae4b7f31
Jorn Horschig <jm.horschig@donders.ru.nl>
2012-10-05 09:28:13 +0000

    seriously guys, if you are writing a test script make at least sure that the variables you are using in the script itself are assigned in the test script
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6702 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1412.m
+++ test/test_bug1412.m
@@ -5,7 +5,7 @@ function test_bug1412
 
 filename = '/home/common/matlab/fieldtrip/data/test/bug1412/Sub1_1.cnt';
 
-ft_read_header(filename)
+hdr = ft_read_header(filename)
 
 assert(hdr.nSamples == 1505200);
 
