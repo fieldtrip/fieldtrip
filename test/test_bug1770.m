f86bf1bee82c0471ecbabebcd67e1bc760c93562
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-08 19:18:17 +0000

    enhancement - created test script for the NRD data from Conrado
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6706 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1770.m
@@ -0,0 +1,35 @@
+% function test_bug1770
+
+% TEST test_bug1770
+% TEST read_neuralynx_dma read_neuralynx_ncs
+
+filenameA = '/home/common/matlab/fieldtrip/data/test/bug1770/2012-07-14_15-33-09';
+filenameB = '/home/common/matlab/fieldtrip/data/test/bug1770/DigitalLynxRawDataFile.nrd';
+
+hdrA = ft_read_header(filenameA);
+hdrB = ft_read_header(filenameB);
+
+begsample = 1;
+endsample = 100000;
+
+datA = ft_read_data(filenameA, 'begsample', begsample, 'endsample', endsample);
+datB = ft_read_data(filenameB, 'begsample', begsample, 'endsample', endsample);
+
+ch1A = double(datA( 1,:));
+ch1B = double(datB(18,:));
+
+figure
+subplot(2,1,1);
+plot(ft_preproc_standardize(ch1A))
+subplot(2,1,2);
+plot(-ft_preproc_standardize(ch1B))
+
+ch1As =  ft_preproc_standardize(ch1A);
+ch1Bs = -ft_preproc_standardize(ch1B);
+
+figure
+hold on
+plot(ch1As, 'b.');
+plot(ch1Bs, 'r.');
+
+% sofar there seems no reason for concern w.r.t. the reading function
