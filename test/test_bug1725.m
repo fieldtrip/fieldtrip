404af6dd9d7262643727e63aa4cb7d038af3e77a
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-10 12:47:02 +0000

    enhancement - copied the test data to the common directory, don't do the interactive plot at the end
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6727 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1725.m
+++ test/test_bug1725.m
@@ -1,20 +1,24 @@
-function test_bug1725
+% function test_bug1725
 
 % TEST test_bug1725
 % TEST ft_read_atlas ft_prepare_atlas
 
+cd /home/common/matlab/fieldtrip/data/test/bug1725/afni
+
 % try to read a collection of atlasses
 % the first one is from AFNI
-filename = '/Users/robert/tmp/TTatlas+tlrc.BRIK';
-atlas = ft_read_atlas(filename)
+filename = 'TTatlas+tlrc.BRIK';
+atlas = ft_read_atlas(filename);
+
+cd /home/common/matlab/fieldtrip/data/test/bug1725/wfu_pickatlas
 
 % these ones are from http://fmri.wfubmc.edu/
 filename = {
-% 'MNI_atlas_templates/MNI_T1.nii'
+  % 'MNI_atlas_templates/MNI_T1.nii'
   'MNI_atlas_templates/TD_brodmann.nii'
   'MNI_atlas_templates/TD_hemisphere.nii'
   'MNI_atlas_templates/TD_label.nii'
-% 'MNI_atlas_templates/TD_label_extended_modified.nii'
+  % 'MNI_atlas_templates/TD_label_extended_modified.nii'
   'MNI_atlas_templates/TD_lobe.nii'
   'MNI_atlas_templates/TD_type.nii'
   'MNI_atlas_templates/aal_MNI_V4.nii'
@@ -46,12 +50,14 @@ for i=14:length(filename)
 end
 
 % plot the template MRI together with the atlas
-mri = ft_read_mri('MNI_atlas_templates/MNI_T1.nii');
+mri   = ft_read_mri('MNI_atlas_templates/MNI_T1.nii');
 atlas = ft_read_atlas('MNI_atlas_templates/aal_MNI_V4.nii'); % FIXME this is how it should be
 atlas = ft_prepare_atlas('MNI_atlas_templates/aal_MNI_V4.nii'); % use the old representation
 
-cfg = [];
-cfg.atlas = atlas;
-cfg.interactive = 'yes';
-ft_sourceplot(cfg, mri);
+if false
+  cfg = [];
+  cfg.atlas = atlas;
+  cfg.interactive = 'yes';
+  ft_sourceplot(cfg, mri);
+end
 
