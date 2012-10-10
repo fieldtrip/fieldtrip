449b6eafba799a189856534f75aa3a038cdc56f4
Boris Reuderink <b.reuderink@gmail.com>
2012-10-10 15:02:28 +0000

    enhancement - factored unreliable test for #1168.
    Test was giving different results depending on in what MATLAB environment was
    run. Since different test results were combined with 'unconventional' boolean operations, with different expected outcomes, this was very hard to debug. I have separated the different test cases.
    
    TODO: 
    - properly detect exceptions.
    - investigate failures.
    
    
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6743 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1168.m
+++ test/test_bug1168.m
@@ -1,116 +1,107 @@
-function test_bug1168
+function test_suite = test_bug1168
 
 % TEST test_bug1168
 % TEST ft_multiplotTFR
 
-cfg = [];
-cfg.layout = 'CTF151.lay';
-lay = ft_prepare_layout(cfg);
+initTestSuite;  % for xUnit
 
-label = ft_channelselection('MEG', lay.label);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function d = setup
 
-nchan = length(label);
+d.cfg = [];
+d.cfg.layout = 'CTF151.lay';
+d.cfg.lay = ft_prepare_layout(d.cfg);
+d.cfg.parameter = 'powspctrm';
+d.label = ft_channelselection('MEG', d.cfg.lay.label);
+d.nchan = length(d.label);
+
+
+function test_chan_freq_time(d)
 nfreq = 50;
 ntime = 100;
 
 freq = [];
-freq.label = label;
-freq.freq = 1:nfreq;
-freq.time = 1:ntime;
-freq.dimord = 'chan_freq_time';
-freq.powspctrm = randn(nchan, nfreq, ntime);
+freq.label     = d.label;
+freq.freq      = 1:nfreq;
+freq.time      = 1:ntime;
+freq.dimord    = 'chan_freq_time';
+freq.powspctrm = randn(d.nchan, nfreq, ntime);
 
-cfg = [];
-cfg.parameter = 'powspctrm';
-cfg.layout = lay;
-ft_multiplotTFR(cfg, freq) % sofar it works
+cfg = d.cfg;
+ft_multiplotTFR(cfg, freq); % sofar it works
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function test_chan_time_freq(d)
+nfreq = 50;
+ntime = 100;
 
 freq = [];
-freq.label = label;
+freq.label = d.label;
 freq.freq = 1:nfreq;
 freq.time = 1:ntime;
 freq.dimord = 'chan_time_freq'; % swap time and freq
-freq.powspctrm = randn(nchan, ntime, nfreq);
+freq.powspctrm = randn(d.nchan, ntime, nfreq);
 
-cfg = [];
-cfg.parameter = 'powspctrm';
-cfg.layout = lay;
-ft_multiplotTFR(cfg, freq) % this still works
+cfg = d.cfg;
+ft_multiplotTFR(cfg, freq); % this still works
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % the following one is more specifically for Linsey
-failed = true;
+function test_linsey(d)
 
-nchan = length(label);
+nchan = length(d.label);
 nfreq = 1;
 ntime = 100;
 
 freq = [];
-freq.label = label;
+freq.label = d.label;
 freq.freq = 1:nfreq;
 freq.time = 1:ntime;
 freq.dimord = 'chan_freq_time';
 freq.powspctrm = randn(nchan, nfreq, ntime);
-try
-  ft_multiplotTFR(cfg, freq) % this fails, nothing is plotted, visual inspection needed
-  failed = false;
-catch
-  failed = true;
-end
+
+ft_multiplotTFR(d.cfg, freq); % this fails, nothing is plotted, visual
+                              % inspection needed
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+function test_unrecognized(d)
+% the following rightfully fails because the data structure is not recognized
+% as freq structure but if the data structure would have had a freq
+% dimension, it should have worked
+nax1 = 50;
+nax2 = 100;
 
-try
-  % the following rightfully fails because the data structure is not recognized as freq structure
-  % but if the data structure would have had a freq dimension, it should have worked
-  nax1 = 50;
-  nax2 = 100;
-  
-  freq = [];
-  freq.label = label;
-  freq.ax1 = 1:nax1;
-  freq.ax2 = 1:nax2;
-  freq.dimord = 'chan_ax1_ax2';
-  freq.powspctrm = randn(nchan, nax1, nax2);
-  
-  cfg = [];
-  cfg.parameter = 'powspctrm';
-  cfg.layout = lay;
-  ft_multiplotTFR(cfg, freq)
-  failed = false;
-catch
-  failed = failed & true;
-end
+freq = [];
+freq.label = d.label;
+freq.ax1 = 1:nax1;
+freq.ax2 = 1:nax2;
+freq.dimord = 'chan_ax1_ax2';
+freq.powspctrm = randn(d.nchan, nax1, nax2);
 
-try
-  % the following rightfully fails because ax2 is not known as dimension
-  nfreq = 50;
-  nax2 = 100;
-  
-  freq = [];
-  freq.label = label;
-  freq.freq = 1:nfreq;
-  freq.ax2 = 1:nax2;
-  freq.dimord = 'chan_freq_ax2';
-  freq.powspctrm = randn(nchan, nfreq, nax2);
-  
-  cfg = [];
-  cfg.parameter = 'powspctrm';
-  cfg.layout = lay;
-  ft_multiplotTFR(cfg, freq)
-  failed = false;
-catch
-  failed = failed & true;
-end
+cfg = d.cfg;
+% Test for specific exception using xUnit.
+% TODO: find id's for errors (gerp warning -> sort.)
+f = @() ft_multiplotTFR(cfg, freq)
+assertExceptionThrown(f, 'fieldtrip:dimord')
 
-if ~failed
-  error('either of the two cases which should fail works')
-end
-  
+
+function test_unknown_dim(d)
+% the following rightfully fails because ax2 is not known as dimension
+nfreq = 50;
+nchan = length(d.label);
+nax2 = 100;
+
+freq = [];
+freq.label = d.label;
+freq.freq = 1:nfreq;
+freq.ax2 = 1:nax2;
+freq.dimord = 'chan_freq_ax2';
+freq.powspctrm = randn(nchan, nfreq, nax2);
+
+% Test for specific exception using xUnit.
+% TODO: find id's for errors (gerp warning -> sort.)
+f = @() ft_multiplotTFR(cfg, freq)
+assertExceptionThrown(f, 'fieldtrip:dimord')
