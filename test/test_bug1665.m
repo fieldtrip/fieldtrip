61049754663f2241a0ace2fcb3cccd50e366e8c6
Lilla Magyari <l.magyari@donders.ru.nl>
2012-10-10 13:19:02 +0000

    bugfix test_bug1665
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6732 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1665.m
+++ test/test_bug1665.m
@@ -1,11 +1,13 @@
 function test_bug1665
 
 % TEST test_bug1665
-% TEST ft_volumesegment ft_volumenormalise align_ctf2spm
+% TEST ft_volumesegment ft_volumenormalise align_ctf2spm ft_hastoolbox
+% ft_checkdata
 
 % this function tests whether align_ctf2spm works robustly when the input
 % MRI contains NaNs
 
+ft_hastoolbox('spm8',1,0);
 load('/home/common/matlab/fieldtrip/data/test/bug1665/segmentedS2.mat');
 struct_reslice = ft_checkdata(struct_reslice, 'datatype', 'volume');
 
