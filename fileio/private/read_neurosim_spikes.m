5ea00800629f014dff57aea3ec8ebdb9d5e97d2d
Bart Gips <b.gips@donders.ru.nl>
2012-10-11 07:48:59 +0000

    Improvement: read_neurosim_spikes now uses matlab's 'textscan', Making the reading in of neurosim spike data much, much faster.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6744 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/private/read_neurosim_spikes.m
+++ fileio/private/read_neurosim_spikes.m
@@ -6,7 +6,7 @@ function [spike] = read_neurosim_spikes(filename)
 %
 % See also FT_READ_SPIKE, FT_DATATYPE_SPIKE
 
-% Copyright (C) 2012 Robert Oostenveld
+% Copyright (C) 2012 Robert Oostenveld, Bart Gips
 %
 % This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
 % for the documentation and details.
@@ -27,68 +27,25 @@ function [spike] = read_neurosim_spikes(filename)
 % $Id$
 
 if isdir(filename)
-  filename = fullfile(filename, 'spikes');
+    filename = fullfile(filename, 'spikes');
 end
 fid = fopen(filename, 'rb');
 
-% count the number of lines in the file
-numlines = 0;
-num = 1024;
-while num==1024
-  [str, num] = fread(fid, num, 'uchar');
-  numlines = numlines + sum(str==10);
-end
-fseek(fid, 0, 'bof');
 
-line = '#';
-while (line(1)=='#')
-  prevline = line;
-  line = fgetl(fid);
-  numlines = numlines - 1; % reduce the number of data lines
-end
-numlines = numlines + 1; % the line that was just read actually contains data
-numlines = numlines - 1; % the last line of the file is usually an empty line, or is not properly defined
-
-% try to parse the 
-header  = tokenize(prevline, ' ', 1);
-header  = header(2:end); % the first element is '#'
-
-% determine the number of data columns
-tok     = tokenize(line, ' ', 1);
-islabel = cellfun(@isempty, cellfun(@str2num, tok, 'UniformOutput', false));
-
-numeric = zeros(numlines,sum(~islabel));
-label   = cell(numlines,sum(islabel));
-
-ft_progress('init', 'etf', 'reading ascii file...');
-for i=1:numlines
-  ft_progress(i/numlines, 'reading line %d from %d', i, numlines)
-  tok = tokenize(line, ' ', 1);
-  numeric(i,:) = str2double(tok(~islabel));
-  label(i,:)   = tok(islabel);
-  line = fgetl(fid);
-end
-ft_progress('close');
+dat=textscan(fid, '%f %d %s %*s %*s %*s %*s %*s', 'CommentStyle', '#');
 fclose(fid);
 
-% Having all the data, the next is to get it all represented according
-% to the FieldTrip spike representation, see T_DATATYPE_SPIKE
+% data is loaded; now put it in a structure that FieldTrip can use.
+% every neuron is considered to have its own channel (identified by its
+% neuron number).
 
-% A minimal representation would be to pretend that all neurons were measured
-% with a single electrode/channel and that they were afterwards sorted in the
-% different units
-%
-% spike.label     = {'spikes'}
-% spike.timestamp = {numeric(:,1)'};
-% spike.unit      = {numeric(:,2)'};
+[number, ~, idx]=unique(dat{2});
+spike.label=cell(1,length(number));
+spike.timestamp=cell(1,length(number));
 
-% it is more convenient to represent each neuron in its own channel
-number          = unique(numeric(:,2)');
-spike.label     = cell(1, length(number), 1);
-spike.timestamp = cell(1, length(number), 1);
-for i=1:length(number)
-  sel = numeric(:,2)==number(i);
-  spike.label{i}     = sprintf('%d', number(i));
-  spike.timestamp{i} = numeric(sel,1)';
+
+for n=1:length(number)
+    spike.label{n}=num2str(number(n));
+    sel=idx==number(n);
+    spike.timestamp{n}=dat{1}(sel)';
 end
-
