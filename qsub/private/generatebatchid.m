9cb0ae2732d7352f1b02fa61abcc000b67f7d7fd
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-11 19:10:06 +0000

    documentation - added documentation, help and copyrights. No functional changes.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6746 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- qsub/private/generatebatchid.m
+++ qsub/private/generatebatchid.m
@@ -1,8 +1,28 @@
 function id = generatebatchid(batch)
 
-% GENERATEBATCHID
+% GENERATEBATCHID generates a unique string that can be used as batch identifier.
 %
 % See also GENERATEJOBID, GENERATESESSIONID
+
+% Copyright (C) 2011-2012, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
 
 if nargin~=1
   error('incorrect number of input arguments');
@@ -10,3 +30,4 @@ end
 
 id = sprintf('%s_%s_p%d_b%d', getusername(), gethostname(), getpid(), batch);
 id = fixname(id);
+
--- qsub/private/getbatch.m
+++ qsub/private/getbatch.m
@@ -3,9 +3,30 @@ function [retval] = getbatch
 % GETBATCH returns a number that can be used to distinguish the subsequent
 % batches. The number automatically increases over subsequent calls.
 
+% Copyright (C) 2011-2012, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id: ft_checkopt.m 6107 2012-06-22 07:54:39Z roboos $
+
 persistent batch
 if isempty(batch)
   batch = 0;
 end
 batch = batch + 1;
 retval = batch;
+
