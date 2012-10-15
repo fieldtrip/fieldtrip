462e4a7268f8cacea7dc5bf458a2190b38b89380
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-10-15 14:58:40 +0000

    bugfix - add path to qsub module, otherwise test function crashes due to not finding the specific function to be tested, i.e. qsubcellfun
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6756 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1742.m
+++ test/test_bug1742.m
@@ -4,6 +4,8 @@ function test_bug1742
 % TEST qsubcellfun fexec
 
 % this initially failed due to bug 1742
+ftroot = which('ft_preprocessing');
+addpath([ftroot,filesep,'qsub']);
 qsubcellfun(@rand, {1, 2, 3}, 'memreq', 1e9, 'timreq', 600, 'backend', 'system');
 % whereas this worked
 a = qsubcellfun(@rand, {1, 2, 3}, 'memreq', 1e9, 'timreq', 600, 'backend', 'system');
