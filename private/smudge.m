30c1a9ff2a0820059c9be27ab7f4e151dd19c4a4
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-10-23 09:41:39 +0000

    enhancement - increased performance leading to ~10x speed increase
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6780 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/smudge.m
+++ private/smudge.m
@@ -1,15 +1,19 @@
-function [datout, S] = smudge(datin, tri, niter)
+function [datout, S] = smudge(datin, tri, niter, threshold)
 
 % SMUDGE(DATIN, TRI) computes a smudged version of the input data datain,
 % given a triangulation tri. The algorithm is according to what is in
 % MNE-Suite, documented in chapter 8.3
 
-if nargin<3,
+if nargin<3 || isempty(niter),
   niter = 1;
 end
 
+if nargin<4
+  threshold = 0;
+end
+
 for k = 1:niter
-  [tmp, Stmp] = do_smudge(datin, tri);
+  [tmp, Stmp] = do_smudge(datin, tri, threshold);
   if k==1,
     S      = Stmp;
   else
@@ -19,15 +23,15 @@ for k = 1:niter
   datin  = tmp;
 end
 
-function [datout, S] = do_smudge(datin, tri)
+function [datout, S] = do_smudge(datin, tri, threshold)
 
 % number of points
 npnt = numel(datin);
 
 % non-zero points
-nz  = find(datin);
+nz  = find(datin>threshold);
 
-% triangles containing non-zero points
+% triangles containing 1 or 2 non-zero points 
 nzt = ismember(tri, nz);
 tnz = sum(nzt, 2)>0 & sum(nzt,2)<3;
 
@@ -47,41 +51,29 @@ for k = 1:size(nzn,1)
   
 end
 
-vecx = sortrows([vec1 vec2]);
-vecy = sortrows([vec2 vec1]);
+% matrices that define edges which has one of the members 0
+% sorted according to the left or the right column respectively
+vecx = sortrows([vec1 vec2]); % having the non-zero vertex upfront, sorted accordingly
+vecy = sortrows([vec2 vec1]); % having the zero vertex upfront, sorted accordingly
 clear vec1 vec2;
 
 % in a closed surface the edges occur in doubles
 vecx = vecx(1:2:end,:);
 vecy = vecy(1:2:end,:);
 
-tmp   = diff([0;vecx(:,1)])>0;
-begix = find([tmp;1]==1); 
+% vecx now has in the first column the column indices for matrix S
+% vecx now has in the second column the row indices for matrix S
 
-uvecy = unique(vecy(:,1));
+% reconstruct the value that has to be put into the matrix
+[uval,i1,i2] = unique(vecy(:,1));
 tmp   = diff([0;vecy(:,1)])>0;
 nix   = diff(find([tmp;1]==1));
-nix(end+1:numel(uvecy)) = 1;
+nix(end+1:numel(uval)) = 1;
 
-tmpval = zeros(npnt,1);
-for k = 1:numel(uvecy)
-  tmpval(uvecy(k)) = nix(k);
-end
+[~,i1,i2] = unique(vecx(:,2));
+val  = 1./nix(i2);
 
-val = zeros(size(vecx,1),1);
-for k = 1:numel(val)
-  val(k) = 1./tmpval(vecx(k,2));
-end
-
-% allocate memory
-S    = spalloc(npnt, npnt, numel(val)+numel(nz)); 
-for k = 1:numel(begix)-1
-  indx1 = vecx(begix(k),1);
-  indx2 = begix(k):(begix(k+1)-1);
-  indx3 = vecx(indx2, 2);
-  S(indx3, indx1) = val(indx2);  
-  %FIXME this can be speeded up by using the sparse command
-end
-S = S + spdiags(datin(:)>0, 0, npnt, npnt);
+S = sparse(vecx(:,2),vecx(:,1),val,npnt,npnt);
+S = S + spdiags(datin(:)>threshold, 0, npnt, npnt);
 
 datout = S*datin(:);
