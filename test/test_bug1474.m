340f8d75fe79b17b194e3e9f3eff3f6ac63343d4
Eelke Spaak <eelke.spaak@donders.ru.nl>
2012-10-24 07:53:47 +0000

    added return statement to top of test_bug1474, to avoid false negative in unit test
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6783 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1474.m
+++ test/test_bug1474.m
@@ -6,6 +6,11 @@ function test_bug1474
 % this script tests ft_rejectvisual with cfg.method='summary' for the case
 % where the data only contains one channel
 
+% since ft_rejectvisual requires user interaction with the GUI, we should
+% just return here, to avoid false negatives in the testing suite.
+% The rest of the code is only for reference.
+return;
+
 load test_bug1474.mat;
 
 cfg = [];
