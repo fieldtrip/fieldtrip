1262cd4c6fad8bcb4560c773406c5276dab654f4
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-25 08:02:58 +0000

    bugfix - changed the TMS-EEG test script into a function
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6790 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1791.m
+++ test/test_bug1791.m
@@ -1,3 +1,8 @@
+function test_bug1791
+
+% TEST test_bug1791
+% TEST ft_removetmsartifact ft_artifact_tmspulse
+
 cfg = [];
 cfg.method = 'twopassfilter';
 cfg.latency = 0.008/2;
@@ -9,9 +14,9 @@ cfg.latency = 0.008/2;
 cfg.width = 0.008;
 data2 = ft_removetmsartifact(cfg, data1);
 
-
 figure 
 hold on
 plot(data.time{2},  data.trial{2}(1,:),  'b')
 plot(data1.time{2}, data1.trial{2}(1,:), 'r')
 plot(data2.time{2}, data2.trial{2}(1,:), 'g')
+
