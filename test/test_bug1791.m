173f2b89d710ca3b34da6083f77322247b5f838e
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-25 08:02:04 +0000

    enhancement - added the first version of a test script for EEG-TMS, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1791
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6789 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1791.m
@@ -0,0 +1,17 @@
+cfg = [];
+cfg.method = 'twopassfilter';
+cfg.latency = 0.008/2;
+data1 = ft_removetmsartifact(cfg, data);
+
+cfg = [];
+cfg.method = 'interpolatepulse';
+cfg.latency = 0.008/2;
+cfg.width = 0.008;
+data2 = ft_removetmsartifact(cfg, data1);
+
+
+figure 
+hold on
+plot(data.time{2},  data.trial{2}(1,:),  'b')
+plot(data1.time{2}, data1.trial{2}(1,:), 'r')
+plot(data2.time{2}, data2.trial{2}(1,:), 'g')
