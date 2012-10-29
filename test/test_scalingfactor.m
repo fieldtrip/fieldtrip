693ebe9563ac691599ca13d4e247475b0577f0f3
Robert Oostenveld <r.oostenveld@gmail.com>
2012-10-29 17:09:30 +0000

    bugfix in test script, autosync set on function itself
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6810 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_scalingfactor.m
+++ test/test_scalingfactor.m
@@ -3,6 +3,9 @@ function test_scalingfactor
 % TEST test_scalingfactor
 % TEST scalingfactor ft_convert_units
 
+p = fileparts(which('ft_defaults'));
+cd(fullfile(p, 'utilities', 'private'));
+
 assert(scalingfactor('m', 'mm') == 1000);
 assert(scalingfactor('mm', 'm') == 0.001);
 assert(scalingfactor('T/cm', 'fT/m') == 1e17);
