e822a41a8ec5716aada0bb16c8b6e73ffe9c1698
Bart Gips <b.gips@donders.ru.nl>
2012-10-31 14:30:59 +0000

    Exchanged the missing 'generatevonmisesrandvar' for a quick but equally effective wrapped normal distribution sampling. Also transposed the imagesc plots such that the dimensions are correct (time and frequency).
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6840 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- contrib/spike/test/test_ft_spiketriggeredspectrum_stat.m
+++ contrib/spike/test/test_ft_spiketriggeredspectrum_stat.m
@@ -8,12 +8,12 @@ kappa = 1;
 nChans = 3;
 for iChan = 1:nChans
   for iFreq = 1:6
-    randPhases(:,iChan,iFreq) = generatevonmisesrandvar(nSpikes+1,kappa/iChan);
+    randPhases(:,iChan,iFreq) = randnwrap(nSpikes+1,kappa/iChan);
   end
 end
 figure, rose(randPhases(:,1,1))
 sts = [];
-sts.fourierspctrm{1} = rand(size(randPhases)).*exp(i*randPhases); % random weighting
+sts.fourierspctrm{1} = rand(size(randPhases)).*exp(1i*randPhases); % random weighting
 sts.trial{1}     = sort(ceil(rand(1,nSpikes)*100));
 sts.time{1}         = rand(1,nSpikes);
 for iChan = 1:nChans
@@ -30,18 +30,18 @@ cfg.timwin = 0.5;
 cfg.winstepsize = 0.001;
 cfg.method = 'ppc0';
 sts_tfr = ft_spiketriggeredspectrum_stat(cfg,sts);
-figure, imagesc(sts_tfr.time, sts_tfr.freq, squeeze(sts_tfr.ppc0(1,:,:))'), colorbar
+figure, imagesc(sts_tfr.time, sts_tfr.freq, squeeze(sts_tfr.ppc0(1,:,:))), colorbar
 %
 cfg.method = 'ppc1';
 sts_tfr = ft_spiketriggeredspectrum_stat(cfg,sts);
 
-figure, imagesc(sts_tfr.time, sts_tfr.freq, squeeze(sts_tfr.ppc1(1,:,:))'), colorbar
+figure, imagesc(sts_tfr.time, sts_tfr.freq, squeeze(sts_tfr.ppc1(1,:,:))), colorbar
 %
 cfg = [];
 cfg.method = 'ppc1';
 sts_tfr = ft_spiketriggeredspectrum_stat(cfg,sts);
 
-figure, plot(sts_tfr.freq, squeeze(sts_tfr.ppc1(1,:,:))'), colorbar
+figure, plot(sts_tfr.freq, squeeze(sts_tfr.ppc1(1,:,:))), colorbar
 
 %%
 % now make a case where no phase locking should be present
@@ -54,7 +54,7 @@ kappa = 1;
 nChans = 3;
 for iChan = 1:nChans
   for iFreq = 1:6
-    randPhases(:,iChan,iFreq) = generatevonmisesrandvar(nSpikes+1,0.001/iChan);
+    randPhases(:,iChan,iFreq) = randnwrap(nSpikes+1,0.001/iChan);
   end
 end
 figure, rose(randPhases(:,1,1))
@@ -77,4 +77,6 @@ cfg.winstepsize = 0.001;
 cfg.method = 'ppc0';
 sts_tfr = ft_spiketriggeredspectrum_stat(cfg,sts);
 
-figure, imagesc(sts_tfr.time, sts_tfr.freq, squeeze(sts_tfr.ppc0(1,:,:))'), colorbar
+figure, imagesc(sts_tfr.time, sts_tfr.freq, squeeze(sts_tfr.ppc0(1,:,:))), colorbar
+function r=randnwrap(n,k)
+r=angle(exp(1i*randn(n,1)*sqrt(1/k)));
