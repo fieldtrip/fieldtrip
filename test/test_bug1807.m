22a52588b1b643041babe2b90d56aa3290c1788d
Robert Oostenveld <r.oostenveld@gmail.com>
2012-11-02 09:02:05 +0000

    enhancement - added test script for bug 1807 and include bti248grad in ft_senstype, note that the bug is not yet fixed
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6845 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1807.m
@@ -0,0 +1,13 @@
+function test_bug1807
+
+% TEST test_bug1807
+% TEST ft_senstype bti2grad ft_datatype_sens ft_read_header ft_read_sens
+
+dataset = '/home/common/matlab/fieldtrip/data/test/original/meg/bti248grad/e,rfhp1.0Hz,COH';
+
+hdr  = ft_read_header(dataset);
+grad = ft_read_sens(dataset);
+
+assert(ft_senstype(hdr, 'bti248grad'));
+assert(ft_senstype(grad, 'bti248grad'));
+
