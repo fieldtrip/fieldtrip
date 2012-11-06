dcb346dbf9b04747c38bdb275db7c63acc1f45cd
Robert Oostenveld <r.oostenveld@gmail.com>
2012-11-06 08:48:31 +0000

    enhancement - added test script for bug 1808, which pertains to neuromag306 data with one misisng channel
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6876 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1808.m
@@ -0,0 +1,11 @@
+function test_bug1808
+
+% TEST test_bug1808
+% TEST ft_read_header ft_read_sens mne2grad
+
+dataset = '/home/common/matlab/fieldtrip/data/test/bug1808/reduced.fif';
+
+hdr  = ft_read_header(dataset);
+grad = ft_read_sens(dataset);
+
+
