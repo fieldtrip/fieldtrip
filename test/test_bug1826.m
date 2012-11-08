800602692eef16615afe74f5b64d0007cc83091c
Robert Oostenveld <r.oostenveld@gmail.com>
2012-11-08 10:53:21 +0000

    enhancement - added a test script with some example code for getting started with aligning the T1, T2 and DTi
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6889 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1826.m
@@ -0,0 +1,34 @@
+function test_bug1826
+
+% at this moment the test script does not yet work, but we don't want the automatic regression testing to flag it as failure
+return
+
+% TEST test_bug1826
+% TEST ft_read_mri ft_write_mri ft_volumerealign
+
+subjectT1  = 'name_to_T1.nii';
+subjectT2  = 'name_to_T2.nii';
+subjectDTi = 'name_to_DTi.nii';
+
+T1  = ft_read_mri(subjectT1);
+T2  = ft_read_mri(subjectT2);
+DTi = ft_read_mri(subjectDTi);
+
+figure; ft_plot_ortho(T1); title('T1 before aligning')
+figure; ft_plot_ortho(T2); title('T2 before aligning')
+figure; ft_plot_ortho(DTi);title('DTi before aligning')
+
+% they are now in memory, which would be the normal starting point in a
+% fieldtrip analysis pipeline
+
+% INSERT THE NEW CODE HERE, THIS PROBABLY INVOLVES
+% step 1) write them to disk, perhaps using ft_write_mri
+% step 2) call external code, e.g. FSL (using a system call) or SPM
+% step 3) read the results from disk
+
+% they are now again in memory, but realigned to each other. This is where
+% a normal fieldtrip analysis pipeline would continue.
+figure; ft_plot_ortho(T1); title('T1 after aligning')
+figure; ft_plot_ortho(T2); title('T2 after aligning')
+figure; ft_plot_ortho(DTi);title('DTi after aligning')
+
