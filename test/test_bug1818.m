cc242312a18a507a63f5a34981bdca190b792162
Robert Oostenveld <r.oostenveld@gmail.com>
2012-11-13 14:44:28 +0000

    enhancement - some stylistic changes, nothing functional
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6921 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1818.m
+++ test/test_bug1818.m
@@ -1,56 +1,51 @@
 function test_bug1818
 
-% to test the reading function of meshes used for FEM headmodel
+% test the reading function of meshes used for constructing SIMBIO FEM head models
+% see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1818
 
 % TEST test_bug1818
 % TEST ft_read_headshape ft_datatype_parcellation
 
-% vista 
- % mesh = ft_read_headshape('~/test_cases/fem/cube2mm3layervorwerk_ns_127_127_127.v');  % error
- 
- % path needs to be changed
- mesh = ft_read_headshape('/home/common/matlab/fieldtrip/data/test/bug1818/cube2mm3layervorwerk_ns_127_127_127.v');
+% vista
+mesh = ft_read_headshape('/home/common/matlab/fieldtrip/data/test/bug1818/cube2mm3layervorwerk_ns_127_127_127.v');
 
- parcellation1 = ft_datatype_parcellation(mesh); 
- parcellation2 = ft_datatype_parcellation(mesh, 'parcellationstyle', 'probabilistic');
- 
- if ~(ft_datatype(parcellation1, 'parcellation'))
-     error 'vista headshape is not parcellation datatype';
- end
- 
- if ~(ft_datatype(parcellation2, 'parcellation'))
-     error 'vista headshape is not parcellation datatype';
- end
+parcellation1 = ft_datatype_parcellation(mesh);
+parcellation2 = ft_datatype_parcellation(mesh,'parcellationstyle','probabilistic');
 
+if~(ft_datatype(parcellation1,'parcellation'))
+  error('the conversion to a parcellation failed');
+end
 
-% tetgen - elements
-mesh = ft_read_headshape('/home/common/matlab/fieldtrip/data/test/bug1818/tet_4layer_127_127_127.1.ele');   
+if~(ft_datatype(parcellation2,'parcellation'))
+  error('the conversion to a parcellation failed');
+end
 
-parcellation1 = ft_datatype_parcellation(mesh); 
-parcellation2 = ft_datatype_parcellation(mesh, 'parcellationstyle', 'probabilistic');
- 
- if ~(ft_datatype(parcellation1, 'parcellation'))
-     error 'vista headshape is not parcellation datatype';
- end
- 
- if ~(ft_datatype(parcellation2, 'parcellation'))
-     error 'vista headshape is not parcellation datatype';
- end
- 
-% tetgen - node 
-mesh = ft_read_headshape('/home/common/matlab/fieldtrip/data/test/bug1818/tet_4layer_127_127_127.1.node');   
+% tetgen-elements
+mesh = ft_read_headshape('/home/common/matlab/fieldtrip/data/test/bug1818/tet_4layer_127_127_127.1.ele');
 
-parcellation1 = ft_datatype_parcellation(mesh); 
-parcellation2 = ft_datatype_parcellation(mesh, 'parcellationstyle', 'probabilistic');
- 
- if ~(ft_datatype(parcellation1, 'parcellation'))
-     error 'vista headshape is not parcellation datatype';
- end
- 
- if ~(ft_datatype(parcellation2, 'parcellation'))
-     error 'vista headshape is not parcellation datatype';
- end
- 
- 
- 
+parcellation1 = ft_datatype_parcellation(mesh);
+parcellation2 = ft_datatype_parcellation(mesh,'parcellationstyle','probabilistic');
+
+if~(ft_datatype(parcellation1,'parcellation'))
+  error('the conversion to a parcellation failed');
+end
+
+if~(ft_datatype(parcellation2,'parcellation'))
+  error('the conversion to a parcellation failed');
+end
+
+% tetgen-node
+mesh = ft_read_headshape('/home/common/matlab/fieldtrip/data/test/bug1818/tet_4layer_127_127_127.1.node');
+
+parcellation1 = ft_datatype_parcellation(mesh);
+parcellation2 = ft_datatype_parcellation(mesh,'parcellationstyle','probabilistic');
+
+if~(ft_datatype(parcellation1,'parcellation'))
+  error('vistaheadshapeisnotparcellationdatatype');
+end
+
+if~(ft_datatype(parcellation2,'parcellation'))
+  error('vistaheadshapeisnotparcellationdatatype');
+end
+
 
