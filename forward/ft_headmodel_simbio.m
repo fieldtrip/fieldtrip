89d4df3492d2b337f96c491fc8cecde2042b8022
Robert Oostenveld <r.oostenveld@gmail.com>
2012-11-15 08:18:54 +0000

    enhancement - updated the simbio EEG FEM forward calculation according to the suggestions by Johannes, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1820
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6929 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/ft_headmodel_simbio.m
+++ forward/ft_headmodel_simbio.m
@@ -1,185 +1,85 @@
-function vol = ft_headmodel_simbio(seg,varargin)
+function vol = ft_headmodel_simbio(geom, varargin)
 
-% FT_HEADMODEL_SIMBIO reads a volume conduction model from a Vista *.v file
+% FT_HEADMODEL_SIMBIO creates a volume conduction model of the head
+% using the finite element method (FEM) for EEG. This function takes
+% as input a volumetric mesh (hexahedral or tetrahedral) and
+% returns as output a volume conduction model which can be used to
+% compute leadfields.
+%
+% This implements
+%       ...
 %
 % Use as
-%   vol = ft_headmodel_simbio(seg)
+%   vol = ft_headmodel_simbio(geom,'conductivity', conductivities, ...)
+%
+% The geom is given as a volumetric mesh, using ft_datatype_parcellation
+%   geom.pos = vertex positions
+%   geom.tet/geom.hex = list of volume elements
+%   geom.tissue = tissue assignment for elements
+%   geom.tissuelabel = labels correspondig to tissues
+%
+% Required input arguments should be specified in key-value pairs and have
+% to include
+%   conductivity   = vector containing tissue conductivities using ordered
+%                    corresponding to geom.tissuelabel
+%
 %
 % See also FT_PREPARE_VOL_SENS, FT_COMPUTE_LEADFIELD
 
-% Copyright (C) 2011, Cristiano Micheli
+% $Id$
 
 ft_hastoolbox('simbio', 1);
 
-wfmethod     = ft_getopt(varargin, 'wfmethod', 'cubes'); % wireframe method (default: cubes)
-transform    = ft_getopt(varargin, 'transform', []);     % contains the tr. matrix from voxels to head coordinates
-unit         = ft_getopt(varargin, 'unit', 'mm');        % contains the units of the transformation matrix (mm, cm, ...)
-tissue       = ft_getopt(varargin, 'tissue', []);        % contains the labels of the tissues
-tissueval    = ft_getopt(varargin, 'tissueval', []);     % contains the tissue values (an integer for each compartment)
-tissuecond   = ft_getopt(varargin, 'tissuecond', []);    % contains the tissue conductivities
-tissueweight = ft_getopt(varargin, 'tissueweight', ones(1,numel(tissueval))); % contains the weigths for vgrid (how dense the wireframe of each tissue conpartment should be done)
-deepelec     = istrue(ft_getopt(varargin, 'deepelec', 'no'));                 % used in the case of deep voxel solution
-% bnd          = ft_getopt(varargin, 'bnd', []);         % used in the case the solution has to be calculated on a triangulated surface (typically the scalp)
-sens         = ft_getopt(varargin, 'sens', []);
-geomfile     = ft_getopt(varargin, 'geomfile', []);
+% get the optional arguments
+conductivity    = ft_getopt(varargin, 'conductivity');
 
-% define a wireframe for each compartment (FEM grid)
-wf = [];
-if ~isempty(geomfile)
-  if ft_hastoolbox('fileio',1)
-    [wf] = ft_read_headshape(geomfile);
-  else
-    error('you need the fileio module!\n')
-  end
-  if ~isfield(wf,'tet')
-    error('the input should be a tetrahedrical mesh')
-  end
-end
-
-if isempty(sens)
-  error('A set of sensors is required')
-end
-
-if ~ft_hastoolbox('simbio',1,0)
-  error('Cannot proceed without Simbio toolbox')
-end
-
-if isunix
-  % temporary file names for simbio call
-  [tmp,tname] = fileparts(tempname);
-  meshfile  = [tname '.v'];
-  [tmp,tname] = fileparts(tempname);
-  exefile   = [tname '.sh'];
-  [tmp,tname] = fileparts(tempname);
-  transfermatrix = [tname];
-  [tmp,tname] = fileparts(tempname);
-  parfile   = [tname '.par'];
-  [tmp,tname] = fileparts(tempname);
-  elcfile   = [tname '.elc'];
-  
-  if strcmp(wfmethod,'cubes')
-    try % write wireframe grid with vgrid
-      tmpfolder = cd;
-      cd(tempdir)
-      
-      % temporary file names for vgrid call
-      [tmp,tname] = fileparts(tempname);
-      shfile    = [tname '.sh'];
-      [tmp,tname] = fileparts(tempname);
-      MRfile = [tname '.v'];
-      [tmp,tname] = fileparts(tempname);
-      materialsfile = [tname '.mtr'];
-      
-      if ~ft_hastoolbox('vgrid',1,0)
-        error('Cannot proceed without Vgrid toolbox')
-      end
-      
-      % write the segmented volume in a Vista format .v file
-      write_vista_vol(size(seg), seg, MRfile);
-      
-      % write the materials file (assign tissue values to the elements of the FEM grid)
-      % see tutorial http://www.rheinahrcampus.de/~medsim/vgrid/manual.html
-      sb_write_materials(materialsfile,tissueval,tissue,tissueweight);
-      
-      % write the shell file
-      efid  = fopen(shfile, 'w');
-      fprintf(efid,'#!/usr/bin/env bash\n');
-      fprintf(efid,['vgrid -in ' MRfile ' -out ' meshfile ' -min 1 -max 1 -elem cube', ...
-        ' -material ' materialsfile ' -smooth shift -shift 0.49 2>&1 > /dev/null\n']);
-      fclose(efid);
-      dos(sprintf('chmod +x %s', shfile));
-      disp('Vgrid is writing the wireframe mesh file, this may take some time ...')
-      stopwatch = tic;
-      
-      % Use vgrid to get the wireframe
-      dos(['./' shfile]);
-      disp([ 'elapsed time: ' num2str(toc(stopwatch)) ])
-      % FIXME: think about adding a translation due to conversion between indices and vertices' world coordinates
-      % read the mesh points and assign wireframe (also called 'FEM grid', or '3d mesh')
-      if ft_hastoolbox('fileio',1)
-        [wf] = ft_read_headshape(meshfile);
-      else
-        error('you need the fileio module!\n')
-      end
-      
-    catch ME
-      disp('Vgrid failed to create the hexahedrons mesh')
-      cleaner(shfile,meshfile,MRfile,materialsfile,exefile,transfermatrix,parfile,elcfile)
-      cd(tmpfolder)
-      rethrow(ME)
-    end
-    
-  elseif strcmp(wfmethod,'tetra')
-    % writes the input mesh into the vista format
-    labels = ones(size(wf.tet,1),1);
-    % FIXME: assign labels according to vertices of the tetra and their
-    % centroid
-    write_vista_mesh(meshfile,wf.pnt,wf.tet,labels);
-  else
-    error('Unsupported method')
-  end
-  
-  % Simbio specific commands
-  if deepelec
-    sb_write_elc(warp_apply(inv(transform),sens.chanpos),sens.label,elcfile,1);
-  else
-    sb_write_elc(warp_apply(inv(transform),sens.chanpos),sens.label,elcfile);
-  end
-  
-  % calculate the FE transfer matrix
-  try
-    % write parfile
-    disp('writing the parameters file on disk...')
-    sb_write_par(parfile,'cond',tissuecond,'labels',tissueval);
-    
-    % write exefile
-    efid = fopen(exefile, 'w');
-    fprintf(efid,'#!/usr/bin/env bash\n');
-    fprintf(efid,['ipm_linux_opt -i FEtransfermatrix -h ./' meshfile ' -s ./' elcfile, ...
-      ' -o ./' transfermatrix ' -p ./' parfile ' -sens EEG \n']); %2>&1 > /dev/null
-    fclose(efid);
-    dos(sprintf('chmod +x %s', exefile));
-    disp('simbio is calculating the transfer matrix, this may take some time ...')
-    
-    % generate the transfer matrix
-    stopwatch = tic;
-    dos(['./' exefile]);
-    disp([ 'elapsed time: ' num2str(toc(stopwatch)) ])
-    
-    % read the transfer matrix
-    transfer = sb_read_transfer(transfermatrix);
-    cleaner(shfile,meshfile,MRfile,materialsfile,exefile,transfermatrix,parfile,elcfile)
-  catch
-    disp('The transfer matrix was not written')
-    cleaner(meshfile,exefile,transfermatrix,parfile,elcfile)
-    cd(tmpfolder)
-    rethrow(ME)
-  end
-  
-end %  if isunix
-
+% start with an empty volume conductor
+geom = ft_datatype_parcellation(geom);
 vol = [];
-vol.wf        = wf;
-vol.cond      = tissuecond;
-vol.transform = transform;
-vol.unit      = unit;
-vol.type      = 'simbio';
-vol.transfer  = transfer;
-
-% if ~isempty(bnd)
-%   vol.bnd        = bnd;
-% else
-if ~isempty(deepelec)
-  vol.deepelec  = deepelec;
+if isfield(geom,'pos')
+  vol.pos = geom.pos;
+else
+  error('Vertex field is required!')
 end
 
-function cleaner(shfile,meshfile,MRfile,materialsfile,exefile,transfermatrix,parfile,elcfile)
-delete(shfile);
-delete(meshfile);
-delete(MRfile);
-delete(materialsfile);
-delete(exefile);
-delete([transfermatrix '.bin']);
-delete([transfermatrix '.mat']);
-delete(parfile);
-delete(elcfile);
+if isfield(geom,'tet')
+  vol.tet = geom.tet;
+elseif isfield(geom,'hex')
+  vol.hex = geom.hex;
+else
+  error('Connectivity information is required!')
+end
+
+if isfield(geom,'tissue')
+  vol.tissue = geom.tissue;
+else
+  error('No element indices declared!')
+end
+
+if isempty(conductivity)
+  error('No conductivity information!')
+end
+
+if length(conductivity) >= length(unique(vol.tissue))
+  vol.cond = conductivity;
+else
+  error('Wrong conductivity information!')
+end
+
+if ~isfield(geom,'tissuelabel')
+        numlabels = size(unique(geom.tissue),1);
+    vol.tissuelabel = {};
+            ulabel = unique(labels);
+    for i = 1:numlabels
+        shape.tissuelabel{i} = num2str(ulabel(i));
+    end
+else
+    vol.tissuelabel = geom.tissuelabel;
+end
+    
+vol.stiff = sb_calc_stiff(vol);
+vol.type = 'simbio';
+
+end
+
+
--- forward/private/leadfield_simbio.m
+++ forward/private/leadfield_simbio.m
@@ -1,115 +1,28 @@
-function [lf] = leadfield_simbio(dip, elc, vol)
+function [lf] = leadfield_simbio(pos, vol)
 
 % leadfield_simbio leadfields for a set of dipoles
 %
-% [lf] = leadfield_simbio(elc, vol, headmeshfile);
+% [lf] = leadfield_simbio(pos, vol);
 %
 % with input arguments
-%   elc     positions of the electrodes (matrix of dimensions mx3)
-%           contains the positions of the vertices on which
-%           the potentials are calculated
+%   pos     a matrix of dipole positions
 %           there can be 'deep electrodes' too!
-%   vol.wf  contains a wireframe structure (or fem grid) of 'elements'
-%           which is needed to perform a fem analysis.
-%   dip     a matrix of dipoles (of dimensions nx3)
+%   vol     contains a FE volume conductor (output of ft_prepare_vol_sens)
 %
-% the output lf is the leadfields matrix of dimensions m (rows) x n*3 (cols)
+% the output lf is the leadfield matrix of dimensions m (rows) x n*3 (cols)
 
-% copyright (c) 2011, Cristiano Micheli
-
-% store the current path and change folder to the temporary one
-tmpfolder = cd;
-
-% add the simbio folder to the path
-if ~isunix
-  error('this only works on linux and osx')
-end
+% copyright (c) 2012, Johannes Vorwerk
 
 try 
-  cd(tempdir)
-  [~,tname] = fileparts(tempname);
-  exefile   = [tname '.sh'];
-  [~,tname] = fileparts(tempname);
-  elcfile   = [tname '.elc'];
-  [~,tname] = fileparts(tempname);
-  dipfile   = [tname '.dip'];
-  [~,tname] = fileparts(tempname);
-  parfile   = [tname '.par'];
-  [~,tname] = fileparts(tempname);
-  meshfile  = [tname '.v'];
-  [~,tname] = fileparts(tempname);
-  transfermatrix = [tname];
-  [~,tname] = fileparts(tempname);
-  outfile   = [tname];
-  
-  % write the electrodes and dipoles positions in the temporary folder
-  disp('writing the electrodes file on disk...\n')
-  if ~isfield(vol,'deepelec')
-    sb_write_elc(warp_apply(inv(vol.transform),elc.chanpos),elc.label,elcfile);
-  else
-    sb_write_elc(warp_apply(inv(vol.transform),elc.chanpos),elc.label,elcfile,1);
-  end
-  disp('writing the dipoles file on disk...\n')
-  sb_write_dip(warp_apply(inv(vol.transform),dip),dipfile);
-  
-  % write the parameters file, contains tissues conductivities, the fe
-  % grid and simbio call details, mixed together
-  disp('writing the parameters file on disk...')
-  sb_write_par(parfile,'cond',vol.cond,'labels',unique(vol.wf.index)-100);
-  
-  % write the vol.wf in a vista format .v file
-  disp('writing the mesh file on disk...\n')
-  if ft_hastoolbox('fileio',1)
-    ft_write_headshape(meshfile,vol.wf,'format','vista');
-  else
-    error('you need the fileio module!\n')
-  end
-  
-  % write the transfer matrix on disk
-  disp('writing the transfer matrix on disk...')
-  sb_write_transfer(vol.transfer,transfermatrix);
-  
-  if ~ispc
-    efid = fopen(exefile, 'w');
-    fprintf(efid,'#!/usr/bin/env bash\n');
-    fprintf(efid,['ipm_linux_opt -i sourcesimulation  -h ./' meshfile ' -s ./' elcfile ' -t ./' transfermatrix, ...
-      ' -dip ./' dipfile ' -o ./' outfile ' -p ./' parfile ' -fwd FEM -sens EEG 2>&1 > /dev/null\n']);
-    fclose(efid);
-    
-%     fprintf(efid,['ipm_linux_opt_Venant -i sourcesimulation -h ./' meshfile ' -s ./' elcfile, ...
-%       ' -dip ./' dipfile ' -o ./' outfile ' -p ./' parfile ' -fwd FEM -sens EEG 2>&1 > /dev/null\n']);
-
-    dos(sprintf('chmod +x %s', exefile));
-    disp('simbio is calculating the leadfields, this may take some time ...')
-
-    stopwatch = tic;
-    dos(['./' exefile]);
-    disp([ 'elapsed time: ' num2str(toc(stopwatch)) ])
-
-    % extract the lead-fields
-    [lf] = sb_read_msr(outfile);
-    cleaner(exefile,elcfile,dipfile,parfile,meshfile,outfile,transfermatrix)
-    cd(tmpfolder)
-  end
-  
+    lf = zeros(size(3*pos,1),size(vol.transfer,1));
+    dir = diag([1,1,1]);
+    for i=1:size(pos,1)
+        locpos = repmat(pos(i,:),3,1);
+        rhs = sb_rhs_venant(locpos,dir,vol);
+        lf((3*(i-1)+1):(3*(i-1)+3),:) = (vol.transfer * rhs)';
+    end    
+    lf = lf';
 catch
   warning('an error occurred while running simbio');
   rethrow(lasterror)
-  cleaner(exefile,elcfile,dipfile,parfile,meshfile,outfile,transfermatrix)
-  cd(tmpfolder)
 end
-
-function cleaner(exefile,elcfile,dipfile,parfile,meshfile,outfile,transfermatrix)
-delete(exefile);
-delete(elcfile);
-delete(dipfile);
-delete(parfile);
-delete(meshfile);
-delete(outfile);
-delete(transfermatrix);
-delete([outfile '.fld']);
-delete([outfile '.hex']);
-delete([outfile '.pot']);
-delete([outfile '.pts']);
-delete(['*.v.ascii']);
-delete(['*.v.potential']);
