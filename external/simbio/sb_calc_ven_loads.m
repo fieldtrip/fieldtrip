b3811bc110b8e26e3309130f3b7e49e4e38ebe68
Johannes Vorwerk <j.vorwerk@uni-muenster.de>
2012-11-15 09:51:08 +0000

    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6937 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ external/simbio/sb_calc_ven_loads.m
@@ -0,0 +1,26 @@
+function loads = sb_calc_ven_loads(pos,dir,ven_nd,node);
+%aref setzen
+aref = 20;
+%lambda setzen
+lambda = 10e-6;
+%r setzen
+r = 1;
+loads = zeros(size(pos,1),size(ven_nd,2));
+for i=1:size(pos,1);
+    x = bsxfun(@minus,node(ven_nd(i,ven_nd(i,:)~=0),:),pos(i,:))./aref;
+    X = zeros(9,size(x,1));
+    X(1:3:7,:) = ones(3,size(x,1));
+    X(2:3:8,:) = x';
+    X(3:3:9,:) = (x.^2)';
+    T = zeros(9,1);
+    T(1:3:7) = 0;
+    T(2:3:8) = dir(i,:)./aref;
+    T(3:3:9) = 0;
+    W = zeros(size(x,1)*3,size(x,1));
+    W(1:size(x,1),:) = diag(x(:,1).^r);
+    W(size(x,1)+1:2*size(x,1),:) = diag(x(:,2).^r);
+    W(size(x,1)*2+1:3*size(x,1),:) = diag(x(:,3).^r);
+    tmp = ((X')*X + lambda*(W')*W) \ X'*T;
+    loads(i,ven_nd(i,:)~=0) = tmp;
+end
+end
