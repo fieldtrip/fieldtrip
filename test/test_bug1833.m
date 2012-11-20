843df7901c2defe28e6e17d762a09f48864274cc
Robert Oostenveld <r.oostenveld@gmail.com>
2012-11-20 11:04:26 +0000

    enahncement - added a test script for ft_plot_mesh with tetraheders and hexaheders, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1833
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6964 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1833.m
@@ -0,0 +1,20 @@
+function test_bug1833
+
+% TEST test_bug1833
+% TEST ft_plot_mesh ft_read_headshape
+
+% at this moment (20 November 2012) this test script is known not to work
+% and the bugzilla report is still open
+return
+
+% this test script borrows some data from another bug
+cd /home/common/matlab/fieldtrip/data/test/bug1820
+
+mesh = ft_read_headshape('tet_4layer_127_127_127.1.node');
+mesh.tet = mesh.tet(1:10000,:); % prune the number of elements
+ft_plot_mesh(mesh, 'vertexcolor', 'none', 'edgecolor', 'none');
+
+mesh = ft_read_headshape('cube2mm3layervorwerk_ns_127_127_127.v');
+mesh.hex = mesh.hex(1:1000,:); % prune the number of elements
+ft_plot_mesh(mesh, 'vertexcolor', 'none', 'edgecolor', 'none');
+
