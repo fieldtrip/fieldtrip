610834dffb0d83888595f6afe52fe74776f8cee7
Robert Oostenveld <r.oostenveld@gmail.com>
2012-11-27 15:54:38 +0000

    automatically synchronized identical files to revision 7000
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7001 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- inverse/private/ft_voltype.m
+++ inverse/private/ft_voltype.m
@@ -18,19 +18,21 @@ function [type] = ft_voltype(vol, desired)
 %   asa                boundary element method, based on the (commercial) ASA software
 %   simbio             finite element method, based on the SimBio software
 %   fns                finite difference method, based on the FNS software
+%   interpolate        interpolate the potential based on pre-computed leadfields
 %
 % and for MEG the following volume conduction models are recognized
 %   singlesphere       analytical single sphere model
 %   localspheres       local spheres model for MEG, one sphere per channel
 %   singleshell        realisically shaped single shell approximation, based on the implementation from Guido Nolte
 %   infinite           magnetic dipole in an infinite vacuum
+%   interpolate        interpolate the potential based on pre-computed leadfields
 %
 % See also FT_COMPUTE_LEADFIELD, FT_READ_VOL, FT_HEADMODEL_BEMCP,
 % FT_HEADMODEL_ASA, FT_HEADMODEL_DIPOLI, FT_HEADMODEL_SIMBIO,
 % FT_HEADMODEL_FNS, FT_HEADMODEL_HALFSPACE, FT_HEADMODEL_INFINITE,
 % FT_HEADMODEL_OPENMEEG, FT_HEADMODEL_SINGLESPHERE,
 % FT_HEADMODEL_CONCENTRICSPHERES, FT_HEADMODEL_LOCALSPHERES,
-% FT_HEADMODEL_SINGLESHELL
+% FT_HEADMODEL_SINGLESHELL, FT_HEADMODEL_INTERPOLATE
 
 % Copyright (C) 2007-2012, Robert Oostenveld
 %
