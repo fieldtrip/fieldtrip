b755866c0d817cb22e8acf438c0668f6f7355fb5
Robert Oostenveld <r.oostenveld@gmail.com>
2012-11-27 08:58:32 +0000

    enhancement - restored old default functionality for conductivity, now specifically for 3 and 4 sphere versions after sorting the spheres, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1854
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@6994 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/ft_headmodel_concentricspheres.m
+++ forward/ft_headmodel_concentricspheres.m
@@ -26,14 +26,14 @@ function vol = ft_headmodel_concentricspheres(geometry, varargin)
 % See also FT_PREPARE_VOL_SENS, FT_COMPUTE_LEADFIELD
 
 % get the optional input arguments
-conductivity = ft_getopt(varargin, 'conductivity', [1 1/80 1]*0.33); % see below
+conductivity = ft_getopt(varargin, 'conductivity'); % default is determined below
 fitind       = ft_getopt(varargin, 'fitind', 'all');
 unit         = ft_getopt(varargin, 'unit');
 
 % The condictivity default applies to a 3-sphere model. Providing defaults
 % for a 4-sphere model are not so easy because the user might have
 % specified the geometry scalp-skull-csf-brain or the other way around.
- 
+
 % start with an empty volume conductor
 vol = [];
 
@@ -43,7 +43,7 @@ if ~isempty(unit)
 elseif isfield(geometry, 'unit')
   % copy the geometrical units into he volume conductor
   % assume that in case of multiple meshes that they have the same units
-  vol.unit = geometry(1).unit; 
+  vol.unit = geometry(1).unit;
 end
 
 if isnumeric(geometry) && size(geometry,2)==3
@@ -60,10 +60,6 @@ end
 
 % determine the number of compartments
 numboundaries = numel(geometry);
-
-if numel(conductivity)~=numboundaries
-  error('a conductivity value should be specified for each compartment');
-end
 
 % concatenate the vertices of all surfaces
 pnt = [];
@@ -96,7 +92,21 @@ vol      = ft_convert_units(vol); % ensure the object to have a unit
 
 % sort the spheres from the smallest to the largest ('insidefirst' order)
 [vol.r, indx] = sort(vol.r);
-vol.c = vol.c(indx);
+
+if isempty(vol.c)
+  % it being empty indicates that the user did not specify a conductivity, use a default instead
+  if length(vol.r)==3
+    vol.c = [0.3300   0.0042 0.3300]; % brain,      skull, skin
+  elseif length(vol.r)==4
+    vol.c = [0.3300 1 0.0042 0.3300]; % brain, csf, skull, skin
+  else
+    error('conductivity values should be specified for each tissue type');
+  end
+else
+  % the conductivity as specified by the user should be in the same order as the geometries
+  % sort the spheres from the smallest to the largest ('insidefirst' order)
+  vol.c = vol.c(indx);
+end
 
 for i=1:numel(geometry)
   fprintf('concentric sphere %d: radius = %.1f, conductivity = %f\n', i, vol.r(i), vol.c(i));
