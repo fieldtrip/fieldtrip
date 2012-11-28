fieldtrip/fieldtrip

commit 77f1a941b144ce095c2e0cbe5846d2d803ffcc22

enhancement - correctly detect bti248grad, addressing bug 1807
git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7044 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

fileio/ft_chantype.m
@@ -72,6 +72,14 @@ if isheader
     grad  = hdr.grad;
   end
   label = hdr.label;
+  
+  % ensure that the label order in the grad matches the label order in the
+  % label list
+  [i1,i2] = match_str(label, grad.label);
+  tmptra  = zeros(numel(label), size(grad.tra,2));
+  grad.label = grad.label(i2);
+  tmptra(i1,:) = grad.tra(i2,:);
+  grad.tra   = tmptra;
 elseif isgrad
   label   = grad.label;
   numchan = length(label);
@@ -100,7 +108,7 @@ elseif ft_senstype(input, 'neuromag') && isheader
     for sel=find(hdr.orig.channames.KI(:)==3)'
       type{sel} = 'digital trigger';
     end
-    % determinge the MEG channel subtype
+    % determine the MEG channel subtype
     selmeg=find(hdr.orig.channames.KI(:)==1)';
     for i=1:length(selmeg)
       if hdr.orig.chaninfo.TY(i)==0
@@ -289,7 +297,7 @@ elseif ft_senstype(input, 'bti')
   type(strncmp('G', label, 1)) = {'refgrad'};
   
   if isfield(grad, 'tra')
-    selchan = find(strcmp('mag', type));
+    selchan = find(strcmp('meg', type));
     for k = 1:length(selchan)
       ncoils = length(find(grad.tra(selchan(k),:)==1));
       if ncoils==1,
@@ -298,6 +306,7 @@ elseif ft_senstype(input, 'bti')
         type{selchan(k)} = 'meggrad';
       end
     end
+    
   end
   
   % This is to allow setting additional channel types based on the names

fileio/private/bti2grad.m
@@ -222,11 +222,44 @@ elseif isfield(hdr, 'config'),
     end
   end
   if haseegpos
-    elec.pnt   = hdr.user_block_data{userblock}.pnt;
-    elec.label = hdr.user_block_data{userblock}.label;
+    % now we need to match the labels in the label field of the specified
+    % user block with the labels in the header. a discrepancy can exist 
+    % because of the messy file structure. also, we need to remove the 
+    % coils, and the LRNCI 'channels', as well as the one called 'reserved'
+    % also remove the position which has all 0
+    label = hdr.user_block_data{userblock}.label;
+    pnt   = hdr.user_block_data{userblock}.pnt;
+    
+    label(sum(pnt==0,2)==3) = [];
+    pnt(sum(pnt==0,2)==3,:) = [];
+
+    pnt(strncmpi('coil',label,4),:) = [];
+    label(strncmpi('coil',label,4)) = [];
+
+    pnt(ismember(label,{'L','R','N','C','I'}),:) = [];
+    label(ismember(label,{'L','R','N','C','I'})) = []; 
+    
+    if ~isempty(label)  
+      elec.pnt   = pnt;
+      elec.label = label;
+    else
+      elec = [];
+    end
   else
     elec = [];
   end
+  
+  % add the chantype
+  type  = ft_chantype(grad);
+  ngrad = sum(strcmp('meggrad',type));
+  nmag  = sum(strcmp('megmag', type));
+    
+  if ngrad>nmag
+    grad.type = [ft_senstype(grad) 'grad'];
+  else
+    grad.type = ft_senstype(grad);
+  end
+  grad.chantype = type;
   
 elseif isfield(hdr, 'grad'),
   % hdr has been derived in read_bti_m4d and grad is already there

forward/ft_senstype.m
@@ -199,7 +199,6 @@ end
 if isfield(sens, 'type')
   % preferably the structure specifies its own type
   type = sens.type;
-  
 elseif isfield(input, 'nChans') && input.nChans==1 && isfield(input, 'label') && ~isempty(regexp(input.label{1}, '^csc', 'once'))
   % this is a single channel header that was read from a Neuralynx file, might be fcdc_matbin or neuralynx_nsc
   type = 'neuralynx';
@@ -235,7 +234,9 @@ else
   % start with unknown, then try to determine the proper type by looking at the labels
   type = 'unknown';
   
-  if isgrad
+  if isgrad && isfield(sens, 'type')
+    type = sens.type;
+  elseif isgrad
     % probably this is MEG, determine the type of magnetometer/gradiometer system
     % note that the order here is important: first check whether it matches a 275 channel system, then a 151 channel system, since the 151 channels are a subset of the 275
     if     (mean(ismember(ft_senslabel('ctf275'),        sens.label)) > 0.8)

commit 3eb3cb7a9d022c52c0b922b2b33ac8925885a6d5

bugfix - fixed a bug introduced in r7044 regarding grad.tra, due to not keeping in mind that it should also work for eeg electrode structures
git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7051 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

fileio/ft_chantype.m
@@ -67,19 +67,18 @@ grad  = input;
 label = input;
 
 if isheader
+  label  = hdr.label;
   numchan = length(hdr.label);
   if isfield(hdr, 'grad')
-    grad  = hdr.grad;
+      grad         = hdr.grad;
+      % ensure that the grad.label order matches the hdr.label order
+      [i1, i2]     = match_str(label, grad.label);
+      grad.label   = grad.label(i2);                        % reorder the channel labels
+      tmptra       = zeros(numel(label), size(grad.tra,2)); % FIXME why not size(grad.tra)?
+      tmptra(i1,:) = grad.tra(i2,:);                        % reorder the rows from the tra matrix
+      grad.tra     = tmptra;
   end
-  label = hdr.label;
   
-  % ensure that the label order in the grad matches the label order in the
-  % label list
-  [i1,i2] = match_str(label, grad.label);
-  tmptra  = zeros(numel(label), size(grad.tra,2));
-  grad.label = grad.label(i2);
-  tmptra(i1,:) = grad.tra(i2,:);
-  grad.tra   = tmptra;
 elseif isgrad
   label   = grad.label;
   numchan = length(label);