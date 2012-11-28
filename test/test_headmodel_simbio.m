cd79a1297b3818dc8d2c89eeffa7e7f7be411af9
Lilla Magyari <l.magyari@donders.ru.nl>
2012-11-28 17:18:52 +0000

    bugfix: test_headmodel_simbio is updated
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7050 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_headmodel_simbio.m
+++ test/test_headmodel_simbio.m
@@ -1,129 +1,192 @@
 function test_headmodel_simbio
 
 % TEST test_headmodel_simbio
-% TEST ft_headmodel_fem_simbio ft_prepare_vol_sens ft_compute_leadfield ft_headmodel_concentricspheres
-
+% TEST ft_prepare_headmodel ft_prepare_mesh ft_prepare_leadfield ft_datatype_headmodel
+% TEST ft_datatype_segmentation ft_datatype_sens ft_datatype_parcellation ft_datatype
 % this function tests that simbio forward model works, comparing the results with a 3 concentric
 % spheres model
+% this script is under development, earlier version of test script written by Cristiano is in
+% test_headmodel_simbio_old 
+
+return;
 
 ft_hastoolbox('simbio', 1);
 
-% create 3 spherical meshes and the corresponding volumes
-[pnt, tri] = icosahedron162;
+% create a 3D volume with 3 spheres
+% volume and mesh creation is adapted from test_bug1815.m
+example.dim = [91 104 111];   % slightly different numbers
+example.transform = eye(4);   
+example.coordsys = 'ctf';
+example.unit = 'mm';
+example.seg = zeros(example.dim);
 
-% radiuses and origins are defined in mm
-svol(1).o = [0,0,0];
-svol(1).r = 30;
-svol(1).bnd.pnt = pnt;
-svol(1).bnd.tri = tri;
+% adjusting transformation matrix: center of the head-coordinates [0 0 0] should
+% be the center of volume
 
-svol(2).o = [0,0,0];
-svol(2).r = 50;
-svol(2).bnd.pnt = svol(2).r*pnt;
-svol(2).bnd.tri = tri;
+% center of volume in voxel coordinates     
+x = round(example.dim(1)/2);
+y = round(example.dim(2)/2);
+z = round(example.dim(3)/2);
+    
+x = round(x);
+y = round(y);
+z = round(z);      
+             
+origin = [x y z];
 
-svol(3).o = [0,0,0];
-svol(3).r = 60;
-svol(3).bnd.pnt = svol(3).r*pnt;
-svol(3).bnd.tri = tri;
+example.transform(1:4,4)   = [-origin(:); 1];  % head-coordinate [0 0 0] is in the center of 
+                                                % the volume (x y z in voxel-coordinates)
 
-% % generate a volume of 3 concentric spheres (works if number of voxels is odd)
-% res = 1; % in mm
-% for i=3:-1:1
-%   tmp2 = zeros(151,151,151);
-%   xgrid = -svol(i).r:res:svol(i).r;
-%   ygrid = xgrid;
-%   zgrid = xgrid;
-%   [X, Y, Z]  = ndgrid(xgrid, ygrid, zgrid);
-%   pos = [X(:) Y(:) Z(:)];
-%   [inside] = bounding_mesh(pos, svol(i).bnd.pnt, svol(i).bnd.tri);
-%   l = length(xgrid)
-%   c = 76; sel = (l-1)./2; % in voxel
-%   tmp = reshape(inside,[l l l]); 
-%   tmp2(c-sel:c+sel,c-sel:c+sel,c-sel:c+sel) = tmp;
-%   MR{i} = tmp2;
-% end
-% bkgrnd = zeros(151,151,151);
-% bkgrnd = MR{1}+MR{2}+MR{3};
-% % save('~crimic/test/SimBio/spheres','bkgrnd')
-fprintf('Loading a volume with a number N = %d of compartments ... \n', numel(svol))
-tmp = load('~crimic/fieldtrip-dev/forward/test/spheres.mat','bkgrnd');
-bkgrnd = tmp.bkgrnd;
+% compute position for each voxel in voxelspace and in headspace
 
-% generate volume's external surface (mm)
-[pnt, tri] = icosahedron162;
-o = [0,0,0];
-r = 60;
-bnd.pnt = r*pnt;
-bnd.tri = tri;
+[X, Y, Z] = ndgrid(1:example.dim(1), 1:example.dim(2), 1:example.dim(3));
+voxelpos = [X(:) Y(:) Z(:)];
+headpos = warp_apply(example.transform, voxelpos);
 
-% create a set of electrodes (mm)
-clear sens
-sel = find(bnd.pnt(:,3)>0);
-sens.chanpos = bnd.pnt(sel,:);
-sens.elecpos = bnd.pnt(sel,:);
-for i=1:length(sel)
-  sens.label{i} = sprintf('chan%03d', i);
+% create 3 spheres
+
+radius1 = 40;
+radius2 = 30;
+radius3 = 20;
+
+for i=1:size(headpos,1)
+    % from small to large
+    if norm(headpos(i,:))<radius3
+        example.seg(i) = 3;
+    elseif norm(headpos(i,:))<radius2
+        example.seg(i) = 2;
+    elseif norm(headpos(i,:))<radius1
+        example.seg(i) = 1;
+    end
 end
-sens.type = 'eeg';
 
-% Generate the head model for FEM simbio (conductivities, wireframe, etc.)
-% load('~crimic/test/SimBio/spheres','bkgrnd')
-transform = eye(4);
-transform(1:3,4) = [-76 -76 -76]'; % voxels to mm
-% ATTENTION: simbio wants the coordinates in voxel units,
-% conductivity in S/m, sensors in voxels, dipoles in voxels
-% FIXME: actually sens shouldnt be required, generate fake sens def inside
-% this function?
-vol  = ft_headmodel_fem_simbio(bkgrnd,'tissue',{'sph1','sph2','sph3'}, ...
-                                      'tissueval',[1 2 3], ...
-                                      'tissuecond',[0.022 0.33 0.33], ...
-                                      'sens',sens, ...
-                                      'transform',transform,'unit','mm'); 
+elec = {};
+radius4 = 45;   % electrode positions 5 mm outside of spheres 
 
-% project the electrodes on the volume conduction model
-[vol, sens] = ft_prepare_vol_sens(vol, sens);
-
-% compute the lead fields in the output voxels
-lf  = ft_compute_leadfield([0 0 30], sens, vol);
-
-% plot things
-figure;
-subplot(2,2,1); ft_plot_topo3d(sens.chanpos, lf(:,1))
-subplot(2,2,2); ft_plot_topo3d(sens.chanpos, lf(:,2))
-subplot(2,2,3); ft_plot_topo3d(sens.chanpos, lf(:,3))
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% compare with concentric sphere model
-clear geom
-for i=1:3
-  geom(i).pnt = svol(i).bnd.pnt;
+sel=zeros(3);
+for i=1:size(headpos,1)
+    % create also sensor positions, only in positive z
+    if headpos(i,3) > 0
+       if norm(headpos(i,:)) == radius4
+       n = size(sel,1);
+       p = n+1;
+            sel(p,:) = headpos(i,:);
+       end
+    end
 end
-vol2 = ft_headmodel_concentricspheres(geom, 'conductivity', 1e-3*[0.022 0.33 0.33]); %S/mm 
 
-% sensors
-[pnt, tri] = icosahedron162;
-o = [0,0,0];
-r = 60;
-bnd.pnt = r*pnt;
-bnd.tri = tri;
 
-clear sens2
-sel = find(bnd.pnt(:,3)>0);
-sens2.chanpos = bnd.pnt(sel,:);
-sens2.elecpos = sens2.chanpos;
-for i=1:length(sel)
-  sens2.label{i} = sprintf('chan%03d', i);
+elec.pnt = sel(4:size(sel,1),:); %first 3 rows are only zeros
+
+for i=1:length(elec.pnt)
+  elec.label{i} = sprintf('electrode%d', i);
 end
-sens2.type = 'eeg';
+elec.unit = 'mm';
 
-% reproject electrodes on the spherical geometry
-[vol2, sens2] = ft_prepare_vol_sens(vol2, sens2);
+elec = ft_datatype_sens(elec);
 
-% compute an example leadfield
-lf2 = ft_compute_leadfield([0 0 30], sens2, vol2);
 
-figure;
-subplot(2,2,1); ft_plot_topo3d(sens2.chanpos, lf2(:,1))
-subplot(2,2,2); ft_plot_topo3d(sens2.chanpos, lf2(:,2))
-subplot(2,2,3); ft_plot_topo3d(sens2.chanpos, lf2(:,3))
+clear X Y Z headpos origin radius1 radius2 radius3  radius4 voxelpos x y z sel
+
+
+figure; imagesc(example.seg(:,:,50));
+
+
+
+% create meshes of the volume
+% hexahedral
+
+% not working yet; see test_bug1815
+cfg=[];
+cfg.tissue = {'seg'};
+cfg.numvertices = 3000;
+cfg.method = 'hex';          % option for hexahedral mesh generation
+hexmesh = ft_prepare_mesh(cfg,example);
+
+% check mesh
+parcellation = ft_datatype_parcellation(hexmesh);
+assert(ft_datatype(parcellation,'parcellation'),'the conversion to a parcellation failed');
+
+% tetrahedral
+% not working yet; see test_bug1815
+cfg=[];
+cfg.tissue = {'seg'};
+cfg.numvertices = 3000;
+cfg.method = 'tet';          % option for hexahedral mesh generation
+tetmesh = ft_prepare_mesh(cfg,example);
+
+% check mesh
+parcellation = ft_datatype_parcellation(tetmesh);
+assert(ft_datatype(parcellation,'parcellation'),'the conversion to a parcellation failed');
+
+
+cfg=[];
+cfg.method = 'simbio';
+cfg.conductivity = [0.1 0.4 0.1];       % I just typed in some numbers, but probablt more information
+                                        % is necessary on what is the unit of conductivity and if there are
+                                        % "default" or suggested values
+vol_hex = ft_prepare_headmodel(hexmesh);
+vol_tet = ft_prepare_headmodel(tetmesh);
+
+% check output structure
+
+% disp(vol_hex)
+%    vol.pos ...
+%    vol.hex ...
+%    vol.tissue ...
+%    vol.tissuelabel  ...
+%    vol.cond ...
+%    vol.type ... 
+%    vol.unit ...
+%    vol.cfg ...
+
+% ft_datatype_headmodel could be updated for simbio
+assert(ft_datatype_headmodel(vol_hex),'Headmodel does not correspond to datatype headmodel');
+assert(ft_datatype_headmodel(vol_tet),'Headmodel does not correspond to datatype headmodel');
+
+assert(isequal(hexmesh.pnt,vol_hex.pos), 'Positions of headmodel is not equal to mesh points');
+assert(isequal(hexmesh.hex,vol_hex.hex), 'Hexahedrons in headmodel do not correpond to mesh hexahedrons');
+assert(isequal(hexmesh.tissue,vol_hex.tissue), 'Tissue field in headmodel do not correspond to tissues in mesh');
+assert(isequal(hexmesh.label,vol_hex.label), 'Labels in headmodel do not correspond to mesh labels');
+
+assert(isequal(tetmesh.pnt,vol_tet.pos), 'Positions of headmodel is not equal to mesh points');
+assert(isequal(tetmesh.tet,vol_tet.tet), 'Tetrahedrons in headmodel do not correpond to mesh tetrahedrons');
+assert(isequal(tetmesh.tissue,vol_tet.tissue), 'Tissue field in headmodel do not correspond to tissues in mesh');
+assert(isequal(tetmesh.label,vol_tet.label), 'Labels in headmodel do not correspond to mesh labels');
+
+%%%%
+ 
+% % compute the lead fields 
+cfg=[];
+cfg.vol = vol_tet;
+cfg.elec = elec;
+lf_tet  = ft_prepare_leadfield(cfg);
+
+cfg=[];
+cfg.vol = vol_hex;
+cfg.elec = elec;
+lf_hex  = ft_prepare_leadfield(cfg);
+
+
+% compare it with concentric spheres
+
+example = ft_datatype_segmentation(example,'segmentationstyle','probabilistic');
+cfg=[];
+cfg.tissue = {'tissue_1', 'tissue_2', 'tissue_3'};
+cfg.numvertices = [3000 2000 1000];
+cfg.method = 'tri';                       % default
+bnd = ft_prepare_mesh(cfg,example); 
+
+cfg=[];
+cfg.method = 'concentricspheres';
+cfg.conductivity = [0.1 0.4 0.1];
+
+vol_cc = ft_prepare_headmodel(cfg,bnd);
+
+cfg=[];
+cfg.vol = vol_cc;
+cfg.elec = elec;
+lf_cc  = ft_prepare_leadfield(cfg);
+
+% plot leadfields
+
