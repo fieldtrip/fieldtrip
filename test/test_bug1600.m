8e961e9722023904f428eebda1321e19491856a2
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2012-11-28 14:32:20 +0000

    added test function
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7031 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1600.m
@@ -0,0 +1,20 @@
+function test_bug1600
+
+% TEST test_bug1600
+% TEST ft_checkdata
+% TEST ft_datatype_source
+
+% The problem: ft_checkdata(volume, 'datatype', 'source') does not seem to
+% convert the inside back to vectorial representation
+
+% verify the problem
+volume         = [];
+volume.dim     = [3 4 5];
+volume.anatomy = zeros(3,4,5);
+volume.anatomy(2,2:3,2:4) = rand(1,2,3);
+volume.inside  = volume.anatomy>0;
+volume.transform = eye(4);
+
+source = ft_checkdata(volume, 'datatype', 'source');
+
+assert(all(size(source.inside)==[60 1]));
