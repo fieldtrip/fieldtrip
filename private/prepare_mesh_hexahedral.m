990429506752d529095e99df59d0435bb215ca07
Lilla Magyari <l.magyari@donders.ru.nl>
2012-12-04 17:43:58 +0000

    enhancement: new functionality (prepare_mesh_hexahedral) which is under development has been added
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7085 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ private/prepare_mesh_hexahedral.m
@@ -0,0 +1,145 @@
+function mesh=prepare_mesh_hexahedral(cfg,mri)
+
+% PREPARE_MESH_HEXAHEDRAL
+%
+% See also PREPARE_MESH_SEGMENTATION, PREPARE_MESH_MANUAL, PREPARE_MESH_HEADSHAPE
+%
+% Configuration options for generating a regular 3-D grid
+%   cfg.tissue = cell with the names of the compartments that should be
+%   meshed
+%   cfg.resolution = desired resolution of the mesh (standard = 1)
+%
+% Copyrights (C) 2012, Johannes Vorwerk
+%
+% Subversion does not use the Log keyword, use 'svn log <filename>' or 'svn -v log | less' to get detailled information
+
+% get the default options
+cfg.tissue      = ft_getopt(cfg, 'tissue');
+cfg.resolution = ft_getopt(cfg, 'resolution');
+
+if isempty(cfg.tissue)
+  mri = ft_datatype_segmentation(mri, 'segmentationstyle', 'indexed');
+  fn = fieldnames(mri);
+  for i=1:numel(fn),if numel(mri.(fn{i}))==prod(mri.dim), segfield=fn{i};end;end
+  cfg.tissue=setdiff(unique(mri.(segfield)(:)),0);
+end
+
+if ischar(cfg.tissue)
+  % it should either be something like {'brain', 'skull', 'scalp'}, or something like [1 2 3]
+  cfg.tissue = {cfg.tissue};
+end
+
+if iscell(cfg.tissue)
+    % the code below assumes that it is a probabilistic representation
+    if any(strcmp(cfg.tissue, 'brain'))
+        mri = ft_datatype_segmentation(mri, 'segmentationstyle', 'probabilistic', 'hasbrain', 'yes');
+    else
+        mri = ft_datatype_segmentation(mri, 'segmentationstyle', 'probabilistic');
+    end
+else
+    % the code below assumes that it is an indexed representation
+    mri = ft_datatype_segmentation(mri, 'segmentationstyle', 'indexed');
+end
+
+if isempty(cfg.resolution)
+    warning('Using standard resolution 1 mm')
+    cfg.resolution = 1;
+end
+
+% do the mesh extraction
+% this has to be adjusted for FEM!!!
+if iscell(cfg.tissue)
+    % this assumes that it is a probabilistic representation
+    % for example {'brain', 'skull', scalp'}
+    try
+        temp = zeros(size(mri.(cfg.tissue{1})(:)));
+        for i=1:numel(cfg.tissue)
+            temp = [temp,mri.(cfg.tissue{i})(:)];
+        end
+        [val,seg] = max(temp,[],2);
+        seg = seg - 1;
+        seg = reshape(seg,mri.dim);
+    catch
+        error('Please specify cfg.tissue to correspond to tissue types in the segmented MRI')
+    end
+    tissue = cfg.tissue;
+else
+    % this assumes that it is an indexed representation
+    % for example [3 2 1]
+    seg = zeros(mri.dim);
+    tissue = {};
+    for i=1:numel(cfg.tissue)
+        seg = seg + i*(mri.seg==cfg.tissue(i));
+        if isfield(mri, 'seglabel')
+            try
+                tissue{i} = mri.seglabel{cfg.tissue(i)};
+            catch
+                error('Please specify cfg.tissue to correspond to (the name or number of) tissue types in the segmented MRI')
+            end
+        else
+            tissue{i} = sprintf('tissue %d', i);
+        end
+    end
+end
+
+% ensure that the segmentation is binary and that there is a single contiguous region
+% FIXME is this still needed when it is already binary?
+%seg = volumethreshold(seg, 0.5, tissue);
+
+tmpfolder = cd;
+cd(tempdir)
+
+% temporary file names for vgrid call
+[~,tname] = fileparts(tempname);
+shfile    = [tname '.sh'];
+[~,tname] = fileparts(tempname);
+meshfile  = [tname '.v'];
+[~,tname] = fileparts(tempname);
+MRfile = [tname '.v'];
+[~,tname] = fileparts(tempname);
+materialsfile = [tname '.mtr'];
+
+% write the segmented volume in a Vista format .v file
+write_vista_vol(size(seg), seg, MRfile);
+
+% write the materials file (assign tissue values to the elements of the FEM grid)
+% see tutorial http://www.rheinahrcampus.de/~medsim/vgrid/manual.html
+weight(1:numel(cfg.tissue)) = 1.;
+sb_write_materials(materialsfile,[1:numel(cfg.tissue)],tissue,weight);
+
+% determin vgrid path
+str = which('vgrid');
+[p, f, x] = fileparts(str);
+vgridpath = p;
+
+% write the shell file
+efid  = fopen(shfile, 'w');
+fprintf(efid,'#!/usr/bin/env bash\n');
+fprintf(efid,[vgridpath '/vgrid -in ' MRfile ' -out ' meshfile ' -min ' num2str(cfg.resolution) ' -max ' num2str(cfg.resolution) ' -elem cube', ...
+    ' -material ' materialsfile ' -smooth shift -shift 0.30 2>&1 > /dev/null\n']);
+fclose(efid);
+dos(sprintf('chmod +x %s', shfile));
+disp('vgrid is writing the wireframe mesh file, this may take some time ...')
+stopwatch = tic;
+
+% Use vgrid to get the wireframe
+dos(['./' shfile]);
+disp([ 'elapsed time: ' num2str(toc(stopwatch)) ])
+% FIXME: think about adding a translation due to conversion between indices and vertices' world coordinates
+
+% read the mesh points
+[mesh.pnt,mesh.hex,labels] = read_vista_mesh(meshfile);
+
+cd(tmpfolder);
+
+mesh.tissue = zeros(size(labels));
+numlabels = size(unique(labels),1);
+mesh.tissuelabel = {};
+ulabel = sort(unique(labels));
+for i = 1:numlabels
+    mesh.tissue(labels == ulabel(i)) = i;
+    mesh.tissuelabel{i} = num2str(ulabel(i));
+    mesh.tissuename{i} = tissue{i};
+end
+  
+end
