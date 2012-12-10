c58f3117506c385836c40d0535aa580c0a8c4b91
Diego Lozano Soldevilla <d.lozanosoldevilla@donders.ru.nl>
2012-12-10 15:02:52 +0000

    bugfix 1701 - test_ft_channelselection added
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7129 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/ref_datasets.m
+++ test/ref_datasets.m
@@ -18,102 +18,217 @@ elseif ispc
   origdir = fullfile('H:', 'common', 'matlab', 'fieldtrip', 'data', 'test');
 end
 
+datasets = [];
+
 % eeg dataformats
-datasets(1).origdir  = origdir; 
-datasets(1).type     = 'eeg';
-datasets(1).datatype = 'bdf';
-datasets(1).filename = 'Newtest17-256.bdf';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'eeg';
+datasets(end).datatype  = 'bdf';
+datasets(end).filename  = 'Newtest17-256.bdf';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = nan;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'unknown';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'eeg';
-datasets(end).datatype = 'brainvision';
-datasets(end).filename = 'Mischa.vhdr';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'eeg';
+datasets(end).datatype  = 'brainvision';
+datasets(end).filename  = 'Mischa.vhdr';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = nan;
+datasets(end).numeog    = 2;
+datasets(end).numecg    = 2;
+datasets(end).numemg    = 2;
+datasets(end).senstype  = 'unknown';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'eeg';
-datasets(end).datatype = 'edf';
-datasets(end).filename = '0601_s.edf';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'eeg';
+datasets(end).datatype  = 'edf';
+datasets(end).filename  = '0601_s.edf';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = nan;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'electrode';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'eeg';
-datasets(end).datatype = 'neuroscan16';
-datasets(end).filename = '0500.cnt';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'eeg';
+datasets(end).datatype  = 'neuroscan16';
+datasets(end).filename  = '0500.cnt';
+datasets(end).numeeg    = 58;
+datasets(end).nummeg    = nan;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'ext1020';% type=ft_senstype(data.label)
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'eeg';
-datasets(end).datatype = 'neuroscan32';
-datasets(end).filename = 'Subject1_MP.cnt';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'eeg';
+datasets(end).datatype  = 'neuroscan32';
+datasets(end).filename  = 'Subject1_MP.cnt';
+datasets(end).numeeg    = 62;
+datasets(end).nummeg    = nan;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'ext1020';% type=ft_senstype(data.label)
 
 % meg dataformats
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'bti148';
-datasets(end).filename = 'c,rfhp0.1Hz.m4d';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'bti148';
+datasets(end).filename  = 'c,rfhp0.1Hz.m4d';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 148;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'bti148';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'bti248';
-datasets(end).filename = 'e,rfDC';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'bti248';
+datasets(end).filename  = 'e,rfDC';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 248;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'bti248';
 
-datasets(end+1).origdir  = origdir;
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'bti248grad';
-datasets(end).filename = 'e,rfhp1.0Hz,COH';
+datasets(end+1).origdir = origdir;
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'bti248grad';
+datasets(end).filename  = 'e,rfhp1.0Hz,COH';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 248;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'bti248grad';%type=ft_senstype(data.label)
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'ctf151';
-datasets(end).filename = 'Subject01.ds';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'ctf151';
+datasets(end).filename  = 'Subject01.ds';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 151;
+datasets(end).numeog    = 1;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'ctf151';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'ctf275';
-datasets(end).filename = 'A0132_Aud-Obj-Recognition_20051115_02.ds';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'ctf275';
+datasets(end).filename  = 'A0132_Aud-Obj-Recognition_20051115_02.ds';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 275;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'ctf275';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'ctf64';
-datasets(end).filename = 'Wat123r1raw.ds';
-datasets(end).dataformat = 'ctf_old';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'ctf64';
+datasets(end).filename  = 'Wat123r1raw.ds';
+datasets(end).dataformat= 'ctf_old';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 64;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'ctf64';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'itab153';
-datasets(end).filename = 'srgcst85_0105.raw';
+datasets(end).numecg    = nan;
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'itab153';
+datasets(end).filename  = 'srgcst85_0105.raw';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 153;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'itab153';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'itab28';
-datasets(end).filename = 'gibb0101.raw';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'itab28';
+datasets(end).filename  = 'gibb0101.raw';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = nan;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'itab28';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'itab28_old';
-datasets(end).filename = 'gibb0101.raw';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'itab28_old';
+datasets(end).filename  = 'gibb0101.raw';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = nan;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'itab28';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'neuromag122';
-datasets(end).filename = 'jg_single_01raw.fif';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'neuromag122';
+datasets(end).filename  = 'jg_single_01raw.fif';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 122;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'neuromag122';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'neuromag306';
-datasets(end).filename = 'raw.fif';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'neuromag306';
+datasets(end).filename  = 'raw.fif';
+datasets(end).numeeg    = 2;
+datasets(end).nummeg    = 306;
+datasets(end).nummegmag = 102;
+datasets(end).nummeggrad= 204;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'neuromag306';
 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'meg';
-datasets(end).datatype = 'yokogawa160';
-datasets(end).filename = 'Continuous1.con';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'meg';
+datasets(end).datatype  = 'yokogawa160';
+datasets(end).filename  = 'Continuous1.con';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = 160;
+datasets(end).nummegmag = nan;
+datasets(end).nummeggrad= 160;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'yokogawa160';
 
 % lfp dataformats 
-datasets(end+1).origdir  = origdir; 
-datasets(end).type     = 'lfp';
-datasets(end).datatype = 'neuralynx';
-datasets(end).filename = '2005-2-10_11-57-40';
+datasets(end+1).origdir = origdir; 
+datasets(end).type      = 'lfp';
+datasets(end).datatype  = 'neuralynx';
+datasets(end).filename  = '2005-2-10_11-57-40';
+datasets(end).numeeg    = nan;
+datasets(end).nummeg    = nan;
+datasets(end).numeog    = nan;
+datasets(end).numecg    = nan;
+datasets(end).numemg    = nan;
+datasets(end).senstype  = 'unknown';
 
 % first trial has lot's of NaNs in raw data
-% datasets(end+1).origdir  = origdir; 
+% datasets(end+1).origdir= origdir; 
 % datasets(end).type     = 'lfp';
 % datasets(end).datatype = 'plexon';
 % datasets(end).filename = 'p213parall.nex';
---
+++ test/test_ft_channelselection.m
@@ -0,0 +1,48 @@
+function test_ft_channelselection
+% TEST test_ft_channelselection 
+% TEST ft_senstype ref_datasets
+
+datasets=ref_datasets;
+
+for i=1:size(datasets,2);
+    filename = fullfile(datasets(i).origdir, 'original', datasets(i).type, datasets(i).datatype, datasets(i).filename);
+    
+    if isempty(datasets(i).dataformat);
+        hdr = ft_read_header(filename);
+    else
+        hdr = ft_read_header(filename,'headerformat',datasets(i).dataformat);
+    end
+    
+    if ~ft_senstype(hdr, datasets(i).senstype);
+        error(['incorrect senstype detection: ' datasets(i).datatype]);
+    else
+        type=ft_senstype(hdr);
+    end
+    
+    if ~isnan(datasets(i).numeeg) && length(ft_channelselection('EEG', hdr.label, type))~=datasets(i).numeeg
+        error('incorrect number of EEG channels');
+    end
+    if ~isnan(datasets(i).nummeg) && length(ft_channelselection('MEG', hdr.label, type))~=datasets(i).nummeg
+        error('incorrect number of MEG channels');
+    end
+    if ~isnan(datasets(i).numeog) && length(ft_channelselection('EOG', hdr.label, type))~=datasets(i).numeog
+        error('incorrect number of EOG channels');
+    end
+    if ~isnan(datasets(i).numecg) && length(ft_channelselection('ECG', hdr.label, type))~=datasets(i).numecg
+        error('incorrect number of ECG channels');
+    end
+    if ~isnan(datasets(i).numemg) && length(ft_channelselection('EMG', hdr.label, type))~=datasets(i).numemg
+        error('incorrect number of EMG channels');
+    end
+
+    %exceptions for 'neuromag306' and 'yokogawa160' MEG datasets:
+    %selecting gradiometer/magnetometer sensors
+    if strcmp(type,'neuromag306') || strcmp(type,'yokogawa160');
+        if ~isnan(datasets(i).numemg) && length(ft_channelselection('MEGMAG', hdr.label, type))~=datasets(i).nummegmag
+            error('incorrect number of MEGMAG channels');
+        end
+        if ~isnan(datasets(i).numemg) && length(ft_channelselection('MEGGRAD', hdr.label, type))~=datasets(i).nummeggrad
+            error('incorrect number of MEGGRAD channels');
+        end
+    end
+end
