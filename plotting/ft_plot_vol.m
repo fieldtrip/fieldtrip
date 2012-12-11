fc6b405178449d9599f72617dbd17a5159443710
Robert Oostenveld <r.oostenveld@gmail.com>
2012-12-11 08:24:41 +0000

    bugfix - accidentally had ft_datatype_vol instead of ft_datatype_headmodel
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7135 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/ft_plot_vol.m
+++ plotting/ft_plot_vol.m
@@ -43,7 +43,7 @@ function ft_plot_vol(vol, varargin)
 ws = warning('on', 'MATLAB:divideByZero');
 
 % ensure that the volume conduction model description is up-to-date (Dec 2012)
-vol = ft_datatype_vol(vol);
+vol = ft_datatype_headmodel(vol);
 
 % get the optional input arguments
 faceindex   = ft_getopt(varargin, 'faceindex',   'none');
