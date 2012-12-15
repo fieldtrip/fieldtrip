488267b334670ea8c60c275cd51af898437a144f
Robert Oostenveld <r.oostenveld@gmail.com>
2012-12-15 10:51:45 +0000

    bugfix - added a leading / to the full path of the spm template
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7198 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1832.m
+++ test/test_bug1832.m
@@ -1,15 +1,15 @@
 function test_bug1832
+
 % TEST test_bug1832
 % TEST ft_read_mri ft_volumesegment ft_prepare_headmodel ft_prepare_sourcemodel
-%
+
 % for the warp template example script:
 % 1) make test script that checks on the units in these objects (with assert)
 % 2) also go through the rest of the example script and check on units
 
-
 % NOTE: the path to the template file is user-specific
 if isunix
-  template = ft_read_mri('home/common/matlab/fieldtrip/external/spm8/templates/T1.nii');
+  template = ft_read_mri('/home/common/matlab/fieldtrip/external/spm8/templates/T1.nii');
 elseif ispc
   template = ft_read_mri('H:/common/matlab/fieldtrip/external/spm8/templates/T1.nii');
 end
@@ -39,3 +39,4 @@ template_grid  = ft_prepare_sourcemodel(cfg);
 assert(isfield(template_grid, 'unit'), 'unit field in grid missing')
 
 % check more here
+
