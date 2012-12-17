eb41661d848b9575a731644dd2daaf9d245f0c40
Robert Oostenveld <r.oostenveld@gmail.com>
2012-12-17 20:15:40 +0000

    enhancement - added test script for priavte functions, see bug 1891
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7219 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_qsubcellfun4.m
@@ -0,0 +1,22 @@
+function test_qsubcellfun4
+
+% TEST test_qsubcellfun4
+
+% this test is designed for executing functions that are private to the function in which qsubcellfun is called
+% see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1891
+
+p = which('ft_defaults');
+if ~isempty(p)
+  % clean up the path, only qsub and somepath
+  restoredefaultpath
+  addpath(fullfile(fileparts(p), 'qsub'));
+end
+
+addpath /home/common/matlab/fieldtrip/data/test/bug1891/somepath
+
+a1a = mainfunction({3, 4, 5}, 'qsubcellfun');
+a1b = mainfunction({3, 4, 5}, 'qsubcellfun_nostack');
+a2  = mainfunction({3, 4, 5}, 'cellfun'    );
+
+assert(isequal(a1a, a2));
+assert(isequal(a1b, a2));
