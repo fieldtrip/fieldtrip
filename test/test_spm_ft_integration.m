10b21bf549a85fc81ee52db9706514d69e4b8f23
Robert Oostenveld <r.oostenveld@gmail.com>
2012-12-21 12:08:25 +0000

    enhancement - changed from script into function, otherwise the automated test won't run
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7250 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_spm_ft_integration.m
+++ test/test_spm_ft_integration.m
@@ -1,4 +1,4 @@
-% function test_spm_ft_integration
+function test_spm_ft_integration
 
 % TEST test_spm_ft_integration
 % TEST ft_prepare_layout ft_prepare_headmodel ft_compute_leadfield ft_prepare_vol_sens
