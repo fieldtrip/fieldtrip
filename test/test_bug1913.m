4715b941cd8f599f92c40ebd521ddeac4e354bec
Robert Oostenveld <r.oostenveld@gmail.com>
2012-12-22 15:08:14 +0000

    enhancement - added a test script and test data for external/eeprobe, see bug 1931
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7257 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1913.m
@@ -0,0 +1,15 @@
+function test_bug1913
+
+% TEST test_bug1913
+
+fn1 = '/home/common/matlab/fieldtrip/data/test/original/eeg/eeprobe/martlbc1.cnt';
+fn2 = '/home/common/matlab/fieldtrip/data/test/original/eeg/eeprobe/martlbc1s1.avr';
+fn3 = '/home/common/matlab/fieldtrip/data/test/original/eeg/eeprobe/VP_05_1.cnt';
+
+hdr1 = ft_read_header(fn1);
+hdr2 = ft_read_header(fn2);
+hdr3 = ft_read_header(fn3);
+
+dat1 = ft_read_data(fn1); clear dat*
+dat2 = ft_read_data(fn2); clear dat*
+dat3 = ft_read_data(fn3); clear dat*
