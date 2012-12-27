1890c9d26034025d5f9f8a153179282fc2c1bada
Robert Oostenveld <r.oostenveld@gmail.com>
2012-12-27 10:55:06 +0000

    bugfix - fixed problem with debugging code that occurred in ft_prepare_atlas because it does not merge ft_default with the input cfg
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7265 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/private/ft_postamble_debug.m
+++ utilities/private/ft_postamble_debug.m
@@ -1,19 +1,24 @@
-% FT_POSTAMBLE_DEBUG is a helper script for debugging problems with FieldTrip functions
+% ft_postamble_debug is a helper script for debugging problems with fieldtrip functions
 %
-% See also FT_PREAMBLE_DEBUG DEBUGCLEANUP
+% see also ft_preamble_debug debugcleanup
 
 % these variables are shared by the three debug handlers
-global Ce9dei2ZOo_debug Ce9dei2ZOo_funname Ce9dei2ZOo_argin
+global ce9dei2zoo_debug ce9dei2zoo_funname ce9dei2zoo_argin
+
+if ~isfield(cfg, 'debug')
+  % do not provide extra debugging facilities
+  return
+end
 
 switch cfg.debug
   case {'displayonsuccess' 'saveonsuccess'}
     % do not clean up the global variables yet
     % these are still needed by the cleanup function
     
-otherwise
+  otherwise
     % clean up the global variables
     % this results in the cleanup function doing nothing
-    Ce9dei2ZOo_debug   = 'no';
-    Ce9dei2ZOo_funname = [];
-    Ce9dei2ZOo_argin   = [];
+    ce9dei2zoo_debug   = 'no';
+    ce9dei2zoo_funname = [];
+    ce9dei2zoo_argin   = [];
 end
