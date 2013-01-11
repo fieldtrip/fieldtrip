097be16c8e7af29dc5dcd19cf043eec789207bbd
Robert Oostenveld <r.oostenveld@gmail.com>
2013-01-11 14:27:49 +0000

    enhancement - added test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1887
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7298 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1887.m
@@ -0,0 +1,36 @@
+function test_bug1887
+
+% TEST test_bug1887
+% TEST ft_checkdata ft_datatype_raw ft_datatype_comp ft_datatype_timelock ft_componentanalysis ft_connectivityanalysis
+
+% this contains raw data, 10 trials with nans
+load(dccnfilename('/home/common/matlab/fieldtrip/data/test/bug1887.mat'));
+
+for i=1:10
+data.trial{i} = randn(size(data.trial{i}));
+end
+
+cfg = [];
+cfg.method = 'pca';
+comp = ft_componentanalysis(cfg, data);
+
+cfg = [];
+timelock = ft_timelockanalysis(cfg, data);
+
+% simple checks
+tmp = ft_checkdata(data, 'datatype', 'raw');
+tmp = ft_checkdata(comp, 'datatype', 'comp');
+tmp = ft_checkdata(timelock, 'datatype', 'timelock');
+
+% this should also work
+tmp = ft_checkdata(comp, 'datatype', 'raw');
+assert(isfield(tmp, 'topo'));
+
+% this should also work, but the output should not be comp
+tmp = ft_checkdata(comp, 'datatype', 'timelock');
+assert(~isfield(tmp, 'topo'));
+
+
+
+
+
