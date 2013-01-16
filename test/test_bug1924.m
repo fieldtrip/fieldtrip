7e4bd3e23fdb51a092dbdebfeae25bb38d1836f4
Robert Oostenveld <r.oostenveld@gmail.com>
2013-01-16 10:56:01 +0000

    enhancement - added test scripf for deymed eeg file format
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7330 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1924.m
@@ -0,0 +1,35 @@
+function test_bug1924
+
+% TEST test_bug1924
+% TEST read_deymed_dat read_deymed_ini ft_filetype ft_read_header ft_read_data ft_read_event
+
+datfile = 'R2.09.Dat';
+edffile = 'R2.09-export.edf';
+
+hdr1 = ft_read_header(datfile);
+hdr2 = ft_read_header(edffile);
+
+dat1 = ft_read_data(datfile);
+dat2 = ft_read_data(edffile);
+
+sel1 = (1:25600);
+sel2 = 1:25600;
+
+x = dat1(2,sel1);
+y = dat2(2,sel2);
+
+figure
+plot(x, 'b')
+hold on
+plot(y, 'r.')
+
+z = xcorr(x, y, 'coeff');
+[m, i] = max(z);
+
+% this should peak in the middle
+figure
+plot(z)
+
+assert(identical(dat1, dat2, 'reltol', 1e-6));
+
+
