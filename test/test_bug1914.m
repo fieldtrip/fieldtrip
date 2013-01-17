e545163338cebcd84abd460568a81af55810fd7c
Robert Oostenveld <r.oostenveld@gmail.com>
2013-01-17 15:57:40 +0000

    enhancement - made test script for babySQUID system, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=1914
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7345 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug1914.m
@@ -0,0 +1,26 @@
+function test_bug1914
+
+% TEST test_bug1914
+% TEST ft_filetype ft_read_header ft_read_data ft_read_event
+
+cd(dccnfilename('/home/common/matlab/fieldtrip/data/test/bug1914'));
+
+dataset = {
+  'conversion_testing_MEGEEG_raw.fif'
+  'conversion_testing_MEGandperipheral_raw.fif'
+  'conversion_testing_OnlyMEG_no_triggers_raw.fif'
+  'conversion_testing_OnlyMEG_with_triggers_raw.fif'
+  };
+
+for i=1:length(dataset)
+  filename = dataset{i};
+  
+  hdr = ft_read_header(filename);
+  dat = ft_read_data(filename, 'begsample', 1, 'endsample', hdr.Fs);
+  event = ft_read_event(filename);
+  
+  fprintf('============ %s ============\n', filename);
+  disp(hdr);
+  disp(event);
+  
+end
