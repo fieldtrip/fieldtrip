2ca01344f90bc766f20988de8dab992f82d33ed6
Jorn Horschig <jm.horschig@donders.ru.nl>
2013-01-23 13:55:17 +0000

    automatically synchronized identical files to revision 7391
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7392 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- connectivity/private/nanmean.m
+++ connectivity/private/nanmean.m
@@ -1,3 +1,10 @@
 % NANMEAN provides a replacement for MATLAB's nanmean.
 %
 % For usage see MEAN.
+
+
+function y = nanmean(x, dim)
+N = sum(~isnan(t), dim);
+y = nansum(x, dim) ./ N;
+
+end
--- src/nansum.m
+++ src/nansum.m
@@ -1,3 +1,13 @@
 % NANSUM provides a replacement for MATLAB's nanmean.
 %
 % For usage see SUM.
+
+function y = nansum(x, dim)
+
+if nargin == 1
+  dim = 1;
+end
+idx = isnan(x);
+x(idx) = 0;
+y = sum(x, dim);
+end
