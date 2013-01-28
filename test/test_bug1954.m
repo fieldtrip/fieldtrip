79b740c38b261aebfbb9d154130bcbe936359ccd
Johanna Zumer <j.zumer@donders.ru.nl>
2013-01-28 11:02:51 +0000

    bugfix - cfg.coordsys = 'ras' added so interactive not needed for test function
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7412 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1954.m
+++ test/test_bug1954.m
@@ -5,16 +5,15 @@ load /home/common/matlab/fieldtrip/template/headmodel/standard_mri.mat
 
 cfg           = [];
 cfg.output    = {'brain','skull','scalp'};
+cfg.coordsys  = 'ras';
 segmentedmri  = ft_volumesegment(cfg, mri);
-% I said 'n' when asked 
-% Do you want to change the anatomical labels for the axes [Y, n]? 
-% Saying 'Y' and giving 'rasn' gives same answer.
-
 % I get warning:
 % Warning: could not open /tmp/tpe20fae07_8585_42ab_a6e4_0e4c656f97b3.img 
 
-figure;imagesc(squeeze(segmentedmri.scalp(:,110,:)))
-% voxels on top of head included in scalp
+if 0 % so this doesn't run during automated testing
+  figure;imagesc(squeeze(segmentedmri.scalp(:,110,:)))
+  % voxels on top of head included in scalp
+end
 
 cfg = [];
 cfg.method = 'bemcp';
