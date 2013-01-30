b2ffb61e1d98b806280a89e09440522dee8bca27
Robert Oostenveld <r.oostenveld@gmail.com>
2013-01-30 16:22:48 +0000

    enhancement - decoded the modular EEG (openeeg) data stream, made a pure matlab implementation for reading the data from serial or bluetooth, copy (write) it to fieldtrip buffer for processing elsewhere. This is part of http://bugzilla.fcdonders.nl/show_bug.cgi?id=1961
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7425 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ realtime/example/ft_realtime_modeegproxy.m
@@ -0,0 +1,113 @@
+function ft_realtime_modeegproxy(cfg)
+
+% FT_REALTIME_MODEEGPROXY reads continuous data from a modeeg EEG acquisition
+% system through the serial port or through BlueTooth and writes it to a FieldTrip
+% buffer.
+%
+% The FieldTrip buffer is a network transparent server that allows the
+% acquisition client to stream data to it. An analysis client can connect
+% to read the data upon request. Multiple clients can connect simultaneously,
+% each analyzing a specific aspect of the data concurrently.
+%
+% Use as
+%   ft_realtime_modeegproxy(cfg)
+%
+% The configuration should contain
+%   cfg.filename             = string, name of the serial port (default = '/dev/tty.FireFly-B106-SPP')
+%   cfg.feedback             = 'yes' or 'no' (default = 'no')
+%   cfg.blocksize            = number, in seconds (default = 0.125)
+%
+% The target to write the data to is configured as
+%   cfg.target.datafile      = string, target destination for the data (default = 'buffer://localhost:1972')
+%   cfg.target.dataformat    = string, default is determined automatic
+%
+% To stop this realtime function, you have to press Ctrl-C
+
+% Copyright (C) 2012, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
+cfg = ft_checkconfig(cfg);
+
+% set the defaults
+if ~isfield(cfg, 'filename'),           cfg.filename = '/dev/tty.FireFly-B106-SPP';       end
+if ~isfield(cfg, 'channel'),            cfg.channel = 'all';                              end
+if ~isfield(cfg, 'feedback'),           cfg.feedback = 'no';                              end
+if ~isfield(cfg, 'blocksize'),          cfg.blocksize = 0.125;                            end
+if ~isfield(cfg, 'target'),             cfg.target = [];                                  end
+if ~isfield(cfg.target, 'datafile'),    cfg.target.datafile = 'buffer://localhost:1972';  end
+if ~isfield(cfg.target, 'dataformat'),  cfg.target.dataformat = [];                       end % default is to use autodetection of the output format
+
+
+
+% make a connection to the serial port
+fid = fopen(cfg.filename, 'r');
+if fid<0
+  error('cannot open %s', cfg.filename);
+else
+  fprintf('opened %s\n', cfg.filename);
+  c = onCleanup(@()fclose(fid));
+end
+
+rem   = [];   % this will contain the remaining bytes following each conversion
+bytes = 0;    % number of bytes read in total
+count = 0;    % number of blocks read
+blocksize = round(17*256*cfg.blocksize); % 17 bytes per sample, 256 Hz sampling rate
+feedback  = strcmp(cfg.feedback, 'yes'); % convert to boolean value
+
+% create a fixed header
+hdr.Fs          = 256;
+hdr.nChans      = 6;
+ndr.nSamples    = inf;
+hdr.nSamplesPre = 0;
+hdr.label       = cellfun(@num2str, {1, 2, 3, 4, 5, 6}, 'uniformoutput', false);
+chanindx        = 1:2;
+
+while (true)
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % read some data from the serial stream
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  raw = fread(fid, [1 blocksize], 'uint8=>uint8');
+  bytes = bytes + numel(raw);
+  if feedback
+    fprintf('%d bytes read\n', bytes);
+  end
+  
+  raw = cat(2, raw, rem);
+  [dat, rem] = decode_modeeg(raw);
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % from here onward it is specific to writing the data to another stream
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  if ~isempty(dat)
+    count = count + 1;
+    fprintf('writing %d channels, %d samples\n', size(dat,1), size(dat,2));
+    if count==1
+      % flush the file, write the header and subsequently write the data segment
+      ft_write_data(cfg.target.datafile, dat(chanindx,:), 'header', hdr, 'dataformat', cfg.target.dataformat, 'chanindx', chanindx, 'append', false);
+    else
+      % write the data segment
+      ft_write_data(cfg.target.datafile, dat(chanindx,:), 'header', hdr, 'dataformat', cfg.target.dataformat, 'chanindx', chanindx, 'append', true);
+    end
+  end
+  
+  
+end % while true
---
+++ realtime/example/private/decode_modeeg.m
@@ -0,0 +1,64 @@
+function [data, rem] = decode_modeeg(raw)
+
+% DECODE_MODEEG takes a piece of the Modular EEG (OpenEEG) data
+% stream from the serial port or bluetooth and decodes it.
+% 
+% Use as
+%  [dat, rem] = decode_modeeg(raw)
+% where
+%   raw = vector with bytes that was read from the serial port
+%   dat = 6xN matrix with EEG values
+%   rem = vector with remaining bytes that were not decoded
+%
+% The remaining bytes should be added to the raw data upon the next
+% call.
+
+% Copyright (C) 2012, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
+packetsize = 17;
+
+begbyte   = find(raw(1:17)==hex2dec('a5') & raw(2:18)==hex2dec('5a'));
+numpacket = floor((length(raw)-begbyte)/packetsize);
+endbyte   = begbyte + numpacket*packetsize -1;
+
+% trim the junk at the beginning
+rem = raw(endbyte+1:end);
+raw = raw(begbyte:endbyte);
+raw = reshape(raw, 17, numpacket);
+
+%   uint8_t   sync0;     // = 0xA5
+%   uint8_t   sync1;     // = 0x5A
+%   uint8_t   version;   // = 2
+%   uint8_t   count;     // packet counter. Increases by 1 each packet
+%   uint16_t  data[6];   // 10-bit sample (= 0 - 1023) in big endian (Motorola) format
+%   uint8_t   switches;  // State of PD5 to PD2, in bits 3 to 0
+
+sync0    = raw(1,:);
+sync1    = raw(2,:);
+version  = raw(3,:);
+count    = raw(4,:);
+data     = raw(5:16,:);
+switches = raw(17,:);
+
+% convert the data from uint8 into uint16 and swap the bytes
+% this assumes that MATLAB is running on a little endian computer
+data = swapbytes(reshape(typecast(data(:), 'uint16'), 6, numpacket));
+
