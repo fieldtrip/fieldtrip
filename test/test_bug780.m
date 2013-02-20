b7cd0e83d03b640f2c6912be3b9a0d74086f6aa6
Robert Oostenveld <r.oostenveld@donders.ru.nl>
2013-02-20 14:39:17 +0100

    enhancement - implemented the three variations for dealing with filter instabilities as suggested in http://bugzilla.fcdonders.nl/show_bug.cgi?id=780. Also added a test script.

---
+++ test/test_bug780.m
@@ -0,0 +1,62 @@
+function test_bug780
+
+% TEST test_bug780
+% TEST ft_preproc_lowpassfilter ft_preproc_highpassfilter ft_preproc_bandpassfilter ft_preproc_bandstopfilter filter_with_correction
+
+dat = randn(1,1000);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ft_preproc_highpassfilter(dat, 1000, 0.1, 9, [], [], 'split');
+ft_preproc_highpassfilter(dat, 1000, 0.1, 9, [], [], 'reduce');
+try
+  ft_preproc_highpassfilter(dat, 1000, 0.1, 9, [], [], 'none');
+  fail = false;
+catch
+  fail = true;
+end
+if ~fail
+  error('this test should have failed');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ft_preproc_lowpassfilter(dat, 1000, 0.1, 9, [], [], 'split');
+ft_preproc_lowpassfilter(dat, 1000, 0.1, 9, [], [], 'reduce');
+try
+  ft_preproc_lowpassfilter(dat, 1000, 0.1, 9, [], [], 'none');
+  fail = false;
+catch
+  fail = true;
+end
+if ~fail
+  error('this test should have failed');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ft_preproc_bandpassfilter(dat, 1000, [49 51], 9, [], [], 'split');
+ft_preproc_bandpassfilter(dat, 1000, [49 51], 9, [], [], 'reduce');
+try
+  ft_preproc_bandpassfilter(dat, 1000, [49 51], 9, [], [], 'none');
+  fail = false;
+catch
+  fail = true;
+end
+if ~fail
+  error('this test should have failed');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ft_preproc_bandstopfilter(dat, 1000, [49 51], 9, [], [], 'split');
+ft_preproc_bandstopfilter(dat, 1000, [49 51], 9, [], [], 'reduce');
+try
+  ft_preproc_bandstopfilter(dat, 1000, [49 51], 9, [], [], 'none');
+  fail = false;
+catch
+  fail = true;
+end
+if ~fail
+  error('this test should have failed');
+end
