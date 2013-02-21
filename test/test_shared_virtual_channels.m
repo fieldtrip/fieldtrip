9dea4d45b580081b3bd20c29093f8b8c5f5428f1
Jorn Horschig <jm.horschig@donders.ru.nl>
2013-02-21 15:46:25 +0000

    fixed test script
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7527 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_shared_virtual_channels.m
+++ test/test_shared_virtual_channels.m
@@ -1,53 +1,51 @@
 function test_shared_virtual_channels
 
+% TEST test_shared_virtual_channels
+% TEST ft_timelockanalysis ft_sourceanalysis ft_channelselection ft_databrowser
+
+% disable verbose output
+global ft_default;
+ft_default.feedback = 'no';
 
 if ispc
   datadir  = 'H:\common\matlab\fieldtrip\data\ftp\tutorial\beamformer_extended';
-  templatedir  = 'H:/common/matlab/fieldtrip/template/sourcemodel';
 elseif isunix
-  datadir = '/home/common/matlab/fieldtrip/data/ftp/tutorial/sensor_analysis';
   datadir = '/home/common/matlab/fieldtrip/data/ftp/tutorial/beamformer_extended';
 end
 
-load hdm;
-load source_diff;
-load source_coh_lft;
-
-
-
+load(fullfile(datadir, 'hdm.mat'));
+load(fullfile(datadir, 'source_diff'));
+load(fullfile(datadir, 'source_coh_lft'));
+load(fullfile(datadir, 'data_cmb'));
 
 [maxval, maxcohindx] = max(source_coh_lft.avg.coh);
 source_coh_lft.pos(maxcohindx, :)
 
 assert(identical(source_coh_lft.pos(maxcohindx, :), [3.2000   -0.6000   7.4000], 'reltol', 0.001), 'coherence peak location not what it used to be!');
 
-
 [maxval, maxpowindx] = max(source_diff.avg.pow);
 source_diff.pos(maxpowindx, :)
 
 assert(identical(source_diff.pos(maxpowindx, :), [ 0.8000   -8.6000    2.6000], 'reltol', 0.001), 'gamma power peak location not what it used to be!');
 
+cfg                   = [];
+cfg.covariance        = 'yes';
+cfg.channel           = 'MEG';
+cfg.vartrllength      = 2;
+cfg.covariancewindow  = 'all';
+tlock                 = ft_timelockanalysis(cfg, data_cmb);
 
 
+cfg             = [];
+cfg.method      = 'lcmv';
+cfg.vol         = hdm;
+cfg.grid.pos    = source_diff.pos([maxcohindx maxpowindx], :);
+cfg.grid.inside = 1:size(cfg.grid.pos, 1);
+cfg.grid.outside = [];
+cfg.keepfilter  = 'yes';
+source_idx      = ft_sourceanalysis(cfg, tlock);
 
-  cfg                   = [];
-  cfg.covariance        = 'yes';
-  cfg.channel           = 'MEG';
-  cfg.vartrllength      = 2;
-  cfg.covariancewindow  = 'all';
-  tlock                 = ft_timelockanalysis(cfg, data_cmb);
-  
-  
-  cfg             = [];
-  cfg.method      = 'lcmv';
-  cfg.vol         = hdm;
-  cfg.grid.pos    = source_diff.pos([maxcohindx maxpowindx], :);
-  cfg.grid.inside = 1:size(cfg.grid.pos, 1);
-  cfg.grid.outside = [];
-  cfg.keepfilter  = 'yes';
-  source_idx      = ft_sourceanalysis(cfg, tlock);
-  
-  
+
 beamformer_lft_coh = source_idx.avg.filter{1};
 beamformer_gam_pow = source_idx.avg.filter{2};
 
@@ -65,6 +63,6 @@ for i=1:length(data_cmb.trial)
   gam_pow_data.trial{i} = beamformer_gam_pow * data_cmb.trial{i}(chansel,:);
 end
 
-  cfg = [];
-  cfg.viewmode = 'vertical';  % you can also specify 'butterfly'
-  ft_databrowser(cfg, gam_pow_data);
+cfg = [];
+cfg.viewmode = 'vertical';  % you can also specify 'butterfly'
+ft_databrowser(cfg, gam_pow_data);
