bda7e4a7b6a7ba6bab01d3a9b652d86409377c40
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2013-02-23 11:58:39 +0000

    enhancement - added testfunction for bug2004
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7535 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2004.m
@@ -0,0 +1,20 @@
+% TEST test_bug2004 
+% TEST ft_connectivityanalysis
+% TEST ft_connectivity_corr
+% TEST ft_connectivity_powcorr_ortho
+
+%% test the functionality of ft_connectivityanalysis with respect to source level data (pcc)
+
+load ('/home/common/matlab/fieldtrip/data/test/bug2004.mat');
+
+cfg = [];
+cfg.method = 'coh';
+out = ft_connectivityanalysis(cfg, data);
+cfg.method = 'plv';
+out2 = ft_connectivityanalysis(cfg, data);
+cfg.method = 'amplcorr';
+out3 = ft_connectivityanalysis(cfg, data);
+cfg.method = 'powcorr';
+out4 = ft_connectivityanalysis(cfg, data);
+cfg.method = 'powcorr_ortho';
+out5 = ft_connectivityanalysis(cfg, data);
