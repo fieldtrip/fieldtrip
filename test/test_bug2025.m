b0704b440c2c7fedf10da001d449733d2843cb6b
Robert Oostenveld <r.oostenveld@gmail.com>
2013-03-04 15:32:03 +0000

    enhancement - added a test script for bug 2025
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7570 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2025.m
@@ -0,0 +1,13 @@
+function test_bug2025
+
+% TEST test_bug2025
+% TEST ft_plot_vol ft_compute_leadfield
+
+vol = [];
+assert(ft_voltype(vol, 'infinite'))
+
+vol.type = 'infinite';
+
+% there is nothing to plot, but nevertheless it should not result in an error
+ft_plot_vol(vol);
+
