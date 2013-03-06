97deca456c432fb5847915b25e5bc68d777ff769
Jan-Mathijs Schoffelen <jan.schoffelen@donders.ru.nl>
2013-03-06 14:21:58 +0000

    enhancement - upgraded to test 4 datasets from different sites: seems to work so far
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7597 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug2027.m
+++ test/test_bug2027.m
@@ -1,13 +1,18 @@
 % TEST test_bug2027
 % TEST read_4d_hdr
 % TEST ft_read_header
+% TEST ft_channelselection
 
 % Bug: ft_read_header returns only 152 MEG channels in the hdr.label
 % This goes wrong at a low level, i.e. in read_4d_hdr
 
 % reproduce
-dataset = '/home/common/matlab/fieldtrip/data/test/bug2027/e,rfhp1.0Hz,COH';
-hdr     = ft_read_header(dataset);
-
-assert(numel(ft_channelselection('MEG',hdr.label))==248);
-
+datasets = {'/home/common/matlab/fieldtrip/data/test/bug2027/colorado/e,rfhp1.0Hz,COH';
+            '/home/common/matlab/fieldtrip/data/test/bug2027/glasgow/e,rfDC';
+            '/home/common/matlab/fieldtrip/data/test/bug2027/marseille/e,rfhp1.0Hz,COH';
+            '/home/common/matlab/fieldtrip/data/test/bug2027/stlouis/e,rfhp1.0Hz,COH'};
+                        
+for k = 1:numel(datasets)
+  hdr = ft_read_header(datasets{k});
+  assert(numel(ft_channelselection('MEG',hdr.label))==248);
+end
