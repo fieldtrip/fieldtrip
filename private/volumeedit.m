fdbb4fb121e075f3690a1adf88f96005695582af
Jorn Horschig <jm.horschig@donders.ru.nl>
2013-03-12 16:18:28 +0000

    enhancement - accounting for non-boolean data
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7643 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/volumeedit.m
+++ private/volumeedit.m
@@ -124,7 +124,7 @@ opt.handleseraser = [he1(:)';he2(:)';he3(:)'];
 opt.ijk           = [xi yi zi];
 opt.dim           = dim;
 opt.quit          = 0;
-opt.mask          = opt.data;
+opt.mask          = opt.data~=0;
 opt.radius        = [3 3 3];
 
 setappdata(h, 'opt', opt);
@@ -137,7 +137,7 @@ end
 opt = getappdata(h, 'opt');
 delete(h);
 
-dataout = opt.mask;
+dataout = datain(opt.mask);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SUBFUNCTION
@@ -295,7 +295,8 @@ if isfield(opt, 'bckgrnd')
   set(opt.handlesaxes(3),'nextplot','replace');
 end
 
-tmpdata = opt.mask;
+tmpdata = opt.data;;
+tmpdata(opt.mask) = 0;
 xi2  = xi+(-opt.radius(1):opt.radius(1)); xi2(xi2<1) = 1; xi2(xi2>opt.dim(1)) = opt.dim(1);
 yi2  = yi+(-opt.radius(2):opt.radius(2)); yi2(yi2<1) = 1; yi2(yi2>opt.dim(2)) = opt.dim(2);
 zi2  = zi+(-opt.radius(3):opt.radius(3)); zi2(zi2<1) = 1; zi2(zi2>opt.dim(3)) = opt.dim(3);
