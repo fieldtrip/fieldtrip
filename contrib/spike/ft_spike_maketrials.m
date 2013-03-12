0d7b2e607c99f0d17c91b990617104d9e6686e13
Martin Vinck <martinvinck@gmail.com>
2013-03-12 14:38:35 +0000

    ensuring correct converstion of samples to timestamps when cfg.trl is in samples for ft_spike_maketrials.m
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7642 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- contrib/spike/ft_spike_maketrials.m
+++ contrib/spike/ft_spike_maketrials.m
@@ -83,9 +83,12 @@ if size(cfg.trl,2)<3,
 end
 
 % check if the cfg.trl is in the right order and whether the trials are overlapping
-if ~all(cfg.trl(:,2)>cfg.trl(:,1))
+if strcmp(cfg.trlunit, 'timestamps') && ~all(cfg.trl(:,2)>cfg.trl(:,1))
   warning('the end of some trials does not occur after the beginning of some trials in cfg.trl'); %#ok<*WNTAG>
-end
+elseif strcmp(cfg.trlunit, 'samples') && ~all((1+cfg.trl(:,2))>cfg.trl(:,1))
+  warning('the end of some trials does not occur after the beginning of some trials in cfg.trl'); %#ok<*WNTAG>
+end  
+  
 if size(cfg.trl,1)>1
   if ~all(cfg.trl(2:end,1)>cfg.trl(1:end-1,2))
     warning('your trials are overlapping, trials will not be statistically independent, some spikes will be duplicated'); %#ok<*WNTAG>
@@ -208,16 +211,23 @@ elseif strcmp(cfg.trlunit,'samples')
         FirstTimeStamp = cast(FirstTimeStamp, classTs);
     end
     sample = double(ts-FirstTimeStamp)/TimeStampPerSample + 1; % no rounding (compare ft_appendspike)
-    waveSel = [];
+    
+    % ensure that cfg.trl is of class double
+    if ~strcmp(class(cfg.trl), 'double')
+      cfg.trl = double(cfg.trl);
+    end
+    
+    % see which spikes fall into the trials
+    waveSel = [];        
     for iTrial = 1:nTrials
-      begsample = cfg.trl(iTrial,1);
-      endsample = cfg.trl(iTrial,2);
-      sel       = find((sample>=begsample) & (sample<=endsample));
+      begsample = cfg.trl(iTrial,1) - 1/2;
+      endsample = cfg.trl(iTrial,2) + 1/2;
+      sel       = find((sample>=begsample) & (sample<endsample));
       dSample   = sample(sel)-begsample;
       offset    = cfg.trl(iTrial,3)/Fs;               
       tTrial    = dSample/Fs + offset;
       trialNum  = ones(1,length(tTrial))*iTrial;
-      trialDur  = (cfg.trl(iTrial,2)-cfg.trl(iTrial,1))/Fs;
+      trialDur  = (1 + cfg.trl(iTrial,2)-cfg.trl(iTrial,1))/Fs;
       
       spike.time{iUnit}         = [spike.time{iUnit} tTrial(:)'];
       spike.trial{iUnit}        = [spike.trial{iUnit} trialNum];
