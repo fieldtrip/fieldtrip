ee3eaf54f78646d5978cdc0cf63514bfd64b5f79
Martin Vinck <martinvinck@gmail.com>
2013-03-13 02:28:08 +0000

    corrected bug with checking class of FirstTimeStamp in ft_spike_maketrials.m
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7649 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- contrib/spike/ft_spike_maketrials.m
+++ contrib/spike/ft_spike_maketrials.m
@@ -63,9 +63,10 @@ revision = '$Id$';
 
 % do the general setup of the function
 ft_defaults
-ft_preamble help
-ft_preamble callinfo
-ft_preamble trackconfig
+ft_preamble help            % this will show the function help if nargin==0 and return an error
+ft_preamble provenance      % this records the time and memory usage at teh beginning of the function
+ft_preamble trackconfig     % this converts the cfg structure in a config object, which tracks the cfg options that are being used
+ft_preamble debug           % this allows for displaying or saving the function name and input arguments upon an error
 
 % check if input data is indeed spikeraw format
 spike   = ft_checkdata(spike,'datatype', 'spike', 'feedback', 'yes');
@@ -197,11 +198,11 @@ elseif strcmp(cfg.trlunit,'samples')
     
     if ~strcmp(classTs, class(FirstTimeStamp))
         flag = 1;
-        if strcmp(classTs, 'double') || class(FirstTimeStamp, 'double')
+        if strcmp(classTs, 'double') || strcmp(class(FirstTimeStamp), 'double')
           mx = 2^53;
           flag = 0;
         end
-        if strcmp(classTs, 'single') || class(FirstTimeStamp, 'single')          
+        if strcmp(classTs, 'single') || strcmp(class(FirstTimeStamp), 'single')          
           mx = 2^24; % largest precision number
           flag = 0;
         end
@@ -250,8 +251,9 @@ if size(cfg.trl,2) > 3
 end
 
 % do the general cleanup and bookkeeping at the end of the function
-ft_postamble trackconfig
-ft_postamble callinfo
+ft_postamble debug            % this clears the onCleanup function used for debugging in case of an error
+ft_postamble trackconfig      % this converts the config object back into a struct and can report on the unused fields
+ft_postamble provenance       % this records the time and memory at the end of the function, prints them on screen and adds this information together with the function name and matlab version etc. to the output cfg
 ft_postamble previous spike
 ft_postamble history spike
 
