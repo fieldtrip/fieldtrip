14b4b08918e3dd26433752e0745d979de173c502
Robert Oostenveld <r.oostenveld@gmail.com>
2013-03-17 15:03:43 +0000

    Merge branch 'master' of github.com:oostenveld/fieldtrip
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7683 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_prepare_layout.m
+++ ft_prepare_layout.m
@@ -149,7 +149,7 @@ end
 if isstruct(cfg.layout) && isfield(cfg.layout, 'pos') && isfield(cfg.layout, 'label') && isfield(cfg.layout, 'width') && isfield(cfg.layout, 'height')
   layout = cfg.layout;
   cfg.channel = ft_channelselection(cfg.channel, layout.label);
-  chansel = match_str(layout.label, {cfg.channel{:} 'COMNT' 'SCALE'}); % this keeps them in the order of the layout
+  chansel = match_str(layout.label, cat(1, cfg.channel(:), 'COMNT', 'SCALE')); % this keeps them in the order of the layout
   % return the layout for the subset of channels
   layout.pos    = layout.pos(chansel,:);
   layout.width  = layout.width(chansel);
@@ -278,7 +278,14 @@ elseif ischar(cfg.layout)
     if ~exist(cfg.layout, 'file')
       error('the specified layout file %s was not found', cfg.layout);
     end
-    load(cfg.layout, 'lay');
+    tmp = load(cfg.layout, 'lay');
+    if isfield(tmp, 'layout')
+      layout = tmp.layout;
+    elseif isfield(tmp, 'lay')
+      layout = tmp.lay;
+    else
+      error('mat file does not contain a layout');
+    end
     
   elseif ft_filetype(cfg.layout, 'layout')
     
