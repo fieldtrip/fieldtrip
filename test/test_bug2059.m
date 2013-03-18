91e378dda2233b7bf133292329bd1fe11a85705f
Jorn Horschig <jm.horschig@donders.ru.nl>
2013-03-18 12:29:41 +0000

    bugfix #2059 #2066 - subselection of channels when plotting works again for all cases, testscript extended
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7690 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug2059.m
+++ test/test_bug2059.m
@@ -27,7 +27,7 @@ assert(~isequalfigure(1, 2));
 
 % the problem seemed to be in a buggy interaction between interactive and channel selection
 
-cfg.interactive = 'yes';
+cfg.interactive = 'no';
 cfg.channel = {'4','11','16','19','20','21'};
 figure(3)
 ft_topoplotER(cfg, GA_loc_diff);
@@ -40,3 +40,41 @@ ft_topoplotER(cfg, GA_loc_diff);
 
 % these figures should be the same
 assert(isequalfigure(3, 4));
+
+%% specifying layout as a string
+
+cfg.layout = 'easycapM14.lay';
+cfg.interactive = 'no';
+cfg.channel = {'4','11','16','19','20','21'};
+figure(5)
+ft_topoplotER(cfg, GA_loc_diff);
+
+
+cfg.interactive = 'yes';
+cfg.channel = {'4','11','16','19','20','21'};
+figure(6)
+ft_topoplotER(cfg, GA_loc_diff);
+
+% read in layout directly
+cfg.layout = ft_prepare_layout(cfg);
+cfg.interactive = 'no';
+cfg.channel = {'4','11','16','19','20','21'};
+figure(7)
+ft_topoplotER(cfg, GA_loc_diff);
+
+
+cfg.interactive = 'yes';
+cfg.channel = {'4','11','16','19','20','21'};
+figure(8)
+ft_topoplotER(cfg, GA_loc_diff);
+
+
+
+% all these figures should be the same
+assert(isequalfigure(5, 6));
+assert(isequalfigure(5, 7));
+assert(isequalfigure(5, 8));
+assert(isequalfigure(6, 7));
+assert(isequalfigure(6, 8));
+assert(isequalfigure(7, 8));
+
