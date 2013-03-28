dbcdf2c3c5e23da90248e94607bb3da4afdaa75a
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2013-03-28 12:33:07 +0000

    automatically synchronized identical files to revision 7713
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7714 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- trialfun/private/select_channel_list.m
+++ trialfun/private/select_channel_list.m
@@ -42,6 +42,7 @@ end
 pos      = get(0,'DefaultFigurePosition');
 pos(3:4) = [290 300];
 dlg      = dialog('Name', titlestr, 'Position', pos);
+axis off % explicitly turn of axis, sometimes an axis system appears
 
 select            = select(:)';     % ensure that it is a row array
 userdata.label    = label;
