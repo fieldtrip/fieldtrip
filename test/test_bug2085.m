dc7ee0f8090ce5b580d4e2f6d5a963e7686d7142
Robert Oostenveld <r.oostenveld@gmail.com>
2013-03-31 12:13:11 +0000

    enhancement - created test script for timing the leadfield computation
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7731 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2085.m
@@ -0,0 +1,53 @@
+% function test_bug2085
+
+% TEST test_bug2085
+% TEST ft_senstype ft_senslabel
+
+%% create a volume conductor
+vol = [];
+vol.r = 10;
+vol.o = [0 0 0];
+vol.unit = 'cm';
+vol = ft_datatype_headmodel(vol);
+
+%% create a set of sensors
+[pnt, tri] = icosahedron162;
+pnt = pnt .* 10; % convert to cm
+sel = find(pnt(:,3)>0);
+grad.pnt = pnt(sel,:) .* 1.2;
+grad.ori = pnt(sel,:);
+grad.tra = eye(length(sel));
+for i=1:length(sel)
+  grad.ori(i,:) = grad.ori(i,:) ./ norm(grad.ori(i,:));
+  grad.label{i} = sprintf('magnetometer%d', i);
+end
+grad.unit = 'cm';
+
+grad1 = ft_datatype_sens(grad);
+grad2 = ft_datatype_sens(grad);
+
+grad2.type = 'magnetometer'; % this makes ft_senstype much faster
+
+%% determine the time to compute some leadfields
+
+cfg = [];
+cfg.vol = vol;
+cfg.resolution = 4;
+cfg.channel = 'all';
+
+tic 
+cfg.grad = grad1;
+grid1 = ft_prepare_leadfield(cfg);
+time1 = toc;
+
+tic 
+cfg.grad = grad2;
+grid2 = ft_prepare_leadfield(cfg);
+time2 = toc;
+
+%% compare the time that it took, this fraction 1.5 is rather arbitrary
+
+if (time1/time2)>1.5
+  error('the leadfield computation without grad.type takes too long (%d seconds compared to %d seconds)', time1, time2);
+end
+
