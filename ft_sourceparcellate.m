9b160b05af8e0fe1426de654e564f04118cb0232
Robert Oostenveld <r.oostenveld@gmail.com>
2013-04-02 19:23:40 +0000

    enhancement - updated the documentation and added pre/postable, see #1775
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7741 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_sourceparcellate.m
+++ ft_sourceparcellate.m
@@ -1,20 +1,55 @@
-function parcel = hcp_sourceparcellate(cfg, source)
+function parcel = ft_sourceparcellate(cfg, source)
 
-% HCP_SOURCEPARCELLATE combines the source-reconstruction values in the
-% parcels by averaging, or by concatenating.
+% FT_SOURCEPARCELLATE combines the source-reconstruction values in the
+% parcels by averaging or by concatenating.
 %
 % Use as
-%    parcel = hcp_sourceparcellate(cfg, source)
+%    parcel = ft_sourceparcellate(cfg, source)
 % where the input is a 3D voxel-based or a 2D surface-based source grid
 % and the output is a channel-based representation with the averages or
 % concatenared representation per parcel.
 %
+% The input data structure is for example obtained from FT_SOURCEANALYSIS
+% or FT_COMPUTE_LEADFIELD and is described in detail in FT_DATATYPE_SOURCE.
+% The output data structure is described in detail in
+% FT_DATATYPE_PARCELLATION.
+%
 % The configuration "cfg" is a structure that can contain the following
 % fields
+%   cfg.method       = string
 %   cfg.parcellation = string, fieldname that contains the desired parcellation
-%   cfg.parameter    = cell-array with strings, fields that should be parcellated (can be 'all')
+%   cfg.parameter    = cell-array with strings, fields that should be parcellated (default = 'all')
+%
+% See also FT_SOURCEANALYSIS, FT_DATATYPE_PARCELLATION, FT_DATATYPE_SEGMENTATION
 
-% Copyright (C) 2012, Robert Oostenveld
+% Copyright (C) 2012-2013, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
+revision = '$Id$';
+
+ft_defaults                 
+ft_preamble help            
+ft_preamble provenance      
+ft_preamble trackconfig     
+ft_preamble debug           
+ft_preamble loadvar source  
 
 % get the defaults
 cfg.parcellation = ft_getopt(cfg, 'parcellation');
@@ -84,6 +119,7 @@ for i=1:numel(fn)
     dimord{i} = source.dimord; % the general dimord
   end
 end
+
 % these two will now contain the fields and corresponding dimord to work on
 fn     = fn(sel);
 dimord = dimord(sel);
@@ -246,5 +282,10 @@ for i=1:numel(fn)
   clear dat tmp tmpdimord
 end % for each of the fields that should be parcellated
 
-try, cfg.previous = source.cfg; end
-parcel.cfg = cfg;
+ft_postamble debug            
+ft_postamble trackconfig      
+ft_postamble provenance       
+ft_postamble previous source  
+ft_postamble history parcel  
+ft_postamble savevar parcel  
+
