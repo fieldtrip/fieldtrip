0834ee487920bbdcc0851544f78e5b0c7a4d5cc7
Robert Oostenveld <r.oostenveld@donders.ru.nl>
2013-04-03 18:26:48 -0500

    fixed even more merge conflicts

--- test/test_bug1775.m
+++ test/test_bug1775.m
@@ -34,32 +34,18 @@ vol = ft_datatype_headmodel(vol);
 %% create some precomputed leadfields
 
 cfg = [];
-<<<<<<< HEAD
 cfg.grad            = grad;
 cfg.vol             = vol;
 cfg.grid.resolution = 1;
 cfg.channel         = 'all';
-=======
-cfg.grad = grad;
-cfg.vol = vol;
-cfg.grid.resolution = 1;
-cfg.channel = 'all';
->>>>>>> a13ab85c7c3c7dc51acd25cffda5c04c94d97ec0
 grid = ft_prepare_leadfield(cfg);
 
 %% create an anatomical parcellation
 parcellation = [];
-<<<<<<< HEAD
 parcellation.pos        = grid.pos;
 parcellation.unit       = grid.unit;
 parcellation.type       = zeros(size(grid.pos,1),1);
 parcellation.typelabel  = {};
-=======
-parcellation.pos = grid.pos;
-parcellation.unit = grid.unit;
-parcellation.type      = zeros(size(grid.pos,1),1);
-parcellation.typelabel = {};
->>>>>>> a13ab85c7c3c7dc51acd25cffda5c04c94d97ec0
 height = [3 4 5 6 7 8 9];
 for i=1:length(height)
   sel = parcellation.pos(:,3)==height(i);
@@ -70,13 +56,8 @@ parcellation.cfg = 'manual'; % to check whether the provenance is correct
 
 %% create simulated data
 cfg = [];
-<<<<<<< HEAD
 cfg.grad    = grad;
 cfg.vol     = vol;
-=======
-cfg.grad = grad;
-cfg.vol = vol;
->>>>>>> a13ab85c7c3c7dc51acd25cffda5c04c94d97ec0
 cfg.dip.pos = [0 0 4];
 data = ft_dipolesimulation(cfg);
 
@@ -85,7 +66,6 @@ cfg.covariance = 'yes';
 timelock = ft_timelockanalysis(cfg, data);
 
 cfg = [];
-<<<<<<< HEAD
 cfg.method  = 'mtmfft';
 cfg.taper   = 'hanning';
 freq1 = ft_freqanalysis(cfg, data);
@@ -133,30 +113,6 @@ source4.freq      = 1:5;
 source4.time      = 1:3;
 source4.coh       = randn(size(source2.pos,1), size(source2.pos,1), 5, 3);
 source4.cohdimord = 'pos_pos_freq_time';
-=======
-cfg.method = 'mtmfft';
-cfg.taper = 'hanning';
-freq1 = ft_freqanalysis(cfg, data);
-
-cfg = [];
-cfg.method = 'wavelet';
-cfg.toi = data.time{1};
-freq2 = ft_freqanalysis(cfg, data);
-
-cfg = [];
-cfg.grad = grad;
-cfg.vol = vol;
-cfg.grid = grid;
-cfg.method = 'lcmv';
-source1 = ft_sourceanalysis(cfg, timelock);
-
-cfg = [];
-cfg.grad = grad;
-cfg.vol = vol;
-cfg.grid = grid;
-cfg.method = 'mne';
-cfg.lambda = 0;
-source2 = ft_sourceanalysis(cfg, timelock);
 
 cfg = [];
 source4p = ft_sourceparcellate(cfg, source4, parcellation);
@@ -175,7 +131,6 @@ for i=source2.inside(:)'
 end
 source5.cohdimord = '{pos_pos}_ori_freq';
 
-<<<<<<< HEAD
 cfg = [];
 cfg.method = 'mean';
 source5p = ft_sourceparcellate(cfg, source5, parcellation);
@@ -192,13 +147,6 @@ source6.outside   = source2.outside;
 source6.time      = 1:20;
 source6.mom       = randn(size(source2.pos,1),20);
 source6.momdimord = 'pos_time';
-=======
-%% make some parcellations
-cfg = [];
-gridp    = ft_sourceparcellate(cfg, grid, parcellation);
-source1p = ft_sourceparcellate(cfg, source1, parcellation);
-source2p = ft_sourceparcellate(cfg, source2, parcellation);
->>>>>>> a13ab85c7c3c7dc51acd25cffda5c04c94d97ec0
 
 cfg = [];
 cfg.method = 'mean';
