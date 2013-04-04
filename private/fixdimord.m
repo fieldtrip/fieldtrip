b8e18ee4d9a77957632eb8cfb803ffb947836dbb
Robert Oostenveld <r.oostenveld@gmail.com>
2013-04-04 12:25:47 +0000

    automatically synchronized identical files to revision 7763
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7764 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/fixdimord.m
+++ private/fixdimord.m
@@ -103,6 +103,10 @@ if strcmp(data.dimord, 'voxel')
 end
 
 dimtok = tokenize(data.dimord, '_');
+if strncmp('{pos_pos}', data.dimord, 9)
+  % keep these together for bivariate source structures
+  dimtok = {'{pos_pos}', dimtok{3:end}};
+end
 
 for i=1:length(dimtok)
   switch dimtok{i}
@@ -147,6 +151,9 @@ for i=1:length(dimtok)
     case {'{pos}'}
       % this is for source data on a 3-d grid, a cortical sheet, or unstructured positions
       % the data itself is represented in a cell-array, e.g. source.mom or source.leadfield
+
+    case {'{pos_pos}'}
+      % this is for bivariate source data on a 3-d grid, a cortical sheet, or unstructured positions
       
     otherwise
       error(sprintf('unexpected dimord "%s"', data.dimord));
