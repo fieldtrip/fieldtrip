52d47f0172dec1cc7e2e36978bb980335a1a6aa9
Roemer van der Meij <roemer.van.der.meij@gmail.com>
2013-04-16 18:08:18 +0200

    bugfix: test script contained reference to nonexistent layout, fixed

--- test/test_bug2086.m
+++ test/test_bug2086.m
@@ -9,12 +9,10 @@ return
 load(dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2086.mat'));
 
 cfg = [];
-%cfg.dataset = sprintf('%s', 'data/' , subj{isub} ,'/', cond{icond},  set{1},time{itime}  ,'_all.mat');
 cfg.continuous = 'no';
 cfg.preproc.channel = {'all'  };
 cfg.preproc.demean = 'yes';
 cfg.viewmode = 'butterfly';
 cfg.ylim = [-200 200];
-%cfg.layout = 'J:\Projects\ANT_trial\Export\customANT.lay';     
 cfg.channel = 'all';
 cfg = ft_databrowser(cfg,data);
