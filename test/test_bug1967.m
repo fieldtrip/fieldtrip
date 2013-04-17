4c52b81eca9af64ca3eb7e3ebde584c701a90def
Robert Oostenveld <r.oostenveld@gmail.com>
2013-04-17 13:51:27 +0000

    enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7961 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1967.m
+++ test/test_bug1967.m
@@ -12,6 +12,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> enhancement - started making a test script for bug 1967
 =======
@@ -55,6 +56,8 @@ function test_bug1967
 =======
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 =======
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 function test_bug1967
 >>>>>>> merge conflict
 =======
@@ -66,8 +69,11 @@ function test_bug1967
 =======
 function test_bug1967
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
+<<<<<<< HEAD
 =======
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 
 % TEST test_bug1967
 % TEST ft_prepare_vol_sens
@@ -116,6 +122,9 @@ ft_sourceplot(cfg, volume);
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 =======
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 =======
@@ -127,6 +136,7 @@ ft_sourceplot(cfg, volume);
 =======
 %% convert it into a mesh 
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
@@ -172,6 +182,8 @@ ft_sourceplot(cfg, volume);
 %% convert it into a head model
 >>>>>>> enhancement - started making a test script for bug 1967
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 cfg = [];
 cfg.tissue = {'skin' 'skull' 'brain'};
 cfg.method = 'hexahedral';
@@ -191,6 +203,9 @@ mesh = ft_prepare_mesh(cfg, volume);
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 =======
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 =======
@@ -227,6 +242,7 @@ if isequal(sens.elecpos, elec.elecpos)
 end
 =======
 =======
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
@@ -242,12 +258,17 @@ end
 %% convert it into a head model
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
+=======
+%% convert it into a head model
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 cfg = [];
 cfg.tissue = {'skin' 'skull' 'brain'};
 cfg.conductivity = [1 1/80 1];
 cfg.method = 'simbio';
 headmodel = ft_prepare_headmodel(cfg, mesh);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -424,5 +445,32 @@ cfg.method = 'simbio';
 headmodel = ft_prepare_headmodel(cfg, mesh);
 
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+%% make some electrodes
+[pnt, tri] = icosahedron42;
+pnt = pnt(pnt(:,3)>0,:);
+pnt = pnt*55; % not precisely fitting on the mesh
+elec = [];
+elec.chanpos = pnt;
+elec.elecpos = pnt;
+for i=1:size(pnt,1)
+  elec.label{i} = num2str(i);
+end
+elec.unit = 'mm';
+
+figure
+ft_plot_mesh(mesh, 'edgeonly', 1)
+ft_plot_sens(elec)
+
+%% prepare the volume conductor and electrodes for leadfield computation
+[vol, sens] = ft_prepare_vol_sens(headmodel, elec)
+
+% elec is the original one, sens is the one after projecting
+if isequal(sens.elecpos, elec.elecpos)
+  error('the electrodes were not projected');
+end
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 
