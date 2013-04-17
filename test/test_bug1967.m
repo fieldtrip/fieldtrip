18da2243d7f44f4be0634236eb8dd74f9ce49978
Robert Oostenveld <r.oostenveld@gmail.com>
2013-04-17 12:40:10 +0000

    enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7888 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1967.m
+++ test/test_bug1967.m
@@ -1,18 +1,27 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 function test_bug1967
 =======
 % function test_bug1967
 >>>>>>> enhancement - started making a test script for bug 1967
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 function test_bug1967
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 =======
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+=======
+function test_bug1967
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 
 % TEST test_bug1967
 % TEST ft_prepare_vol_sens
@@ -50,6 +59,9 @@ ft_sourceplot(cfg, volume);
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 %% convert it into a mesh 
 =======
 %% convert it into a head model
@@ -57,12 +69,15 @@ ft_sourceplot(cfg, volume);
 =======
 %% convert it into a mesh 
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
+<<<<<<< HEAD
 =======
 %% convert it into a mesh 
 =======
 %% convert it into a head model
 >>>>>>> enhancement - started making a test script for bug 1967
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 cfg = [];
 cfg.tissue = {'skin' 'skull' 'brain'};
 cfg.method = 'hexahedral';
@@ -71,6 +86,9 @@ mesh = ft_prepare_mesh(cfg, volume);
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 %% convert it into a head model
 cfg = [];
 cfg.tissue = {'skin' 'skull' 'brain'};
@@ -103,8 +121,11 @@ if isequal(sens.elecpos, elec.elecpos)
 end
 =======
 =======
+<<<<<<< HEAD
 =======
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 %% convert it into a head model
 >>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 cfg = [];
@@ -113,6 +134,7 @@ cfg.conductivity = [1 1/80 1];
 cfg.method = 'simbio';
 headmodel = ft_prepare_headmodel(cfg, mesh);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> enhancement - started making a test script for bug 1967
 =======
@@ -149,5 +171,32 @@ cfg.method = 'simbio';
 headmodel = ft_prepare_headmodel(cfg, mesh);
 
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 >>>>>>> enhancement - started making a test script for bug 1967
+=======
+%% make some electrodes
+[pnt, tri] = icosahedron42;
+pnt = pnt(pnt(:,3)>0,:);
+pnt = pnt*55; % not precisely fitting on the mesh
+elec = [];
+elec.chanpos = pnt;
+elec.elecpos = pnt;
+for i=1:size(pnt,1)
+  elec.label{i} = num2str(i);
+end
+elec.unit = 'mm';
+
+figure
+ft_plot_mesh(mesh, 'edgeonly', 1)
+ft_plot_sens(elec)
+
+%% prepare the volume conductor and electrodes for leadfield computation
+[vol, sens] = ft_prepare_vol_sens(headmodel, elec)
+
+% elec is the original one, sens is the one after projecting
+if isequal(sens.elecpos, elec.elecpos)
+  error('the electrodes were not projected');
+end
+>>>>>>> enhancement - extended test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=1967
 
