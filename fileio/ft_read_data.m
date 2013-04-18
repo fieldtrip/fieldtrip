b03febd2d7bde1982c80539fb398eb27c1abfd48
Robert Oostenveld <r.oostenveld@gmail.com>
2013-04-18 15:14:32 +0000

    computing median timestamps per sample in uint64 format
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7994 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/ft_read_data.m
+++ fileio/ft_read_data.m
@@ -773,12 +773,17 @@ switch dataformat
     endsample = endsample - (begrecord-1)*512;
     if istrue(timestamp)   
 <<<<<<< HEAD
+<<<<<<< HEAD
       ncs.dat = cast(ncs.dat, class(ncs.TimeStamp));
       d = ncs.TimeStamp(2:end)-ncs.TimeStamp(1:end-1);
       medianTimestampPerBlock  = median(double(d)); % to avoid influence of the gaps
 =======
       medianTimestampPerBlock  = median(double(diff(ncs.TimeStamp))); % to avoid influence of the gaps
 >>>>>>> avoiding round-off errors in ft_read_data.m for neuralynx data
+=======
+      d = ncs.TimeStamp(2:end)-ncs.TimeStamp(1:end-1);
+      medianTimestampPerBlock  = median(double(d)); % to avoid influence of the gaps
+>>>>>>> computing median timestamps per sample in uint64 format
       TimestampPerSample       = medianTimestampPerBlock/512; % divide by known block size
       cls = class(ncs.TimeStamp);
       % replace the data with the timestamp of each sample
