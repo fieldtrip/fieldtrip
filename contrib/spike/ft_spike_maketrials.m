4684ada42f7a934278d4de68aa1cafdb4be38fa8
Robert Oostenveld <r.oostenveld@gmail.com>
2013-04-18 15:14:25 +0000

    avoiding roundoff errors in ft_spike_maketrials.m
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@7987 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- contrib/spike/ft_spike_maketrials.m
+++ contrib/spike/ft_spike_maketrials.m
@@ -167,7 +167,15 @@ if strcmp(cfg.trlunit,'timestamps')
     trialNum = [];
     sel       = [];
     for iTrial = 1:nTrials
+<<<<<<< HEAD
       isVld = find(ts>=trlEvent(iTrial,1) & ts<=trlEvent(iTrial,2));
+=======
+      if ~strcmp(class(ts), class(cfg.trl))
+        isVld = find(double(ts)>=double(events(1,iTrial)) & double(ts)<=double(events(2,iTrial)));
+      else
+        isVld = find(ts>=events(1,iTrial) & ts<=events(2,iTrial));
+      end        
+>>>>>>> avoiding roundoff errors in ft_spike_maketrials.m
       if ~isempty(isVld)
         trialNum = [trialNum; iTrial*ones(length(isVld),1)];  %#ok<*AGROW>
       end
@@ -182,7 +190,7 @@ if strcmp(cfg.trlunit,'timestamps')
       dt = dt/cfg.timestampspersecond + trlDouble(trialNum,3)/cfg.timestampspersecond;
 =======
       if ~strcmp(class(ts), class(cfg.trl))
-        warning('timestamps are of class %s and cfg.trl is of class %s, rounding errors are possible', class(ts), class(cfg.trl));
+        warning_once('timestamps are of class %s and cfg.trl is of class %s, rounding errors are possible', class(ts), class(cfg.trl));
         dt = double(ts) - double(cfg.trl(trialNum,1));
       else
         dt = double(ts - cfg.trl(trialNum,1)); % convert to double only here
@@ -261,7 +269,7 @@ elseif strcmp(cfg.trlunit,'samples')
 =======
     ts = spike.timestamp{iUnit}(:);
     if ~strcmp(class(ts), class(FirstTimeStamp))
-      warning('timestamps are of class %s and hdr.FirstTimeStamp is of class %s, rounding errors are possible', class(ts), class(FirstTimeStamp));
+      warning_once('timestamps are of class %s and hdr.FirstTimeStamp is of class %s, rounding errors are possible', class(ts), class(FirstTimeStamp));
       sample = (double(ts)-double(FirstTimeStamp))/TimeStampPerSample + 1;
     else
       sample = double(ts-FirstTimeStamp)/TimeStampPerSample + 1; % no rounding (compare ft_appendspike)
