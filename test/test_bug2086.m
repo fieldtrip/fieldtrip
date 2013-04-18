c3c2977bbcf92a3f53eedafff78c0654e4885e68
Robert Oostenveld <r.oostenveld@gmail.com>
2013-04-18 15:15:29 +0000

    enhancement - added test script for issue #2086
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8048 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug2086.m
+++ test/test_bug2086.m
@@ -9,10 +9,18 @@ return
 load(dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2086.mat'));
 
 cfg = [];
+<<<<<<< HEAD
+=======
+%cfg.dataset = sprintf('%s', 'data/' , subj{isub} ,'/', cond{icond},  set{1},time{itime}  ,'_all.mat');
+>>>>>>> enhancement - added test script for issue #2086
 cfg.continuous = 'no';
 cfg.preproc.channel = {'all'  };
 cfg.preproc.demean = 'yes';
 cfg.viewmode = 'butterfly';
 cfg.ylim = [-200 200];
+<<<<<<< HEAD
+=======
+%cfg.layout = 'J:\Projects\ANT_trial\Export\customANT.lay';     
+>>>>>>> enhancement - added test script for issue #2086
 cfg.channel = 'all';
 cfg = ft_databrowser(cfg,data);
