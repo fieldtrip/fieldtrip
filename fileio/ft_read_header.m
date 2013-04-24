dbb2bd4a8584ef5018111a93f1ac651783758d17
Joseph Dien <jdien07@mac.com>
2013-04-24 20:15:41 +0000

    Added workaround for Matlab bug that resulted in global variables being cleared whenever reading 'egi_mff_v2' file format.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8073 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/ft_read_header.m
+++ fileio/ft_read_header.m
@@ -927,7 +927,30 @@ switch headerformat
     % ensure that the EGI_MFF toolbox is on the path
     ft_hastoolbox('egi_mff', 1);
     % ensure that the JVM is running and the jar file is on the path
+    %%%%%%%%%%%%%%%%%%%%%%
+    %workaround for Matlab bug resulting in global variables being cleared
+    globalTemp=cell(0);
+    globalList=whos('global');
+    varList=whos;
+    for i=1:length(globalList)
+        eval(['global ' globalList(i).name ';']);
+        eval(['globalTemp{end+1}=' globalList(i).name ';']);
+    end;
+    %%%%%%%%%%%%%%%%%%%%%%
+    
     mff_setup;
+    
+    %%%%%%%%%%%%%%%%%%%%%%
+    %workaround for Matlab bug resulting in global variables being cleared
+    varNames={varList.name};
+    for i=1:length(globalList)
+        eval([globalList(i).name '=globalTemp{i};']);
+        if ~any(strcmp(globalList(i).name,varNames)) %was global variable originally out of scope?
+            eval(['clear ' globalList(i).name ';']); %clears link to global variable without affecting it
+        end;
+    end;
+    clear globalTemp globalList varNames varList;
+    %%%%%%%%%%%%%%%%%%%%%%
 
     if isunix && filename(1)~=filesep
       % add the full path to the dataset directory
