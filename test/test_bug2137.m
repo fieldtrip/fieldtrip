c61b62c7b82729c61afbcbb96b2f92d7fe2fedd4
Robert Oostenveld <r.oostenveld@gmail.com>
2013-05-17 15:34:21 +0000

    enhancement - added test script for (resolved) bug report #2137
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8134 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2137.m
@@ -0,0 +1,27 @@
+function test_bug2137
+
+% TEST test_bug2137
+
+% the following lines are for interactive/manual testing
+if false
+  restoredefaultpath
+  clear all
+  addpath /home/common/matlab/fieldtrip
+  ft_defaults
+end
+
+cfg = [];
+cfg.dataset = '/home/common/matlab/fieldtrip/data/Subject01.ds';
+% cfg.dataset = dccnfilename('/home/common/matlab/fieldtrip/data/Subject01.ds');
+data = ft_preprocessing(cfg);
+
+cfg            = [];
+cfg.detrend    = 'no';
+cfg.resamplefs = 150;
+data           = ft_resampledata(cfg, data);
+
+cfg            = [];
+cfg.channel    = {'MEG'};
+cfg.numcomponent = 60;
+comp           = ft_componentanalysis(cfg, data);
+
