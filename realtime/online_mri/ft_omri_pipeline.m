e7063d13cb302d230eecd5663a6a1e163a8388d0
Robert Oostenveld <r.oostenveld@gmail.com>
2013-05-23 13:24:12 +0000

    enhancement - updated the online mri documentation, provide full path in startup script for monitor_quality
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8143 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- realtime/online_mri/ft_omri_pipeline.m
+++ realtime/online_mri/ft_omri_pipeline.m
@@ -2,13 +2,17 @@ function ft_omri_pipeline(cfg)
 
 % FT_OMRI_PIPELINE implements an online fMRI pre-processing pipeline
 %
+% Use as
+%   ft_omri_pipeline(cfg)
+% where cfg is a structure with configuration settings.
+%
 % Configuration options are
-%   cfg.input            - FieldTrip buffer containing raw scans (default 'buffer://localhost:1972')
-%   cfg.output           - where to write processed scans to     (default 'buffer://localhost:1973')
-%   cfg.numDummy         - how many scans to ignore initially    (default 0)
-%   cfg.smoothFWHM       - kernel width in mm (Full Width Half Maximum) for smoothing (default = 0 => no smoothing)
-%   cfg.correctMotion 	 - flag indicating whether to correct motion artifacts (default = 1 = yes)
-%   cfg.correctSliceTime - flag indicating whether to correct slice timing (default = 1 = yes)
+%   cfg.input            = FieldTrip buffer containing raw scans (default 'buffer://localhost:1972')
+%   cfg.output           = where to write processed scans to     (default 'buffer://localhost:1973')
+%   cfg.numDummy         = how many scans to ignore initially    (default 0)
+%   cfg.smoothFWHM       = kernel width in mm (Full Width Half Maximum) for smoothing (default = 0 => no smoothing)
+%   cfg.correctMotion 	 = flag indicating whether to correct motion artifacts (default = 1 = yes)
+%   cfg.correctSliceTime = flag indicating whether to correct slice timing (default = 1 = yes)
 
 % Copyright (C) 2010, Stefan Klanke
 %
