9bf0442e1da0b4eeb9e5b7f0603e9f9d14c617bb
Jorn Horschig <jm.horschig@donders.ru.nl>
2013-05-23 14:15:58 +0000

    added testcript for warning_once
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8146 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_warning_once.m
@@ -0,0 +1,55 @@
+function test_warning_once
+
+warning1 = 'hululu';
+warning2 = 'aloah hey';
+for i=1:2
+  [output] = evalc(['warning_caller(warning1, warning2)']);
+  w1size = strfind(output, warning1);
+  w2size = strfind(output, warning2);
+  if numel(w1size)~=2 ||  numel(w2size)~=2
+    error('too few warnings thrown at iteration %d', i);
+  end
+  warning_once('-clear');
+end
+
+[output] = evalc(['warning_caller(warning1, warning2)']);
+w1size = strfind(output, warning1);
+w2size = strfind(output, warning2);
+if numel(w1size)~=2 ||  numel(w2size)~=2
+  error('too few warnings thrown at iteration %d', i);
+end
+% no clearing to verify whether these warnings stay!
+
+% check some ft_ functions, therefore get dummy data
+datainfo = ref_datasets;
+dataset = datainfo(1);
+load(fullfile(dataset.origdir,'latest','raw',dataset.type,['preproc_',dataset.datatype]));
+
+
+cfg            = [];
+cfg.method     = 'mtmconvol';
+cfg.foi        = 1:.01:2;
+cfg.taper      = 'hanning';
+cfg.t_ftimwin  = ones(1,numel(cfg.foi)).*0.5;
+cfg.toi        = (250:50:750)./1000;
+cfg.polyremoval= 0;
+% one warning should be thrown
+ft_freqanalysis(cfg, data);
+% again!
+ft_freqanalysis(cfg, data);
+end
+
+function warning_caller(warning1, warning2)
+
+for i=1:10
+  warning_once(warning1);
+  warning_once('FieldTrip:TEST', warning2);
+end
+
+% these warnings should be thrown now !again!
+for i=1:10
+  warning_once(warning1);
+  warning_once('FieldTrip:TEST', warning2);
+end
+
+end
