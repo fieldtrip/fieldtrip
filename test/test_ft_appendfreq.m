2fa4ed3f3d99b4abb01bac3505782a3a69743d0b
Diego Lozano Soldevilla <d.lozanosoldevilla@donders.ru.nl>
2013-06-05 13:21:57 +0000

    bugfix 1984 2187 - adding test data and test function
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8188 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_ft_appendfreq.m
+++ test/test_ft_appendfreq.m
@@ -30,21 +30,38 @@ freq2         = freq1;
 freq2.freq    = 11:20;
 cfg.appenddim = 'freq';
 freqfreq      = ft_appendfreq(cfg, freq1, freq2);
+if ~isfield(freqfreq,'freq');
+  error('freq field is not appeneded: see bugs 1984 and 2187');
+end
+
 cfg.appenddim = 'auto';
 freqfreqauto  = ft_appendfreq(cfg, freq1, freq2);
+if ~isfield(freqfreqauto,'freq');
+  error('freq field is not appeneded: see bugs 1984 and 2187');
+end
 
 freq2         = freq1;
 freq2.time    = 6:10;
 cfg.appenddim = 'time';
 freqtime      = ft_appendfreq(cfg, freq1, freq2);
+if ~isfield(freqfreqauto,'freq');
+  error('freq field is not appeneded: see bugs 1984 and 2187');
+end
+
 cfg.appenddim = 'auto';
 freqtimeauto  = ft_appendfreq(cfg, freq1, freq2);
+if ~isfield(freqtimeauto,'freq');
+  error('freq field is not appeneded: see bugs 1984 and 2187');
+end
 
 % now test for numerical inaccurracies, should concatenate across 'rpt'
 freq2          = freq1;
 freq2.time     = freq1.time+0.0000001;
 cfg.appenddim  = 'auto';
 freqrpt2       = ft_appendfreq(cfg, freq1, freq2);
+if ~isfield(freqrpt2,'freq');
+  error('freq field is not appeneded: see bugs 1984 and 2187');
+end
 
 %% test for data with labels shuffled around
 freq1 = [];
@@ -62,6 +79,9 @@ freqshuffled = ft_appendfreq(cfg, freq1, freq2);
 
 if ~strcmp(freqshuffled.dimord, 'rpt_chan_freq_time')
   error('unexpected dimord when appending freqs with differently permuted labels');
+end
+if ~isfield(freqshuffled,'freq');
+  error('freq field is not appeneded: see bugs 1984 and 2187');
 end
 
 % now check whether channels were correctly appended
