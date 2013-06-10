265513427624251da02bfe872c203b641ba49574
Robert Oostenveld <r.oostenveld@gmail.com>
2013-06-10 09:32:44 +0000

    enhancement - more input data for testing
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8224 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug2193.m
+++ test/test_bug2193.m
@@ -26,3 +26,22 @@ assert(max(aal.tissue(:))==length(aal.tissuelabel), 'inconsistent number of tiss
 cfg = [];
 cfg.atlas = filename;
 bbl = ft_prepare_atlas(cfg);
+
+
+% The following section depends on the current (10 June 2013) situation with spm8
+% on home/common, which will probably not persist forever. Therefore this section
+% should not be included in the automatic regression test.
+
+if false
+  cfg.atlas = '/home/common/matlab/spm8/toolbox/wfu_pickatlas_3.03-old2/MNI_atlas_templates/aal_MNI_V4.nii'
+  aal1a = ft_prepare_atlas(cfg);
+  aal1b = ft_read_atlas(cfg.atlas);
+  
+  cfg.atlas = '/home/common/matlab/spm8/toolbox/wfu_pickatlas/MNI_atlas_templates/aal_MNI_V4.nii'
+  aal2a = ft_prepare_atlas(cfg);
+  aal2b = ft_read_atlas(cfg.atlas);
+  
+  cfg.atlas = '/home/common/matlab/spm8/toolbox/wfu_pickatlas-old/MNI_atlas_templates/aal_MNI_V4.img'
+  aal3a = ft_prepare_atlas(cfg);
+  aal3b = ft_read_atlas(cfg.atlas);
+end
