4599bbc156562f4b1d7fc3493f58c869a06353d4
Robert Oostenveld <r.oostenveld@gmail.com>
2013-06-10 09:22:57 +0000

    enhancement - extended test script with wfu and original aal version
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8223 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug2193.m
+++ test/test_bug2193.m
@@ -3,10 +3,23 @@ function test_bug2193
 % TEST test_bug2193
 % TEST ft_read_atlas
 
-filename = dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2193/aal_MNI_V4.nii');
+%%%%%% from http://fmri.wfubmc.edu
+
+filename = dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2193/wfu/aal_MNI_V4.nii');
 
 aal = ft_read_atlas(filename);
+assert(all(~cellfun(@isempty, aal.tissuelabel)), 'there is an empty tissuelabel');
+assert(max(aal.tissue(:))==length(aal.tissuelabel), 'inconsistent number of tissues');
 
+cfg = [];
+cfg.atlas = filename;
+bbl = ft_prepare_atlas(cfg);
+
+
+%%%%%% from http://www.cyceron.fr
+filename = dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2193/aal/ROI_MNI_V4.nii');
+
+aal = ft_read_atlas(filename);
 assert(all(~cellfun(@isempty, aal.tissuelabel)), 'there is an empty tissuelabel');
 assert(max(aal.tissue(:))==length(aal.tissuelabel), 'inconsistent number of tissues');
 
