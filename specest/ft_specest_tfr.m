dd1cb17de6cf54efe855b608ec6f4858f1778443
Jorn Horschig <jm.horschig@donders.ru.nl>
2013-06-12 15:16:47 +0000

    warnings are thrown only once, hehe
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8254 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- specest/ft_specest_tfr.m
+++ specest/ft_specest_tfr.m
@@ -92,13 +92,13 @@ if isnumeric(freqoiinput)
   % check whether padding is appropriate for the requested frequency resolution
   rayl = 1/endtime;
   if any(rem(freqoiinput,rayl)) % not always the case when they mismatch
-    warning('padding not sufficient for requested frequency resolution, for more information please see the FAQs on www.ru.nl/neuroimaging/fieldtrip')
+    warning_once('padding not sufficient for requested frequency resolution, for more information please see the FAQs on www.ru.nl/neuroimaging/fieldtrip')
   end
   if numel(freqoiinput) ~= numel(freqoi) % freqoi will not contain double frequency bins when requested
-    warning('output frequencies are different from input frequencies, multiples of the same bin were requested but not given');
+    warning_once('output frequencies are different from input frequencies, multiples of the same bin were requested but not given');
   else
     if any(abs(freqoiinput-freqoi) >= eps*1e6)
-      warning('output frequencies are different from input frequencies');
+      warning_once('output frequencies are different from input frequencies');
     end
   end
 end
@@ -120,10 +120,10 @@ end
 % throw a warning if input timeoi is different from output timeoi
 if isnumeric(timeoiinput)
   if numel(timeoiinput) ~= numel(timeoi) % timeoi will not contain double time-bins when requested
-    warning('output time-bins are different from input time-bins, multiples of the same bin were requested but not given');
+    warning_once('output time-bins are different from input time-bins, multiples of the same bin were requested but not given');
   else
     if any(abs(timeoiinput-timeoi) >= eps*1e6) 
-      warning('output time-bins are different from input time-bins');
+      warning_once('output time-bins are different from input time-bins');
     end
   end
 end
