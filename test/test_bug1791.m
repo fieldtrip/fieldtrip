840de90f9f9529e42db53e1800aab539be13a91c
Jim Herring <j.herring@donders.ru.nl>
2013-06-18 14:12:03 +0000

    bugfix-made some changes to ft_removetmsartifact based on user feedback, see bug1791
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8275 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1791.m
+++ test/test_bug1791.m
@@ -6,6 +6,8 @@ function test_bug1791
 load /home/common/matlab/fieldtrip/data/test/bug1791/data.mat;
 
 cfg = [];
+cfg.pulsewidth = 0.010;
+cfg.offset = -0.005;
 cfg.method = 'twopassfilter';
 cfg.latency = 0.008/2;
 data1 = ft_removetmsartifact(cfg, data);
@@ -13,12 +15,14 @@ data1 = ft_removetmsartifact(cfg, data);
 cfg = [];
 cfg.method = 'interpolatepulse';
 cfg.latency = 0.008/2;
-cfg.width = 0.008;
-data2 = ft_removetmsartifact(cfg, data1);
+cfg.pulsewidth = 0.010;
+cfg.offset = -0.005;
+data2 = ft_removetmsartifact(cfg, data);
 
 figure 
 hold on
 plot(data.time{2},  data.trial{2}(1,:),  'b')
 plot(data1.time{2}, data1.trial{2}(1,:), 'r')
 plot(data2.time{2}, data2.trial{2}(1,:), 'g')
+legend({'original';'twopass';'interpolate'});
 
