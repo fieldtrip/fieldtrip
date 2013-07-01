bc4659819b92ac97142f94767330782c5c815a6d
Robert Oostenveld <r.oostenveld@gmail.com>
2013-07-01 09:38:27 +0000

    restructuring - renamed a GUI test_xxx script into inspect_xxx, as it is not supposed to run automatically
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8292 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/inspect_bug1474.m
+++ test/inspect_bug1474.m
@@ -1,15 +1,13 @@
-function test_bug1474
+function inspect_bug1474
 
-% TEST test_bug1474
+% TEST inspect_bug1474
 % TEST ft_rejectvisual
 
 % this script tests ft_rejectvisual with cfg.method='summary' for the case
 % where the data only contains one channel
 
 % since ft_rejectvisual requires user interaction with the GUI, we should
-% just return here, to avoid false negatives in the testing suite.
-% The rest of the code is only for reference.
-return;
+% not run this automatically
 
 cd(dccnfilename('/home/common/matlab/fieldtrip/data/test'))
 load bug1474.mat
