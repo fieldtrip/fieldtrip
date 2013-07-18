bd4f2ec6703a9c420cf1befac18aedb887879f3d
Todor Jordanov <tjordanov@besa.de>
2013-07-18 18:59:59 +0000

    New function for reading GFP and RV was added and a few modifications for better stability were made.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8325 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- external/besa/readBESAavr.m
+++ external/besa/readBESAavr.m
@@ -4,16 +4,30 @@ function avr = ReadBESAavr(filename)
 
 fp = fopen(filename,'r');
 if (fp)
-    % Read header of .avr file
+    % Read header of .avr file.
     avr.Npts = fscanf(fp,'Npts= %i');
     avr.TSB = fscanf(fp,' TSB= %f');    
     avr.DI = fscanf(fp,'DI= %f');
     avr.Time = [avr.TSB:avr.DI:avr.TSB+avr.DI*(avr.Npts-1)];
+    
+    % Read optional information.
+    try
     temp = fscanf(fp,' SB= %f SC= %f');
-    try 
-        avr.Nchan = fscanf(fp,'Nchan= %i\n'); 
     catch
     end
+    
+    try 
+        avr.Nchan = fscanf(fp,'Nchan= %i'); 
+    catch
+    end
+    
+    try 
+        avr.SegmentName = fscanf(fp,'SegmentName= %s\n'); 
+    catch
+    end
+
+    % a line added by TJ: 20.06.2011
+    tline = fgetl(fp);
 
     if avr.Nchan >0
         avr.ChannelLabels = fgetl(fp);
