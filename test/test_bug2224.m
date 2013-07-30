d81674fbcde64f027ac03f88d7dac2b76c39332c
Robert Oostenveld <r.oostenveld@gmail.com>
2013-07-30 13:50:22 +0000

    enhancement - added (failing) test script for #2224
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8352 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2224.m
@@ -0,0 +1,27 @@
+function test_bug2224
+
+% TEST test_bug2224
+% TEST ft_selectdata ft_selectdata_new ft_postamble ft_postamble_previous ft_postamble_history
+
+freq1 = [];
+freq1.dimord = 'chan_freq';
+freq1.freq = 1:10;
+freq1.label = {'1', '2', '3'};
+freq1.powspctrm = randn(3,10);
+freq1.powspctrm(1) = 1;
+freq1.cfg.v = 1;
+
+freq2 = [];
+freq2.dimord = 'chan_freq';
+freq2.freq = 1:10;
+freq2.label = {'1', '2', '3'};
+freq2.powspctrm = randn(3,10);
+freq2.powspctrm(1) = 2;
+freq2.cfg.v = 2;
+
+[f1, f2] = ft_selectdata([], freq1, freq2)
+
+assert(isfield(f1.cfg, 'previous'), 'one failed');
+assert(isfield(f2.cfg, 'previous'), 'two failed');
+assert(numel(f1.cfg.previous)==1,   'three failed');
+assert(numel(f2.cfg.previous)==1,   'four failed');
