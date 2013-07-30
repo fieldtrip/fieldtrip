d29f50281f0e6ae29ef7567d0e735014c48c0e80
Todor Jordanov <tjordanov@besa.de>
2013-07-30 10:40:56 +0000

    The reader was changed in order to work more reliable. The function fscanf was replaced by fgetl and sscanf.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8347 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- external/besa/readBESAavr.m
+++ external/besa/readBESAavr.m
@@ -1,41 +1,100 @@
 function avr = readBESAavr(filename)
+% READBESAAVR reads sensor level data from an AVR-file. 
+%
+% Parameters:
+%     [filename]
+%         In the case that the current folder is not the folder containing 
+%         the file it should be the full path including the name of the 
+%         elp file else only the name of the file should be specified. 
+% 
+% Return:
+%     [avr] 
+%         A Matlab structure containing the data and the corresponding
+%         parameters stored in the AVR-file.
+% 
+% Copyright (C) 2013, BESA GmbH
+%
+% File name: readBESAavr.m
+%
+% Author: Todor Jordanov
+% Created: 2013-07-30
 
-% Reads BESA *.avr files
+fp = fopen(filename, 'r');
 
-fp = fopen(filename,'r');
 if (fp)
-    % Read header of .avr file.
-    avr.Npts = fscanf(fp,'Npts= %i');
-    avr.TSB = fscanf(fp,' TSB= %f');    
-    avr.DI = fscanf(fp,'DI= %f');
-    avr.Time = [avr.TSB:avr.DI:avr.TSB+avr.DI*(avr.Npts-1)];
     
-    % Read optional information.
-    try
-    temp = fscanf(fp,' SB= %f SC= %f');
-    catch
-    end
-    
-    try 
-        avr.Nchan = fscanf(fp,'Nchan= %i'); 
-    catch
+    % Get the first line of the file. It looks something like that:
+    % Npts= 512   TSB= -400.000 DI= 3.125000 SB= 1.000 SC= 200.0 ...
+    FirstLine = fgetl(fp);
+    % First of all check if the parameter SegmentName exists.
+    if(~isempty(strfind(FirstLine, 'SegmentName')))
+        
+        tmp = regexp(FirstLine, 'SegmentName= ', 'split');
+        avr.SegmentName = tmp{2};
+        FirstLine = tmp{1};
+        
     end
     
-    try 
-        avr.SegmentName = fscanf(fp,'SegmentName= %s\n'); 
-    catch
+    % Parse header information of .avr file.
+    [DataParams] = sscanf(FirstLine, ...
+        'Npts= %i TSB= %f DI= %f SB= %f SC= %f Nchan= %i');
+    avr.Npts = DataParams(1);
+    avr.TSB = DataParams(2);    
+    avr.DI = DataParams(3);
+    avr.Time = avr.TSB:avr.DI:avr.TSB+avr.DI*(avr.Npts-1);
+    
+    if(size(DataParams, 2) > 5)
+        
+        avr.Nchan = DataParams(6);
+        
     end
-
-    if avr.Nchan >0
-        avr.ChannelLabels = fgetl(fp);
-    end
-
-    for i=1:1000
-        try avr.Data(i,:)=fscanf(fp,'%f',[avr.Npts,1]);
-        catch
-            break
+    
+    avr.ChannelLabels = [];
+    
+    % The second line could contain the channel labels but it also could be
+    % that it contains data.
+    SecondLine = fgetl(fp);
+    
+    % Check if the second line contains labels or data values
+    Characters = {'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' ...
+        'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z'};
+    NumChars = size(Characters, 2);
+    iCounter = 1;
+    for ch = 1:NumChars
+        
+        % If the second line contains characters then these are the channel
+        % labels.
+        if(~isempty(regexpi(SecondLine, Characters{ch}, 'match')))
+            
+            avr.ChannelLabels = SecondLine;
+            break;
+        
+        else
+            
+            avr.Data(iCounter, :) = sscanf(SecondLine, '%f', [avr.Npts,1]);
+            iCounter = 2;
+            
         end
+        
     end
-    avr.Nchan=size(avr.Data,1);
+
+    while(true)
+        
+        CurrentLine = fgetl(fp);
+        % Check if end of file.
+        if(~ischar(CurrentLine))
+            
+            break;
+            
+        end
+        
+        avr.Data(iCounter, :) = sscanf(CurrentLine, '%f', [avr.Npts,1]);
+        iCounter = iCounter + 1;
+        
+    end
+    
+    avr.Nchan=size(avr.Data, 1);
+    
 end
+
 fclose(fp);
