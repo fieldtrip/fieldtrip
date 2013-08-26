d6caa5ccf49aae760d3e59f6e9535c3b33bfd16d
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2013-08-26 08:15:14 +0000

    bugfix - removed unnecessary trailing occurrences of profile calls, causing deployed application to crash, consulted with Eelke who introduced those lines
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8425 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_sourceplot.m
+++ ft_sourceplot.m
@@ -329,7 +329,7 @@ if hasatlas
     % initialize the atlas
     [p, f, x] = fileparts(cfg.atlas);
     fprintf(['reading ', f,' atlas coordinates and labels\n']);
-    atlas = ft_prepare_atlas(cfg);
+    atlas = ft_read_atlas(cfg.atlas);
   else
     atlas = cfg.atlas;
   end
@@ -1262,8 +1262,6 @@ axis xy
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function cb_redraw(h, eventdata)
 
-profile resume;
-
 h   = getparent(h);
 opt = getappdata(h, 'opt');
 
@@ -1504,7 +1502,6 @@ set(h, 'currentaxes', curr_ax);
 
 uiresume
 
-profile off;
 
 function cb_keyboard(h, eventdata)
 
