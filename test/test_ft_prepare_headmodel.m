6f03ce650186f448ddfba953c22663db1a023925
Lilla Magyari <l.magyari@donders.ru.nl>
2013-09-10 15:17:52 +0000

    cleaning up test directory
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8458 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_ft_prepare_headmodel.m
+++ test/test_ft_prepare_headmodel.m
@@ -1,185 +1,466 @@
 function test_ft_prepare_headmodel
 
-return;
-
 % TEST test_ft_prepare_headmodel
+% TEST ft_headmodel_localspheres ft_prepare_localspheres
 
-
-% input: mesh - 1, 3 or 5 compartment
-%        headshape
-%        hdmfile
-%        elec
-%        nothing
-
-% method: singlesphere       
-%   concentricspheres  
-%   openmeeg           
-%   bemcp              
-%   dipoli             
-%   asa                
-%   simbio             
-%   fns                
-%   infinite           
-%   halfspace          
+% function to test ft_prepare_headmodel.
+% This function allows for different types of structure information to be
+% given, via 1) bnd, 2) seg, 3) elec, or 4) vol
+% Then each of these must be dealt with appropriately for the different
+% cfg.method options.
 %
-%   singlesphere       
-%   localspheres       
-%   singleshell        
-%   infinite       
+% (initial version by Johanna Zumer 2012)
 
-
-%%%%%%%%%%%%%%%%%%%%
-%% input: nothing %%
-%%%%%%%%%%%%%%%%%%%%
-
-% methods for which it is applicable: infinite, ? halfspace and
-% singlesphere
-
-cfg = [];
-cfg.method = 'infinite';
-vol = ft_prepare_headmodel(cfg);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%
-%% input: segmentation %%
-%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% methods for which it is applicable: singlesphere and singleshell,?
-% localspheres (see test_ft_prepare_localspheres)?
-
-% example segmentation 
-example.dim = [91 104 111];   % slightly different numbers
-example.transform = eye(4);   
-example.coordsys = 'ctf';
-example.unit = 'mm';
-example.seg = zeros(example.dim);
-
-% adjusting transformation matrix: center of the head-coordinates [0 0 0] should
-% be the center of volume
-
-% center of volume in voxel coordinates     
-x = round(example.dim(1)/2);
-y = round(example.dim(2)/2);
-z = round(example.dim(3)/2);
-    
-x = round(x);
-y = round(y);
-z = round(z);      
-             
-origin = [x y z];
-
-example.transform(1:4,4)   = [-origin(:); 1];  % head-coordinate [0 0 0] is in the center of 
-                                                % the volume (x y z in voxel-coordinates)
-
-% compute position for each voxel in voxelspace and in headspace
-
-[X, Y, Z] = ndgrid(1:example.dim(1), 1:example.dim(2), 1:example.dim(3));
-voxelpos = [X(:) Y(:) Z(:)];
-headpos = warp_apply(example.transform, voxelpos);
-
-% create a 3 spheres and a 1 sphere example
-
-example1 = example;
-radius1 = 40;
-radius2 = 30;
-radius3 = 20;
-
-for i=1:size(headpos,1)
-    % from small to large
-    if norm(headpos(i,:))<radius3
-        example.seg(i) = 3;
-        example1.seg(i) = 1;
-    elseif norm(headpos(i,:))<radius2
-        example.seg(i) = 2;
-    elseif norm(headpos(i,:))<radius1
-        example.seg(i) = 1;
-    end
-end
-%%
-cfg=[];
-cfg.method = 'singlesphere';
-vol = ft_prepare_headmodel(cfg,example1);
-cfg.method = 'singleshell';
-vol = ft_prepare_headmodel(cfg,example1);
-
-%%%%%%%%%%%%%%%%%
-%% input: mesh %%
-%%%%%%%%%%%%%%%%%
-
-cfg=[];
-cfg.numvertices = 1000;
-mesh = ft_prepare_mesh(cfg,example1);
-cfg.numvertices = [3000 2000 1000];
-bnd = ft_prepare_mesh(cfg,example);
-%%
-
-cfg=[];
-cfg.method = 'singlesphere';
-vol = ft_prepare_headmodel(cfg,mesh);
-cfg.method = 'singleshell';
-vol = ft_prepare_headmodel(cfg,mesh);
-cfg=[];
-cfg.method = 'concentricspheres';
-vol = ft_prepare_headmodel(cfg,bnd); 
-cfg=[];
-cfg.method = 'openmeeg';
-vol = ft_prepare_headmodel(cfg,bnd);
-cfg=[];
-cfg.method = 'bemcp';
-vol = ft_prepare_headmodel(cfg,bnd);
-cfg=[];
-cfg.method = 'dipoli';
-vol = ft_prepare_headmodel(cfg,bnd);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% get the data which is needed
 
 % read in the gradiometer information
 hdr  = ft_read_header('/home/common/matlab/fieldtrip/data/Subject01.ds');
 grad = hdr.grad;
 
+% read in the mri
+load standard_mri
+
 % read in the segmented mri
 load('/home/common/matlab/fieldtrip/data/ftp/tutorial/beamformer/segmentedmri.mat');
-mri = segmentedmri; clear segmentedmri;
+
+% specify the file for the headshape
+shapefile  = '/home/common/matlab/fieldtrip/data/Subject01.shape';
+
+% read in the headshape
+shape = ft_read_headshape(shapefile);
+shapevol.bnd=shape;
+
+% hdmfile
+hdmfile  = '/home/common/matlab/fieldtrip/data/Subject01.hdm';
+
+
+%   vol = ft_prepare_headmodel(cfg)       or
+%   vol = ft_prepare_headmodel(cfg, bnd)  with the output of FT_PREPARE_MESH
+%   vol = ft_prepare_headmodel(cfg, seg)  with the output of FT_VOLUMESEGMENT
+%   vol = ft_prepare_headmodel(cfg, elec) with the output of FT_READ_SENS
+%   vol = ft_prepare_headmodel(cfg, vol)  with the output of FT_READ_VOL
+
+csvol.o = [0,0,0];
+csvol.r = [10 50 60];
+assert(ft_voltype(csvol,'concentricspheres'))
+cfg = [];
+cfg.numvertices = 1000;
+csbnd = ft_prepare_mesh(cfg,csvol);
+
+ssvol.o = [0,0,0];
+ssvol.r = 60;
+assert(ft_voltype(ssvol,'singlesphere'))
+cfg = [];
+cfg.numvertices = 1000;
+ssbnd = ft_prepare_mesh(cfg,ssvol);
+
+cs4vol.o = [0,0,0];
+cs4vol.r = [10 30 50 60];
+assert(ft_voltype(cs4vol,'concentricspheres'))
+cfg = [];
+cfg.numvertices = 1000;
+cs4bnd = ft_prepare_mesh(cfg,cs4vol);
+
+
+load /home/common/matlab/fieldtrip/data/test/bug1646/seg2  % brain skull scalp (3 binary masks)
+load /home/common/matlab/fieldtrip/data/test/bug1646/seg5  % seg (brain, skull & scalp indexed)
+
+elcfile ='/home/common/matlab/fieldtrip/data/test/original/electrodes/asa/standard_primed.elc';
+elec = ft_convert_units(ft_read_sens(elcfile));
+
+
+%%%%%%%%%%%%%%%%%%%%%
+% do the computations
+
+%% random/wrong cfg.method
+try
+  cfg=[];
+  cfg.method='random';
+  vol111=ft_prepare_headmodel(cfg,segmentedmri);
+  success=true;
+catch
+  success=false;
+end
+if success
+  error('this should fail with cfg.method nonsense');
+end
+
+%% asa  - INPUT asa *.vol file
 
 cfg=[];
-cfg.numvertices = 1000;
-mesh2 = ft_prepare_mesh(cfg,mri);
+cfg.hdmfile='/home/common/matlab/fieldtrip/data/test/original/headmodel/asa/standard.vol';
+cfg.method='asa';
+vol11=ft_prepare_headmodel(cfg);
+
+cfg=[];
+cfg.method='asa';
+cfg.hdmfile=hdmfile;
+try % we need the asa .vol file
+  vol11=ft_prepare_headmodel(cfg);
+  success = true;
+catch
+  success=false;
+end
+if success, error('11');end
+
+
+%% bemcp, dipoli, openmeeg  - INPUT: segmentation or mesh (3 or also 1 compartment)
+
+method={'bemcp','dipoli','openmeeg'};
+for ii=1:numel(method)
+  cfg=[];
+  cfg.method=method{ii};
+  
+  % wrong input: nothing
+  try 
+    vol21=ft_prepare_headmodel(cfg);
+    success = true;
+  catch
+    success=false;
+  end
+  if success, error('21');end
+  % wrong input: headshape
+  try
+    vol22=ft_prepare_headmodel(cfg,shape);
+    success = true;
+  catch
+    success=false;
+  end
+  if success, error('22');end
+  
+  % wrong input: headshape
+  try
+    vol23=ft_prepare_headmodel(cfg,shapevol);
+    success = true;
+  catch
+    success=false;
+  end
+  if success, error('23');end
+  
+  vol24=ft_prepare_headmodel(cfg,csbnd);  % 3 compartment MESH: should work with all 3 methods
+  switch method{ii}
+    case 'bemcp' % this needs a 3 compartment mesh or three layered segmentation
+      try % wrong input: single mesh
+        vol25=ft_prepare_headmodel(cfg,ssbnd);
+        success = true;
+      catch
+        success=false;
+      end
+      if success, error('25');end
+      try % wrong input: 4 compartments mesh
+        vol26=ft_prepare_headmodel(cfg,cs4bnd);
+        success = true;
+      catch
+        success=false;
+      end
+      if success, error('26');end
+      try % wrong input: 1 layered segmentation
+        vol27=ft_prepare_headmodel(cfg,segmentedmri);
+        success = true;
+      catch
+        success=false;
+      end
+      % segmentedmari has white/gray/csf which becomes brain, but only 1 shell.
+      if success, error('27');end
+    case 'dipoli' % it should work also with single or 4 layers
+      vol25=ft_prepare_headmodel(cfg,ssbnd);
+      vol26=ft_prepare_headmodel(cfg,cs4bnd);
+      vol27=ft_prepare_headmodel(cfg,segmentedmri);
+    case 'openmeeg' % it should work also with single compartment
+      vol25=ft_prepare_headmodel(cfg,ssbnd);
+      try % but not with 4 
+        vol26=ft_prepare_headmodel(cfg,cs4bnd);
+        success = true;
+      catch
+        success=false;
+      end
+      if success, error('26');end
+      vol27=ft_prepare_headmodel(cfg,segmentedmri);
+  end
+  
+  vol28=ft_prepare_headmodel(cfg,seg2);   % 3 layered segmentation
+  vol29=ft_prepare_headmodel(cfg,seg5);   % 3 layered segmentation (indexed style)  
+  
+  
+end
+
+
+%% concentricspheres - INPUT: 1,3,4 compartment mesh, 1,3 layered segmentation, headshape
+
+cfg=[];
+cfg.method='concentricspheres';
+try % wrong input: doesn't work for hdm files
+  cfg.hdmfile=hdmfile;
+  vol31=ft_prepare_headmodel(cfg);
+  success=true;
+catch
+  success=false;
+end
+if success, error('31');end
+% it should work with everything else
+% headshape
+cfg=[];
+cfg.method='concentricspheres';
+cfg.headshape=shape;
+vol32=ft_prepare_headmodel(cfg);
+
+% headshape
+cfg=[];
+cfg.method='concentricspheres';
+cfg.headshape=shape.pnt;
+vol33=ft_prepare_headmodel(cfg);
+
+% headshape
+cfg=[];
+cfg.method='concentricspheres';
+cfg.headshape=shapefile;
+vol34=ft_prepare_headmodel(cfg);
+
+% 3 comp mesh
+cfg=[];
+cfg.method='concentricspheres';
+% cfg.conductivity=[.33];
+vol35=ft_prepare_headmodel(cfg,csbnd);
+
+% 1 comp mesh
+vol36=ft_prepare_headmodel(cfg,ssbnd);
+% FIXME: should we allow for this unusual but error-free way of calling
+% concentricspheres?  only supplying 1 bnd and 1 conducitivity value,
+% thus output is a 1-shell vol.
+
+% 4 comp mesh
+vol37=ft_prepare_headmodel(cfg,cs4bnd);
+
+% 3 comp segmentation
+vol38=ft_prepare_headmodel(cfg,seg2);
+vol39=ft_prepare_headmodel(cfg,seg5);
+
+% 1 comp segmentation
+vol310=ft_prepare_headmodel(cfg,segmentedmri);
+
+% grad
+vol311=ft_prepare_headmodel(cfg,grad);
+
+% elec
+vol312=ft_prepare_headmodel(cfg,elec);
+
+
+
+%% halfspace
+
+cfg=[];
+cfg.method='halfspace';
+% create pnt of x,y points with z on same plane (with noise)
+pnt=randn(100,3);
+pnt(:,1:2)=pnt(:,1:2)*10;
+plane.pnt=pnt;
+try
+  vol41=ft_prepare_headmodel(cfg,plane);
+  success=true;
+catch
+  success=false;
+end
+if success, error('41');end
+cfg.point=[0 0 -10];
+vol42=ft_prepare_headmodel(cfg,plane);
+
+
+
+%% infinite
+
+% takes no input arguments
+% ft_headmodel_infinite();
+cfg=[];
+cfg.method='infinite';
+vol51=ft_prepare_headmodel(cfg);
+
+%% localspheres
+cfg         = [];
+cfg.method  = 'localspheres';
+try
+  vol61        = ft_prepare_headmodel(cfg,shape);
+  success = true;
+catch
+  success=false;
+end
+if success, error('61: grad is required for localspheres');end
 
 cfg         = [];
 cfg.method  = 'localspheres';
 cfg.grad    = grad;
-vol        = ft_prepare_headmodel(cfg, mesh2);
+vol62        = ft_prepare_headmodel(cfg,shape);
+vol63        = ft_prepare_headmodel(cfg,ssbnd);
+try
+  vol64        = ft_prepare_headmodel(cfg,csbnd);
+  success = true;
+catch
+  success=false;
+end
+if success, error('64');end
 
-% ? halfspace
-v = mesh.pnt(1,3);
-h = mesh.pnt(:,3)';
-l = find(h==v);
-plain = mesh.pnt(l,:);
-k = find(h~=v);
-point = mesh.pnt(k(1),:);
+vol65        = ft_prepare_headmodel(cfg, segmentedmri);
+try
+  vol66=ft_prepare_headmodel(cfg,elec);
+  success = true;
+catch
+  success=false;
+end
+if success, error('66');end
 
-%?
+try % with an MRI in the input: this should fail, requires segmentation
+  vol66        = ft_prepare_headmodel(cfg, mri);
+  success=true;
+catch
+  success=false;
+end
+if success
+  error('this should fail with unsegmented MRI input');
+end
+vol67=ft_prepare_headmodel(cfg,seg2);
+
+try % this should not work since it has 3 seg values and not specified which to use
+  vol68=ft_prepare_headmodel(cfg,seg5);
+  success = true;
+catch
+  success=false;
+end
+if success, error('68');end
+
+cfg.tissue=3;
+vol69=ft_prepare_headmodel(cfg,seg5);
 
 
-%%%%%%%%%
-%%
-
-% test_ft_prepare_localspheres
-
-hdr  = ft_read_header('/home/common/matlab/fieldtrip/data/Subject01.ds');
-grad = hdr.grad;
-
-
-% specify the file for the headshape
-hdmfile  = '/home/common/matlab/fieldtrip/data/Subject01.shape';
-
-% read in the headshape
-shape = ft_read_headshape(hdmfile);
-
-
+%% singleshell
 
 cfg=[];
-cfg.method = 'halfspace';
-vol3 = ft_prepare_headmodel(cfg,mesh); % doesn't work
+cfg.method = 'singleshell';
+vol71 = ft_prepare_headmodel(cfg,shape);
+vol72 = ft_prepare_headmodel(cfg,ssbnd);
+try
+  vol73=ft_prepare_headmodel(cfg,csbnd);
+  success=1;
+catch
+  success=0;
+end
+if success==1,error('73');end
+
+vol74=ft_prepare_headmodel(cfg,segmentedmri);
+
+try
+  vol75=ft_prepare_headmodel(cfg,elec);
+  success = true;
+catch
+  success=false;
+end
+if success, error('75');end
+
+try % with an MRI in the input: this should fail, requires segmentation
+  vol76        = ft_prepare_headmodel(cfg, mri);
+  success=true;
+catch
+  success=false;
+end
+if success
+  error('76: this should fail with unsegmented MRI input');
+end
+
+vol77=ft_prepare_headmodel(cfg,seg2);
+
+try % this should not work since it has 3 seg values and not specified which to use
+  vol78=ft_prepare_headmodel(cfg,seg5);
+  success = true;
+catch
+  success=false;
+end
+if success, error('78');end
+
+try
+  cfg.elec=elec;
+  vol79=ft_prepare_headmodel(cfg);
+  success = true;
+catch
+  success=false;
+end
+if success, error('79');end
+
+cfg=[];
+cfg.method = 'singleshell';
+cfg.tissue=3;
+vol710=ft_prepare_headmodel(cfg,seg5);
 
 
+%% singlesphere
+
+cfg=[];
+cfg.method='singlesphere';
+try
+  vol81=ft_prepare_headmodel(cfg);
+  success=true;
+catch
+  success=false;
+end
+if success, error('81');end
+
+vol82=ft_prepare_headmodel(cfg,ssbnd);
+vol83 = ft_prepare_headmodel(cfg,shape);
+
+try
+  vol84=ft_prepare_headmodel(cfg,csbnd);
+  success=1;
+catch
+  success=0;
+end
+if success==1,error('84');end
+
+vol85=ft_prepare_headmodel(cfg,segmentedmri);
+vol86=ft_prepare_headmodel(cfg,seg2);
+vol87=ft_prepare_headmodel(cfg,elec);
+
+try
+cfg.elec=elec;
+vol88=ft_prepare_headmodel(cfg);
+  success = true;
+catch
+  success=false;
+end
+if success, error('88');end
+
+try % this should not work since it has 3 seg values and not specified which to use
+  vol89=ft_prepare_headmodel(cfg,seg5);
+  success = true;
+catch
+  success=false;
+end
+if success, error('89');end
+
+cfg=[];
+cfg.method='singlesphere';
+cfg.tissue=3;
+vol810=ft_prepare_headmodel(cfg,seg5);
+
+cfg=[];
+cfg.method='singlesphere';
+cfg.headshape=shapefile;
+vol811=ft_prepare_headmodel(cfg);
+
+cfg=[];
+cfg.method='singlesphere';
+cfg.headshape=shape;
+vol812=ft_prepare_headmodel(cfg);
+
+cfg=[];
+cfg.method='singlesphere';
+cfg.headshape=shape.pnt;
+vol813=ft_prepare_headmodel(cfg);
+
+
+%% simbio, fns
+% test_headmodel_simbio exists
+% test_headmodel_fns exists
+
+%% interpolate
+% test_headmodel_interpolate
 
 
 
