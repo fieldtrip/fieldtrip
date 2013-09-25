24ff747d3d2afe8157360a509b3f77e2baa54c37
Jorn Horschig <jm.horschig@donders.ru.nl>
2013-09-25 07:56:10 +0000

    minor - not cding to another directory
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8532 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1315.m
+++ test/test_bug1315.m
@@ -3,8 +3,7 @@ function test_bug1315
 % TEST test_bug1315
 % TEST ft_checkdata ft_prepare_neighbours ft_megplanar ft_combineplanar
 
-cd(dccnfilename('/home/common/matlab/fieldtrip/data/test'))
-load bug1315.mat
+load(dccnfilename('/home/common/matlab/fieldtrip/data/test/bug1315.mat'))
 
 % neighbours
 cfg = [];
