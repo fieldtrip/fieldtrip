57fee515bf795e9a85af706f6b497b82b66ffc5c
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2013-09-26 07:28:09 +0000

    enhancement - updated code to be able to deprecate ft_prepare_atlas
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8546 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_ft_volumelookup.m
@@ -0,0 +1,41 @@
+function test_ft_volumelookup
+
+% TEST test_ft_volumelookup
+% TEST ft_volumelookup
+% TEST ft_read_atlas
+% TEST atlas_lookup
+
+atlasfilename = dccnfilename('/home/common/matlab/fieldtrip/template/atlas/afni/TTatlas+tlrc.BRIK');
+mrifilename   = dccnfilename('/home/common/matlab/fieldtrip/external/spm8/templates/T1.nii');
+
+mri = ft_read_mri(mrifilename);
+
+cfg       = [];
+cfg.atlas = atlasfilename;
+cfg.inputcoord = 'mni';
+cfg.roi   = {'Precentral Gyrus'};
+mask1     = ft_volumelookup(cfg,mri);
+cfg.roi   = {'Brodmann area 47'};
+mask2     = ft_volumelookup(cfg,mri);
+cfg.roi   = {'Precentral Gyrus';'Brodmann area 47'};
+mask3     = ft_volumelookup(cfg,mri);
+
+assert(isequal(sum(mask1(:)),8214));
+assert(isequal(sum(mask2(:)),2102));
+assert(isequal(sum(mask3(:)&(mask2(:)|mask1(:))),sum(mask3(:))));
+
+
+% the following is just to check whether the functionality does not crash
+% no idea who would use it anyway
+cfg       = [];
+cfg.roi   = [15 30 40;-40 -20 80];
+cfg.sphere = [10 8];
+mask4     = ft_volumelookup(cfg,mri);
+
+mri.mask  = mask4;
+
+cfg       = [];
+cfg.atlas = atlasfilename;
+cfg.inputcoord = 'mni';
+cfg.maskparameter = 'mask';
+mask5     = ft_volumelookup(cfg, mri);
