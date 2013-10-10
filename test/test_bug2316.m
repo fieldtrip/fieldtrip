bb87ff343449f892a832cb1ec9bf7bca566bfa46
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2013-10-10 09:53:01 +0000

    bugreport - created test-function for bug reproduction, see bug2316
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8598 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2316.m
@@ -0,0 +1,16 @@
+function test_bug2316
+
+% TEST test_bug2316
+% TEST ft_databrowser
+
+% this function currently serves as a placeholder to reproduce bug2316,
+% i.e. it crashes Matlab (2011a, but possibly other versions as well) on
+% a PC running Windows 7. on MacOS it seems fine. 
+
+filename = dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2316.mat');
+load(filename);
+
+cfg = [];
+cfg.layout = 'biosemi128.lay';
+cfg.viewmode = 'component';
+ft_databrowser(cfg, datacomp);
