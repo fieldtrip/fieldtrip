fieldtrip/fieldtrip

commit 175cb2c7213f5985a896bba87b04061b095c5e76

bugfix - #2354 allow for specified parameter different than avg (e.g. individual) to be plotted
git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8685 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

ft_singleplotER.m
@@ -271,6 +271,8 @@ if strcmp(dtype, 'timelock') && hasrpt,
   for i=1:Ndata
     varargin{i} = ft_timelockanalysis(tmpcfg, varargin{i});
   end
+  varargin{i}.(cfg.parameter) = varargin{i}.avg;
+  varargin{i}   = rmfield(varargin{i}, 'avg');
   dimord        = varargin{1}.dimord;
   dimtok        = tokenize(dimord, '_');
 elseif strcmp(dtype, 'freq') && hasrpt,

private/topoplot_common.m
@@ -281,6 +281,8 @@ if strcmp(dtype, 'timelock') && hasrpt,
   tmpcfg        = [];
   tmpcfg.trials = cfg.trials;
   data          = ft_timelockanalysis(tmpcfg, data);
+  data.(cfg.parameter) = data.avg;
+  data          = rmfield(data, 'avg');
   dimord        = data.dimord;
   dimtok        = tokenize(dimord, '_');
 elseif strcmp(dtype, 'freq') && hasrpt,

ft_multiplotER.m
@@ -328,6 +328,10 @@ if strcmp(dtype, 'timelock') && hasrpt,
       tmpmask = varargin{i}.(cfg.maskparameter);
     end
     varargin{i} = ft_timelockanalysis(tmpcfg, varargin{i});
+    % rename avg back into the parameter
+    varargin{i}.(cfg.parameter) = varargin{i}.avg;
+    varargin{i} = rmfield(varargin{i}, 'avg');
+    
     % put back mask
     if ~isempty(cfg.maskparameter)
       varargin{i}.(cfg.maskparameter) = tmpmask;

commit a76ab11238e74cf4dc4bcb53b3757834337fa6c1

bugfix - fixed two more regression errors that were introduced in r8685, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=2354#c5
git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8708 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

ft_singleplotER.m
@@ -271,8 +271,11 @@ if strcmp(dtype, 'timelock') && hasrpt,
   for i=1:Ndata
     varargin{i} = ft_timelockanalysis(tmpcfg, varargin{i});
   end
-  varargin{i}.(cfg.parameter) = varargin{i}.avg;
-  varargin{i}   = rmfield(varargin{i}, 'avg');
+  if ~strcmp(cfg.parameter, 'avg')
+    % rename avg back into the parameter
+    varargin{i}.(cfg.parameter) = varargin{i}.avg;
+    varargin{i}                 = rmfield(varargin{i}, 'avg');
+  end
   dimord        = varargin{1}.dimord;
   dimtok        = tokenize(dimord, '_');
 elseif strcmp(dtype, 'freq') && hasrpt,

private/topoplot_common.m
@@ -281,8 +281,11 @@ if strcmp(dtype, 'timelock') && hasrpt,
   tmpcfg        = [];
   tmpcfg.trials = cfg.trials;
   data          = ft_timelockanalysis(tmpcfg, data);
-  data.(cfg.parameter) = data.avg;
-  data          = rmfield(data, 'avg');
+  if ~strcmp(cfg.parameter, 'avg')
+    % rename avg back into the parameter
+    data.(cfg.parameter) = data.avg;
+    data                 = rmfield(data, 'avg');
+  end
   dimord        = data.dimord;
   dimtok        = tokenize(dimord, '_');
 elseif strcmp(dtype, 'freq') && hasrpt,