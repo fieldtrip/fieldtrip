00be3afe41c711227cee2e740aa7825c225a0d9f
Robert Oostenveld <r.oostenveld@gmail.com>
2013-11-06 13:57:03 +0000

    enhancement - convert a config oinput object into a structure before trying to print it
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8722 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/printstruct.m
+++ utilities/printstruct.m
@@ -46,6 +46,12 @@ if nargin==1
   name = inputname(1);
 end
 
+if isa(val, 'config')
+  % this is fieldtrip specific: the @config object resembles a structure but tracks the
+  % access to each field.  In this case it is to be treated as a normal structure.
+  val = struct(val);
+end
+
 str = '';
 if isstruct(val)
   if numel(val)>1
