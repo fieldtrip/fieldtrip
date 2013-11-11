d16963c38375df1bc7b58e3efb6a98725b2af37e
Robert Oostenveld <r.oostenveld@gmail.com>
2013-11-11 12:53:49 +0000

    documentation - fixed typo
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8756 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/ft_warp_error.m
+++ utilities/ft_warp_error.m
@@ -1,6 +1,6 @@
 function [dist] = ft_warp_error(M, input, target, varargin)
 
-% T_WARP_ERROR computes the mean distance after linear or non-linear warping
+% FT_WARP_ERROR computes the mean distance after linear or non-linear warping
 % and can be used as the goalfunction in a 3D warping minimalisation
 %
 % Use as
