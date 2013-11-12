dfa32e0291a51cc2549df8dca0289aa79dfbe5f9
Robert Oostenveld <r.oostenveld@gmail.com>
2013-11-12 13:55:00 +0000

    automatically synchronized identical files to revision 8770
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8771 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/tokenize.m
+++ utilities/tokenize.m
@@ -9,12 +9,12 @@ function [tok] = tokenize(str, sep, rep)
 % where
 %   str = the string that you want to cut into pieces
 %   sep = the separator at which to cut (default is whitespace)
-%   rep = whether to treat repeating seperator characters as one (default is false)
+%   rep = whether to treat repeating separator characters as one (default is false)
 %
 % With the optional boolean flag "rep" you can specify whether repeated
-% seperator characters should be squeezed together (e.g. multiple
+% separator characters should be squeezed together (e.g. multiple
 % spaces between two words). The default is rep=1, i.e. repeated
-% seperators are treated as one.
+% separators are treated as one.
 %
 % See also STRTOK, TEXTSCAN
 
