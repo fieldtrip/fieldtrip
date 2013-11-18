78afd752b8ad20d8e5c54535ee8e444e270b0e82
Robert Oostenveld <r.oostenveld@gmail.com>
2013-11-18 13:30:00 +0000

    bugfix - the value of mu0 was off by 4*pi
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8809 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/private/magnetic_dipole.m
+++ forward/private/magnetic_dipole.m
@@ -29,14 +29,13 @@ function [lf] = magnetic_dipole(R, pos, ori)
 %
 % $Id$
 
-u0 = 1e-7;
+mu0   = 4*pi*1e-7;
 nchan = size(pos,1);
 
-% ensure that the dipole position is a row vector
-R = reshape(R, [1 3]);
-
-% shift the magnetometers so that the dipole is in the origin
-pos = pos - repmat(R, [nchan 1]);
+% shift the magnetometer coils so that the dipole is in the origin
+pos(:,1) = pos(:,1) - R(1);
+pos(:,2) = pos(:,2) - R(2);
+pos(:,3) = pos(:,3) - R(3);
 
 % change the variable names for convenience
 % R position of magnetometer, relative to dipole
@@ -48,11 +47,11 @@ r = sqrt(sum(R.^2,2));
 % this is the original code, which follows the physical formulation closely
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % magnetic field on all magnetometer positions for an x-oriented dipole
-% Bmx = u0/(4*pi) * (3 * repmat(R(:,1), [1 3]) .* R - repmat([1 0 0], [nchan 1]) .* repmat(r.^2, [1 3])) ./ repmat(r.^5, [1 3]);
+% Bmx = mu0/(4*pi) * (3 * repmat(R(:,1), [1 3]) .* R - repmat([1 0 0], [nchan 1]) .* repmat(r.^2, [1 3])) ./ repmat(r.^5, [1 3]);
 % magnetic field on all magnetometer positions for an y-oriented dipole
-% Bmy = u0/(4*pi) * (3 * repmat(R(:,2), [1 3]) .* R - repmat([0 1 0], [nchan 1]) .* repmat(r.^2, [1 3])) ./ repmat(r.^5, [1 3]);
+% Bmy = mu0/(4*pi) * (3 * repmat(R(:,2), [1 3]) .* R - repmat([0 1 0], [nchan 1]) .* repmat(r.^2, [1 3])) ./ repmat(r.^5, [1 3]);
 % magnetic field on all magnetometer positions for an z-oriented dipole
-% Bmz = u0/(4*pi) * (3 * repmat(R(:,3), [1 3]) .* R - repmat([0 0 1], [nchan 1]) .* repmat(r.^2, [1 3])) ./ repmat(r.^5, [1 3]);
+% Bmz = mu0/(4*pi) * (3 * repmat(R(:,3), [1 3]) .* R - repmat([0 0 1], [nchan 1]) .* repmat(r.^2, [1 3])) ./ repmat(r.^5, [1 3]);
 % compute the field along the orientation of each magnetometer for an x/y/z oriented dipole
 % lf(:,1) = dot(Bmx, ori, 2);
 % lf(:,2) = dot(Bmy, ori, 2);
@@ -64,9 +63,9 @@ r = sqrt(sum(R.^2,2));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 r2 = repmat(r.^2, [1 3]);
 r5 = repmat(r.^5, [1 3]);
-x = R(:,1); x = [x x x];
-y = R(:,2); y = [y y y];
-z = R(:,3); z = [z z z];
+x = R(1); x = [x x x];
+y = R(2); y = [y y y];
+z = R(3); z = [z z z];
 mx = zeros(nchan,3); mx(:,1) = 1;
 my = zeros(nchan,3); my(:,2) = 1;
 mz = zeros(nchan,3); mz(:,3) = 1;
@@ -76,5 +75,5 @@ Tz = (3 * z .* R - mz .* r2);
 lf(:,1) = dot(Tx, ori, 2);
 lf(:,2) = dot(Ty, ori, 2);
 lf(:,3) = dot(Tz, ori, 2);
-lf = u0/(4*pi) * lf ./ r5;
+lf = mu0/(4*pi) * lf ./ r5;
 
