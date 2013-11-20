9688d6e6718f7a3eb0ee7828657d32eb4d338a2a
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2013-11-20 08:05:13 +0000

    added test function to replicate bug 2381
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8821 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2381.m
@@ -0,0 +1,25 @@
+function test_bug2381
+
+%MEM 4gb
+%WALLTIME 00:10:00
+
+%TEST: test_bug2381
+%TEST: ft_sourcestatistics
+
+%filename = dccnfilename(fullfile('/home/common/matlab/fieldtrip/data/test/bug2381','AVG.mat'));
+filename = dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2381');
+load(filename);
+
+nsubj = 15;
+cfg = [];
+cfg.method = 'montecarlo';
+cfg.statistic = 'depsamplesT';
+cfg.parameter = 'pow';
+cfg.correctm  = 'cluster';
+cfg.numrandomization = 10;
+cfg.design(1,:) = [1:nsubj 1:nsubj];
+cfg.design(2,:) = [ones(1,nsubj) 2*ones(1,nsubj)];
+cfg.uvar = 1;
+cfg.ivar = 2;
+%stat = ft_sourcestatistics(cfg, AverageSource1, AverageSource2);
+stat = ft_sourcestatistics(cfg, source1, source2);
