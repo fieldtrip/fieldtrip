3dd517fe523ccdd9ac16cbdb0b108b900bc7b005
Eelke Spaak <eelke.spaak@donders.ru.nl>
2013-11-25 15:40:39 +0000

    enhancement: massive performance increase in channelconnectivity
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8844 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/channelconnectivity.m
+++ private/channelconnectivity.m
@@ -9,33 +9,53 @@ if isfield(cfg, 'avgoverchan') && strcmp(cfg.avgoverchan, 'yes')
   nchan = 1;
   connectivity = false(nchan,nchan);
 else
-  if nargin < 2
-    nchan = length(cfg.channel);
-    connectivity = false(nchan,nchan);
-    for chan=1:length(cfg.neighbours)
-      [seld] = match_str(cfg.channel, cfg.neighbours(chan).label);
-      [seln] = match_str(cfg.channel, cfg.neighbours(chan).neighblabel);
-      if isempty(seld)
-        % this channel was not present in the data
-        continue;
-      else
-        % add the neighbours of this channel to the matrix
-        connectivity(seld, seln) = true;
-      end
-    end
-  else        
-    nchan = length(data.label);
-    connectivity = false(nchan,nchan);
-    for chan=1:length(cfg.neighbours)
-      [seld] = match_str(data.label, cfg.neighbours(chan).label);
-      [seln] = match_str(data.label, cfg.neighbours(chan).neighblabel);
-      if isempty(seld)
-        % this channel was not present in the data
-        continue;
-      else
-        % add the neighbours of this channel to the matrix
-        connectivity(seld, seln) = true;
-      end
-    end
+  
+  % check whether to use channels in the cfg or data
+  if nargin < 2 && isfield(cfg, 'channel')
+    chans = cfg.channel;
+  elseif nargin == 2
+    chans = data.label;
+  else
+    error('either the cfg needs to have both cfg.channel and cfg.neighbours, or a second (data) input argument needs to be specified');
   end
+  
+  nchan = length(chans);
+  connectivity = false(nchan,nchan);
+  
+  % the original loop was the following, which is very slow:
+%   for chan=1:length(cfg.neighbours)
+%     [seld] = match_str(chans, cfg.neighbours(chan).label);
+%     [seln] = match_str(chans, cfg.neighbours(chan).neighblabel);
+%     if isempty(seld)
+%       % this channel was not present in the data
+%       continue;
+%     else
+%       % add the neighbours of this channel to the matrix
+%       connectivity(seld, seln) = true;
+%     end
+%   end
+  % the following code should be equivalent:
+    
+  % avoid using match_str inside the loop for performance reasons
+  [sel1,sel2] = match_str(cfg.channel, {cfg.neighbours.label}');
+
+  % make big list of all neighbour labels...
+  allneighb = {cfg.neighbours.neighblabel};
+  allneighb = allneighb(sel2);
+  % ...store dimensionality...
+  numAllNeighb = cellfun(@numel, allneighb);
+  allneighb = cat(1, allneighb{:});
+  % ...so that we can do with one call of match_str (with fullout=1)...
+  selneighb = match_str(chans, allneighb, 1);
+  % ...and then we can reshape the indices again to get the neighbours
+  selneighb = mat2cell(selneighb, numAllNeighb);
+
+  for k = 1:numel(sel1)
+    % remove zeroes from selneighb (these correspond to channels absent)
+    seln = selneighb{k};
+    seln = seln(seln > 0);
+    % add the neighbours of this channel to the matrix
+    connectivity(sel1(k), seln) = true;
+  end
+
 end
