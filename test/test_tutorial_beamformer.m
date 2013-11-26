e94c28f730d28a7ba330ed3c95f18c1fb57f10f4
Robert Oostenveld <r.oostenveld@gmail.com>
2013-11-26 15:44:24 +0000

    enhancement - increased mem and walltime to accomodate torque
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8858 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_tutorial_beamformer.m
+++ test/test_tutorial_beamformer.m
@@ -1,7 +1,7 @@
 function test_tutorial_beamformer(datadir)
 
-% MEM 2gb
-% WALLTIME 00:15:19
+% MEM 6gb
+% WALLTIME 00:30:00
 
 % TEST test_tutorial_beamformer
 % TEST ft_redefinetrial ft_freqanalysis ft_volumesegment ft_prepare_singleshell ft_sourceanalysis ft_prepare_leadfield ft_sourceinterpolate ft_sourceplot ft_volumenormalise
