c2682e8d3dd39f761766ad965d6794087a95e2e9
Robert Spangler <rspangler@besa.de>
2013-11-27 12:50:50 +0000

    bugfix - provide script to read individual FE mesh and individual leadfields created by BESA MRI. also see http://bugzilla.fcdonders.nl/show_bug.cgi?id=2336
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8869 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ external/besa/test_bug2336.m
@@ -0,0 +1,48 @@
+% test_bug2336 script to test the integration of FEM output files created
+% by BESA MRI.
+% 
+% Copyright (C) 2013, BESA GmbH
+%
+% File name: test_bug2336.m
+%
+% Author: Robert Spangler
+% Created: 2013-11-27
+
+
+%% Specify path to data folder
+strPath_Data = '/';
+
+
+%% Filenames of BESA MRI output files
+strPath_Leadfield = sprintf('%sBESA_MRI_Electrode_Config_34_PB_FEM_DATA.lft', strPath_Data);
+strPath_SourceSpaceNodes = sprintf('%sBESA_MRI_Electrode_Config_34_PB_FEM_DATA.loc', strPath_Data);
+strPath__ElectrodeConfig = sprintf('%sElectrode_Config_34.sfh', strPath_Data);
+strPath__SkullSurface = sprintf('%sBESA_MRI_MRI_PB_FEM_SKULL_ACPC.srf', strPath_Data);
+strPath__BrainSurface = sprintf('%sBESA_MRI_PB_FEM_BRAIN_ACPC.srf', strPath_Data);
+strPath__CSFSurface = sprintf('%sBESA_MRI_PB_FEM_CSF_ACPC.srf', strPath_Data);
+strPath__ScalpSurface = sprintf('%sBESA_MRI_PB_FEM_SCALP_ACPC.srf', strPath_Data);
+
+
+%% Read leadfield
+[dim lf] = readBESAlft(strPath_Leadfield);   
+fprintf('Number of channels: %i\n', dim(1));
+fprintf('Number of nodes: %i\n', dim(2));
+fprintf('Number of orientations: %i\n', dim(3));
+
+    
+%% Read source space grid nodes
+[ssg IdxNeighbour] = readBESAloc(strPath_SourceSpaceNodes);
+fprintf('Number of nodes: %i\n', size(ssg, 1));
+fprintf('Number of neighbours/node: %i\n', size(IdxNeighbour, 1));
+
+
+%% Read electrode config, surface points & fiducials
+sfh = readBESAsfh(strPath__ElectrodeConfig);
+
+
+%% Read scalp, skull, csf & brain surface
+[pnt_scalp, tri_scalp, srf_scalp] = ft_read_bv_srf(strPath__ScalpSurface);
+[pnt_skull, tri_skull, srf_skull] = ft_read_bv_srf(strPath__SkullSurface);
+[pnt_csf, tri_csf, srf_csf] = ft_read_bv_srf(strPath__CSFSurface);
+[pnt_brain, tri_brain, srf_brain] = ft_read_bv_srf(strPath__BrainSurface);
+
