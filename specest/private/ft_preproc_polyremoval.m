2e81f5e1224e8c0eec6a94be00a74242ba1e9f5c
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2013-11-28 19:18:20 +0000

    automatically synchronized identical files to revision 8889
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8890 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- specest/private/ft_preproc_polyremoval.m
+++ specest/private/ft_preproc_polyremoval.m
@@ -1,15 +1,21 @@
-function [dat,beta,x] = ft_preproc_polyremoval(dat, order, begsample, endsample)
+function [dat,beta,x] = ft_preproc_polyremoval(dat, order, begsample, endsample, flag)
 
 % FT_PREPROC_POLYREMOVAL removed an Nth order polynomal from the data
 %
 % Use as
-%   dat = ft_preproc_polyremoval(dat, order, begsample, endsample)
+%   dat = ft_preproc_polyremoval(dat, order, begsample, endsample, flag)
 % where
 %   dat        data matrix (Nchans X Ntime)
 %   order      the order of the polynomial
 %   begsample  index of the begin sample for the estimate of the polynomial
 %   endsample  index of the end sample for the estimate of the polynomial
-%
+%   flag       optional boolean to specify whether the first order basis 
+%               vector will zscored prior to computing higher order basis
+%               vectors from the first-order basis vector (and the beta
+%               weights). This is to avoid numerical problems with the
+%               inversion of the covariance when the polynomial is of high
+%               order/number of samples is large
+% 
 % If begsample and endsample are not specified, it will use the whole
 % window to estimate the polynomial.
 %
@@ -49,9 +55,16 @@ if nargin<4
   endsample = size(dat,2);
 end
 
+if nargin<5
+  flag = 0;
+end
+
 % construct a "time" axis
 nsamples = size(dat,2);
 basis    = (1:nsamples)-1;
+if flag
+  basis = standardise(basis);
+end
 
 % create a set of basis functions that will be fitted to the data
 x = zeros(order+1,nsamples);
