e37f5aafd8f0cbd91c2d0730fec502c3efe3273b
Robert Oostenveld <r.oostenveld@gmail.com>
2013-12-04 10:50:09 +0000

    documentation - updated the link to the fieldtrip wiki, now http://fieldtrip.fcdonders.nl/faq/how_do_i_install_the_openmeeg_binaries
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8954 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- external/openmeeg/om_checkombin.m
+++ external/openmeeg/om_checkombin.m
@@ -18,8 +18,8 @@ if status
     disp('Download OpenMEEG from')
     disp('http://gforge.inria.fr/frs/?group_id=435')
     disp(' ')
-    disp('See wiki page for installation instructions:')
-    disp('http://fieldtrip.fcdonders.nl/development/openmeeg/testinginstallation')
+    disp('See the installation instructions on')
+    disp('http://fieldtrip.fcdonders.nl/faq/how_do_i_install_the_openmeeg_binaries')
     disp('---------------------------------------------')
     disp('---------------------------------------------')
     disp(result);
