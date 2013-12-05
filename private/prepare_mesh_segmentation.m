66c5d9a4180a61b2f8b99686f9bb274937798f87
Robert Oostenveld <r.oostenveld@gmail.com>
2013-12-05 10:40:01 +0000

    enhancement - included the mesh cleanup from ft_iso2mesh to ft_prepare_mesh/prepare_mesh_segmentation, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=2397
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@8970 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/prepare_mesh_segmentation.m
+++ private/prepare_mesh_segmentation.m
@@ -178,4 +178,32 @@ for i =1:numel(cfg.tissue)
   bnd(i).tri = tri;
   bnd(i).unit = mri.unit;
   
+  
 end % for each tissue
+
+if strcmp(cfg.method, 'iso2surf')
+  % order outside in (trying to be smart here)
+  [dum, order] = sort(numvoxels,'descend');
+  bnd = bnd(order);
+  % clean up the triangulated meshes
+  bnd = decouplesurf(bnd);
+  % put them back in the original order
+  [dum, order] = sort(order);
+  bnd = bnd(order);
+end
+
+end % function
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function bnd = decouplesurf(bnd)
+for ii = 1:length(bnd)-1
+  % Despite what the instructions for surfboolean says, surfaces should
+  % be ordered from inside-out!!
+  [newnode, newelem] = surfboolean(bnd(ii+1).pnt,bnd(ii+1).tri,'decouple',bnd(ii).pnt,bnd(ii).tri);
+  bnd(ii+1).tri = newelem(newelem(:,4)==2,1:3) - size(bnd(ii+1).pnt,1);
+  bnd(ii+1).pnt = newnode(newnode(:,4)==2,1:3);
+end % for
+end %function
