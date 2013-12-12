e197b058c013c3c0a14f2741f12f571df2faeb3e
Jorn Horschig <jm.horschig@donders.ru.nl>
2013-12-12 12:59:54 +0000

    bugfix #2420 - grid specified in 'cm' units explicitly
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9028 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_tutorial_beamformer.m
+++ test/test_tutorial_beamformer.m
@@ -136,6 +136,7 @@ cfg.vol             = vol;
 cfg.reducerank      = 2;
 cfg.channel         = {'MEG','-MLP31', '-MLO12'};
 cfg.grid.resolution = 1;   % use a 3-D grid with a 1 cm resolution
+cfg.grid.unit       = 'cm';
 cfg.normalize       = 'yes';
 [gridn] = ft_prepare_leadfield(cfg);
 
