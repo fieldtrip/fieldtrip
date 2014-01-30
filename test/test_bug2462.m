c0744271247ab529f55df38b534d0c3d6669d0a0
Jim Herring <j.herring@donders.ru.nl>
2014-01-30 10:48:47 +0000

    bugfix-added testscript for bug2462
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9174 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2462.m
@@ -0,0 +1,12 @@
+function test_bug2462
+
+% MEM 1500mb
+% WALLTIME 00:03:00
+% test_bug2462
+cfg = [];
+cfg.dataset = dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2462/scan1_Filters_125HzLP.dat');
+
+data = ft_preprocessing(cfg);
+end
+
+
