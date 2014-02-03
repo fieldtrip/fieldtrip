b8b85155675beabe1ad49eb6832c018c9c957cc3
Robert Oostenveld <r.oostenveld@gmail.com>
2014-02-03 09:03:53 +0000

    bugfix - resolved issue with obsolete isglobal function, also fixed  another bug in the test script
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9180 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug2133.m
+++ test/test_bug2133.m
@@ -15,7 +15,17 @@ testvariable = 1;
 
 hdr = ft_read_header(filename,'dataformat','egi_mff_v2', 'headerformat','egi_mff_v2');
 
-assert(exist(testvariable, 'var'));
-assert(isglobal(testvariable));
-assert(testvariable==1);
+assert(exist('testvariable', 'var')==1);
 
+% The MATLAB isglobal function is obsolete and will be discontinued in a future version of MATLAB.
+% Hence we need a local replication of this functionality
+list = whos;
+name = {list.name};
+glob = [list.global];
+x = any(strcmp('testvariable', name(glob)));
+assert(x);
+
+% it used to read
+% assert(isglobal('testvariable'));
+
+assert(testvariable==1);
