3985af9f7a3040ecb86181ed92b85eeadf3de888
Jim Herring <j.herring@donders.ru.nl>
2014-02-05 16:10:12 +0000

    bugfix-information about brainvision ascii datasets in header used. see bug2462
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9187 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fileio/private/read_brainvision_vhdr.m
+++ fileio/private/read_brainvision_vhdr.m
@@ -78,19 +78,35 @@ if strcmpi(hdr.DataFormat, 'binary')
     case 'ieee_float_32';
       hdr.nSamples = info.bytes./(hdr.NumberOfChannels*4);
   end
-elseif strcmpi(hdr.DataFormat, 'ascii') && strcmpi(hdr.DataOrientation, 'vectorized')
-  % this is a very inefficient fileformat to read data from, it looks like this:
-  % Fp1   -2.129 -2.404 -18.646 -15.319 -4.081 -14.702 -23.590 -8.650 -3.957
-  % AF3   -24.023 -23.265 -30.677 -17.053 -24.889 -35.008 -21.444 -15.896 -12.050
-  % F7    -10.553 -10.288 -19.467 -15.278 -21.123 -25.066 -14.363 -10.774 -15.396
-  % F3    -28.696 -26.314 -35.005 -27.244 -31.401 -39.445 -30.411 -20.194 -16.488
-  % FC1   -35.627 -29.906 -38.013 -33.426 -40.532 -49.079 -38.047 -26.693 -22.852
-  % ...
-  fid = fopen(datafile, 'rt');
-  tline = fgetl(fid);             % read the complete first line
-  fclose(fid);
-  t = tokenize(tline, ' ', true); % cut the line into pieces
-  hdr.nSamples = length(t) - 1;   % the first element is the channel label
+elseif strcmpi(hdr.DataFormat, 'ascii') 
+  hdr.skipLines = 0;
+  hdr.skipColumns = 0;
+  
+  % Read ascii info from header (if available).
+  dataPoints = read_asa(filename, 'DataPoints=', '%d');
+  skipLines = read_asa(filename, 'SkipLines=', '%d');
+  skipColumns = read_asa(filename, 'SkipColumns=', '%d');
+  decimalSymbol = read_asa(filename, 'DecimalSymbol=', '%s'); % This is not used in reading dataset yet
+  
+  if ~isempty(dataPoints); hdr.nSamples = dataPoints; end;
+  if ~isempty(skipLines); hdr.skipLines = skipLines; end;
+  if ~isempty(skipColumns); hdr.skipColumns = skipColumns; end;
+  if ~isempty(decimalSymbol); hdr.decimalSymbol = decimalSymbol; end;
+  
+  if isempty(dataPoints) && strcmpi(hdr.DataOrientation, 'vectorized')
+    % this is a very inefficient fileformat to read data from, it looks like this:
+    % Fp1   -2.129 -2.404 -18.646 -15.319 -4.081 -14.702 -23.590 -8.650 -3.957
+    % AF3   -24.023 -23.265 -30.677 -17.053 -24.889 -35.008 -21.444 -15.896 -12.050
+    % F7    -10.553 -10.288 -19.467 -15.278 -21.123 -25.066 -14.363 -10.774 -15.396
+    % F3    -28.696 -26.314 -35.005 -27.244 -31.401 -39.445 -30.411 -20.194 -16.488
+    % FC1   -35.627 -29.906 -38.013 -33.426 -40.532 -49.079 -38.047 -26.693 -22.852
+    % ...
+    fid = fopen(datafile, 'rt');
+    tline = fgetl(fid);             % read the complete first line
+    fclose(fid);
+    t = tokenize(tline, ' ', true); % cut the line into pieces
+    hdr.nSamples = length(t) - 1;   % the first element is the channel label
+  end;
 end
 
 if isinf(hdr.nSamples)
--- test/test_bug2462.m
+++ test/test_bug2462.m
@@ -3,10 +3,31 @@ function test_bug2462
 % MEM 1500mb
 % WALLTIME 00:10:00
 % test_bug2462
-cfg = [];
-cfg.dataset = dccnfilename('/home/common/matlab/fieldtrip/data/test/bug2462/scan1_Filters_125HzLP.dat');
 
-data = ft_preprocessing(cfg);
+homedir = '/home/common/matlab/fieldtrip/data/test/bug2462/';
+datasets = {'scan1_Filters_125HzLP-ascii-multiplexed.dat';
+           'scan1_Filters_125HzLP-ascii-vectorized.dat';
+           'scan1_Filters_125HzLP-binary-multiplexed.dat';
+           'scan1_Filters_125HzLP-binary-vectorized.dat'};
+
+data = cell(numel(datasets),1);
+
+for i=1:numel(datasets)
+  cfg = [];
+  cfg.dataset = dccnfilename([homedir datasets{i}]);
+  
+  data{i} = ft_preprocessing(cfg);
+end;
+
+
+tolerance = 1;
+% Fixme - create and load different brainvision datasets.
+for i=1:numel(data)
+  for j=1:numel(data)
+    assert(sum(abs(data{i}.trial{1}(:)-data{j}.trial{1}(:)))<tolerance);
+  end
+end;
+
 end
 
 
