2c7492ffe50c32c4b94f620b586089a18bd2cc9d
Diego Lozano Soldevilla <d.lozanosoldevilla@donders.ru.nl>
2014-02-21 12:51:07 +0000

    bug 1666 - volume conductivity definition cfg call updated
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9215 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_example_simulate_forward_dipolefit.m
+++ test/test_example_simulate_forward_dipolefit.m
@@ -23,7 +23,7 @@ end
 % create a concentric 3-sphere volume conductor, the radius is the same as for the electrodes
 vol = [];
 vol.r = [0.88 0.92 1.00]; % radii of spheres
-vol.c = [1 1/80 1];       % conductivity
+vol.cond = [1 1/80 1];       % conductivity
 vol.o = [0 0 0];          % center of sphere
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
