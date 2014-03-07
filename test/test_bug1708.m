3db0533cdb83ef5a8c6426e725e3a54c357228f5
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2014-03-07 08:34:56 +0000

    enhancement - updated test code
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9260 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1708.m
+++ test/test_bug1708.m
@@ -13,6 +13,10 @@ function test_bug1708
 load('/home/common/matlab/fieldtrip/data/test/latest/raw/meg/preproc_ctf275.mat');
 
 cfg  = [];
+cfg.gradient = 'none';
+data = ft_denoise_synthetic(cfg, data);
+
+cfg = [];
 cfg.gradient = 'G3BR';
 data = ft_denoise_synthetic(cfg, data);
 
@@ -20,3 +24,18 @@ assert(all(isfinite(data.grad.coilori(:))));
 assert(all(isfinite(data.grad.coilpos(:))));
 assert(all(isfinite(data.grad.chanori(:))));
 assert(all(isfinite(data.grad.chanpos(:))));
+
+load('test_bug1708.mat');
+
+avgData2 = ft_denoise_synthetic(cfg, avgData); % this confirmed the bug
+
+% however avgData had G1BR balancing, could it be caused by the fact that
+% this balancing is not undone?
+
+cfg = [];
+cfg.gradient = 'none';
+avgData3 = ft_denoise_synthetic(cfg, avgData);
+cfg.gradient = 'G3BR';
+avgData4 = ft_denoise_synthetic(cfg, avgData3);
+
+% this indeed seems to be the case
