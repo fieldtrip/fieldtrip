d41572e2d2df2067c540edb2c99a86ca51c04412
Arjen Stolk <a.stolk@donders.ru.nl>
2014-03-17 21:35:08 +0000

    documentation update
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9294 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- fieldtrip2spss.m
+++ fieldtrip2spss.m
@@ -1,14 +1,17 @@
 function fieldtrip2spss(filename, labels, data)
 
-% FIELDTRIP2SPSS compiles a dataset and its labels into a textfile, 
-% suitable for import to SPSS. 
+% FIELDTRIP2SPSS compiles data and correpsonding labels into a textfile, 
+% suitable for import to SPSS.
 %
-% When exporting from MATLAB, use:
+% Use as:
+% fieldtrip2spss(filename, labels, data)
+%
+% When exporting from MATLAB, set:
 %   - filename; should be string (e.g. 'counts.txt')
 %   - labels; should be a cell array (e.g. {'ones','twos','threes'})
 %   - data; should be either a vector or matrix (e.g. [1 2 3; 1 2 3; 1 2 3])
 %
-% When importing to SPSS, use;
+% When importing to SPSS, set;
 %   - variables included at top of file: 'yes'
 %   - first case of data on line number: '2' (default)
 %   - delimiter appearing between variables: 'tab' (default)
@@ -16,14 +19,12 @@ function fieldtrip2spss(filename, labels, data)
 % In case the columns that make up the data matrix have unequal lengths
 % (e.g. because of different number of subjects per group), use:
 %   data         = ones(30,2)*9999
-%   data(:,1)    = Group1 (30 subj) 
-%   data(1:20,2) = Group2 (only 20 subj)
+%   data(1:30,1) = 1 (30 subj in Group 1) 
+%   data(1:20,2) = 2 (20 subj in Group 2)
 % After importing to SPSS, click the Missing cell in the Variable View
 % window and enter 9999 as the missing value definition.
-%
-% See also NUTMEG2FIELDTRIP, SPASS2FIELDTRIP, LORETA2FIELDTRIP
 
-% Copyright (C) 2011-2013, Arjen Stolk
+% Copyright (C) 2011-2014, Arjen Stolk
 %
 % This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
 % for the documentation and details.
