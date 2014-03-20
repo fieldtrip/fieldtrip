24ee88897ff283afec3641c5fbdc3cbddd863f5b
Jim Herring <j.herring@donders.ru.nl>
2014-03-20 08:18:19 +0000

    bugfix-added testscript for bug2473.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9301 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2473.m
@@ -0,0 +1,20 @@
+function test_bug2473
+
+% MEM 1500mb
+% WALLTIME 00:05:00
+
+% TEST test_bug2473
+% TEST ft_databrowser
+
+load(dccnpath('/home/common/matlab/fieldtrip/data/test/avgFIC.mat'));
+
+cfg = [];
+cfg.preproc.bpfilter = 'yes';
+cfg.preproc.bpfreq = [3 45];
+ft_databrowser(cfg, avgFIC);
+
+% Opening the preproc window in the databrowser and clicking on save and
+% close produces an error because the cfg.preproc.bpfreq value is not on
+% the same line anymore.
+
+end
