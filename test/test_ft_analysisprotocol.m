be15d5245b5cf8ffde67f4b89e3b7a6988d91a8e
Robert Oostenveld <r.oostenveld@gmail.com>
2014-04-05 06:29:27 +0000

    enhancement - extended test with timelock+comp and freq+comp structures, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=2518 and revision 9336. Added a number of historical test directories.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9356 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_ft_analysisprotocol.m
+++ test/test_ft_analysisprotocol.m
@@ -10,6 +10,10 @@ function test_ft_analysisprotocol
 
 dirlist = {
   '/home/common/matlab/fieldtrip/data/test/latest'
+  '/home/common/matlab/fieldtrip/data/test/20131231'
+  '/home/common/matlab/fieldtrip/data/test/20130630'
+  '/home/common/matlab/fieldtrip/data/test/20121231'
+  '/home/common/matlab/fieldtrip/data/test/20120630'
   '/home/common/matlab/fieldtrip/data/test/20111231'
   '/home/common/matlab/fieldtrip/data/test/20110630'
   '/home/common/matlab/fieldtrip/data/test/20101231'
