0998e924d70e02742c2e5c194b0d13756bfe5396
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2014-04-16 10:25:21 +0000

    bugfix 2540 units are correctly handled
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9429 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_volumesegment.m
+++ ft_volumesegment.m
@@ -274,8 +274,15 @@ end
 if dotpm
   
   % remember the original transformation matrix coordinate system
+  original = [];
   original.transform = mri.transform;
   original.coordsys  = mri.coordsys;
+  if isfield(mri,'unit')
+    original.unit    = mri.unit;
+  else
+    mri = ft_convert_units(mri); % guess the unit field if not present
+    original.unit = mri.unit;
+  end
   mri = ft_convert_units(mri,    'mm');
   if isdeployed
     mri = ft_convert_coordsys(mri, 'spm', 2, cfg.template);
@@ -419,12 +426,10 @@ if dotpm
   
   % collect the results
   segmented.dim       = size(V(1).dat);
-  segmented.dim       = segmented.dim(:)';    % enforce a row vector
+  segmented.dim       = segmented.dim(:)';  % enforce a row vector
   segmented.transform = original.transform; % use the original transform
   segmented.coordsys  = original.coordsys;  % use the original coordsys
-  if isfield(mri, 'unit')
-    segmented.unit = mri.unit;
-  end
+  segmented.unit      = original.unit;      % use the original units
   segmented.gray      = V(1).dat;
   if length(V)>1, segmented.white     = V(2).dat; end
   if length(V)>2, segmented.csf       = V(3).dat; end
