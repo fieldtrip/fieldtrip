b28619cc043a1e902a9469ce05f03e6f4e6b0019
Robert Oostenveld <r.oostenveld@gmail.com>
2014-05-07 14:19:37 +0000

    bugfix - resolved dimord issue in ft_selectdata for less usual data fields in which one of the dimensions was "subj". See http://bugzilla.fcdonders.nl/show_bug.cgi?id=2563
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9472 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug2563.m
+++ test/test_bug2563.m
@@ -7,26 +7,39 @@ function test_bug2563
 % MEM 1gb
 
 
-nchan = 3;
-nfreq = 4;
-ntime = 5;
+nsubj = 3;
+nchan = 4;
+nfreq = 5;
 
 freq = [];
-freq.MIspctrm = randn(nchan,nfreq,ntime);
+freq.dimord = 'subj_chan_freq';
+freq.MIspctrm = randn(nsubj,nchan,nfreq);
 freq.freq = 1:nfreq;
-freq.time = 1:ntime;
 for i=1:nchan
   freq.label{i} = num2str(i);
 end
 
 cfg = [];
-cfg.avgoverfreq = 'yes'
+cfg.avgoverfreq = 'yes';
 output = ft_selectdata(cfg, freq);
 
-assert(isequal(size(output.MIspctrm), [3 1 5]));
+assert(isequal(size(output.MIspctrm), [3 4]));
 
 disp(freq)
 disp(output)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% the following was reported as http://bugzilla.fcdonders.nl/show_bug.cgi?id=2561
+% but seems to be related, i.e. both have subjects in the data
 
+timelock = [];
+timelock.individual = randn(7,204,1500);
+timelock.time = 1:1500;
+for i=1:204
+  timelock.label{i} = num2str(i);
+end
+timelock.dimord = 'subj_chan_time';
+
+cfg = [];
+output = ft_selectdata(cfg, timelock);
 
