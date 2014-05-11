635ae5d40e8b4b2b01e60cd1f825b1520c5ee98c
Robert Oostenveld <r.oostenveld@gmail.com>
2014-05-11 08:12:36 +0000

    restructuring - removed private isrow and iscolumn, as they would cause lots of MATLAB warnings. They are now subfunctions. See bug 2567.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9478 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/ft_selectdata.m
+++ utilities/ft_selectdata.m
@@ -1210,3 +1210,45 @@ end
 end % function cellmatmean
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%ISROW True if input is a row vector.
+%   ISROW(V) returns logical 1 (true) if SIZE(V) returns [1 n]
+%   with a nonnegative integer value n, and logical 0 (false) otherwise.
+%
+% This is a drop-in replacement for the MATLAB function with the same name,
+% which does not exist in versions < 2010.
+%
+% See http://bugzilla.fcdonders.nl/show_bug.cgi?id=2567
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function s = isrow(v)
+siz = size(v);
+m = siz(1);
+n = siz(2);
+if numel(siz)==2 && m==1 && n>0
+  s = true;
+else
+  s = false;
+end
+end % function isrow
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%ISROW True if input is a column vector.
+%   ISROW(V) returns logical 1 (true) if SIZE(V) returns [m 1]
+%   with a nonnegative integer value m, and logical 0 (false) otherwise.
+%
+% This is a drop-in replacement for the MATLAB function with the same name,
+% which does not exist in versions < 2010.
+%
+% See http://bugzilla.fcdonders.nl/show_bug.cgi?id=2567
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function s = iscolumn(v)
+siz = size(v);
+m = siz(1);
+n = siz(2);
+if numel(siz)==2 && m>0 && n==1
+  s = true;
+else
+  s = false;
+end
+end % function iscolumn
+
