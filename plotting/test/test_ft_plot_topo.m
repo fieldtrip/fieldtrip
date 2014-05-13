717af9d3c06d8c2397ab2004fb7ad37870bc8f04
Robert Oostenveld <r.oostenveld@gmail.com>
2014-05-13 09:16:43 +0000

    restructuring - some small changes to code to clarify the structure, added documentation. See http://bugzilla.fcdonders.nl/show_bug.cgi?id=2574
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9510 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/test/test_ft_plot_topo.m
+++ plotting/test/test_ft_plot_topo.m
@@ -6,9 +6,29 @@ function test_ft_plot_topo
 % TEST test_ft_plot_topo
 % TEST ft_plot_topo
 
-x = randn(10,1);
-y = randn(10,1);
-val = randn(10,1);
+x = randn(30,1);
+y = randn(30,1);
+
+% make something round between 0 and 1
+val = -sqrt(x.^2 + y.^2);
+val = val-min(val);
+val = val./max(val);
 
 figure
 ft_plot_topo(x, y, val);
+hold on; plot(x, y, 'k*')
+
+figure
+ft_plot_topo(x, y, val, 'shading', 'interp');
+hold on; plot(x, y, 'k*')
+
+figure
+ft_plot_topo(x, y, val, 'datmask', val>0.5, 'style', 'surf');
+hold on; plot(x, y, 'k*')
+
+figure
+ft_plot_topo(x, y, val, 'datmask', val>0.5, 'style', 'imsat', 'clim', [0 1]);
+hold on; plot(x, y, 'k*')
+
+
+
