fa53697d1340eb0f2253c5779f7ef27223d77310
Robert Oostenveld <r.oostenveld@gmail.com>
2014-05-15 06:49:18 +0000

    enhancement - added test script for windaq data from Tineke, see http://bugzilla.fcdonders.nl/show_bug.cgi?id=2569
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9535 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2569.m
@@ -0,0 +1,20 @@
+function test_bug2569
+
+% WALLTIME 00:10:00
+% MEM 1500mb
+
+% TEST test_bug2569
+% TEST read_wdq_header read_wdq_data read_wdq_event
+
+cfg = [];
+cfg.dataset = dccnpath('/home/common/matlab/fieldtrip/data/test/bug2569/WoD dec2013 rat nr 11.WDQ');
+ft_databrowser(cfg)
+
+hdr = ft_read_header(cfg.dataset);
+dat = ft_read_data(cfg.dataset);
+evt = ft_read_event(cfg.dataset);
+
+assert(hdr.nChans==8);
+assert(size(dat,1)==8);
+assert(size(dat,2)==20408500);
+assert(length(evt)==12);
