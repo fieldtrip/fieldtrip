c9d733a7367c5b66ef3cadbe089f719285efda2e
Robert Oostenveld <r.oostenveld@gmail.com>
2014-05-21 15:17:03 +0000

    documentation - fixed the function help
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9573 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/ft_postamble.m
+++ utilities/ft_postamble.m
@@ -1,13 +1,13 @@
 function ft_postamble(cmd, varargin)
 
-% FT_PREAMBLE is a helper function that is included in many of the FieldTrip
-% functions and which takes care of some general settings and operations at the
-% end of the function.
+% FT_POSTAMBLE is a helper function that is included in many of the FieldTrip
+% functions and which takes care of some general settings and operations at the end
+% of the function.
 %
-% This ft_postamble m-file is a function, but internally it executes a
-% number of private scripts in the callers workspace. This allows the
-% private script to access the variables in the callers workspace and
-% behave as if the script were included as a header file in C-code.
+% This ft_postamble m-file is a function, but internally it executes a number of
+% private scripts in the callers workspace. This allows the private script to access
+% the variables in the callers workspace and behave as if the script were included as
+% a header file in C-code.
 %
 % See also FT_PREAMBLE
 
