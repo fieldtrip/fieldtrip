7766d499bc9601fc2d60f32820198715d6ba6c6d
Robert Oostenveld <r.oostenveld@gmail.com>
2014-06-22 05:56:30 +0000

    bugfix - fixed typo in path and added missing bracket, updated to fiducial/interactive and added manual fiducial locations for 1st and 2nd volumerealign
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9660 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_tutorial_MNE.m
+++ test/test_tutorial_MNE.m
@@ -3,26 +3,32 @@ function test_tutorial_MNE
 % MEM 4gb
 % WALLTIME 00:30:00
 
-cd(dccnpath('/home/common/matab/fieldtrip/data');
+cd(dccnpath('/home/common/matlab/fieldtrip/data'));
 mri = ft_read_mri('Subject01.mri');
 
 cfg        = [];
-cfg.method = 'interactive';
-mri        = ft_volumerealign(cfg, mri_other);
-mri_other = ft_determine_coordsys(mri, 'interactive', 'yes');
-
+% cfg.method = 'interactive';
+cfg.method = 'fiducial'; % the following voxel coords were determined interactive
+cfg.fiducial.nas = [89 60 116];
+cfg.fiducial.lpa = [29 145 154];
+cfg.fiducial.rpa = [143 142 158];
+mri        = ft_volumerealign(cfg, mri);
 
 cfg            = [];
 cfg.resolution = 1;
 cfg.dim        = [256 256 256];
-mrirs          = ft_volumereslice(cfg, mri);
+mri_rs         = ft_volumereslice(cfg, mri);
 
-cfg        = [];
-cfg.method = 'interactive';
-mri_mni    = ft_volumerealign(cfg, mrirs);
+cfg          = [];
+% cfg.method   = 'interactive';
+cfg.method = 'fiducial'; % the following voxel coords were determined interactive
+cfg.coordsys = 'spm';
+cfg.fiducial.ac = [ 153 128 125 ];
+cfg.fiducial.pc = [ 101 128 138 ];
+cfg.fiducial.xzpoint = [ 138 128 180 ];
+mri_mni      = ft_volumerealign(cfg, mri_rs);
 
 cfg           = [];
-cfg.coordsys  = 'spm';
 cfg.output    = {'skullstrip' 'brain'};
-seg           = ft_volumesegment(cfg, mri_mni);
+mri_seg       = ft_volumesegment(cfg, mri_mni);
 
