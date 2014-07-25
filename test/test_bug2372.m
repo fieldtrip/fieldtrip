30719841809402d1f0dfb748aab5e82fb85129ee
Diego Lozano Soldevilla <d.lozanosoldevilla@donders.ru.nl>
2014-07-25 15:25:00 +0000

    bugfix2372 - test function added
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9757 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2372.m
@@ -0,0 +1,39 @@
+function test_bug2372
+
+global ft_default;
+ft_default.feedback = 'no';
+
+if ispc
+  datadir = 'H:';
+else
+  datadir = '/home';
+end
+
+load(fullfile(datadir, 'common', 'matlab', 'fieldtrip', 'data', 'ftp', 'tutorial', 'eventrelatedaveraging', 'dataFC_LP.mat'));
+load(fullfile(datadir, 'common', 'matlab', 'fieldtrip', 'data', 'ftp', 'tutorial', 'eventrelatedaveraging', 'dataFIC_LP.mat'));
+load(fullfile(datadir, 'common', 'matlab', 'fieldtrip', 'data', 'ftp', 'tutorial', 'eventrelatedaveraging', 'dataIC_LP.mat'));
+
+%% creating timelock structures with avg and trial fields
+cfg=[];
+cfg.keeptrials ='yes';
+tlk1 = ft_timelockanalysis(cfg,dataFC_LP);
+tlk2 = ft_timelockanalysis(cfg,dataFIC_LP);
+tlk3 = ft_timelockanalysis(cfg,dataIC_LP);
+
+%% checking for a general ft_timelockgrandaverage
+%here ft_timelockgrandaverage should discard the trial field
+cfg = [];
+cfg.parameter = 'avg';
+gavg = ft_timelockgrandaverage(cfg,tlk1,tlk2,tlk3);
+  
+%% test the cfg.parameter = 'trial' expected error
+try
+  cfg.parameter = 'trial';
+  gavg = ft_timelockgrandaverage(cfg,tlk1,tlk2,tlk3);
+  success=true;
+catch
+  success=false;
+end
+if success
+  error('this should fail with cfg.parameter = trial');
+end
