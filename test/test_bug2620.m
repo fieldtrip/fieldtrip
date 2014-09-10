07551f1be79e9a32d71da61d2778447864604e39
Tzvetan Popov <t.popov@fcdonders.ru.nl>
2014-09-10 13:08:26 +0000

    enhancement - implemented test script for http://bugzilla.fcdonders.nl/show_bug.cgi?id=2620
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9784 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

---
+++ test/test_bug2620.m
@@ -0,0 +1,24 @@
+function test_bug2620
+
+% MEM 1gb
+% WALLTIME 00:10:00
+
+% TEST test_bug2620
+% TEST ft_multiplotER ft_multiplotER
+
+load(dccnpath('bug2060.mat'));
+
+close all
+
+cfg = [];
+cfg.showoutline = 'yes';
+figure; ft_multiplotER(cfg, timelock);
+
+cfg.showoutline = 'yes';
+figure; ft_multiplotTFR(cfg, freq);
+
+cfg.showoutline = 'no';
+figure; ft_multiplotER(cfg, timelock); % THIS WAS THE PROBLEMATIC ONE
+
+cfg.showoutline = 'no';
+figure; ft_multiplotTFR(cfg, freq);
