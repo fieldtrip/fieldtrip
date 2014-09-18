1ac7cf8634d3072c0ab58da8470d898216f92b84
Diego Lozano Soldevilla <d.lozanosoldevilla@donders.ru.nl>
2014-09-18 16:27:42 +0000

    enhancement - documentation update
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9813 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_combineplanar.m
+++ ft_combineplanar.m
@@ -2,11 +2,12 @@ function [data] = ft_combineplanar(cfg, data)
 
 % FT_COMBINEPLANAR computes the planar gradient magnitude over both directions
 % combining the two gradients at each sensor to a single positive-valued number.
-% This can be done for averaged ERFs or TFRs (i.e. powerspectra).
+% This can be done for averaged planar gradient ERF or single-trial/avegared TFR 
+% (i.e. powerspectra).
 %
 % Use as
 %   [data] = ft_combineplanar(cfg, data)
-% where data contains an averaged planar gradient (either ERF or TFR).
+% where data contains an averaged planar gradient ERF or single-trial/avegared TFR.
 %
 % In the case of ERFs, the configuration can contain
 %   cfg.demean         = 'yes' or 'no' (default)
