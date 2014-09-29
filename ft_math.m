2cbf8819f176d1ab1efcc6684353299d57790010
Robert Oostenveld <r.oostenveld@gmail.com>
2014-09-29 11:30:11 +0000

    bugfix #2711 - there was a problem with keeping elec and/or grad
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9866 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_math.m
+++ ft_math.m
@@ -350,15 +350,16 @@ data.dimord = dimord;
 
 % certain fields should remain in the output, but only if they are identical in all inputs
 keepfield = {'grad', 'elec'};
+keepfield = {'grad', 'elec'};
 for j=1:numel(keepfield)
-  if isfield(varargin{i}, keepfield{j})
+  if isfield(varargin{1}, keepfield{j})
+    tmp  = varargin{i}.(keepfield{j});
     keep = true;
-    tmp = varargin{i}.(keepfield{j});
   else
     keep = false;
   end
   for i=1:numel(varargin)
-    if ~isequal(varargin{i}.(keepfield{j}), tmp)
+    if ~isfield(varargin{i}, keepfield{j}) || ~isequal(varargin{i}.(keepfield{j}), tmp)
       keep = false;
       break
     end
