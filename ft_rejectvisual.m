74cd49debb8458dd5170c2531cba9138a15480d3
Robert Oostenveld <r.oostenveld@gmail.com>
2014-10-07 08:42:26 +0000

    enhancement - return selected artifacts in dataout.cfg.artfctdef.summary.artifact (idem for method=channel/trial) to make output cfg compatible with ft_rejectartifact. Use ft_getopt and some cleanups in cfg handling.
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@9881 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_rejectvisual.m
+++ ft_rejectvisual.m
@@ -90,6 +90,7 @@ function [data] = ft_rejectvisual(cfg, data)
 % cfg.feedback
 
 % Copyright (C) 2005-2006, Markus Bauer, Robert Oostenveld
+% Copyright (C) 2006-2014, Robert Oostenveld
 %
 % This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
 % for the documentation and details.
@@ -128,46 +129,42 @@ if abort
   return
 end
 
-% ft_checkdata is done further down
-
-% check if the input cfg is valid for this function
-cfg = ft_checkconfig(cfg, 'renamedval',  {'metric',  'absmax',  'maxabs'});
-cfg = ft_checkconfig(cfg, 'renamedval',  {'method',  'absmax',  'maxabs'});
-
-% set the defaults
-if ~isfield(cfg, 'channel'),     cfg.channel = 'all';          end
-if ~isfield(cfg, 'trials'),      cfg.trials = 'all';           end
-if ~isfield(cfg, 'latency'),     cfg.latency = 'maxperlength'; end
-if ~isfield(cfg, 'keepchannel'), cfg.keepchannel = 'no';       end
-if ~isfield(cfg, 'keeptrial'),   cfg.keeptrial = 'no';         end
-if ~isfield(cfg, 'feedback'),    cfg.feedback = 'textbar';     end
-if ~isfield(cfg, 'method'),      cfg.method = 'summary';       end
-if ~isfield(cfg, 'alim'),        cfg.alim = [];                end
-if ~isfield(cfg, 'eegscale'),    cfg.eegscale = [];            end
-if ~isfield(cfg, 'eogscale'),    cfg.eogscale = [];            end
-if ~isfield(cfg, 'ecgscale'),    cfg.ecgscale = [];            end
-if ~isfield(cfg, 'emgscale'),    cfg.emgscale = [];            end
-if ~isfield(cfg, 'megscale'),    cfg.megscale = [];            end
-if ~isfield(cfg, 'gradscale'),   cfg.gradscale = [];           end
-if ~isfield(cfg, 'magscale'),    cfg.magscale = [];            end
-if ~isfield(cfg, 'plotlayout'),  cfg.plotlayout = 'square';    end
-if ~isfield(cfg, 'viewmode'),    cfg.viewmode   = 'remove';    end
-
 % store original datatype
 dtype = ft_datatype(data);
 
 % check if the input data is valid for this function, this will convert it to raw if needed
 data = ft_checkdata(data, 'datatype', {'raw+comp', 'raw'}, 'feedback', 'yes', 'hassampleinfo', 'yes');
 
-% for backward compatibility
-if ~isfield(cfg, 'metric') && any(strcmp(cfg.method, {'var', 'min', 'max', 'maxabs', 'range'}))
-  cfg.metric = cfg.method;
-  cfg.method = 'summary';
-end
+% check if the input cfg is valid for this function
+cfg = ft_checkconfig(cfg, 'renamedval',  {'metric',  'absmax',  'maxabs'});
+cfg = ft_checkconfig(cfg, 'renamedval',  {'method',  'absmax',  'maxabs'});
 
-if ~isfield(cfg, 'metric')
-  cfg.metric = 'var';
-end
+% resolve some common typing errors
+cfg = ft_checkconfig(cfg, 'renamed',  {'keeptrials',  'keeptrial'});
+cfg = ft_checkconfig(cfg, 'renamed',  {'keepchannels',  'keepchannel'});
+
+% set the defaults
+cfg.channel     = ft_getopt(cfg, 'channel'    , 'all');
+cfg.trials      = ft_getopt(cfg, 'trials'     , 'all');
+cfg.latency     = ft_getopt(cfg, 'latency'    , 'maxperlength');
+cfg.keepchannel = ft_getopt(cfg, 'keepchannel', 'no');
+cfg.keeptrial   = ft_getopt(cfg, 'keeptrial'  , 'no');
+cfg.feedback    = ft_getopt(cfg, 'feedback'   , 'textbar');
+cfg.method      = ft_getopt(cfg, 'method'     , 'summary');
+cfg.metric      = ft_getopt(cfg, 'metric'     , 'var');
+cfg.alim        = ft_getopt(cfg, 'alim'       );
+cfg.eegscale    = ft_getopt(cfg, 'eegscale'   );
+cfg.eogscale    = ft_getopt(cfg, 'eogscale'   );
+cfg.ecgscale    = ft_getopt(cfg, 'ecgscale'   );
+cfg.emgscale    = ft_getopt(cfg, 'emgscale'   );
+cfg.megscale    = ft_getopt(cfg, 'megscale'   );
+cfg.gradscale   = ft_getopt(cfg, 'gradscale'  );
+cfg.magscale    = ft_getopt(cfg, 'magscale'   );
+cfg.plotlayout  = ft_getopt(cfg, 'plotlayout' , 'square');
+cfg.viewmode    = ft_getopt(cfg, 'viewmode'   , 'remove');
+
+% ensure that the preproc specific options are located in the cfg.preproc substructure
+cfg = ft_checkconfig(cfg, 'createsubcfg',  {'preproc'});
 
 % determine the duration of each trial
 for i=1:length(data.time)
@@ -198,86 +195,85 @@ elseif (strcmp(cfg.latency, 'poststim'))
   cfg.latency(2) = maxperlength(2);
 end
 
-% ensure that the preproc specific options are located in the cfg.preproc substructure
-cfg = ft_checkconfig(cfg, 'createsubcfg',  {'preproc'});
-
 % apply scaling to the selected channel types to equate the absolute numbers (i.e. fT and uV)
 % make a seperate copy to prevent the original data from being scaled
 tmpdata = data;
-scaled  = 0;
+scaled  = false;
 if ~isempty(cfg.eegscale)
-  scaled = 1;
+  scaled = true;
   chansel = match_str(tmpdata.label, ft_channelselection('EEG', tmpdata.label));
   for i=1:length(tmpdata.trial)
     tmpdata.trial{i}(chansel,:) = tmpdata.trial{i}(chansel,:) .* cfg.eegscale;
   end
 end
 if ~isempty(cfg.eogscale)
-  scaled = 1;
+  scaled = true;
   chansel = match_str(tmpdata.label, ft_channelselection('EOG', tmpdata.label));
   for i=1:length(tmpdata.trial)
     tmpdata.trial{i}(chansel,:) = tmpdata.trial{i}(chansel,:) .* cfg.eogscale;
   end
 end
 if ~isempty(cfg.ecgscale)
-  scaled = 1;
+  scaled = true;
   chansel = match_str(tmpdata.label, ft_channelselection('ECG', tmpdata.label));
   for i=1:length(tmpdata.trial)
     tmpdata.trial{i}(chansel,:) = tmpdata.trial{i}(chansel,:) .* cfg.ecgscale;
   end
 end
 if ~isempty(cfg.emgscale)
-  scaled = 1;
+  scaled = true;
   chansel = match_str(tmpdata.label, ft_channelselection('EMG', tmpdata.label));
   for i=1:length(tmpdata.trial)
     tmpdata.trial{i}(chansel,:) = tmpdata.trial{i}(chansel,:) .* cfg.emgscale;
   end
 end
 if ~isempty(cfg.megscale)
-  scaled = 1;
+  scaled = true;
   chansel = match_str(tmpdata.label, ft_channelselection('MEG', tmpdata.label));
   for i=1:length(tmpdata.trial)
     tmpdata.trial{i}(chansel,:) = tmpdata.trial{i}(chansel,:) .* cfg.megscale;
   end
 end
 if ~isempty(cfg.gradscale)
-  scaled = 1;
+  scaled = true;
   chansel = match_str(tmpdata.label, ft_channelselection('MEGGRAD', tmpdata.label));
   for i=1:length(tmpdata.trial)
     tmpdata.trial{i}(chansel,:) = tmpdata.trial{i}(chansel,:) .* cfg.gradscale;
   end
 end
 if ~isempty(cfg.magscale)
-  scaled = 1;
+  scaled = true;
   chansel = match_str(tmpdata.label, ft_channelselection('MEGMAG', tmpdata.label));
   for i=1:length(tmpdata.trial)
     tmpdata.trial{i}(chansel,:) = tmpdata.trial{i}(chansel,:) .* cfg.magscale;
   end
 end
 
-if strcmp(cfg.method, 'channel')
-  if scaled
-    fprintf('showing the scaled data per channel, all trials at once\n');
-  else
-    fprintf('showing the data per channel, all trials at once\n');
-  end
-  [chansel, trlsel, cfg] = rejectvisual_channel(cfg, tmpdata);
-elseif strcmp(cfg.method, 'trial')
-  if scaled
-    fprintf('showing the scaled per trial, all channels at once\n');
-  else
-    fprintf('showing the data per trial, all channels at once\n');
-  end
-  [chansel, trlsel, cfg] = rejectvisual_trial(cfg, tmpdata);
-elseif strcmp(cfg.method, 'summary')
-  if scaled
-    fprintf('showing a summary of the scaled data for all channels and trials\n');
-  else
-    fprintf('showing a summary of the data for all channels and trials\n');
-  end
-  
-  [chansel, trlsel, cfg] = rejectvisual_summary(cfg, tmpdata);
-end
+switch cfg.method
+  case 'channel'
+    if scaled
+      fprintf('showing the scaled data per channel, all trials at once\n');
+    else
+      fprintf('showing the data per channel, all trials at once\n');
+    end
+    [chansel, trlsel, cfg] = rejectvisual_channel(cfg, tmpdata);
+  case 'trial'
+    if scaled
+      fprintf('showing the scaled per trial, all channels at once\n');
+    else
+      fprintf('showing the data per trial, all channels at once\n');
+    end
+    [chansel, trlsel, cfg] = rejectvisual_trial(cfg, tmpdata);
+  case 'summary'
+    if scaled
+      fprintf('showing a summary of the scaled data for all channels and trials\n');
+    else
+      fprintf('showing a summary of the data for all channels and trials\n');
+    end
+    [chansel, trlsel, cfg] = rejectvisual_summary(cfg, tmpdata);
+  otherwise
+    error('unsupported method %s', cfg.method);
+end % switch method
 
 fprintf('%d trials marked as GOOD, %d trials marked as BAD\n', sum(trlsel), sum(~trlsel));
 fprintf('%d channels marked as GOOD, %d channels marked as BAD\n', sum(chansel), sum(~chansel));
@@ -344,18 +340,14 @@ if ~all(trlsel)
         data.trial{i}(:,:) = nan;
       end
       
-      
     otherwise
       error('invalid specification of cfg.keeptrial')
   end % case
 end
 
-% construct an artifact matrix from the trl matrix prior to making the selection
 if isfield(data, 'sampleinfo')
-  cfg.artifact = data.sampleinfo(~trlsel,:);
-else
-  % since sample numbers are unknown, it is not possible to remember them
-  cfg.artifact = [];
+  % construct the matrix with sample numbers prior to making the selection
+  cfg.artfctdef.(cfg.method).artifact = data.sampleinfo(~trlsel,:);
 end
 
 % perform the selection of channels and trials
@@ -371,7 +363,6 @@ data = ft_selectdata(tmpcfg, data);
 % restore the provenance information
 [cfg, data] = rollback_provenance(cfg, data);
 cfg = copyfields(orgcfg, cfg, {'channel', 'trials'});
-
 
 % convert back to input type if necessary
 switch dtype
