7442532ec68e544ad2fdd8b7a1c295fb41e2ad78
Robert Oostenveld <r.oostenveld@gmail.com>
2014-12-03 09:14:18 +0000

    restructuring - some changes in whitespace, nothing functional
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10012 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- forward/ft_convert_units.m
+++ forward/ft_convert_units.m
@@ -177,17 +177,17 @@ if isfield(obj, 'bnd'),
   end
 end
 
-% gradiometer array
+% old-fashioned gradiometer array
 if isfield(obj, 'pnt1'), obj.pnt1 = scale * obj.pnt1; end
 if isfield(obj, 'pnt2'), obj.pnt2 = scale * obj.pnt2; end
 if isfield(obj, 'prj'),  obj.prj  = scale * obj.prj;  end
 
 % gradiometer array, electrode array, head shape or dipole grid
-if isfield(obj, 'pnt'),        obj.pnt     = scale * obj.pnt; end
-if isfield(obj, 'chanpos'),    obj.chanpos = scale * obj.chanpos; end
+if isfield(obj, 'pnt'),        obj.pnt        = scale * obj.pnt;        end
+if isfield(obj, 'chanpos'),    obj.chanpos    = scale * obj.chanpos;    end
 if isfield(obj, 'chanposorg'), obj.chanposorg = scale * obj.chanposorg; end
-if isfield(obj, 'coilpos'),    obj.coilpos = scale * obj.coilpos; end
-if isfield(obj, 'elecpos'),    obj.elecpos = scale * obj.elecpos; end
+if isfield(obj, 'coilpos'),    obj.coilpos    = scale * obj.coilpos;    end
+if isfield(obj, 'elecpos'),    obj.elecpos    = scale * obj.elecpos;    end
 
 % gradiometer array that combines multiple coils in one channel
 if isfield(obj, 'tra') && isfield(obj, 'chanunit')
