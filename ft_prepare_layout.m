49223e81312d600e6b2488ba85985647501aea85
Robert Oostenveld <r.oostenveld@gmail.com>
2014-12-05 12:56:39 +0000

    enhancement - converted script into function, some non-significant changes
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10023 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- ft_prepare_layout.m
+++ ft_prepare_layout.m
@@ -58,7 +58,7 @@ function [layout, cfg] = ft_prepare_layout(cfg, data)
 %   layout.mask    = optional cell-array with line segments that determine the area for topographic interpolation
 %   layout.outline = optional cell-array with line segments that represent the head, nose, ears, sulci or other anatomical features
 %
-% See also FT_LAYOUTPLOT, FT_TOPOPLOTER, FT_TOPOPLOTTFR, FT_MULTIPLOTER, FT_MULTIPLOTTFR
+% See also FT_TOPOPLOTER, FT_TOPOPLOTTFR, FT_MULTIPLOTER, FT_MULTIPLOTTFR, FT_PLOT_LAY
 
 % undocumented and non-recommended option (for SPM only)
 %   cfg.style       string, '2d' or '3d' (default = '2d')
