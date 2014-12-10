fieldtrip/fieldtrip

commit 8f69c35ddea7ce00756a0aaa0139f24b5b33eab1

error when no trials or channels are selected
git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10032 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

ft_freqanalysis.m
@@ -227,6 +227,15 @@ data = ft_selectdata(tmpcfg, data);
 % restore the provenance information
 [cfg, data] = rollback_provenance(cfg, data);
 
+% some proper error handling
+if numel(data.trial)==0
+  error('no trials selected.');
+end
+
+if numel(data.trial)==0
+  error('no channels selected.');
+end
+
 % switch over method and do some of the method specfic checks and defaulting
 switch cfg.method
   

commit 4e14234c1ff33c9e07a923f466e817dda5a72fc0

bugfix - don't check trial count for mvar data (bug was introduced in r10032)
git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10033 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

ft_freqanalysis.m
@@ -228,11 +228,11 @@ data = ft_selectdata(tmpcfg, data);
 [cfg, data] = rollback_provenance(cfg, data);
 
 % some proper error handling
-if numel(data.trial)==0
-  error('no trials selected.');
+if isfield(data, 'trial') && numel(data.trial)==0
+  error('no trials selected.'); % this does not apply for MVAR data
 end
 
-if numel(data.trial)==0
+if numel(data.label)==0
   error('no channels selected.');
 end
 