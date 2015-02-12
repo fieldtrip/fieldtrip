f32dcb5a15f5aa59ba98b9a57b57d19e2a336bf9
Robert Oostenveld <r.oostenveld@gmail.com>
2015-02-12 09:29:27 +0000

    bugfix - some small fixes and updates to the test script to get it to work. See http://bugzilla.fcdonders.nl/show_bug.cgi?id=1954
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10225 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1954.m
+++ test/test_bug1954.m
@@ -3,50 +3,45 @@ function test_bug1954
 % MEM 2gb
 % WALLTIME 00:10:00
 
-load /home/common/matlab/fieldtrip/template/headmodel/standard_mri.mat
+load(fullfile(fileparts(which('ft_defaults')), 'template', 'headmodel', 'standard_mri.mat'));
 
 mri.coordsys = 'ras'; % this can also be determined with ft_determine_coordsys
 
-cfg           = [];
-cfg.output    = {'brain','skull','scalp'};
-% cfg.coordsys  = 'ras'; % not supported any more, should be specified in the input data
-segmentedmri  = ft_volumesegment(cfg, mri);
+cfg = [];
+cfg.output = {'brain','skull','scalp'};
+% cfg.coordsys = 'ras'; % not supported any more, should be specified in the input data
+segmentedmri = ft_volumesegment(cfg, mri);
 
 % I get warning (exact file name changes each time):
-% Warning: could not open /tmp/tpe20fae07_8585_42ab_a6e4_0e4c656f97b3.img 
+% Warning: could not open /tmp/tpe20fae07_8585_42ab_a6e4_0e4c656f97b3.img
 
-if 0 % so this doesn't run during automated testing
-  figure;imagesc(squeeze(segmentedmri.scalp(:,110,:)))
-  % voxels on top of head included in scalp
-end
+figure; imagesc(squeeze(segmentedmri.scalp(:,110,:)))
+% voxels on top of head included in scalp
 
-cfg=[];
-cfg.numvertices=3000;
-bnd=ft_prepare_mesh(cfg,segmentedmri);
-if 0 
- figure;ft_plot_mesh(bnd(1),'facealpha',.2);hold on;ft_plot_mesh(bnd(2))
- figure;ft_plot_mesh(bnd(1),'facealpha',.2);hold on;ft_plot_mesh(bnd(3))
- figure;ft_plot_mesh(bnd(3),'facealpha',.2);hold on;ft_plot_mesh(bnd(2))
- % order is now 1) scalp, 2) brain, 3) skull
-end
+cfg = [];
+cfg.numvertices = 1000;
+cfg.tissue = {'scalp', 'skull', 'brain'};
+bnd = ft_prepare_mesh(cfg,segmentedmri);
 
+figure;ft_plot_mesh(bnd(1),'facecolor', 'none'); hold on; ft_plot_mesh(bnd(2),'facecolor', 'none')
+figure;ft_plot_mesh(bnd(1),'facecolor', 'none'); hold on; ft_plot_mesh(bnd(3),'facecolor', 'none')
+figure;ft_plot_mesh(bnd(2),'facecolor', 'none'); hold on; ft_plot_mesh(bnd(3),'facecolor', 'none')
+% order is now 1) scalp, 2) brain, 3) skull
 
 cfg = [];
 cfg.method = 'bemcp';
+cfg.numvertices = 1000;
+cfg.tissue = {'scalp', 'skull', 'brain'};
 vol1 = ft_prepare_headmodel(cfg, segmentedmri);
-any(isnan(vol1.mat(:)))
-% vol1.mat: none are NaN
+assert(~any(isnan(vol1.mat(:))), 'there is a NaN in vol1.mat');
 
 cfg = [];
 cfg.method = 'dipoli';
 vol2 = ft_prepare_headmodel(cfg, segmentedmri);
-% looks ok, but no .mat to assess
+assert(~any(isnan(vol2.mat(:))), 'there is a NaN in vol2.mat');
 
-segmentedmri.bnd=bnd;
-cfg=[];
-cfg.method='bemcp';
-vol1o=ft_prepare_bemmodel(cfg,segmentedmri);
-any(isnan(vol1o.mat(:)))
-% vol1o.mat: all are NaN
-
-
+cfg = [];
+cfg.method = 'bemcp';
+vol3 = ft_prepare_headmodel(cfg,bnd);
+any(isnan(vol3.mat(:)))
+assert(~any(isnan(vol3.mat(:))), 'there is a NaN in vol3.mat');
