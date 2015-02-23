b1674d881bdf7e40798e19113281a7c2f1d29474
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2015-02-23 13:21:26 +0000

    enhancement - moved over code from fieldtrip/private into ft_checkdata: not functional yet
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10244 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- utilities/ft_checkdata.m
+++ utilities/ft_checkdata.m
@@ -1998,6 +1998,99 @@ for iUnit = 1:nUnits
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% convert between datatypes
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function timelock = freq2timelock(freq)
+
+% FREQ2TIMELOCK  transform the frequency data into something
+% on which the timelocked source reconstruction methods can
+% perform their trick.
+%
+% This function also performs frequency and channel selection, using
+%   cfg.frequency
+%   cfg.channel
+%
+% After source reconstruction, you should use TIMELOCK2FREQ.
+
+if isfield(freq, 'fourierspctrm')
+  fprintf('constructing real/imag data representation from single trial fourier representation\n');
+  % select the complex amplitude at the frequency of interest
+  cdim = dimnum(freq.dimord, 'chan');  % should be 2
+  fdim = dimnum(freq.dimord, 'freq');     % should be 3
+  fbin = nearest(freq.freq, cfg.frequency);
+  cfg.frequency = freq.freq(fbin);
+  if cdim==2 && fdim==3
+    % other dimords are not supported, since they do not occur
+    spctrm = dimindex(freq.fourierspctrm, fdim, fbin)';
+  end
+  % select the desired channels in the data
+  cfg.channel = ft_channelselection(cfg.channel, freq.label);
+  [dum, chansel] = match_str(cfg.channel, freq.label);
+  spctrm = spctrm(chansel,:);
+  % concatenate the real and imaginary part
+  avg = [real(spctrm) imag(spctrm)];
+elseif isfield(freq, 'crsspctrm')
+  fprintf('constructing real/imag data representation from csd matrix\n');
+  % hmmm... I have no idea whether this is correct
+  cfg.channel = ft_channelselection(cfg.channel, freq.label);
+  % this subfunction also takes care of the channel selection
+  [Cf, Cr, Pr, Ntrials, dum] = prepare_freq_matrices(cfg, freq);
+  cfg.frequency = dum.frequency;
+  cfg.channel   = dum.channel;
+  if length(size(Cf))==3
+    % average the cross-spectrum over trials
+    Cf = squeeze(mean(Cf,1));
+  end
+  % reconstruct something that spans the same space as the fft of the data, hmmm...
+  [u, s, v] = svd(Cf);
+  spctrm = u * sqrt(s);
+  % concatenate the real and imaginary part
+  avg = [real(spctrm) imag(spctrm)];
+else
+  error('unknown representation of frequency domain data');
+end
+
+timelock        = [];
+timelock.avg    = avg;
+timelock.label  = cfg.channel;
+timelock.time   = 1:size(timelock.avg,2);
+if isfield(freq, 'cfg'), timelock.cfg = freq.cfg; end
+timelock.dimord = 'chan_time';
+
+if isfield(freq, 'grad')
+  timelock.grad = freq.grad;
+end
+
+if isfield(freq, 'elec')
+  timelock.elec = freq.elec;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% convert between datatypes
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function timelock = comp2timelock(comp)
+
+% COMP2TIMELOCK transform the independent components into something
+% on which the timelocked source reconstruction methods can
+% perform their trick.
+
+% only convert, do not perform channel or component selection
+timelock        = [];
+timelock.avg    = comp.topo;
+timelock.label  = comp.topolabel;
+timelock.time   = 1:size(timelock.avg,2);
+if isfield(comp, 'cfg'), timelock.cfg = comp.cfg; end
+timelock.dimord = 'chan_time';
+
+if isfield(comp, 'grad')
+  timelock.grad = comp.grad;
+end
+
+if isfield(comp, 'elec')
+  timelock.elec = comp.elec;
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SUBFUNCTION for detection of channels
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [spikelabel, eeglabel] = detectspikechan(data)
