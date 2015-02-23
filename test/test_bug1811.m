a7785f61fc419fb698b8d92b4f68748c55a3a3da
Jim Herring <j.herring@donders.ru.nl>
2015-02-23 13:45:04 +0000

    bugfix-ft_analysprotocol removed and code updated to use ft_analysispipeline
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10246 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1811.m
+++ test/test_bug1811.m
@@ -4,7 +4,7 @@ function test_bug1811
 % WALLTIME 00:10:00
 
 % TEST test_bug1811
-% TEST ft_analysisprotocol
+% TEST ft_analysispipeline
 
 % loading meg + eyelink channels data appended with function_handle variable
 cd(dccnpath('/home/common/matlab/fieldtrip/data/test'))
@@ -12,7 +12,7 @@ load bug1811.mat
  
 cfg              =[];
 cfg.feedback     ='no';
-[script, details]=ft_analysisprotocol(cfg,meeg);
+[script, details]=ft_analysispipeline(cfg,meeg);
 
 if isempty(script) || isempty(details);
    error('script or details output are empty');
@@ -21,7 +21,7 @@ clear script details;
 
 %check ctf275 meg preoprocessed data
 load '/home/common/matlab/fieldtrip/data/test/latest/raw/meg/preproc_ctf275.mat'
-[script, details]=ft_analysisprotocol(cfg,data);
+[script, details]=ft_analysispipeline(cfg,data);
 
 if isempty(script) || isempty(details);
    error('script or details output are empty');
