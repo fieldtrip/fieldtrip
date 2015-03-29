e04549982512954fbe3ead34d56db4325d0e942f
Robert Oostenveld <r.oostenveld@gmail.com>
2015-03-29 20:58:31 +0000

    documentation - reformatted help, included proper copyright statement
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10307 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- specest/ft_specest_mtmfft.m
+++ specest/ft_specest_mtmfft.m
@@ -1,33 +1,47 @@
 function [spectrum,ntaper,freqoi] = ft_specest_mtmfft(dat, time, varargin)
 
 % FT_SPECEST_MTMFFT computes a fast Fourier transform using multitapering with
-% the DPSS sequence or using a variety of single tapers
+% multiple tapers from the DPSS sequence or using a variety of single tapers.
 %
 % Use as
 %   [spectrum,ntaper,freqoi] = ft_specest_mtmfft(dat,time...)
 % where
-%   dat      = matrix of chan*sample
-%   time     = vector, containing time in seconds for each sample
-%   spectrum = matrix of taper*chan*freqoi of fourier coefficients
-%   ntaper   = vector containing number of tapers per element of freqoi
-%   freqoi   = vector of frequencies in spectrum
+%   dat        = matrix of chan*sample
+%   time       = vector, containing time in seconds for each sample
+%   spectrum   = matrix of taper*chan*freqoi of fourier coefficients
+%   ntaper     = vector containing number of tapers per element of freqoi
+%   freqoi     = vector of frequencies in spectrum
 %
-% Optional arguments should be specified in key-value pairs and can include:
+% Optional arguments should be specified in key-value pairs and can include
 %   taper      = 'dpss', 'hanning' or many others, see WINDOW (default = 'dpss')
 %   pad        = number, total length of data after zero padding (in seconds)
 %   padtype    = string, indicating type of padding to be used (see ft_preproc_padding, default: zero)
 %   freqoi     = vector, containing frequencies of interest
 %   tapsmofrq  = the amount of spectral smoothing through multi-tapering. Note: 4 Hz smoothing means plus-minus 4 Hz, i.e. a 8 Hz smoothing box
 %   dimord     = 'tap_chan_freq' (default) or 'chan_time_freqtap' for memory efficiency (only when use variable number slepian tapers)
-%   polyorder  = number, the order of the polynomial to fitted to and removed from the data
-%                  prior to the fourier transform (default = 0 -> remove DC-component)
+%   polyorder  = number, the order of the polynomial to fitted to and removed from the data prior to the fourier transform (default = 0 -> remove DC-component)
 %   taperopt   = additional taper options to be used in the WINDOW function, see WINDOW
 %   verbose    = output progress to console (0 or 1, default 1)
-%
 %
 % See also FT_FREQANALYSIS, FT_SPECEST_MTMCONVOL, FT_SPECEST_TFR, FT_SPECEST_HILBERT, FT_SPECEST_WAVELET
 
 % Copyright (C) 2010, Donders Institute for Brain, Cognition and Behaviour
+%
+% This file is part of FieldTrip, see http://www.ru.nl/neuroimaging/fieldtrip
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
 %
 % $Id$
 
