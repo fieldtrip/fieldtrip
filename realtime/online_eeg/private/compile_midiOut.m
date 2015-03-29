ba5c701942e4083204a8eb0b56f056ada2d93c36
Robert Oostenveld <r.oostenveld@gmail.com>
2015-03-29 11:51:28 +0000

    restructuring - some updated to try to get it to compile on maci64, does not work yet
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10296 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- realtime/online_eeg/private/compile_midiOut.m
+++ realtime/online_eeg/private/compile_midiOut.m
@@ -4,8 +4,10 @@ elseif strcmp(computer, 'PCWIN64')
   mex midiOut.c -I../portmidi/pm_common libportmidi_win64.a C:\MinGW64\x86_64-w64-mingw32\lib\libwinmm.a
 elseif strcmp(computer, 'GLNX86')
   mex midiOut.c -I../portmidi/pm_common ../portmidi/libportmidi_linux32.a
-elseif trcmp(computer, 'GLNXA64')
+elseif strcmp(computer, 'GLNXA64')
   mex midiOut.c -I../portmidi/pm_common ../portmidi/libportmidi_linux64.a
+elseif strcmp(computer, 'MACI64')
+  mex midiOut.c -I/opt/local/include -L/opt/local/lib -lportmidi_s 
 else
   error('Unsupported platform');
 end
