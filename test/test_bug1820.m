df0eef3ddd970d624e9eafd4784bb48355186ff2
Robert Oostenveld <r.oostenveld@gmail.com>
2015-03-30 06:42:30 +0000

    enhancement - increased walltime for test scripts
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10309 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug1820.m
+++ test/test_bug1820.m
@@ -1,7 +1,7 @@
 function test_bug1820
 
 % MEM 12gb
-% WALLTIME 02:00:00
+% WALLTIME 02:30:00
 
 % TEST test_bug1820
 % TEST ft_prepare_mesh ft_headmodel_simbio ft_prepare_vol_sens ft_compute_leadfield
