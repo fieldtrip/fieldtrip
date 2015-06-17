41f10c4888e730ac0966dd534b7b715fa6dd03ea
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-06-17 13:00:03 +0200

    RF: use ft_platform_support to check matlab version

--- ft_platform_supports.m
+++ ft_platform_supports.m
@@ -1,4 +1,4 @@
-function tf=ft_platform_supports(what)
+function tf=ft_platform_supports(what,varargin)
 % return whether the current platform supports a specific capability
 
 if ~ischar(what)
@@ -6,12 +6,30 @@ if ~ischar(what)
 end
 
 switch what
-   
+    case 'matlabversion'
+        tf=is_matlab() && matlabversion(varargin{:});
+
+    case 'which-all'
+        tf=is_matlab();
+
     otherwise
         error('unsupported value for first argument: %s', what);
-    
+
 end % switch
 
-
 end % function
+
+function tf=environment_is_matlab()
+    tf=~environment_is_octave();
+end
+
+function tf=environment_is_octave()
+    persistent cached_tf;
+
+    if isempty(cached_tf)
+        cached_tf=logical(exist('OCTAVE_VERSION', 'builtin'));
+    end
+
+    tf=cached_tf;
+end
 
