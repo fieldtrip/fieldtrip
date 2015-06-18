2db0af0060cfc02e97cc438820c4403018a4b666
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-06-18 10:33:23 +0200

    MSC: make function name match file name

--- compat/matlablt2010b/@uint64/rdivide.m
+++ compat/matlablt2010b/@uint64/rdivide.m
@@ -1,6 +1,6 @@
-function [varargout] = funname(varargin)
+function [varargout] = rdivide(varargin)
 
-% FUNNAME compile the missing mex file on the fly
+% RDIVIDE compile the missing mex file on the fly
 
 % Copyright (C) 2012, Donders Centre for Cognitive Neuroimaging, Nijmegen, NL
 %
