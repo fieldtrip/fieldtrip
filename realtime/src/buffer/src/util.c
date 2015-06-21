40268e5aa14cf65dcc36b6d0f00c242fa190c61b
Robert Oostenveld <r.oostenveld@gmail.com>
2015-06-21 04:45:47 +0000

    enhancement and restructuring - added sample adjustment for incoming events that receive their sample number from the buffer server, this reduces the jitter due to the blocksize. See http://bugzilla.fieldtriptoolbox.org/show_bug.cgi?id=2872
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10463 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- realtime/src/buffer/src/util.c
+++ realtime/src/buffer/src/util.c
@@ -9,8 +9,12 @@
 #include <stdlib.h>
 #include <string.h>       /* for strerror */
 
-#include "buffer.h"
+#include <time.h>
+#include <sys/time.h>
+
 #include <pthread.h>
+
+#include "buffer.h"
 #include "extern.h"
 
 unsigned int bufread(int s, void *buf, unsigned int numel) {
@@ -308,9 +312,6 @@ error:
 }
 
 
-
-
-
 #ifdef WIN32
 int open_unix_connection(const char *name) {
 	return -1;
@@ -355,59 +356,3 @@ int open_unix_connection(const char *name) {
 }
 #endif
 
-
-
-#ifdef WIN32
-#ifndef COMPILER_MINGW
-
-/*
- * timeval.h    1.0 01/12/19
- *
- * Defines gettimeofday, timeval, etc. for Win32
- *
- * By Wu Yongwei
- *
- */
-
-#define EPOCHFILETIME ((INT64_T) 116444736000000000LL)
-
-#ifdef COMPILER_LCC
-VOID STDCALL GetSystemTimeAsFileTime(LPFILETIME);
-#endif
-
-int gettimeofday(struct timeval *tv, struct timezone *tz)
-{
-    FILETIME        ft;
-    LARGE_INTEGER   li;
-    INT64_T         t;
-    static int      tzflag;
-    
-    if (tv) {
-        GetSystemTimeAsFileTime(&ft);
-        li.LowPart  = ft.dwLowDateTime;
-        li.HighPart = ft.dwHighDateTime;
-        t  = li.QuadPart;       /* In 100-nanosecond intervals */
-        t -= EPOCHFILETIME;     /* Offset to the Epoch time */
-        t /= 10;                /* In microseconds */
-        tv->tv_sec  = (long)(t / 1000000);
-        tv->tv_usec = (long)(t % 1000000);
-    }
-    
-	#ifndef COMPILER_LCC
-	/* LCC that comes with Matlab has problems with _timezone and _daylight, 
-		and we don't need it anyway */
-    if (tz) {
-        if (!tzflag) {
-            _tzset();
-            tzflag++;
-        }
-        tz->tz_minuteswest = _timezone / 60;
-        tz->tz_dsttime = _daylight;
-    }
-	#endif
-
-    return 0;
-}
-
-#endif
-#endif
