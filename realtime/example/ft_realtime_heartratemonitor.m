7e6c7b87e483a07dc756d2b0fa360a14d8b643a0
Robert Oostenveld <r.oostenveld@gmail.com>
2015-08-16 11:05:18 +0000

    enhancement - use optimal single-beat delay in giving auditory user feedback, use blocking reads for data, some speedups to figures
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10594 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- realtime/example/ft_realtime_heartratemonitor.m
+++ realtime/example/ft_realtime_heartratemonitor.m
@@ -8,6 +8,7 @@ function ft_realtime_heartratemonitor(cfg)
 % with the following configuration options
 %   cfg.blocksize  = number, size of the blocks/chuncks that are processed (default = 1 second)
 %   cfg.channel    = cell-array, see FT_CHANNELSELECTION (default = 'all')
+%   cfg.jumptoeof  = whether to skip to the end of the stream/file at startup (default = 'yes')
 %   cfg.bufferdata = whether to start on the 'first or 'last' data that is available (default = 'last')
 %
 % The source of the data is configured as
@@ -88,10 +89,11 @@ else
 end
 
 prevState = [];
-pad = [];
+pad   = [];
+pads  = [];
 count = 0;
 
-ws_noPeaks = warning('off', 'signal:findpeaks:noPeaks');
+ws_noPeaks    = warning('off', 'signal:findpeaks:noPeaks');
 ws_PeakHeight = warning('off', 'signal:findpeaks:largeMinPeakHeight');
 
 % start the timer
@@ -101,119 +103,147 @@ n0 = 0;
 t1 = t0;
 n1 = n0;
 
-% these are for the feedback
-beat = [];
-beep = sin(1000*2*pi*(1:800)/8192);
-close all
-h1 = figure
-set(h1, 'Position', [010 300 560 420]);
-h2 = figure
-set(h2, 'Position', [580 300 560 420]);
+% this will keep the heartrate history
+heartrate = [];
 
+% these are for the feedback
+close all
+
+h1f = figure;
+plot(nan);
+h1a = get(h1f, 'children');
+h1c = get(h1a, 'children');
+set(h1f, 'Position', [010 300 560 420]);
+xlabel('time (s)');
+ylim([-6 6]);
+
+h2f = figure;
+plot(nan, '.');
+h2a = get(h2f, 'children');
+h2c = get(h2a, 'children');
+set(h2f, 'Position', [580 300 560 420]);
+title('heartrate');
+xlabel('time (s)');
+ylabel('beats per minute');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % this is the general BCI loop where realtime incoming data is handled
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 while true
   
-  % determine number of samples available in buffer
-  hdr = ft_read_header(cfg.headerfile, 'headerformat', cfg.headerformat, 'cache', true);
+  % determine the samples to process
+  if strcmp(cfg.bufferdata, 'last')
+    % determine number of samples available in buffer
+    hdr = ft_read_header(cfg.headerfile, 'headerformat', cfg.headerformat, 'cache', true);
+    begsample  = hdr.nSamples*hdr.nTrials - blocksize + 1;
+    endsample  = hdr.nSamples*hdr.nTrials;
+  elseif strcmp(cfg.bufferdata, 'first')
+    begsample  = prevSample+1;
+    endsample  = prevSample+blocksize ;
+  else
+    error('unsupported value for cfg.bufferdata');
+  end
   
-  % see whether new samples are available
-  newsamples = (hdr.nSamples*hdr.nTrials-prevSample);
+  % this allows overlapping data segments
+  if overlap && (begsample>overlap)
+    begsample = begsample - overlap;
+    endsample = endsample - overlap;
+  end
   
-  if newsamples>=blocksize
-    
-    % determine the samples to process
-    if strcmp(cfg.bufferdata, 'last')
-      begsample  = hdr.nSamples*hdr.nTrials - blocksize + 1;
-      endsample  = hdr.nSamples*hdr.nTrials;
-    elseif strcmp(cfg.bufferdata, 'first')
-      begsample  = prevSample+1;
-      endsample  = prevSample+blocksize ;
-    else
-      error('unsupported value for cfg.bufferdata');
-    end
-    
-    % this allows overlapping data segments
-    if overlap && (begsample>overlap)
-      begsample = begsample - overlap;
-      endsample = endsample - overlap;
-    end
-    
-    % remember up to where the data was read
-    prevSample  = endsample;
-    count       = count + 1;
-    fprintf('processing segment %d from sample %d to %d\n', count, begsample, endsample);
-    
-    % read data segment from buffer
-    dat = ft_read_data(cfg.datafile, 'header', hdr, 'dataformat', cfg.dataformat, 'begsample', begsample, 'endsample', endsample, 'chanindx', chanindx, 'checkboundary', false);
-    
-    % keep track of the timing
-    t1 = toc;
-    n1 = n1 + size(dat,2);
-    fprintf('read %d samples in %f seconds, realtime ratio = %f\n', n1-n0, t1-t0, ((n1-n0)/(t1-t0))/hdr.Fs);
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % from here onward it is specific to the display of the data
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    time   = offset2time(begsample, hdr.Fs, endsample-begsample+1);
-    sample = begsample:endsample;
-    label  = hdr.label(chanindx);
-    
-    % apply some preprocessing options
-    [dat, prevState] = ft_preproc_standardize(dat, [], [], prevState);
-    
-    if isempty(pad)
-      % this only applies to the first segment being processed
-      pad = dat(:,1);
-    else
-      % remember the last sample for padding the next segment
-      pad = dat(:,end);
-    end
-    
-    dat    = [pad dat];
-    sample = [sample(1)-1 sample];
-    time   = [time(1)-1/hdr.Fs time];
-    
-    if true
-      figure(h1)
-      % plot the data
-      plot(time, dat);
-      xlim([time(1) time(end)]);
-      ylim([-6 6]);
-    end
-    
-    if cfg.threshold<0
-      % detect negative peaks
-      [peakval, peakind] = findpeaks(-dat, 'minpeakheight', -cfg.threshold);
-      peakval = -peakval;
-    else
-      [peakval, peakind] = findpeaks(dat, 'minpeakheight', cfg.threshold);
-    end
-    % the last sample should not be detected as peak but should be carried on to the next segment as padding
-    sel = (peakind==size(dat,2));
-    peakval(sel) = [];
-    peakind(sel) = [];
-    
-    for i=1:length(peakval)
-      % make a sound for each heart beat
-      soundsc(beep);
-      % FIXME the beat vector growing is a bad idea
-      beat(end+1) = time(peakind(i));
-      figure(h2)
-      plot(beat(2:end), diff(beat)*60, 'b.');
-      title('heartrate');
-      xlabel('time (s)');
-      ylabel('beats per minute');
-    end
-    
-    % force Matlab to update the figure
-    drawnow
-    
-  end % if enough new samples
+  % remember up to where the data was read
+  prevSample  = endsample;
+  count       = count + 1;
+  % fprintf('processing segment %d from sample %d to %d\n', count, begsample, endsample);
+  
+  % read data segment from buffer
+  dat = ft_read_data(cfg.datafile, 'header', hdr, 'dataformat', cfg.dataformat, 'begsample', begsample, 'endsample', endsample, 'chanindx', chanindx, 'checkboundary', false, 'blocking', true);
+  
+  % fprintf('time between subsequent reads is %f seconds\n', toc-t1);
+  
+  % keep track of the timing
+  t1 = toc;
+  n1 = n1 + size(dat,2);
+  
+  % fprintf('read %d samples in %f seconds, realtime ratio = %f\n', n1-n0, t1-t0, ((n1-n0)/(t1-t0))/hdr.Fs);
+  % fprintf('time lag %6.3f seconds\n', (n1-n0)/hdr.Fs - (t1-t0));
+  % fprintf('estimated sampling rate %.2f Hz\n', (n1-n0)/(t1-t0));
+  
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % from here onward it is specific to the display of the data
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+  time   = offset2time(begsample, hdr.Fs, endsample-begsample+1);
+  sample = begsample:endsample;
+  
+  % apply some preprocessing options
+  [dat, prevState] = ft_preproc_standardize(dat, [], [], prevState);
+  
+  if isempty(pad)
+    % this only applies to the first segment being processed
+  else
+    dat    = [pad  dat];
+    sample = [pads sample];
+    time   = sample./hdr.Fs;
+  end
+  
+  % remember the last few samples, to be used for padding the next segment
+  pad  = dat(:,[end-1 end-0]);
+  pads = sample([end-1 end-0]);
+  
+  if cfg.threshold<0
+    % detect negative peaks
+    [peakval, peakind] = findpeaks(-dat, 'minpeakheight', -cfg.threshold);
+    peakval = -peakval;
+  else
+    % detect positive peaks
+    [peakval, peakind] = findpeaks(dat, 'minpeakheight', cfg.threshold);
+  end
+  
+  for i=1:length(peakval)
+    % a heartbeat that is detected at the start should be played back immediately
+    % a heartbeat that is detected at the end should be delayed a bit
+    delay = (length(dat)-peakind(i))/hdr.Fs;
+    delay = round(1000*delay)/1000; % only milisecond precision
+    feedback_timer = timer('TimerFcn', @feedback_beep, 'ExecutionMode', 'singleShot', 'StartDelay', delay);
+    start(feedback_timer);
+  end
+  
+  % FIXME having the heartrate vector growing is not a very good idea
+  heartrate = [heartrate time(peakind)];
+  
+  if ishandle(h1f)
+    set(h1c, 'xdata', time, 'ydata', dat);
+    set(h1a, 'xlim', time([1 end]));
+  end
+  
+  if numel(heartrate)>2 && ishandle(h2f)
+    set(h2c, 'xdata', heartrate(2:end), 'ydata', diff(heartrate)*60);
+    set(h2a, 'xlim', heartrate([2 end]));
+    set(h2a, 'ylim', [0 max(diff(heartrate)*90)]);
+  end
+  
+  % force Matlab to redraw the figures
+  drawnow
+  
 end % while true
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION that gives a beep as feedback
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function feedback_beep(varargin)
+persistent beep time
+if isempty(beep)
+  beep = sin(1000*2*pi*(1:800)/8192);
+end
+if isempty(time)
+  time = toc;
+end
+disp(toc-time);
+soundsc(beep);
+time = toc;
+% delete the timer object that called this function
+stop(varargin{1});
+delete(varargin{1});
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SUBFUNCTION
