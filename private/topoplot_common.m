221a7e85d60821e525c48d4cd0b66c163802c99c
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2015-09-14 07:53:27 +0000

    enhancement - allow for variable size colormaps in combination with saturation masking, bug 2876
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10666 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- private/topoplot_common.m
+++ private/topoplot_common.m
@@ -200,7 +200,10 @@ end
 if isfield(cfg,'colormap')
   if size(cfg.colormap,2)~=3, error('topoplot(): Colormap must be a n x 3 matrix'); end
   colormap(cfg.colormap);
-end;
+  ncolors = size(cfg.colormap,1);
+else
+  ncolors =[]; % let the low-level function deal with this
+end
 
 dtype  = ft_datatype(data);
 
@@ -764,7 +767,7 @@ if ~strcmp(cfg.style,'blank')
     'datmask', msk};
   if strcmp(style,'imsat') || strcmp(style,'imsatiso')
     % add clim to opt
-    opt = [opt {'clim',[zmin zmax]}];
+    opt = [opt {'clim',[zmin zmax],'ncolors',ncolors}];
   end
   ft_plot_topo(chanX,chanY,dat,opt{:});
 elseif ~strcmp(cfg.style,'blank')
