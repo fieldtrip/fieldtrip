f658655fc142dde727102a522015b9e736473d74
Robert Oostenveld <r.oostenveld@gmail.com>
2015-09-29 11:00:10 +0000

    automatically synchronized identical files to revision 10715
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10716 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/private/ft_datatype_volume.m
+++ plotting/private/ft_datatype_volume.m
@@ -122,16 +122,18 @@ switch version
       % move the average fields to the main structure
       fn = fieldnames(volume.avg);
       for i=1:length(fn)
-        dat = volume.avg.(fn{i});
-        try
-          volume.(fn{i}) = reshape(dat, volume.dim);
-        catch
-          warning('could not reshape %s to expected dimensions');
-          volume.(fn{i}) = dat;
-        end
-        clear dat
+        volume.(fn{i}) = volume.avg.(fn{i});
       end
       volume = rmfield(volume, 'avg');
+    end
+    
+    fn = getdatfield(volume);
+    for i=1:numel(fn)
+      try
+        volume.(fn{i}) = reshape(volume.(fn{i}), volume.dim);
+      catch
+        warning('could not reshape %s to the expected dimensions', fn{i});
+      end
     end
     
     % ensure that it is always logical
