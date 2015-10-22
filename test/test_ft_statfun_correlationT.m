eb6e4b919cdd58e0b467a87b093c64dc67663a9e
Arjen Stolk <a.stolk@donders.ru.nl>
2015-10-22 03:47:50 +0000

    enhancement and documentation change: added warning when using permutation resampling method
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10815 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_ft_statfun_correlationT.m
+++ test/test_ft_statfun_correlationT.m
@@ -1,20 +1,23 @@
 function test_ft_statfun_correlationT
 
+% A. Stolk, oct 2015
+
 % simulate simple single-subject timelock structures
 data_brain = [];
-data_brain.trial = [1:10]'; % increasing 
+data_brain.trial = rand(10,1); % increasing
 data_brain.dimord = 'rpt_chan_time';
 data_brain.time = 1;
 data_brain.label = {'1'};
 
 data_behav = data_brain;
-data_behav.trial = data_brain.trial*-1000+50; % add scaling difference
+data_behav.trial = rand(10,1)+1000; % add offset (which should be not allowed to bias the results)
 
 % compute statistics with correlationT
 cfg = [];
-cfg.method           = 'montecarlo';
 cfg.statistic        = 'ft_statfun_correlationT';
-cfg.numrandomization = 100;
+cfg.method           = 'montecarlo';
+cfg.resampling       = 'bootstrap'; % this parameter is crucial, the default (permutation) is prone to systematic across-condition bias
+cfg.numrandomization = 1000;
 
 n1 = 10;    % n1 is the number of trials
 design              = zeros(2, n1 * 2);
@@ -25,29 +28,27 @@ cfg.design           = design;
 
 cfg.ivar             = 1;
 cfg.uvar             = 2;
-stat = ft_timelockstatistics(cfg, data_brain, data_behav);
-
-assert(isequal(stat.rho, -1));
-
+stat1 = ft_timelockstatistics(cfg, data_brain, data_behav);
 
 % simulate simple multiple subjects timelock structures
 data_brain = [];
 data_behav = [];
 for j=1:10
-data_brain{j}.avg = j; % increasing 
-data_brain{j}.dimord = 'chan_time';
-data_brain{j}.time = 1;
-data_brain{j}.label = {'1'};
-
-data_behav{j} = data_brain{j};
-data_behav{j}.avg = data_brain{j}.avg*-1000+50; % add scaling difference
+  data_brain{j}.avg = rand; % increasing
+  data_brain{j}.dimord = 'chan_time';
+  data_brain{j}.time = 1;
+  data_brain{j}.label = {'1'};
+  
+  data_behav{j} = data_brain{j};
+  data_behav{j}.avg = rand+1000; % add scaling difference
 end
 
 % compute statistics with correlationT
 cfg = [];
-cfg.method           = 'montecarlo';
 cfg.statistic        = 'ft_statfun_correlationT';
-cfg.numrandomization = 100;
+cfg.method           = 'montecarlo';
+cfg.resampling       = 'bootstrap'; % this parameter is crucial, the default (permutation) is prone to systematic across-condition bias
+cfg.numrandomization = 1000;
 
 n1 = 10;    % n1 is the number of subjects
 design              = zeros(2, n1 * 2);
@@ -58,6 +59,6 @@ cfg.design           = design;
 
 cfg.ivar             = 1;
 cfg.uvar             = 2;
-stat = ft_timelockstatistics(cfg, data_brain{:}, data_behav{:});
+stat2 = ft_timelockstatistics(cfg, data_brain{:}, data_behav{:});
 
-assert(isequal(stat.rho, -1));
+assert(~stat1.prob<0.05 && ~stat2.prob<0.05); % probability-wise, this should fail once every millenium :)
