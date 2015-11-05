12768ef5d0e82b415bdf7611330ba5a89a8d831c
Roemer van der Meij <r.vandermeij@donders.ru.nl>
2015-11-05 23:36:54 +0000

    added some more plot variations for testing
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@10848 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- plotting/test/test_ft_plot_matrix.m
+++ plotting/test/test_ft_plot_matrix.m
@@ -6,7 +6,20 @@ function test_ft_plot_matrix
 % TEST test_ft_plot_matrix
 % TEST ft_plot_matrix
 
-dat = randn(30,50);
+dat = rand(30,50);
+% make first and last couple of columns NaN
+dat(:,[1 2 3 48 49 50]) = NaN;
 
+% basic
 figure
-ft_plot_matrix(dat);
+ft_plot_matrix(dat,'clim', [0 1]);
+
+
+% hightlightstyle opacity
+figure
+ft_plot_matrix(dat,'clim',[0 1],'highlightstyle','opacity','highlight',dat>0.5)
+
+
+% hightlightstyle saturation
+figure
+ft_plot_matrix(dat,'clim',[0 1],'highlightstyle','saturation','highlight',dat>0.5)
