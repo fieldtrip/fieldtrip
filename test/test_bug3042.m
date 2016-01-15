eea6e8cf519bd28829783582daedda55188f4804
Robert Oostenveld <r.oostenveld@gmail.com>
2016-01-15 11:32:41 +0000

    ENH added call to ft_sourceparcellate as example, see bug 3042
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@11063 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- test/test_bug3042.m
+++ test/test_bug3042.m
@@ -9,18 +9,26 @@ function test_bug3042
 filename = dccnpath('/home/common/matlab/fieldtrip/data/test/bug3042/tess_cortex_pial_low.mat');
 
 %%
+
 original = load(filename);
 
 %%
+
 sourcemodel = ft_read_headshape(filename);
 figure
 ft_plot_mesh(sourcemodel, 'vertexcolor', sourcemodel.curv)
 
 %%
-parcellation = ft_read_atlas(filename);
+
+atlas = ft_read_atlas(filename);
 figure
-ft_plot_mesh(parcellation, 'vertexcolor', parcellation.desikan_killiany);
+ft_plot_mesh(atlas, 'vertexcolor', atlas.desikan_killiany);
 colormap colorcube
 
+%%
+
+cfg = [];
+cfg.parcellation = 'destrieux';
+parcels = ft_sourceparcellate(cfg, sourcemodel, atlas);
 
 
