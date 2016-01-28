c011e5b31bcbe4400a6850b02fbee783a80d05db
Robert Oostenveld <r.oostenveld@gmail.com>
2016-01-28 08:47:37 +0000

    ENH - use bash instead of sh
    git-svn-id: svn+ssh://svn.fcdonders.nl/home/svnroot/fieldtrip/trunk@11146 0cf7c7f0-3615-4144-b4e6-68da3bce3cd0

--- bin/synchronize-private.sh
+++ bin/synchronize-private.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ################################################################################
 #
 # This Bash script synchronizes multiple copies of the same file in the repository.
