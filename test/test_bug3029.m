b1d0cf5d1d2acdee60e7fb7ac7a6f992b84c66c6
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2016-02-15 11:04:18 +0100

    bugfix - fix to allow for linearly indexed crsspctrm, was broken

--- test/test_bug3029.m
+++ test/test_bug3029.m
@@ -84,6 +84,7 @@ end
 
 % Tthe following cfg should give an error due to the mismatch in
 % channels in data and leadfield
+if 0,
 try,
 	cfg           = [];
 	cfg.method    = 'dics';
@@ -99,7 +100,9 @@ catch
 		error('there''s a problem');
 	end
 end
+end
 
+if 0,
 % check whether it also works with pcc as a method
 cfg           = [];
 cfg.method    = 'dics';
@@ -122,5 +125,101 @@ cfg.refchan   = 'BR1';
 s3b           = ft_sourceanalysis(cfg, freq);
 s3c           = ft_sourcedescriptives([], s3b);
 assert(identical(s3.avg.coh, s3c.avg.coh, 'reltol', 1e-8));
+end
 
 % ALSO: check whether it also works with powandcsd data
+filename = fullfile(dccnpath('/home/common/matlab/fieldtrip/data/test/latest/freq/meg'), 'freq_mtmfft_powandcsd_trl_ctf275');
+load(filename);
+
+if 1,
+cfg           = [];
+cfg.method    = 'dics';
+cfg.frequency = 10;
+cfg.headmodel = vol;
+cfg.grid      = lf;
+cfg.channel   = 'MEG'; % this is needed, otherwise there's a detected mismatch in channels vs leadfields
+s1            = ft_sourceanalysis(cfg, freq);
+end
+
+% the following runs through irrespective of the bug being fixed, yet the
+% behavior might be unexpected: no single trial output is generated.
+if 0,
+cfg           = [];
+cfg.method    = 'dics';
+cfg.frequency = 10;
+cfg.headmodel = vol;
+cfg.grid      = lf;
+cfg.channel   = 'MEG'; % this is needed, otherwise there's a detected mismatch in channels vs leadfields
+cfg.keeptrials = 'yes';
+s2             = ft_sourceanalysis(cfg, freq);
+end
+
+if 0,
+% with the bug unfixed, this gives a problem with the refchan handling, due
+% to ft_selectdata only keeping the MEG channels from an early stage
+cfg           = [];
+cfg.method    = 'dics';
+cfg.frequency = 10;
+cfg.headmodel = vol;
+cfg.grid      = lf;
+cfg.channel   = 'MEG'; % this is needed, otherwise there's a detected mismatch in channels vs leadfields
+cfg.refchan   = 'BR1';
+s3             = ft_sourceanalysis(cfg, freq);
+
+cfg           = [];
+cfg.method    = 'dics';
+cfg.frequency = 10;
+cfg.headmodel = vol;
+cfg.grid      = lf;
+cfg.channel   = 'MEG'; % this is needed, otherwise there's a detected mismatch in channels vs leadfields
+cfg.refchan   = 'BR1';
+cfg.keeptrials = 'yes';
+s4             = ft_sourceanalysis(cfg, freq);
+
+assert(identical(s3.avg.coh, s4.avg.coh, 'reltol', 1e-8));
+end
+
+% Tthe following cfg should give an error due to the mismatch in
+% channels in data and leadfield
+if 0,
+try,
+	cfg           = [];
+	cfg.method    = 'dics';
+	cfg.frequency = 10;
+	cfg.headmodel = vol;
+	cfg.grid      = lf;
+	s5            = ft_sourceanalysis(cfg, freq);
+catch
+	% this should be solved with proper error handling
+	if ~isempty(strfind(lasterr,'There''s a mismatch'))
+		% this is OK
+	else
+		error('there''s a problem');
+	end
+end
+end
+
+% check whether it also works with pcc as a method
+if 0,
+cfg           = [];
+cfg.method    = 'dics';
+cfg.frequency = 10;
+cfg.headmodel = vol;
+cfg.grid      = lf;
+cfg.channel   = 'MEG'; % this is needed, otherwise there's a detected mismatch in channels vs leadfields
+cfg.refchan   = 'BR1';
+cfg.dics.keepcsd = 'yes';
+cfg.dics.realfilter = 'yes';
+s3             = ft_sourceanalysis(cfg, freq);
+
+cfg           = [];
+cfg.method    = 'pcc';
+cfg.frequency = 10;
+cfg.headmodel = vol;
+cfg.grid      = lf;
+cfg.channel   = 'MEG'; % this is needed, otherwise there's a detected mismatch in channels vs leadfields
+cfg.refchan   = 'BR1';
+s3b           = ft_sourceanalysis(cfg, freq);
+s3c           = ft_sourcedescriptives([], s3b);
+assert(identical(s3.avg.coh, s3c.avg.coh, 'reltol', 1e-8));
+end
