bfc6741ade28ad2f30236fabf45b596a8d276669
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2016-02-23 11:18:04 +0100

    ENH - ensure datachannel to be a column vector to avoid unexpected behaviour

--- contrib/spike/ft_spike_xcorr.m
+++ contrib/spike/ft_spike_xcorr.m
@@ -128,7 +128,7 @@ cfg = ft_checkconfig(cfg, 'allowed', {'latency', 'trials', 'keeptrials', 'method
 doShiftPredictor  = strcmp(cfg.method, 'shiftpredictor'); % shift predictor
 
 % determine the corresponding indices of the requested channel combinations
-cfg.channelcmb = ft_channelcombination(cfg.channelcmb, spike.label,true);
+cfg.channelcmb = ft_channelcombination(cfg.channelcmb, spike.label(:), true);
 cmbindx        = zeros(size(cfg.channelcmb));
 for k=1:size(cfg.channelcmb,1)
   cmbindx(k,1) = strmatch(cfg.channelcmb(k,1), spike.label, 'exact');
