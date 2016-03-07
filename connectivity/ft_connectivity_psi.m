ae274554b7f13d7ec1a62e29ce06e056cd9c73c9
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2016-03-07 10:24:34 +0100

    updated some user feedback plus indentation of documentation

--- connectivity/ft_connectivity_psi.m
+++ connectivity/ft_connectivity_psi.m
@@ -28,8 +28,8 @@ function [c, v, n] = ft_connectivity_psi(input, varargin)
 %   feedback	= 'none', 'text', 'textbar' type of feedback showing progress of
 %               computation
 %   dimord		= string, specifying how the input matrix should be interpreted
-%   powindx 
-%   normalize 
+%   powindx
+%   normalize
 %
 % The output p contains the phase slope index, v is a variance estimate
 % which only can be computed if the data contains leave-one-out samples,
@@ -70,76 +70,76 @@ normalize = ft_getopt(varargin, 'normalize', 'no');
 nbin      = ft_getopt(varargin, 'nbin');
 
 if isempty(dimord)
-  error('input parameters should contain a dimord');
+	error('input parameters should contain a dimord');
 end
 
 if (length(strfind(dimord, 'chan'))~=2 || ~isempty(strfind(dimord, 'pos'))>0) && ~isempty(powindx),
-  %crossterms are not described with chan_chan_therest, but are linearly indexed
-  
-  siz = size(input);
-  
-  outsum = zeros(siz(2:end));
-  outssq = zeros(siz(2:end));
-  pvec   = [2 setdiff(1:numel(siz),2)];
-  
-  ft_progress('init', feedback, 'computing metric...');
-  %first compute coherency and then phaseslopeindex
-  for j = 1:siz(1)
-    ft_progress(j/siz(1), 'computing metric for replicate %d from %d\n', j, siz(1));
-    c      = reshape(input(j,:,:,:,:), siz(2:end));
-    p1     = abs(reshape(input(j,powindx(:,1),:,:,:), siz(2:end)));
-    p2     = abs(reshape(input(j,powindx(:,2),:,:,:), siz(2:end)));
-    
-    p      = ipermute(phaseslope(permute(c./sqrt(p1.*p2), pvec), nbin, normalize), pvec);
-    
-    outsum = outsum + p;
-    outssq = outssq + p.^2;
-  end
-  ft_progress('close');
-  
+	%crossterms are not described with chan_chan_therest, but are linearly indexed
+	
+	siz = size(input);
+	
+	outsum = zeros(siz(2:end));
+	outssq = zeros(siz(2:end));
+	pvec   = [2 setdiff(1:numel(siz),2)];
+	
+	ft_progress('init', feedback, 'computing metric...');
+	%first compute coherency and then phaseslopeindex
+	for j = 1:siz(1)
+		ft_progress(j/siz(1), 'computing metric for replicate %d from %d\n', j, siz(1));
+		c      = reshape(input(j,:,:,:,:), siz(2:end));
+		p1     = abs(reshape(input(j,powindx(:,1),:,:,:), siz(2:end)));
+		p2     = abs(reshape(input(j,powindx(:,2),:,:,:), siz(2:end)));
+		
+		p      = ipermute(phaseslope(permute(c./sqrt(p1.*p2), pvec), nbin, normalize), pvec);
+		
+		outsum = outsum + p;
+		outssq = outssq + p.^2;
+	end
+	ft_progress('close');
+	
 elseif length(strfind(dimord, 'chan'))==2 || length(strfind(dimord, 'pos'))==2,
-  %crossterms are described by chan_chan_therest
-  
-  siz = size(input);
-  
-  outsum = zeros(siz(2:end));
-  outssq = zeros(siz(2:end));
-  pvec   = [3 setdiff(1:numel(siz),3)];
-  
-  ft_progress('init', feedback, 'computing metric...');
-  for j = 1:siz(1)
-    ft_progress(j/siz(1), 'computing metric for replicate %d from %d\n', j, siz(1));
-    p1  = zeros([siz(2) 1 siz(4:end)]);
-    p2  = zeros([1 siz(3) siz(4:end)]);
-    for k = 1:siz(2)
-      p1(k,1,:,:,:,:) = input(j,k,k,:,:,:,:);
-      p2(1,k,:,:,:,:) = input(j,k,k,:,:,:,:);
-    end
-    c      = reshape(input(j,:,:,:,:,:,:), siz(2:end));
-    p1     = p1(:,ones(1,siz(3)),:,:,:,:);
-    p2     = p2(ones(1,siz(2)),:,:,:,:,:);
-    p      = ipermute(phaseslope(permute(c./sqrt(p1.*p2), pvec), nbin, normalize), pvec);
-    p(isnan(p)) = 0;
-    outsum = outsum + p;    
-    outssq = outssq + p.^2;
-  end
-  ft_progress('close');
-  
+	%crossterms are described by chan_chan_therest
+	
+	siz = size(input);
+	
+	outsum = zeros(siz(2:end));
+	outssq = zeros(siz(2:end));
+	pvec   = [3 setdiff(1:numel(siz),3)];
+	
+	ft_progress('init', feedback, 'computing metric...');
+	for j = 1:siz(1)
+		ft_progress(j/siz(1), 'computing metric for replicate %d from %d\n', j, siz(1));
+		p1  = zeros([siz(2) 1 siz(4:end)]);
+		p2  = zeros([1 siz(3) siz(4:end)]);
+		for k = 1:siz(2)
+			p1(k,1,:,:,:,:) = input(j,k,k,:,:,:,:);
+			p2(1,k,:,:,:,:) = input(j,k,k,:,:,:,:);
+		end
+		c      = reshape(input(j,:,:,:,:,:,:), siz(2:end));
+		p1     = p1(:,ones(1,siz(3)),:,:,:,:);
+		p2     = p2(ones(1,siz(2)),:,:,:,:,:);
+		p      = ipermute(phaseslope(permute(c./sqrt(p1.*p2), pvec), nbin, normalize), pvec);
+		p(isnan(p)) = 0;
+		outsum = outsum + p;
+		outssq = outssq + p.^2;
+	end
+	ft_progress('close');
+	
 end
 
 n = siz(1);
 c = outsum./n;
 
 if n>1,
-  n = shiftdim(sum(~isnan(input),1),1);    
-  if hasjack
-    bias = (n-1).^2;
-  else
-    bias = 1;
-  end
-  v = bias.*(outssq - (outsum.^2)./n)./(n - 1);
+	n = shiftdim(sum(~isnan(input),1),1);
+	if hasjack
+		bias = (n-1).^2;
+	else
+		bias = 1;
+	end
+	v = bias.*(outssq - (outsum.^2)./n)./(n - 1);
 else
-  v = [];
+	v = [];
 end
 
 %---------------------------------------
@@ -150,18 +150,18 @@ y   = zeros(size(x));
 x(1:end-1,:,:,:,:) = conj(x(1:end-1,:,:,:,:)).*x(2:end,:,:,:,:);
 
 if strcmp(norm, 'yes')
-  coh = zeros(size(x));
-  coh(1:end-1,:,:,:,:) = (abs(x(1:end-1,:,:,:,:)) .* abs(x(2:end,:,:,:,:))) + 1;
-  %FIXME why the +1? get the coherence
-  for k = 1:m
-    begindx = max(1,k-n);
-    endindx = min(m,k+n);
-    y(k,:,:,:,:) = imag(nansum(x(begindx:endindx,:,:,:,:)./coh(begindx:endindx,:,:,:,:),1));
-  end
+	coh = zeros(size(x));
+	coh(1:end-1,:,:,:,:) = (abs(x(1:end-1,:,:,:,:)) .* abs(x(2:end,:,:,:,:))) + 1;
+	%FIXME why the +1? get the coherence
+	for k = 1:m
+		begindx = max(1,k-n);
+		endindx = min(m,k+n);
+		y(k,:,:,:,:) = imag(nansum(x(begindx:endindx,:,:,:,:)./coh(begindx:endindx,:,:,:,:),1));
+	end
 else
-  for k = 1:m
-    begindx = max(1,k-n);
-    endindx = min(m,k+n);
-    y(k,:,:,:,:) = imag(nansum(x(begindx:endindx,:,:,:,:),1));
-  end
+	for k = 1:m
+		begindx = max(1,k-n);
+		endindx = min(m,k+n);
+		y(k,:,:,:,:) = imag(nansum(x(begindx:endindx,:,:,:,:),1));
+	end
 end
