6ed155526457c825bd0f63d4540bdd61b07fde22
Sarang S. Dalal <sarang@cfin.au.dk>
2016-03-30 00:19:24 +0200

    preserve vectors of Elekta planar gradiometers
    added "complex" output option to ft_combineplanar, which combines the
    original planar gradiometer values into a complex value (first
    gradiometer goes into the real component, second gradiometer goes into
    the imaginary component).
    
    Support for vectors implemented as such complex values was added to
    topoplot_common.m, with the new cfg.vector input.

--- ft_combineplanar.m
+++ ft_combineplanar.m
@@ -7,7 +7,7 @@ function [data] = ft_combineplanar(cfg, data)
 %
 % Use as
 %   [data] = ft_combineplanar(cfg, data)
-% where data contains an averaged planar gradient ERF or single-trial/avegared TFR.
+% where data contains an averaged planar gradient ERF or single-trial/averaged TFR.
 %
 % The configuration can contain
 %   cfg.method         = 'sum', 'svd' or 'abssvd' (default = 'sum')
@@ -222,6 +222,15 @@ elseif (israw || istimelock)
       Nrpt = length(data.trial);
       for k = 1:Nrpt
         combined = sqrt(data.trial{k}(sel_dH,:).^2 + data.trial{k}(sel_dV,:).^2);
+        other    = data.trial{k}(sel_other,:);
+        data.trial{k} = [combined; other];
+      end
+      data.label = cat(1, lab_comb(:), lab_other(:));
+
+    case 'complex'
+      Nrpt = length(data.trial);
+      for k = 1:Nrpt
+        combined = data.trial{1}(sel_dH,:)*i + data.trial{1}(sel_dV,:);
         other    = data.trial{k}(sel_other,:);
         data.trial{k} = [combined; other];
       end
