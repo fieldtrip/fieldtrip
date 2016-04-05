0daee717f3edbb10ef2d1fee0fc09942f78be2e1
Sarang S. Dalal <sarang@cfin.au.dk>
2016-04-05 16:56:52 +0200

    beamformer_lcmv: added NAI option and associated orientation selection
    beamformer_lcmv: added NAI option and associated orientation selection
    (from Sekihara & Nagarajan 2008, eqn 4.47)

--- inverse/beamformer_lcmv.m
+++ inverse/beamformer_lcmv.m
@@ -86,6 +86,7 @@ projectnoise   = keyval('projectnoise',  varargin); if isempty(projectnoise),  p
 projectmom     = keyval('projectmom',    varargin); if isempty(projectmom),    projectmom = 'no';            end
 fixedori       = keyval('fixedori',      varargin); if isempty(fixedori),      fixedori = 'no';              end
 computekurt    = keyval('kurtosis',      varargin); if isempty(computekurt),   computekurt = 'no';           end
+nai     = keyval('nai',    varargin); if isempty(nai),      nai = 'no';            end
 
 % convert the yes/no arguments to the corresponding logical values
 keepfilter     = istrue(keepfilter);
@@ -96,6 +97,7 @@ projectnoise   = istrue(projectnoise);
 projectmom     = istrue(projectmom);
 fixedori       = istrue(fixedori);
 computekurt    = istrue(computekurt);
+nai            = istrue(nai);
 
 % default is to use the trace of the covariance matrix, see Van Veen 1997
 if isempty(powmethod)
@@ -249,21 +251,37 @@ for i=1:size(dip.pos,1)
   end
   
   if fixedori
-    % compute the leadfield for the optimal dipole orientation
-    % subsequently the leadfield for only that dipole orientation will be used for the final filter computation
-    % filt = pinv(lf' * invCy * lf) * lf' * invCy;
-    % [u, s, v] = svd(real(filt * Cy * ctranspose(filt)));
-    % in this step the filter computation is not necessary, use the quick way to compute the voxel level covariance (cf. van Veen 1997)
-    [u, s, v] = svd(real(pinv(lf' * invCy *lf)));
-    eta = u(:,1);
-    lf  = lf * eta;
-    if ~isempty(subspace), lforig = lforig * eta; end
-    dipout.ori{i} = eta;
+      if nai
+          % optimal orientation calculation for weight normalized beamformer,
+          % based on equation 4.47 from Sekihara & Nagarajan 2008
+          [vv, dd] = eig(pinv(lf' * invCy *lf)*(lf' * invCy^2 *lf));
+          [~,maxeig]=max(diag(dd));
+          eta = vv(:,maxeig);
+          lf  = lf * eta;
+          if ~isempty(subspace), lforig = lforig * eta; end
+          dipout.ori{i} = eta;
+      else
+          % compute the leadfield for the optimal dipole orientation
+          % subsequently the leadfield for only that dipole orientation will be used for the final filter computation
+          % filt = pinv(lf' * invCy * lf) * lf' * invCy;
+          % [u, s, v] = svd(real(filt * Cy * ctranspose(filt)));
+          % in this step the filter computation is not necessary, use the quick way to compute the voxel level covariance (cf. van Veen 1997)
+          [u, s, v] = svd(real(pinv(lf' * invCy *lf)));
+          eta = u(:,1);
+          lf  = lf * eta;
+          if ~isempty(subspace), lforig = lforig * eta; end
+          dipout.ori{i} = eta;
+      end
   end
   
   if isfield(dip, 'filter')
     % use the provided filter
     filt = dip.filter{i};
+  elseif nai
+    % below equation is equivalent to following:  
+    % filt = pinv(lf' * invCy * lf) * lf' * invCy; 
+    % filt = filt/sqrt(noise*filt*filt');
+    filt = pinv(sqrt(noise * lf' * invCy^2 * lf)) * lf' *invCy;
   else
     % construct the spatial filter
     filt = pinv(lf' * invCy * lf) * lf' * invCy;              % van Veen eqn. 23, use PINV/SVD to cover rank deficient leadfield
