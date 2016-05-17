372364313fc0b7023c7ab61a06178ed57ace6e63
Robert Oostenveld <r.oostenveld@gmail.com>
2016-05-17 15:16:19 +0200

    automatically synchronized identical files to 9ccf1d99ccb99f72353e488b7e06546e803c6c20

--- forward/ft_apply_montage.m
+++ forward/ft_apply_montage.m
@@ -3,7 +3,7 @@ function [input] = ft_apply_montage(input, montage, varargin)
 % FT_APPLY_MONTAGE changes the montage of an electrode or gradiometer array. A
 % montage can be used for EEG rereferencing, MEG synthetic gradients, MEG
 % planar gradients or unmixing using ICA. This function applies the montage
-% to the inputor array. The inputor array can subsequently be used for
+% to the input EEG or MEG sensor array, which can subsequently be used for
 % forward computation and source reconstruction of the data.
 %
 % Use as
@@ -66,6 +66,14 @@ function [input] = ft_apply_montage(input, montage, varargin)
 %
 % $Id$
 
+if iscell(input) && iscell(input)
+  % this represents combined EEG, ECoG and/or MEG
+  for i=1:numel(input)
+    input{i} = ft_apply_montage(input{i}, montage, varargin{:});
+  end
+  return
+end
+
 % get optional input arguments
 keepunused  = ft_getopt(varargin, 'keepunused',  'no');
 inverse     = ft_getopt(varargin, 'inverse',     'no');
@@ -79,8 +87,7 @@ else
   warningfun = @nowarning;
 end
 
-% these are optional, at the end we will clean up the output in case they did not
-% exist
+% these are optional, at the end we will clean up the output in case they did not exist
 haschantype = (isfield(input, 'chantype') || isfield(input, 'chantypenew')) && all(isfield(montage, {'chantypeorg', 'chantypenew'}));
 haschanunit = (isfield(input, 'chanunit') || isfield(input, 'chanunitnew')) && all(isfield(montage, {'chanunitorg', 'chanunitnew'}));
 
