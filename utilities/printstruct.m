4b41e96ad9fce7cc14ff4d358a7f84bdf758b5d8
Robert Oostenveld <r.oostenveld@gmail.com>
2016-05-17 14:09:47 +0200

    don't include a double \n\n at the end of a sub-structure

--- utilities/printstruct.m
+++ utilities/printstruct.m
@@ -99,7 +99,12 @@ elseif isstruct(val)
         otherwise
           error('unsupported');
       end
-      str  = [str line];
+      if numel(line)>1 && line(end)==10 && line(end-1)==10
+        % do not repeat the end-of-line
+        str = [str line(1:end-1)];
+      else
+        str = [str line];
+      end
     end
   end
 elseif ~isstruct(val)
