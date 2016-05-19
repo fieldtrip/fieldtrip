c2598673f2aceb7c36a7d58c57588b3039014ecb
Robert Oostenveld <r.oostenveld@gmail.com>
2016-05-19 17:43:09 +0200

    extended test script

--- test/test_bug2978.m
+++ test/test_bug2978.m
@@ -1,4 +1,4 @@
-function test_bug2978
+% function test_bug2978
 
 % WALLTIME 00:20:00
 % MEM 2GB
@@ -14,37 +14,43 @@ dataset = dccnpath('/home/common/matlab/fieldtrip/data/Subject01.ds');
 cfg = [];
 cfg.channel = 'MEG';
 cfg.demean = 'yes';
+cfg.dataset = dataset;
 data = ft_preprocessing(cfg); % 266 trials
-
 
 cfg = [];
 timelock1 = ft_timelockanalysis(cfg, data);
-
 cfg.keeptrials = 'yes';
 timelock2 = ft_timelockanalysis(cfg, data);
 
 
 cfg = [];
 cfg.method = 'mtmfft';
-cfg.taper = 'hanning'
+cfg.taper = 'hanning';
 freq1 = ft_freqanalysis(cfg, data);
-
 cfg.keeptrials = 'yes';
 freq2 = ft_freqanalysis(cfg, data);
+
+cfg = [];
+cfg.foilim = [1 20];
+cfg.method = 'wavelet';
+cfg.toi = -1:0.100:2;
+timefreq1 = ft_freqanalysis(cfg, data);
+cfg.keeptrials = 'yes';
+timefreq2 = ft_freqanalysis(cfg, data);
 
 
 cfg = [];
 cfg.method = 'mtmfft';
 cfg.taper = 'hanning';
 cfg.output = 'powandcsd'; % this implies keeptrials
-freq = ft_freqanalysis(cfg, data);
-
+powandcsd = ft_freqanalysis(cfg, data);
 
 cfg = [];
 cfg.method = 'coh';
-coh1 = ft_connectivityanalysis(cfg, freq);
+coh1 = ft_connectivityanalysis(cfg, powandcsd);
 
-coh1.dimord = 'chancmb_freq'; % HACK, see http://bugzilla.fieldtriptoolbox.org/show_bug.cgi?id=3128
+% HACK, see http://bugzilla.fieldtriptoolbox.org/show_bug.cgi?id=3128
+% coh1.dimord = 'chancmb_freq'; 
 
 % I don't know how to get single trial (pseudo) estimates of coherence
 
@@ -76,6 +82,20 @@ cfg = [];
 cfg.layout = 'CTF151.lay';
 cfg.trials = 255; % contains a jump
 figure; ft_multiplotER(cfg, freq2);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% make the figures, timefreq
+
+cfg = [];
+cfg.layout = 'CTF151.lay';
+figure; ft_multiplotER(cfg, timefreq1);
+figure; ft_multiplotER(cfg, timefreq2);
+
+cfg = [];
+cfg.layout = 'CTF151.lay';
+cfg.trials = 255; % contains a jump
+figure; ft_multiplotER(cfg, timefreq2);
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
