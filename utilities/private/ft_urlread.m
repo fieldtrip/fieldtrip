18a35a5c33187174f7fb1f308a189b4faeca7053
Robert Oostenveld <r.oostenveld@gmail.com>
2016-07-11 11:58:28 +0200

    FIX - http://bugzilla.fieldtriptoolbox.org/show_bug.cgi?id=2901#c9

--- utilities/private/ft_urlread.m
+++ utilities/private/ft_urlread.m
@@ -9,8 +9,12 @@ method  = ft_getopt(varargin, 'method', 'get');
 timeout = ft_getopt(varargin, 'timeout', 15);
 
 % the timeout option is only available from MATLAB 2012b onward
-if ft_platform_supports('urlread-timeout')
-  [output, status] = urlread(event_http, 'method', method, 'timeout', timeout);
+if strcmp(method, 'get')
+  if ft_platform_supports('urlread-timeout')
+    [output, status] = urlread(event_http, 'timeout', timeout);
+  else
+    [output, status] = urlread(event_http);
+  end
 else
-  [output, status] = urlread(event_http, 'method', method);
+  error('method "%s" is not yet implemented', method);
 end
