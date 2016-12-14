8e8b413b401506f87df6e13d90ea08f2ba6b253e
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2016-12-14 11:41:52 +0100

    ENH - copied test script provided by Pim

---
+++ test/test_bug3218.m
@@ -0,0 +1,25 @@
+clear all;
+
+load('tmp_data_small.mat', 'data');
+
+% When rejecting artifacts using 'partial', ft_rejectvisual works fine
+cfg = [];
+cfg.artfctdef.visual.artifact = data.sampleinfo(1, :) + [300 -300];
+cfg.artfctdef.reject = 'partial';
+dataReject = ft_rejectartifact(cfg, data);
+
+ft_rejectvisual([], dataReject);
+
+% When rejecting artifacts using 'nan', ft_rejectvisual recalculates the
+% metric after each trial that is excluded. With this small data set it's
+% not really a problem, but it becomes cumbersome with many trials and
+% channels.
+
+cfg = [];
+cfg.artfctdef.visual.artifact = data.sampleinfo(1, :) + [300 -300];
+cfg.artfctdef.reject = 'nan';
+dataReject = ft_rejectartifact(cfg, data);
+
+ft_rejectvisual([], dataReject);
+
+
