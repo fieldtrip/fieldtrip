0552e3c8920d67dee9eaa23dfafc0ec3a6d78449
Robert Oostenveld <r.oostenveld@gmail.com>
2017-03-09 17:57:22 +0100

    FIX - also convert the labelorg etc in the balancing structure

--- plotting/private/fixmontage.m
+++ plotting/private/fixmontage.m
@@ -1,16 +1,30 @@
-function montage = fixmontage(montage)
+function montage = fixmontage(montage, revert)
 
 % FIXMONTAGE use "old/new" instead of "org/new"
 
-if isfield(montage, 'labelorg')
-  montage.labelold = montage.labelorg;
-  montage = rmfield(montage, 'labelorg');
+if nargin<2
+  revert = false;
 end
-if isfield(montage, 'chantypeorg')
-  montage.chantypeold = montage.chantypeorg;
-  montage = rmfield(montage, 'chantypeorg');
+
+if ~revert
+  from = {'labelorg', 'chantypeorg', 'chanunitorg', 'chanposorg', 'chanoriorg'};
+  to   = {'labelold', 'chantypeold', 'chanunitold', 'chanposold', 'chanoriold'};
+else
+  to   = {'labelorg', 'chantypeorg', 'chanunitorg', 'chanposorg', 'chanoriorg'};
+  from = {'labelold', 'chantypeold', 'chanunitold', 'chanposold', 'chanoriold'};
 end
-if isfield(montage, 'chanunitorg')
-  montage.chanunitold = montage.chanunitorg;
-  montage = rmfield(montage, 'chanunitorg');
+
+for i=1:numel(from)
+  if isfield(montage, from{i})
+    montage.(to{i}) = montage.(from{i});
+    montage = rmfield(montage, from{i});
+  end
+end
+
+% use recursion to update the subfields
+fn = fieldnames(montage);
+for i=1:numel(fn)
+  if isstruct(montage.(fn{i}))
+    montage.(fn{i}) = fixmontage(montage.(fn{i}), revert);
+  end
 end
