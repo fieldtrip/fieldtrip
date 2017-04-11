441e291f44dd82766f22b6ecebd7b69b89972d82
Sandon Griffin <sandongriffin@gmail.com>
2017-04-11 10:55:18 -0700

    test_ft_plot_cloud

--- plotting/test/test_ft_plot_cloud.m
+++ plotting/test/test_ft_plot_cloud.m
@@ -8,6 +8,20 @@ elec = ft_read_sens('easycap-M10.txt');
 pos = elec.chanpos;
 val = linspace(-1, 1, numel(elec.label));  % values between -1 and 1
 
+% Create test meshes
+seg.dim = [50 50 50];
+seg.transform = eye(4);
+seg.unit = 'mm';
+seg.brain = ones(50, 50, 50);
+cfg = [];
+cfg.tissue = 'brain';
+cfg.numvertices = 100;
+mesh1 = ft_prepare_mesh(cfg, seg);
+
+seg.brain = zeros(50, 50, 50);
+seg.brain(11:40, 11:40, 11:40) = ones(30, 30, 30);
+mesh2 = ft_prepare_mesh(cfg, seg);
+
 %%
 % the following three figures should be identical, since the units should be correctly detected
 
@@ -46,7 +60,31 @@ figure; ft_plot_cloud(pos, val); colorbar
 figure; ft_plot_cloud(pos, val, 'ptdensity', 5);
 figure; ft_plot_cloud(pos, val, 'ptdensity', 50);
 figure; ft_plot_cloud(pos, val, 'colorgrad', 1);
+figure; ft_plot_cloud(pos, val, 'scalerad', 'no');
 
+%% 
+% mesh options
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1);
+figure; ft_plot_cloud(pos, val, 'mesh', {mesh1, mesh2}, 'facealpha', [.5 1]); % should be able to see one mesh inside the other
 
+% these two should be the same
+figure; ft_plot_cloud(pos, val, 'mesh', {mesh1, mesh2}, 'facealpha', [.5 1], 'facecolor', {'r', 'b'});
+figure; ft_plot_cloud(pos, val, 'mesh', {mesh1, mesh2}, 'facealpha', [.5 1], 'facecolor', {[1 0 0], [0 0 1]});
 
+% these two should be the same
+figure; ft_plot_cloud(pos, val, 'mesh', {mesh1, mesh2}, 'facealpha', 0, 'edgealpha', [.5 1], 'edgecolor', {'r', 'b'});
+figure; ft_plot_cloud(pos, val, 'mesh', {mesh1, mesh2}, 'facealpha', 0, 'edgealpha', [.5 1], 'edgecolor', {[1 0 0], [0 0 1]});
+
+%%
+% slice options
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '2d');
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '3d', 'nslices', 3);
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '2d', 'nslices', 3);
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '3d', 'nslices', 3, 'minspace', 20);
+
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '3d', 'slicepos', 33);
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '2d', 'slicepos', 33);
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '2d', 'slicepos', 33, 'slicetype', 'surf');
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '3d', 'ori', 'x', 'slicepos', 33);
+figure; ft_plot_cloud(pos, val, 'mesh', mesh1, 'slice', '2d', 'ori', 'x', 'slicepos', 33);
 
