85d9e9c32c57039509473f448a12a35e85ca6eaa
Robert Oostenveld <r.oostenveld@gmail.com>
2017-04-12 21:23:52 +0200

    first test script to test the behaviour of http://bugzilla.fieldtriptoolbox.org/show_bug.cgi?id=3280

---
+++ test/test_bug3280.m
@@ -0,0 +1,19 @@
+function test_bug3280
+
+% WALLTIME 00:10:00
+% MEM 2gb
+
+%%
+cd(dccnpath('/home/common/matlab/fieldtrip/data/test/bug3280'));
+
+dataset = 'A1826_comparisonSEF_20161212_02_signletrial.ds';
+
+%%
+
+hdr = ft_read_header(dataset);
+
+grad = ft_read_sens(dataset, 'senstype', 'meg');
+elec = ft_read_sens(dataset, 'senstype', 'eeg');
+
+assert(isfield(grad,'coilpos'));
+assert(isfield(elec,'elecpos'));
