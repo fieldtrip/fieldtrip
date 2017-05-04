8319fec823a5b636a3ac85d317d177f3567831a5
Robert Oostenveld <r.oostenveld@gmail.com>
2017-05-04 10:49:29 +0200

    Update test_ft_determine_coordsys.m
    added walltime and mem

--- test/test_ft_determine_coordsys.m
+++ test/test_ft_determine_coordsys.m
@@ -1,5 +1,8 @@
 function test_ft_determine_coordsys
 
+% WALLIME 00:10:00
+% MEM 1gb
+
 mrifile = dccnpath('/home/common/matlab/fieldtrip/data/test/latest/mri/nifti/single_subj_T1.nii');
 mri     = ft_read_mri(mrifile);
 
@@ -9,4 +12,3 @@ close
 ft_determine_coordsys(mri,'clim',[0 .1],'interactive','no')
 close
 
-clear mri
