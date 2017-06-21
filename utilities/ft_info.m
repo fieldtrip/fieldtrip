48d84710474a0741e834fedc5941315b383fa80c
Robert Oostenveld <r.oostenveld@gmail.com>
2017-06-21 17:03:48 +0200

    updated output arguments

--- utilities/ft_info.m
+++ utilities/ft_info.m
@@ -1,4 +1,4 @@
-function ft_info(varargin)
+function varargout = ft_info(varargin)
 
 % FT_INFO prints an informational message on screen, depending on the verbosity
 % settings of the calling high-level FieldTrip function.
@@ -9,26 +9,34 @@ function ft_info(varargin)
 %   ft_info(msgId, ...)
 % with arguments similar to warning.
 %
-% See also FT_ERROR, FT_WARNING, FT_NOTICE, FT_INFO, FT_DEBUG
+% See also FT_ERROR, FT_WARNING, FT_NOTICE, FT_INFO, FT_DEBUG, ERROR, WARNING
 
-% get the configuration from the calling function
-try
-  cfg = evalin('caller', 'cfg');
-catch
-  cfg = [];
+% Copyright (C) 2017, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
+if nargout
+  varargout{:} = ft_notification(varargin{:});
+elseif isequal(varargin, {'last'})
+  % return an answer anyway
+  varargout{1} = ft_notification(varargin{:});
+else
+  ft_notification(varargin{:});
 end
-
-% set the default
-cfg.feedback = ft_getopt(cfg, 'feedback', 'notice');
-
-switch cfg.feedback
-  case {'emergency' 'alert' 'critical' 'error' 'warning' 'warning_once' 'notice'}
-    % do not show the message
-  case {'info' 'debug'}
-    % show the message
-    fprintf(varargin{:});
-  otherwise
-    % show the message
-    fprintf(varargin{:});
-end % switch
 
--- utilities/ft_warning.m
+++ utilities/ft_warning.m
@@ -1,4 +1,45 @@
-function [ws, warned] = ft_warning(varargin)
+function varargout = ft_warning(varargin)
+
+% FT_ERROR prints an error message on screen, just like the standard ERROR function.
+%
+% Use as
+%   ft_error(...)
+% with arguments similar to fprintf, or
+%   ft_error(msgId, ...)
+% with arguments similar to error.
+%
+% See also FT_ERROR, FT_WARNING, FT_NOTICE, FT_INFO, FT_DEBUG, ERROR, WARNING
+
+% Copyright (C) 2012-2017, Robert Oostenveld, J?rn M. Horschig
+%
+% This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
+if nargout
+  varargout{:} = ft_notification(varargin{:});
+elseif isequal(varargin, {'last'})
+  % return an answer anyway
+  varargout{1} = ft_notification(varargin{:});
+else
+  ft_notification(varargin{:});
+end
+
+return
 
 % FT_WARNING will throw a warning for every unique point in the
 % stacktrace only, e.g. in a for-loop a warning is thrown only once.
