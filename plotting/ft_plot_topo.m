ab332f895c6f512d282ba57df346a9fcecfb9795
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2017-07-04 14:04:50 +0200

    ENH - call shared function when doing saturation based masking, added colormix to ft_plot_matrix

--- plotting/ft_plot_topo.m
+++ plotting/ft_plot_topo.m
@@ -130,7 +130,7 @@ chanYorg = chanY;
 chanX = chanX(:) * xScaling + hpos;
 chanY = chanY(:) * yScaling + vpos;
 
-if strcmp(interplim, 'electrodes'),
+if strcmp(interplim, 'electrodes')
   hlim = [min(chanX) max(chanX)];
   vlim = [min(chanY) max(chanY)];
 elseif (strcmp(interplim, 'mask') || strcmp(interplim, 'mask_individual')) && ~isempty(mask),
@@ -271,42 +271,27 @@ elseif strcmp(style, 'imsat') || strcmp(style, 'imsatiso')
   if isempty(clim)
     error('clim is required for style = ''imsat'' or style = ''imsatiso''')
   end
-  satmask = maskimage;
-  cdat = Zi;
-  % below code is shared with ft_plot_matrix
   
+  cmap    = get(gcf, 'colormap');
+  rgbcdat = cdat2rgb(Zi, cmap, clim, maskimage);
   
-  % This approach changes the color of pixels to white, regardless of colormap, without using opengl
-  % It does by converting by:
-  % 1) convert the to-be-plotted data to their respective rgb color values (determined by colormap)
-  % 2) convert these rgb color values to hsv values, hue-saturation-value
-  % 3) for to-be-masked-pixels, set saturation to 0 and value to 1 (hue is irrelevant when they are)
-  % 4) convert the hsv values back to rgb values
-  % 5) plot these values
-  
-  % enforce mask properties (satmask is 0 when a pixel needs to be masked, 1 if otherwise)
-  satmask = round(double(satmask));   % enforce binary white-masking, the hsv approach cannot be used for 'white-shading'
-  satmask(isnan(cdat)) = false;       % make sure NaNs are plotted as white pixels, even when using non-integer mask values
-  
-  % do 1, by converting the data-values to zero-based indices of the colormap
-  ncolors = size(get(gcf, 'colormap'), 1); % determines range of index, if a figure has been created by the caller function, gcf changes nothing, if not, a figure is created (which the below would do otherwise)
-  indcdat = (cdat + -clim(1)) * (ncolors / (-clim(1) + clim(2))); % transform cdat-values to have a 0-(ncolors-1) range (range depends on colormap used, and thus also on clim)
-  rgbcdat = ind2rgb(uint8(floor(indcdat)), colormap);
-  % do 2
-  hsvcdat = rgb2hsv(rgbcdat);
-  % do 3
-  hsvs = hsvcdat(:,:,2);
-  hsvs(~satmask) = 0;
-  hsvv = hsvcdat(:,:,3);
-  hsvv(~satmask) = 1;
-  hsvcdat(:,:,2) = hsvs;
-  hsvcdat(:,:,3) = hsvv;
-  % do 4
-  rgbcdat = hsv2rgb(hsvcdat);
-  % do 5
   h = imagesc(xi, yi, rgbcdat, clim);
   set(h, 'tag', tag);
 
+elseif strcmp(style, 'colormix')
+  % Plot the surface in an alternate style (using imagesc and saturation masking) so that it can be nicely saved to a vectorized format
+ 
+  % set mask and cdat, and check for clim
+  if isempty(clim)
+    error('clim is required for style = ''imsat'' or style = ''imsatiso''')
+  end
+  
+  cmap    = get(gcf, 'colormap');
+  rgbcdat = bg_rgba2rgb([1 1 1], cdat, cmap, clim, highlight, 'rampup', [0 1]);
+      
+  h = imagesc(xi, yi, rgbcdat, clim);
+  set(h,'tag',tag);
+      
 end
 
 % Plot the outline of the head, ears and nose
