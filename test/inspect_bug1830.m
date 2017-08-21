ddd847ab56443212205e838c85248b01ebafb1ed
Robert Oostenveld <r.oostenveld@gmail.com>
2017-08-21 17:53:58 +0200

    added some extra test cases

--- test/inspect_bug1830.m
+++ test/inspect_bug1830.m
@@ -7,6 +7,7 @@ function inspect_bug1830
 
 [v, p] = ft_version;
 
+mri = ft_read_mri(fullfile(p, 'template', 'anatomy', 'single_subj_T1.nii'));
 skin = ft_read_headshape(fullfile(p, 'template', 'headmodel', 'skin', 'standard_skin_14038.vol'));
 elec = ft_read_sens(fullfile(p, 'template', 'electrode', 'standard_1020.elc'));
 
@@ -29,7 +30,12 @@ t = [
   0     0     0     1
   ];
 
-elec = ft_transform_sens(t * r, elec);
+% move the electrodes a little bit away from the original
+elec = ft_transform_geometry(t * r, elec);
+
+% move the MRI a little bit away from the original
+mri = ft_transform_geometry(t * r, mri);
+
 
 % figure
 % ft_plot_mesh(skin, 'facecolor', 'skin');
@@ -77,6 +83,17 @@ out = ft_interactiverealign(cfg);
 %%
 
 cfg = [];
+cfg.method = 'interactive';
+cfg.elec = ft_convert_units(elec_ctf, 'cm'); % can also be passed as 2nd input data element
+cfg.headshape = skin_ctf;
+elec_realigned = ft_electroderealign(cfg);
+
+assert(isequal(elec_realigned.unit, 'cm')); % should not have changed
+
+
+%%
+
+cfg = [];
 cfg.template.headshape = skin_ctf;
 cfg.template.headshapestyle = 'surface';
 
@@ -87,4 +104,36 @@ out = ft_interactiverealign(cfg);
 cfg.individual.grad = grad_ctf;
 cfg.individual.gradstyle = {'coilshape', 'circle'};
 out = ft_interactiverealign(cfg);
+
+%%
+% following the redesign and cleanup of ft_interactiverealign, the goal is to call 
+% interactiverealign from the other data-type specific realign functions: 
+%  - ft_electroderealign.m	(method=interactive) 
+%  - ft_volumerealign.m     (for the optional interactive alignment in method=headshape)
+%  - ft_surfacerealign.m    (should be renamed in ft_meshrealign)
+%  - ft_sensorrealign.m	    (has been deprecated and will not get any further updates)
+
+cfg = [];
+cfg.method = 'interactive';
+cfg.elec = ft_convert_units(elec_ctf, 'cm'); % can also be passed as 2nd input data element
+cfg.target = elec;
+elec_realigned = ft_electroderealign(cfg);
+
+assert(isequal(elec_realigned.unit, 'cm')); % should not have changed
+
+%%
+
+cfg = [];
+cfg.method = 'headshape';
+cfg.headshape.interactive = 'yes';
+cfg.headshape.icp = 'yes';
+cfg.headshape.headshape = skin;
+cfg.coordsys = 'mni';
+cfg.spmversion = 'spm12';
+mri_realigned = ft_volumerealign(cfg, mri);
+
+% only interactive final check
+cfg.headshape.icp = 'no';
+mri_realigned2 = ft_volumerealign(cfg, mri_realigned);
+
 
