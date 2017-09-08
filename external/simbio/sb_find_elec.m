8a4fadad43a6d129bd537d8c472d87665582cb54
Maria Carla Piastra <mcpiastra@uni-muenster.de>
2017-09-08 12:23:22 +0200

    sanity check on electrode positions

--- external/simbio/sb_find_elec.m
+++ external/simbio/sb_find_elec.m
@@ -5,7 +5,11 @@ function diri = sb_find_elec(vol,sens)
 % $Id$
 
 diri = zeros(size(sens.elecpos,1),1);
+dist = zeros(size(sens.elecpos,1),1);
 for i=1:size(sens.elecpos,1)
-    [dist, diri(i)] = min(sum(bsxfun(@minus,vol.pos,sens.elecpos(i,:)).^2,2));
+    [dist(i), diri(i)] = min(sum(bsxfun(@minus,vol.pos,sens.elecpos(i,:)).^2,2));
+end
+if ~all(dist < 1e-8)
+    error('Electrode positions are not located on mesh nodes! This should not happen, please contact support.');
 end
 end
