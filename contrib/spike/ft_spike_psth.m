383c1b3613b9a5d19eadeb73b15764bd5bd4434a
Robert Oostenveld <r.oostenveld@gmail.com>
2017-09-15 10:36:45 +0200

    pstk (similar to timelock) should not contain avg and trials in same data structure; compute avg on the fly when needed

--- contrib/spike/ft_spike_psth.m
+++ contrib/spike/ft_spike_psth.m
@@ -5,10 +5,10 @@ function [psth] = ft_spike_psth(cfg, spike)
 % Use as
 %   [psth] = ft_spike_psth(cfg, spike)
 %
-% The input SPIKE should be organised as a) the spike datatype, obtained
-% from FT_SPIKE_MAKETRIALS b) the raw datatype, containing binary spike
-% trains, obtained from FT_APPENDSPIKE or FT_CHECKDATA. In this case the
-% raw datatype is converted to the spike datatype.
+% The input SPIKE should be organised as either the spike datatype,
+% obtained from FT_SPIKE_MAKETRIALS, or the raw datatype, containing binary
+% spike trains, obtained from FT_APPENDSPIKE or FT_CHECKDATA. In this case
+% the raw datatype is converted to the spike datatype.
 %
 % Configurations:
 %   cfg.binsize          =  [binsize] in sec or string. 
@@ -43,7 +43,7 @@ function [psth] = ft_spike_psth(cfg, spike)
 %                          available 'minperiod', i.e., the minimal period
 %                          all trials share, 'prestim' (all t<=0) 'poststim'
 %                          (all t>=0).
-%   cfg.keeptrials       = 'yes' or 'no' (default).
+%   cfg.keeptrials       = 'yes' or 'no' (default)
 %   cfg.trials           =  numeric or logical selection of trials (default = 'all')
 %
 % Outputs:
@@ -101,7 +101,7 @@ cfg.spikechannel = ft_getopt(cfg, 'spikechannel', 'all');
 cfg.trials       = ft_getopt(cfg, 'trials', 'all');
 cfg.latency      = ft_getopt(cfg, 'latency','maxperiod');
 cfg.vartriallen  = ft_getopt(cfg, 'vartriallen', 'yes');
-cfg.keeptrials   = ft_getopt(cfg, 'keeptrials', 'yes');
+cfg.keeptrials   = ft_getopt(cfg, 'keeptrials', 'no');
 
 % ensure that the options are valid
 cfg = ft_checkopt(cfg,'outputunit',   'char',  {'rate', 'spikecount'});
