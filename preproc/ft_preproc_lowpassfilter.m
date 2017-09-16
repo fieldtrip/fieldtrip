e1519009c97585bfcb2c923590e7bcd6ff826c96
Robert Oostenveld <r.oostenveld@gmail.com>
2017-09-16 11:13:55 +0200

    use "off backtrace" rather than "backtrace off" for ft_warning

--- preproc/ft_preproc_lowpassfilter.m
+++ preproc/ft_preproc_lowpassfilter.m
@@ -287,16 +287,16 @@ catch
     case 'no'
       rethrow(lasterror);
     case 'reduce'
-      ft_warning('backtrace', 'off')
+      ft_warning off backtrace
       ft_warning('instability detected - reducing the %dth order filter to an %dth order filter', N, N-1);
-      ft_warning('backtrace', 'on')
+      ft_warning on backtrace
       filt = ft_preproc_lowpassfilter(dat,Fs,Flp,N-1,type,dir,instabilityfix);
     case 'split'
       N1 = ceil(N/2);
       N2 = floor(N/2);
-      ft_warning('backtrace', 'off')
+      ft_warning off backtrace
       ft_warning('instability detected - splitting the %dth order filter in a sequential %dth and a %dth order filter', N, N1, N2);
-      ft_warning('backtrace', 'on')
+      ft_warning on backtrace
       filt = ft_preproc_lowpassfilter(dat ,Fs,Flp,N1,type,dir,instabilityfix);
       filt = ft_preproc_lowpassfilter(filt,Fs,Flp,N2,type,dir,instabilityfix);
     otherwise
