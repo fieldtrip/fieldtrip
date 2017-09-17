913eb6a47a9ee35fd97b6c1764ad2f5a55aac6d6
Robert Oostenveld <r.oostenveld@gmail.com>
2017-09-17 15:13:29 +0200

    fixed small typo in help

--- ft_multiplotTFR.m
+++ ft_multiplotTFR.m
@@ -78,7 +78,7 @@ function [cfg] = ft_multiplotTFR(cfg, data)
 % The layout defines how the channels are arranged and what the size of each
 % subplot is. You can specify the layout in a variety of ways:
 %  - you can provide a pre-computed layout structure (see ft_prepare_layout)
-%  - you can give the name of an ascii layout file with extension *.cfg.layout
+%  - you can give the name of an ascii layout file with extension *.lay
 %  - you can give the name of an electrode file
 %  - you can give an electrode definition, i.e. "elec" structure
 %  - you can give a gradiometer definition, i.e. "grad" structure
