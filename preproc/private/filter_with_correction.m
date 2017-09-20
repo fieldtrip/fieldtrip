9676bd56b09adc725bd89f1547b8d3278036af24
Mats van Es <m.vanes@donders.ru.nl>
2017-09-20 15:00:32 +0200

    add firws filter with dir: onepass-reverse-zerophase. It is a combination of a onepass-reverse and onepass-zerophase filter

--- preproc/private/filter_with_correction.m
+++ preproc/private/filter_with_correction.m
@@ -9,11 +9,14 @@ function filt = filter_with_correction(B,A,dat,dir,usefftfilt)
 %   B,A        filter coefficients
 %   dat        data matrix (Nchans X Ntime)
 %   dir        optional filter direction, can be
-%                'onepass'         forward filter only
-%                'onepass-reverse' reverse filter only, i.e. backward in time
-%                'twopass'         zero-phase forward and reverse filter (default)
-%                'twopass-reverse' zero-phase reverse and forward filter
-%                'twopass-average' average of the twopass and the twopass-reverse
+%                'onepass'                   forward filter only
+%                'onepass-reverse'           reverse filter only, i.e. backward in time
+%                'twopass'                   zero-phase forward and reverse filter (default)
+%                'twopass-reverse'           zero-phase reverse and forward filter
+%                'twopass-average'           average of the twopass and the twopass-reverse
+%                'onepass-zerophase'         zero-phase forward filter with delay compensation (default for firws, linear-phase symmetric FIR only)
+%                'onepass-reverse-zerophase' zero-phase reverse filter with delay compensation
+%                'onepass-minphase'          minimum-phase converted forward filter (non-linear!, firws only)
 %
 % Note that a one- or two-pass filter has consequences for the
 % strength of the filter, i.e. a two-pass filter with the same filter
