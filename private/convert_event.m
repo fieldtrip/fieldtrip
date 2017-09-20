fb06ed12db7d174d6f68bb578ee7033fbac8bcbf
Robert Oostenveld <r.oostenveld@gmail.com>
2017-09-20 22:01:06 +0200

    ENH - added support for Nx2 trl matrix that is represented as table object

--- private/convert_event.m
+++ private/convert_event.m
@@ -53,6 +53,10 @@ end
 endsample = ft_getopt(varargin, 'endsample');
 typenames = ft_getopt(varargin, 'typenames');
 
+if istable(obj)
+  obj = table2array(obj);
+end
+
 % Determine what the input object is
 if isempty(obj)
   input_obj = 'empty';
