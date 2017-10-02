fieldtrip/fieldtrip

commit 86c63a0568b5ff6b0cebb912ad8a1e09ea4f8fe9

ft_rejectartifact – field trialinfo from original data removed in line 520 to avoid the appeareance of the in this case misleading warning - Original data has trialinfo, using user specified trialinfo instead – in ft_redefinetrial.

ft_rejectartifact.m
@@ -517,6 +517,7 @@ else
   if hasdata && ~strcmp(cfg.artfctdef.reject, 'nan') % Skip this step to avoid removing parts that should be filled with nans
     % apply the updated trial definition on the data
     tmpcfg     = keepfields(cfg, {'trl', 'showcallinfo'});
+    data       = rmfield(data, {'trialinfo'});
     data       = ft_redefinetrial(tmpcfg, data);
     if isfield(data, 'offset')
       data = rmfield(data, 'offset');

commit 520138aff5d0560b1c957952e2e6e8fb75f606da

Fixed bug that was introduced in 86c63a05, trialinfo is not always present. Added rollback_provenance after ft_redefinetrial.

ft_rejectartifact.m
@@ -516,12 +516,11 @@ if isempty(cfg.trl)
 else
   if hasdata && ~strcmp(cfg.artfctdef.reject, 'nan') % Skip this step to avoid removing parts that should be filled with nans
     % apply the updated trial definition on the data
-    tmpcfg     = keepfields(cfg, {'trl', 'showcallinfo'});
-    data       = rmfield(data, {'trialinfo'});
-    data       = ft_redefinetrial(tmpcfg, data);
-    if isfield(data, 'offset')
-      data = rmfield(data, 'offset');
-    end
+    tmpcfg      = keepfields(cfg, {'trl', 'showcallinfo'});
+    data        = removefields(data, {'trialinfo'});
+    data        = ft_redefinetrial(tmpcfg, data);
+    % restore the provenance information
+    [cfg, data] = rollback_provenance(cfg, data);
   end
 end
 