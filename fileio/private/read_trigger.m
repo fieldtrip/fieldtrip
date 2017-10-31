8a951eb7dcea58aa38dd9a3f8d417544c08d8bc5
Robert Oostenveld <r.oostenveld@gmail.com>
2017-10-31 11:38:14 +0100

    FIX - fixing negative trigger values for neuromag failed when there are more than two binary trigger channels

--- fileio/private/read_trigger.m
+++ fileio/private/read_trigger.m
@@ -134,7 +134,7 @@ end
 % fix suggested by Ralph Huonker to deal with triggers that need to be
 % interpreted as unsigned integers, rather than signed
 if strncmpi(dataformat, 'neuromag', 8) && ~fixneuromag
-  if any(dat<0)
+  if any(dat(:)<0)
     tmpdat = zeros(size(dat));
     for k = 1:size(dat,1)
       tmpdat(k,:) = double(typecast(int16(dat(k,:)), 'uint16'));
