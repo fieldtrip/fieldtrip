ad1af04d73aad1bdfece80a4decd394aff2e6bc4
Daniel Matthes <dmatthes@cbs.mpg.de>
2017-12-20 16:50:42 +0100

    fprintf replaced with ft_info in ft_componentanalysis additional optional argument showcallinfo in forward/ft_apply_montage added → used with function  ft_selectdata fprintf replaced with ft_info in ft_rejectcomponent call of ft_apply_montage in ft_rejectcomponent extended

--- ft_rejectcomponent.m
+++ ft_rejectcomponent.m
@@ -119,18 +119,18 @@ end
 
 if hasdata && strcmp(cfg.demean, 'yes')
   % optionally perform baseline correction on each trial
-  fprintf('baseline correcting data \n');
+  ft_info('baseline correcting data \n');
   for trial=1:numel(data.trial)
     data.trial{trial} = ft_preproc_baselinecorrect(data.trial{trial});
   end
 end
 
 % set the rejected component amplitudes to zero
-fprintf('removing %d components\n', length(reject));
+ft_info('removing %d components\n', length(reject));
 if ~hasdata
-  fprintf('keeping %d components\n',  ncomps-length(reject));
+  ft_info('keeping %d components\n',  ncomps-length(reject));
 else
-  fprintf('keeping %d components\n',  nchans-length(reject));
+  ft_info('keeping %d components\n',  nchans-length(reject));
 end
 
 % create a projection matrix by subtracting the subspace spanned by the
@@ -175,7 +175,7 @@ else
 end % if hasdata
 
 % apply the linear projection to the data
-data = ft_apply_montage(data, montage, 'keepunused', keepunused, 'feedback', cfg.feedback);
+data = ft_apply_montage(data, montage, 'keepunused', keepunused, 'feedback', cfg.feedback, 'showcallinfo', cfg.showcallinfo);
 
 if isfield(data, 'grad')
   sensfield = 'grad';
@@ -190,7 +190,7 @@ end
 % apply the linear projection also to the sensor description
 if ~isempty(sensfield)
   if  strcmp(cfg.updatesens, 'yes')
-    fprintf('also applying the backprojection matrix to the %s structure\n', sensfield);
+    ft_info('also applying the backprojection matrix to the %s structure\n', sensfield);
 
     % the balance field is needed to keep the sequence of linear projections
     if ~isfield(data.(sensfield), 'balance')
@@ -231,7 +231,7 @@ if ~isempty(sensfield)
     data.(sensfield)  = sens;
     
   else
-    fprintf('not applying the backprojection matrix to the %s structure\n', sensfield);
+    ft_info('not applying the backprojection matrix to the %s structure\n', sensfield);
     % simply copy it over
     comp.(sensfield) = data.(sensfield);
   end
