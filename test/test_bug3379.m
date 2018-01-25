f9c46258352b93224f957291786dff590b779e7b
Robert Oostenveld <r.oostenveld@gmail.com>
2018-01-25 17:16:18 +0100

    Update test_bug3379.m
    made the error detection more robust

--- test/test_bug3379.m
+++ test/test_bug3379.m
@@ -9,17 +9,20 @@ load(dccnpath('/home/common/matlab/fieldtrip/data/test/bug3379.mat'));
 
 
 assert(isequal(tmpcfg.trl(:,1:2), testdata.sampleinfo(setdiff(1:93, [5 6]),:)));
-% since the tmpcfg.trl is the same as testdata.sampleinfo, the call to
+
+% Since the tmpcfg.trl is the same as testdata.sampleinfo, the call to
 % ft_redefinetrial is expected to lead to the same data, which it doesn't
 % note: the data contains overlapping trials.
 % Yet, the overlapping segments of the data are not identical, which should
 % have led to an error in ft_fetch_data, which it didn't. This has now been
 % fixed in ft_fetch_data, so ft_redefinetrial correctly returns an error
 % now, since the discrepancy is detected in ft_fetch_data
+
 try
   testdata2 = ft_redefinetrial(tmpcfg, testdata);
-  error('at this point, there should have been an informative error in FieldTrip');
-catch ME
-  fprintf([ME.message '\n']);
-  assert(isequal(ME.message,'some of the requested samples occur twice in the data and have conflicting values'));
+  % at this point, there should have been an informative error in FieldTrip
+  failed = false;
+catch
+  failed = true;
 end
+assert(failed,'some of the requested samples occur twice in the data and have conflicting values');
