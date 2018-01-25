5e1b3c7db468d21b0371834fe206e893e4bbd96a
StolkArjen <a.stolk8@gmail.com>
2018-01-25 10:24:24 -0800

    changed default to use curv when present

--- plotting/ft_plot_cloud.m
+++ plotting/ft_plot_cloud.m
@@ -133,7 +133,7 @@ facecolor          = ft_getopt(varargin, 'facecolor', [0.781 0.762 0.664]);
 edgecolor          = ft_getopt(varargin, 'edgecolor', 'none');
 facealpha          = ft_getopt(varargin, 'facealpha', 1);
 edgealpha          = ft_getopt(varargin, 'edgealpha', 0);
-vertexcolor        = ft_getopt(varargin, 'vertexcolor', 'none');
+vertexcolor        = ft_getopt(varargin, 'vertexcolor', 'curv');
 
 if rmin < 1 * ft_scalingfactor('mm', unit)
   ft_error('cfg.rmin must be equal or larger than 1 mm');
