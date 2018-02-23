ba94e4e74b43277fd0c7643b921650244d95e718
Kristijan Armeni <kristijan.armeni@gmail.com>
2018-02-23 14:18:56 +0100

    Colormix colorbar (#667)
    * when <colormix> is used, the colorbar axis is now set to [fcolmin fcolmax]
    
    * removed the code that set the ticks, colorscale for <colormix> is now set within ft_plot_mesh higher in the code

--- ft_sourceplot.m
+++ ft_sourceplot.m
@@ -1267,9 +1267,6 @@ switch cfg.method
             set(hc, 'YLim', [fcolmin fcolmax]);
         else
             % functional values have been transformed to be scaled
-            set(hc,'ticks',(0:0.1:1));
-            set(hc,'ticklabels',round(100*linspace(fcolmin,fcolmax,numel(get(hc,'ticks'))'))./100);
-            set(hc,'limits',[0 1]);
         end
       else
         ft_warning('no colorbar possible without functional data')
