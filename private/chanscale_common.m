364cf9bacf0cf332a83453ecf6b45af400dc32ee
Robert Oostenveld <r.oostenveld@gmail.com>
2018-03-31 08:06:28 +0200

    added copyright statement

--- private/chanscale_common.m
+++ private/chanscale_common.m
@@ -23,6 +23,26 @@ function data = chanscale_common(cfg, data)
 %   cfg.mychanscale             = number, scaling to apply to the channels specified in cfg.mychan
 %   cfg.mychan                  = Nx1 cell-array with selection of channels
 
+% Copyright (C) 2017, Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
 cfg.eegscale    = ft_getopt(cfg, 'eegscale');
 cfg.eogscale    = ft_getopt(cfg, 'eogscale');
 cfg.ecgscale    = ft_getopt(cfg, 'ecgscale');
