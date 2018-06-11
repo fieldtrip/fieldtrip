881c3af9e6ae6ec82f0e77cf275b9fa4715f59d1
Robert Oostenveld <r.oostenveld@gmail.com>
2018-06-11 09:45:39 +0200

    automatically synchronized identical files to f416b6397e50154d620fb1679b49467f5d54be35

--- inverse/private/ft_hastoolbox.m
+++ inverse/private/ft_hastoolbox.m
@@ -78,7 +78,8 @@ url = {
   'MATLAB2BESA'  'see http://www.besa.de/downloads/matlab/ and get the "MATLAB to BESA Export functions"'
   'EEPROBE'    'see http://www.ant-neuro.com, or contact Maarten van der Velde'
   'YOKOGAWA'   'this is deprecated, please use YOKOGAWA_MEG_READER instead'
-  'YOKOGAWA_MEG_READER' 'see http://www.yokogawa.com/me/me-login-en.htm'
+  'YOKOGAWA_MEG_READER' 'contact Ricoh engineers'
+  'RICOH_MEG_READER' 'contact Ricoh engineers'
   'BEOWULF'    'see http://robertoostenveld.nl, or contact Robert Oostenveld'
   'MENTAT'     'see http://robertoostenveld.nl, or contact Robert Oostenveld'
   'SON2'       'see http://www.kcl.ac.uk/depsta/biomedical/cfnr/lidierth.html, or contact Malcolm Lidierth'
@@ -239,7 +240,9 @@ switch toolbox
   case 'YOKOGAWA16BITBETA6'
     dependency = @()hasyokogawa('16bitBeta6');
   case 'YOKOGAWA_MEG_READER'
-    dependency = @()hasyokogawa('1.4');
+    dependency = @()hasyokogawa('1.5');
+  case 'RICOH_MEG_READER'
+    dependency = @()hasricoh('1.0');
   case 'BEOWULF'
     dependency = {'evalwulf', 'evalwulf', 'evalwulf'};
   case 'MENTAT'
@@ -377,7 +380,7 @@ switch toolbox
   case 'PLOTLY'
     dependency = {'fig2plotly' 'savejson'};
     
-    % the following are FieldTrip modules/toolboxes
+  % the following are FieldTrip modules/toolboxes
   case 'FILEIO'
     dependency = {'ft_read_header', 'ft_read_data', 'ft_read_event', 'ft_read_sens'};
   case 'FORWARD'
@@ -414,13 +417,13 @@ end
 
 % try to determine the path of the requested toolbox
 if autoadd>0 && ~status
-  
+
   % for core FieldTrip modules
   prefix = fileparts(which('ft_defaults'));
   if ~status
     status = myaddpath(fullfile(prefix, lower(toolbox)), silent);
   end
-  
+
   % for external FieldTrip modules
   prefix = fullfile(fileparts(which('ft_defaults')), 'external');
   if ~status
@@ -432,7 +435,7 @@ if autoadd>0 && ~status
       feval(licensefile);
     end
   end
-  
+
   % for contributed FieldTrip extensions
   prefix = fullfile(fileparts(which('ft_defaults')), 'contrib');
   if ~status
@@ -444,25 +447,25 @@ if autoadd>0 && ~status
       feval(licensefile);
     end
   end
-  
+
   % for linux computers in the Donders Centre for Cognitive Neuroimaging
   prefix = '/home/common/matlab';
   if ~status && isdir(prefix)
     status = myaddpath(fullfile(prefix, lower(toolbox)), silent);
   end
-  
+
   % for windows computers in the Donders Centre for Cognitive Neuroimaging
   prefix = 'h:\common\matlab';
   if ~status && isdir(prefix)
     status = myaddpath(fullfile(prefix, lower(toolbox)), silent);
   end
-  
+
   % use the MATLAB subdirectory in your homedirectory, this works on linux and mac
   prefix = fullfile(getenv('HOME'), 'matlab');
   if ~status && isdir(prefix)
     status = myaddpath(fullfile(prefix, lower(toolbox)), silent);
   end
-  
+
   if ~status
     % the toolbox is not on the path and cannot be added
     sel = find(strcmp(url(:,1), toolbox));
