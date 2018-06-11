6e8c98bf7c17b5709302d3547e65f6b650650052
Robert Oostenveld <r.oostenveld@gmail.com>
2018-06-11 09:42:20 +0200

    ENH - updated location of ricoh160 file in test script

--- test/test_ricoh.m
+++ test/test_ricoh.m
@@ -1,19 +1,21 @@
+function test_ricoh
+
+% WALLTIME 01:00:00
+% MEM 4gb
+
+% TEST hasricoh isricohmegfile read_ricoh_data read_ricoh_event read_ricoh_header ricoh2grad
+
 %% Test function for Ricoh *.con (Third-party exported) data
-
+%
 % Recording data of a session of AEF for right-ear stimulation composed of 100 trials
-% This is an "exported .con" data that includes full of recording information: 
-% MEG data, EEG data, digitized points, acquisition conditions, etc.  
+% This is an "exported .con" data that includes full of recording information:
+% MEG data, EEG data, digitized points, acquisition conditions, etc.
 
-% This script tests hasricoh, isricohmegfile, read_ricoh_data, read_ricoh_event, read_ricoh_header, & ricoh2grad 
-
-% DATA
-%meg_path = < Appropriate path >
-%meg_file = 'rightearAEF_export.con';
-filename = 'rightearAEF_export.con';   % fullfile(meg_path, meg_file)
-
+% filename = 'rightearAEF_export.con';
+filename = dccnpath('/home/common/matlab/fieldtrip/data/test/original/meg/ricoh160/rightearAEF_export.con');
 
 if ~ft_filetype(filename, 'ricoh_con')
-	error('definition error of ricoh_con');
+  error('definition error of ricoh_con');
 end
 
 %% ft_read_header
@@ -24,27 +26,27 @@ if length(ft_channelselection('MEG', hdr.label))~=160
 end
 
 if length(ft_channelselection('EEG*', hdr.label))~=65
-	error('did not select all EEG channels');
+  error('did not select all EEG channels');
 end
-	
+
 
 if ~ft_senstype(hdr, 'yokogawa160')
   error('hdr ~= yokogawa160');
 end
-  
+
 %% ft_preprocessing
 cfg = [];
 cfg.dataset  = filename;
 data = ft_preprocessing(cfg)
 
 %% ft_read_event
-event = ft_read_event(filename,'threshold', 1.6)
+event = ft_read_event(filename, 'threshold', 1.6)
 
 %% Define trials
 cfg = [];
 cfg.dataset          = filename;
 cfg.trialfun  = 'ft_trialfun_general';
-cfg.trialdef.eventtype  = 'triginfo';    
+cfg.trialdef.eventtype  = 'triginfo';
 cfg.trialdef.eventvalue = 'AEF';
 cfg.trialdef.prestim    = 0.1; %sec
 cfg.trialdef.poststim   = 0.5; %sec
@@ -52,7 +54,7 @@ cfg = ft_definetrial(cfg);
 trl = cfg.trl;
 
 if length(trl(:,1))~=100
-	error('could not define trials appropriately');
+  error('could not define trials appropriately');
 end
 
 %% ft_read_headshape
@@ -62,7 +64,5 @@ headshape = ft_read_headshape(filename)
 grad = ft_read_sens(filename)
 
 if length(ft_chanunit(grad,'T'))~=160
-	error('did not select all MEG channels');
+  error('did not select all MEG channels');
 end
-
-
