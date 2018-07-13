e62505c8e8d9dcecc859ebbd1b6aec188161d0c2
Sarang Dalal <sarang@cfin.au.dk>
2018-07-13 17:43:11 +0200

    Moves check and correction for surface normal convention here. This avoids the need to duplicate the code elsewhere in FieldTrip.

--- external/openmeeg/om_save_tri.m
+++ external/openmeeg/om_save_tri.m
@@ -8,6 +8,14 @@ function [] = om_save_tri(filename,points,faces,nrmls)
 
 % Copyright (C) 2010-2017, OpenMEEG developers
 
+% OpenMEEG vs 2.3 and up internally adjusts the convention for surface
+% normals, but OpenMEEG v2.2 expects surface normals to point inwards;
+% this checks and corrects if needed
+ok = checknormals(points,faces);
+if ~ok
+    faces = fliplr(faces);
+end
+
 if nargin<4 || isempty(nrmls)
     nrmls = om_normals(points,faces);
 end
@@ -24,3 +32,26 @@ fprintf(fid,'%g %g %g\n',faces');
 fclose(fid);
 
 end %  function
+
+function ok = checknormals(points,faces)
+% FIXME: this method is rigorous only for star shaped surfaces
+ok = 0;
+% translate to the center
+org = mean(points,1);
+points(:,1) = points(:,1) - org(1);
+points(:,2) = points(:,2) - org(2);
+points(:,3) = points(:,3) - org(3);
+
+w = sum(solid_angle(points, faces));
+
+if w<0 && (abs(w)-4*pi)<1000*eps
+  ok = 0;
+  disp('Your surface normals are outwards oriented, flipping for OpenMEEG')
+elseif w>0 && (abs(w)-4*pi)<1000*eps
+  ok = 1;
+%   ft_warning('your normals are inwards oriented')
+else
+  ft_error('your surface probably is irregular\n')
+  ok = 0;
+end
+end %   function
