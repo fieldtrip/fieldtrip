416822b8da502ded7ac8e7eda67755133020ce82
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2018-08-10 14:50:31 +0200

    ENH - updated test function

--- test/test_ft_headmovement.m
+++ test/test_ft_headmovement.m
@@ -9,6 +9,7 @@ dataset = dccnpath('/home/common/matlab/fieldtrip/data/SubjectRest.ds');
 
 cfg = [];
 cfg.dataset = dataset;
+cfg.method  = 'updatesens';
 data1 = ft_headmovement(cfg);
 
 cfg = [];
@@ -18,14 +19,18 @@ cfg.numclusters = 10;
 data2 = cell(1,cfg.numclusters);
 [data2{:}] = ft_headmovement(cfg);
 
-cfg = [];
-cfg.length = 10;
-data_chunked = ft_redefinetrial(cfg, data1);
-
-cfg = [];
-cfg.dataset = dataset;
-cfg.trl    = data_chunked.sampleinfo;
+cfg          = [];
+cfg.dataset  = dataset;
+cfg.trl      = [(1:1000:100000)' (1000:1000:100000)'];
 cfg.trl(:,3) = 0;
-cfg.method = 'pertrial';
-data3 = cell(1,numel(data_chunked.trial));
+cfg.method = 'pertrial_cluster';
+cfg.numclusters = 10;
+data3 = cell(1,cfg.numclusters);
 [data3{:}] = ft_headmovement(cfg);
+
+cfg         = [];
+cfg.dataset = dataset;
+cfg.trl      = [(1:1000:100000)' (1000:1000:100000)'];
+cfg.trl(:,3) = 0;
+cfg.method  = 'avgoverrpt';
+data4 = ft_headmovement(cfg);
