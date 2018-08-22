d21844204071ffae7f24906fe6d25ade74dcaf27
Robert Oostenveld <r.oostenveld@gmail.com>
2018-08-22 16:05:49 +0200

    added option emptymeaningful, when false the empty fields are removed

--- private/mergeconfig.m
+++ private/mergeconfig.m
@@ -1,18 +1,23 @@
-function input = mergeconfig(input, default)
+function original = mergeconfig(original, default, emptymeaningful)
 
-% MERGECONFIG merges the fields of a default structure into an input
-% configuration structure
+% MERGECONFIG merges the fields of a structure with defaults into an original
+% configuration structure. The default is only copied in case the field is absent in
+% the original.
 %
 % Use as
-%   output = mergeconfig(input, default)
+%   output = mergeconfig(original, default)
 
 % Copyright (C) 2009-2012, Robert Oostenveld
 %
 % $Id$
 
-if isempty(input) && ~isstruct(input)
+if nargin<3
+  emptymeaningful = true;
+end
+
+if isempty(original) && ~isstruct(original)
   % ensure that it is an empty struct, not an empty double
-  input = struct;
+  original = struct;
 end
 
 if isempty(default) && ~isstruct(default)
@@ -20,30 +25,35 @@ if isempty(default) && ~isstruct(default)
   default = struct;
 end
 
-% merge the input with the fields from default
+if ~emptymeaningful
+  original = remove_empty(original);
+  default = remove_empty(default);
+end
+
+% merge the original with the fields from default
 defaultfields = fieldnames(default);
-inputfields = fieldnames(input);
+inputfields = fieldnames(original);
 allfields = union(defaultfields, inputfields);
 
-if numel(default)>1 && numel(default)==numel(input)
+if numel(default)>1 && numel(default)==numel(original)
   % create an empty structure that has all the fields
-  tmp = emptystruct(allfields);
-  for i=1:numel(input)
+  tmp = empty_struct(allfields);
+  for i=1:numel(original)
     for j=1:numel(allfields)
-      if isfield(input(i), allfields{j})
-        tmp(i).(allfields{j}) = input(i).(allfields{j});
+      if isfield(original(i), allfields{j})
+        tmp(i).(allfields{j}) = original(i).(allfields{j});
       else
         tmp(i).(allfields{j}) = default(i).(allfields{j});
       end
     end
   end
-  input = tmp;
+  original = tmp;
   
-elseif numel(default)~=numel(input)
+elseif numel(default)~=numel(original)
   for j=1:numel(allfields)
-    % ensure that the input has all fields
-    if ~isfield(input, allfields{j})
-      input(1).(allfields{j}) = [];
+    % ensure that the original has all fields
+    if ~isfield(original, allfields{j})
+      original(1).(allfields{j}) = [];
     end
     % ensure that the default has all fields
     if ~isfield(default, allfields{j})
@@ -51,31 +61,43 @@ elseif numel(default)~=numel(input)
     end
   end
   % simply concatenate them
-  input = cat(1, input(:), default(:));
+  original = cat(1, original(:), default(:));
   
 else
   for i=1:length(defaultfields)
     fn = defaultfields{i};
-    if  ~isfield(input, fn) && ~isstruct(default.(fn))
+    if  ~isfield(original, fn) && ~isstruct(default.(fn))
       % simply copy the value over
-      input.(fn) = default.(fn);
-    elseif ~isfield(input, fn) &&  isstruct(default.(fn))
+      original.(fn) = default.(fn);
+    elseif ~isfield(original, fn) &&  isstruct(default.(fn))
       % simply copy the substructure over
-      input.(fn) = default.(fn);
-    elseif  isfield(input, fn) && ~isstruct(default.(fn))
+      original.(fn) = default.(fn);
+    elseif  isfield(original, fn) && ~isstruct(default.(fn))
       % do not copy it over, keep the original value
-    elseif  isfield(input, fn) &&  isstruct(default.(fn))
+    elseif  isfield(original, fn) &&  isstruct(default.(fn))
       % merge the two substructures using recursive call
-      input.(fn) = mergeconfig(input.(fn), default.(fn));
+      original.(fn) = mergeconfig(original.(fn), default.(fn));
     end
   end % for all default fields
   
 end % dealing with struct-arrays
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function s = remove_empty(s)
+if isempty(s)
+  return
+else
+  fn = fieldnames(s);
+  fn = fn(structfun(@isempty, s));
+  s = removefields(s, fn);
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SUBFUNCTION
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function s = emptystruct(fn)
+function s = empty_struct(fn)
 s = struct;
 for i=1:numel(fn)
   s.(fn{i}) = [];
