3e6df31bf25faee5f599dd9b6d4809afd7df90f9
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2018-09-14 09:35:11 +0200

    ENH - added multi-trial data as test case

--- test/test_ft_denoise_dssp.m
+++ test/test_ft_denoise_dssp.m
@@ -1,8 +1,9 @@
 function test_ft_denoise_dssp
 
-% TEST blabla
+% MEM 3000mb
+% WALLTIME 00:10:00
 
-
+% TEST ft_denoise_dssp
 
 % below is based on simulated data using Kensuke's code, and should behave as the
 % bdssp_main script that he uses to demonstrate the algorithm
@@ -15,3 +16,6 @@ cfg.dssp.n_space = 30;
 cfg.dssp.n_in    = 30;
 cfg.dssp.n_out   = 30;
 dataout = ft_denoise_dssp(cfg, data);
+
+load(dccnpath('/home/common/matlab/fieldtrip/data/test/pull800/data_lowsnr.mat'));
+dataout2 = ft_denoise_dssp(cfg, data);
