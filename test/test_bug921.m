346f19db43907128ccd5573cd1812e1648b9b7cc
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2018-09-20 16:16:18 +0200

    FIX - obsoleted use of ft_selectdata_old/new

--- test/test_bug921.m
+++ test/test_bug921.m
@@ -7,11 +7,11 @@ function test_bug921
 
 % See also bug 798 that was reported by Yoni, from which I am reusing the data to test this bug
 
-cd(dccnpath('/home/common/matlab/fieldtrip/data/test/bug798'));
-load t2_subj1.mat
-load t2_subj1_null
-load t2_subj2
-load t2_subj2_null
+datadir = (dccnpath('/home/common/matlab/fieldtrip/data/test/bug798'));
+load(fullfile(datadir,'t2_subj1'));
+load(fullfile(datadir,'t2_subj1_null'));
+load(fullfile(datadir,'t2_subj2'));
+load(fullfile(datadir,'t2_subj2_null'));
 
 argin{1} = ft_checkdata(t2_subj1, 'datatype', 'freq');
 argin{2} = ft_checkdata(t2_subj1_null, 'datatype', 'freq');
@@ -20,11 +20,19 @@ argin{4} = ft_checkdata(t2_subj2_null, 'datatype', 'freq');
 
 % the following should select and concatenate only the powspctrm field
 % but not the prob and stat fields
-data = ft_selectdata(argin{:}, 'param', 'powspctrm');
+%data = ft_selectdata(argin{:}, 'param', 'powspctrm');
 
-assert(~isfield(data, 'stat'));
-assert(~isfield(data, 'prob'));
-assert(strcmp(data.dimord, 'rpt_chan_freq'));
+%assert(~isfield(data, 'stat'));
+%assert(~isfield(data, 'prob'));
+%assert(strcmp(data.dimord, 'rpt_chan_freq'));
+
+data = cell(1,numel(argin));
+[data{:}] = ft_selectdata([],argin{:});
+
+cfg = [];
+cfg.parameter = 'powspctrm';
+cfg.appenddim = 'rpt';
+data = ft_appendfreq(cfg, data{:});
 
 assert(size(data.powspctrm,1)==4);
 assert(size(data.powspctrm,2)==274);
