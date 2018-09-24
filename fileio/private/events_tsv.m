06b24f098e84c06412c83a9860719d9bf691cb6f
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2018-09-24 10:46:22 +0200

    ENH - fix to be able to read columns with mixed numeric and string values

--- fileio/private/events_tsv.m
+++ fileio/private/events_tsv.m
@@ -3,8 +3,30 @@ function event = events_tsv(filename)
 % helper function to extract events from a BIDS events.tsv file
 
 opts = detectImportOptions(filename,'filetype','text');
-tsv  = readtable(filename, opts); % this gets the numeric stuff as doubles right away
-tsv  = table2struct(tsv);
+
+% this gets the numeric stuff as doubles right away, yet might erroneously
+% typecast intended strings (e.g. in event_value) into doubles.
+tsv       = readtable(filename, opts); 
+tsv_naive = readtable(filename, 'filetype', 'text');
+
+tsv       = table2struct(tsv);
+tsv_naive = table2struct(tsv_naive);
+
+fn = fieldnames(tsv);
+checkflag = false(1,numel(fn));
+for k = 1:numel(fn)
+  checkflag(1,k) = ~ischar(tsv(1).(fn{k})) && ischar(tsv_naive(1).(fn{k}));
+end
+
+% heuristic: the element can stay double if all elements of the corresponding
+% element in tsv_naive contain only numbers, or nan
+for k = find(checkflag)
+  for m = 1:numel(tsv_naive)
+    if ~is_a_number(tsv_naive(m).(fn{k})) && ~is_a_nan(tsv_naive(m).(fn{k}))
+      tsv(m).(fn{k}) = tsv_naive(m).(fn{k});
+    end
+  end
+end
 
 % a FieldTrip event structure should contain
 % type, value, sample, offset and duration as fields
@@ -40,3 +62,15 @@ if ~isempty(event)
     event(1).duration = [];
   end
 end
+
+
+function bool = is_a_number(input)
+
+pattern = '[0-9\.]';
+bool    = length(regexp(input, pattern))==length(input);
+
+function bool = is_a_nan(input)
+
+pattern = 'nan';
+bool    = ~isempty(regexp(lower(input), pattern));
+
