844363ba4c821f0b3fc26a7de58f7d1203f10648
Robert Oostenveld <r.oostenveld@gmail.com>
2018-10-21 23:23:10 +0200

    better exclusion/inclusion of clock_gettime replacement for SDK 10.12 and 10.14

--- realtime/src/buffer/src/clock_gettime.c
+++ realtime/src/buffer/src/clock_gettime.c
@@ -3,11 +3,14 @@
 #include "platform_includes.h"
 
 #ifdef PLATFORM_OSX
-#ifndef __CLOCK_AVAILABILITY
+#include <Availability.h>
+#if __MAC_OS_X_VERSION_MAX_ALLOWED < 101300
 
 /*
- * OS X did not have clock_gettime for a long time, whereas in the most recent
- * macOS High Sierra and the accompanying XCode with MacOSX10.12.sdk it is available.
+ * OS X did not have clock_gettime for a long time.
+ *
+ * In OS X El Captain and the accompanying XCode with MacOSX10.12.sdk it is not available.
+ * In macOS High Sierra and the accompanying XCode with MacOSX10.14.sdk it is available.
  *
  * This is a drop-in replacement based on https://gist.github.com/jbenet/1087739
  * that uses clock_get_time. The first argument to this function is CLOCK_REALTIME,
@@ -29,7 +32,7 @@ int clock_gettime(int ignore, struct timespec *ts) {
   ts->tv_nsec = mts.tv_nsec;
   return 0;
 }
-#endif
+#endif /* __MAC_OS_X_VERSION_MAX_ALLOWED */
 
 #elif defined (COMPILER_MINGW_ORG)
 
--- realtime/src/buffer/src/osx/clock_gettime.h
+++ realtime/src/buffer/src/osx/clock_gettime.h
@@ -1,11 +1,14 @@
 #include <time.h>
 
 #ifdef PLATFORM_OSX
-#ifndef __CLOCK_AVAILABILITY
+#include <Availability.h>
+#if __MAC_OS_X_VERSION_MAX_ALLOWED < 101300
 
 /*
- * OS X did not have clock_gettime for a long time, whereas in the most recent
- * macOS High Sierra and the accompanying XCode with MacOSX10.12.sdk it is available.
+ * OS X did not have clock_gettime for a long time.
+ *
+ * In OS X El Captain and the accompanying XCode with MacOSX10.12.sdk it is not available.
+ * In macOS High Sierra and the accompanying XCode with MacOSX10.14.sdk it is available.
  *
  * This is a drop-in replacement based on https://gist.github.com/jbenet/1087739
  * that uses clock_get_time. The first argument to this function is CLOCK_REALTIME,
@@ -26,5 +29,5 @@ int clock_gettime(int ignore, struct timespec *tp);
 }
 #endif
 
-#endif /* if __CLOCK_AVAILABILITY */
+#endif /* __MAC_OS_X_VERSION_MAX_ALLOWED */
 #endif /* if PLATFORM_OSX */
