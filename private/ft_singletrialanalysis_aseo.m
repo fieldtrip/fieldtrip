566fe41389c3980ce4f332e82a90288c0651a856
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2018-11-27 16:08:50 +0100

    ENH - moved function to be a subfunction

--- private/ft_singletrialanalysis_aseo.m
+++ private/ft_singletrialanalysis_aseo.m
@@ -507,3 +507,112 @@ if step <0
     seq= [zeros(abs(step), col); seq(1:end+step,:) ];
   end
 end
+
+function [erp_est, amp_est, lat_est, residual_f] = ft_estimate_erp(cfg, data, erp_in, amp_in, lat_in, noise, rejectflag)
+
+% Estimate the ERP waveforms, amplitudes and latencies in the frequency domain
+%
+% INPUT----
+% data:  Observed data samples in frequency domain
+% erp_est    : The most-recent estimates of the ERP waveforms in frequency domain
+% amp_est    : The most-recent estimates of ERP amplitudes
+% lat_est    : The most-recent estimates of ERP latencies
+% noise      : The most=recent estimates of spectrum of on-going activity
+% rejectflag : Reject flags
+%
+% OUTPUT ----
+% erp_est    : Updated estimates of the ERP waveforms in frequency domain
+% amp_est    : Updated estimates of ERP amplitudes
+% lat_est    : Updated estimates of ERP latencies
+% residual_f : Residual signal after removing ERPs in frequency domain
+  
+ntrl          = ft_getopt(cfg.aseo, 'ntrl');
+ncomp         = ft_getopt(cfg.aseo, 'ncomp');
+Nsmp          = size(erp_in, 1); % Sample number and component number
+freqSeq       = 2*pi/(2*Nsmp-2)*(0:(Nsmp-1))';
+  
+invsqrt_noise = 1./sqrt(noise);
+acceptIndex   = find(~rejectflag);
+for k = 1:Nsmp
+  A_tilde = amp_in(acceptIndex,:).*exp(-1i*freqSeq(k)*lat_in(acceptIndex,:)).* (invsqrt_noise(k, acceptIndex).'*ones(1,ncomp)) ;  
+  X_tilde = data(k,acceptIndex).'.*invsqrt_noise(k,acceptIndex).';
+  S_tilde = (A_tilde'* A_tilde)\(A_tilde'*X_tilde);
+       
+  erp_est(k,:) = S_tilde.';   
+end
+     
+% update the latency and amplitude estimates
+lat_est = lat_in;
+amp_est = amp_in;
+for compNo = 1:ncomp
+  [tmp_lat, tmp_amp] = ft_estimate_parameters(cfg, data, erp_est, amp_in, lat_in, compNo, noise);
+  lat_est(:,compNo) = tmp_lat(:,compNo);%+lat_in(:,compNo);
+  amp_est(:,compNo) = tmp_amp(:,compNo);
+end
+      
+% Compute residual signal by removing ERPs
+residual_f      = zeros(Nsmp, ntrl);
+reconstructed_f = zeros(Nsmp, ntrl); 
+for trialNo = 1:ntrl
+  reconstructed_f(:,trialNo) = ( exp(-1i*freqSeq*lat_est(trialNo,:) ).*erp_est )* amp_est(trialNo, :).';
+  residual_f(:,trialNo) = data(:,trialNo) - reconstructed_f(:,trialNo);
+end
+
+function [rejectflag, corr_est] = ft_rejecttrial(cfg, erp_est, amp_est, lat_est, data)
+
+% Description: Reject trials
+%
+% INPUT----
+% erp_est: Estimates of ERP waveforms in frequency domain
+% amp_est: Estimates of ERP components
+% lat_est: Estimates of ERP components
+% data:    Observed data samples in frequency domain
+%
+% OUTPUT----
+% rejectflag : Boolean vector indicating whether or not a trial is rejected
+% corr_est   : Correlation between the original data and the recovered signal
+
+thresholdAmpH   = ft_getopt(cfg.aseo, 'thresholdAmpH'); % maximum acceptable amplitude of trials, times of avergae amplitude
+thresholdAmpL   = ft_getopt(cfg.aseo, 'thresholdAmpL'); % minimum  acceptable amplitude of trials, times of avergae amplitude
+thresholdCorr   = ft_getopt(cfg.aseo, 'thresholdCorr'); % minimum correlation with the original data
+ncomp   = ft_getopt(cfg.aseo, 'ncomp'); 
+ntrl    = ft_getopt(cfg.aseo, 'ntrl');
+
+
+Nsmp = size(data,1); 
+freqSeq      = 2*pi/(2*Nsmp-1)*(0:(Nsmp-1))';
+rejectflag   = false(ntrl,1);
+corr_est     = zeros(ntrl,1);
+
+for k = 1:ncomp
+  rejectflag = rejectflag | sign(amp_est(:,k))~=median(sign(amp_est(:,k)));
+end
+
+% for k = 1:ncomp
+%   amp_est_k    = amp_est(:,k);
+%   sel          = 1:numel(amp_est_k);%sign(amp_est_k)==polarity(k);
+%   amp_est_mean = mean(amp_est_k(sel));
+%   
+%   % check the amplitudes for each trial, but take the polarity into account
+%   if ~isempty(polarity)
+%     switch polarity(k)
+%       case 1
+%         rejectflag = rejectflag | (amp_est_k>thresholdAmpH*amp_est_mean) | (amp_est_k<thresholdAmpL*amp_est_mean); 
+%       case -1
+%         rejectflag = rejectflag | (amp_est_k<thresholdAmpH*amp_est_mean) | (amp_est_k>thresholdAmpL*amp_est_mean);
+%     end
+%   else
+%   
+%     rejectflag = rejectflag|(amp_est_k<thresholdAmpL*amp_est_mean)|(amp_est_k>thresholdAmpH*amp_est_mean);
+%   end
+% end
+
+% check the correlation between the original data samples and estimated ERPs
+for k = 1:ntrl
+  signal = exp(-1i*freqSeq* lat_est(k,:) ).*erp_est;     
+  signal = signal*amp_est(k, :).';
+        
+  corr_tmp      = real(signal'*data(:,k))/norm(signal)/norm(data(:,k)) ; 
+  rejectflag(k) = rejectflag(k) | real(corr_tmp) < thresholdCorr;
+  corr_est(k)   = corr_tmp;
+end
