9c0336ef59a1a4a2607e0595f0bf62f3da3a7060
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2018-11-29 12:58:34 +0100

    ENH - fixed default latency

--- ft_rejectvisual.m
+++ ft_rejectvisual.m
@@ -150,7 +150,7 @@ cfg = ft_checkconfig(cfg, 'renamed',  {'keepchannels',  'keepchannel'});
 % set the defaults
 cfg.channel     = ft_getopt(cfg, 'channel'    , 'all');
 cfg.trials      = ft_getopt(cfg, 'trials'     , 'all', 1);
-cfg.latency     = ft_getopt(cfg, 'latency'    , 'maxperlength');
+cfg.latency     = ft_getopt(cfg, 'latency'    , 'maxperiod');
 cfg.keepchannel = ft_getopt(cfg, 'keepchannel', 'no');
 cfg.keeptrial   = ft_getopt(cfg, 'keeptrial'  , 'no');
 cfg.feedback    = ft_getopt(cfg, 'feedback'   , 'textbar');
