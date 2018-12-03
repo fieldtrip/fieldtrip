9bf9e64b5dc5e46153f1f1ae6e36b34b73bda59a
Robert Oostenveld <r.oostenveld@gmail.com>
2018-12-03 15:20:59 +0100

    the stack is not 3 deep when executed with ft_test

--- test/test_bug3336.m
+++ test/test_bug3336.m
@@ -19,8 +19,7 @@ end
 
 assert(~isequal(err1.message, err2.message))
 assert(isequal(err1.identifier, err2.identifier))
-assert(numel(err1.stack)==3)
-assert(numel(err2.stack)==3)
+assert(numel(err1.stack)==numel(err2.stack))
 
 % idem for warnings
 % but here I cannot get access to the stack
