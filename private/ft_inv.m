5d3b285abb50a3af0b35f00f914a7d906327c0cb
Robert Oostenveld <r.oostenveld@gmail.com>
2019-02-06 15:47:30 +0100

    fixed typo

--- private/ft_inv.m
+++ private/ft_inv.m
@@ -64,7 +64,7 @@ function Y = ft_inv(X, varargin)
 %
 %    FieldTrip is distributed in the hope that it will be useful,
 %    but WITHOUT ANY WARRANTY; without even the implied warranty of
-%    MERCHANTABILITY or FITNESS FOR X PARTICULAR PURPOSE.  See the
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %    GNU General Public License for more details.
 %
 %    You should have received a copy of the GNU General Public License
