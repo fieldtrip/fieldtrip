a1ae21a1ebdd76434c127221be4fc66c9f916db6
Robert Oostenveld <r.oostenveld@gmail.com>
2019-02-14 15:54:14 +0100

    automatically synchronized identical files to 6de0a699372b3bc06f54b9075203244392285f9a

--- private/ignorefields.m
+++ private/ignorefields.m
@@ -114,6 +114,7 @@ switch purpose
       'checksize'
       'debug'
       'notification'
+      'reproducescript'
       'showcallinfo'
       'trackcallinfo'
       'trackconfig'
@@ -129,6 +130,8 @@ switch purpose
       'checksize'
       'debug'
       'notification'
+      'callinfo'
+      'version'
       'showcallinfo'
       'trackcallinfo'
       'trackconfig'
@@ -144,6 +147,7 @@ switch purpose
       'grad'
       'elec'
       'opto'
+      'event'
       };
 
   case 'trackconfig'
