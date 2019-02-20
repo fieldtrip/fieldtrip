20934ddd3645286c001780bf0bb938af84719a92
Robert Oostenveld <r.oostenveld@gmail.com>
2019-02-20 13:33:59 +0100

    improve detection for non-builtin functions that are provided by MATLAB, also implemented and tested for "istable"

--- compat/matlablt2013b/istable.m
+++ compat/matlablt2013b/istable.m
@@ -1,7 +1,27 @@
-function s = istable(varargin)
+function [varargout] = istable(varargin)
 
 % ISTABLE is a drop-in replacement for the same function that was
 % introduced in MATLAB R2013b. In versions prior to 2013b this function
 % returns false.
 
-s = false;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% see https://github.com/fieldtrip/fieldtrip/issues/899
+
+if exist(mfilename, 'builtin') || any(strncmp(which(mfilename, '-all'), matlabroot, length(matlabroot)))
+  % remove this directory from the path
+  p = fileparts(mfilename('fullpath'));
+  ft_warning('removing %s from your path, see http://bit.ly/2SPPjUS', p);
+  rmpath(p);
+  % call the original MATLAB function
+  if exist(mfilename, 'builtin')
+    [varargout{1:nargout}] = builtin(mfilename, varargin{:});
+  else
+    [varargout{1:nargout}] = feval(mfilename, varargin{:});
+  end
+  return
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% deal with the output arguments
+
+varargout = {false};
--- compat/matlablt2016b/contains.m
+++ compat/matlablt2016b/contains.m
@@ -10,13 +10,17 @@ function [varargout] = contains(varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % see https://github.com/fieldtrip/fieldtrip/issues/899
 
-if exist(mfilename, 'builtin') || strncmp(mfilename('fullpath'), matlabroot, length(matlabroot))
+if exist(mfilename, 'builtin') || any(strncmp(which(mfilename, '-all'), matlabroot, length(matlabroot)))
   % remove this directory from the path
   p = fileparts(mfilename('fullpath'));
   ft_warning('removing %s from your path, see http://bit.ly/2SPPjUS', p);
   rmpath(p);
   % call the original MATLAB function
-  [varargout{1:nargout}] = builtin(mfilename, varargin{:});
+  if exist(mfilename, 'builtin')
+    [varargout{1:nargout}] = builtin(mfilename, varargin{:});
+  else
+    [varargout{1:nargout}] = feval(mfilename, varargin{:});
+  end
   return
 end
 
