c3ed015c774b4155de95d7e1a37f1113a4dc4970
Robert Oostenveld <r.oostenveld@gmail.com>
2019-02-20 13:17:20 +0100

    improved handling of builtin or MATLAB function

--- compat/matlablt2016b/contains.m
+++ compat/matlablt2016b/contains.m
@@ -1,4 +1,4 @@
-function tf = contains(s, pattern, str, boolean)
+function [varargout] = contains(varargin)
 
 %CONTAINS True if text contains a pattern.
 %   TF = contains(S,PATTERN) returns true if any element of string array S
@@ -8,12 +8,25 @@ function tf = contains(s, pattern, str, boolean)
 % MATLAB versions prior to 2016b.
 
 % see https://github.com/fieldtrip/fieldtrip/issues/899
-if exist(mfilename, 'builtin')
-  [p, f, x] = fileparts(mfilename('fullpath'));
+if exist(mfilename, 'builtin') || strncmp(mfilename('fullpath'), matlabroot, length(matlabroot))
+  % remove this directory from the path
+  p = fileparts(mfilename('fullpath'));
   ft_warning('removing incorrect directory %s from your path', p)
   rmpath(p);
-  tf = builtin('contains', s, pattern, str, boolean);
+  % call the original MATLAB function
+  [varargout{1:nargout}] = builtin(mfilename, varargin{:});
   return
+end
+
+% deal with the input arguments
+if nargin==1
+  [s                       ] = deal(varargin{1:1});
+elseif nargin==2
+  [s, pattern              ] = deal(varargin{1:2});
+elseif nargin==3
+  [s, pattern, str         ] = deal(varargin{1:3});
+elseif nargin==4
+  [s, pattern, str, boolean] = deal(varargin{1:4});
 end
 
 if ~ischar(s) && ~iscellstr(s)
@@ -44,3 +57,5 @@ end
 
 tf = ~cellfun(@isempty, strfind(s, pattern));
 
+% deal with the output arguments
+varargout = {tf};
