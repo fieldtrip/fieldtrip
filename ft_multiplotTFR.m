8cf580a6eac36721885864637d9e447e6b89a11d
Mats <m.vanes@donders.ru.nl>
2019-03-18 14:51:17 +0100

    Update ft_multiplotTFR.m
    only give necessary configuration options to ft_prepare_layout

--- ft_multiplotTFR.m
+++ ft_multiplotTFR.m
@@ -338,7 +338,7 @@ data = chanscale_common(tmpcfg, data);
 %% Section 3: select the data to be plotted and determine min/max range
 
 % Read or create the layout that will be used for plotting
-tmpcfg = removefields(cfg, 'inputfile'); % ensure the inputfile field not to exist
+tmpcfg = keepfields(cfg, {'layout', 'elec', 'grad', 'opto', 'showcallinfo'});
 cfg.layout = ft_prepare_layout(tmpcfg, data);
 
 % Take the subselection of channels that is contained in the layout, this is the same in all datasets
