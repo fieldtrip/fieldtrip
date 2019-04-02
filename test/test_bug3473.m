c499bc45da0012be77c1709e73e6924809ca1684
Simon Homölle <simhom@dccn.nl>
2019-04-02 13:37:25 +0200

    update test_bug3473

--- test/test_bug3473.m
+++ test/test_bug3473.m
@@ -3,8 +3,9 @@ function test_bug3473
 % WALLTIME 00:10:00
 % MEM 2gb
 
-% DEPENDENCY ft_prepare_mesh 
+% DEPENDENCY ft_prepare_mesh
 
+addpath(genpath(dccnpath('/home/common/matlab/fieldtrip/data/test/bug3473/cpd')));
 filename = dccnpath('/home/common/matlab/fieldtrip/data/ftp/tutorial/epilepsy/case1/ctf_data/case1.pos');
 
 polhemus = ft_read_headshape(filename);
@@ -13,12 +14,61 @@ polhemus = ft_convert_units(polhemus,'mm');
 template = ft_read_vol('standard_bem.mat');
 template = ft_convert_units(template,'mm');
 
+m = [0.0000    0.9000         0   40.5000;
+    -0.8966    0.0000    0.0784         0;
+    0.0784   -0.0000    0.8966   18.0000;
+    0         0         0    1.0000];
+template = ft_transform_geometry(m, template);
+
+% deface the head surfaces, here I use only the undefaced meshes, defaced
+% ones would need an interactive function
+defaced_template      = template.bnd(1);
+defaced_template.unit = template.unit;
+
+defaced_polhemus = polhemus;
+
+ft_plot_mesh(defaced_template);
+ft_plot_mesh(defaced_polhemus);
+
+cfg=[];
+cfg.method='singlesphere';
+template_sphere = ft_prepare_headmodel(cfg, defaced_template);
+
+cfg=[];
+cfg.method = 'singlesphere';
+polhemus_sphere = ft_prepare_headmodel(cfg, defaced_polhemus);
+
+scale = polhemus_sphere.r/template_sphere.r;
+
+T2 = [1 0 0 template_sphere.o(1);
+    0 1 0 template_sphere.o(2);
+    0 0 1 template_sphere.o(3);
+    0 0 0 1                ];
+
+T1 = [1 0 0 -template_sphere.o(1);
+    0 1 0 -template_sphere.o(2);
+    0 0 1 -template_sphere.o(3);
+    0 0 0 1                 ];
+
+S  = [scale 0 0 0;
+    0 scale 0 0;
+    0 0 scale 0;
+    0 0 0 1 ];
+
+transformation = T1*S*T2;
+
+template_sphere = ft_transform_geometry(transformation, template);
+
 cfg             = [];
 cfg.headshape   = polhemus;
-cfg.template    = template.bnd(1);
+cfg.template    = defaced_template;
 cfg.method      = 'fittemplate';
 fitted          = ft_prepare_mesh(cfg, template.bnd);
 
 cfg = [];
-cfg.method = 'bemcp';
-headmodel_bem = ft_prepare_headmodel(cfg, fitted);
+cfg.method = 'openmeeg';
+headmodel_sphere = ft_prepare_headmodel(cfg, template_sphere);
+
+cfg = [];
+cfg.method = 'openmeeg';
+headmodel_surface = ft_prepare_headmodel(cfg, template_surface);
