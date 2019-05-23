2e4d444f03a68d91acd7b2f0cce31e3107e39036
Robert Oostenveld <r.oostenveld@gmail.com>
2019-05-23 12:40:40 +0200

    moved the examples frmo data2bids over to the test script

--- test/test_data2bids.m
+++ test/test_data2bids.m
@@ -5,6 +5,86 @@ function test_data2bids
 
 % DEPENDENCY data2bids
 
+%% Example with a CTF dataset on disk that needs no conversion
+cd(dccnpath('/home/common/matlab/fieldtrip/data/test/data2bids/decorate'))
+
+cfg = [];
+% cfg.method                      = 'decorate';
+% cfg.outputfile                  = 'Subject01.ds';
+cfg.dataset                     = 'Subject01.ds';
+cfg.TaskName                    = 'language';
+cfg.meg.PowerLineFrequency      = 50;
+cfg.InstitutionName             = 'Radboud University';
+cfg.InstitutionalDepartmentName = 'Donders Institute for Brain, Cognition and Behaviour';
+data2bids(cfg)
+
+%% Example with a NeuroScan EEG dataset on disk that needs to be converted
+cd(dccnpath('/home/common/matlab/fieldtrip/data/test/data2bids/convert'))
+
+cfg = [];
+cfg.method                      = 'convert';
+cfg.dataset                     = 'Subject1_MP.cnt';
+cfg.outputfile                  = 'sub-MP_task-visual_eeg.vhdr';
+cfg.eeg.writesidecar            = 'replace';
+cfg.channels.writesidecar       = 'replace';
+cfg.events.writesidecar         = 'replace';
+cfg.TaskName                    = 'visual';
+cfg.TaskDescription             = 'Visual response task';
+cfg.Instructions                = 'Press as fast with your right index finger as you see the target appear on screen.';
+cfg.InstitutionName             = 'Radboud University';
+cfg.InstitutionalDepartmentName = 'Donders Institute for Brain, Cognition and Behaviour';
+cfg.channels.status             = 'good'; % all channels are good
+data2bids(cfg)
+
+%% Example with preprocessed EEG data in memory
+cd(dccnpath('/home/common/matlab/fieldtrip/data/test/data2bids/convert'))
+
+cfg = [];
+cfg.dataset                     = 'Subject1_MP.cnt';
+cfg.bpfilter                    = 'yes';
+cfg.bpfreq                      = [0.1 40];
+data = ft_preprocessing(cfg);
+
+cfg = [];
+cfg.outputfile                  = 'sub-MP_task-visual_eeg.vhdr';
+cfg.eeg.writesidecar            = 'yes';
+cfg.channels.writesidecar       = 'yes';
+cfg.events.writesidecar         = 'yes';
+cfg.TaskName                    = 'visual';
+cfg.TaskDescription             = 'Visual response task';
+cfg.Instructions                = 'Press as fast with your right index finger as you see the target appear on screen.';
+cfg.InstitutionName             = 'Radboud University';
+cfg.InstitutionalDepartmentName = 'Donders Institute for Brain, Cognition and Behaviour';
+data2bids(cfg, data)
+
+%% Example with an anatomical MRI on disk that needs no conversion
+cd(dccnpath('/home/common/matlab/fieldtrip/data/test/data2bids/decorate'))
+
+cfg = [];
+% cfg.method                      = 'decorate';
+% cfg.outputfile                  = 'oostenveld_r.nii';
+cfg.dataset                     = 'oostenveld_r.nii';
+cfg.mri.dicomfile               = 'ERIVDBER_030731_R.OOSTERVELD.MR.PAUGAA_ANATOMICAL-3D.2.142.2003.7.31.11.19.16.140000.53832621.IMA';
+cfg.mri.StationName             = 'Sonata';
+cfg.InstitutionName             = 'Radboud University';
+cfg.InstitutionalDepartmentName = 'Donders Institute for Brain, Cognition and Behaviour';
+
+cfg.mri.writesidecar            = 'merge';
+data2bids(cfg)
+
+
+%% Example with realigned and resliced anatomical MRI data in memory
+cd(dccnpath('/home/common/matlab/fieldtrip/data/test/data2bids/convert'))
+
+mri = ft_read_mri('dicom/ERIVDBER_030731_R.OOSTERVELD.MR.PAUGAA_ANATOMICAL-3D.2.56.2003.7.31.11.19.16.468000.53834351.IMA');
+
+cfg = [];
+cfg.method                      = 'convert';
+cfg.outputfile                  = 'sub-RO_T1w.nii';
+cfg.InstitutionName             = 'Radboud University';
+cfg.InstitutionalDepartmentName = 'Donders Institute for Brain, Cognition and Behaviour';
+data2bids(cfg, mri)
+
 %%
 cd(dccnpath('/home/common/matlab/fieldtrip/data/test/data2bids/mous'));
 
