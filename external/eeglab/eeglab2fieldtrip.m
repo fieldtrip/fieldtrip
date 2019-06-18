cc2c9eb56dd4cb26fd4bc9361dd6f05da7ecd5b2
arno <>
2019-06-18 18:09:07 +0200

    New improved options for support of Fieldtrip raw, timelock and comp data type

--- external/eeglab/eeglab2fieldtrip.m
+++ external/eeglab/eeglab2fieldtrip.m
@@ -1,27 +1,32 @@
 % eeglab2fieldtrip() - This function converts EEGLAB datasets to Fieldtrip
-%                      for source localization (DIPFIT). IN PRACTICE TO 
-%                      CONVERT DATASETS, IT IS RECOMMENDED TO USE FILE-IO
-%                      see https://sccn.ucsd.edu/wiki/EEGLAB_and_Fieldtrip
+%                      for source localization (DIPFIT).
 %
 % Usage:
-%  >> [header,dat,evt] = eeglab2fieldtrip( EEG, fieldbox, transform );
+%  >> data = eeglab2fieldtrip( EEG, fieldbox, transform );
 %
 % Inputs:
 %   EEG       - [struct] EEGLAB structure
-%   fieldbox  - ['preprocessing'|'freqanalysis'|'timelockanalysis'|'componentanalysis']
+%   fieldbox  - ['raw'|'timelock'|'comp'] for reasons, you can also
+%               specify 'preprocessing' (raw), 'timelockanalysis' (timelock)
+%               and 'componentanalysis' (comp).
 %   transform - ['none'|'dipfit'] transform channel locations for DIPFIT
 %               using the transformation matrix in the field
 %               'coord_transform' of the dipfit substructure of the EEG
 %               structure.
 % Outputs:
-%   header    - FIELDTRIP header structure
-%   dat       - raw data (compatible with Fieldtrip)
-%   evt       - FIELDTRIP event structure
+%   data      - FIELDTRIP data structure (see data type functions below)
+%
+% Note:
+%   For conversion of low level raw data structures, save the EEGLAB
+%   dataset and use:
+%      hdr    = ft_read_header( EEGLABFILE ); 
+%      data   = ft_read_data(   EEGLABFILE, 'header', hdr ); 
+%      events = ft_read_event(  EEGLABFILE, 'header', hdr );
 %
 % Author: Robert Oostenveld, F.C. Donders Centre, May, 2004.
 %         Arnaud Delorme, SCCN, INC, UCSD
 %
-% See also:
+% See also: FT_DATATYPE_RAW, FT_DATATYPE_TIMELOCK, FT_DATATYPE_COMP
 
 % Copyright (C) 2004 Robert Oostenveld, F.C. Donders Centre, roberto@smi.auc.dk
 %
@@ -39,20 +44,18 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-function [header,data,evt] = eeglab2fieldtrip(EEG, fieldbox, transform)
+function data = eeglab2fieldtrip(EEG, fieldbox, transform)
 
 if nargin < 2
     help eeglab2fieldtrip
     return;
-end;
+end
 
-% start with an empty header object
-header = [];
+% start with an empty data object
+data = [];
 
 % add the objects that are common to all fieldboxes
 tmpchanlocs  = EEG.chanlocs;
-header.label   = { tmpchanlocs(EEG.icachansind).labels };
-header.fsample = EEG.srate;
 
 % get the electrode positions from the EEG structure: in principle, the number of
 % channels can be more or less than the number of channel locations, i.e. not
@@ -70,86 +73,94 @@ if strcmpi(fieldbox, 'chanloc_withfid')
             for f = 1:length( fields )
                 EEG.chanlocs = setfield(EEG.chanlocs, { ind }, fields{f}, ...
                     getfield( EEG.chaninfo.nodatchans, { index },  fields{f}));
-            end;
-        end;
-    end;
-end;
+            end
+        end
+    end
+end
 
-header.elec.pnt   = zeros(length( EEG.chanlocs ), 3);
-for ind = 1:length( EEG.chanlocs )
-    header.elec.label{ind} = EEG.chanlocs(ind).labels;
-    if ~isempty(EEG.chanlocs(ind).X)
-        header.elec.pnt(ind,1) = EEG.chanlocs(ind).X;
-        header.elec.pnt(ind,2) = EEG.chanlocs(ind).Y;
-        header.elec.pnt(ind,3) = EEG.chanlocs(ind).Z;
-    else
-        header.elec.pnt(ind,:) = [0 0 0];
-    end;
-end;
+if ~isempty(EEG.chanlocs) && ~isempty(EEG.chanlocs(1).X)
+    data.elec.elecpos = zeros(length( EEG.chanlocs ), 3);
+    for ind = 1:length( EEG.chanlocs )
+        data.elec.label{ind} = EEG.chanlocs(ind).labels;
+        if ~isempty(EEG.chanlocs(ind).X)
+            data.elec.elecpos(ind,1) = EEG.chanlocs(ind).X;
+            data.elec.elecpos(ind,2) = EEG.chanlocs(ind).Y;
+            data.elec.elecpos(ind,3) = EEG.chanlocs(ind).Z;
+        else
+            data.elec.elecpos(ind,:) = [0 0 0];
+        end
+    end
+end
 
 if nargin > 2
     if strcmpi(transform, 'dipfit')
         if ~isempty(EEG.dipfit.coord_transform)
             disp('Transforming electrode coordinates to match head model');
             transfmat = traditionaldipfit(EEG.dipfit.coord_transform);
-            header.elec.pnt = transfmat * [ header.elec.pnt ones(size(header.elec.pnt,1),1) ]';
-            header.elec.pnt = header.elec.pnt(1:3,:)';
+            data.elec.elecpos = transfmat * [ data.elec.elecpos ones(size(data.elec.elecpos,1),1) ]';
+            data.elec.elecpos = data.elec.elecpos(1:3,:)';
         else
             disp('Warning: no transformation of electrode coordinates to match head model');
-        end;
-    end;
-end;
+        end
+    end
+end
 
 switch fieldbox
-    case 'preprocessing'
+    case { 'preprocessing' 'raw' }
+        data.fsample = EEG.srate;
         for index = 1:EEG.trials
-            header.trial{index}  = EEG.data(:,:,index);
-            header.time{index}   = linspace(EEG.xmin, EEG.xmax, EEG.pnts); % should be checked in FIELDTRIP
-        end;
-        header.label   = { tmpchanlocs(1:EEG.nbchan).labels };
+            data.trial{index}  = EEG.data(:,:,index);
+            data.time{index}   = linspace(EEG.xmin, EEG.xmax, EEG.pnts); % should be checked in FIELDTRIP
+        end
+        data.label = getchanlabels(tmpchanlocs(1:EEG.nbchan));
+        if EEG.trials > 1
+            res = std_maketrialinfo([], EEG);
+            data.trialinfo = struct2table(res.datasetinfo.trialinfo);
+        else
+            res = struct('subject', EEG.subject, 'condition', EEG.condition, 'group', EEG.group, 'session', EEG.session);
+            data.trialinfo = struct2table(res);
+            if isempty(data.trialinfo)
+                data = rmfield(data, 'trialinfo');
+            end
+        end
         
+    case { 'timelock' 'timelockanalysis' }
+        data.avg  = mean(EEG.data, 3);
+        data.var  = std(EEG.data, [], 3).^2;
+        data.time = linspace(EEG.xmin, EEG.xmax, EEG.pnts); % should be checked in FIELDTRIP
+        data.label = getchanlabels(tmpchanlocs(1:EEG.nbchan));
         
-    case 'timelockanalysis'
-        header.avg  = mean(EEG.data, 3);
-        header.var  = std(EEG.data, [], 3).^2;
-        header.time = linspace(EEG.xmin, EEG.xmax, EEG.pnts); % should be checked in FIELDTRIP
-        header.label   = { tmpchanlocs(1:EEG.nbchan).labels };
-        
-    case 'componentanalysis'
+    case { 'comp' 'componentanalysis' }
         if isempty(EEG.icaact)
             icaacttmp = eeg_getica(EEG);
         end
         for index = 1:EEG.trials
-            % the trials correspond to the raw header trials, except that they
+            % the trials correspond to the raw data trials, except that they
             % contain the component activations
             try
                 if isempty(EEG.icaact)
-                    header.trial{index} = icaacttmp(:,:,index); % Using icaacttmp to not change EEG structure
+                    data.trial{index} = icaacttmp(:,:,index); % Using icaacttmp to not change EEG structure
                 else
-                    header.trial{index}  = EEG.icaact(:,:,index);
+                    data.trial{index}  = EEG.icaact(:,:,index);
                 end
             catch
                 
-            end;
-            header.time{index}   = linspace(EEG.xmin, EEG.xmax, EEG.pnts); % should be checked in FIELDTRIP
-        end;
-        header.label = [];
+            end
+            data.time{index}   = linspace(EEG.xmin, EEG.xmax, EEG.pnts); % should be checked in FIELDTRIP
+        end
+        data.label = [];
         for comp = 1:size(EEG.icawinv,2)
-            % the labels correspond to the component activations that are stored in header.trial
-            header.label{comp} = sprintf('ica_%03d', comp);
+            % the labels correspond to the component activations that are stored in data.trial
+            data.label{comp} = sprintf('ica_%03d', comp);
         end
         % get the spatial distribution and electrode positions
         tmpchanlocs    = EEG.chanlocs;
-        header.topolabel = { tmpchanlocs(EEG.icachansind).labels };
-        header.topo      = EEG.icawinv;
+        data.topolabel = { tmpchanlocs(EEG.icachansind).labels };
+        data.topo      = EEG.icawinv;
 		% Copy weights & sphere, too
-    	header.weights = EEG.icaweights;
-    	header.sphere  = EEG.icasphere;
+    	data.unmixing = EEG.icaweights*EEG.icasphere;
         
     case { 'chanloc' 'chanloc_withfid' }
-        
-    case 'freqanalysis'
-        error('freqanalysis fieldbox not implemented yet')
         
     otherwise
         error('unsupported fieldbox')
@@ -157,14 +168,21 @@ end
 
 try
     % get the full name of the function
-    header.cfg.version.name = mfilename('fullpath');
+    data.cfg.version.name = mfilename('fullpath');
 catch
     % required for compatibility with Matlab versions prior to release 13 (6.5)
     [st, i] = dbstack;
-    header.cfg.version.name = st(i);
+    data.cfg.version.name = st(i);
 end
 
-data = EEG.data;
+% convert EEGLAB channel labels to Fieldtrip channel labels
+function label = getchanlabels(tmpchanlocs)
+if ~isempty(tmpchanlocs)
+    label   = { tmpchanlocs.labels };
+else
+    for iChan = 1:EEG.nbchan
+        label{iChan} = num2str(iChan);
+    end
+end
 
-% convert event structure
 
