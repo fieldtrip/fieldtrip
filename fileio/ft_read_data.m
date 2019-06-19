042349d44c168e9e434742c881c1813d7b6e53a2
Robert Oostenveld <r.oostenveld@gmail.com>
2019-06-19 12:44:32 +0200

    FIX #1146 - removed 2nd occurence of blackrock_nsx

--- fileio/ft_read_data.m
+++ fileio/ft_read_data.m
@@ -1447,22 +1447,7 @@ switch dataformat
       ft_hastoolbox('yokogawa', 1); % error if it cannot be added
       dat = read_yokogawa_data(filename, hdr, begsample, endsample, chanindx);
     end
-    
-  case 'blackrock_nsx'
-    % use the NPMK toolbox for the file reading
-    ft_hastoolbox('NPMK', 1);
-    
-    % ensure that the filename contains a full path specification,
-    % otherwise the low-level function fails
-    [p,f,e] = fileparts(filename);
-    if ~isempty(p)
-      % this is OK
-    elseif isempty(p)
-      filename = which(filename);
-    end
-    orig = openNSx(filename, 'duration', [begsample endsample], 'channels', chanindx);
-    dat  = double(orig.Data);
-    
+        
   otherwise
     % attempt to run dataformat as a function
     % in case using an external read function was desired, this is where it is executed
