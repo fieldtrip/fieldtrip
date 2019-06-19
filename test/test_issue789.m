91658fad4f88d460a084d0dd380ff8a9a88d47b9
Robert Oostenveld <r.oostenveld@gmail.com>
2019-06-19 11:11:14 +0200

    implemented support for reading multiple streams from an XDF file, see #789

---
+++ test/test_issue789.m
@@ -0,0 +1,20 @@
+function test_issue789
+
+% WALLTIME 00:10:00
+% MEM 2gb
+% DEPENDENCY xdf2fieldtrip ft_read_header ft_read_data ft_read_event
+
+filename = dccnpath('/home/common/matlab/fieldtrip/data/test/original/xdf/example_EEG_eyeTracking_rigidBody.xdf');
+
+%%
+% test the combination of multiple streams, which resamples them to a common time axis
+
+data = xdf2fieldtrip(filename);
+
+%%
+% test the reading of a single EEG stream
+
+cfg = [];
+cfg.dataset = filename;
+data = ft_preprocessing(cfg);
+
