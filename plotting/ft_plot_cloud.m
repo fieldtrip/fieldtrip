177b05097c6ac9e6548971e45e2d46ec6cbb429f
StolkArjen <a.stolk8@gmail.com>
2019-08-02 23:20:44 -0700

    fix: color scaling 2

--- plotting/ft_plot_cloud.m
+++ plotting/ft_plot_cloud.m
@@ -314,11 +314,11 @@ else
   cmapsc = cmap;
 end
 
-cmid   = size(cmapsc,1)/2;                       % colorbar middle
-colscf = 2*((val-clim(1))./(clim(2)-clim(1)))-1; % color between -1 and 1, used when colorgrad = 'white'
-colscf(colscf>1)=1; colscf(colscf<-1)=-1;        % clamp values outside the [-1 1] range, i.e. [clim(1) clim(2)]
-radscf = (val-clim(1))./(clim(2)-clim(1));       % radius between 0 and 1, used when colorgrad = scalar
-radscf(radscf>1)=1; radscf(radscf<0)=0;          % clamp values outside the [0 1] range, i.e. [0 clim(2)]
+cmid   = size(cmapsc,1)/2;                          % colorbar middle
+colscf = 2*( (val-clim(1)) / (clim(2)-clim(1)) )-1; % color between -1 and 1, bottom vs. top colorbar
+colscf(colscf>1)=1; colscf(colscf<-1)=-1;           % clip values outside the [-1 1] range
+radscf = val-min(abs(val));                         % radius between 0 and 1, small vs. large pos/neg effect
+radscf = abs( radscf / max(abs(radscf)) );
 
 if strcmp(scalerad, 'yes')
   rmax = rmin+(radius-rmin)*radscf; % maximum radius of the clouds
