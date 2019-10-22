419ee89358c2923118ae0fe3e80803c201018773
Robert Oostenveld <r.oostenveld@gmail.com>
2019-10-22 23:36:42 +0200

    fixed some unicode characters, use a standard ASCII character instead

--- external/artinis/private/ft_nirs_optodedistance.m
+++ external/artinis/private/ft_nirs_optodedistance.m
@@ -20,8 +20,8 @@ function optodedistance = ft_nirs_optodedistance(datain)
 % -----------------------------------
 % You are free to:
 %
-%     Share � copy and redistribute the material in any medium or format
-%     Adapt � remix, transform, and build upon the material
+%     Share - copy and redistribute the material in any medium or format
+%     Adapt - remix, transform, and build upon the material
 %     for any purpose, even commercially.
 %
 %     The licensor cannot revoke these freedoms as long as you follow the
@@ -29,16 +29,16 @@ function optodedistance = ft_nirs_optodedistance(datain)
 %
 % Under the following terms:
 %
-%     Attribution � You must give appropriate credit, provide a link to
+%     Attribution - You must give appropriate credit, provide a link to
 %                    the license, and indicate if changes were made. You
 %                    may do so in any reasonable manner, but not in any way
 %                    that suggests the licensor endorses you or your use.
 %
-%     ShareAlike � If you remix, transform, or build upon the material,
+%     ShareAlike - If you remix, transform, or build upon the material,
 %                   you must distribute your contributions under the same
 %                   license as the original.
 %
-%     No additional restrictions � You may not apply legal terms or
+%     No additional restrictions - You may not apply legal terms or
 %                                   technological measures that legally
 %                                   restrict others from doing anything the
 %                                   license permits.
