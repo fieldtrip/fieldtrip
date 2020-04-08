231b03ef8822289a96f1c04b9915b7c34fb13ffd
Robert Oostenveld <r.oostenveld@gmail.com>
2020-04-08 18:16:19 +0200

    also test the plotting without headshape

--- test/inspect_issue1368.m
+++ test/inspect_issue1368.m
@@ -9,20 +9,25 @@ load(dccnpath('/home/common/matlab/fieldtrip/data/test/issue1368/elec.mat'))
 
 %%
 
+ft_plot_sens(elec, 'elecshape', 'point', 'headshape', []);
 ft_plot_sens(elec, 'elecshape', 'point', 'headshape', hull);
 
 %%
 
+ft_plot_sens(elec, 'elecshape', 'circle', 'headshape', []);
 ft_plot_sens(elec, 'elecshape', 'circle', 'headshape', hull);
 
 %%
 
+ft_plot_sens(elec, 'elecshape', 'square', 'headshape', []);
 ft_plot_sens(elec, 'elecshape', 'square', 'headshape', hull);
 
 %%
 
+ft_plot_sens(elec, 'elecshape', 'sphere', 'headshape', []);
 ft_plot_sens(elec, 'elecshape', 'sphere', 'headshape', hull);
 
 %%
 
+ft_plot_sens(elec, 'elecshape', 'disc', 'headshape', []);
 ft_plot_sens(elec, 'elecshape', 'disc', 'headshape', hull);
