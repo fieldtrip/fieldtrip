7556680cf46a2f9f068683727851413101d8dea3
Robert Oostenveld <r.oostenveld@gmail.com>
2020-04-11 12:13:38 +0200

    also test with ft_preprocessing, also test c3d format

--- test/test_pull1374.m
+++ test/test_pull1374.m
@@ -2,9 +2,27 @@ function test_pull1374
 
 # WALLTIME 00:10:00
 # MEM 2gb
-# DEPENDENCY xsens_mvnx
+# DEPENDENCY xsens_mvnx motion_c3d
+
+%% 
 
 filename=dccnpath('/home/common/matlab/fieldtrip/data/test/original/motion/xsens/pull1374.mvnx');
 
 ft_read_header(filename);
 ft_read_data(filename);
+
+cfg = [];
+cfg.dataset = filename;
+data = ft_preprocessing(cfg);
+
+%%
+
+filename=dccnpath('/home/common/matlab/fieldtrip/data/test/original/motion/xsens/sub-03_rec-01_take-02_motion.c3d');
+
+ft_read_header(filename);
+ft_read_data(filename);
+
+cfg = [];
+cfg.dataset = filename;
+data = ft_preprocessing(
+
