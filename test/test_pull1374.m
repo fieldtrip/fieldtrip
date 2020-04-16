95b581ead9359abd64f5ba5d1bb362f9f420be9c
Robert Oostenveld <r.oostenveld@gmail.com>
2020-04-16 10:22:35 +0200

    recompiled mex files, made test script conditional on MATLAB version. See #1374

--- test/test_pull1374.m
+++ test/test_pull1374.m
@@ -17,11 +17,19 @@ data = ft_preprocessing(cfg);
 
 %%
 
-filename=dccnpath('/home/common/matlab/fieldtrip/data/test/original/motion/xsens/sub-03_rec-01_take-02_motion.c3d');
-
-ft_read_header(filename);
-ft_read_data(filename);
-
-cfg = [];
-cfg.dataset = filename;
-data = ft_preprocessing(cfg);
+if ft_platform_supports('matlabversion', '2018b', inf)
+  
+  filename=dccnpath('/home/common/matlab/fieldtrip/data/test/original/motion/xsens/sub-03_rec-01_take-02_motion.c3d');
+  
+  ft_read_header(filename);
+  ft_read_data(filename);
+  
+  cfg = [];
+  cfg.dataset = filename;
+  data = ft_preprocessing(cfg);
+  
+else
+  
+  warning('the ezc3d mex file is not compatible with older MATLAB versions');
+  
+end
