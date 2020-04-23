f5d89f1718bff2c4faad3093c95cec8ab1432aff
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2020-04-23 09:28:19 +0200

    ENH - changed file to be read after update of the files on disk

--- test/test_tutorial_beamformer20120321.m
+++ test/test_tutorial_beamformer20120321.m
@@ -4,7 +4,10 @@ function test_tutorial_beamformer20120321
 % WALLTIME 02:30:00
 % DEPENDENCY ft_redefinetrial ft_freqanalysis ft_volumesegment ft_prepare_singleshell ft_sourceanalysis ft_prepare_leadfield ft_sourceinterpolate ft_sourceplot ft_volumenormalise
 
-load(dccnpath('/home/common/matlab/fieldtrip/data/ftp/tutorial/beamformer/dataFIC.mat'));
+load(dccnpath('/home/common/matlab/fieldtrip/data/ftp/tutorial/beamformer/data_all.mat'));
+
+% let's just rename the variable
+dataFIC = data_all;
 
 %% Preprocess time windows of interest
 
