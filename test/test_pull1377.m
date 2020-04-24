d7a60cf874ad68032acf1b608ebb87ba3d69c299
mariacarlapiastra <mcpiastra@users.noreply.github.com>
2020-04-24 13:58:30 +0200

    reshape of the script and polishing

--- test/test_pull1377.m
+++ test/test_pull1377.m
@@ -6,38 +6,23 @@ function test_pull1377
 
 % this function creates a set of input-structures to be used for testing source analysis steps prior to inverse solution
 
-%% get volume conductor model
+% struct of the script:
+% 1. get the data (MEG and EEG)
+% 2. preprocess the data (MEG and EEG): (timelock with covariance for lcmv beamforming and minimumnormestimate, 
+%                                        timelock without keeptrials for dipolefitting,
+%                                        freq for dics beamforming and pcc beamforming)
+% 3. create leadfield: 3.a: internally computed leadfield (prepare volume conductor first and leadfield afterwards (MEG localspheres, EEG single sphere))
+%                      3.b: load externally computed leadfield and convert to appropriate format
+% 4. perform source analysis (MEG and EEG): LCMV beamforming, MNE, DICS, PCC, dipolefitting
 
-% for MEG, localsphere
-load('/home/common/matlab/fieldtrip/data/test/latest/vol/Subject01vol_localspheres.mat')
-vol_localsphere     = vol;
-clear vol
-
-% for EEG, singlesphere
-% read in the single sphere models produced with CTF software
-vol_singlesphere = ft_read_headmodel('/home/common/matlab/fieldtrip/data/Subject01.hdm');
-%also this is ok:
-% load('/home/common/matlab/fieldtrip/data/test/latest/vol/Subject01vol_singlesphere.mat')
-% vol_singlesphere = vol;% 
-% vol_singlesphere.c = 1;
-% clear vol 
-% vol_singlesphere = 
-%volname = dccnpath('/home/common/matlab/fieldtrip/template/headmodel/standard_seg.mat'); %% is there an already segmented?
-%cfg = [];
-%cfg.method='singlesphere';
-%vol_singlesphere = ft_prepare_headmodel(cfg, segmentedmri);
-
-%%
-%vol_singleshell = load('/home/common/matlab/fieldtrip/template/headmodel/standard_singleshell.mat');
-vol_bem = load('/home/common/matlab/fieldtrip/template/headmodel/standard_bem.mat');
-
-%% get MEG data + sensor info
+%% 1. get the data
+% for MEG data + sensor info
 dataname = dccnpath('/home/common/matlab/fieldtrip/data/test/latest/raw/meg/preproc_ctf151.mat');
 load(dataname);
 datameg = data; 
 clear data
 
-% get EEG data + channel info
+% for EEG data + channel info
 cfg=[];
 cfg.dataset = '/home/common/matlab/fieldtrip/data/ftp/tutorial/preprocessing_erp/s04.eeg';
 hdr        = ft_read_header(cfg.dataset);
@@ -53,7 +38,6 @@ cfg.trl = [begsample endsample offset];
 
 dataeeg = ft_preprocessing(cfg);
 
-
 % !! create worse-case scenario, whereby order and nr of chans don't match across inputs
 % remove 2-3 random chans from both MEG and EEG raw data
 cfg=[];
@@ -62,86 +46,9 @@ datameg = ft_selectdata(cfg, datameg);
 cfg.channel = randperm(length(dataeeg.label)-2);
 dataeeg = ft_selectdata(cfg, dataeeg);
 
-
-%% create leadfield (internally to ft)
-% for MEG
-cfg      = [];
-cfg.grad = datameg.grad; %% check that this has remained the same size despite sensors removed
-cfg.headmodel = vol_localsphere;
-cfg.channel = 'MEG';
-cfg.resolution = 1.5;
-gridmeg = ft_prepare_leadfield(cfg);
-
-%% create 2D grid - is this necessary?? Not working
-[pnt, tri] = mesh_sphere(162);
-pnt   = pnt*(vol.orig.MEG_Sphere.RADIUS-1.5);
-shift = [vol.orig.MEG_Sphere.ORIGIN_X vol.orig.MEG_Sphere.ORIGIN_Y vol.orig.MEG_Sphere.ORIGIN_Z];
-pnt   = pnt+repmat(shift,[size(pnt,1) 1]);
-grid2.pnt = pnt;
-grid2.tri = tri;
-grid2.inside = 1:size(grid2.pnt,1);
-grid2.outside = [];
-
-cfg      = [];
-cfg.grad = datameg.grad;
-cfg.headmodel = vol;
-cfg.sourcemodel = grid2;
-cfg.channel = 'MEG';
-grid2 = ft_prepare_leadfield(cfg);
-%%
-
-% for EEG
-elecname = '/home/common/matlab/fieldtrip/template/electrode/standard_1020.elc';
-elec = ft_read_sens(elecname, 'senstype', 'eeg');
-
-cfg      = [];
-cfg.grad = elec;
-cfg.headmodel = vol_singlesphere;
-cfg.channel = 'EEG';
-cfg.resolution = 1.5;
-grideeg = ft_prepare_leadfield(cfg);
-
-
-%% mimic externally created leadfields
-% for MEG
-%leadf = randn(442953, 151);
-leadf = randn(1000, length(datameg.grad)); % here we are assuming that the externally computed leadfield has the structure Ndip x Nsens
-ext_leadfield_meg.unit  = 'cm';
-ext_leadfield_meg.leadfielddimord = '{pos}_chan_ori';
-
-% the leadfield
-Ndip = size(leadf,1)/3;
-ext_leadfield_meg.leadfield = cell(1,Ndip);
-
-for d =1:Ndip
-    ext_leadfield_meg.leadfield{d} = [leadf(d,:); leadf(d+Ndip,:); leadf(d+2*Ndip,:)]';
-end
-
-ext_leadfield_meg.inside = ones(size(ext_leadfield.leadfield));
-ext_leadfield_meg.pos = randn(length(ext_leadfield.leadfield),3);
-
-
-% for EEG - not sure what this would be... likely same structure, right?
-% for MEG
-%leadf = randn(442953, 151);
-leadf = randn(1000, length(dataeeg.elec)); % here we are assuming that the externally computed leadfield has the structure Ndip x Nsens
-ext_leadfield_eeg.unit  = 'cm';
-ext_leadfield_eeg.leadfielddimord = '{pos}_chan_ori';
-
-% the leadfield
-Ndip = size(leadf,1)/3;
-ext_leadfield_eeg.leadfield = cell(1,Ndip);
-
-for d =1:Ndip
-    ext_leadfield_eeg.leadfield{d} = [leadf(d,:); leadf(d+Ndip,:); leadf(d+2*Ndip,:)]';
-end
-
-ext_leadfield_eeg.inside = ones(size(ext_leadfield.leadfield));
-ext_leadfield_eeg.pos = randn(length(ext_leadfield.leadfield),3);
-
-%% sensor-level data
-% create timelock structure with covariance for lcmv beamforming and
-% minimumnormestimate
+%% 2. preprocess the data
+% create timelock structure with covariance for lcmv beamforming and minimumnormestimate, 
+% and timeloch without keeptrials fro dipolefitting
 % for MEG
 cfg  = [];
 cfg.covariance = 'yes';
@@ -149,11 +56,24 @@ cfg.keeptrials = 'yes';
 cfg.channel    = 'MEG';
 MEG_tlck = ft_timelockanalysis(cfg, datameg);
 
+cfg  = [];
+cfg.covariance = 'yes';
+% cfg.keeptrials = 'yes'; %if this is not commented, the .avg field necessary in dipolefitting is missing
+cfg.channel    = 'MEG';
+MEG_tlck_df = ft_timelockanalysis(cfg, datameg);
+
+
 % for EEG 
 cfg  = [];
 cfg.covariance = 'yes';
 cfg.keeptrials = 'yes';
 EEG_tlck = ft_timelockanalysis(cfg, dataeeg);
+
+% for EEG (dimension problems in dipolefitting!!)
+cfg  = [];
+cfg.covariance = 'yes';
+% cfg.keeptrials = 'yes';
+EEG_tlck_df = ft_timelockanalysis(cfg, dataeeg);
 
 % create freq structure for dics beamforming and pcc beamforming
 % for MEG,
@@ -173,11 +93,95 @@ cfg.tapsmofrq = 4;
 cfg.foilim = [0 20];
 EEG_freq = ft_freqanalysis(cfg, dataeeg);
 
+%% 3. create leadfield 
+% 3.a internally
 
-outputdir = fullfile(dccnpath('/home/common/matlab/fieldtrip/data/test/latest/source/meg'));
+% get volume conductor model
+% for MEG, localsphere
+load('/home/common/matlab/fieldtrip/data/test/latest/vol/Subject01vol_localspheres.mat')
+vol_localsphere     = vol;
+clear vol
+
+% for EEG, singlesphere
+vol_singlesphere.o = [0 0 2];
+vol_singlesphere.r = 8;
+vol_singlesphere.c = 1;
+vol_singlesphere.unit = 'cm';
+
+% create leadfield (internally to ft)
+% for MEG
+cfg      = [];
+cfg.grad = datameg.grad; %% check that this has remained the same size despite sensors removed
+cfg.headmodel = vol_localsphere;
+cfg.channel = 'MEG';
+cfg.resolution = 1.5;
+gridmeg = ft_prepare_leadfield(cfg);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% create 2D grid - is this necessary?? Not working
+[pnt, tri] = mesh_sphere(162);
+pnt   = pnt*(vol.orig.MEG_Sphere.RADIUS-1.5);
+shift = [vol.orig.MEG_Sphere.ORIGIN_X vol.orig.MEG_Sphere.ORIGIN_Y vol.orig.MEG_Sphere.ORIGIN_Z];
+pnt   = pnt+repmat(shift,[size(pnt,1) 1]);
+grid2.pnt = pnt;
+grid2.tri = tri;
+grid2.inside = 1:size(grid2.pnt,1);
+grid2.outside = [];
+
+cfg      = [];
+cfg.grad = datameg.grad;
+cfg.headmodel = vol;
+cfg.sourcemodel = grid2;
+cfg.channel = 'MEG';
+grid2 = ft_prepare_leadfield(cfg);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% for EEG
+elecname = '/home/common/matlab/fieldtrip/template/electrode/standard_1020.elc';
+elec = ft_read_sens(elecname, 'senstype', 'eeg');
+
+cfg      = [];
+cfg.grad = elec; % why not dataeeg.elec?
+cfg.headmodel = vol_singlesphere;
+cfg.channel = 'EEG';
+cfg.resolution = 1.5;
+grideeg = ft_prepare_leadfield(cfg);
+
+%% 3.b externally (mimic externally created leadfields)
+% for MEG
+leadf = randn(3000, length(datameg.grad)); % here we are assuming that the externally computed leadfield has the structure Ndip x Nsens
+ext_leadfield_meg.unit  = 'cm';
+ext_leadfield_meg.leadfielddimord = '{pos}_chan_ori';
+
+% the leadfield
+Ndip = size(leadf,1)/3;
+ext_leadfield_meg.leadfield = cell(1,Ndip);
+
+for d =1:Ndip
+    ext_leadfield_meg.leadfield{d} = [leadf(d,:); leadf(d+Ndip,:); leadf(d+2*Ndip,:)]';
+end
+
+ext_leadfield_meg.inside = ones(size(ext_leadfield.leadfield));
+ext_leadfield_meg.pos = randn(length(ext_leadfield.leadfield),3);
 
 
-%% inverse solutions
+% for EEG
+leadf = randn(3000, length(dataeeg.elec)); % here we are assuming that the externally computed leadfield has the structure Ndip x Nsens
+ext_leadfield_eeg.unit  = 'cm';
+ext_leadfield_eeg.leadfielddimord = '{pos}_chan_ori';
+
+% the leadfield
+Ndip = size(leadf,1)/3;
+ext_leadfield_eeg.leadfield = cell(1,Ndip);
+
+for d =1:Ndip
+    ext_leadfield_eeg.leadfield{d} = [leadf(d,:); leadf(d+Ndip,:); leadf(d+2*Ndip,:)]';
+end
+
+ext_leadfield_eeg.inside = ones(size(ext_leadfield.leadfield));
+ext_leadfield_eeg.pos = randn(length(ext_leadfield.leadfield),3);
+
+%% 4. perform source analysis
 
 % do LCMV beamforming
 cfg            = [];
@@ -211,16 +215,16 @@ cfg.mne.keepfilter = 'yes';
 cfg.mne.lambda     = 1e4;
 cfg.headmodel = vol_localsphere;
 cfg.sourcemodel = gridmeg;
-sourcemne3d1 = ft_sourceanalysis(cfg, tlck);
+sourcemne3d1 = ft_sourceanalysis(cfg, MEG_tlck); %this does not exist, you mean MEG_tlck?
 %cfg.sourcemodel = grid2;
 %cfg.outputfile = fullfile(outputdir, 'ctf151_mne2d');
 %sourcemne2d1 = ft_sourceanalysis(cfg, MEG_tlck);
 
-%cfg.rawtrial    = 'yes';
-%cfg.sourcemodel        = grid;
-%cfg.sourcemodel.filter = sourcemne3d1.avg.filter;
+cfg.rawtrial    = 'yes';
+cfg.sourcemodel        = gridmeg;
+cfg.sourcemodel.filter = sourcemne3d1.avg.filter;
 %cfg.outputfile  = fullfile(outputdir, 'ctf151_mne3d_trial');
-%ft_sourceanalysis(cfg, tlck);
+ft_sourceanalysis(cfg, MEG_tlck);
 %cfg.sourcemodel        = grid2;
 %cfg.sourcemodel.filter = sourcemne2d1.avg.filter;
 %cfg.outputfile  = fullfile(outputdir, 'ctf151_mne2d_trial');
@@ -267,6 +271,26 @@ cfg.sourcemodel = gridmeg;
 %cfg.outputfile = fullfile(outputdir, 'ctf151_pcc3d');
 ft_sourceanalysis(cfg, MEG_freq);
 
+% do dipolefit
+% for MEG
+cfg = [];
+cfg.numdipoles    = 1;                              
+cfg.headmodel     = vol_localsphere;                     
+cfg.grid          = gridmeg;                     
+cfg.nonlinear     = 'no';                           
+cfg.grad          = MEG_tlck.grad;                        
+cfg.latency       = 0.025;                         
+ft_dipolefitting(cfg,MEG_tlck_df);
+
+% for EEG
+cfg = [];
+cfg.numdipoles    =  1;             
+cfg.headmodel     = vol_singlesphere;         
+cfg.grid          = grideeg;                 
+cfg.elec          = elec;                         
+cfg.latency       = 0.025;                          
+ft_dipolefitting(cfg,EEG_tlck_df); %problems with dimensions!
+
 %% %%%%%%%%%%%%%%%%%%%%%%
 % Previous ones work, what about externally generated leadfield:
 %% %%%%%%%%%%%%%%%%%%%%%%
@@ -305,39 +329,8 @@ cfg.channel = randperm(length(vol_localsphere.label)-3);
 vol_localsphere = ft_selectdata(cfg, vol_localsphere);
 %% Inverse solutions break with this vol - maybe I'm doing something wrong
 
-
-%% dipolefitting
-cfg  = [];
-cfg.covariance = 'yes';
-% cfg.keeptrials = 'yes'; %if this is not commented, the .avg field necessary in dipolefitting is missing
-cfg.channel    = 'MEG';
-MEG_tlck = ft_timelockanalysis(cfg, datameg);
-
-% do dipolefit
-cfg = [];
-cfg.numdipoles    = 1;                              
-cfg.headmodel     = vol_localsphere;                     
-cfg.grid          = gridmeg;                     
-cfg.nonlinear     = 'no';                           
-cfg.grad          = MEG_tlck.grad;                        
-cfg.latency       = 0.025;                         
-dipfit_meg = ft_dipolefitting(cfg,MEG_tlck); 
+%% dipolefit
 
 
-% for EEG (dimension problems!!)
-cfg  = [];
-cfg.covariance = 'yes';
-% cfg.keeptrials = 'yes';
-EEG_tlck = ft_timelockanalysis(cfg, dataeeg);
-
-%%
-
-cfg = [];
-cfg.numdipoles    =  1;                             %number of expected
-cfg.headmodel     = vol1;                  %the head model
-cfg.grid          = grideeg;                  %the precomputed leadfield
-cfg.elec          = elec;                           %the electrode model
-cfg.latency       = 0.025;                          %the latency of interest
-dipfit_bem        = ft_dipolefitting(cfg,EEG_tlck);
 
 
