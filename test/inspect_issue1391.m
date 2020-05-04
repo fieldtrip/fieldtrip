ec18b6c1c5a132fee031092bb37039d76a2d0016
Eelke Spaak <eelke.spaak@gmail.com>
2020-05-04 09:45:00 +0200

    adding test/inspect script for issue 1391

---
+++ test/inspect_issue1391.m
@@ -0,0 +1,35 @@
+function inspect_issue1391
+
+% WALLTIME 00:05:00
+% MEM 2gb
+
+% DEPENDENCY ft_rejectvisual rejectvisual_summary
+
+%% create data
+
+data = [];
+data.label = {'a' 'b' 'c' 'd'}';
+
+% ensure the channels are distinguishable by their data (helps in the
+% GUI)
+dat = ones(4,1000);
+dat(2,:) = 2*dat(2,:);
+dat(3,:) = 3*dat(3,:);
+dat(4,:) = 4*dat(4,:);
+
+data.trial = {dat dat};
+data.time = {1:1000 1:1000};
+
+%% check it
+
+cfg = [];
+cfg.channel = {'a' 'c' 'd'};
+cfg.method = 'summary';
+cfg.metric = 'max';
+datout = ft_rejectvisual(cfg, data);
+
+% Now I select the second channel to reject, which corresponds to channel
+% label 'c' (due to the selection above). Weirdly, datout now has channels
+% a and c, so channel d was removed!
+
+end
