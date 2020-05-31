9796542dd37118734d156bb7131237a564265401
Robert Oostenveld <r.oostenveld@gmail.com>
2020-05-31 12:20:16 +0200

    DOC - cleaned up the help

--- ft_artifact_ecg.m
+++ ft_artifact_ecg.m
@@ -31,16 +31,14 @@ function [cfg, artifact] = ft_artifact_ecg(cfg, data)
 %   cfg.artfctdef.ecg.cutoff  = 3; peak-threshold
 %   cfg.artfctdef.ecg.inspect = Nx1 list of channels which will be shown in a QRS-locked average
 %
-% The output argument "artifact" is a Nx2 matrix comparable to the
-% "trl" matrix of FT_DEFINETRIAL. The first column of which specifying the
-% beginsamples of an artifact period, the second column contains the
-% endsamples of the artifactperiods.
+% The output argument "artifact" is a Nx2 matrix comparable to the "trl" matrix of
+% FT_DEFINETRIAL. The first column of which specifying the beginsamples of an
+% artifact period, the second column contains the endsamples of the artifactperiods.
 %
-% To facilitate data-handling and distributed computing you can use
+% To facilitate data-handling and distributed computing, you can use
 %   cfg.inputfile   =  ...
-% If you specify this option the input data will be read from a *.mat
-% file on disk. This mat files should contain only a single variable named 'data',
-% corresponding to the input structure.
+% to read the input data from a *.mat file on disk. This mat files should contain
+% only a single variable named 'data', corresponding to the input structure.
 %
 % See also FT_REJECTARTIFACT, FT_REMOVETEMPLATEARTIFACT, FT_ARTIFACT_CLIP, FT_ARTIFACT_ECG,
 % FT_ARTIFACT_EOG, FT_ARTIFACT_JUMP, FT_ARTIFACT_MUSCLE, FT_ARTIFACT_THRESHOLD,
--- ft_artifact_tms.m
+++ ft_artifact_tms.m
@@ -1,7 +1,7 @@
 function [cfg, artifact] = ft_artifact_tms(cfg, data)
 
-% FT_ARTIFACT_TMS reads the data segments of interest from file and identifies artefacts in
-% EEG recordings that were done during TMS stimulation.
+% FT_ARTIFACT_TMS reads the data segments of interest from file and identifies
+% artefacts in EEG recordings that were done during TMS stimulation.
 %
 % Use as
 %   [cfg, artifact] = ft_artifact_tms(cfg)
@@ -39,34 +39,32 @@ function [cfg, artifact] = ft_artifact_tms(cfg, data)
 %   cfg.artfctdef.tms.trlpadding  = 0.1
 %   cfg.artfctdef.tms.fltpadding  = 0.1
 %   cfg.artfctdef.tms.artpadding  = 0.01
-% Be aware that if one artifact falls within this specified range of another artifact, both
-% artifact will be counted as one. Depending on cfg.prestim and cfg.poststim you may not mark
-% enough data as artifactual.
+% Be aware that if one artifact falls within this specified range of another
+% artifact, both artifact will be counted as one. Depending on cfg.prestim and
+% cfg.poststim you may not mark enough data as artifactual.
 %
-% With cfg.method='marker', TMS-artifact onsets and offsets are based on markers/triggers that
-% are written into the EEG dataset. This method acts as a wrapper around FT_DEFINETRIAL to
-% determine on- and offsets of TMS pulses by reading markers in the EEG.
+% With cfg.method='marker', TMS-artifact onsets and offsets are based on
+% markers/triggers that are written into the EEG dataset. This method acts as a
+% wrapper around FT_DEFINETRIAL to determine on- and offsets of TMS pulses by reading
+% markers in the EEG.
 %   cfg.trialfun            = function name, see below (default = 'ft_trialfun_general')
 %   cfg.trialdef.eventtype  = 'string'
 %   cfg.trialdef.eventvalue = number, string or list with numbers or strings
-% The cfg.trialfun option is a string containing the name of a function that you wrote
-% yourself and that FT_ARTIFACT_TMS will call. The function should take the cfg-structure as
-% input and should give a NxM matrix with M equal to or larger than 3) in the same format as
-% "trl" as the output. You can add extra custom fields to the configuration structure to
-% pass as arguments to your own trialfun.  Furthermore, inside the trialfun you can use the
-% FT_READ_EVENT function to get the event information from your data file.
+% The cfg.trialfun option is a string containing the name of a function that you
+% wrote yourself and that FT_ARTIFACT_TMS will call. The function should take the
+% cfg-structure as input and should give a NxM matrix with M>=3 in the same format as
+% "trl" as the output. You can add extra custom fields to the configuration structure
+% to pass as arguments to your own trialfun. Furthermore, inside the trialfun you can
+% use the FT_READ_EVENT function to get the event information from your data file.
 %
-% The output argument "artifact" is a Nx2 matrix comparable to the
-% "trl" matrix of FT_DEFINETRIAL. The first column of which specifying the
-% beginsamples of an artifact period, the second column contains the
-% endsamples of the artifactperiods.
+% The output argument "artifact" is a Nx2 matrix comparable to the "trl" matrix of
+% FT_DEFINETRIAL. The first column of which specifying the beginsamples of an
+% artifact period, the second column contains the endsamples of the artifactperiods.
 %
-% To facilitate data-handling and distributed computing with the peer-to-peer
-% module, this function has the following option:
+% To facilitate data-handling and distributed computing, you can use
 %   cfg.inputfile   =  ...
-% If you specify this option the input data will be read from a *.mat
-% file on disk. This mat files should contain only a single variable named 'data',
-% corresponding to the input structure.
+% to read the input data from a *.mat file on disk. This mat files should contain
+% only a single variable named 'data', corresponding to the input structure.
 %
 % See also FT_REJECTARTIFACT, FT_ARTIFACT_CLIP, FT_ARTIFACT_ECG, FT_ARTIFACT_EOG,
 % FT_ARTIFACT_JUMP, FT_ARTIFACT_MUSCLE, FT_ARTIFACT_THRESHOLD, FT_ARTIFACT_ZVALUE
