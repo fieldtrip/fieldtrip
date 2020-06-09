dca0fb5bf3872c087a214563c7102b363317dfb0
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2020-06-09 08:51:10 +0200

    fix edf+ annotation timestamps to include millisecond aspect of recording starttime

--- test/test_issue1425.m
+++ test/test_issue1425.m
@@ -1,11 +1,13 @@
-function test_issue1425
+function test_issue1425(testfilename)
 
 % MEM 2gb
 % WALLTIME 00:10:00
 % DEPENDENCY ft_preprocessing ft_read_data
 
-d = dir(dccnpath('/home/common/matlab/fieldtrip/data/test/issue1425/*.edf'));
-
-hdr = ft_read_header(fullfile(d.folder,d.name));
-event = ft_read_event(fullfile(d.folder,d.name));
+if nargin==0
+  d = dir(dccnpath('/home/common/matlab/fieldtrip/data/test/issue1425/*.edf'));
+  testfilename = fullfile(d.folder, d.name);
+end
+hdr   = ft_read_header(fullfile(testfilename));
+event = ft_read_event(fullfile(testfilename));
 
