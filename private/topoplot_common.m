c005f7d166ff50807ed4d8b38db23f6f2253ad93
Robert Oostenveld <r.oostenveld@gmail.com>
2020-06-10 14:50:35 +0200

    support either string as colormap or a cell-array
    as a cell-array, it should be for example {'jet', 64} or {'BrBG', 128}

--- private/topoplot_common.m
+++ private/topoplot_common.m
@@ -239,8 +239,10 @@ end
 
 % check colormap is proper format and set it
 if isfield(cfg, 'colormap')
-  if ~isnumeric(cfg.colormap)
+  if ischar(cfg.colormap)
     cfg.colormap = ft_colormap(cfg.colormap);
+  elseif isstruct(cfg.colormap)
+    cfg.colormap = ft_colormap(cfg.colormap{:});
   end
   if size(cfg.colormap,2)~=3
     ft_error('cfg.colormap must be Nx3');
