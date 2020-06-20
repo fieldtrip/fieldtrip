eec0a6be0c2943550443178946cb035067f8f298
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2020-06-20 23:38:36 +0200

    FIX - fix negative determinant also in indexed

--- utilities/ft_datatype_segmentation.m
+++ utilities/ft_datatype_segmentation.m
@@ -274,7 +274,7 @@ probabilistic = probabilistic(sel);
 
 if all(indexed)
   % get the volume of a cubic element
-  v = det(segmentation.transform(1:3,1:3));
+  v = abs(det(segmentation.transform(1:3,1:3)));
   u = sprintf('%s^3', segmentation.unit);
   for k = 1:numel(fn)
     lab = segmentation.([fn{k},'label']);
