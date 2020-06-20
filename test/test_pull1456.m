488cbd799568a9e574ae011797602159ee70bed0
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2020-06-20 22:01:04 +0200

    ENH - updated test script (commented out non-functional methods)

--- test/test_pull1456.m
+++ test/test_pull1456.m
@@ -21,30 +21,44 @@ cfg             = [];
 cfg.method      = 'eloreta';
 cfg.sourcemodel = sourcemodel;
 cfg.headmodel   = vol.vol;
-% source_eloreta1     = ft_sourceanalysis(cfg, data_eloreta1);  % compute the source model
-% assert(isequal(size(source_eloreta1.avg.pow),[size(source_eloreta1.pos,1) numel(source_eloreta1.freq)]));
-% 
-% source_eloreta2     = ft_sourceanalysis(cfg, data_eloreta2);  % compute the source model
-% i1 = find(source_eloreta2.inside,1,'first');
-% assert(isequal(size(source_eloreta2.avg.mom{i1}),[3 80 193]));
-% 
+source_eloreta1     = ft_sourceanalysis(cfg, dataFreq1);  % compute the source model
+assert(isequal(size(source_eloreta1.avg.pow),[size(source_eloreta1.pos,1) numel(source_eloreta1.freq)]));
+
+source_eloreta2     = ft_sourceanalysis(cfg, dataFreq2);  % compute the source model
+i1 = find(source_eloreta2.inside,1,'first');
+assert(isequal(size(source_eloreta2.avg.mom{i1}),[3 80 193]));
+
 cfg.snr    = 10;
-% cfg.method = 'mne';
-% source_mne1 = ft_sourceanalysis(cfg, dataFreq1);
-% source_mne2 = ft_sourceanalysis(cfg, dataFreq2);
+cfg.method = 'mne';
+source_mne1 = ft_sourceanalysis(cfg, dataFreq1);
+source_mne2 = ft_sourceanalysis(cfg, dataFreq2);
 
 % -> this does not work. AT ALLedit f
 %cfg.method = 'harmony';
 %source_harmony1 = ft_sourceanalysis(cfg, dataFreq1);
 %source_harmony2 = ft_sourceanalysis(cfg, dataFreq2);
 
+% -> this should throw an explicit error
 cfg.method = 'music';
-source_music1 = ft_sourceanalysis(cfg, dataFreq1);
-source_music2 = ft_sourceanalysis(cfg, dataFreq2);
+try
+  source_music1 = ft_sourceanalysis(cfg, dataFreq1);
+  ok = true;
+catch
+  ok = false;
+end
+assert(~ok);
+try
+  source_music2 = ft_sourceanalysis(cfg, dataFreq2);
+  ok = true;
+catch
+  ok = false;
+end
+assert(~ok);
 
-cfg.method = 'rv';
-source_rv1 = ft_sourceanalysis(cfg, dataFreq1);
-source_rv2 = ft_sourceanalysis(cfg, dataFreq2);
+% -> this currently also does note work, but throws a non informative error
+%cfg.method = 'rv';
+%source_rv1 = ft_sourceanalysis(cfg, dataFreq1);
+%source_rv2 = ft_sourceanalysis(cfg, dataFreq2);
 
 % -----------------------------
 
