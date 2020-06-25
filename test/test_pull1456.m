01f5b3c379cd8d620c3958b011914fe4d7ee93a4
Robert Oostenveld <r.oostenveld@gmail.com>
2020-06-25 16:21:38 +0200

    ENH - increased memory for test script to run on HPC cluster; also cleaned up whitespace

--- test/test_pull1456.m
+++ test/test_pull1456.m
@@ -1,34 +1,34 @@
 function test_pull1456
 
-% MEM 2gb
+% MEM 4gb
 % WALLTIME 00:20:00
 % DEPENDENCY ft_sourceanalysis ft_eloreta
 
 load(dccnpath('/home/common/matlab/fieldtrip/data/test/test_pull1456.mat'));
 
 % Compute spectrum
-cfg         = [];
-cfg.output  = 'powandcsd';
+cfg = [];
+cfg.output = 'powandcsd';
 cfg.channel = 'all';
-cfg.method  = 'mtmfft';
-cfg.taper   = 'hanning';%'boxcar';
-dataFreq1   = ft_freqanalysis(cfg, dataPre);
-cfg.output  = 'fourier';
-dataFreq2   = ft_freqanalysis(cfg, dataPre);
+cfg.method = 'mtmfft';
+cfg.taper = 'hanning';%'boxcar';
+dataFreq1 = ft_freqanalysis(cfg, dataPre);
+cfg.output = 'fourier';
+dataFreq2 = ft_freqanalysis(cfg, dataPre);
 
 % source reconstruction
-cfg             = [];
-cfg.method      = 'eloreta';
+cfg = [];
+cfg.method = 'eloreta';
 cfg.sourcemodel = sourcemodel;
-cfg.headmodel   = vol.vol;
-source_eloreta1     = ft_sourceanalysis(cfg, dataFreq1);  % compute the source model
+cfg.headmodel = vol.vol;
+source_eloreta1 = ft_sourceanalysis(cfg, dataFreq1); % compute the source model
 assert(isequal(size(source_eloreta1.avg.pow),[size(source_eloreta1.pos,1) numel(source_eloreta1.freq)]));
 
-source_eloreta2     = ft_sourceanalysis(cfg, dataFreq2);  % compute the source model
+source_eloreta2 = ft_sourceanalysis(cfg, dataFreq2); % compute the source model
 i1 = find(source_eloreta2.inside,1,'first');
 assert(isequal(size(source_eloreta2.avg.mom{i1}),[3 80 193]));
 
-cfg.snr    = 10;
+cfg.mne.snr = 10;
 cfg.method = 'mne';
 source_mne1 = ft_sourceanalysis(cfg, dataFreq1);
 source_mne2 = ft_sourceanalysis(cfg, dataFreq2);
@@ -63,15 +63,15 @@ assert(~ok);
 % -----------------------------
 
 % Compute an ERP
-cfg                  = [];
-cfg.covariance       = 'yes';
-cfg.covariancewindow = [-1 0]; % calculate the average of the covariance matrices 
-                                   % for each trial (but using the pre-event baseline  data only)
+cfg = [];
+cfg.covariance = 'yes';
+cfg.covariancewindow = [-1 0]; % calculate the average of the covariance matrices
+% for each trial (but using the pre-event baseline data only)
 dataAvg = ft_timelockanalysis(cfg, dataPre);
 
 % source reconstruction
-cfg             = [];
-cfg.method      = 'eloreta';
+cfg = [];
+cfg.method = 'eloreta';
 cfg.sourcemodel = sourcemodel;
-cfg.headmodel   = vol.vol;
-sourceTime      = ft_sourceanalysis(cfg, dataAvg); 
+cfg.headmodel = vol.vol;
+sourceTime = ft_sourceanalysis(cfg, dataAvg);
