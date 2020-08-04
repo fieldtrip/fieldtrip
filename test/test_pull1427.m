deef8333da6d6e27efb8830cbeaa560210bc0648
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2020-08-04 13:50:18 +0200

    ENH - added some test cases to investigate mesh alignment

--- test/test_pull1427.m
+++ test/test_pull1427.m
@@ -14,6 +14,8 @@ segprob = [];
 segprob.brain = false(10,10,10); segprob.brain(4:7,4:7,4:7) = true;
 segprob.skull = false(10,10,10); segprob.skull(3:8,3:8,3:8) = true;
 segprob.scalp = false(10,10,10); segprob.scalp(2:9,2:9,2:9) = true;
+% segprob.air = true(10,10,10);
+% segprob.air(2:9,2:9,2:9) = false;
 %segprob.brain = false(11,11,11); segprob.brain(4:8,4:8,4:8) = true;
 %segprob.skull = false(11,11,11); segprob.skull(3:9,3:9,3:9) = true;
 %segprob.scalp = false(11,11,11); segprob.scalp(2:10,2:10,2:10) = true;
@@ -37,18 +39,48 @@ cfg = [];
 cfg.shift = 0.3;
 cfg.method = 'hexahedral';
 cfg.resolution = 1;
-mesh_vol_hex = ft_prepare_mesh(cfg, segprob);
+mesh_vol_hex1 = ft_prepare_mesh(cfg, segprob);
+
+cfg.resolution = 0.5;
+mesh_vol_hex05 = ft_prepare_mesh(cfg, segprob);
+
+minmaxpos(:,1) = segprob.transform*([0 0 0 1])';
+minmaxpos(:,2) = segprob.transform*[segprob.dim+1 1]';
+
+cfg_reslice = [];
+cfg_reslice.resolution = 0.5;
+%cfg_reslice.dim        = ceil(mri.dim./cfg.resolution);
+cfg_reslice.xrange     = minmaxpos(1,:); % this is more robust than the dim, in case the origin is not inside the volume
+cfg_reslice.yrange     = minmaxpos(2,:);
+cfg_reslice.zrange     = minmaxpos(3,:);
+cfg_reslice.method     = 'nearest';
+segprob2               = ft_volumereslice(cfg_reslice, segprob);
+
+
+mesh_vol_hex05b = ft_prepare_mesh(cfg, segprob2);
 
 figure
 ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5.5 5.5 5.5], 'style', 'intersect');
 hold on
-ft_plot_mesh(mesh_vol_hex, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
+ft_plot_mesh(mesh_vol_hex1, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
+view(120, 30)
+
+figure
+ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5.5 5.5 5.5], 'style', 'intersect');
+hold on
+ft_plot_mesh(mesh_vol_hex05, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
+view(120, 30)
+
+figure
+ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5.5 5.5 5.5], 'style', 'intersect');
+hold on
+ft_plot_mesh(mesh_vol_hex05b, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
 view(120, 30)
 
 cfg = [];
 cfg.method ='simbio';
 cfg.conductivity = [0.33, 0.43, 0.53]; % order follows mesh.tissuelabel
-ft_prepare_headmodel(cfg, mesh_vol_hex)
+ft_prepare_headmodel(cfg, mesh_vol_hex1)
 
 %%
 
