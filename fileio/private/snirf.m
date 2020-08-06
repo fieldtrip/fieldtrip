319e4ea17a518dee6fe0c74a4d40e61a54c1dd4c
Robert Oostenveld <r.oostenveld@gmail.com>
2020-08-06 13:09:56 +0200

    added copyright statement

--- fileio/private/snirf.m
+++ fileio/private/snirf.m
@@ -10,6 +10,26 @@ function varargout = snirf(filename, hdr, begsample, endsample, chanindx)
 %
 % See also FT_FILETYPE, FT_READ_HEADER, FT_READ_DATA, FT_READ_EVENT, QUALISYS_TSV, MOTION_C3D
 
+% Copyright (C) 2020 Robert Oostenveld
+%
+% This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
+% $Id$
+
 % this uses the SNIRF reading functions from the Homer3 toolbox
 ft_hastoolbox('homer3', 1);
 
