36df25c38a19052dfea1cde91c344ee81c7da1a8
Robert Oostenveld <r.oostenveld@gmail.com>
2020-08-06 16:32:51 +0200

    added more plotting, added a 3x resliced segmentation for testing

--- test/test_pull1427.m
+++ test/test_pull1427.m
@@ -7,7 +7,7 @@ function test_pull1427
 %%
 
 % This function creates an hexahedral and tetrahedral volumetric mesh from a
-% two-compartments volume to be passed as input to ft_prepare_headmodel with
+% three-compartments volume to be passed as input to ft_prepare_headmodel with
 % the method 'simbio'.
 
 segprob = [];
@@ -20,9 +20,12 @@ segprob.scalp = false(10,10,10); segprob.scalp(2:9,2:9,2:9) = true;
 %segprob.skull = false(11,11,11); segprob.skull(3:9,3:9,3:9) = true;
 %segprob.scalp = false(11,11,11); segprob.scalp(2:10,2:10,2:10) = true;
 segprob.dim = size(segprob.brain);
-segprob.unit = 'mm';
+segprob.unit = 'cm';
 segprob.coordsys = 'ctf';
 segprob.transform = eye(4);
+segprob.transform(1,4) = -0.5;
+segprob.transform(2,4) = -0.5;
+segprob.transform(3,4) = -0.5;
 
 % it is more difficult to visualize a probabilistic segmentation than an indexed one
 segindx = ft_datatype_segmentation(segprob, 'segmentationstyle', 'indexed');
@@ -30,8 +33,14 @@ segindx = ft_datatype_segmentation(segprob, 'segmentationstyle', 'indexed');
 cfg = [];
 cfg.funparameter = 'seg';
 cfg.method = 'ortho';
-cfg.location = [5.5 5.5 5.5]; % this is the center of the volume, in this plot it will be rounded off to the nearest voxel
+cfg.location = [5 5 5]; % this is the center of the volume, in this plot it will be rounded off to the nearest voxel
 ft_sourceplot(cfg, segindx);
+
+% determine the range of the bounding box
+[X, Y, Z] = ndgrid(1:segprob.dim(1), 1:segprob.dim(2), 1:segprob.dim(3));
+voxpos = ft_warp_apply(segprob.transform, [X(:) Y(:) Z(:)]);
+minmaxpos(:,1) = min(voxpos) - 0.5;
+minmaxpos(:,2) = max(voxpos) + 0.5;
 
 %%
 
@@ -40,42 +49,10 @@ cfg.shift = 0.3;
 cfg.method = 'hexahedral';
 mesh_vol_hex1 = ft_prepare_mesh(cfg, segprob);
 
-%cfg.resolution = 0.5;
-%mesh_vol_hex05 = ft_prepare_mesh(cfg, segprob);
-
-minmaxpos(:,1) = segprob.transform*([0 0 0 1])';
-minmaxpos(:,2) = segprob.transform*[segprob.dim+1 1]';
-
-cfg_reslice = [];
-cfg_reslice.resolution = 0.5;
-%cfg_reslice.dim        = ceil(mri.dim./cfg.resolution);
-cfg_reslice.xrange     = minmaxpos(1,:); % this is more robust than the dim, in case the origin is not inside the volume
-cfg_reslice.yrange     = minmaxpos(2,:);
-cfg_reslice.zrange     = minmaxpos(3,:);
-cfg_reslice.method     = 'nearest';
-segprob2               = ft_volumereslice(cfg_reslice, segprob);
-segindx2               = ft_volumereslice(cfg_reslice, segindx);
-
-mesh_vol_hex05b = ft_prepare_mesh(cfg, segprob2);
-
 figure
-ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5.5 5.5 5.5], 'style', 'intersect');
+ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5 5 5], 'style', 'intersect');
 hold on
 ft_plot_mesh(mesh_vol_hex1, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
-view(120, 30)
-
-% % this one does not look OK
-% figure
-% ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5.5 5.5 5.5], 'style', 'intersect');
-% hold on
-% ft_plot_mesh(mesh_vol_hex05, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
-% view(120, 30)
-
-% this one looks OK in terms of alginment
-figure
-ft_plot_ortho(segindx2.seg, 'transform', segindx2.transform, 'location', [5.5 5.5 5.5], 'style', 'intersect');
-hold on
-ft_plot_mesh(mesh_vol_hex05b, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
 view(120, 30)
 
 cfg = [];
@@ -85,12 +62,72 @@ ft_prepare_headmodel(cfg, mesh_vol_hex1)
 
 %%
 
+% make a volume with 2x the resolution
+cfg = [];
+cfg.resolution = 1/2;
+cfg.xrange     = minmaxpos(1,:); % this is more robust than the dim, in case the origin is not inside the volume
+cfg.yrange     = minmaxpos(2,:);
+cfg.zrange     = minmaxpos(3,:);
+cfg.method     = 'nearest';
+segprob2 = ft_volumereslice(cfg, segprob);
+segindx2 = ft_volumereslice(cfg, segindx);
+
+cfg = [];
+cfg.shift = 0.3;
+cfg.method = 'hexahedral';
+mesh_vol_hex2 = ft_prepare_mesh(cfg, segprob2);
+
+% this one looks OK in terms of alignment
+figure
+% ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5 5 5], 'style', 'intersect');
+ft_plot_ortho(segindx2.seg, 'transform', segindx2.transform, 'location', [5 5 5], 'style', 'intersect'); % this one looks OK in terms of alignment
+hold on
+ft_plot_mesh(mesh_vol_hex2, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
+view(120, 30)
+
+cfg = [];
+cfg.method ='simbio';
+cfg.conductivity = [0.33, 0.43, 0.53]; % order follows mesh.tissuelabel
+ft_prepare_headmodel(cfg, mesh_vol_hex1)
+
+%%
+
+% make a volume with 3x the resolution
+cfg = [];
+cfg.resolution = 1/3;
+cfg.xrange     = minmaxpos(1,:); % this is more robust than the dim, in case the origin is not inside the volume
+cfg.yrange     = minmaxpos(2,:);
+cfg.zrange     = minmaxpos(3,:);
+cfg.method     = 'nearest';
+segprob3 = ft_volumereslice(cfg, segprob);
+segindx3 = ft_volumereslice(cfg, segindx);
+
+cfg = [];
+cfg.shift = 0.3;
+cfg.method = 'hexahedral';
+mesh_vol_hex3 = ft_prepare_mesh(cfg, segprob3);
+
+figure
+% ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5 5 5], 'style', 'intersect');
+% ft_plot_ortho(segindx2.seg, 'transform', segindx2.transform, 'location', [5 5 5], 'style', 'intersect');
+ft_plot_ortho(segindx3.seg, 'transform', segindx3.transform, 'location', [5 5 5], 'style', 'intersect'); % this one looks OK in terms of alignment
+hold on
+ft_plot_mesh(mesh_vol_hex3, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
+view(120, 30)
+
+cfg = [];
+cfg.method ='simbio';
+cfg.conductivity = [0.33, 0.43, 0.53]; % order follows mesh.tissuelabel
+ft_prepare_headmodel(cfg, mesh_vol_hex3)
+
+%%
+
 cfg = [];
 cfg.method = 'tetrahedral';
 mesh_vol_tet = ft_prepare_mesh(cfg, segprob);
 
 figure
-ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5.5 5.5 5.5], 'style', 'intersect');
+ft_plot_ortho(segindx.seg, 'transform', segindx.transform, 'location', [5 5 5], 'style', 'intersect');
 hold on
 ft_plot_mesh(mesh_vol_tet, 'surfaceonly', false, 'facecolor', 'none', 'edgecolor', 'm');
 view(120, 30)
