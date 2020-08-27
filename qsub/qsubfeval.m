99b1eb691f3d5c0276ffbfc088e13acfd72e22cb
Michael Krause <krause@mpib-berlin.mpg.de>
2020-08-27 15:41:36 +0200

    ENH: qsub: use timreq and memreq for slurm
    The options --mem (in MB) and --time (in seconds) should be safe to use

--- qsub/qsubfeval.m
+++ qsub/qsubfeval.m
@@ -378,13 +378,11 @@ switch backend
     end
 
     if ~isempty(timreq) && ~isnan(timreq) && ~isinf(timreq)
-      % TESTME this is experimental and needs more testing!
-      % submitoptions = [submitoptions sprintf('--time=%d ', timreq+timoverhead)];
+      submitoptions = [submitoptions sprintf('--time=%d ', round((timreq+timoverhead)/60))];
     end
 
     if ~isempty(memreq) && ~isnan(memreq) && ~isinf(memreq)
-      % TESTME this is experimental and needs more testing!
-      % submitoptions = [submitoptions sprintf('--mem-per-cpu=%.0f ', round((memreq+memoverhead)./1024^2))];
+      submitoptions = [submitoptions sprintf('--mem=%.0f ', round((memreq+memoverhead)./1024^2))];
     end
 
     % specifying the o and e names might be useful for the others as well
