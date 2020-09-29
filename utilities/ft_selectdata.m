a9a76a90319889377bc36cf35bdd367b6fd1636f
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2020-09-29 20:34:33 +0200

    ENH - bypass expensive calls to ft_channelselection if not needed

--- utilities/ft_selectdata.m
+++ utilities/ft_selectdata.m
@@ -644,40 +644,49 @@ varargin = varargin(1:ndata);
 % loop over data once to initialize
 chanindx = cell(ndata,1);
 label    = cell(1,0);
-
-for k = 1:ndata
-  selchannel      = cell(0,1);
-  selgrad         = [];
-  selelec         = [];
-  selopto         = [];
-  if isfield(varargin{k}, 'grad') && isfield(varargin{k}.grad, 'type')
-    % this makes channel selection more robust, e.g. when using wildcards in cfg.channel
-    [selgrad, dum] = match_str(varargin{k}.label, varargin{k}.grad.label);
-    selchannel     = cat(1, selchannel, ft_channelselection(cfg.channel, varargin{k}.label(selgrad), varargin{k}.grad.type));
+if ndata==1 && isequal(cfg.channel,'all')
+  % the loop across data arguments, as well as the expensive calls to
+  % ft_channelselection can be avoided if theres only a single data
+  % argument and if 'all' channels are to be returned in the output
+  label = varargin{1}.label(:);
+  
+else
+  
+  for k = 1:ndata
+    selchannel      = cell(0,1);
+    selgrad         = [];
+    selelec         = [];
+    selopto         = [];
+    if isfield(varargin{k}, 'grad') && isfield(varargin{k}.grad, 'type')
+      % this makes channel selection more robust, e.g. when using wildcards in cfg.channel
+      [selgrad, dum] = match_str(varargin{k}.label, varargin{k}.grad.label);
+      selchannel     = cat(1, selchannel, ft_channelselection(cfg.channel, varargin{k}.label(selgrad), varargin{k}.grad.type));
+    end
+    if isfield(varargin{k}, 'elec') && isfield(varargin{k}.elec, 'type')
+      % this makes channel selection more robust, e.g. when using wildcards in cfg.channel
+      [selelec, dum] = match_str(varargin{k}.label, varargin{k}.elec.label);
+      selchannel     = cat(1, selchannel, ft_channelselection(cfg.channel, varargin{k}.label(selelec), varargin{k}.elec.type));
+    end
+    if isfield(varargin{k}, 'opto') && isfield(varargin{k}.opto, 'type')
+      % this makes channel selection more robust, e.g. when using wildcards in cfg.channel
+      [selopto, dum] = match_str(varargin{k}.label, varargin{k}.opto.label);
+      selchannel     = cat(1, selchannel, ft_channelselection(cfg.channel, varargin{k}.label(selopto), varargin{k}.opto.type));
+    end
+    selrest    = setdiff((1:numel(varargin{k}.label))', [selgrad; selelec; selopto]);
+    selchannel = cat(1, selchannel, ft_channelselection(cfg.channel, varargin{k}.label(selrest)));
+    label      = union(label, selchannel);
   end
-  if isfield(varargin{k}, 'elec') && isfield(varargin{k}.elec, 'type')
-    % this makes channel selection more robust, e.g. when using wildcards in cfg.channel
-    [selelec, dum] = match_str(varargin{k}.label, varargin{k}.elec.label);
-    selchannel     = cat(1, selchannel, ft_channelselection(cfg.channel, varargin{k}.label(selelec), varargin{k}.elec.type));
-  end
-  if isfield(varargin{k}, 'opto') && isfield(varargin{k}.opto, 'type')
-    % this makes channel selection more robust, e.g. when using wildcards in cfg.channel
-    [selopto, dum] = match_str(varargin{k}.label, varargin{k}.opto.label);
-    selchannel     = cat(1, selchannel, ft_channelselection(cfg.channel, varargin{k}.label(selopto), varargin{k}.opto.type));
-  end
-  selrest    = setdiff((1:numel(varargin{k}.label))', [selgrad; selelec; selopto]);
-  selchannel = cat(1, selchannel, ft_channelselection(cfg.channel, varargin{k}.label(selrest)));
-  label      = union(label, selchannel);
+  label = label(:);   % ensure that this is a column array
+  
+  % this call to match_str ensures that that labels are always in the
+  % order of the first input argument see bug_2917, but also temporarily keep
+  % the labels from the other data structures not present in the first one
+  % (in case selmode = 'union')
+  [ix, iy] = match_str(varargin{1}.label, label);
+  label1   = varargin{1}.label(:); % ensure column array
+  label    = [label1(ix); label(setdiff(1:numel(label),iy))];
+  
 end
-label = label(:);   % ensure that this is a column array
-
-% this call to match_str ensures that that labels are always in the
-% order of the first input argument see bug_2917, but also temporarily keep
-% the labels from the other data structures not present in the first one
-% (in case selmode = 'union')
-[ix, iy] = match_str(varargin{1}.label, label);
-label1   = varargin{1}.label(:); % ensure column array
-label    = [label1(ix); label(setdiff(1:numel(label),iy))];
 
 indx = nan+zeros(numel(label), ndata);
 for k = 1:ndata
