9cbf53eb51d68e9b97a44ab0dbcea6e4cbb7ab9a
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2020-10-26 09:50:27 +0100

    ENH - decorated the file with some assertions, some of which should start failing once the issue is fixed

--- test/test_pull1566.m
+++ test/test_pull1566.m
@@ -22,7 +22,12 @@ freq2_s = ft_selectdata(cfg,freq2);
 cfg=[];
 cfg.appenddim = 'chan';
 cfg.parameter = 'powspctrm';
-freqA = ft_appendfreq(cfg,freq1_s,freq2_s)
+freqA = ft_appendfreq(cfg,freq1_s,freq2_s);
+
+% The below assertions are 'incorrect', i.e. they should be throwing an
+% error if this issue is solved
+assert(isequal(freqA.freq, [0;0]));
+assert(~isfield(freqA, 'powspctrm'));
 
 % another situation
 freq1=[];
@@ -45,12 +50,16 @@ cfg.appenddim = 'chan';
 cfg.parameter = 'powspctrm';
 freqB = ft_appendfreq(cfg,freq1_s,freq2_s);
 
+% This scenario yields an OK structure
+assert(isfield(freqB, 'powspctrm'));
+assert(isequal(freqB.freq, 0));
+
 % another situation
 freq1=[];
 freq1.label={'chan1'};
 freq1.dimord='chan_freq';
 freq1.freq=[1:2];
-freq1.powspctrm = rand(2,size(freq1.freq,2));
+freq1.powspctrm = rand(1,size(freq1.freq,2));
 
 freq2=freq1;
 freq2.label={'chan2'};
@@ -58,4 +67,10 @@ freq2.label={'chan2'};
 cfg=[];
 cfg.appenddim = 'chan';
 cfg.parameter = 'powspctrm';
-freqC = ft_appendfreq(cfg,freq1,freq2)
+freqC = ft_appendfreq(cfg,freq1,freq2);
+
+% This scenario seems to correctly append the powerspectra, but the
+% freq-field is incorrect, the second assertion should throw an error once
+% this has been solved.
+assert(isequal(freqC.powspctrm, [freq1.powspctrm;freq2.powspctrm]));
+assert(isequal(freqC.freq, [1 2;1 2]));
