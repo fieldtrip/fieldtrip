67db13870d001024b9345ccd2773c2b2a5817bd7
Robert Oostenveld <r.oostenveld@gmail.com>
2020-12-04 10:20:48 +0100

    improved formatting of the help

--- specest/ft_specest_irasa_new.m
+++ specest/ft_specest_irasa_new.m
@@ -20,11 +20,12 @@ function [spectrum,ntaper,freqoi] = ft_specest_irasa_new(dat, time, varargin)
 %   verbose    = boolean, output progress to console (0 or 1, default 1)
 %   output     = string, indicating type of output('fractal' or 'orignal', default 'fractal')
 %
-% This implements: Wen.H. & Liu.Z.(2016), Separating fractal and oscillatory components in the power spectrum of neurophysiological signal. Brain Topogr. 29(1):13-26.
-%                  The source script avaible at https://purr.purdue.edu/publications/1987/1
+% This implements: Wen.H. & Liu.Z.(2016), Separating fractal and oscillatory components in the power 
+% spectrum of neurophysiological signal. Brain Topogr. 29(1):13-26. The source code accompanying the 
+% original paper is avaible from https://purr.purdue.edu/publications/1987/1
 % 
 % See also FT_FREQANALYSIS, FT_SPECEST_MTMFFT, FT_SPECEST_MTMCONVOL, FT_SPECEST_TFR, FT_SPECEST_HILBERT, FT_SPECEST_WAVELET
-%
+
 % Copyright (C) 2020, Rui Liu
 %
 % This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
