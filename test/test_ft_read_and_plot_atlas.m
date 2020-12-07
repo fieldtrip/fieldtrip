83a8b65c13411a95c417fbe7cb6b2cc6f0caa88c
Robert Seymour <robbyseymour@gmail.com>
2020-12-07 14:42:04 +0000

    Add Melbourne Subcortical Test
    Added Melbourne Subcortical Atlas to test_ft_read_and_plot_atlas.m

--- test/test_ft_read_and_plot_atlas.m
+++ test/test_ft_read_and_plot_atlas.m
@@ -84,3 +84,16 @@ ft_sourceplot(cfg, atlas);
 
 cfg.method = 'surface';
 ft_sourceplot(cfg, atlas, pial.mesh)
+
+atlas = ft_read_atlas(fullfile(ftpath, '/template/atlas/brainnetome/BNA_MPM_thr25_1.25mm.nii'));
+
+% Plot Melbourne Subcortical Atlas
+atlas = ft_read_atlas(fullfile(ftpath, '/template/atlas/melb_subcortical/melb_sub.mat'));
+
+cfg = [];
+cfg.atlas = atlas;
+cfg.funparameter = 'tissue';
+ft_sourceplot(cfg, atlas);
+
+cfg.method = 'surface';
+ft_sourceplot(cfg, atlas, pial.mesh)
