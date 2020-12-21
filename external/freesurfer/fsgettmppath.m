5b57a171de8bde363859dbce59f23f8c98aaa9dd
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2020-12-21 17:13:17 +0100

    ENH - changed file back

--- external/freesurfer/fsgettmppath.m
+++ external/freesurfer/fsgettmppath.m
@@ -4,35 +4,38 @@ function tmppath = fsgettmppath(tmppathdefault)
 % First looks in the following order:
 %  1. $TMPDIR - env var must exist and folder must exist
 %  2. $TEMPDIR - env var must exist and folder must exist
-%  3. /scratch - folder must exist
-%  4. /tmp - folder must exist
+%  3. /tmp - folder must exist   -> JMS changed order here
+%  4. /scratch - folder must exist -> JMS changed order here
 %  5. tmppathdefault - if passed
 %  6. current folder, ie, ./ (prints warning)
 
 
 tmppath = getenv('TMPDIR');
 if(~isempty(tmppath)) 
-  if(exist(tmppath) == 7) 
+  if(exist(tmppath)) 
     return; 
   end
 end
 
 tmppath = getenv('TEMPDIR');
 if(~isempty(tmppath)) 
-  if(exist(tmppath) == 7) 
+  if(exist(tmppath)) 
     return; 
   end
 end  
 
-tmppath = '/scratch';
-if(exist(tmppath) == 7) return; end
-
+% JMS: let /tmp prevail, rather than scratch, at DCCN we don't have write
+% permission in /scratch
 tmppath = '/tmp';
-if(exist(tmppath) == 7) return; end
+if(exist(tmppath)) return; end
+
+tmppath = '/scratch';
+if(exist(tmppath)) return; end
+
 
 if(nargin > 0)
   tmppath = tmppathdefault;
-  if(exist(tmppath) == 7) return; end
+  if(exist(tmppath)) return; end
 end
 
 tmppath = './';
@@ -40,3 +43,6 @@ fprintf(['WARNING: fsgettmppath: could not find a temporary folder,' ...
 	 ' using current folder\n']);
 
 return;
+
+
+
