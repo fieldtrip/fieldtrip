66377493c0c4dd951f85c788197a617ea327db5e
Robert Oostenveld <r.oostenveld@gmail.com>
2021-01-21 18:08:08 +0100

    speed up the checking of the grad and elec structures (#1649)
    * ENH - improve and speed up the checking of the input data structure, especially for CTF headers with hdr.orig
    * ENH - add chantype and chanunit to the output grad and elec to ensure that in ft_read_header these are passed to the high-level FT function. This speeds up subsequent checks of ft_dataype_sens
    * ENH - speed up the detection of eeg/meg/nirs input by simply chjecking on elecpos/coilpos/optopos. The more elaborate check with ft_senstype is still there, but only used when really needed.
    * ENH - remove the elec and grad structure to prevent some unnecessary low-level checks. See also #1551
    * ENH - removed extra whitespace between lines of code, nothing functional
    * FIX - enabled the persistent-variable check on the input again

--- utilities/ft_datatype_sens.m
+++ utilities/ft_datatype_sens.m
@@ -152,10 +152,25 @@ end
 % this is needed further down
 nchan = length(sens.label);
 
-% there are many cases which are MEG, EEG or NIRS specific
-ismeg  = ft_senstype(sens, 'meg');
-iseeg  = ft_senstype(sens, 'eeg');
-isnirs = ft_senstype(sens, 'nirs');
+% these are used at multiple places, therefore we determine them only once
+if isfield(sens, 'coilpos')
+  ismeg = true;
+  iseeg = true;
+  isnirs = false;
+elseif isfield(sens, 'elecpos')
+  ismeg = false;
+  iseeg = true;
+  isnirs = false;
+elseif isfield(sens, 'optopos')
+  ismeg = false;
+  iseeg = false;
+  isnirs = true;
+else
+  % doing it this way takes a lot more CPU time
+  ismeg  = ft_senstype(sens, 'meg');
+  iseeg  = ft_senstype(sens, 'eeg');
+  isnirs = ft_senstype(sens, 'nirs');
+end
 
 switch version
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
