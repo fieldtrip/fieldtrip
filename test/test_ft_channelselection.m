fieldtrip/fieldtrip

commit 1a728349d2d0e80b9ee730bf9186e4a4b79091f2

ENH - speed up the selection of channels in case of 'all'
this was needed to make the processing of video and fmri as raw data more manageable

utilities/ft_channelselection.m
@@ -50,7 +50,7 @@ function [channel] = ft_channelselection(desired, datachannel, senstype)
 % See also FT_PREPROCESSING, FT_SENSLABEL, FT_MULTIPLOTER, FT_MULTIPLOTTFR,
 % FT_SINGLEPLOTER, FT_SINGLEPLOTTFR
 
-% Copyright (C) 2003-2016, Robert Oostenveld
+% Copyright (C) 2003-2021, Robert Oostenveld
 %
 % This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
 % for the documentation and details.
@@ -77,22 +77,18 @@ if isempty(recursion)
   recursion = false;
 end
 
-if isempty(desired)
-  % ensure this is an empty cell-array, not an empty numeric array
-  desired = {};
+if nargin<3
+  % the actual senstype will be determined further down
+  % postponing the detection speeds up some common use patterns, such as 'all' and []
+  senstype = [];
 end
-
-if nargin<3 || isempty(senstype)
-  senstype = ft_senstype(datachannel);
-end
-
-% this will be specified further down
-datachantype = [];
 
 if iscell(datachannel)
-  % this is the expected input
+  % this is the expected format of the input
+  datachantype = {};
 elseif ischar(datachannel)
   datachannel = {datachannel};
+  datachantype = {};
 elseif isstruct(datachannel) && isfield(datachannel, 'label')
   % it looks like a header structure
   hdr = datachannel;
@@ -102,13 +98,6 @@ else
   ft_error('please specify the data channels as a cell-array');
 end
 
-if ~ischar(desired) && ~isnumeric(desired) && ~iscell(desired)
-  ft_error('please specify the desired channels as a cell-array or a string');
-end
-
-% start with the list of desired channels, this will be pruned/expanded
-channel = desired;
-
 if length(datachannel)~=length(unique(datachannel))
   ft_warning('discarding non-unique channel names');
   sel = false(size(datachannel));
@@ -116,26 +105,34 @@ if length(datachannel)~=length(unique(datachannel))
     sel(i) = sum(strcmp(datachannel, datachannel{i}))==1;
   end
   datachannel = datachannel(sel);
+  if ~isempty(datachantype)
+    datachantype = datachantype(sel);
+  end
 end
 
-if any(size(channel) == 0)
-  % there is nothing to do if it is empty
+if isempty(desired)
+  % return an empty selection
+  channel = {};
   return
-end
-
-if isnumeric(channel)
-  % remove channels tha fall outside the range
-  channel = channel(channel>=1 & channel<=numel(datachannel));
-  % change index into channelname
-  channel = datachannel(channel);
+elseif isequal(desired, 'all') || isequal(desired, {'all'})
+  % this is a very common use pattern that can be dealt with quickly
+  channel = datachannel;
   return
-end
-
-if ~iscell(channel)
+elseif isnumeric(desired)
+  % remove channels that fall outside the range
+  desired = desired(desired>=1 & desired<=numel(datachannel));
+  % change the numeric index into the channel name
+  channel = datachannel(desired);
+  return
+elseif ischar(desired)
   % ensure that a single input argument like 'all' also works
-  % the case of a vector with channel indices has already been dealt with
-  channel = {channel};
+  desired = {desired};
+elseif ~iscell(desired)
+  ft_error('please specify the desired channels as a cell-array or a string');
 end
+
+% start with the list of desired channels, this will be pruned/expanded further down
+channel = desired;
 
 % ensure that both inputs are column vectors
 channel     = channel(:);
@@ -247,6 +244,10 @@ end
 if ~isempty(findreg)
   findreg  = unique(findreg); % remove multiple occurances due to multiple wildcards
   labelreg = datachannel(labelreg);
+end
+
+if isempty(senstype)
+  senstype = ft_senstype(datachannel);
 end
 
 switch senstype

utilities/match_str.m
@@ -16,7 +16,7 @@ function [sel1, sel2] = match_str(a, b, fullout)
 % in the output, in this case. This can be convenient in rare cases if the
 % size of the input lists is meaningful.
 
-% Copyright (C) 2000-2012, Robert Oostenveld
+% Copyright (C) 2000-2021, Robert Oostenveld
 %
 % This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
 % for the documentation and details.
@@ -66,6 +66,13 @@ a = a(:);
 b = b(:);
 Na = numel(a);
 Nb = numel(b);
+
+% this is a very common use pattern that can be dealt with quickly
+if isequal(a, b)
+  sel1 = 1:Na;
+  sel2 = 1:Nb;
+  return
+end
 
 % According to the original implementation empty numeric elements are
 % allowed, but are not returned as match. This is different to empty string

commit b24a3b08d03c247e1d0a2809da22c02a49f2b069

FIX - fixed regression error introduced with 1a72834 the error was due to ft_channelselection and match_str returning row vectors rather than column vectors (as expected by the FT code elsewhere)

utilities/ft_channelselection.m
@@ -116,7 +116,8 @@ if isempty(desired)
   return
 elseif isequal(desired, 'all') || isequal(desired, {'all'})
   % this is a very common use pattern that can be dealt with quickly
-  channel = datachannel;
+  % ensure that the output is a column vector
+  channel = datachannel(:);
   return
 elseif isnumeric(desired)
   % remove channels that fall outside the range

utilities/match_str.m
@@ -69,8 +69,9 @@ Nb = numel(b);
 
 % this is a very common use pattern that can be dealt with quickly
 if isequal(a, b)
-  sel1 = 1:Na;
-  sel2 = 1:Nb;
+  % the returned vectors must be columns
+  sel1 = (1:Na)';
+  sel2 = (1:Nb)';
   return
 end
 

test/test_ft_channelselection.m
@@ -6,47 +6,47 @@ function test_ft_channelselection
 
 datasets = ref_datasets;
 
-for i=1:size(datasets,2);
-    filename = fullfile(datasets(i).origdir, 'original', datasets(i).type, datasets(i).datatype, datasets(i).filename);
-    
-    if isempty(datasets(i).dataformat)
-        hdr = ft_read_header(filename);
-    else
-        hdr = ft_read_header(filename,'headerformat',datasets(i).dataformat);
+for i=1:size(datasets,2)
+  filename = fullfile(datasets(i).origdir, 'original', datasets(i).type, datasets(i).datatype, datasets(i).filename);
+  
+  if isempty(datasets(i).dataformat)
+    hdr = ft_read_header(filename);
+  else
+    hdr = ft_read_header(filename,'headerformat',datasets(i).dataformat);
+  end
+  
+  if ~ft_senstype(hdr, datasets(i).senstype)
+    error(['incorrect senstype detection: ' datasets(i).datatype]);
+  else
+    type=ft_senstype(hdr);
+  end
+  
+  if ~isnan(datasets(i).numeeg) && length(ft_channelselection('EEG', hdr.label, type))~=datasets(i).numeeg
+    error('incorrect number of EEG channels');
+  end
+  if ~isnan(datasets(i).nummeg) && length(ft_channelselection('MEG', hdr.label, type))~=datasets(i).nummeg
+    error('incorrect number of MEG channels');
+  end
+  if ~isnan(datasets(i).numeog) && length(ft_channelselection('EOG', hdr.label, type))~=datasets(i).numeog
+    error('incorrect number of EOG channels');
+  end
+  if ~isnan(datasets(i).numecg) && length(ft_channelselection('ECG', hdr.label, type))~=datasets(i).numecg
+    error('incorrect number of ECG channels');
+  end
+  if ~isnan(datasets(i).numemg) && length(ft_channelselection('EMG', hdr.label, type))~=datasets(i).numemg
+    error('incorrect number of EMG channels');
+  end
+  
+  %exceptions for 'neuromag306' and 'yokogawa160' MEG datasets:
+  %selecting gradiometer/magnetometer sensors
+  if strcmp(type,'neuromag306') || strcmp(type,'yokogawa160')
+    if ~isnan(datasets(i).numemg) && length(ft_channelselection('MEGMAG', hdr.label, type))~=datasets(i).nummegmag
+      error('incorrect number of MEGMAG channels');
     end
-    
-    if ~ft_senstype(hdr, datasets(i).senstype)
-        error(['incorrect senstype detection: ' datasets(i).datatype]);
-    else
-        type=ft_senstype(hdr);
+    if ~isnan(datasets(i).numemg) && length(ft_channelselection('MEGGRAD', hdr.label, type))~=datasets(i).nummeggrad
+      error('incorrect number of MEGGRAD channels');
     end
-    
-    if ~isnan(datasets(i).numeeg) && length(ft_channelselection('EEG', hdr.label, type))~=datasets(i).numeeg
-        error('incorrect number of EEG channels');
-    end
-    if ~isnan(datasets(i).nummeg) && length(ft_channelselection('MEG', hdr.label, type))~=datasets(i).nummeg
-        error('incorrect number of MEG channels');
-    end
-    if ~isnan(datasets(i).numeog) && length(ft_channelselection('EOG', hdr.label, type))~=datasets(i).numeog
-        error('incorrect number of EOG channels');
-    end
-    if ~isnan(datasets(i).numecg) && length(ft_channelselection('ECG', hdr.label, type))~=datasets(i).numecg
-        error('incorrect number of ECG channels');
-    end
-    if ~isnan(datasets(i).numemg) && length(ft_channelselection('EMG', hdr.label, type))~=datasets(i).numemg
-        error('incorrect number of EMG channels');
-    end
-
-    %exceptions for 'neuromag306' and 'yokogawa160' MEG datasets:
-    %selecting gradiometer/magnetometer sensors
-    if strcmp(type,'neuromag306') || strcmp(type,'yokogawa160')
-        if ~isnan(datasets(i).numemg) && length(ft_channelselection('MEGMAG', hdr.label, type))~=datasets(i).nummegmag
-            error('incorrect number of MEGMAG channels');
-        end
-        if ~isnan(datasets(i).numemg) && length(ft_channelselection('MEGGRAD', hdr.label, type))~=datasets(i).nummeggrad
-            error('incorrect number of MEGGRAD channels');
-        end
-    end
+  end
 end
 
 %% test http://bugzilla.fieldtriptoolbox.org/show_bug.cgi?id=3135
@@ -55,7 +55,7 @@ label   = {'Cz','FCz','FC3'};
 
 sel = ft_channelselection(desired, label);
 assert(all(ismember(sel, desired)), 'not all selected channels were desired');
-  
+
 %% test http://bugzilla.fieldtriptoolbox.org/show_bug.cgi?id=3225#c7
 
 sel = ft_channelselection('Cz', label);
@@ -73,5 +73,30 @@ assert(numel(sel)==1) % anything with a 3 in it
 sel = ft_channelselection('*C*', label);
 assert(numel(sel)==3) % anything with a C in it
 
+%% test regression error introduced with https://github.com/fieldtrip/fieldtrip/commit/1a728349d2d0e80b9ee730bf9186e4a4b79091f2
 
+desired = {'Cz','FCz','FC3'};
+label   = {'Cz','FCz','FC3'};
 
+sel = ft_channelselection('all', label);
+assert(size(sel,1)==3); % it must be a column vector, regardless of the input shape
+sel = ft_channelselection('all', label');
+assert(size(sel,1)==3); % it must be a column vector, regardless of the input shape
+
+sel = ft_channelselection(desired, label);
+assert(size(sel,1)==3); % it must be a column vector, regardless of the input shape
+sel = ft_channelselection(desired, label');
+assert(size(sel,1)==3); % it must be a column vector, regardless of the input shape
+sel = ft_channelselection(desired', label);
+assert(size(sel,1)==3); % it must be a column vector, regardless of the input shape
+sel = ft_channelselection(desired', label');
+assert(size(sel,1)==3); % it must be a column vector, regardless of the input shape
+
+[sel1, sel2] = match_str(desired, label);
+assert(size(sel1,1)==3 && size(sel2,1)==3); % it must be a column vector, regardless of the input shape
+[sel1, sel2] = match_str(desired, label');
+assert(size(sel1,1)==3 && size(sel2,1)==3); % it must be a column vector, regardless of the input shape
+[sel1, sel2] = match_str(desired', label);
+assert(size(sel1,1)==3 && size(sel2,1)==3); % it must be a column vector, regardless of the input shape
+[sel1, sel2] = match_str(desired', label');
+assert(size(sel1,1)==3 && size(sel2,1)==3); % it must be a column vector, regardless of the input shape