e1891eadf1788157dd26ba93b16851078994b281
Robert Oostenveld <r.oostenveld@gmail.com>
2021-05-06 10:19:44 +0200

    added missing dependencies as subfunction

--- fileio/ft_write_tsv.m
+++ fileio/ft_write_tsv.m
@@ -38,3 +38,37 @@ for i=1:numel(fn)
   tsv.(fn{i}) = output_compatible(tsv.(fn{i}));
 end
 writetable(tsv, filename, 'Delimiter', 'tab', 'FileType', 'text');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION this is shared with DATA2BIDS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function val = output_compatible(val)
+if istable(val)
+  fn = val.Properties.VariableNames;
+  for i=1:numel(fn)
+    val.(fn{i}) = output_compatible(val.(fn{i}));
+  end
+elseif iscell(val)
+  % use recursion to make all elements compatible
+  val = cellfun(@output_compatible, val, 'UniformOutput', false);
+elseif isnumeric(val) && numel(val)>1 && any(isnan(val))
+  % convert and use recursion to make all elements compatible
+  val = num2cell(val);
+  val = cellfun(@output_compatible, val, 'UniformOutput', false);
+else
+  % write [] as 'n/a'
+  % write nan as 'n/a'
+  % write boolean as 'True' or 'False'
+  if isempty(val)
+    val = 'n/a';
+  elseif isnan(val)
+    val = 'n/a';
+  elseif islogical(val)
+    if val
+      val = 'True';
+    else
+      val = 'False';
+    end
+  end
+end
+
