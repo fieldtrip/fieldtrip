de5c7b82aa1c72fb1c8013cda0b4c4e15857b8b2
Jan-Mathijs Schoffelen <1517611+schoffelen@users.noreply.github.com>
2021-06-01 11:09:53 +0200

    ENH - improve varargout handling for octave (#1771)

--- utilities/ft_error.m
+++ utilities/ft_error.m
@@ -31,7 +31,7 @@ function varargout = ft_error(varargin)
 % $Id$
 
 if nargout
-  varargout{:} = ft_notification(varargin{:});
+  [varargout{1:nargout}] = ft_notification(varargin{:});
 elseif isequal(varargin, {'last'})
   % return an answer anyway
   varargout{1} = ft_notification(varargin{:});
