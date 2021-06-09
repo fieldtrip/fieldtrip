4792c437305b1013d7204ce64e5121af09d2f67f
Robert Oostenveld <r.oostenveld@gmail.com>
2021-06-09 12:43:09 +0200

    give explicit error if file does not exist

--- fileio/private/read_yorkinstruments_hdf5_meta.m
+++ fileio/private/read_yorkinstruments_hdf5_meta.m
@@ -1,9 +1,11 @@
 function [info] = read_yorkinstruments_hdf5_meta(datafile, acq_run)
 
-% info=read_yorkinstruments_hdf5_meta(datafile)
-% Collects the required Fieldtrip header data from the data file 'filename'
+% READ_YPRKINSTRUMENTS_HDF5_META reads the metatada and header information from a .meghdf5 file
 %
-% Copyright (C) 2020, York Neuroimaging Centre, University of York, R Aveyard
+% Use as
+%   info=read_yorkinstruments_hdf5_meta(datafile)
+
+% Copyright (C) 2020-2021, York Neuroimaging Centre, University of York, R Aveyard
 %
 % This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
 % for the documentation and details.
@@ -29,47 +31,48 @@ if nargin < 2
 end
 
 %Check that the datafile exists
-if ~isempty(datafile)
-  %Check that the acquisition exists in the data file
-  try
-    check = h5info(datafile,strcat('/acquisitions/',num2str(acq_run)));
+if ~exist(datafile, 'file')
+  error('%s does not exist', datafile);
+end
+
+%Check that the acquisition exists in the data file
+try
+  check = h5info(datafile,strcat('/acquisitions/',num2str(acq_run)));
+catch
+  error('Invalid YI HDF5 file: Missing dataset "/acquisitions/%d".',acq_run);
+end
+
+%Check that this acquisition is 'real' data, not COH or something else
+info.acq_type=char(h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'acq_type'));
+if  ~strcmp(info.acq_type,'ACQ')
+  error('"/acquisitions/%d" is not a data acquisition run.',acq_run);
+end
+
+info.SampleFrequency=h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'sample_rate');
+info.Sequence=h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'sequence');
+info.Description=char(h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'description'));
+info.StartTime=char(h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'start_time'));
+info.WeightsConfigured=char(h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'weights_configured'));
+info.ChNames= h5read(datafile,[strcat('/acquisitions/',num2str(acq_run)) '/channel_list/']);
+[info.NChannels, null]=size(info.ChNames);
+Data = h5read(datafile,[strcat('/acquisitions/',num2str(acq_run)) '/data/']);
+[null, info.NSamples]=size(Data);
+try [null, info.NTrials] = size(h5read(datafile,[strcat('/acquisitions/',num2str(acq_run)) '/epochs/trigger_codes']));
+catch info.NTrials=1;
+end
+
+info.ChUnit = strings(info.NChannels,1);
+info.ChType = strings(info.NChannels,1);
+
+for i = 1:info.NChannels
+  info.ChUnit(i)=h5readatt(datafile, char(strcat('/config/channels/',info.ChNames(i) )) ,'units');
+  if isequal(info.ChUnit(i), '?')
+    info.ChUnit(i)='unknown';
+  end
+  
+  info.ChType(i)=h5readatt(datafile, char(strcat('/config/channels/',info.ChNames(i) )) ,'chan_type');
+  try info.ChType(i)=h5readatt(datafile, char(strcat('/config/channels/',info.ChNames(i) )) ,'mode');
   catch
-    error('Invalid YI HDF5 file: Missing dataset "/acquisitions/%d".',acq_run);
+    continue
   end
-  %Check that this acquisition is 'real' data, not COH or something else
-  info.acq_type=char(h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'acq_type'));
-  if  ~strcmp(info.acq_type,'ACQ')
-    error('"/acquisitions/%d" is not a data acquisition run.',acq_run);
-  end
-  
-  info.SampleFrequency=h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'sample_rate');
-  info.Sequence=h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'sequence');
-  info.Description=char(h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'description'));
-  info.StartTime=char(h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'start_time'));
-  info.WeightsConfigured=char(h5readatt(datafile,  strcat('/acquisitions/',num2str(acq_run))  ,'weights_configured'));
-  info.ChNames= h5read(datafile,[strcat('/acquisitions/',num2str(acq_run)) '/channel_list/']);
-  [info.NChannels, null]=size(info.ChNames);
-  Data = h5read(datafile,[strcat('/acquisitions/',num2str(acq_run)) '/data/']);
-  [null, info.NSamples]=size(Data);
-  try [null, info.NTrials] = size(h5read(datafile,[strcat('/acquisitions/',num2str(acq_run)) '/epochs/trigger_codes']));
-  catch info.NTrials=1;
-  end
-  
-  info.ChUnit = strings(info.NChannels,1);
-  info.ChType = strings(info.NChannels,1);
-  
-  for i = 1:info.NChannels
-    info.ChUnit(i)=h5readatt(datafile, char(strcat('/config/channels/',info.ChNames(i) )) ,'units');
-    if isequal(info.ChUnit(i), '?')
-      info.ChUnit(i)='unknown';
-    end
-    
-    info.ChType(i)=h5readatt(datafile, char(strcat('/config/channels/',info.ChNames(i) )) ,'chan_type');
-    try info.ChType(i)=h5readatt(datafile, char(strcat('/config/channels/',info.ChNames(i) )) ,'mode');
-    catch
-      continue
-    end
-  end
-  
-  
 end
