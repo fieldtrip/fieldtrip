ec7b125d5fde6b1b407e3d84f3d2abee001b9784
Jan-Mathijs Schoffelen <jm.schoffelen@gmail.com>
2021-06-15 09:59:50 +0200

    DOCU - update

--- preproc/ft_preproc_dftfilter.m
+++ preproc/ft_preproc_dftfilter.m
@@ -18,7 +18,7 @@ function [filt] = ft_preproc_dftfilter(dat, Fs, Fl, varargin)
 %              the default European value of 50 Hz is assumed)
 % 
 % Additional optional arguments are to be provided as key-value pairs:
-%   dftreplace = 'zero' (default) or 'dftreplace'.
+%   dftreplace = 'zero' (default), 'dftreplace' or 'dftreplace_fft'.
 % 
 % If dftreplace = 'zero', the powerline component's amplitude is estimated by
 % fitting a sine and cosine at the specified frequency, and subsequently
@@ -30,12 +30,25 @@ function [filt] = ft_preproc_dftfilter(dat, Fs, Fl, varargin)
 % used to estimate the line noise. The estimate is subtracted from the
 % complete data.
 %
+% If dftreplace = 'neighbour' the powerline component is reduced via
+% spectrum interpolation (Leske & Dalal, 2019, NeuroImage 189,
+% doi: 10.1016/j.neuroimage.2019.01.026), estimating the required signal
+% components by fitting sines and cosines. The algorhmic steps are
+% described in more detail below. % Preferably the data should have a length 
+% that is an integer multiple of the oscillation period of the line noise
+% (i.e. 20ms for 50Hz noise). If the data is of different length, then only
+% the first N complete periods are used to estimate the line noise. The 
+% estimate is subtracted from the complete data. Due to the possibility of
+% using slightly truncated data for the estimation of the necessary signal
+% components, this method is more forgiving with respect to numerical
+% issues with respect to the sampling frequency, and suboptimal epoch
+% lengths, in comparison to the method below.
+%
 % If dftreplace = 'neighbour_fft' the powerline component is reduced via spectrum 
-% interpolation (Leske & Dalal, 2019, NeuroImage 189,
-%  doi: 10.1016/j.neuroimage.2019.01.026)
-% The signal is:
-% I)   transformed into the frequency domain via a discrete Fourier
-%       transform (DFT),
+% interpolation, in its original implementation, based on an algorithm that
+% uses an FFT and iFFT for the estimation of the spectral components. The signal is:
+% I)   transformed into the frequency domain via a fast Fourier
+%       transform (FFT),
 % II)  the line noise component (e.g. 50Hz, Flwidth = 1 (±1Hz): 49-51Hz) is
 %       interpolated in the amplitude spectrum by replacing the amplitude
 %       of this frequency bin by the mean of the adjacent frequency bins
@@ -44,8 +57,8 @@ function [filt] = ft_preproc_dftfilter(dat, Fs, Fl, varargin)
 %       Neighwidth = 2 (±2Hz) implies 47-49 Hz and 51-53 Hz).
 %       The original phase information of the noise frequency bin is
 %       retained.
-% III) the signal is transformed back into the time domain via inverse DFT
-%       (iDFT).
+% III) the signal is transformed back into the time domain via inverse FFT
+%       (iFFT).
 % If Fline is a vector (e.g. [50 100 150]), harmonics are also considered.
 % Preferably the data should be continuous or consist of long data segments
 % (several seconds) to avoid edge effects. If the sampling rate and the
@@ -59,7 +72,7 @@ function [filt] = ft_preproc_dftfilter(dat, Fs, Fl, varargin)
 %
 %   dftwidth          bandwidth of line noise frequencies, applies to spectrum interpolation, in Hz
 %   dftneighbourwidth width of frequencies neighbouring line noise frequencies, applies to spectrum interpolation (dftreplace = 'neighbour'), in Hz
-%
+% 
 % If the data contains NaNs, the output of the affected channel(s) will be
 % all(NaN).
 %
