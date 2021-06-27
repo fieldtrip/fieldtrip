160d6baa1780ad3be80a6039312e245d1e405eb6
Robert Oostenveld <r.oostenveld@gmail.com>
2021-06-27 12:28:05 +0200

    some code cleanups to existing test script

--- test/test_pull1602.m
+++ test/test_pull1602.m
@@ -1,38 +1,36 @@
-function test_ft_specest_irasa
+function test_pull1602
 
 % MEM 2gb
 % WALLTIME 00:10:00
-% DEPENDENCY ft_freqanalysis ft_specest_irasa
-
-% We do not have that many concurrent licenses of the DSP Systems toolbox (https://nl.mathworks.com/products/dsp-system.html)
-% which was used in the initial version of this script. Therefore this test script does not use the DSP toolbox by default.
-usedsptoolbox = false;
+% DEPENDENCY ft_specest_irasa ft_freqanalysis
 
 % simulate data
-t = (1:2000)/1000; % time axis
+time = (1:2000)/1000;
 for rpt = 1:20
-    if usedsptoolbox
-      % generate pink noise using the DSP toolbox
-        dspobj = dsp.ColoredNoise('Color', 'pink', 'SamplesPerFrame', length(t));
-        fn = dspobj()';
-    else
-        % use another method to make pink noise
-        fn = cumsum(randn(1,length(t))); 
-    end
-    
-    % add line noise
-    data.trial{1,rpt} = fn + cos(2*pi*50*t) + cos(2*pi*100*t);
-    data.time{1,rpt}  = t;
-    data.label{1}     = 'chan';
-    data.trialinfo(rpt,1) = rpt;
+  if false
+    % We do not have that many concurrent licenses of the DSP Systems toolbox (https://nl.mathworks.com/products/dsp-system.html)
+    % which was used in the initial version of this script. Therefore this test script does not use the DSP toolbox by default.
+    dspobj = dsp.ColoredNoise('Color', 'pink', 'SamplesPerFrame', length(time));
+    signal = dspobj()';
+  else
+    % integrate over time to create pink noise with 1/f power spectrum
+    signal = cumsum(randn(1,length(time)));
+  end
+  
+  % add line noise
+  data.trial{1,rpt}     = signal + cos(2*pi*50*time) + cos(2*pi*100*time);
+  data.time{1,rpt}      = time;
+  data.label{1}         = 'chan';
+  data.trialinfo(rpt,1) = rpt;
 end
 
 % using unfiltered data
 cfg = [];
-cfg.method = 'irasa';
-cfg.output = 'original';
-cfg.pad   = 'nextpow2';
+cfg.method  = 'irasa';
+cfg.output  = 'original';
+cfg.pad     = 'nextpow2';
 freq = ft_freqanalysis(cfg, data);
+
 cfg.output = 'fractal';
 freqI = ft_freqanalysis(cfg, data);
 
@@ -41,33 +39,50 @@ cfg2            = [];
 cfg2.bpfilter   = 'yes';
 cfg2.bpfilttype = 'firws';
 cfg2.bpfreq     = [60 150];
-datafilt       = ft_preprocessing(cfg2, data);
+datafilt1 = ft_preprocessing(cfg2, data);
 
-cfg2.bpfilter = 'no';
-cfg2.hpfilter = 'yes';
-cfg2.hpfreq   = 60;
+cfg2.bpfilter   = 'no';
+cfg2.hpfilter   = 'yes';
+cfg2.hpfreq     = 60;
 cfg2.hpfilttype = 'firws';
-datafilt2       = ft_preprocessing(cfg2, data);
+datafilt2 = ft_preprocessing(cfg2, data);
 
 cfg2.bpfilter   = 'no';
 cfg2.hpfilter   = 'no';
 cfg2.dftfilter  = 'yes'; % notch filter to filter out line noise
 cfg2.dftfreq    = [50 100];
-datafilt3       = ft_preprocessing(cfg2, data);
+datafilt3 = ft_preprocessing(cfg2, data);
 
 cfg.output = 'original';
-freqfilt = ft_freqanalysis(cfg, datafilt);
+freqfilt1  = ft_freqanalysis(cfg, datafilt1);
 freqfilt2 = ft_freqanalysis(cfg, datafilt2);
 freqfilt3 = ft_freqanalysis(cfg, datafilt3);
 
 cfg.output = 'fractal';
-freqfiltI = ft_freqanalysis(cfg, datafilt);
+freqfiltI = ft_freqanalysis(cfg, datafilt1);
 freqfiltI2 = ft_freqanalysis(cfg, datafilt2);
 freqfiltI3 = ft_freqanalysis(cfg, datafilt3);
 
-figure; 
-semilogy(freq.freq, [freq.powspctrm;freqfilt.powspctrm;freqfilt2.powspctrm;freqfilt3.powspctrm;...
-                     freqI.powspctrm;freqfiltI.powspctrm;freqfiltI2.powspctrm;freqfiltI3.powspctrm]);
-legend({'orig-unfilterd';'orig-bpfiltered';'orig-hpfiltered';'orig-dftfiltered';...
-        'frac-unfiltered';'frac-bpfiltered';'frac-hpfiltered';'frac-dftfiltered'});
+allpow = [
+  freq.powspctrm
+  freqfilt1.powspctrm
+  freqfilt2.powspctrm
+  freqfilt3.powspctrm
+  freqI.powspctrm
+  freqfiltI.powspctrm
+  freqfiltI2.powspctrm
+  freqfiltI3.powspctrm
+  ];
 
+figure;
+semilogy(freq.freq, allpow);
+legend({
+  'orig-unfilterd'
+  'orig-bpfiltered'
+  'orig-hpfiltered'
+  'orig-dftfiltered'
+  'frac-unfiltered'
+  'frac-bpfiltered'
+  'frac-hpfiltered'
+  'frac-dftfiltered'
+  });
