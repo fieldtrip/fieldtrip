109d01e5c2651cbc4842d0655b16de8435ad4e29
LucaLaFisca <lucalafisca@gmail.com>
2021-06-30 16:57:34 +0200

    DEV - added test_ft_preproc_xxxfilter functions

---
+++ test/test_ft_preproc_bandpassfilter.m
@@ -0,0 +1,83 @@
+function test_ft_preproc_bandpassfilter
+
+% WALLTIME 00:10:00
+% MEM 2gb
+% DEPENDENCY ft_preproc_bandpassfilter
+
+% tic
+inspect_results = false; %true if visual inspection required
+
+nconfig = 8; %number of different configurations to test
+
+fs = 500;
+nchan = 32;
+passband = [4 70];
+start_time = -1; %s
+end_time = 2.5; %s
+nsamples = (end_time - start_time) * fs;
+data = [];
+data.time{1} = linspace(start_time, end_time, nsamples);
+
+data.trial{1} = randn(nchan,nsamples);
+data.label = cellstr(num2str((1:nchan).'));
+data.fsample = fs;
+
+datafilt = cell(1,nconfig);
+
+cfg              = [];
+cfg.bpfilter    = 'yes'; %apply line noise filter with spectrum interpolation
+cfg.bpfreq      = passband;
+datafilt{1} = ft_preprocessing(cfg, data);
+
+choices = num2cell(2:6);
+for i = 1:length(choices)
+    cfg.bpfiltord = choices{i};
+    datafilt{2} = ft_preprocessing(cfg, data);
+end
+
+choices = {'firws' , 'fir' , 'firls'};
+for i = 1:length(choices)
+    cfg.bpfilttype = choices{i};
+    datafilt{3} = ft_preprocessing(cfg, data);
+end
+
+choices = {'onepass' , 'onepass-reverse' , 'onepass-zerophase'};
+for i = 1:length(choices)
+    cfg.bpfiltdir = choices{i};%= filter direction, 'twopass' (default), 'onepass' or 'onepass-reverse' or 'onepass-zerophase' (default for firws) or 'onepass-minphase' (firws, non-linear!)
+    datafilt{4} = ft_preprocessing(cfg, data);
+end
+
+choices = {'reduce', 'split'};
+for i = 1:length(choices)
+    cfg.bpinstabilityfix = choices{i};
+    datafilt{5} = ft_preprocessing(cfg, data);
+end
+
+choices = num2cell(1:5);
+for i = 1:length(choices)
+    cfg.bpfiltdf = choices{i};
+    datafilt{6} = ft_preprocessing(cfg, data);
+end
+
+choices = {'hann' , 'blackman' , 'kaiser'};
+for i = 1:length(choices)
+    cfg.bpfiltwintype = choices{i};
+    datafilt{7} = ft_preprocessing(cfg, data);
+end
+
+choices = num2cell(logspace(-3,0,4));
+for i = 1:length(choices)
+    cfg.bpfiltdev = choices{i};
+    datafilt{8} = ft_preprocessing(cfg, data);
+end
+
+
+if inspect_results
+    figure;
+    for i = 1:nconfig
+        subplot(ceil(nconfig/2),2,i);plot(datafilt{i}.time{1}, datafilt{i}.trial{1}(1,:)); ylim([-2.1 2.1]);xlabel(['config ' num2str(i)]);
+    end
+end
+
+clear datafilt
+% toc
