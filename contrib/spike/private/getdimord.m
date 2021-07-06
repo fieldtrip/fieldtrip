b021e449e5c0297b6a5b2f4164ee80c39607b200
Robert Oostenveld <r.oostenveld@gmail.com>
2021-07-06 12:23:45 +0200

    automatically synchronized identical files to 7f7d3c30ae4e79d8256a23f064734e3567db1ace

--- contrib/spike/private/getdimord.m
+++ contrib/spike/private/getdimord.m
@@ -137,7 +137,7 @@ if isfield(data, 'cumtapcnt')
     nrpttap = sum(data.cumtapcnt);
   else
     % it is a matrix, hence it is repetitions by frequencies
-    % this happens after  mtmconvol with keeptrials
+    % this happens after mtmconvol with keeptrials
     nrpttap = sum(data.cumtapcnt,2);
     if any(nrpttap~=nrpttap(1))
       ft_warning('unexpected variation of the number of tapers over trials')
