de31911833fa69ce55173c23b68b6159f4ba52d9
Robert Oostenveld <r.oostenveld@gmail.com>
2021-07-07 22:03:48 +0200

    use data.elec instead of data.sens

--- test/test_ft_sourceanalysis.m
+++ test/test_ft_sourceanalysis.m
@@ -16,18 +16,18 @@ data.time{1} = linspace(start_time, end_time, nsamples);
 data.trial{1} = randn(nchan,nsamples);
 data.sampleinfo = [1 nsamples];
 data.label = cellstr(num2str((1:nchan).'));
-data.sens.label = data.label;
-data.sens.chanpos = randn(nchan,3);
-data.sens.elecpos = data.sens.chanpos;
-data.sens.tra = eye(nchan);
-data.sens.unit = 'cm';
+data.elec.label = data.label;
+data.elec.elecpos = randn(nchan,3);
+for i=1:nchan
+  data.elec.elecpos(i,:) = 10*data.elec.elecpos(i,:)/norm(data.elec.elecpos(i,:));
+end
+data.elec.tra = eye(nchan);
+data.elec.unit = 'cm';
 
 geometry = [];
-geometry.pos = data.sens.chanpos;
-geometry.unit = data.sens.unit;
-cfg = [];
-cfg.conductivity = [0.33 1.00 0.042 0.33];
-headmodel = ft_headmodel_concentricspheres(geometry, 'conductivity', cfg.conductivity);
+geometry.pos = data.elec.chanpos;
+geometry.unit = data.elec.unit;
+headmodel = ft_headmodel_concentricspheres(geometry, 'conductivity', [0.33 1.00 0.042 0.33]);
 
 % test for comp type
 cfg = [];
@@ -37,25 +37,23 @@ cfg = [];
 cfg.xgrid = 'auto';
 cfg.ygrid = 'auto';
 cfg.zgrid = 'auto';
-cfg.resolution = 1; %cm
+cfg.resolution = 1; % cm
 cfg.component = 1:5;
 cfg.headmodel = headmodel;
-cfg.elec = data.sens;
 sourceout = ft_sourceanalysis(cfg, comp);
 
 % test for timelock type
 cfg = [];
-timelock = ft_timelockanalysis(cfg,data);
+timelock = ft_timelockanalysis(cfg, data);
 ft_checkdata(timelock, 'datatype', 'timelock');
 cfg = [];
 cfg.xgrid = 'auto';
 cfg.ygrid = 'auto';
 cfg.zgrid = 'auto';
-cfg.resolution = 1; %cm
+cfg.resolution = 1; % cm
 cfg.latency = [0 1];
 cfg.model = 'regional';
 cfg.headmodel = headmodel;
-cfg.elec = data.sens;
 sourceout = ft_sourceanalysis(cfg, timelock);
 
 % test for freq type
@@ -64,8 +62,9 @@ cfg.method = 'mtmfft';
 cfg.taper = 'dpss';
 cfg.tapsmofrq = 2;
 cfg.output = 'fourier';
-freq = ft_freqanalysis(cfg,data);
+freq = ft_freqanalysis(cfg, data);
 ft_checkdata(freq, 'datatype', 'freq');
+
 cfg = [];
 cfg.xgrid = 'auto';
 cfg.ygrid = 'auto';
@@ -73,5 +72,4 @@ cfg.zgrid = 'auto';
 cfg.resolution = 1; %cm
 cfg.frequency = 10; %Hz
 cfg.headmodel = headmodel;
-cfg.elec = data.sens;
 sourceout = ft_sourceanalysis(cfg, freq);
