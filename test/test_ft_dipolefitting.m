af6b4524ce89dda1452ecf2c238885222fab8e8a
Robert Oostenveld <r.oostenveld@gmail.com>
2021-07-07 21:59:49 +0200

    use data.elec rather than data.sens, scaled electrodes to fit on 10cm sphere

--- test/test_ft_dipolefitting.m
+++ test/test_ft_dipolefitting.m
@@ -7,8 +7,8 @@ function test_ft_dipolefitting
 
 fs = 500;
 nchan = 32;
-start_time = -1; %s
-end_time = 2.5; %s
+start_time = -1; % seconds
+end_time = 2.5; % seconds
 nsamples = (end_time - start_time) * fs + 1;
 
 data = [];
@@ -16,23 +16,26 @@ data.time{1} = linspace(start_time, end_time, nsamples);
 data.trial{1} = randn(nchan,nsamples);
 data.sampleinfo = [1 nsamples];
 data.label = cellstr(num2str((1:nchan).'));
-data.sens.label = data.label;
-data.sens.chanpos = randn(nchan,3);
-data.sens.elecpos = data.sens.chanpos;
-data.sens.tra = eye(nchan);
-data.sens.unit = 'cm';
+data.elec.label = data.label;
+data.elec.elecpos = randn(nchan,3);
+for i=1:nchan
+  data.elec.elecpos(i,:) = 10*data.elec.elecpos(i,:)/norm(data.elec.elecpos(i,:));
+end
+data.elec.chanpos = data.elec.elecpos;
+data.elec.tra = eye(nchan);
+data.elec.unit = 'cm';
 
 geometry = [];
-geometry.pos = data.sens.chanpos;
+geometry.pos = data.sens.elecpos;
 geometry.unit = data.sens.unit;
-cfg = [];
-cfg.conductivity = [0.33 1.00 0.042 0.33];
-headmodel = ft_headmodel_concentricspheres(geometry, 'conductivity', cfg.conductivity);
+% fit a 4-sphere concentric model to the geometry
+headmodel = ft_headmodel_concentricspheres(geometry, 'conductivity', [0.33 1.00 0.042 0.33]);
 
 % test for comp type
 cfg = [];
-comp = ft_componentanalysis(cfg,data);
+comp = ft_componentanalysis(cfg, data);
 ft_checkdata(comp, 'datatype', 'comp');
+
 cfg = [];
 cfg.xgrid = 'auto';
 cfg.ygrid = 'auto';
@@ -40,13 +43,13 @@ cfg.zgrid = 'auto';
 cfg.resolution = 1; %cm
 cfg.component = 1:5;
 cfg.headmodel = headmodel;
-cfg.elec = data.sens;
 sourceout = ft_dipolefitting(cfg, comp);
 
 % test for timelock type
 cfg = [];
 timelock = ft_timelockanalysis(cfg,data);
 ft_checkdata(timelock, 'datatype', 'timelock');
+
 cfg = [];
 cfg.xgrid = 'auto';
 cfg.ygrid = 'auto';
@@ -55,7 +58,6 @@ cfg.resolution = 1; %cm
 cfg.latency = [0 1];
 cfg.model = 'regional';
 cfg.headmodel = headmodel;
-cfg.elec = data.sens;
 sourceout = ft_dipolefitting(cfg, timelock);
 
 % test for freq type
@@ -66,6 +68,7 @@ cfg.tapsmofrq = 2;
 cfg.output = 'fourier';
 freq = ft_freqanalysis(cfg,data);
 ft_checkdata(freq, 'datatype', 'freq');
+
 cfg = [];
 cfg.xgrid = 'auto';
 cfg.ygrid = 'auto';
@@ -73,5 +76,4 @@ cfg.zgrid = 'auto';
 cfg.resolution = 1; %cm
 cfg.frequency = 10; %Hz
 cfg.headmodel = headmodel;
-cfg.elec = data.sens;
 sourceout = ft_dipolefitting(cfg, freq);
