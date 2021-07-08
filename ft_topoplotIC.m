fieldtrip/fieldtrip

commit 12d82fac01e33ac883aabbe31b57595846b62c2d

fixed a bug in the detection of component data, which caused comp+raw input data to be reformatted in test_ft_sourceanalysis

utilities/ft_checkdata.m
@@ -319,7 +319,7 @@ if ~isempty(dtype)
       case 'timelock'
         okflag = okflag + (istimelock & ~iscomp);
       case 'comp'
-        okflag = okflag + (iscomp & ~(israw | istimelock | isfreq));
+        okflag = okflag + iscomp; % it can simultaneously be raw, timelock, or freq
       case 'spike'
         okflag = okflag + isspike;
       case 'volume'

commit 0b03b048d32efaf4fd643542ac6e1b179485ee3c

FIX - the commit 12d82fac01e33ac883aabbe31b57595846b62c2d introduced (rather than solved) a bug in the detection and conversion of comp-only data. This reverts it and includes some other small changes.

ft_topoplotIC.m
@@ -109,7 +109,7 @@ ft_nargout  = nargout;
 ft_defaults
 ft_preamble init
 ft_preamble debug
-ft_preamble loadvar comp
+ft_preamble loadvar    comp
 ft_preamble provenance comp
 ft_preamble trackconfig
 

private/topoplot_common.m
@@ -270,6 +270,7 @@ for indx=1:Ndata
       if ~isfield(cfg,  'xparam')
         cfg.xlim = [1 1];
         xparam   = '';
+        yparam   = '';
       end
   end
   
@@ -391,18 +392,20 @@ for indx=1:Ndata
   end
   
   % Get physical min/max range of x
-  if strcmp(cfg.xlim, 'maxmin')
-    xmin = min(data.(xparam));
-    xmax = max(data.(xparam));
-  else
-    xmin = cfg.xlim(1);
-    xmax = cfg.xlim(2);
+  if ~isempty(xparam)
+    if strcmp(cfg.xlim, 'maxmin')
+      xmin = min(data.(xparam));
+      xmax = max(data.(xparam));
+    else
+      xmin = cfg.xlim(1);
+      xmax = cfg.xlim(2);
+    end
+    xminindx = nearest(data.(xparam), xmin);
+    xmaxindx = nearest(data.(xparam), xmax);
+    xmin = data.(xparam)(xminindx);
+    xmax = data.(xparam)(xmaxindx);
+    selx = xminindx:xmaxindx;
   end
-  xminindx = nearest(data.(xparam), xmin);
-  xmaxindx = nearest(data.(xparam), xmax);
-  xmin = data.(xparam)(xminindx);
-  xmax = data.(xparam)(xmaxindx);
-  selx = xminindx:xmaxindx;
   
   % Get physical min/max range of y
   if ~isempty(yparam)

utilities/ft_checkdata.m
@@ -319,7 +319,7 @@ if ~isempty(dtype)
       case 'timelock'
         okflag = okflag + (istimelock & ~iscomp);
       case 'comp'
-        okflag = okflag + iscomp; % it can simultaneously be raw, timelock, or freq
+        okflag = okflag + (iscomp && ~(israw || istimelock || isfreq));
       case 'spike'
         okflag = okflag + isspike;
       case 'volume'

test/test_bug1243.m
@@ -8,9 +8,9 @@ load(dccnpath('/home/common/matlab/fieldtrip/data/test/bug1243.mat'));
 
 figure
 for i=1:9
-  subplot(3,3,i);
   cfg = [];
   cfg.component = i;
+  cfg.figure = subplot(3,3,i);
   cfg.layout = 'EEG1020.lay';
   ft_topoplotIC(cfg, comp);
 end