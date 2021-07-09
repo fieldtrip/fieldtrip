fb0db872504b2db966764bdf93418cafb179e80c
Robert Oostenveld <r.oostenveld@gmail.com>
2021-07-09 09:02:46 +0200

    renamed data.sens into data.elec

--- test/test_ft_scalpcurrentdensity.m
+++ test/test_ft_scalpcurrentdensity.m
@@ -1,25 +1,30 @@
 function test_ft_scalpcurrentdensity
 
-
 % MEM 2gb
 % WALLTIME 00:10:00
 % DEPENDENCY ft_scalpcurrentdensity
 
 fs = 500;
 nchan = 32;
-start_time = -1; %s
-end_time = 2.5; %s
+start_time = -1; % seconds
+end_time = 2.5; % seconds
 nsamples = (end_time - start_time) * fs + 1;
 
 data = [];
 data.time{1} = linspace(start_time, end_time, nsamples);
 data.trial{1} = randn(nchan,nsamples);
 data.label = cellstr(num2str((1:nchan).'));
-data.sens.label = data.label;
-data.sens.chanpos = randn(nchan,3);
-data.sens.elecpos = data.sens.chanpos;
-data.sens.tra = eye(nchan);
+
+% construct a set of electrodes randomly distributed over the upper hemisphere
+data.elec.label = data.label;
+data.elec.unit = 'cm';
+data.elec.tra = eye(nchan);
+data.elec.elecpos = randn(nchan,3);
+data.elec.elecpos(:,3) = abs(data.elec.elecpos(:,3));
+for i=1:nchan
+  data.elec.elecpos(i,:) = 10*data.elec.elecpos(i,:)/norm(data.elec.elecpos(i,:));
+end
+data.elec.chanpos = data.elec.elecpos;
 
 cfg = [];
-cfg.elec = data.sens;
-dataout = ft_scalpcurrentdensity(cfg,data);
+dataout = ft_scalpcurrentdensity(cfg, data);
