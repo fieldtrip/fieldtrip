ca495933b89a44747937fd05010e0e28edba9322
Jan-Mathijs Schoffelen <1517611+schoffelen@users.noreply.github.com>
2021-11-17 18:11:19 +0100

    Fix regressionerrors (#1927)
    * FIX - addpath to obsolete so that ft_qualitycheck can be found
    
    * FIX - be lenient on cell-arrays when converting from source 2 volume
    
    * typo
    
    somehow my push into this PR failed
    
    * ENH - also allow for 6D cell arrays

--- test/test_ft_qualitycheck.m
+++ test/test_ft_qualitycheck.m
@@ -4,6 +4,8 @@ function test_ft_qualitycheck
 % WALLTIME 00:30:00
 % DEPENDENCY ft_qualitycheck
 
+ft_hastoolbox('compat/obsolete', 1);
+
 cfg           = [];
 cfg.dataset   = dccnpath('/home/common/matlab/fieldtrip/data/test/original/meg/ctf275/A0132_Aud-Obj-Recognition_20051115_02.ds'); 
 cfg.savemat   = 'no';
