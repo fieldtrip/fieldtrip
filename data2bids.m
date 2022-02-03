1fa0f3ef9f5fe4fbcb902de94740e9697b30587f
Robert Oostenveld <r.oostenveld@gmail.com>
2022-02-03 10:32:11 +0100

    FIX #1942 - format scans.acq_date correctly when specified as a datetime object

--- data2bids.m
+++ data2bids.m
@@ -2447,6 +2447,9 @@ elseif isnumeric(val) && numel(val)>1 && any(isnan(val))
   % convert and use recursion to make all elements compatible
   val = num2cell(val);
   val = cellfun(@output_compatible, val, 'UniformOutput', false);
+elseif isdatetime(val)
+  % see https://bids-specification.readthedocs.io/en/stable/02-common-principles.html#units
+  val = datestr(val, 'yyyy-mm-ddTHH:MM:SS');
 else
   % write [] as 'n/a'
   % write nan as 'n/a'
