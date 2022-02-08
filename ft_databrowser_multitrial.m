726b59955db09219014e1bd19c844f3d549bd9db
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2022-02-08 09:27:46 +0100

    edit ft_databrowser_multitrial to use original sampleinfo indices to find samples for events and artfctdef.xxx.artifact

--- ft_databrowser_multitrial.m
+++ ft_databrowser_multitrial.m
@@ -18,35 +18,47 @@ else
   event = ft_fetch_event(data);
 end
 
-%%%%% shifting sampleinfo and event
-
-% store original events
+% store original sampleinfo and events
 urevent = event;
-% samples to shift
-eventsample = [event.sample];
-sampleinfo = data.sampleinfo;
 
-% time chunks that will be removed
-chunksout = [1 sampleinfo(1,1)-1;
-  [sampleinfo(1:end-1,2)+1 sampleinfo(2:end,1)-1];
-  sampleinfo(end,2)+1 data.hdr.nSamples];
+% add the sample indices as extra channel
+for i=1:numel(data.trial)
+  smp{i} = data.sampleinfo(i,1):data.sampleinfo(i,2);
+end
+smp = cat(2,smp{:});
 
-% samples to be subtracted
-si2subtract = zeros(size(sampleinfo));
-esmp2subtract = zeros(size(eventsample));
-for ichunk = 1:size(chunksout,1)
-  si2subtract(sampleinfo > chunksout(ichunk,2)) = si2subtract(sampleinfo > chunksout(ichunk,2)) + diff(chunksout(ichunk,:))+1;
-  esmp2subtract(eventsample > chunksout(ichunk,2)) = esmp2subtract(eventsample > chunksout(ichunk,2)) + diff(chunksout(ichunk,:))+1;
+% compute new sampleinfo
+begsample = [0 cumsum(cellfun(@length, data.trial(1:end-1)))]' + 1;
+endsample = cumsum(cellfun(@length, data.trial))';
+% pretend that the trials are pseudo-continuous
+data.sampleinfo = [begsample endsample];
+
+% retrieve event samples
+event = renamefields(event,'sample','ursample');
+for iev = 1:numel(event)
+  event(iev).sample = find(smp == event(iev).ursample);
+  % events outside of trials aren't found
+  if isempty(event(iev).sample)
+    event(iev).sample = NaN;
+  end
+end
+% retrieve artifact samples
+if isfield(cfg,'artfctdef')
+  artnames = fieldnames(cfg.artfctdef);
+  for iartfct = 1:numel(artnames)
+    art = cfg.artfctdef.(artnames{iartfct}).artifact;
+    if isempty(art)
+      continue
+    end
+    cfg.artfctdef.(artnames{iartfct}).urartifact = cfg.artfctdef.(artnames{iartfct}).artifact;
+    for iart = 1:numel(art)
+      cfg.artfctdef.(artnames{iartfct}).artifact(iart) = find(smp == art(iart));
+    end
+  end
 end
 
-sampleinfo = sampleinfo - si2subtract;
-eventsample = eventsample - esmp2subtract;
-
-[event.ursample] = rep2struct([event.sample]);
-[event.sample] = rep2struct(eventsample);
-
-%%
-boundarysample = sampleinfo(:,1);
+%% add events to highlight trial boundaries
+boundarysample = data.sampleinfo(:,1);
 boundary = [];
 for i = 1:numel(boundarysample)
   boundary(i).type = '';
@@ -57,28 +69,7 @@ for i = 1:numel(boundarysample)
   boundary(i).timestamp = boundary(i).sample / data.fsample;
   boundary(i).ursample = NaN;
 end
-event = [event boundary];
-event = sortstruct(event,'sample');
-
-%%%%%%%%%%% now dealing with artfctdef
-
-if isfield(cfg,'artfctdef')
-  artnames = fieldnames(cfg.artfctdef);
-  for iart = 1:numel(artnames)
-    art = cfg.artfctdef.(artnames{iart}).artifact;
-    art2subtract = zeros(size(art));
-    for ichunk = 1:size(chunksout,1)
-      art2subtract(art > chunksout(ichunk,2)) = art2subtract(art > chunksout(ichunk,2)) + diff(chunksout(ichunk,:)) + 1;
-    end
-    art = art - art2subtract;
-    cfg.artfctdef.(artnames{iart}).artifact = art;
-  end
-end
-
-data.hdr.nSamples = sum(cellfun(@numel,data.time));
-data.sampleinfo = sampleinfo;
-
-cfg.event = event;
+cfg.event = [event boundary];
 
 %%%%%%%%%% forward to databrowser
 
@@ -92,123 +83,13 @@ cfgout = ft_databrowser(cfg,data);
 cfgout.event = urevent;
 
 %%%%%%%%%% switch artifacts back to original time frame
-
 if isfield(cfgout,'artfctdef')
   artnames = fieldnames(cfgout.artfctdef);
   for iart = 1:numel(artnames)
     art = cfgout.artfctdef.(artnames{iart}).artifact;
-    for ichunk = 1:size(chunksout,1)
-      art(chunksout(ichunk,1) < art) = art(chunksout(ichunk,1) < art) + diff(chunksout(ichunk,:)) + 1;
-    end
-    cfgout.artfctdef.(artnames{iart}).artifact = art;
+    cfgout.artfctdef.(artnames{iart}).artifact = smp(art);
   end
 end
 
 cfg = cfgout;
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%% SUBFUNCTIONS %%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function [varargout] = rep2struct(varargin)
-
-% [s.target] = rep2struct(dat)
-% replicate the value dat into each element of structure s in field target.
-% if dat has same nb or elements as s, each element of dat goes into one
-% element of s. if dat is more dimensional and doesn't have the same number
-% of elements as s, and has same size along dimension 1, then pass each
-% slice into s.target.
-
-varargout = cell(1,nargout);
-if numel(varargin) == 1
-    dat = varargin{1};
-    if numel(dat) == nargout
-        for i = 1:nargout
-            if iscell(dat(i))
-                varargout{i} = dat{i};
-            else
-                varargout{i} = dat(i);
-            end
-        end
-    elseif size(dat,1) == nargout
-        for i = 1:nargout
-            varargout{i} = dat(i,:);
-        end
-    else
-        for i = 1:nargout
-            varargout{i} = dat;
-        end
-    end
-elseif numel(varargin) == nargout
-    for i = 1:nargout
-        varargout{i} = varargin{i};
-    end
-else
-    error('Wrong number of arguments');
-end
-return
-
-
-function S = sortstruct(S, by, direction,sorter)
-
-% S = sortstruct(S, by, direction,sorter)
-% sort vector structure by values in field(s) by (may be cell array of
-% str).
-% direction is 'ascend' or 'descend'
-% sorter is a list of unique values that will determine output order.
-%
-% if by is empty, no sorting occurs.
-
-if isscalar(S) || isempty(S)
-    return
-elseif ~isvector(S)
-    error('input structure must be a vector')
-end
-s = size(S);
-S = S(:);
-if nargin < 2
-    by = fieldnames(S);
-    warning('sortstruct:sortallfields','no sorting fields provided. sorting by all of them...')
-end
-defifnotexist('direction','ascend');
-if ischar(by)
-    defifnotexist('sorter',[]);
-    by = {by};
-    sorter = {sorter};
-else
-    defifnotexist('sorter',repmat({[]},1,numel(by)));
-end
-if all(cellfun(@isempty,by))
-    return
-end
-if ischar(S(1).(by{1}))
-    L = {S.(by{1})};% list all values
-    U = unique(L);% unique values (sorted)
-    U = [sorter{1} setxor(sorter{1},U)];
-    nS = [];
-    for i_u = 1:numel(U)% for each unique value
-        tmpS = S(strcmp(L,U{i_u}));% extract them
-        if numel(by) > 1
-            tmpS = sortstruct(tmpS,by(2:end),direction,sorter(2:end));% sort under
-        end
-        nS = [nS; tmpS];
-    end
-elseif isnumeric(S(1).(by{1}))
-    L = [S.(by{1})];% list all values
-    U = unique(L);% unique values (sorted)
-    U = [sorter{1} setxor(sorter{1},U)];
-    nS = [];
-    for i_u = 1:numel(U)% for each unique value
-        tmpS = S(L == U(i_u));% extract them
-        if numel(by) > 1
-            tmpS = sortstruct(tmpS,by(2:end),direction,sorter(2:end));% sort under
-        end
-        nS = [nS; tmpS];
-    end
-else
-    error todo
-end
-S = nS;
-S = reshape(S,s);
 
