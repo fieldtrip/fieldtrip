efc2b0ceacc4883f182e6ba03d25f11f682a05a7
Jan-Mathijs Schoffelen <1517611+schoffelen@users.noreply.github.com>
2022-02-25 08:41:39 +0100

    implement Neuromag SSP similar to montage/balancing for CTF synthetic gradients , see issue #1952 and messed up PR #1984 (#1988)
    * first push - problem with unknown units
    
    * test script
    
    * updated
    
    * corrected author name
    
    * ENH - converted into a function, attempt to load data from ftp, if outside Donders
    
    * ENH - use a dataset that contains SSPs
    
    * ENH - ualsbombe added functionality for individual components to be removed
    
    * ENH - some updates by ualsbombe
    
    * ENH - fixed the name of the ssp's for the specific test data
    
    * DOC - small doc updates
    
    Co-authored-by: ualsbombe <ualsbombe@gmail.com>

--- ft_denoise_pca.m
+++ ft_denoise_pca.m
@@ -32,14 +32,14 @@ function data = ft_denoise_pca(cfg, varargin)
 % if 0 < cfg.truncate < 1, the singular value spectrum will be thresholded at the
 % fraction cfg.truncate of the largest singular value.
 %
-% See also FT_PREPROCESSING, FT_DENOISE_SYNTHETIC
+% See also FT_PREPROCESSING, FT_DENOISE_SYNTHETIC, FT_DENOISE_SSP
 
 % Undocumented cfg-option: cfg.pca the output structure of an earlier call
 % to the function. Can be used regress out the reference channels from
 % another data set.
 
 % Copyright (c) 2008-2009, Jan-Mathijs Schoffelen, CCNi Glasgow
-% Copyright (c) 2010-2011, Jan-Mathijs Schoffelen, DCCN Nijmegen
+% Copyright (c) 2010-2022, Jan-Mathijs Schoffelen, DCCN Nijmegen
 %
 % This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
 % for the documentation and details.
