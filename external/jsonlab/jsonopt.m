9b765d3bc89902864af281dde970cb1fc62157ff
Robert Oostenveld <r.oostenveld@gmail.com>
2022-03-11 15:29:36 +0100

    updated external/jsonlab to version 2.9.8 (aa3913e) from https://github.com/fangq/jsonlab

--- external/jsonlab/jsonopt.m
+++ external/jsonlab/jsonopt.m
@@ -7,8 +7,6 @@ function val=jsonopt(key,default,varargin)
 %
 % authors:Qianqian Fang (q.fang <at> neu.edu)
 %
-% $Id: loadjson.m 371 2012-06-20 12:43:06Z fangq $
-%
 % input:
 %      key: a string with which one look up a value from a struct
 %      default: if the key does not exist, return default
@@ -18,18 +16,21 @@ function val=jsonopt(key,default,varargin)
 %      val: if key exists, val=optstruct.key; otherwise val=default
 %
 % license:
-%     BSD License, see LICENSE_BSD.txt files for details
+%     BSD or GPL version 3, see LICENSE_{BSD,GPLv3}.txt files for details
 %
-% -- this function is part of jsonlab toolbox (http://iso2mesh.sf.net/cgi-bin/index.cgi?jsonlab)
+% -- this function is part of JSONLab toolbox (http://iso2mesh.sf.net/cgi-bin/index.cgi?jsonlab)
 % 
 
 val=default;
-if(nargin<=2) return; end
+if(nargin<=2)
+    return;
+end
+key0=lower(key);
 opt=varargin{1};
 if(isstruct(opt))
-    if(isfield(opt,key))
-       val=getfield(opt,key);
-    elseif(isfield(opt,lower(key)))
-       val=getfield(opt,lower(key));
+    if(isfield(opt,key0))
+       val=opt.(key0);
+    elseif(isfield(opt,key))
+       val=opt.(key);
     end
 end
