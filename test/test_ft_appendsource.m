15e8c333ccb6fad03880688e3dd4dc1bd21e6e83
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2022-03-21 14:14:44 +0100

    ENH - add support for concatenation across freq and time and pos

--- test/test_ft_appendsource.m
+++ test/test_ft_appendsource.m
@@ -20,7 +20,7 @@ cfg.parameter = 'pow';
 cfg.appenddim = 'rpt';
 sourceout = ft_appendsource(cfg, source, source2);
 
-return; %following cases do not work yet (cf. issue #1833)
+%return; %following cases do not work yet (cf. issue #1833)
 %%
 % test case of different positions, same time frames
 source2 = source;
@@ -36,5 +36,28 @@ source2.pow = randn(gridsize,nsamples);
 source2.time = nsamples+1:nsamples*2;
 cfg = [];
 cfg.parameter = 'pow';
-cfg.appenddim = 'rpt';
 sourceout = ft_appendsource(cfg, source, source2);
+
+% this should reorder the inputs
+sourceout2 = ft_appendsource(cfg, source2, source);
+assert(isequal(sourceout.time, sourceout2.time));
+
+% this should throw an error
+source2.time = source.time + 2;
+try
+  sourceout    = ft_appendsource(cfg, source, source2);
+  ok = true;
+catch
+  ok = false;
+end
+assert(~ok); clear ok
+
+% this should also throw an error
+source2.time = 2.*source.time -nsamples - 0.5;
+try
+  sourceout    = ft_appendsource(cfg, source, source2);
+  ok = true;
+catch
+  ok = false;
+end
+assert(~ok); clear ok
