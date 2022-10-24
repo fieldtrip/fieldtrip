8f367acbc7356e66dcbfcab00eea87ce0ad9bbbb
Robert Seymour <robbyseymour@gmail.com>
2022-10-24 16:51:18 +0100

    Upload HFC function and test function
    - FT_DENOISE_HFC implements harmonic field correction, which models interference as a harmonic magnetic field.
    
    - spm_opm_vslm generates the harmonic basis set
    
    - test_ft_denoise_hfc is the test function for HFC
    
    The test function uses some custom OPM data from our lab, which is stored as a .mat file. After some discussion with @schoffelen I have used variable inputs so it works on my PC. For DCCN you will need to provide an input and change the path (data can be downloaded from https://doi.org/10.17605/OSF.IO/CJNXH).

---
+++ test/test_ft_denoise_hfc.m
@@ -0,0 +1,45 @@
+function test_ft_denoise_hfc(testdata)
+
+% WALLTIME XX:XX:XX
+% MEM 8gb
+% DEPENDENCY test_ft_denoise_hfc
+
+switch nargin
+    case 0
+        locate_function = which('test_ft_denoise_hfc');
+        % load data
+        load(fullfile(locate_function,'..','data_UCL_OPM.mat'));
+    case 1
+        % If user inputs a filename, use path specific for DCCN
+        % Find this data: https://doi.org/10.17605/OSF.IO/CJNXH
+        load(dccnpath('...data_UCL_OPM.mat'));
+end
+
+
+% Test of Harmonic Field Correction (HFC) where L = 1
+cfg               = [];
+cfg.L             = 1;
+cfg.residualcheck = 'yes';
+[data_out]        = ft_denoise_hfc(cfg,data);
+
+% Check L = 2
+cfg               = [];
+cfg.L             = 2;
+cfg.residualcheck = 'yes';
+[data_out]        = ft_denoise_hfc(cfg,data);
+
+% Check L = 3
+cfg               = [];
+cfg.L             = 3;
+cfg.residualcheck = 'yes';
+[data_out]        = ft_denoise_hfc(cfg,data);
+
+% Check L = 1 without residual check
+cfg               = [];
+cfg.L             = 1;
+cfg.residualcheck = 'no';
+[data_out]        = ft_denoise_hfc(cfg,data);
+
+
+
+
