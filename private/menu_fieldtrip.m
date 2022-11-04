6ff665acbe049a4fd7a780ccbb6291ccb2328923
Robert Oostenveld <r.oostenveld@gmail.com>
2022-11-04 14:30:05 +0100

    ENH - made menu_fieldtrip consistent, support recursion, added example

--- private/menu_fieldtrip.m
+++ private/menu_fieldtrip.m
@@ -4,6 +4,14 @@ function menu_fieldtrip(handle, cfg, allowsubplot)
 %
 % See also MENU_ABOUT, MENU_PIPELINE
 
+if nargin<2
+  cfg = [];
+end
+
+if nargin<3
+  allowsubplot = true;
+end
+
 if ~allowsubplot && numel(findobj(gcf, 'type', 'axes')) > 1
   % do not add a menu if there are multiple axes, i.e. subplots
   % these may contain different data structures
@@ -13,9 +21,11 @@ end
 if ft_platform_supports('uimenu')
   % delete any possibly existing previous menu, this is safe because delete([]) does nothing
   delete(findobj(handle, 'type', 'uimenu', 'label', 'FieldTrip'));
-  
+
   % add the menu with callbacks
   ftmenu = uimenu(handle, 'Label', 'FieldTrip');
-  uimenu(ftmenu, 'Label', 'Show pipeline', 'Callback', {@menu_pipeline, cfg});
+  if ~isempty(cfg)
+    uimenu(ftmenu, 'Label', 'Show pipeline', 'Callback', {@menu_pipeline, cfg});
+  end
   uimenu(ftmenu, 'Label', 'About', 'Callback', @menu_about);
 end
