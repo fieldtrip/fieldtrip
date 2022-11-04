f17aa8f91d8e7bb1551280e724f876bbcbce0c99
Richard Koehler <richard.koehler@outlook.de>
2022-11-04 09:50:50 +0100

    Rename file for testing

--- test/test_pull2111.m
+++ test/test_pull2111.m
@@ -4,7 +4,7 @@ function test_pull2111
 % MEM 1gb
 % DEPENDENCY ft_read_data, ft_read_header
 
-filename = dccnpath('/home/common/matlab/fieldtrip/data/test/pull2110/F220916-0001.mat');
+filename = dccnpath('/home/common/matlab/fieldtrip/data/test/pull2110/data_test_pull2111.mat');
 
 hdr = ft_read_header(filename, 'headerformat', 'neuroomega_mat', 'chantype', 'ecog');
 data = ft_read_data(filename, 'header', hdr, 'dataformat', 'neuroomega_mat', 'headerformat', 'neuroomega_mat');
