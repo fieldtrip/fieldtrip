93e6bf8f91e15ef1c52f0a906e8162ad0ee84193
StolkArjen <a.stolk8@gmail.com>
2022-11-18 14:31:54 -0500

    fix: cloud radius scaling

--- plotting/ft_plot_cloud.m
+++ plotting/ft_plot_cloud.m
@@ -317,8 +317,7 @@ end
 cmid   = size(cmapsc,1)/2;                          % colorbar middle
 colscf = 2*( (val-clim(1)) / (clim(2)-clim(1)) )-1; % color between -1 and 1, bottom vs. top colorbar
 colscf(colscf>1)=1; colscf(colscf<-1)=-1;           % clip values outside the [-1 1] range
-radscf = val-(min(abs(val)) * sign(max(val)));      % radius between 0 and 1, small vs. large pos/neg effect
-radscf = abs( radscf / max(abs(radscf)) );
+radscf = abs(colscf);                               % radius between 0 and 1, small vs. large pos/neg effect
 
 if strcmp(scalerad, 'yes')
   rmax = rmin+(radius-rmin)*radscf; % maximum radius of the clouds
