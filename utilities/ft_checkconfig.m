1a1f6fc0e5f8834d74d9abfe456a04af118af96e
Robert Oostenveld <r.oostenveld@gmail.com>
2022-12-06 15:35:27 +0100

    updated help

--- utilities/ft_checkconfig.m
+++ utilities/ft_checkconfig.m
@@ -1,31 +1,25 @@
 function [cfg] = ft_checkconfig(cfg, varargin)
 
 % FT_CHECKCONFIG checks the input cfg of the main FieldTrip functions
-% in three steps.
 %
-% 1: It checks whether the cfg contains all the required options, it gives
+% It checks whether the cfg contains all the required options, it gives
 % a warning when renamed or deprecated options are used, and it makes sure
 % no forbidden options are used. If necessary and possible, this function
 % will adjust the cfg to the input requirements. If the input cfg does NOT
 % correspond to the requirements, this function gives an elaborate warning
 % message.
 %
-% 2: It controls the relevant cfg options that are being passed on to other
+% It controls the relevant cfg options that are being passed on to other
 % functions, by putting them into substructures or converting them into the
 % required format.
-%
-% 3: It controls the output cfg (data.cfg) such that it only contains
-% relevant and used fields. The size of fields in the output cfg is also
-% controlled: fields exceeding a certain maximum size are emptied.
-% This part of the functionality is still under construction!
 %
 % Use as
 %   [cfg] = ft_checkconfig(cfg, ...)
 %
-% The behavior of checkconfig can be controlled by the following cfg options,
-% which can be set as global FieldTrip defaults (see FT_DEFAULTS)
-%   cfg.checkconfig = 'pedantic', 'loose' or 'silent' (control the feedback behavior of checkconfig)
-%   cfg.checksize   = number in bytes, can be inf (set max size allowed for output cfg fields)
+% The behavior of checkconfig can be controlled by the following cfg options, which
+% can be set as global FieldTrip defaults (see FT_DEFAULTS)
+%   cfg.checkconfig = 'pedantic', 'loose' or 'silent', this controls the how strict this function is
+%   cfg.checksize   = number in bytes (can be inf), this controls the maximum size of output cfg fields
 %
 % Optional input arguments should be specified as key-value pairs and can include
 %   renamed         = {'old',  'new'}        % list the old and new option
