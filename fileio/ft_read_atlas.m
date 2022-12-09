558e476880adc890f7e54d3fc16b3e8f1e9ce62f
ualsbombe <ualsbombe@gmail.com>
2022-12-09 15:23:13 +0100

    first go at reading the lookup table from SimNIBS v4

--- fileio/ft_read_atlas.m
+++ fileio/ft_read_atlas.m
@@ -17,7 +17,7 @@ function atlas = ft_read_atlas(filename, varargin)
 %   'map'         = string, 'maxprob' (default), or 'prob', for FSL-based atlases, providing 
 %                   either a probabilistic segmentation or a maximum a posterior probability map
 %
-% For individual surface-based atlases from FreeSurfer you should specify two
+% For individual surface-based atlases from FreeSurfer and SimNIBS you should specify two
 % filenames as a cell-array: the first points to the file that contains information
 % with respect to the parcels' labels, the second points to the file that defines the
 % mesh on which the parcellation is defined.
@@ -100,6 +100,9 @@ elseif ft_filetype(filename, 'caret_label')
 elseif contains(filename, 'MPM')
   % assume to be from the spm_anatomy toolbox
   format = 'spm_anatomy';
+elseif contains(filename, 'final_tissues_LUT')
+  % assume to be from SimNIBS version 4
+  format = 'SimNIBS_v4';
 elseif strcmp(x, '.xml') && (isfolder(strtok(fullfile(p,f), '_')) || isfolder(strtok(fullfile(p,f), '-')))
   % fsl-format atlas, this is assumed to consist of an .xml file that
   % specifies the labels, as well as the filenames of the files with the actual data stored
@@ -799,6 +802,35 @@ switch fileformat
     elseif ~isfield(atlas, 'coordsys')
       atlas.coordsys = 'unknown';
     end
+
+  case 'SimNIBS_v4'
+    tmp = ft_read_mri(filenamemesh);
+    lookuptable = readtable(filename);
+    n_tissues = size(lookuptable, 1);
+    tissue_ids = zeros(1, n_tissues);
+    tissuelabels = cell(1, n_tissues);
+    for tissue_index = 1:n_tissues
+      tissue_ids(tissue_index) = table2array(lookuptable(tissue_index, 1)); % first is identifier
+      tissuelabel = table2array(lookuptable(tissue_index, 2));
+      tissuelabels{tissue_index} = tissuelabel{1};
+    end
+    atlas = [];
+    atlas.unit = tmp.unit;
+    atlas.transform = tmp.transform;
+    atlas.dim = tmp.dim;
+    atlas.tissue = tmp.anatomy;
+    max_index = max(tissue_ids);
+    atlas.tissuelabels = repmat({''}, 1, max_index);
+
+    tissue_counter = 1;
+    for index = 1:max_index
+      if any(tissue_ids == index)
+        atlas.tissuelabels{index} = tissuelabels{tissue_counter};
+        tissue_counter = tissue_counter + 1;
+      end
+    end
+
+
     
   case 'spm_anatomy'
     ft_hastoolbox('spm8up', 1);
