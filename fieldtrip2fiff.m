003fb2a3e3c47c650fd4a92a5bdc9e0aeafbd2b9
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2023-03-15 09:05:01 +0100

    ENH - improve code to reflect sensor locations + orientations

--- fieldtrip2fiff.m
+++ fieldtrip2fiff.m
@@ -1,8 +1,7 @@
 function fieldtrip2fiff(filename, data)
 
 % FIELDTRIP2FIFF saves a FieldTrip raw data structure as a fiff-file, allowing it
-% to be further analyzed by the Neuromag/Elekta/Megin software, or in the MNE suite
-% software.
+% to be further analyzed by the Neuromag/Elekta/Megin software, or in MNE-python.
 %
 % Use as
 %   fieldtrip2fiff(filename, data)
@@ -12,11 +11,11 @@ function fieldtrip2fiff(filename, data)
 %
 % If the data comes from preprocessing and has only one trial, then it writes the
 % data into raw continuous format. If present in the data, the original header
-% from neuromag is reused (also removing the non-used channels). Otherwise, the
-% function tries to create a correct header, which might or might not contain the
-% correct scaling and channel location. If the data contains events in the cfg
-% structure, it writes the events in the MNE format (three columns) into a file
-% based on "filename", ending with "-eve.fif"
+% is reused (also removing the non-used channels). Otherwise, the function 
+% attempts to create the header, which might or might not be correct (e.g. with
+% respect to the scaling and the sensor locations. If the data contains events in 
+% the cfg structure, it writes the events in the MNE format (three columns) into 
+% a file based on "filename", ending with "-eve.fif"
 %
 % See also FT_DATATYPE_RAW, FT_DATATYPE_TIMELOCK
 
@@ -48,73 +47,57 @@ ft_defaults
 if ~strcmp(ext, '.fif')
   ft_error('if the filename is specified with extension, this should read .fif');
 end
-fifffile = fullfile(pathstr ,[name '.fif']);
+fifffile  = fullfile(pathstr ,[name '.fif']);
 eventfile = fullfile(pathstr ,[name '-eve.fif']);
 
 % ensure the mne-toolbox to be on the path
 ft_hastoolbox('mne', 1);
 
 % check if the input data is valid for this function
-data   = ft_checkdata(data, 'datatype', {'raw', 'timelock'}, 'feedback', 'yes');
+data   = ft_checkdata(data, 'datatype', {'raw', 'timelock'}, 'hassampleinfo', 'yes', 'feedback', 'yes');
 istlck = ft_datatype(data, 'timelock');
 isepch = ft_datatype(data, 'raw');
-israw = false;
+israw  = false;
 if isepch && numel(data.trial) == 1
   isepch = false;
-  israw = true;
+  israw  = true;
+end
+if istlck
+  fsample = 1./mean(diff(data.time));
+else
+  fsample = 1./mean(diff(data.time{1}));
 end
 
-% Create a fiff-header, or take it from the original header if possible
-if ft_senstype(data, 'neuromag') && isfield(data, 'hdr')
-  fprintf('Using the original FIFF header, but channel locations are read \nfrom .grad and .elec in the data, if they exist\n')
+% Create a fiff-header, or take information from the original header if possible
+if isfield(data, 'hdr') && isfield(data.hdr, 'orig') && isfield(data.hdr.orig, 'meas_id')
+  ft_notice('The data contains header information that seems to be derived from a FIFF file,\nre-using header information, channel locations may be read \nfrom .grad and .elec in the data');
   info = data.hdr.orig;
-  
 else
   info.meas_id.version = NaN;
   info.meas_id.machid  = [NaN;NaN];
   info.meas_id.secs    = NaN;
   info.meas_id.usecs   = NaN;
   info.meas_date       = [NaN;NaN];
-  info.acq_pars = []; % needed by raw
-  info.acq_stim = []; % needed by raw
+  info.acq_pars        = []; % needed by raw
+  info.acq_stim        = []; % needed by raw
+  info.highpass        = NaN;
+  info.lowpass         = NaN;
   
-  info.highpass = NaN;
-  info.lowpass  = NaN;
-  % no strictly necessary, but the inverse functions in MNE works better if
-  % this matrix is present
-  info.dev_head_t.from = 1;
-  info.dev_head_t.to = 4;
+  % these are not strictly necessary, but the inverse functions in MNE works better if this matrix is present
+  info.dev_head_t.from  = 1;
+  info.dev_head_t.to    = 4;
   info.dev_head_t.trans = eye(4);
   
   info.ctf_head_t = [];
-  info.dig      = [];
-  info.projs = struct('kind', {}, 'active', {}, 'desc', {}, 'data', {});
-  info.comps = struct('ctfkind', {}, 'kind', {}, 'save_calibrated', {}, ...
-    'rowcals', {}, 'colcals', {}, 'data', {});
-  info.bads     = [];
+  info.dig        = [];
+  info.projs      = struct('kind', {}, 'data', {}, 'active', {}, 'desc', {});
+  info.comps      = struct('kind', {}, 'data', {}, 'ctfkind', {}, 'save_calibrated', {}, 'rowcals', {}, 'colcals', {});
+  info.bads       = [];
   
-  if isepch
-    info.sfreq     = 1./mean(diff(data.time{1}));
-    info.isaverage = 0;
-    info.isepoched = 1;
-    info.iscontinuous = 0;
-    
-  elseif istlck
-    info.sfreq     = 1./mean(diff(data.time));
-    info.isaverage = 1;
-    info.isepoched = 0;
-    info.iscontinuous = 0;
-    
-  end
-  
-end
-
-if israw
-  info.sfreq = data.fsample;
-elseif isepch
-  info.sfreq = 1 ./ mean(diff(data.time{1}));
-elseif istlck
-  info.sfreq = 1 ./ mean(diff(data.time));
+  info.isaverage    = double(istlck);
+  info.isepoched    = double(isepch);
+  info.iscontinuous = double(israw);
+  info.sfreq        = fsample;
 end
 
 info.ch_names = data.label(:)';
@@ -140,23 +123,21 @@ if israw
   
 elseif isepch
   
-  ft_error('writing epochs to MNE is not implemented yet')
+  ft_warning('writing epochs to MNE is not implemented yet');
   
-  for j = 1:length(data.trial)
-    evoked(j).aspect_kind = 100;
-    evoked(j).is_smsh     = 0; % FIXME: How could we tell?
-    evoked(j).nave        = 1; % FIXME: Use the real value
-    evoked(j).first       = round(data.time{j}(1)*info.sfreq);
-    evoked(j).last        = round(data.time{j}(end)*info.sfreq);
-    evoked(j).times       = data.time{j};
-    evoked(j).comment     = sprintf('FieldTrip data, category/trial %d', j);
-    evoked(j).epochs      = data.trial{j};
+  ntrl    = numel(data.trial);
+  aspect_kind = num2cell(ones(ntrl,1)*100);
+  is_smsh = num2cell(zeros(ntrl,1)); % FIXME: How could we tell? + Don't know what this is
+  nave    = num2cell(ones(ntrl,1));  % FIXME: Use the real value
+  first   = num2cell(data.sampleinfo(:, 1));
+  last    = num2cell(data.sampleinfo(:, 2));
+  comment = cell(ntrl,1);
+  for j = 1:ntrl
+    comment{j,1} = sprintf('FieldTrip data, category/trial %d', j);
   end
-  
-  % fiffdata.info   = info;
-  % fiffdata.evoked = evoked;
-  % fiff_write_XXX(fifffile, fiffdata);
-  
+  evoked = struct('aspect_kind', aspect_kind, 'is_smsh', is_smsh, 'nave', nave, ...
+    'first', first, 'last', last, 'comment', comment, 'times', data.time(:), 'epochs', data.trial(:));
+
 elseif istlck
   evoked.aspect_kind = 100;
   evoked.is_smsh     = 0;
@@ -178,104 +159,117 @@ end
 function [chs] = sens2fiff(data)
 
 % use orig information if available
-if isfield(data, 'hdr') && isfield(data.hdr, 'orig') && ...
-    isfield(data.hdr.orig, 'chs')
+if isfield(data, 'hdr') && isfield(data.hdr, 'orig') && isfield(data.hdr.orig, 'chs')
   
-  [dummy, i_label, i_chs] = intersect(data.label, {data.hdr.orig.chs.ch_name});
-  chs(i_label) = data.hdr.orig.chs(i_chs);
-  return
-  
-end
-
-% otherwise reconstruct it
-fprintf('Reconstructing channel locations, it might be inaccurate\n')
-FIFF = fiff_define_constants; % some constants are not defined in the MATLAB function
-
-if isfield(data, 'grad')
-  hasgrad = true;
+  ft_warning('Using the original channel information from the header, this may be wrong if channels have been pruned, reordered, rescaled');
+  [i_label, i_chs] = match_str(data.label, {data.hdr.orig.chs.ch_name}');
+  chs(i_label)     = data.hdr.orig.chs(i_chs);
 else
-  hasgrad = false;
-end
 
-if isfield(data, 'elec')
-  haselec = true;
-  elec = ft_convert_units(data.elec, 'cm'); % that MNE uses cm
-else
-  haselec = false;
-end
-
-cnt_grad = 0;
-cnt_elec = 0;
-cnt_else = 0;
-
-for k = 1:numel(data.label)
-  % create a struct for each channel
-  chs(1,k).scanno       = k;
-  chs(1,k).ch_name      = data.label{k};
+  % otherwise reconstruct a usable channel information array
+  ft_warning('Reconstructing channel information, based on the available data, it might be inaccurate\n');
+  FIFF = fiff_define_constants; % some constants are not defined in the MATLAB function
   
-  chs(1,k).range        = 1;
-  chs(1,k).cal          = 1;
+  hasgrad = isfield(data, 'grad');
+  haselec = isfield(data, 'elec');
+  nchan   = numel(data.label);
   
-  i_grad = false;
-  i_elec = false;
+  stype = zeros(nchan, 1).*-1;
+  indx  = nan(nchan,1);
+  
+  if haselec
+    [i_labeeg, i_elec] = match_str(data.label, data.elec.label);
+    stype(i_labeeg) = 0; %meg/eeg/other = 1/0/-1
+    indx(i_labeeg)  = i_elec; %indexes into the grad/elec
+   
+    data.elec = ft_convert_units(data.elec, 'cm'); % MNE uses cm for EEG (FIXME is this true)
+  end
+  
   if hasgrad
-    i_grad = strcmp(data.grad.label, data.label{k});
-  elseif haselec
-    i_elec = strcmp(elec.label, data.label{k});
-  end
+    [i_labmeg, i_grad] = match_str(data.label, data.grad.label);
+    stype(i_labmeg) = 1;
+    indx(i_labmeg)  = i_grad;
   
-  if any(i_grad)
-    chs(1,k).kind         = FIFF.FIFFV_MEG_CH;
-    cnt_grad = cnt_grad + 1;
-    chs(1,k).logno        = cnt_grad;
-    switch data.grad.chantype{i_grad}
-      case 'megmag'
-        chs(1,k).coil_type    = 3024;
-        chs(1,k).unit         = FIFF.FIFF_UNIT_T;
-      case 'megplanar'
-        chs(1,k).coil_type    = 3012;
-        chs(1,k).unit         = FIFF.FIFF_UNIT_T_M;
-      case 'meggrad'
-        chs(1,k).coil_type    = 3022;
-        chs(1,k).unit         = FIFF.FIFF_UNIT_T;
+    data.grad = undobalancing(data.grad); % if this fails, then it's difficult to write out meaningful channel info to begin with
+    coiltype  = grad2coiltype(data.grad);
+    coilunit  = grad2coilunit(data.grad, FIFF);
+  end
+
+  scanno  = num2cell((1:nchan)');
+  ch_name = data.label(:);
+  range   = num2cell(ones(nchan,1));
+  cal     = num2cell(ones(nchan,1));
+
+  
+  cnt_grad = 0;
+  cnt_elec = 0;
+  cnt_else = 0;
+  chs = struct('scanno', scanno, 'ch_name', ch_name, 'range', range, 'cal', cal)';
+  for k = 1:nchan
+  
+    switch stype(k)
+      case 1
+        % MEG
+        cnt_grad = cnt_grad + 1;
+        
+        chs(1,k).kind      = FIFF.FIFFV_MEG_CH;
+        chs(1,k).logno     = cnt_grad;
+        chs(1,k).coil_type = coiltype(indx(k));
+        chs(1,k).unit      = coilunit(indx(k));
+        
+        pos = data.grad.chanpos(indx(k),:);
+        ori = data.grad.chanori(indx(k),:);
+
+        this_z = ori;          
+        this_z = this_z / norm(this_z);
+        this_x = cross([0 0 1], this_z);
+        if all(this_x==0)
+          this_x = [1 0 0];
+        else
+          this_x = this_x / norm(this_x);
+        end
+        this_y = cross(this_z, this_x);
+        R      = [this_x(:) this_y(:) this_z(:)];
+
+        chs(1,k).coil_trans   = [R pos(:); 0 0 0 1];
+        chs(1,k).unit_mul     = 0;
+        chs(1,k).coord_frame  = FIFF.FIFFV_COORD_HEAD;
+        chs(1,k).eeg_loc      = [];
+        chs(1,k).loc          = [pos(:); R(:)];
+      case 0
+        % EEG
+        cnt_elec = cnt_elec + 1;
+       
+        chs(1,k).kind         = FIFF.FIFFV_EEG_CH;
+        chs(1,k).logno        = cnt_elec;
+        chs(1,k).coil_type    = NaN;
+        chs(1,k).coil_trans   = [];
+        chs(1,k).unit         = 107; % volts FIFF.FIFF_UNIT_V
+        chs(1,k).unit_mul     = -6; % micro FIFF.FIFF_UNITM_MU
+        chs(1,k).coord_frame  = FIFF.FIFFV_COORD_DEVICE;
+        chs(1,k).eeg_loc      = [elec.chanpos(i_elec,:)' zeros(3,1)] / 100;
+        chs(1,k).loc          = [chs(1,k).eeg_loc(:); 0; 1; 0; 0; 0; 1];
+
+      case -1
+        % OTHER
+        cnt_else = cnt_else + 1;
+        
+        chs(1,k).kind         = NaN;
+        chs(1,k).logno        = cnt_else;
+        chs(1,k).coil_type    = NaN;
+        chs(1,k).coil_trans   = [];
+        chs(1,k).unit         = NaN;
+        chs(1,k).unit_mul     = 0;
+        chs(1,k).coord_frame  = NaN;
+        chs(1,k).eeg_loc      = [];
+        chs(1,k).loc          = zeros(12,1);
+
       otherwise
-        fprintf('Unknown channel type %s, assigned to meggrad', data.grad.chantype{i_grad})
-        chs(1,k).coil_type    = 3022;
-        chs(1,k).unit         = FIFF.FIFF_UNIT_T;
     end
-    chs(1,k).coil_trans   = eye(4);
-    chs(1,k).unit_mul     = 0;
-    chs(1,k).coord_frame  = FIFF.FIFFV_COORD_HEAD;
-    chs(1,k).eeg_loc      = [];
-    chs(1,k).loc          = [data.grad.chanpos(i_grad,:)'; reshape(eye(3),[9 1])];
-    
-  elseif any(i_elec)
-    chs(1,k).kind         = FIFF.FIFFV_EEG_CH;
-    cnt_elec = cnt_elec + 1;
-    chs(1,k).logno        = cnt_elec;
-    chs(1,k).coil_type    = NaN;
-    chs(1,k).coil_trans   = [];
-    chs(1,k).unit         = 107; % volts FIFF.FIFF_UNIT_V
-    chs(1,k).unit_mul     = -6; % micro FIFF.FIFF_UNITM_MU
-    chs(1,k).coord_frame  = FIFF.FIFFV_COORD_DEVICE;
-    chs(1,k).eeg_loc      = [elec.chanpos(i_elec,:)' zeros(3,1)] / 100;
-    chs(1,k).loc          = [chs(1,k).eeg_loc(:); 0; 1; 0; 0; 0; 1];
-    
-  else
-    chs(1,k).kind         = NaN;
-    cnt_else = cnt_else + 1;
-    chs(1,k).logno        = cnt_else;
-    chs(1,k).coil_type    = NaN;
-    chs(1,k).coil_trans   = [];
-    chs(1,k).unit         = NaN;
-    chs(1,k).unit_mul     = 0;
-    chs(1,k).coord_frame  = NaN;
-    chs(1,k).eeg_loc      = [];
-    chs(1,k).loc          = zeros(12,1);
-    
+
   end
-  
 end
+
 
 function eve = convertevent(event)
 % tentative code, with lots of assumption
@@ -335,3 +329,42 @@ else
     fprintf('type: %s, value %s -> % 3d\n', ev_type{i_type}, ev_value{i_value}, newev(i))
   end
 end
+
+function coiltype = grad2coiltype(grad)
+
+stype = ft_senstype(grad);
+def   = mne_load_coil_def('coil_def.dat');
+def   = def([def.accuracy]==0);
+
+coiltype = [];
+switch stype
+  case 'neuromag122'
+  case 'neuromag306'
+  case {'ctf151' 'ctf275'}
+  case 'bti148'
+  case 'bti248'
+  otherwise
+    
+end
+
+% treat as point magnetometer system
+sel = strcmp({def.description}', 'Point magnetometer');
+if isempty(coiltype)
+  coiltype = ones(numel(grad.label),1).*def(sel).id;
+end
+
+function coilunit = grad2coilunit(grad, FIFF)
+
+coilunit = zeros(numel(grad.label),1)-1;
+for k = 1:numel(grad.chanunit)
+  switch grad.chanunit{k}
+    case 'T'
+      coilunit(k) = FIFF.FIFF_UNIT_T;
+    case 'T/m'
+      coilunit(k) = FIFF.FIFF_UNIT_T_M;
+    otherwise
+      % figure out what to do if T/cm or so -> I think that this goes into
+      % the unit_mul field
+  end
+end
+
