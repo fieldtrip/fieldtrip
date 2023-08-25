298d0f54761075bf5e516e8cb25312aa141bcd44
Robert Oostenveld <r.oostenveld@gmail.com>
2023-08-25 14:37:48 +0200

    FIX - more assorted issues with dccnpath related to the update in #2298

--- test/test_bug2879.m
+++ test/test_bug2879.m
@@ -5,7 +5,7 @@ function test_bug2879
 % DEPENDENCY ft_sourcestatistics
 % DATA private
 
-filename = dccnpath('/home/common/matlab/fieldtrip/data/test/bug2879');
+filename = dccnpath('/home/common/matlab/fieldtrip/data/test/bug2879.mat');
 load(filename)
 
 stat = ft_sourcestatistics(cfg,dat1{:},dat2{:});
