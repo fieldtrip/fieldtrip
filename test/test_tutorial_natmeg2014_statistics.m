f02080470943614fbe2ad9a00ad3b1b3d3fb287e
Jan-Mathijs Schoffelen <1517611+schoffelen@users.noreply.github.com>
2023-08-28 09:53:24 +0200

    TYPO - updated spelling (#2307)

--- test/test_tutorial_natmeg2014_statistics.m
+++ test/test_tutorial_natmeg2014_statistics.m
@@ -104,7 +104,7 @@ cfg.correctm  = 'no';
 TFR_stat1     = ft_freqstatistics(cfg, TFR_logpow);
 
 cfg.method    = 'analytic';
-cfg.correctm  = 'bonferoni';
+cfg.correctm  = 'bonferroni';
 TFR_stat2     = ft_freqstatistics(cfg, TFR_logpow);
 cfg.method    = 'analytic';
 cfg.correctm  = 'fdr';
@@ -182,7 +182,7 @@ cfg.correctm  = 'no';
 ERF_stat1     = ft_timelockstatistics(cfg, ERF_all);
 
 cfg.method    = 'analytic';
-cfg.correctm  = 'bonferoni';
+cfg.correctm  = 'bonferroni';
 ERF_stat2     = ft_timelockstatistics(cfg, ERF_all);
 
 cfg.method    = 'analytic';
