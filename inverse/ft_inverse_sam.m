65e911e9c4aedc9fccd99073851287d1d93704d3
Malte <m_hoel20@uni-muenster.de>
2023-10-19 11:30:52 +0200

    merge 'spinning', 'gareth', and 'moiseev' cases
    The three fixedori-approaches 'spinning', 'gareth', and 'moiseev' all
    essentially perform the same procedure for orientation estimation. They
    only differ in whether the whole orientation or only its tangential
    component are estimated, and in how the noise covariance matrix is
    estimated. Hence they can be merged into a single approach. Furthermore,
    the tangential orientation in the 'spinning' case is now estimated from the
    leadfield, instead of by fitting a sphere to the headmodel.

--- inverse/ft_inverse_sam.m
+++ inverse/ft_inverse_sam.m
@@ -15,11 +15,15 @@ function [estimate] = ft_inverse_sam(sourcemodel, sens, headmodel, dat, C, varar
 %   estimate    contains the estimated source parameters
 %
 % Additional input arguments should be specified as key-value pairs and can include
-%   'meansphereorigin'
 %   'feedback'
-%   'fixedori'
+%   'fixedori'          deprecated, control behaviour via 'reducerank' instead
 %   'noisecov'
 %   'toi'
+%
+% If no orientation is specified, the SAM beamformer will try to estimate the orientation from the data.
+% The beamformer will either try to estimate the whole orientation, or only its tangential component.
+% This is controlled by the 'reducerank' parameter. For reducerank=3, the whole orientation is estimated,
+% and for reducerank=2 only the tangential component is estimated.
 %
 % These options influence the forward computation of the leadfield
 %   'reducerank'      = 'no' or number  (default = 3 for EEG, 2 for MEG)
@@ -43,9 +47,8 @@ if mod(nargin-5,2)
 end
 
 % get the optional input arguments, or use defaults
-meansphereorigin  = ft_getopt(varargin, 'meansphereorigin');
 feedback          = ft_getopt(varargin, 'feedback', 'text');
-fixedori          = ft_getopt(varargin, 'fixedori', 'spinning');
+fixedori          = ft_getopt(varargin, 'fixedori');
 noisecov          = ft_getopt(varargin, 'noisecov');
 toi               = ft_getopt(varargin, 'toi');
 
@@ -56,50 +59,19 @@ invopt = ft_setopt(invopt, 'kappa',     ft_getopt(varargin, 'kappa'));
 invopt = ft_setopt(invopt, 'tolerance', ft_getopt(varargin, 'tol'));
 invopt = ft_setopt(invopt, 'method',    ft_getopt(varargin, 'invmethod'));
 
-% construct the low-level options for the leadfield computation as key-value pairs, these are passed to FT_COMPUTE_LEADFIELD
-leadfieldopt = {};
-leadfieldopt = ft_setopt(leadfieldopt, 'reducerank',     ft_getopt(varargin, 'reducerank'));
-leadfieldopt = ft_setopt(leadfieldopt, 'backproject',    ft_getopt(varargin, 'backproject'));
-leadfieldopt = ft_setopt(leadfieldopt, 'normalize',      ft_getopt(varargin, 'normalize'));
-leadfieldopt = ft_setopt(leadfieldopt, 'normalizeparam', ft_getopt(varargin, 'normalizeparam'));
-leadfieldopt = ft_setopt(leadfieldopt, 'weight',         ft_getopt(varargin, 'weight'));
-
-switch fixedori
-  case 'robert'
-    ft_error('fixedori=''robert'' is not supported anymore. The exact same functionality is implemented in ft_inverse_lcmv, using fixedori = ''yes''');
-  case 'gareth'
-    % placeholder: give some user feedback, either error or not, to be discussed, depends on whether it will be merged
-  case 'moiseev'
-    % placeholder: give some user feedback, either error or not, to be discussed, depends on whether it will/can be merged
-    % (apart from the toi functionality)
-    if isempty(noisecov)
-      ft_error('fixedori method ''moiseev'' requires a noise covariance matrix to be specified');
-    end
-  case 'spinning'
-    ft_info('fixedori=''spinning'' has been adjusted to provide an exact estimate of the optimal orientation, see the code for details');
-    leadfieldopt = ft_setopt(leadfieldopt, 'backproject', 'no');
-
-
-  otherwise
-    ft_error('unsupported value for fixedori option');
-end
-
-if strcmp(fixedori, 'spinning')
-  % determine the mean sphere origin, required for spinning
-  % FIXME this currently does not work with precomputed leadfields, since
-  % the recent updates to ft_sourceanalysis throw away the headmodel in
-  % that case
-  if isempty(meansphereorigin)
-    switch ft_headmodeltype(headmodel)
-      case 'singlesphere'
-        meansphereorigin = headmodel.o;
-      case 'localspheres'
-        meansphereorigin = mean(headmodel.o, 1);
-      case 'singleshell'
-        meansphereorigin = mean(headmodel.bnd.pos,1);
-      otherwise
-        ft_error('unsupported headmodel type for determining the mean sphere origin')
-    end
+% backwards compatibility information
+if ~isempty(fixedori)
+  switch fixedori
+    case 'robert'
+      ft_error('fixedori=''robert'' is not supported anymore. The exact same functionality is implemented in ft_inverse_lcmv, using fixedori = ''yes''');
+    case 'gareth'
+      ft_warning('fixedori=''gareth'' is deprecated. Using a default noise covariance matrix, or specifying a noise covariance matrix of the form sigma * I, will reproduce the old ''gareth''-behaviour');
+    case 'moiseev'
+      ft_warning('fixedori=''moiseev'' is deprecated. This is now the default behaviour.');
+    case 'spinning'
+      ft_warning('fixedori=''spinning'' is deprecated. To estimate tangential orientations, please specify ''reducerank''=2, see the code for details.');
+    otherwise
+      ft_error('unknown value for fixedori option. Additionally, note that the parameter ''fixedori'' is deprecated.');
   end
 end
 
@@ -130,7 +102,7 @@ origpos    = sourcemodel.pos;
 sourcemodel.pos = sourcemodel.pos(originside,:);
 
 if hasmom
-  ft_warning('a prespecified mom in the sourcemodel will probably not work because of the spinning/nonspinning source orientation estimate being part of this code');
+  ft_warning('using user-specified moments, no data-based estimation of orientation is performed');
   sourcemodel.mom = sourcemodel.mom(:, sourcemodel.inside);
 end
 
@@ -144,23 +116,51 @@ elseif hasleadfield
   % check that LF backprojection is not used
   lfdim  = cellfun('size', sourcemodel.leadfield, 2);
   lfrank = cellfun(@rank,  sourcemodel.leadfield);
-  if ~strcmp(fixedori, 'spinning') && any(lfdim > lfrank)
-    % case analytical method used, check that LF are full rank or remove it
-    ft_warning('SAM with one of the analytical methods for dipole orientation estimation does not support backprojected leadfields\n');
-    
-    for i=1:numel(sourcemodel.leadfield)
-      lf      = sourcemodel.leadfield{i};
-      [U,S,V] = svd(lf, 'econ');
-      sourcemodel.leadfield{i} = lf*V(:,1:2);
-    end
+  if any(lfdim > lfrank)
+    % if LF was backprojected, we perform the orientation estimation only in the tangential plane
+    ft_warning('at least one of the supplied leadfields does not have full rank, falling back to tangential orientation estimation\n');
+    istangentialestimation = true;
+  else
+    istangentialestimation = false;
   end
 else
   ft_info('computing forward model on the fly\n');
-end
 
-if strcmp(fixedori, 'moiseev')  && ~isempty(toi)
-  ft_info('computing an event-related SAM beamformer... \n')
-end
+   % construct the low-level options for the leadfield computation as key-value pairs, these are passed to FT_COMPUTE_LEADFIELD
+  leadfieldopt = {};
+  leadfieldopt = ft_setopt(leadfieldopt, 'normalize',      ft_getopt(varargin, 'normalize'));
+  leadfieldopt = ft_setopt(leadfieldopt, 'normalizeparam', ft_getopt(varargin, 'normalizeparam'));
+  leadfieldopt = ft_setopt(leadfieldopt, 'weight',         ft_getopt(varargin, 'weight'));
+  
+  % check if a tangential orientation estimation is performed, i.e. if only orientations in the tangential plane are considered.
+  % if this is the case, we perform the lead field rank reduction directly in  this script, since otherwise we would not
+  % have access to the tangential plane defined by the full leadfield
+  reducerank = ft_getopt(varargin, 'reducerank');
+  if isempty(reducerank)
+    if ft_senstype(sens, 'meg')
+      istangentialestimation = true;
+    elseif ft_senstype(sens, 'eeg')
+      istangentialestimation = false;
+    else
+      ft_error('unknown sensor type');
+    end
+  else
+    if (ischar(reducerank) && strcmp(reducerank, 'yes')) || (isnumeric(reducerank) && reducerank == 2)
+      istangentialestimation = true;
+    elseif (ischar(reducerank) && strcmp(reducerank, 'no')) || (isnumeric(reducerank) && reducerank == 3)
+      istangentialestimation = false;
+    else
+      ft_error('invalid option for reducerank, please specify one from {''yes'', ''no'', 2, 3}')
+    end
+  end % if isempty(reducerank) or not
+  leadfieldopt = ft_setopt(leadfieldopt, 'reducerank', 'no');
+  
+  % for singlesphere MEG headmodels, the leadfield of a radial dipole is zero. Hence it does not make sense to try to estimate radial orientation components in this case.
+  if strcmp(headmodel.type,'singlesphere') && ft_senstype(sens, 'meg') && ~istangentialestimation
+    ft_warning('singlesphere MEG approach without rank reduction specified. To avoid numerical difficulties, we override this.');
+    istangentialestimation = true;
+  end
+end % if hasfilter or not
 
 isrankdeficient = (rank(C)<size(C,1));
 
@@ -188,16 +188,40 @@ end
 
 % the inverse only has to be computed once for all dipoles
 invC = ft_inv(C, invopt{:});
+
+% If no orientation is specified, it is estimated from the data. This script implements two approaches
+% for this estimation, both based on choosing an orientation that maximizes some form of "pseudo-Z". Concretely, these two approaches maximize
+% 1): pseudo-Z = projected_signal_power / projected_noise_power = (w' C w) / (w' N w),
+% 2): event-related pseudo-Z = projected_evoked_activity_power / projected_noise_power = (w' Cavg w) / (w' N w), 
+% where w is the spatial filter, C is the covariance matrix, N is the noise covarianc matrix, and Cavg is the
+% second moment matrix of the averaged data over some time interval. For a detailled discussion
+% regarding these approaches, we refer to 
+% Moiseev et al., Application of multi-source minimum variance beamformers for reconstruction of correlated neural activity,
+% NeuroImage, Volume 58, Issue 2, 15 September 2011, Pages 481-496.
+% Concretely, 1) is a 1-dimensional version of the "MPZ" approach in that paper, and 2) is a 1-dimensional version of the "MER" approach
+% Note that w is of the form c * invC * l, where l is the leadfield vector and c is some scalar constant (and hence cancels in the quotients above).
+% Since the matrices C, Cavg, invC, and N do not depend on the source position, we can precompute the corresponding expressions here. 
+if ~isempty(toi)
+  % case 2) above
+  ft_info('computing an event-related SAM beamformer... \n')
+  Avg = dat(:,toi(1):toi(2));
+  Cavg  = Avg*Avg'/size(Avg,2);
+  signal_congruence = invC' * Cavg * invC;
+else
+  % case 1) above
+  signal_congruence = invC;
+end
+
 if isempty(noisecov)
+  ft_info('no noise covariance matrix supplied, using an estimate of the form noise_level * I'); 
   noisecov = noise * eye(size(C));
 end
+noise_congruence = invC' * noisecov * invC;
 
 % start the scanning
 ft_progress('init', feedback, 'scanning grid');
 for i=1:size(sourcemodel.pos,1)
   ft_progress(i/size(sourcemodel.pos,1), 'scanning grid %d/%d\n', i, size(sourcemodel.pos,1));
-  
-  vox_pos = sourcemodel.pos(i,:);
   
   if hasfilter
     filt = sourcemodel.filter{i};
@@ -206,107 +230,57 @@ for i=1:size(sourcemodel.pos,1)
     end
     
   else
-    if hasleadfield && hasmom && size(sourcemodel.mom, 1)==size(sourcemodel.leadfield{i}, 2)
-      % reuse the leadfield that was previously computed and project
-      lf = sourcemodel.leadfield{i} * sourcemodel.mom(:,i);
-    elseif  hasleadfield &&  hasmom
-      % reuse the leadfield that was previously computed but don't project
-      lf = sourcemodel.leadfield{i};
-    elseif  hasleadfield && ~hasmom
-      % reuse the leadfield that was previously computed
-      lf = sourcemodel.leadfield{i};
-    elseif ~hasleadfield &&  hasmom
-      % compute the leadfield for a fixed dipole orientation
-      lf = ft_compute_leadfield(sourcemodel.pos(i,:), sens, headmodel, leadfieldopt{:}) * sourcemodel.mom(:,i);
+    %  we want to compute the spatial filter. For this, we need a leadfield vector. There are 3 options for this.
+    % 1): The leadfield vector is already specified.
+    % 2): A dipole moment is specified. The leadfield vector can then be optained via multiplication with the full n x 3 - leadfield.
+    % 3): Neither a leadfield vector nor a moment are specified. 
+    % In the case of 3), we derive the orientation from the data by choosing the orientation that maximizes the pseudo-Z score.
+    if hasleadfield && size(sourcemodel.leadfield{i}, 2) == 1
+      gain = sourcemodel.leadfield{i};
+    elseif hasmom
+      % try to project moment to leadfield vector
+      if hasleadfield
+        lf = sourcemodel.leadfield{i};
+      else
+        lf = ft_compute_leadfield(sourcemodel.pos(i,:), sens, headmodel, leadfieldopt{:});
+      end
+      
+      if size(sourcemodel.mom, 1) == size(lf, 2)
+        gain = lf * sourcemodel.mom(:, i);
+      else
+        ft_error('first dimension of field "mom" does not match second dimension of leadfield');
+      end
     else
-      % compute the leadfield
-      lf = ft_compute_leadfield(sourcemodel.pos(i,:), sens, headmodel, leadfieldopt{:});
-    end
-    
-    switch fixedori
-      case 'spinning'
-        % compute orientation in tangential plane so that the pseudo-Z score of the corresponding virtual sensor is maximized
-        [tanu, tanv] = calctangent(vox_pos - meansphereorigin); % get tangential components
-        O = [tanu(:), tanv(:)];
-        Y1 = O' * lf' * invC * C        * invC * lf * O;
-        Y2 = O' * lf' * invC * noisecov * invC * lf * O;
-        [U, S] = eig(Y1, Y2);
-        
-      case 'gareth'
-        % Compute Y1 = lf' R(^-1) * lf
-        % and     Y2 = lf' R(^-2) * lf
-        Y1 = lf' * invC * lf;
-        Y2 = lf' * (invC * invC) * lf;
-        % find the eigenvalues and eigenvectors
-        [U,S] = eig(Y1,Y2);
-        
-      case'moiseev'
-        if ~isempty(toi)
-          % use trial averaged variance matrix within a time of interest
-          Avg   = dat(:,toi(1):toi(2));
-          Cavg  = Avg*Avg'/size(Avg,2);
-          Sproj = invC * Cavg * invC';
-        else
-          % in case of induced data, just use the Signal cov only
-          Sproj = invC;
-        end
-        
-        Nproj = invC * noisecov * invC';
-        
-        Y1 = lf' * Sproj * lf;
-        Y2 = lf' * Nproj * lf;
-        % find the eigenvalues and eigenvectors
-        [U,S] = eig(Y1,Y2);
-        
-      otherwise
-        ft_error(sprintf('unknown fixedori method %s', fixedori));
-    end
-    
-    % convert the U matrix into the optimal orientation vector
-    switch fixedori
-      case 'spinning'
-        % project 2d orientation in tangential plane to 3d equivalent
-        [dum, ori_inx] = sort(diag(S), 'descend');
-        or_2d = U(:,ori_inx(1));
-        or_2d = or_2d / norm(or_2d);
-        opt_vox_or = O * or_2d;
-        
-      otherwise
-        % The optimum orientation is the eigenvector that corresponds to the
-        % biggest eigenvalue (biggest value is more logical, as it relates to SNR).
-        
-        if isfield(headmodel,'singlesphere') && ~strcmp(fixedori,'moiseev')
-          % If baseline noise covariance is not used, for single sphere head
-          % model, one of the eigenvectors corresponds to the radial direction,
-          % giving lead fields that are zero (to within machine precission).
-          % The eigenvalue corresponding to this eigenvector can actually be
-          % the biggest and can give the optimum (but wrong) Z-value!)
-          ori1 = U(:,1); ori1 = ori1/norm(ori1);
-          ori2 = U(:,2); ori2 = ori2/norm(ori2);
-          % ori3 = U(:,3); ori3 = ori3/norm(ori3);
-          
-          L1 = lf * ori1;
-          L2 = lf * ori2;
-          
-          if (norm(L1)/norm(L2)) < 1e-6
-            % the first orientation seems to be the silent orientation
-            % use the second orientation instead
-            opt_vox_or = ori2;
-          else
-            opt_vox_or = ori1;
-          end
-          
-        else
-          % select eigenvector with biggest eigenvalue
-          [dum, ori_inx] = sort(diag(S), 'descend');
-          ori = U(:,ori_inx(1));
-          opt_vox_or = ori/norm(ori);
-        end
-    end
-    estimate.ori{i} = opt_vox_or;
-    
-    % compute the spatial filter for the optimal source orientation
-    gain        = lf * opt_vox_or;
+      % estimate orientation from data
+      if hasleadfield
+        lf = sourcemodel.leadfield{i};
+      else
+        lf = ft_compute_leadfield(sourcemodel.pos(i,:), sens, headmodel, leadfieldopt{:});
+      end
+
+      if istangentialestimation
+        [dum_U, dum_S, V_lf] = svd(lf, 'econ');
+        lf = lf * V_lf(:, 1:2);
+      end
+
+      projected_signal = lf' * signal_congruence * lf;
+      projected_noise = lf' * noise_congruence * lf;
+      [U, S] = eig(projected_signal, projected_noise);
+      [dum, ori_inx] = sort(diag(S), 'descend');
+
+      max_ori = U(:, ori_inx(1));
+      max_ori = max_ori / norm(max_ori);
+
+      gain = lf * max_ori;
+
+      if istangentialestimation
+        estimate.tangential_ori{i} = V_lf(:, 1:2) * max_ori;
+      else
+        estimate.ori{i} = max_ori;
+      end
+    end % if hasleadfield or not
+
+    % compute the spatial filter
     trgain_invC = gain' * invC;
     filt  = trgain_invC / (trgain_invC * gain);
     
@@ -319,20 +293,11 @@ for i=1:size(sourcemodel.pos,1)
   if ~isempty(dat)
     estimate.mom{i}  = filt * dat;
   end
-  if strcmp(fixedori,'moiseev') && exist('gain', 'var')
-    % get pseudoZ
-    Ng                  = gain' * Nproj * gain;
-    Sg                  = gain' * Sproj * gain;
-    estimate.pseudoZ(i) = Sg / Ng;
-  elseif strcmp(fixedori, 'spinning')
-    tangential_projection = O' * opt_vox_or;
-    if tangential_projection(2) < 0
-      tangential_projection = -tangential_projection;
-    end
-    angle = atan2(tangential_projection(2), tangential_projection(1));
-    estimate.pseudoZ(i) = 1 / power(SAM_costfun(angle, vox_pos, O(:, 1), O(:, 2), lf, C, invC, noisecov), 2);
-  end
-  
+  if ~isempty(toi)
+    estimate.pseudoZ(i) = (filt * Cavg * filt') / estimate.noise(i);
+  else
+    estimate.pseudoZ(i) = estimate.pow(i) / estimate.noise(i);
+  end 
 end % for each dipole position
 ft_progress('close');
 
@@ -354,6 +319,10 @@ end
 if isfield(estimate, 'ori')
   estimate.ori( originside) = estimate.ori;
   estimate.ori(~originside) = {[]};
+end
+if isfield(estimate, 'tangential_ori')
+  estimate.tangential_ori( originside) = estimate.tangential_ori;
+  estimate.tangential_ori(~originside) = {[]};
 end
 if isfield(estimate, 'pow')
   estimate.pow( originside) = estimate.pow;
