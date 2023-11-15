3eb63a59aba944f9ab9bb949c2d303ef094b794a
NirOfir <43925388+NirOfir@users.noreply.github.com>
2023-11-15 13:51:42 +0200

    Update ft_plot_layout doc
    Filled in missing lines and tried to make the doc more helpful generally.

--- plotting/ft_plot_layout.m
+++ plotting/ft_plot_layout.m
@@ -7,26 +7,31 @@ function ft_plot_layout(layout, varargin)
 % where the layout is a FieldTrip structure obtained from FT_PREPARE_LAYOUT.
 %
 % Additional options should be specified in key-value pairs and can be
-%   'chanindx'    = list of channels to plot (default is all)
-%   'point'       = yes/no
-%   'box'         = yes/no
-%   'label'       = yes/no
+%   'chanindx'    = logical vector or vecotr of indices. Which channels to plot (default is all)
+%   'point'       = 'yes' or 'no' (default 'yes'), plot markers for sensors, comment and scale
+%   'box'         = 'yes' or 'no' (default 'yes'), plot boxes around the sensors, comment and scale
+%   'label'       = 'yes' or 'no' (default 'yes'), plot the labels of the sensors, comment and scale
 %   'labeloffset' = offset of label from point (default = 0)
-%   'labelrotate' = scalar, vector with rotation angle (in degrees) per label (default = 0)
-%   'labelalignh' = string, or cell-array specifying the horizontal alignment of the text (default = 'center')
-%   'labelalignv' = string, or cell-array specifying the vertical alignment of the text (default = 'middle')
-%   'mask'        = yes/no
-%   'outline'     = yes/no
-%   'verbose'     = yes/no
-%   'pointsymbol' = string with symbol (e.g. 'o') - all three point options need to be used together
-%   'pointcolor'  = string with color (e.g. 'k')
-%   'pointsize'   = number indicating size (e.g. 8)
-%   'fontcolor'   = string, color specification (default = 'k')
-%   'fontsize'    = number, sets the size of the text (default = 10)
-%   'fontunits'   =
-%   'fontname'    =
-%   'fontweight'  =
-%   'interpreter' = string, 'none', 'tex' or 'latex'
+%   'labelrotate' = scalar or vector with rotation angle (in degrees) per label (default = 0)
+%   'labelalignh' = string or cell-array specifying the horizontal alignment of the text (default = 'center')
+%   'labelalignv' = string or cell-array specifying the vertical alignment of the text (default = 'middle')
+%   'mask'        = 'yes' or 'no' (default 'yes'), plot the interpolation area of the layout
+%   'outline'     = 'yes' or 'no' (default 'yes'), plot the outline of the layout (e.g. head and MEG helmet)
+%   'verbose'     = 'yes' or 'no' (default 'no'), print explanation of the figure to command window
+%   'fontcolor'   = string, text color specification (default = 'k')
+%   'fontsize'    = scalar, sets the size of the text (default = 10)
+%   'fontunits'   = string, units of the font size (default is the Matlab's session default)
+%   'fontname'    = string, font name (default is the Matlab's session default)
+%   'fontweight'  = scalar, sets the size of the text (default = 10)
+%   'interpreter' = string, 'none', 'tex' or 'latex' (default = 'tex')
+%
+% The following options control the markers of the sensors. If any is defined, the other two must be defined as well.
+% Further note that if 'chanindx' is used, the number of elements in each choice should correspond to the original 
+% labels in the layout, and not to the chosen subset.
+%   'pointsymbol' = string with symbol (e.g. 'o' or 'oooxxx')
+%   'pointcolor'  = string with color (e.g. 'k'), or an NX3 matrix of RGB values
+%   'pointsize'   = scalar or vector for marker size
+% The default marker is a blue dot sorrunded by a yellow circle.
 %
 % It is possible to plot the object in a local pseudo-axis (c.f. subplot), which is specfied as follows
 %   'hpos'        = horizontal position of the lower left corner of the local axes
