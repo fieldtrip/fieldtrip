292d8683d343198a9b207272d72e677c0b748f23
Robert Oostenveld <r.oostenveld@gmail.com>
2024-01-18 19:51:12 +0100

    make sure that cfg.latency and cfg.frequency are not passed onto topoplot

--- ft_multiplotER.m
+++ ft_multiplotER.m
@@ -364,8 +364,8 @@ else
 end
 tmpvar = varargin{1};
 [varargin{:}] = ft_selectdata(tmpcfg, varargin{:});
-% restore the provenance information
-[cfg, varargin{:}] = rollback_provenance(cfg, varargin{:});
+% restore the provenance information, don't keep the ft_selectdata details
+[tmpcfg, varargin{:}] = rollback_provenance(cfg, varargin{:});
 
 if isfield(tmpvar, cfg.maskparameter) && ~isfield(varargin{1}, cfg.maskparameter)
   % the mask parameter is not present after ft_selectdata, because it is
@@ -764,12 +764,13 @@ if ~isempty(range)
   range([1 2]) = polyval(p, range([1 2]));
 
   cfg = removefields(cfg, 'inputfile');   % the reading has already been done and varargin contains the data
-  cfg = removefields(cfg, 'showlabels');  % this is not allowed in topoplotER
+  cfg = removefields(cfg, 'showlabels');  % this is not allowed in ft_singleplotER and ft_topoplotER
+  cfg = removefields(cfg, {'latency', 'frequency'});  % this should be xlim in ft_singleplotER and ft_topoplotER
   cfg.baseline = 'no';                    % make sure the next function does not apply a baseline correction again
   cfg.dataname = info.(ident).dataname;   % put data name in here, this cannot be resolved by other means
   cfg.channel = 'all';                    % make sure the topo displays all channels, not just the ones in this singleplot
-  cfg.comment = 'auto';
   cfg.trials = 'all';                     % trial selection has already been taken care of
+  cfg.comment = 'auto';
   cfg.xlim = range(1:2);
   % if user specified a ylim, copy it over to the zlim of topoplot
   if isfield(cfg, 'ylim')
