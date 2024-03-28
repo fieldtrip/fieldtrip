b77b185da8c2459773e9b4b0b77dddc534307f2f
Robert Oostenveld <r.oostenveld@gmail.com>
2024-03-28 09:57:36 +0100

    DOC - improve the help of reading functions

--- fileio/private/unicorn_csv.m
+++ fileio/private/unicorn_csv.m
@@ -9,9 +9,10 @@ function varargout = unicorn_csv(filename, hdr, begsample, endsample, chanindx)
 %   dat = unicorn_csv(filename, hdr, begsample, endsample, chanindx);
 %   evt = unicorn_csv(filename, hdr);
 %
-% See also FT_FILETYPE, FT_READ_HEADER, FT_READ_DATA, FT_READ_EVENT, MOTION_C3D, QUALISYS_TSV, LIBERTY_CSV
+% See also FT_FILETYPE, FT_READ_HEADER, FT_READ_DATA, FT_READ_EVENT
+% See also BIDS_TSV, BIOPAC_ACQ, BUCN_TXT, EEGSYNTH_TSV, EVENTS_TSV, LIBERTY_CSV, MAUS_TEXTGRID, MOTION_C3D, OPENBCI_TXT, OPENPOSE_KEYPOINTS, OPENSIGNALS_TXT, OPENVIBE_MAT, OPM_FIL, QUALISYS_TSV, SCCN_XDF, SENSYS_CSV, SNIRF, UNICORN_CSV, XSENS_MVNX
 
-% Copyright (C) 2022, Robert Oostenveld
+% Copyright (C) 2022-2024, Robert Oostenveld
 %
 % This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
 % for the documentation and details.
