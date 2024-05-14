7bdb9667828dbf42a47fa98eab48ee47bfee60ef
Jan-Mathijs Schoffelen <1517611+schoffelen@users.noreply.github.com>
2024-05-14 10:17:11 +0200

    ENH - added placeholder file

---
+++ test/test_pull2413.m
@@ -0,0 +1,28 @@
+function test_pull2413
+
+% MEM 1gb
+% WALLTIME 00:10:00
+% DEPENDENCY ft_read_event
+% DATA private
+
+%%
+
+mshdir = dccnpath('/home/common/matlab/fieldtrip/data/test/pull2183/');
+
+d = dir(fullfile(mshdir, '*.msh'));
+for k = 1:numel(d)
+  hs = ft_read_headshape(fullfile(d(k).folder, d(k).name));
+end
+
+%%
+
+filename = dccnpath('/home/common/matlab/fieldtrip/data/test/original/simnibs/v4/ernie.msh');
+hm = ft_read_headmodel(filename, 'fileformat', 'simnibs');
+hm = ft_read_headmodel(filename, 'fileformat', 'simnibs', 'meshtype', 'surface');
+
+%%
+
+filename = dccnpath('/home/common/matlab/fieldtrip/data/test/original/simnibs/v3/ernie_v3.msh');
+hm = ft_read_headmodel(filename, 'fileformat', 'simnibs');
+hm = ft_read_headmodel(filename, 'fileformat', 'simnibs', 'meshtype', 'surface');
+
