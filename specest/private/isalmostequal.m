7cd842077776eb1896f7e87477b9cd4eccc11710
Robert Oostenveld <r.oostenveld@gmail.com>
2025-01-06 10:14:47 +0100

    automatically synchronized identical files to f6b4660e42e8a984391feb9d47837800d0e32626

--- specest/private/isalmostequal.m
+++ specest/private/isalmostequal.m
@@ -21,6 +21,22 @@ function [ok, message] = isalmostequal(a, b, varargin)
 
 % Copyright (C) 2004-2012, Robert Oostenveld & Markus Siegel
 %
+% This file is part of FieldTrip, see http://www.fieldtriptoolbox.org
+% for the documentation and details.
+%
+%    FieldTrip is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    FieldTrip is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
+%
 % $Id$
 
 if nargin==3
