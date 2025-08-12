a60365fa2026adf4b457f2830d1ceb26b0a77a32
Marcel Zwiers <mpzwiers@gmail.com>
2025-08-12 09:55:10 +0200

    Use batch mode to avoid writing huge amount of logfiles (#2517)
    * Use batch mode to avoid writing hue amount of logfiles
    
    qsubfeval uses the `-r` option to execute the job-function. As mentioned in this thread (https://nl.mathworks.com/matlabcentral/answers/2121156-disable-matlabconnector-on-linux-cluster?s_tid=mlc_ans_email_view#answer_1568384) it is recommended to use the `-batch` option instead, so that the new Matlab version don't write enormous amounts of logfiles. I am not aware of people needing to run qsubfeval to run jobs interactively, but otherwise this commit need to be changed such that it becomes an input option.
    
    See also: https://nl.mathworks.com/help/matlab/matlab_env/commonly-used-startup-options.html
    
    Run the specified statement non-interactively.
    -batch "statement"  
    
    Run the specified statement interactively.
    -r "statement"
    
    * ENH - added matlab version dependent flag -r/-batch
    
    * missed a few occurrences
    
    ---------
    
    Co-authored-by: Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>

--- qsub/qsubfeval.m
+++ qsub/qsubfeval.m
@@ -117,6 +117,13 @@ if ~isempty(optbeg)
   varargin = varargin(1:(optbeg-1));
 end
 
+% as of matlab R2019a the -batch is a flag to be preferred over -r if running in non-interactive mode
+if ft_platform_supports('matlabversion', -inf, '2018b')
+  batchflag = '-r';
+else
+  batchflag = '-batch';
+end
+
 if isempty(backend)
   % use the system default backend
   backend = defaultbackend;
@@ -281,7 +288,7 @@ switch backend
       cmdline = sprintf('%s %s %s', compiledfun, matlabroot, jobid);
     else
       % create the shell commands to execute matlab
-      cmdline = sprintf('%s -r "%s"', matlabcmd, matlabscript);
+      cmdline = sprintf('%s %s "%s"', matlabcmd, batchflag, matlabscript);
     end
 
   case 'sge'
@@ -309,7 +316,7 @@ switch backend
       cmdline = sprintf('%s %s %s', compiledfun, matlabroot, jobid);
     else
       % create the shell commands to execute matlab
-      cmdline = sprintf('%s -r \\"%s\\"', matlabcmd, matlabscript);
+      cmdline = sprintf('%s %s \\"%s\\"', matlabcmd, batchflag, matlabscript);
     end
 
     % pass the command to qsub with all requirements
@@ -357,7 +364,7 @@ switch backend
       cmdline = sprintf('%s %s %s', compiledfun, matlabroot, jobid);
     else
       % create the shell commands to execute matlab
-      cmdline = sprintf('%s -r \\"%s\\"', matlabcmd, matlabscript);
+      cmdline = sprintf('%s %s \\"%s\\"', matlabcmd, batchflag, matlabscript);
     end
 
     if any(curPwd==' ')
@@ -397,7 +404,7 @@ switch backend
       % create the command line for the compiled application
       cmdline = sprintf('%s %s %s', compiledfun, matlabroot, jobid);
     else
-      cmdline = sprintf('%s -r \\"%s\\"', matlabcmd, matlabscript);
+      cmdline = sprintf('%s %s \\"%s\\"', matlabcmd, batchflag, matlabscript);
     end
     cmdline = sprintf('sbatch --parsable --job-name=%s %s --output=%s --error=%s --wrap "%s"', ...
                        jobid, submitoptions, logout, logerr, cmdline);
@@ -413,7 +420,7 @@ switch backend
     fprintf(fid, '# Condor submit script\n');
     fprintf(fid, '\n');
     fprintf(fid, 'Executable     = %s\n', matlabcmd);
-    fprintf(fid, 'Arguments      = -r "%s"\n', matlabscript);
+    fprintf(fid, 'Arguments      = %s "%s"\n', batchflag, matlabscript);
     % the timreq and memrequ should be inserted here
     fprintf(fid, 'Requirements   = Memory >= 32 && OpSys == "LINUX" && Arch =="INTEL"\n');
     fprintf(fid, 'Rank           = Memory >= 64\n');
@@ -459,7 +466,7 @@ switch backend
       cmdline = sprintf('%s %s %s', compiledfun, matlabroot, jobid);
     else
       % create the shell commands to execute matlab
-      cmdline = sprintf('%s -r \\"%s\\"', matlabcmd, matlabscript);
+      cmdline = sprintf('%s %s \\"%s\\"', matlabcmd, batchflag, matlabscript);
     end
 
     % pass the command to qsub with all requirements
