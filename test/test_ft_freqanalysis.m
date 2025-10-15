cb70c9c686e67e71f13755b60309ea5a9de83e64
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2025-10-15 15:59:30 +0200

    revamped the old test function, and moved from invalid to primetime

--- test/test_ft_freqanalysis.m
+++ test/test_ft_freqanalysis.m
@@ -1,26 +1,254 @@
-function test_ft_freqanalysis
+function test_ft_freqanalysis2(datainfo, writeflag, version, breakonerror)
 
-% MEM 1gb
-% WALLTIME 00:10:00
-% DEPENDENCY ft_freqanalysis
-% DATA no
+% MEM 6gb
+% WALLTIME 01:30:00
+% DEPENDENCY ft_freqanalysis ref_datasets
+% DATA private
 
-ntrl = 100;
-nchan = 32;
-fs = 500;
-start_time = -1; % seconds
-end_time = 2.5 - 1./500; % seconds
-nsamples = (end_time - start_time) * fs + 1;
+% writeflag determines whether the output should be saved to disk
+% version determines the output directory
 
-data = [];
-data.label = cellstr(num2str((1:nchan).'));
-for i=1:ntrl
-  data.time{i} = linspace(start_time, end_time, nsamples);
-  data.trial{i} = randn(nchan,nsamples);
+if nargin<1 || isempty(datainfo)
+  datainfo = ref_datasets;
+end
+if nargin<2 || isempty(writeflag)
+  writeflag = 0;
+end
+if nargin<3 || isempty(version)
+  version = 'latest';
+end
+if nargin<4
+  breakonerror = true;
 end
 
-cfg = [];
-cfg.method = 'mtmfft';
-cfg.taper = 'hanning';
-cfg.foilim = [4 30];
-freq = ft_freqanalysis(cfg, data);
+for k = 1:numel(datainfo)
+  datanew = freqanalysisMtmfft(datainfo(k), writeflag, version, 'fourier', 'yes');
+  fname   = fullfile(datainfo(k).origdir,version,'freq',datainfo(k).type,['freq_mtmfft_fourier_trl_',datainfo(k).datatype]);
+  load(fname); datanew = rmfield(datanew, 'cfg'); freq = rmfield(freq, 'cfg');
+  [ok,msg] = isalmostequal(freq, datanew, 'reltol', 1e-3);
+  if ~ok && breakonerror
+    error('stored and computed data not identical: %s\n', msg{:});
+  elseif ~ok
+    warning('stored and computed data not identical: %s\n', msg{:});
+  end
+  
+  datanew = freqanalysisMtmfft(datainfo(k), writeflag, version, 'powandcsd', 'yes');
+  fname   = fullfile(datainfo(k).origdir,version,'freq',datainfo(k).type,['freq_mtmfft_powandcsd_trl_',datainfo(k).datatype]);
+  load(fname); datanew = rmfield(datanew, 'cfg'); freq = rmfield(freq, 'cfg');
+  [ok,msg] = isalmostequal(freq, datanew, 'reltol', 1e-3);
+  if ~ok && breakonerror
+    error('stored and computed data not identical: %s\n', msg{:});
+  elseif ~ok
+    warning('stored and computed data not identical: %s\n', msg{:});
+  end
+ 
+  datanew = freqanalysisMtmfft(datainfo(k), writeflag, version, 'powandcsd', 'no');
+  fname   = fullfile(datainfo(k).origdir,version,'freq',datainfo(k).type,['freq_mtmfft_powandcsd_',datainfo(k).datatype]);
+  load(fname); datanew = rmfield(datanew, 'cfg'); freq = rmfield(freq, 'cfg');
+  [ok,msg] = isalmostequal(freq, datanew, 'reltol', 1e-3);
+  if ~ok && breakonerror
+    error('stored and computed data not identical: %s\n', msg{:});
+  elseif ~ok
+    warning('stored and computed data not identical: %s\n', msg{:});
+  end
+ 
+  datanew = freqanalysisMtmfft(datainfo(k), writeflag, version, 'pow', 'no');
+  fname = fullfile(datainfo(k).origdir,version,'freq',datainfo(k).type,['freq_mtmfft_',datainfo(k).datatype]);
+  load(fname); datanew = rmfield(datanew, 'cfg'); freq = rmfield(freq, 'cfg');
+  [ok,msg] = isalmostequal(freq, datanew,'reltol',1e-3);
+  if ~ok && breakonerror
+    error('stored and computed data not identical: %s\n', msg{:});
+  elseif ~ok
+    warning('stored and computed data not identical: %s\n', msg{:});
+  end
+ end
+
+for k = 1:numel(datainfo)
+  datanew = freqanalysisMtmconvol(datainfo(k), writeflag, version, 'fourier', 'yes');
+  fname   = fullfile(datainfo(k).origdir,version,'freq',datainfo(k).type,['freq_mtmconvol_fourier_trl_',datainfo(k).datatype]);
+  load(fname); datanew = rmfield(datanew, 'cfg'); freq = rmfield(freq, 'cfg');
+  [ok,msg] = isalmostequal(freq, datanew, 'reltol', 1e-3);
+  if ~ok && breakonerror
+    error('stored and computed data not identical: %s\n', msg{:});
+  elseif ~ok
+    warning('stored and computed data not identical: %s\n', msg{:});
+  end
+ 
+  datanew = freqanalysisMtmconvol(datainfo(k), writeflag, version, 'powandcsd', 'yes');
+  fname   = fullfile(datainfo(k).origdir,version,'freq',datainfo(k).type,['freq_mtmconvol_powandcsd_trl_',datainfo(k).datatype]);
+  load(fname); datanew = rmfield(datanew, 'cfg'); freq = rmfield(freq, 'cfg');
+  [ok,msg] = isalmostequal(freq, datanew, 'reltol', 1e-3);
+  if ~ok && breakonerror
+    error('stored and computed data not identical: %s\n', msg{:});
+  elseif ~ok
+    warning('stored and computed data not identical: %s\n', msg{:});
+  end
+ 
+  datanew = freqanalysisMtmconvol(datainfo(k), writeflag, version, 'powandcsd', 'no');
+  fname   = fullfile(datainfo(k).origdir,version,'freq',datainfo(k).type,['freq_mtmconvol_powandcsd_',datainfo(k).datatype]);
+  load(fname); datanew = rmfield(datanew, 'cfg'); freq = rmfield(freq, 'cfg');
+  [ok,msg] = isalmostequal(freq, datanew, 'reltol', 1e-3);
+  if ~ok && breakonerror
+    error('stored and computed data not identical: %s\n', msg{:});
+  elseif ~ok
+    warning('stored and computed data not identical: %s\n', msg{:});
+  end
+ 
+  datanew = freqanalysisMtmconvol(datainfo(k), writeflag, version, 'pow', 'no');
+  fname = fullfile(datainfo(k).origdir,version,'freq',datainfo(k).type,['freq_mtmconvol_',datainfo(k).datatype]);
+  load(fname); datanew = rmfield(datanew, 'cfg'); freq = rmfield(freq, 'cfg');
+  [ok,msg] = isalmostequal(freq, datanew,'reltol', 1e-3);
+  if ~ok && breakonerror
+    error('stored and computed data not identical: %s\n', msg{:});
+  elseif ~ok
+    warning('stored and computed data not identical: %s\n', msg{:});
+  end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [freq] = freqanalysisMtmconvol(dataset, writeflag, version, output, keeptrials)
+
+if isempty(output)
+  output = 'pow';
+  % output = 'powandcsd';
+  % output = 'fourier';
+end
+
+if isempty(keeptrials)
+  output = 'no';
+  % output = 'yes';
+end
+
+% the file names should distinguish between the cfg.output and cfg.keeptrials option
+postfix = '';
+switch output
+  case 'pow'
+    % don't change
+  case 'powandcsd'
+    postfix = [postfix 'powandcsd_'];
+  case 'fourier'
+    postfix = [postfix 'fourier_'];
+  otherwise
+    error('unexpected output');
+end
+
+% the file names should distinguish between the cfg.output and cfg.keeptrials option
+switch keeptrials
+  case 'no'
+    % don't change
+  case 'yes'
+    postfix = [postfix 'trl_'];
+  otherwise
+    error('unexpected keeptrials');
+end
+
+% --- HISTORICAL --- attempt forward compatibility with function handles
+if ~exist('ft_freqanalysis') && exist('freqanalysis')
+  eval('ft_freqanalysis = @freqanalysis;');
+end
+
+fprintf('testing mtmconvol with datatype=%s, output=%s, keeptrials=%s...\n',...
+  dataset.datatype, output, keeptrials);
+
+cfg            = [];
+cfg.method     = 'mtmconvol';
+cfg.output     = output;
+cfg.keeptrials = keeptrials;
+cfg.foi        = 2:2:30;
+cfg.taper      = 'hanning';
+cfg.t_ftimwin  = ones(1,numel(cfg.foi)).*0.5;
+cfg.toi        = (250:50:750)./1000;
+cfg.polyremoval= 0;
+cfg.inputfile = fullfile(dataset.origdir,version,'raw',dataset.type,['preproc_',dataset.datatype]);
+if writeflag
+  cfg.outputfile = fullfile(dataset.origdir,version,'freq',dataset.type,['freq_mtmconvol_',postfix,dataset.datatype]);
+end
+if contains(output, 'csd')
+  load(cfg.inputfile);
+  cfg.channelcmb = [data.label repmat(data.label(3), [length(data.label), 1])];
+end
+
+
+if ~strcmp(version, 'latest') && str2num(version)<20100000
+  % -- HISTORICAL --- older FieldTrip versions don't support inputfile and outputfile
+  load(cfg.inputfile, 'data');
+  freq = ft_freqanalysis(cfg, data);
+  save(cfg.outputfile, 'freq');
+else
+  freq = ft_freqanalysis(cfg);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SUBFUNCTION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [freq] = freqanalysisMtmfft(dataset, writeflag, version, output, keeptrials)
+
+% --- HISTORICAL --- attempt forward compatibility with function handles
+if ~exist('ft_freqanalysis') && exist('freqanalysis')
+  eval('ft_freqanalysis = @freqanalysis;');
+end
+
+if isempty(output)
+  output = 'pow';
+  % output = 'powandcsd';
+  % output = 'fourier';
+end
+
+if isempty(keeptrials)
+  keeptrials = 'no';
+  % keeptrials = 'yes';
+end
+
+% the file names should distinguish between the cfg.output and cfg.keeptrials option
+postfix = '';
+switch output
+  case 'pow'
+    % don't change
+  case 'powandcsd'
+    postfix = [postfix 'powandcsd_'];
+  case 'fourier'
+    postfix = [postfix 'fourier_'];
+  otherwise
+    error('unexpected output');
+end
+
+% the file names should distinguish between the cfg.output and cfg.keeptrials option
+switch keeptrials
+  case 'no'
+    % don't change
+  case 'yes'
+    postfix = [postfix 'trl_'];
+  otherwise
+    error('unexpected keeptrials');
+end
+
+fprintf('testing mtmfft with datatype=%s, output=%s, keeptrials=%s...\n',...
+  dataset.datatype, output, keeptrials);
+
+cfg            = [];
+cfg.method     = 'mtmfft';
+cfg.output     = output;
+cfg.keeptrials = keeptrials;
+cfg.foilim     = [0 100];
+cfg.taper      = 'hanning';
+cfg.polyremoval= 0;
+cfg.inputfile  = fullfile(dataset.origdir,version,'raw',dataset.type,['preproc_',dataset.datatype]);
+if writeflag
+  cfg.outputfile = fullfile(dataset.origdir,version,'freq',dataset.type,['freq_mtmfft_',postfix,dataset.datatype]);
+end
+if contains(output, 'csd')
+  load(cfg.inputfile);
+  cfg.channelcmb = [data.label repmat(data.label(3), [length(data.label), 1])];
+end
+
+if ~strcmp(version, 'latest') && str2num(version)<20100000
+  % -- HISTORICAL --- older FieldTrip versions don't support inputfile and outputfile
+  load(cfg.inputfile, 'data');
+  freq = ft_freqanalysis(cfg, data);
+  save(cfg.outputfile, 'freq');
+else
+  freq = ft_freqanalysis(cfg);
+end
+
