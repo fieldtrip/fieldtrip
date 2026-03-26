d419b3d5a39bd2c5c95e4e2ed176a8f55be1c409
Marcel Zwiers <mpzwiers@gmail.com>
2026-03-26 13:04:07 +0100

    Enable pretty printing for JSON error stack

--- qsub/qsubget.m
+++ qsub/qsubget.m
@@ -149,9 +149,9 @@ if completed
     if ischar(err)
       errmsg = err;
     elseif isstruct(err)
-      errmsg = sprintf('%s: %s\n%s', err.identifier, err.message, jsonencode(err.stack));
+      errmsg = sprintf('%s: %s\n%s', err.identifier, err.message, jsonencode(err.stack, 'PrettyPrint', true));
     else
-      errmsg = sprintf('%s: %s\n%s', err.identifier, err.message, jsonencode(err.stack));
+      errmsg = sprintf('%s: %s\n%s', err.identifier, err.message, jsonencode(err.stack, 'PrettyPrint', true));
       % convert the MEexception object into a structure to allow a rethrow further down in the code
       ws = warning('off', 'MATLAB:structOnObject');
       err = struct(err);
